394dfe80e3a301a2745211061b247c8572116312
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-04-29 13:28:03 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/warrens/a_captured_kerran.pl
+++ trunk/quests/warrens/a_captured_kerran.pl
@@ -1,6 +1,24 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("My spirrrit witherrrrs herrre in this dank. stinking. flea infested hole! I must be set frrree frrrom these shackles!!"); }
-}
-#END of FILE Zone:warrens  ID:101085 -- a_captured_kerran 
+###############################
+# NPC: A captured Kerran
+# Zone: The Warrens
+# Quest: Erudite Prisoners
+# Modified by RealityIncarnate
+###############################
 
+sub EVENT_SAY { 
+  if($text=~/Hail/i) {
+    quest::say("My spirrrit witherrrrs herrre in this dank, stinking, flea infested hole! I must be set frrree frrrom these shackles!!"); 
+  }
+}
+
+sub EVENT_ITEM {
+  if (plugin::check_handin(\%itemcount, 6923 => 1)) {
+    quest::say("I thank you for frrreeing me frrrom my dog face captorrrs.");
+    quest::depop();
+    quest::exp(300);
+  }
+  else {
+    plugin::return_items(\%itemcount);
+  }
+}
+#END of FILE Zone:warrens  ID:101085 -- a_captured_kerran

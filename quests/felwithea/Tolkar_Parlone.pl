7f39ca86689d4d4e3ab4067466aa47b4311a1113
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-05-04 18:49:22 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/felwithea/Tolkar_Parlone.pl
+++ quests/felwithea/Tolkar_Parlone.pl
@@ -5,5 +5,16 @@ quest::say("Hail. good adventurer!  I am afraid that my services are not availab
 if($text=~/who is your daughter/i){
 quest::say("My daughter's name is Lenara.  I have not seen her in several months. and I fear the worst.  She went off adventuring. even though I told her how dangerous it was."); }
 }
+
+sub EVENT_ITEM {
+  if(plugin::check_handin(\%itemcount, 5573 => 1)) { #folded note
+    quest::say("What's this? Oh my goodness! I was so worried I had lost my little girl. Thank you so much $name. Here please take this, it was once a magical cloak but its powers have diminished over time. If you take it to my wife in erudin I'm sure she can restore it.");
+    quest::summonitem(1056); #Faded cloak
+  }
+  else {
+    quest::say("I have no need for this, $name.");
+    plugin::return_items(\%itemcount);
+  }
+} 
 #END of FILE Zone:felwithea  ID:61017 -- Tolkar_Parlone 
 

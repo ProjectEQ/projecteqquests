35fca19aa41dca66a956fd822f6c6db4f3c277ab
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-05-05 13:55:04 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/powater/#Coirnav_the_Avatar_of_Water.pl
+++ quests/powater/#Coirnav_the_Avatar_of_Water.pl
@@ -1,4 +1,9 @@
-sub EVENT_DEATH {
-	quest::signalwith(216107,5,0);
-	quest::spawn2(216066,0,0,$x,$y,$z,$h);
-}
+sub EVENT_DEATH {
+
+	quest::signalwith(216107,5,0);
+        quest::say("Im working");
+
+	quest::spawn2(216066,0,0,$x,$y,$z,$h);
+
+}
+

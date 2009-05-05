cc3058e7fc9aa9ea8c8aac0ee7b252fcf7984e7c
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-05-05 17:01:11 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/thurgadinb/#Dain_Frostreaver_IV.pl
+++ trunk/quests/thurgadinb/#Dain_Frostreaver_IV.pl
@@ -21,3 +21,34 @@ sub EVENT_SAY {
     }
   }
 }
+
+sub EVENT_ITEM {
+#Handin for the 9th ring. Needs correct dialogue
+  if(plugin::check_handin(\%itemcount, 1500 => 1) && $faction == 1) {
+    quest::say("%t, you have done a great service to my people. I had not imagined the treachery had run so deeply within our ranks. Here. Take this ring as your reward. From this day forth, you shall be known as the Hero of the Coldain. Take my Dirk as well, and if you wish to further aid us in our cause, then return it to me.");
+    quest::summonitem(30369); #9th ring
+    quest::summonitem(1465); #dirk of the Dain
+    quest::faction(49,50); #coldain
+    quest::faction(67,50); #Dain
+    quest::faction(188,-50); #Kromrif
+    quest::faction(189,-50); #Kromzek
+    quest::exp(4000000);
+  }
+  #Tormax's head
+  elsif(plugin::check_handin(\%itemcount, 30516 => 1) && $faction == 1) {
+    quest::say("You have done what no Coldain could do, %t! This is indeed a glorious say in our people's history. In return for your invaluable service I present you with the Tri-plated Golden Hackle Hammer. Its magic is powerful and I am sure it will serve you well.");
+    quest::ze("Let it be know from this day forth that %t and their companions are Heros of the Coldain Kingdom. King Tormax has been slain, it is a time for celebration. Let no tankard go unfilled!");
+    quest::summonitem(30502);
+    quest::faction(49,100);
+    quest::faction(67,100);
+    quest::faction(179,-100);
+  }
+#Dirk handin for the 10th ring
+  elsif(plugin::check_handin(\%itemcount, 1465 => 1) && $faction == 1) {
+    quest::say("My good %t, you have served me well. You have flushed out all who sought to oppose me and my people. I am afraid I need to call upon you and your friends one final time. The dissention and treason ran deeper than I had anticipated. Our population has been cleansed, but we lost a full third of our army to the poisonous words of those rebels. In retaliation for your deeds, the Kromrif have made plans to attack us in this, our weakest hour. Can I count on your help outlander?");
+    quest::summonitem(1465);
+  }
+  else{
+    plugin::return_items(\%itemcount);
+  }
+}

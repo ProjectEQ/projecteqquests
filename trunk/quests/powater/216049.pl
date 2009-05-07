afcdefa3089f7ccd3f3ebc97aceb8d033769323b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-05-07 01:04:33 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/powater/216049.pl
+++ trunk/quests/powater/216049.pl
@@ -1,3 +1,12 @@
-sub EVENT_DEATH {
-	quest::signalwith(216107,1,0);
-}
+sub EVENT_SPAWN {
+        quest::delglobal(coirnav_done);
+        quest::say("boo!");
+
+}
+
+sub EVENT_DEATH {
+
+	quest::signalwith(216107,1,0);
+
+}
+

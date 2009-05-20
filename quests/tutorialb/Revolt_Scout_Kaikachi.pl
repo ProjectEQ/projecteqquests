14924bcd49599d0a84405569df9504630087e585
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-05-20 18:04:23 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/tutorialb/Revolt_Scout_Kaikachi.pl
+++ quests/tutorialb/Revolt_Scout_Kaikachi.pl
@@ -2,19 +2,14 @@
 #quest - Busted Locks
 
 sub EVENT_SAY {
-      if ($text=~ /Hail/i){
+     if ($text=~ /Hail/i){
          quest::say("Shhhh. We just swum up thro' da secret tunnel. Not even koboldies know it. Lead 'em back 'ere and we'll bash 'em good.");   
    quest::taskselector(20);
  }
 }
 
 sub EVENT_TASKACCEPTED {
-        if($task_id == "20") {
+     if($task_id == "20") {
                 quest::say("Most of da slaves who limp back ta dis cavern are still bound in chains. Our blades are dull from breaking all da kobolds bonds. We needs ya ta find da kobold locksmith and steals his key ring. Da smith iz an albino kobold dat spends most of 'is time near da Gloomingdeep Jail");
-        }
+ }
 }
-
-sub EVENT_ITEM {
-          quest::say("I don't need this."); #text made up
-          plugin::return_items(\%itemcount);
-     }

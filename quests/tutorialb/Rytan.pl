55514df6933409dccb7f366f63aa8b534f33ba46
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-05-21 05:56:49 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/tutorialb/Rytan.pl
+++ quests/tutorialb/Rytan.pl
@@ -1,12 +1,32 @@
 #zone: tutorialb
-#Revised Angelox
 
 sub EVENT_SAY{
   if ($text=~/hail/i){
-  quest::say("Hello. There are many things one can do with spells and you should take the time to learn them. If you wish I can [bless] you with the power of the gods using one such spell.");
-}
-elsif ($text=~/bless/i){
-   quest::say("Very well ......... there you have been blessed.");
-  quest::selfcast(5150);
-  }
+   quest::say("Hello $name. I'm not much of a fighter myself but I can bestow the power of my god's favor. Would you like to be [blessed]?");
+   }
+  if ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Warrior' || $class eq 'Rogue' || $class eq 'Monk' || $class eq 'Berserker')){
+   $npc->CastSpell(5150,$userid);
+   quest::updatetaskactivity(22,5);  
+   }
+  elsif ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Cleric' || $class eq 'Paladin' || $class eq 'Ranger' || $class eq 'Druid' || $class eq 'Shaman' || $class eq 'Beastlord' || $class eq 'Shadowknight')){
+   quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will fnd other powerful spells. Good luck out there!");
+   $npc->CastSpell(5150,$userid);
+   quest::updatetaskactivity(22,5); 
+   quest::summonitem(15213); 
+   }
+  elsif ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Necromancer' || $class eq 'Magician' || $class eq 'Wizard' || $class eq 'Enchanter')){
+   quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will fnd other powerful spells. Good luck out there!");
+   $npc->CastSpell(5150,$userid);
+   quest::updatetaskactivity(22,5); 
+   quest::summonitem(15205);
+   }
+  elsif ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Bard')){
+   quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will fnd other powerful spells. Good luck out there!");
+   $npc->CastSpell(5150,$userid);
+   quest::updatetaskactivity(22,5); 
+   quest::summonitem(15703);
+   }
+   elsif ($text=~/blessed/i){
+   $npc->CastSpell(5150,$userid);
+   }
 }
--- quests/tutorialb/Vahlara.pl
+++ quests/tutorialb/Vahlara.pl
@@ -4,30 +4,39 @@ sub EVENT_SAY {
    if (quest::istaskactivityactive(17,3)){
       quest::say("Well done. The revolt's food supply is safe for the time being. Report back to the guards when you're rested.");
    }
-      else {
+    else {
         if ($amote==2) { #Emote spawnwer "Vast"
              quest::spawn2(189119,0,0,-126.0,-295.8,3.0,124.8);
-      }
+         }
         if ($bmote==2) { #Emote spawner "Screams"
              quest::spawn2(189120,0,0,-385.2,-516.3,-39.3,135.1);
-        }
-        if ($text=~/hail/i) {
-         quest::say("I'm glad you managed to escape the slave warrens in one piece. There are many [others] who were not as lucky. I have something that might save you from the same fate. Take this kobold charm. It will increase in power as you talk to the rest of the members of the revolt. May it bring you good luck, $name.");
-         quest::summonitem(59943); #Kobold Skull Charm
+         }
+   elsif ($text=~/hail/i && quest::istaskactivityactive(22,2)){
+      quest::say("I'm glad you managed to escape the slave warrens in one piece. There are many [others] who were not as lucky. Here, take this burlap armor. It's not much, but it should keep you safe");
+      quest::summonitem(54224);
+      quest::updatetaskactivity(22,2);
+   }
+   #elsif ($text=~/hail/i && quest::istaskactivityactive(22,7)) {
+   #      quest::say("I'm glad you managed to escape the slave warrens in one piece. There are many [others] who were not as lucky. I have something that might save you from the same fate. Take this kobold charm. It will increase in power as you talk to the rest of the members of the revolt. May it bring you good luck, $name.");
+   #      quest::updatetaskactivity(22,7);
+   #   }
+   elsif ($text=~/hail/i){
+      quest::say("I'm glad you managed to escape the slave warrens in one piece. There are many [others] who were not as lucky.");
+   }
+   elsif ($text=~/others/i) {
+      quest::say("These few here. They were injured when the revolt broke out. I'm trying to help, but we have a short supply of [bandages], and more wounded keep coming. I fear many will not survive the hour.");
       }
-      if ($text=~/others/i) {
-         quest::say("These few here. They were injured when the revolt broke out. I'm trying to help, but we have a short supply of [bandages], and more wounded keep coming. I fear many will not survive the hour.");
-      }
-      if ($text=~/bandages/i) {
-         quest::say("I was a skilled tailor before I was enslaved, so I'm able to weave spider silk into makeshift gauzes and dressings. But now even the spiderling silk is running scarce. If you can find three pieces of spiderling silk, bring them to me unstacked, and I'll give you some armor I've crafted from burlap. Burlap's no good for bandages anyway.");
+   elsif ($text=~/bandages/i) {
+      quest::say("I was a skilled tailor before I was enslaved, so I'm able to weave spider silk into makeshift gauzes and dressings. But now even the spiderling silk is running scarce. If you can find three pieces of spiderling silk, bring them to me unstacked, and I'll give you some armor I've crafted from burlap. Burlap's no good for bandages anyway.");
   }     
+   
  }
 }
       
 sub EVENT_ITEM {
 if (plugin::check_handin(\%itemcount, 54234 => 1)) { #Gloomingdeep Silk
     quest::say("You have proven most resourceful $name.");
-quest::summonitem(quest::ChooseRandom(54217,54218,54219,54220,54221,54222,54223,54224,54225,54226,54227,54228)); #Random Stitched Burlap item
+    quest::summonitem(quest::ChooseRandom(54217,54218,54219,54220,54221,54222,54223,54224,54225,54226,54227,54228)); #Random Stitched Burlap item
   }
 
   else {

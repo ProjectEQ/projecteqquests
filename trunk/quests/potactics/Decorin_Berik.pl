d1b0e05b17e8c5b1bfef5459044fa0c4e8802701
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-01 04:06:38 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/potactics/Decorin_Berik.pl
+++ trunk/quests/potactics/Decorin_Berik.pl
@@ -1,10 +1,18 @@
 #spawn Vallon Zek
+
 sub EVENT_DEATH {
         quest::signalwith(214112, 214056, 0);
 }
 
 sub EVENT_SIGNAL {
         quest::depop();
+}
+
+
+sub EVENT_SPAWN {
+        if (defined $qglobals{pop_rallos_respawn_flag}) {
+                quest::depop();
+        }
 }
 
 # End of File  Zone: PoTactics  ID: 214056 -- Decorin_Berik
--- trunk/quests/potactics/Decorin_Grunhork.pl
+++ trunk/quests/potactics/Decorin_Grunhork.pl
@@ -7,4 +7,10 @@ sub EVENT_SIGNAL {
         quest::depop();
 }
 
+sub EVENT_SPAWN {
+        if (defined $qglobals{pop_rallos_respawn_flag}) {
+                quest::depop();
+        }
+}
+
 # End of File  Zone: PoTactics  ID: 214057 -- Decorin_Grunhork

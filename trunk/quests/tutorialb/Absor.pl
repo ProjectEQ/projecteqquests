a7ffde56a393a1bebdbbb9b434b8ddeca23ad01b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-03 05:11:56 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/tutorialb/Absor.pl
+++ trunk/quests/tutorialb/Absor.pl
@@ -9,22 +9,22 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 9997 => 1)) {
-    quest::summonitem(7021);
+    quest::summonitem(59950);
     quest::emote("Absor takes the weapon from you and begins to polish and balance it. When he hands it back to you, it scarcely resembles the decayed old thing that you were using.");
     quest::say("There you go. That should work much better."); 
   }
   elsif (plugin::check_handin(\%itemcount, 9998 => 1)) {
-    quest::summonitem(5042);
+    quest::summonitem(59951);
     quest::emote("Absor takes the weapon from you and begins to polish and balance it. When he hands it back to you, it scarcely resembles the decayed old thing that you were using.");
     quest::say("There you go. That should work much better.");
   }
   elsif (plugin::check_handin(\%itemcount, 9999 => 1)) {
-    quest::summonitem(6030);
+    quest::summonitem(59952);
     quest::emote("Absor takes the weapon from you and begins to polish and balance it. When he hands it back to you, it scarcely resembles the decayed old thing that you were using.");
     quest::say("There you go. That should work much better.");
   }
   elsif (plugin::check_handin(\%itemcount, 55623 => 1)) {
-    quest::summonitem(5071);
+    quest::summonitem(59953);
     quest::emote("Absor takes the weapon from you and begins to polish and balance it. When he hands it back to you, it scarcely resembles the decayed old thing that you were using.");
     quest::say("There you go. That should work much better.");
   }
@@ -32,9 +32,45 @@ sub EVENT_ITEM {
     quest::say("Now let me see... Ah ha! Here ya go! A much better weapon to help fend off those nasties!");
     quest::summonitem(quest::ChooseRandom(54230,54231,54232,54233));
   }
+  elsif (plugin::check_handin(\%itemcount, 54229 => 1)) {
+    #Chunk of Bronze -> Bronze Gloomingdeep weapons
+    quest::say("Now let me see... Ah ha! Here ya go! A much better weapon to help fend off those nasties!");
+    if($class eq 'Warrior'){quest::summonitem(quest::ChooseRandom(54230,54231,54232));}
+    elsif($class eq 'Cleric'){quest::summonitem(54232);}
+    elsif($class eq 'Paladin'){quest::summonitem(quest::ChooseRandom(54231,54232));}
+    elsif($class eq 'Ranger'){quest::summonitem(quest::ChooseRandom(54230,54231,54232));}
+    elsif($class eq 'Shadowknight'){quest::summonitem(quest::ChooseRandom(54230,54231,54232));}
+    elsif($class eq 'Druid'){quest::summonitem(54232);}
+    elsif($class eq 'Monk'){quest::summonitem(54232);}
+    elsif($class eq 'Bard'){quest::summonitem(quest::ChooseRandom(54230,54231,54232));}
+    elsif($class eq 'Rogue'){quest::summonitem(quest::ChooseRandom(54230,54231,54232));}
+    elsif($class eq 'Shaman'){quest::summonitem(54232);}
+    elsif($class eq 'Necromancer'){quest::summonitem(54230);}
+    elsif($class eq 'Wizard'){quest::summonitem(54230);}
+    elsif($class eq 'Magician'){quest::summonitem(54230);}
+    elsif($class eq 'Enchanter'){quest::summonitem(54230);}
+    elsif($class eq 'Beastlord'){quest::summonitem(quest::ChooseRandom(54230,54232));}
+    elsif($class eq 'Berserker'){quest::summonitem(54233);}
+  }
   elsif (plugin::check_handin(\%itemcount, 59954 => 1)) {
+    #Chunk of Iron -> Iron Gloomingdeep weapons
     quest::say("Now let me see... Ah ha! Here ya go! A spiffy, new weapon to aid you in your adventures!");
-    quest::summonitem(quest::ChooseRandom(54235,54236,54237,54238));
+    if($class eq 'Warrior'){quest::summonitem(quest::ChooseRandom(54235,54236,54237));}
+    elsif($class eq 'Cleric'){quest::summonitem(54237);}
+    elsif($class eq 'Paladin'){quest::summonitem(quest::ChooseRandom(54236,54237));}
+    elsif($class eq 'Ranger'){quest::summonitem(quest::ChooseRandom(54235,54236,54237));}
+    elsif($class eq 'Shadowknight'){quest::summonitem(quest::ChooseRandom(54235,54236,54237));}
+    elsif($class eq 'Druid'){quest::summonitem(54237);}
+    elsif($class eq 'Monk'){quest::summonitem(54237);}
+    elsif($class eq 'Bard'){quest::summonitem(quest::ChooseRandom(54235,54236,54237));}
+    elsif($class eq 'Rogue'){quest::summonitem(quest::ChooseRandom(54235,54236,54237));}
+    elsif($class eq 'Shaman'){quest::summonitem(54237);}
+    elsif($class eq 'Necromancer'){quest::summonitem(54235);}
+    elsif($class eq 'Wizard'){quest::summonitem(54235);}
+    elsif($class eq 'Magician'){quest::summonitem(54235);}
+    elsif($class eq 'Enchanter'){quest::summonitem(54235);}
+    elsif($class eq 'Beastlord'){quest::summonitem(quest::ChooseRandom(54235,54237));}
+    elsif($class eq 'Berserker'){quest::summonitem(54238);} 
     }
   else {
     quest::say("I have no need for this, $name.");
--- trunk/quests/tutorialb/Rytan.pl
+++ trunk/quests/tutorialb/Rytan.pl
@@ -4,29 +4,38 @@ sub EVENT_SAY{
   if ($text=~/hail/i){
    quest::say("Hello $name. I'm not much of a fighter myself but I can bestow the power of my god's favor. Would you like to be [blessed]?");
    }
-  if ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Warrior' || $class eq 'Rogue' || $class eq 'Monk' || $class eq 'Berserker')){
-   $npc->CastSpell(5150,$userid);
-   quest::updatetaskactivity(22,5);  
-   }
-  elsif ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Cleric' || $class eq 'Paladin' || $class eq 'Ranger' || $class eq 'Druid' || $class eq 'Shaman' || $class eq 'Beastlord' || $class eq 'Shadowknight')){
-   quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will fnd other powerful spells. Good luck out there!");
-   $npc->CastSpell(5150,$userid);
-   quest::updatetaskactivity(22,5); 
-   quest::summonitem(15213); 
-   }
-  elsif ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Necromancer' || $class eq 'Magician' || $class eq 'Wizard' || $class eq 'Enchanter')){
-   quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will fnd other powerful spells. Good luck out there!");
-   $npc->CastSpell(5150,$userid);
-   quest::updatetaskactivity(22,5); 
-   quest::summonitem(15205);
-   }
-  elsif ($text=~/blessed/i && quest::istaskactivityactive(22,5) && ($class eq 'Bard')){
-   quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will fnd other powerful spells. Good luck out there!");
-   $npc->CastSpell(5150,$userid);
-   quest::updatetaskactivity(22,5); 
-   quest::summonitem(15703);
-   }
-   elsif ($text=~/blessed/i){
-   $npc->CastSpell(5150,$userid);
-   }
-}
+  elsif ($text=~/blessed/i) {
+    $npc->CastSpell(5150,$userid);
+    if (quest::istaskactivityactive(22,5)) {
+      quest::updatetaskactivity(22,5);
+      if ($class eq 'Warror' || $class eq 'Monk' || $class eq 'Berserker' || $class eq 'Rogue') {
+        quest::summonitem(59973); # Tome of Throw Stone
+        # missing some text to send the player to the melee disc NPC
+      }
+      else
+      {
+        quest::say("You seem to be a bit of a caster yourself! You will find that spells and songs can greatly enhance your power. Take this and scribe it in your spell book. I can only hope it helps you against the kobold horde. As you earn money and gain experience you will find other powerful spells. Good luck out there!");
+        if ($class eq 'Paladin' || $class eq 'Ranger' || $class eq 'Beastlord') { quest::summonitem(59971); }
+          # Spell: Salve
+        elsif ($class eq 'Shadowknight') { quest::summonitem(59972); }
+          # Spell: Spike of Disease
+        elsif ($class eq 'Bard') { quest::summonitem(15703); }
+          # Song: Chords of Dissonance
+        elsif ($class eq 'Druid') { quest::summonitem(15242); }
+          # Spell: Snare
+        elsif ($class eq 'Cleric') { quest::summonitem(15014); }
+          # Spell: Strike -- Guess. Alternative being Yaulp
+        elsif ($class eq 'Shaman') { quest::summonitem(9983); }
+          # Spell: Inner Fire* -- Should be a starting item for them
+        elsif ($class eq 'Enchanter') { quest::summonitem(9987); }
+          # Spell: Shallow Breath* -- Should be a starting item for them
+        elsif ($class eq 'Magician') { quest::summonitem(15311); }
+          # Spell: Summon Dagger -- Guess. Alternative would be Summon: Food
+        elsif ($class eq 'Necromancer') { quest::summonitem(9986); }
+          # Spell: Minor Shielding* -- So all casters have Minor Shielding.
+        elsif ($class eq 'Wizard') { quest::summonitem(15054); }
+          # Spell: Frost Bolt -- Guess (maybe Numbing Cold (59964) instead?)
+      }
+    } # end if (task)
+  } # end if (blessed)
+} 

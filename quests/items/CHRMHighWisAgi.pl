386391e905225709988f28c21a63714a64e7c05c
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-10 21:12:13 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/items/CHRMHighWisAgi.pl
@@ -0,0 +1,50 @@
+sub EVENT_SCALE_CALC {
+  my $wis = $client->GetWIS();
+  my $agi = $client->GetAGI();
+    
+  if($wis < 0) {
+    $wis = 0;
+  }
+
+  if($agi < 0) {
+    $agi = 0;
+  }
+  
+  my $scale = $wis + $agi;
+  
+  if($scale < 120) {
+  $questitem->SetScale($scale/260);
+  }
+  
+  if($scale > 119 && $scale < 200) {
+  $questitem->SetScale($scale/250);
+  }
+  
+  if($scale > 199 && $scale < 300) {
+  $questitem->SetScale($scale/240);
+  }
+  
+  if($scale > 299 && $scale < 400) {
+  $questitem->SetScale($scale/230);
+  }
+  
+  if($scale > 399 && $scale < 500) {
+  $questitem->SetScale($scale/220);
+  }
+  
+  if($scale > 499 && $scale < 600) {
+  $questitem->SetScale($scale/210);
+  }
+  
+  if($scale > 599 && $scale < 700) {
+  $questitem->SetScale($scale/200);
+  }
+  
+  if($scale > 799 && $scale < 900) {
+  $questitem->SetScale($scale/180);
+  }
+  
+  if($scale > 899) {
+  $questitem->SetScale($scale/170);
+  }
+}
--- quests/poknowledge/Gram_Dunnar.pl
+++ quests/poknowledge/Gram_Dunnar.pl
@@ -1,13 +1,13 @@
 sub EVENT_SAY
 {
-	if($text=~/Hail/i and $ulevel < 51){
+	if($text=~/Hail/i && $ulevel < 51){
 		quest::emote("The old dwarf glances at you briefly, then returns his attention to a small wooden object he is carving. 'Got no time for kids like you,' he grunts.");
 	}
 	
 	elsif($text=~/Hail/i){
 		quest::say("The old dwarf looks up at you slowly, his wrinkled hands continuing to carve a small wooden object. He grins as he eyes your dust-covered outfit and well-used equipment. 'Ahh, a young adventurer,' he says. 'Warms my heart to see the spirit I once had still going strong. Got too old, you see. He strokes his beard. 'Tried taking up a [craft], but it just isn't the same. Sure would be nice to hear some [stories] about those new places I'll never get to see.");
 	}
-	if($text=~/craft/i){
+	elsif($text=~/craft/i && $ulevel > 50){
 		quest::emote("Gram Dunnar stops carving and holds up the object between his short fingers. It is a figurine of a swordsman with many intricate details. 'No one really wants to buy them, these days. If there's no magic in it...' he shrugs. 'Still, something to take up some time.' He rummages through some finished pieces on the floor around him, picks up one, and tosses it to you. 'Here,' he says. 'Maybe it'll bring you some luck.");
 		if ($class eq 'Warrior') { quest::summonitem(35001); }
 		elsif ($class eq 'Monk') { quest::summonitem(35007); }
@@ -26,7 +26,7 @@ sub EVENT_SAY
 		elsif ($class eq 'Necromancer') { quest::summonitem(35011); }
                 elsif ($class eq 'Wizard') { quest::summonitem(35012); }
 	}
-	if($text=~/stories/i && defined $qglobals{pop_story} && $qglobals{pop_story} == 1){
+	elsif($text=~/stories/i && $ulevel > 50 && defined $qglobals{pop_story} && $qglobals{pop_story} == 1){
 		if(defined $qglobals{pop_pod_alder_fuirstel} &&  defined $qglobals{pop_pod_grimmus_planar_projection} &&  defined $qglobals{pop_pod_elder_fuirstel} && !defined $qglobals{pop_aa_codecay}) {
 				$client->Message(4,"Gram Dunnar shudders as you tell him of the Crypt of Decay. 'A foul place, indeed. I would have loved to slay a few of those creatures, though.' His hand clenches and his eyes look past you. 'Ah, the battles I used to have,' he mutters.");
 				$client->AddAAPoints(1);
@@ -39,7 +39,7 @@ sub EVENT_SAY
 				quest::setglobal("pop_story", 2, 5, "S20");
 			}
 	}	
-	elsif($text=~/stories/i && defined $qglobals{pop_story} && $qglobals{pop_story} == 2){
+	elsif($text=~/stories/i && $ulevel > 50 && defined $qglobals{pop_story} && $qglobals{pop_story} == 2){
 		if(defined $qglobals{pop_poi_behometh_preflag} &&  defined $qglobals{pop_poi_behometh_flag} && !defined $qglobals{pop_aa_tactics}) {
 				$client->Message(4,"Gram Dunnar grins excitedly at your description of the Plane of Tactics. 'I'd show the minions of Rallos Zek something, I would!' he exclaims, swinging his hands around as though wielding an axe. He sits down again and looks sadly at his aged hands.");
 				$client->AddAAPoints(1);
@@ -52,7 +52,7 @@ sub EVENT_SAY
 				quest::setglobal("pop_story", 3, 5, "S20");
 			}
 	}
-	elsif($text=~/stories/i && defined $qglobals{pop_story} && $qglobals{pop_story} == 3){
+	elsif($text=~/stories/i && $ulevel > 50 && defined $qglobals{pop_story} && $qglobals{pop_story} == 3){
 		if(defined $qglobals{pop_poj_mavuin} &&  defined $qglobals{pop_poj_tribunal} &&  defined $qglobals{pop_poj_valor_storms} &&  defined $qglobals{pop_pov_aerin_dar} && !defined $qglobals{pop_aa_hohonor}) {
 				$client->Message(4,"'Animated suits of armor, eh?' Gram says as you tell him of the Halls of Honor. 'Saw something like that once, but never so many as that.' His eyes get a faraway look. 'If I only I still had my youth.'");
 				$client->AddAAPoints(1);
@@ -65,7 +65,7 @@ sub EVENT_SAY
 				quest::setglobal("pop_story", 4, 5, "S20");
 			}
 	}
-	elsif($text=~/stories/i && defined $qglobals{pop_story} && $qglobals{pop_story} == 4){	
+	elsif($text=~/stories/i && $ulevel > 50 && defined $qglobals{pop_story} && $qglobals{pop_story} == 4){	
 		if(defined $qglobals{pop_poj_mavuin} &&  defined $qglobals{pop_poj_tribunal} &&  defined $qglobals{pop_poj_valor_storms} &&  defined $qglobals{pop_pos_askr_the_lost} && $qglobals{pop_pos_askr_the_lost} == 3 &&  defined $qglobals{pop_pos_askr_the_lost_final} && !defined $qglobals{pop_aa_bothunder}) {
 				$client->Message(4,"'Giants and a huge fortress,' Gram sighs wistfully as you finish your story of the Bastion of Thunder. 'Storming through a place like that, killing everything in sight - those were good days.'");
 				$client->AddAAPoints(1);
@@ -78,7 +78,7 @@ sub EVENT_SAY
 				quest::setglobal("pop_story", 5, 5, "S20");
 			}	
 	}
-	elsif($text=~/stories/i && defined $qglobals{pop_story} && $qglobals{pop_story} == 5){
+	elsif($text=~/stories/i && $ulevel > 50 && defined $qglobals{pop_story} && $qglobals{pop_story} == 5){
 		if(defined $qglobals{pop_pod_alder_fuirstel} &&  defined $qglobals{pop_pod_grimmus_planar_projection} &&  defined $qglobals{pop_pod_elder_fuirstel} &&  defined $qglobals{pop_pon_construct} &&  defined $qglobals{pop_pon_hedge_jezith} &&  defined $qglobals{pop_ponb_terris} &&  defined $qglobals{pop_ponb_poxbourne} &&  defined $qglobals{pop_cod_preflag} &&  defined $qglobals{pop_cod_bertox} &&  defined $qglobals{pop_cod_final} && !defined $qglobals{pop_aa_potorment}) {
 				$client->Message(4,"Gram Dunnar listens attentively to your story of the Plane of Torment. 'Ravens with bloody eyes,' he muses, 'and creatures with four mouths?' He shakes his head. 'Never in my day did I see such things.'");
 				$client->AddAAPoints(1);
@@ -91,7 +91,7 @@ sub EVENT_SAY
 				quest::setglobal("pop_story", 6, 5, "S20");
 			}	
 	}
-	elsif($text=~/stories/i && defined $qglobals{pop_story} && $qglobals{pop_story} == 6){	
+	elsif($text=~/stories/i && $ulevel > 50 && defined $qglobals{pop_story} && $qglobals{pop_story} == 6){	
 		if(defined $qglobals{pop_poi_behometh_preflag} &&  defined $qglobals{pop_poi_behometh_flag} &&  defined $qglobals{pop_tactics_tallon} &&  defined $qglobals{pop_tactics_vallon} &&  defined $qglobals{pop_pot_saryrn_final} &&  defined $qglobals{pop_pot_saryrn} &&  defined $qglobals{pop_hohb_marr} && !defined $qglobals{pop_aa_solrotower}) {
 				$client->Message(4,"Gram Dunnar strokes his beard thoughtfully as you tell him of the Tower of Solusek Ro. 'The burning prince himself. A worthy opponent, I'm sure,' he says. 'Thank you for the tales, $name. It's good to imagine these places, even if I may never see them.'");
 				$client->AddAAPoints(1);
@@ -104,7 +104,7 @@ sub EVENT_SAY
 				quest::delglobal("pop_story");
 			}	
 	}
-       elsif($text=~/stories/i){
+       elsif($text=~/stories/i && $ulevel > 50){
 		if(defined $qglobals{pop_poj_mavuin} &&  defined $qglobals{pop_poj_tribunal} &&  defined $qglobals{pop_poj_valor_storms} && !defined $qglobals{pop_aa_stormvalor}) {
 				$client->Message(4,"Gram Dunnar listens eagerly to your tales of the Plane of Valor and Plane of Storms. 'Everything turned to glass by the dragon! That must be a sight,' he exclaims. 'Wish I could go there,' he sighs.");
 				$client->AddAAPoints(1);
@@ -117,6 +117,10 @@ sub EVENT_SAY
 				quest::setglobal("pop_story", 1, 5, "S20");
 			}
 	}
+        else {
+             quest::emote("The old dwarf glances at you briefly, then returns his attention to a small wooden object he is carving. 'Got no time for kids like you,' he grunts.");
+	}
+
 	$qglobals{pop_pon_hedge_jezith}=undef;	
 	$qglobals{pop_pon_construct}=undef;	
 	$qglobals{pop_ponb_terris}=undef;	

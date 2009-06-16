e8ba395e177984a3147bc6216ff4bb9f8cdf2347
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-16 03:49:27 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/butcher/Magus_Tira.pl
+++ quests/butcher/Magus_Tira.pl
@@ -3,28 +3,34 @@
 #
 
 sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("And how are you today, $name?  Ready to do some traveling? Hurry up, then. I have lots of things I need to do today. Tell me where you would like to go and I will send you there. I can send you to any of the other camps in [Everfrost], [Commonlands], [Nedaria's Landing], [North Ro], or [South Ro]. Hopefully I won't burn my hands this time! Long story, but my cohort, Gaelan Charhands, didn't tell me one of the words of the spell as a joke. Apparently it's funny for a beautiful gnome like myself to get her hands singed. I'll get him back some day!");
-  }
-  if($text=~/everfrost/i) {
-    quest::say("");
-    #quest::movepc();
-  }
-  if($text=~/commonlands/i) {
-    quest::say("");
-    #quest::movepc();
-  }
-  if($text=~/nedaria/i) {
-    quest::say("I'm sorry, $name. The Gates of Discord zones are not yet available.");
-    #quest::movepc();
-  }
-  if($text=~/north ro/i) {
-    quest::say("");
-    #quest::movepc();
-  }
-  if($text=~/south ro/i) {
-    quest::say("");
-    #quest::movepc();
+  if($ulevel >= 15) {
+    if(plugin::check_hasitem($client, 41000)) { #Adventurer's Stone
+      if($text=~/hail/i) {
+        quest::say("And how are you today, $name?  Ready to do some traveling? Hurry up, then. I have lots of things I need to do today. Tell me where you would like to go and I will send you there. I can send you to any of the other camps in [Everfrost], [Commonlands], [Nedaria's Landing], [North Ro], or [South Ro]. Hopefully I won't burn my hands this time! Long story, but my cohort, Gaelan Charhands, didn't tell me one of the words of the spell as a joke. Apparently it's funny for a beautiful gnome like myself to get her hands singed. I'll get him back some day!");
+      }
+      if($text=~/everfrost/i) {
+        #quest::movepc(); #Everfrost Peaks 
+      }
+      if($text=~/commonlands/i) {
+        #quest::movepc(); #West Commonlands
+      }
+      if($text=~/nedaria/i) {
+        quest::say("I'm sorry, $name. The Gates of Discord zones are not yet available.");
+        #quest::movepc(); #Nedaria's Landing
+      }
+      if($text=~/north ro/i) {
+        #quest::movepc(); #Northern Desert of Ro
+      }
+      if($text=~/south ro/i) {
+         #quest::movepc(); #Southern Desert of Ro
+      }
+    }
+    else {
+      quest::say("You will have to excuse me, I am quite busy studying this Farstone and the possibility of using the magic stored inside of it. Perhaps you should talk to another at this camp to see if they have any use for you.");
+    }
+  }   
+  else {
+    quest::say("You will have to excuse me, I am quite busy."); #Text made up
   }
 }
 

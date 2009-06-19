4ae7b2ad60b2a15e431610645d50c44f6fcb8877
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-19 04:45:47 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/lakerathe/#Triathalon_Bike.pl
+++ trunk/quests/lakerathe/#Triathalon_Bike.pl
@@ -1,11 +1,15 @@
+##Custom script  for GM event, created by Shadowblade
+
 sub EVENT_SAY {
   if ($text=~/hail/i) {
     quest::say("Hello, $name! If you visited with my friend on the island you should have something to give me.");
   }
+
+}
+
   sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 16889 => 1)) {
     quest::say("Excellent, congratulations  on completing the swim.  Here is something that will help you cross South Karana.  Don't forget to give this to my friend on the bridge before crossing into North Karana.");
     quest::summonitem(21800);
   }  
   }
-}

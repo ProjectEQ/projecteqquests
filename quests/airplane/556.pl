94d1b2d646efcaea112bfb1bd78378885852d962
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-22 03:00:51 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/airplane/556.pl
@@ -0,0 +1,32 @@
+############################################
+# ZONE: Plane of Sky (airplane)
+# LAST EDIT DATE: May 17, 2009
+# VERSION: 1.0
+# DEVELOPER: Congdar
+#
+# *** NPC INFORMATION ***
+#
+# NAME: SumAirR6
+# ID: 556
+# TYPE: Pet
+# RACE: 75 (Elemental)
+# LEVEL: 18
+#
+# *** QUESTS INVOLVED IN ***
+#
+# Plane of Sky Quirkiness
+# Magician Air Elemental Pet becomes a Female Djinn
+# 
+# *** QUESTS AVAILABLE TO ***
+#
+# All Magician Air Elemental Pets
+#
+############################################
+
+sub EVENT_SPAWN
+{
+    quest::npcsize(5);
+    quest::npcrace(126);
+}
+#END of FILE Zone:airplane  ID:556 -- SumAirR6
+

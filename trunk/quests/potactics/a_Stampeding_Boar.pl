f49127d67f1bba7e6eefbead37e1c56f706394f2
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-23 02:25:35 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/potactics/a_Stampeding_Boar.pl
+++ trunk/quests/potactics/a_Stampeding_Boar.pl
@@ -1,3 +1,12 @@
 sub EVENT_SPAWN {
-quest::SetRunning(1);
+		quest::SetRunning(1);
+		quest::settimer("stamboars",1);
+}
+
+sub EVENT_TIMER {
+	if ($x == 1202 && $y == -351){
+		quest::stoptimer("stamboars");
+		quest::spawn_condition(potactics,1,0);
+		quest::depopall(214097);
+	}
 }

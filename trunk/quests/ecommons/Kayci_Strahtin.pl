d91be833e7ba3dccf7057d254c0ad9fdf2768b8b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-06-30 03:51:03 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/ecommons/Kayci_Strahtin.pl
@@ -0,0 +1,14 @@
+sub EVENT_ITEM {
+  if(plugin::check_handin(\%itemcount, 55268 => 1)) { #Kizpi's Note
+    quest::say("Incessant fleas! Ever since I arrived in the Commonlands, they have plagued me! Oh, sorry. What's this? A note from Kizpi? Let me see. So the ol' bugger finally found a way to upgrade the Satchel of Abu-Kar, eh? It says here that you will need a Rujarkian Orc Hide. You are in luck, I just happen to have one that a previous adventure gave to me from his recent excursion to the Rujarkian Hills. You may have it. You will need some other items though. Kizpi didn't know about that?! His brain must be freezing out there in Everfrost!");
+    quest::emote("makes some new notes. 'Take this note to Shelie Dikop at the Adventurer's Camp in North Ro. Tell her to come here and scare these fleas away with her ugly face!'");
+    quest::emote("laughs. 'Perhaps that would be a bad idea. Just hand her this note when you find her. Good luck to you.'");
+    quest::emote("begins to furiously scratch at the fleas behind his ears.");
+    quest::summonitem(55269); #Kayci's Note
+    quest::summonitem(55265); #Rujarkian Orc Hide
+  }
+  else {
+    plugin::return_items(\%itemcount);
+    quest::say("I have no use for this, $name.");
+  }
+}
--- trunk/quests/tutorialb/player.pl
+++ trunk/quests/tutorialb/player.pl
@@ -29,3 +29,73 @@ sub EVENT_TASK_STAGE_COMPLETE {
       quest::ze(15, "A cheer arises from the slaves as the last of the Kobold overseers fall.");
    }
 } 
+
+EVENT_CLICKDOOR {
+ $deity = $client->GetDeity();
+  if ($doorid == 11 || $doorid == 267 && $race eq 'Wood Elf') {
+  quest::movepc(54,-197,27,-0.7);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Human' && $class eq 'Enchanter'){
+  quest::movepc(45,-343,189,-38.22);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Human' && $class eq 'Cleric'){
+  quest::movepc(45,-343,189,-38.22);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Human' && $class eq 'Necromancer'){
+  quest::movepc(45,-343,189,-38.22);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Human' && $class eq 'Magician'){
+  quest::movepc(45,-343,189,-38.22);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Human' && $class eq 'Shadowknight'){
+  quest::movepc(45,-343,189,-38.22);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Human') {
+  quest::movepc(9,-60.9,-61.5,-24.9);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Erudite' && $deity == 203) {
+  quest::movepc(75,200,800,3.39);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Erudite') {
+  quest::movepc(24,-309.8,109.6,23.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'High Elf') {
+  quest::movepc(61,26.3,14.9,3.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Barbarian') {
+  quest::movepc(29,12.2,-32.9,3.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Dark Elf') {
+  quest::movepc(25,-965.3,2434.5,5.6);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Half Elf') {
+  quest::movepc(9,-60.9,-61.5,-24.9);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Dwarf') {
+  quest::movepc(68,-214.5,2940.1,0.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Troll') {
+  quest::movepc(52,1.1,14.5,3.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Ogre') {
+  quest::movepc(49,520.1,235.4,59.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Halfling') {
+  quest::movepc(19,-98.4,11.5,3.1);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Gnome') {
+  quest::movepc(55,7.6,489.0,-24.9);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Froglok') {
+  quest::movepc(50,560,-2234,3);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Vah Shir') {
+  quest::movepc(155,105.6,-850.8,-190.4);
+}
+  elsif ($doorid == 11 || $doorid == 267 && $race eq 'Iksar') {
+  quest::movepc(106,-415.7,1276.6,3.1);
+  
+}else{
+  quest::movepc(202,-55,44,-158.81);
+ }
+}

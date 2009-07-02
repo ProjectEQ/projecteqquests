005fa06ae39ac2974af3456d2186845cacffc3ca
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-02 22:28:14 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/corathus/365019.pl
+++ quests/corathus/365019.pl
@@ -95,7 +95,7 @@ sub EVENT_TIMER
 		my $dist_y = -410 - $npc->GetY();
 		my $dist_z = -8 - $npc->GetZ();
 		my $total_dist = ($dist_x * $dist_x) + ($dist_y * $dist_y) + ($dist_z * $dist_z);
-		if($total_dist > 90000)
+		if($total_dist > 122500)
 		{
 			quest::emote("loses its power link and begins to behave erratically.");
 			quest::modifynpcstat("runspeed", "2.0");

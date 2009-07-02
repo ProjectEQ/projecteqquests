d5c04ecc974269014346feb857eb697bff2f178a
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-02 06:58:58 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/corathus/365077.pl
+++ quests/corathus/365077.pl
@@ -15,31 +15,6 @@ sub EVENT_TIMER
 	}
 	elsif($timer eq "cast")
 	{
-		my $random_client = $entity_list->GetRandomClient($npc->GetX(), $npc->GetY(), $npc->GetZ(), 200);
-		if($random_client) 
-		{
-			my $random_spell = quest::ChooseRandom(1,2,3,4,5);
-			if($random_spell == 1)
-			{
-				$random_client->Damage($npc, 2500, 2993, 4, false); 
-			}
-			elsif($random_spell == 2)
-			{
-				$random_client->Damage($npc, 1000, 2993, 4, false); 
-			}
-			elsif($random_spell == 3)
-			{
-				$random_client->SetMana($random_client->GetMana() - 4000);
-			}
-			elsif($random_spell == 4)
-			{
-				$random_client->SetMana($random_client->GetMana() - 2000);
-			}
-			elsif($random_spell == 5)
-			{
-				$random_client->SetMana($random_client->GetMana() - 1000);
-			}
-		}	
 	}
 }
 

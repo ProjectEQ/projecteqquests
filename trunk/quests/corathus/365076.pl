f6fdd9a85fecd3057a3e2d7044d1cf475e607958
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-02 06:51:21 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/corathus/365076.pl
@@ -0,0 +1,52 @@
+#poison mushroom teal
+
+sub EVENT_SPAWN
+{
+	settimer("wither", 60);
+	settimer("cast", 3);
+}
+
+sub EVENT_TIMER
+{
+	if($timer eq "wither")
+	{
+		stoptimer("wither");
+		quest::emote("withers and dies.");
+		quest::depop();
+	}
+	elsif($timer eq "cast")
+	{
+		my $random_client = $entity_list->GetRandomClient($npc->GetX(), $npc->GetY(), $npc->GetZ(), 200);
+		if($random_client) 
+		{
+			my $random_spell = quest::ChooseRandom(1,2,3,4,5);
+			if($random_spell == 1)
+			{
+				$random_client->Damage($npc, 1000, 2993, 4, false); 
+			}
+			elsif($random_spell == 2)
+			{
+				$random_client->Damage($npc, 500, 2993, 4, false);
+			}
+			elsif($random_spell == 3)
+			{
+				$random_client->Damage($npc, 200, 2993, 4, false);
+				$random_client->SetMana($random_client->GetMana() - 200);
+			}
+			elsif($random_spell == 4)
+			{
+				$random_client->SetMana($random_client->GetMana() - 500);
+			}
+			elsif($random_spell == 5)
+			{
+				$random_client->SetMana($random_client->GetMana() - 200);
+			}
+		}	
+	}
+}
+
+sub EVENT_DEATH
+{
+	stoptimer("wither");
+	stoptimer("cast");
+}

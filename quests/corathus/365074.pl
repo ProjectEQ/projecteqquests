2fbba5ad4d8172221eecf84c5e9ac87adddf3a88
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-02 07:21:07 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/corathus/365074.pl
+++ quests/corathus/365074.pl
@@ -34,14 +34,17 @@ sub EVENT_TIMER
 				emote::say("I would be healing damage but it's nyi");
 				#$random_client->HealDamage(1000, $npc);
 				$random_client->SetMana($random_client->GetMana() + 200);
+				$random_client->Message(264, "Your mind clears.");
 			}
 			elsif($random_spell == 4)
 			{
 				$random_client->SetMana($random_client->GetMana() + 500);
+				$random_client->Message(264, "Your mind clears.");
 			}
 			elsif($random_spell == 5)
 			{
 				$random_client->SetMana($random_client->GetMana() + 200);
+				$random_client->Message(264, "Your mind clears.");
 			}
 		}	
 	}

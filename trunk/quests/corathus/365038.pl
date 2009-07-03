c9a745e061d6bfd3b5d8c5a9e99e0561efc714f6
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-03 23:28:42 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/365038.pl
+++ trunk/quests/corathus/365038.pl
@@ -1,18 +1,12 @@
 #Sporilok the Fungus Commander
-
 sub EVENT_COMBAT
 {
 	if($combat_state == 0)
 	{
-		quest::say("Debug: Leaving combat!");
+		quest::emote("Debug: Leaving combat!");
 	}
 	elsif($combat_state == 1)
 	{
-		quest::say("Debug: Entering combat!");
+		quest::emote("Debug: Entering combat!");
 	}
-}
-
-sub EVENT_DEATH
-{
-	quest::emote("slowly stops moving and comes crashing to the ground.");
 }

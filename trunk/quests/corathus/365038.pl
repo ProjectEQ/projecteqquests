e9adef7f1fe0a387a9769f7928e1022a75439887
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-03 23:44:07 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/365038.pl
+++ trunk/quests/corathus/365038.pl
@@ -1,89 +1,75 @@
-my $targeted_enemy;
-my $targeted_enemy_x;
-my $targeted_enemy_y;
-my $targeted_enemy_z;
-
 sub EVENT_COMBAT
 {
 	if($combat_state == 0)
 	{
-		quest::stoptimer("missle_select");
-		quest::stoptimer("missle_attack");
-		quest::stoptimer("overload");
-		quest::stoptimer("overload_finish");
+		quest::stoptimer("rage");
+		quest::stoptimer("mushroom");
+		quest::stoptimer("rage_two");
+		quest::modifynpcstat("min_hit", "1690");
+		quest::modifynpcstat("max_hit", "3300");
+		quest::modifynpcstat("attack_speed", "33");
+		quest::depopall(365073);
+		quest::depopall(365074);
+		quest::depopall(365075);
+		quest::depopall(365076);
+		quest::depopall(365077);
 	}
 	elsif($combat_state == 1)
 	{
-		quest::settimer("missle_select", 7);
-		quest::settimer("overload", 120);
+		quest::settimer("rage", 840);
+		quest::settimer("mushroom", 5);
+		quest::modifynpcstat("min_hit", "1690");
+		quest::modifynpcstat("max_hit", "3300");
+		quest::modifynpcstat("attack_speed", "33");
 	}
 }
 
 sub EVENT_TIMER
 {
-	if($timer eq "missle_select")
+	if($timer eq "mushroom")
 	{
-		$targeted_enemy = $npc->GetHateRandom();
-		if($targeted_enemy)
+		my $random_target = $npc->GetHateRandom();
+		if($random_target)
 		{
-			quest::stoptimer("missle_select");
-			quest::settimer("missle_attack", 5);
-			my $clean_name = $targeted_enemy->GetCleanName();
-			quest::emote("attempts to lock onto $clean_name with its anti-personnel missles.");
-			$targeted_enemy_x = $targeted_enemy->GetX();
-			$targeted_enemy_y = $targeted_enemy->GetY();
-			$targeted_enemy_z = $targeted_enemy->GetZ();
-		}
-		else
-		{
-			quest::stoptimer("missle_select");
-			quest::settimer("missle_select", 1);
+			my $c_name = $random_target->GetCleanName();
+			my $random_mushroom = quest::ChooseRandom(365073,365074,365075,365076,365077);
+			my $x_loc = $random_target->GetX();
+			my $y_loc = $random_target->GetY();
+			my $z_loc = $random_target->GetZ();
+			quest::emote("Throws a mushroom at $c_name");
+			quest::spawn($random_mushroom, 0, 0, $x_loc, $y_loc, $z_loc);
 		}
 	}
 	
-	if($timer eq "missle_attack")
+	if($timer eq "rage")
 	{
-		if($targeted_enemy)
-		{
-			my $dist_x = $targeted_enemy->GetX() - $targeted_enemy_x;
-			my $dist_y = $targeted_enemy->GetY() - $targeted_enemy_y;
-			my $dist_z = $targeted_enemy->GetZ() - $targeted_enemy_z;
-			my $total_dist = ($dist_x * $dist_x) + ($dist_y * $dist_y) + ($dist_z * $dist_z);
-			if($total_dist < 1200)
-			{
-				$targeted_enemy->Message(0, "You were hit by the missle.");
-				$targeted_enemy->Damage($npc, 15000, 0, 0, false); 
-			}
-			else
-			{
-				$targeted_enemy->Message(0, "You have evaded the missle.");
-			}
-		}
-		quest::stoptimer("missle_attack");
-		quest::settimer("missle_select", 7);
+		quest::stoptimer("rage");
+		quest::settimer("rage_two", 60);
+		quest::emote("begins to fall apart...");
+		quest::modifynpcstat("min_hit", "1690");
+		quest::modifynpcstat("max_hit", "3900");
+		quest::modifynpcstat("attack_speed", "25");		
 	}
 
-	if($timer eq "overload")
+	if($timer eq "rage_two")
 	{
-		quest::emote("gathers energy...");
-		quest::stoptimer("overload");
-		quest::settimer("overload_finish", 5);
-		$npc->Stun(6000);
-	}
-
-	if($timer eq "overload_finish")
-	{
-		quest::emote("releases its stored energy.");
-		quest::stoptimer("overload_finish");
-		quest::settimer("overload", 120);
-		$npc->CastSpell(1948, $npc->GetID(), 10, 0);
+		quest::stoptimer("rage_two");
+		quest::emote("is filled with a bloodlust.");
+		quest::modifynpcstat("min_hit", "15000");
+		quest::modifynpcstat("max_hit", "30000");
+		quest::modifynpcstat("attack_speed", "-50");		
 	}
 }
 
 sub EVENT_DEATH
 {
-	quest::stoptimer("missle_select");
-	quest::stoptimer("missle_attack");
-	quest::stoptimer("overload");
-	quest::stoptimer("overload_finish");
+	quest::emote("slowly stops moving and comes crashing to the ground.");
+	quest::stoptimer("rage");
+	quest::stoptimer("rage_two");
+	quest::stoptimer("mushroom");
+	quest::depopall(365073);
+	quest::depopall(365074);
+	quest::depopall(365075);
+	quest::depopall(365076);
+	quest::depopall(365077);
 }

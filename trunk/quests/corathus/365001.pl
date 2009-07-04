bbfef5ddc34479c16147f81dc2e61e5eb1115de3
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-04 07:31:32 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/365001.pl
+++ trunk/quests/corathus/365001.pl
@@ -1,5 +1,3 @@
-@FlavorText = { "I wish I was a real boy...", "We need to do something about our spider problem here, sheesh!", "Tunare and Nork sitting in a tree, k-i-s-s-i-n-g.", "Cavedude! That's the man that programmed me for evil!", "I am so sick of mushrooms.", "I saw some tiny robot walking around earlier, he was sooooo cute.", "I'm metal, cold and living in a mushroom patch, I blame Reno for all this.", "Word on the street is Daryn's really a dark elf.", "If I see that Dr. Jones guy again I'm going to give him a piece of my mind.", "I hate things smaller than me." };
-
 sub EVENT_COMBAT
 {
 	if($combat_state == 0)
@@ -22,35 +20,19 @@ sub EVENT_COMBAT
 
 sub EVENT_SPAWN
 {
-	quest::settimer("random_talk", (rand(480)+120));
 	quest::setnexthpevent(16);
-}
-
-sub EVENT_TIMER
-{
-	if($timer eq "random_talk")
-	{
-		quest::stoptimer("random_talk");
-		quest::settimer("random_talk", (rand(480)+120));
-		if($npc->IsEngaged() != 1)
-		{
-			my $rand_select = rand(9);
-			quest::say("$FlavorText[$rand_select]");
-		}
-	}
 }
 
 sub EVENT_HP
 {
 	if($hpevent == 16)
 	{
-		quest::say("I'm a charging my lasers!");
+		quest::say("I'm charging my lasers!");
 		$npc->CastSpell(4674, $npc->GetID(), 10, 0);
 	}
 }
 
 sub EVENT_DEATH
 {
-	quest::stoptimer("random_talk");
 	quest::say("I don't want to play with you anymore...");
 }

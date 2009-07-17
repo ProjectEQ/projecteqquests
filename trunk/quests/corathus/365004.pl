0d969acbc616ab69d4e469981e1fc52ee8e4d88d
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-07-17 07:57:26 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/365004.pl
+++ trunk/quests/corathus/365004.pl
@@ -3,20 +3,66 @@ sub EVENT_COMBAT
 {
 	if($combat_state == 0)
 	{
-		quest::signalwith(365057, 2, 0);	
+		quest::signalwith(365057, 2, 0);
+		quest::stoptimer("rage");
+		quest::stoptimer("kill");
 	}
 	elsif($combat_state == 1)
 	{
 		quest::signalwith(365057, 1, 0);
+		$npc->SetEntityVariable(1, 0);
+		$npc->SetEntityVariable(2, 1);
+		quest::shout("Bow down urchins!");
 	}
 }
 
 sub EVENT_DEATH
 {
 	quest::signalwith(365057, 3, 0);
+	quest::shout("The throne... usurped...");
+	quest::stoptimer("rage");
+	quest::stoptimer("kill");
 }
 
 sub EVENT_SIGNAL
 {
-	quest::emote("roars with rage as his comrade falls.");
+	if($signal == 1 || $signal == 2 || $signal == 3 || $signal == 4)
+	{
+		my $num_dead = ($npc->GetEntityVariable(1) + 1);
+		$npc->SetEntityVariable(1, $num_dead);
+		quest::emote("roars with rage as his comrade falls.");
+		quest::stoptimer("rage");
+		quest::settimer("rage", 15);
+		quest::modifynpcstat("attack_speed", "-50");
+	}
+	if($signal == 5)
+	{
+		quest::shout("The progress is amusing don't you think, Duke Rumith?")
+	}
+}
+
+sub EVENT_TIMER
+{
+	if($timer eq "rage")
+	{
+		quest::stoptimer("rage");
+		quest::modifynpcstat("attack_speed", "0");
+	}
+	
+	if($timer eq "kill")
+	{
+		$npc->SetEntityVariable(2, 1);
+		quest::stoptimer("kill");
+	}
+}
+
+sub EVENT_SLAY
+{
+	my $can_shout = $npc->GetEntityVariable(2);
+	if($can_shout == 1)
+	{
+		quest::settimer("kill", 3);
+		$npc->SetEntityVariable(2, 0);
+		quest::shout("That is why I am king!");
+	}
 }

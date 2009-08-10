eb3bab10905ec1baaf47e5c053a58991d2690b75
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-08-10 05:10:58 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/erudnext/Tiam_Khonsir.pl
+++ quests/erudnext/Tiam_Khonsir.pl
@@ -26,58 +26,34 @@
 #
 
 sub EVENT_SAY {
-if($text=~/Hail/i){
-quest::say("Greetings. $name. Are you a follower of our order, the Deepwater Knights. servants of the Ocean Lord, Prexus?");
- }
-if($text=~/yes/i){
-quest::say("That is good. $name. Our followers are few but our faith is strong. With the efforts of those few, our destiny may be reached in time. We must walk with perseverence and devotion, much like the tides that, over time, can destroy a mighty stone cliff. Have you [come to serve our Lord], $name?");
- }
-if($text=~/i have come to serve our lord/i){
-quest::say("Then I shall send you on a quest to prove your devotion. No doubt you have seen the vermin kobolds scuttling about the land as fleas upon a mangy dog. The kobolds have a lair on our continent from which they launch their wantonly destructive raids. You are to enter that lair and destroy as many of these dogs as you can. For each molar you bring me. you will receive praise. admiration. and perhaps some coin.");
- }
-if($text=~/quest of greater importance/i){
-quest::say("Very well then, $name, you may be able to help us. Go to Breya and tell her I sent you. She will brief you.")
- }
+  if($text=~/hail/i) {
+    quest::say("Greetings, $name. Are you a follower of our order, the Deepwater Knights, servants of the Ocean Lord, Prexus?");
+  }
+  if($text=~/yes/i) {
+    quest::say("That is good, $name. Our followers are few but our faith is strong. With the efforts of those few, our destiny may be reached in time. We must walk with perseverence and devotion, much like the tides that, over time, can destroy a mighty stone cliff. Have you [come to serve our Lord], $name?");
+  }
+  if($text=~/come to serve our lord/i) {
+    quest::say("Then I shall send you on a quest to prove your devotion. No doubt you have seen the vermin kobolds scuttling about the land as fleas upon a mangy dog. The kobolds have a lair on our continent from which they launch their wantonly destructive raids. You are to enter that lair and destroy as many of these dogs as you can. For each molar you bring me, you will receive praise, admiration, and perhaps some coin.");
+  }
+  if($text=~/quest of greater importance/i){
+    quest::say("Very well then, $name, you may be able to help us. Go to Breya and tell her I sent you. She will brief you.")
+  }
 }
 
 sub EVENT_ITEM {
+  my $random_result = int(rand(4));
 
-my $random_result = int(rand(4));
-
-if(plugin::check_handin(\%itemcount, 1761=> 1) && $random_result==1){
-
-quest::say("'Wonderful work, friend $name. One less land dwelling, flea ridden, primitive for our wondrous lord to sweep aside when the apocalypse is upon us. Here is your reward. Perhaps if you gather enough molars, we can offer you a [quest of greater importance].");
-
-quest::summonitem(quest::ChooseRandom(3120,3123,3115,3117,3122,3113,3116,3118,3121,3124,3119,3108,3107,3111,3103,3105,3110,3101,3104,3106,3109,3112,3132,3135,3127,3129,3134,3125,3128,3130,3133,3136,3131));
-
-# XP Award
-quest::exp(2000);
-# Pay out.
-quest::givecash("12","14","5","0");
-# High Council of Erudin Faction
-quest::faction("145","5");
-# Heretics faction
-quest::faction("143","-5");
-# Deepwater Knights faction
-quest::faction("79","5");
-}
-
-if(plugin::check_handin(\%itemcount, 1761=> 1)){
-
-quest::say("'Wonderful work, friend $name. One less land dwelling, flea ridden, primitive for our wondrous lord to sweep aside when the apocalypse is upon us. Here is your reward. Perhaps if you gather enough molars, we can offer you a [quest of greater importance].");
-
-# XP Award
-quest::exp(2000);
-# Pay out.
-quest::givecash("12","14","5","0");
-# High Council of Erudin Faction
-quest::faction("145","5");
-# Heretics faction
-quest::faction("143","-5");
-# Deepwater Knights faction
-quest::faction("79","5");
-   }
-   
+  if(plugin::check_handin(\%itemcount, 1761=> 1)) {
+    quest::say("Wonderful work, friend $name. One less land dwelling, flea ridden, primitive for our wondrous lord to sweep aside when the apocalypse is upon us. Here is your reward. Perhaps if you gather enough molars, we can offer you a [quest of greater importance].");
+    if($random_result == 1) {
+      quest::summonitem(quest::ChooseRandom(3120,3123,3115,3117,3122,3113,3116,3118,3121,3124,3119,3108,3107,3111,3103,3105,3110,3101,3104,3106,3109,3112,3132,3135,3127,3129,3134,3125,3128,3130,3133,3136,3131));
+    }
+    quest::exp(2000);
+    quest::givecash(12,14,5,0); # Copper x 12, Silver x 14, Gold x 5
+    quest::faction(145,5); # High Council of Erudin
+    quest::faction(143,-5);# Heretics
+    quest::faction(79,5);  # Deepwater Knights
+  }
   #do all other handins first with plugin, then let it do disciplines
   plugin::try_tome_handins(\%itemcount, $class, 'Paladin');
   plugin::return_items(\%itemcount);

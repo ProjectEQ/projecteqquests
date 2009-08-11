6003ed9418470e97a7a5aa097fc5705ce1c9d477
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-08-11 06:43:46 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/tutorialb/Xenaida_Akh-zrial.pl
+++ quests/tutorialb/Xenaida_Akh-zrial.pl
@@ -1,8 +1,27 @@
+sub EVENT_SPAWN
+{
+	$npc->SetAppearance(1);
+	quest::settimer("try_sit", 5);
+}
+
+sub EVENT_TIMER
+{
+	if($timer eq "try_sit")
+	{
+		$npc->SetAppearance(1);
+	}
+}
+
+sub EVENT_DEATH
+{
+	quest::stoptimer("try_sit");
+}
+
 sub EVENT_SAY {
    if ($text=~/hail/i){
      quest::say("Greetings $name. I need some mushrooms to finish my healing potion for the wounded slaves. I marked the location of some mushrooms on your map. Can you bring one to me. I am rather busy here at the moment.");
    }
-} 
+}
 
 sub EVENT_ITEM {
    if (plugin::check_handin(\%itemcount, 59955 => 1) && quest::istaskactivityactive(22,3)) {

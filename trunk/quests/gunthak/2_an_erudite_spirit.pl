ace563fd5fc78365e725d6125f9bc20b8bfd1485
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-08-20 02:28:22 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/gunthak/2_an_erudite_spirit.pl
+++ trunk/quests/gunthak/2_an_erudite_spirit.pl
@@ -3,14 +3,31 @@
 #note; I did this quest as per the original, "Titanium" version
 #Male erudite spirit for Quest: Ethereal Cleansing
 
-sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount,59027 => 1)){ #Black Skull Necklace
-    quest::summonitem(59003); #Spell: Ethereal Cleansing
-    quest::summonitem(59028); #Glowing Black Skull Necklace
-    quest::depop();
-  }
-  else {
-    quest::emote("seems to ignore you."); #Text made up
-    plugin::return_items(\%itemcount);
-  }
+sub EVENT_ITEM
+{
+        if (plugin::check_handin(\%itemcount,59027 => 1)) #Black Skull Necklace
+        {
+                quest::emote("looks at you with piercing eyes that tear through your heart. His ethereal fingers close around the necklace. A blinding light surrounds both the necklace and the transparent figure as you shield your eyes away. When the light dies down you look to see the necklace, now softly glowing, sitting on the sand. As you pick it up you notice a wet scroll half buried in the sand beneath it. A voice echoes in your mind. 'Find him.'");
+
+                quest::summonitem(59003); #Spell: Ethereal Cleansing
+                quest::summonitem(59028); #Glowing Black Skull Necklace
+
+                quest::delglobal("EthCleanse");
+                quest::setglobal("EthCleanse",2,3,"F");
+                $qglobals{EthCleanse}=undef;
+
+                quest::depop();
+        }
+        else
+        {
+                quest::emote("seems to ignore you."); #Text made up
+                plugin::return_items(\%itemcount);
+        }
+}
+
+sub EVENT_DEATH
+{
+        quest::delglobal("EthCleanse");
+        quest::setglobal("EthCleanse",2,3,"F");
+        $qglobals{EthCleanse}=undef;
 }
--- trunk/quests/gunthak/a_savage_skeletal_sailor.pl
+++ trunk/quests/gunthak/a_savage_skeletal_sailor.pl
@@ -5,6 +5,7 @@ sub EVENT_DEATH
 {
         my $random_result = int(rand(100));
         my $a=224338;
+        my $b=224008;
 
         if(($random_result<=20) && (defined $qglobals{Alina}&& $qglobals{Alina}==2))
         {
@@ -22,4 +23,22 @@ sub EVENT_DEATH
         {
                 quest::emote("'s corpse shatters into fragments before you.");
         }
+
+        if(($random_result>20 && $random_result<=40) && (defined $qglobals{EthCleanse} && $qglobals{EthCleanse}==2))
+        {
+                quest::emote("'s corpse shatters into fragments before you.");
+
+                #spawn 2_an_erudite_spirit (224008)
+                quest::spawn2($b,0,0,-1100.0,-1100.1,19.2,192.6);
+
+                quest::delglobal("EthCleanse");
+                quest::setglobal("EthCleanse",3,3,"F");
+
+                $qglobals{EthCleanse}=undef;
+        }
+        else
+        {
+                quest::emote("'s corpse shatters into fragments before you.");
+        }
+
 }

fd27c0a7166ade8600375c4e09dd5e010a002635
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-08-22 15:02:54 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/paineel/player.pl
+++ quests/paineel/player.pl
@@ -1,5 +1,444 @@
+#This quest file links individual doors together so that double doors will open as one.
+
+sub EVENT_CLICKDOOR {
+ if($doorid == 4 || $doorid == 260) {
+  quest::forcedooropen(5);
+ }
+ if($doorid == 5 || $doorid == 261) {
+  quest::forcedooropen(4);
+ }
+ if($doorid == 6 || $doorid == 262) {
+  quest::forcedooropen(7);
+ }
+ if($doorid == 7 || $doorid == 263) {
+  quest::forcedooropen(6);
+ }
+ if($doorid == 35 || $doorid == 291) {
+  quest::forcedooropen(38);
+ }
+ if($doorid == 38 || $doorid == 294) {
+  quest::forcedooropen(35);
+ }
+ if($doorid == 36 || $doorid == 292) {
+  quest::forcedooropen(37);
+ }
+ if($doorid == 37 || $doorid == 293) {
+  quest::forcedooropen(36);
+ }
+ if($doorid == 39 || $doorid == 295) {
+  quest::forcedooropen(40);
+ }
+ if($doorid == 40 || $doorid == 296) {
+  quest::forcedooropen(39);
+ }
+ if($doorid == 41 || $doorid == 297) {
+  quest::forcedooropen(42);
+ }
+ if($doorid == 42 || $doorid == 298) {
+  quest::forcedooropen(41);
+ }
+ if($doorid == 43 || $doorid == 299) {
+  quest::forcedooropen(44);
+ }
+ if($doorid == 44 || $doorid == 300) {
+  quest::forcedooropen(43);
+ }
+ if($doorid == 45 || $doorid == 301) {
+  quest::forcedooropen(46);
+ }
+ if($doorid == 46 || $doorid == 302) {
+  quest::forcedooropen(45);
+ }
+ if($doorid == 47 || $doorid == 303) {
+  quest::forcedooropen(48);
+ }
+ if($doorid == 48 || $doorid == 304) {
+  quest::forcedooropen(47);
+ }
+ if($doorid == 49 || $doorid == 305) {
+  quest::forcedooropen(50);
+ }
+ if($doorid == 50 || $doorid == 306) {
+  quest::forcedooropen(49);
+ }
+ if($doorid == 51 || $doorid == 307) {
+  quest::forcedooropen(52);
+ }
+ if($doorid == 52 || $doorid == 308) {
+  quest::forcedooropen(51);
+ }
+ if($doorid == 56 || $doorid == 312) {
+  quest::forcedooropen(57);
+ }
+ if($doorid == 57 || $doorid == 313) {
+  quest::forcedooropen(56);
+ }
+ if($doorid == 58 || $doorid == 314) {
+  quest::forcedooropen(59);
+ }
+ if($doorid == 59 || $doorid == 315) {
+  quest::forcedooropen(58);
+ }
+ if($doorid == 60 || $doorid == 316) {
+  quest::forcedooropen(61);
+ }
+ if($doorid == 61 || $doorid == 317) {
+  quest::forcedooropen(60);
+ }
+ if($doorid == 62 || $doorid == 318) {
+  quest::forcedooropen(63);
+ }
+ if($doorid == 63 || $doorid == 319) {
+  quest::forcedooropen(62);
+ }
+ if($doorid == 65 || $doorid == 321) {
+  quest::forcedooropen(66);
+ }
+ if($doorid == 66 || $doorid == 322) {
+  quest::forcedooropen(65);
+ }
+ if($doorid == 67 || $doorid == 323) {
+  quest::forcedooropen(68);
+ }
+ if($doorid == 68 || $doorid == 324) {
+  quest::forcedooropen(67);
+ }
+ if($doorid == 69 || $doorid == 325) {
+  quest::forcedooropen(70);
+ }
+ if($doorid == 70 || $doorid == 326) {
+  quest::forcedooropen(69);
+ }
+ if($doorid == 75 || $doorid == 331) {
+  quest::forcedooropen(76);
+ }
+ if($doorid == 76 || $doorid == 332) {
+  quest::forcedooropen(75);
+ }
+ if($doorid == 77 || $doorid == 333) {
+  quest::forcedooropen(78);
+ }
+ if($doorid == 78 || $doorid == 334) {
+  quest::forcedooropen(77);
+ }
+ if($doorid == 102 || $doorid == 358) {
+  quest::forcedooropen(103);
+ }
+ if($doorid == 103 || $doorid == 359) {
+  quest::forcedooropen(102);
+ }
+ if($doorid == 104 || $doorid == 360) {
+  quest::forcedooropen(105);
+ }
+ if($doorid == 105 || $doorid == 361) {
+  quest::forcedooropen(104);
+ }
+ if($doorid == 107 || $doorid == 363) {
+  quest::forcedooropen(108);
+ }
+ if($doorid == 108 || $doorid == 364) {
+  quest::forcedooropen(107);
+ }
+ if($doorid == 109 || $doorid == 365) {
+  quest::forcedooropen(110);
+ }
+ if($doorid == 110 || $doorid == 366) {
+  quest::forcedooropen(109);
+ }
+ if($doorid == 111 || $doorid == 367) {
+  quest::forcedooropen(112);
+ }
+ if($doorid == 112 || $doorid == 368) {
+  quest::forcedooropen(111);
+ }
+ if($doorid == 113 || $doorid == 369) {
+  quest::forcedooropen(114);
+ }
+ if($doorid == 114 || $doorid == 370) {
+  quest::forcedooropen(113);
+ }
+ if($doorid == 115 || $doorid == 371) {
+  quest::forcedooropen(116);
+ }
+ if($doorid == 116 || $doorid == 372) {
+  quest::forcedooropen(115);
+ }
+ if($doorid == 118 || $doorid == 374) {
+  quest::forcedooropen(119);
+ }
+ if($doorid == 119 || $doorid == 375) {
+  quest::forcedooropen(118);
+ }
+ if($doorid == 124 || $doorid == 380) {
+  quest::forcedooropen(125);
+ }
+ if($doorid == 125 || $doorid == 381) {
+  quest::forcedooropen(124);
+ }
+ if($doorid == 126 || $doorid == 382) {
+  quest::forcedooropen(127);
+ }
+ if($doorid == 127 || $doorid == 383) {
+  quest::forcedooropen(126);
+ }
+ if($doorid == 128 || $doorid == 384) {
+  quest::forcedooropen(129);
+ }
+ if($doorid == 129 || $doorid == 385) {
+  quest::forcedooropen(128);
+ }
+ if($doorid == 130 || $doorid == 386) {
+  quest::forcedooropen(131);
+ }
+ if($doorid == 131 || $doorid == 387) {
+  quest::forcedooropen(130);
+ }
+ if($doorid == 132 || $doorid == 388) {
+  quest::forcedooropen(133);
+ }
+ if($doorid == 133 || $doorid == 389) {
+  quest::forcedooropen(132);
+ }
+ if($doorid == 134 || $doorid == 390) {
+  quest::forcedooropen(135);
+ }
+ if($doorid == 135 || $doorid == 391) {
+  quest::forcedooropen(134);
+ }
+ if($doorid == 136 || $doorid == 392) {
+  quest::forcedooropen(137);
+ }
+ if($doorid == 137 || $doorid == 393) {
+  quest::forcedooropen(136);
+ }
+ if($doorid == 149 || $doorid == 405) {
+  quest::forcedooropen(150);
+ }
+ if($doorid == 150 || $doorid == 406) {
+  quest::forcedooropen(149);
+ }
+ if($doorid == 151 || $doorid == 407) {
+  quest::forcedooropen(152);
+ }
+ if($doorid == 152 || $doorid == 408) {
+  quest::forcedooropen(151);
+ }
+ if($doorid == 158 || $doorid == 414) {
+  quest::forcedooropen(159);
+ }
+ if($doorid == 159 || $doorid == 415) {
+  quest::forcedooropen(158);
+ }
+ if($doorid == 162 || $doorid == 418) {
+  quest::forcedooropen(163);
+ }
+ if($doorid == 163 || $doorid == 419) {
+  quest::forcedooropen(162);
+ }
+ if($doorid == 164 || $doorid == 420) {
+  quest::forcedooropen(165);
+ }
+ if($doorid == 165 || $doorid == 421) {
+  quest::forcedooropen(164);
+ }
+ if($doorid == 166 || $doorid == 422) {
+  quest::forcedooropen(167);
+ }
+ if($doorid == 167 || $doorid == 423) {
+  quest::forcedooropen(166);
+ }
+ if($doorid == 168 || $doorid == 424) {
+  quest::forcedooropen(169);
+ }
+ if($doorid == 169 || $doorid == 425) {
+  quest::forcedooropen(168);
+ }
+ if($doorid == 170 || $doorid == 426) {
+  quest::forcedooropen(171);
+ }
+ if($doorid == 171 || $doorid == 427) {
+  quest::forcedooropen(170);
+ }
+ if($doorid == 175 || $doorid == 431) {
+  quest::forcedooropen(176);
+ }
+ if($doorid == 176 || $doorid == 432) {
+  quest::forcedooropen(175);
+ }
+ if($doorid == 177 || $doorid == 433) {
+  quest::forcedooropen(178);
+ }
+ if($doorid == 178 || $doorid == 434) {
+  quest::forcedooropen(177);
+ }
+ if($doorid == 179 || $doorid == 435) {
+  quest::forcedooropen(180);
+ }
+ if($doorid == 180 || $doorid == 436) {
+  quest::forcedooropen(179);
+ }
+ if($doorid == 182 || $doorid == 438) {
+  quest::forcedooropen(183);
+ }
+ if($doorid == 183 || $doorid == 439) {
+  quest::forcedooropen(182);
+ }
+ if($doorid == 184 || $doorid == 440) {
+  quest::forcedooropen(185);
+ }
+ if($doorid == 185 || $doorid == 441) {
+  quest::forcedooropen(184);
+ }
+ if($doorid == 186 || $doorid == 442) {
+  quest::forcedooropen(187);
+ }
+ if($doorid == 187 || $doorid == 443) {
+  quest::forcedooropen(186);
+ }
+ if($doorid == 188 || $doorid == 444) {
+  quest::forcedooropen(189);
+ }
+ if($doorid == 189 || $doorid == 445) {
+  quest::forcedooropen(188);
+ }
+ if($doorid == 191 || $doorid == 447) {
+  quest::forcedooropen(192);
+ }
+ if($doorid == 192 || $doorid == 448) {
+  quest::forcedooropen(191);
+ }
+ if($doorid == 193 || $doorid == 449) {
+  quest::forcedooropen(194);
+ }
+ if($doorid == 194 || $doorid == 450) {
+  quest::forcedooropen(193);
+ }
+ if($doorid == 195 || $doorid == 451) {
+  quest::forcedooropen(196);
+ }
+ if($doorid == 196 || $doorid == 452) {
+  quest::forcedooropen(195);
+ }
+ if($doorid == 197 || $doorid == 453) {
+  quest::forcedooropen(198);
+ }
+ if($doorid == 198 || $doorid == 454) {
+  quest::forcedooropen(197);
+ }
+ if($doorid == 199 || $doorid == 455) {
+  quest::forcedooropen(200);
+ }
+ if($doorid == 200 || $doorid == 456) {
+  quest::forcedooropen(199);
+ }
+ if($doorid == 201 || $doorid == 457) {
+  quest::forcedooropen(202);
+ }
+ if($doorid == 202 || $doorid == 458) {
+  quest::forcedooropen(201);
+ }
+ if($doorid == 203 || $doorid == 459) {
+  quest::forcedooropen(204);
+ }
+ if($doorid == 204 || $doorid == 460) {
+  quest::forcedooropen(203);
+ }
+ if($doorid == 205 || $doorid == 461) {
+  quest::forcedooropen(206);
+ }
+ if($doorid == 206 || $doorid == 462) {
+  quest::forcedooropen(205);
+ }
+ if($doorid == 207 || $doorid == 463) {
+  quest::forcedooropen(208);
+ }
+ if($doorid == 208 || $doorid == 464) {
+  quest::forcedooropen(207);
+ }
+ if($doorid == 209 || $doorid == 465) {
+  quest::forcedooropen(210);
+ }
+ if($doorid == 210 || $doorid == 466) {
+  quest::forcedooropen(209);
+ }
+ if($doorid == 211 || $doorid == 467) {
+  quest::forcedooropen(212);
+ }
+ if($doorid == 212 || $doorid == 468) {
+  quest::forcedooropen(211);
+ }
+ if($doorid == 213 || $doorid == 469) {
+  quest::forcedooropen(214);
+ }
+ if($doorid == 214 || $doorid == 470) {
+  quest::forcedooropen(213);
+ }
+ if($doorid == 215 || $doorid == 471) {
+  quest::forcedooropen(216);
+ }
+ if($doorid == 216 || $doorid == 472) {
+  quest::forcedooropen(215);
+ }
+ if($doorid == 217 || $doorid == 473) {
+  quest::forcedooropen(218);
+ }
+ if($doorid == 218 || $doorid == 474) {
+  quest::forcedooropen(217);
+ }
+ if($doorid == 219 || $doorid == 475) {
+  quest::forcedooropen(220);
+ }
+ if($doorid == 220 || $doorid == 476) {
+  quest::forcedooropen(219);
+ }
+ if($doorid == 221 || $doorid == 477) {
+  quest::forcedooropen(222);
+ }
+ if($doorid == 222 || $doorid == 478) {
+  quest::forcedooropen(221);
+ }
+ if($doorid == 223 || $doorid == 479) {
+  quest::forcedooropen(224);
+ }
+ if($doorid == 224 || $doorid == 480) {
+  quest::forcedooropen(223);
+ }
+ if($doorid == 225 || $doorid == 481) {
+  quest::forcedooropen(226);
+ }
+ if($doorid == 226 || $doorid == 482) {
+  quest::forcedooropen(225);
+ }
+ if($doorid == 227 || $doorid == 483) {
+  quest::forcedooropen(228);
+ }
+ if($doorid == 228 || $doorid == 484) {
+  quest::forcedooropen(227);
+ }
+ if($doorid == 229 || $doorid == 485) {
+  quest::forcedooropen(230);
+ }
+ if($doorid == 230 || $doorid == 486) {
+  quest::forcedooropen(229);
+ }
+ if($doorid == 231 || $doorid == 487) {
+  quest::forcedooropen(232);
+ }
+ if($doorid == 232 || $doorid == 488) {
+  quest::forcedooropen(231);
+ }
+ if($doorid == 235 || $doorid == 489) {
+  quest::forcedooropen(236);
+ }
+ if($doorid == 236 || $doorid == 490) {
+  quest::forcedooropen(235);
+ }
+}
+
 sub EVENT_ENTERZONE {
   if(($ulevel >= 15) && (!defined($qglobals{Wayfarer})) && ($client->GetStartZone()==$zoneid)) {
     $client->Message(15, "A mysterious voice whispers to you, 'Yenlr Undraie has just joined the Wayfarers Brotherhood and has some information about them, and how you can start doing odd jobs for them. You looked like the heroic sort, so I wanted to contact you . . . discreetly.'");
   }
 }
+
+#Submitted by Jim Mills

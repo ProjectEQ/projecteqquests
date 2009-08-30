da1822bf15e496891add593d0e3ef9cdc736cdb8
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-08-30 06:08:03 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/fearplane/Ireblind_Imp.pl
+++ quests/fearplane/Ireblind_Imp.pl
@@ -1,23 +1,8 @@
 # Ireblind_Imp.pl
 
-sub EVENT_COMBAT
+sub EVENT_SPAWN
 {
-	if($combat == 1)
-	{
-		my $cur_hp_ratio = $npc->GetHPRatio();
-		if($cur_hp_ratio > 75)
-		{
-			quest::setnexthpevent(76);
-		}
-		elsif($cur_hp_ratio > 50)
-		{
-			quest::setnexthpevent(51);
-		}
-		else
-		{
-			quest::setnexthpevent(26);
-		}
-	}
+	quest::setnexthpevent(76);
 }
 
 sub EVENT_HP
@@ -28,24 +13,24 @@ sub EVENT_HP
 	
 	if($hpevent == 76)
 	{
-		quest::spawn2(72068, 0, 0, $x_loc+1, $y_loc+1, $z_loc, 0);
-		quest::spawn2(72068, 0, 0, $x_loc-1, $y_loc-1, $z_loc, 0);
+		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
+		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
 		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
 		quest::setnexthpevent("51");
 	}
 	
 	if($hpevent == 51)
 	{
-		quest::spawn2(72068, 0, 0, $x_loc+1, $y_loc+1, $z_loc, 0);
-		quest::spawn2(72068, 0, 0, $x_loc-1, $y_loc-1, $z_loc, 0);
+		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
+		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
 		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
 		quest::setnexthpevent("26");
 	}
 	
 	if($hpevent == 26)
 	{
-		quest::spawn2(72068, 0, 0, $x_loc+1, $y_loc+1, $z_loc, 0);
-		quest::spawn2(72068, 0, 0, $x_loc-1, $y_loc-1, $z_loc, 0);
+		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
+		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
 		quest::spawn2(72068, 0, 0, $x_loc, $y_loc, $z_loc, 0);
 	}
 }

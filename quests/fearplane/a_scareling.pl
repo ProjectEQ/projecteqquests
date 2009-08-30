fa0518b40eeadbfd84dbb3e5652896eafb3fa40d
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-08-30 06:02:57 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/fearplane/a_scareling.pl
+++ quests/fearplane/a_scareling.pl
@@ -1,18 +1,21 @@
-sub EVENT_SIGNAL {
- if($signal == 0) {
-  quest::say("Such is the will of Cazic-Thule!");
- }
+sub EVENT_SIGNAL
+{
+	if($signal == 0) 
+	{
+		quest::say("Such is the will of Cazic-Thule!");
+	}
 }
 
 sub EVENT_DEATH
 
 {
-$spawn=int(rand(100));
-if($spawn > 88) {
-quest::spawn2(72108,0,0,$x,$y,$z,0);
-}
+	my $spawn = int(rand(100));
+	if($spawn > 66) 
+	{
+		quest::spawn2(72108,0,0,$x,$y,$z,0);
+	}
 }
 
 
 #Submitted by: Jim Mills
-#add on sub EVENT_DEATH for berserker epic spawn, 12% chance on death.
+#add on sub EVENT_DEATH for berserker epic spawn, 33% chance on death.

f5c406c75a38a1756a6fd0eac2abfc1b6c00c285
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-09-12 06:20:26 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/ecommons/Barstre_Songweaver.pl
+++ quests/ecommons/Barstre_Songweaver.pl
@@ -23,7 +23,7 @@ sub EVENT_SAY {
       }
       if($text=~/farstone/i) {
         $client->Message(15, "You have completed a step toward becoming a great adventurer. Well done!");
-        quest::say("Congratulations! I believe now that you are trustworthy enough to take on some of the adventures the Wayfarers Brotherhood have to give. We will be able to continue with our discussions once you complete some adventures. We can also help you with travel between our camps. Be well, fair Guiscard. I'm sure, someday, you will be a great addition to the Wayfarers Brotherhood.");
+        quest::say("Congratulations! I believe now that you are trustworthy enough to take on some of the adventures the Wayfarers Brotherhood have to give. We will be able to continue with our discussions once you complete some adventures. We can also help you with travel between our camps. Be well, fair $name. I'm sure, someday, you will be a great addition to the Wayfarers Brotherhood.");
         quest::faction(356,50); #Wayfarers Brotherhood
         quest::setglobal("Wayfarer",2,5,"F");
         quest::say("Take this Adventurer Stone with you on your journeys into the dungeons. You will find it useful. Should you lose it somehow, come talk to me and I'll replace it.");
@@ -35,14 +35,22 @@ sub EVENT_SAY {
         quest::say("Take this Adventurer Stone with you on your journeys into the dungeons. You will find it useful. Should you lose it somehow, come talk to me and I'll replace it.");
         quest::summonitem(41000); #Adventurer's Stone
       }
-      else { #PC has an Adventurer's Stone already
-        quest::say("Hello, $name. How do you like the adventures?"); #Text made up
+      else {
+#
+# Begin RUJ wins check
+#
+#      if($ruj_wins < 2) {
+        quest::say("Greetings, $name. I always have a story to tell, but I've been consumed with learning about the Rujarkian Hills. If you do enough work for us in those hills, I can share some of my knowledge with you!");
+#      }
+#      elsif($ruj_wins >= 74) {
+#        quest::say("Have you had your fill of story yet $name? You have spent a lot of time learning about the Rujarkian Hills. And, I can tell by the look in your eye that you know just as much as anyone about the orcs and goblins in those hills. I consider you a master of the story here as much as anyone. I have nothing to tell you, but you can stand by as I sing if you like. Be well, $name. I consider you a great adventurer and good friend. I'd fight with you by my side any day.");
+#      }
+#      elsif(player_has_maxed_out_stone) { #PC has maxed out stone
+#        $client->Message(9, "It's time to acknowledge you as a Wayfarer. Give Chaenz Abella in East Commonlands your Adventurer Stone and he'll replace it with your very own Wayfarers Emblem. Or, if it's being used in a charm, you must remove it to get your new one. And, if you simply destroy the stone, he will replace it with your much-deserved emblem. Just tell him you want your [emblem].");
+#        quest::setglobal("Wayfarer",3,5,"F");
+#      }
       }
     }
-#    elsif(player_has_maxed_out_stone) { #PC has maxed out stone
-#      $client->Message(9, "It's time to acknowledge you as a Wayfarer. Give Chaenz Abella in East Commonlands your Adventurer Stone and he'll replace it with your very own Wayfarers Emblem. Or, if it's being used in a charm, you must remove it to get your new one. And, if you simply destroy the stone, he will replace it with your much-deserved emblem. Just tell him you want your [emblem].");
-#      quest::setglobal("Wayfarer",3,5,"F");
-#    }
     else { #PC has not talked to hometown Wayfarer recruiter yet
       quest::say("You will have to excuse me, I am quite busy. Maybe you can talk to a Wayfarer in your home town?"); #Text made up
     }

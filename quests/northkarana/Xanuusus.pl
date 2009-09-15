310945000431ea3d2d918ff7e256013148c81dd1
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-09-15 05:26:14 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/northkarana/Xanuusus.pl
+++ quests/northkarana/Xanuusus.pl
@@ -1,4 +1,5 @@
 # earthcaller - ranger epic
+# naturewalker's scimitar - druid epic
 #
 
 sub EVENT_SAY {
@@ -8,26 +9,29 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
+# earthcaller
   if (plugin::check_handin(\%itemcount, 20483 => 1, 20484 => 1)) {
     quest::emote("waves his hands over the gem of corruption and a wave of warm energy passes through the area. With care for such a huge creature, he melds the gem into the sword and seems to glow all over as he channels energy into the weapon. 'Thus is the link of hate broken between Innoruuk and the spirit of earth. Through your will has balance returned. Your deeds, your work for the good of all, will be remembered by anyone who walks beneath the canopy of a forest or roams the plains. Continue your vigil over nature's balance.'");
     quest::summonitem(20488);
     quest::exp(2500000);
   }
-  if (plugin::check_handin(\%itemcount, 20699 => 1, 20697 => 1, 20698 => 1, 20440 => 1)) {
+# naturewalker's scimitar
+  elsif (plugin::check_handin(\%itemcount, 20699 => 1, 20697 => 1, 20698 => 1, 20440 => 1)) {
+    quest::emote("pushes the spirits into the air, making them dance around the scimitar. With a wave of his hand they melt into the blade and hilt of the weapon. 'With this the spirits of the lands are now free from the corruption of the dark gods.  Through your strength and dedication was this possible, $name.  May the foul poison that once poured into the earth itself never do so again. Take this weapon that you may continue to watch over the balance of nature. Go now, with the gods' blessings.'");
     quest::summonitem(20490);
     quest::exp(2500000);
   }
 # Hand in for Case of JumJum juice
-  if (plugin::check_handin(\%itemcount, 13411 => 1)){
-    quest::say("Heh heh! Excellent, small one!  My roots have been longing for some fresh jumjum juice!  Thank you!  Take this for your trouble.  Be sure to thank the Tagglefoots for me."); 
+  elsif (plugin::check_handin(\%itemcount, 13411 => 1)){
+    quest::say("Heh heh! Excellent, small one!  My roots have been longing for some fresh jumjum juice!  Thank you!  Take this for your trouble.  Be sure to thank the Tagglefoots for me.");
     quest::summonitem(quest::ChooseRandom(13977,84004,13083)); #carrot, fishing pole, pine needles
     quest::givecash(0,1,0,0);
-}
+  }
   else {
     quest::emote("will not accept this item.");
     plugin::return_items(\%itemcount);
   }
 }
 
-# EOF zone: northkarana ID: 13061 NPC: Xanuusus
+# EOF zone: northkarana ID: 13061 NPC: Xanuusus 
 
---
+++ quests/qeynos2/Koalindl.pl
@@ -0,0 +1,7 @@
+#This is actually Rodcet Nife in Koalindl fish form. He's here to make life hard for anyone trying to mass-farm Koalindl fish for the Stein of Moggok quest.
+
+sub EVENT_SLAY {
+ quest::say("That will teach you to interfere with the prime healer as he swims with his creatures!"); #Need actual text, but this is close
+}
+
+#Submitted by Jim Mills

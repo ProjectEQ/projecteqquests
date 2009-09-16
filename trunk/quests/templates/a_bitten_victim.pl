a4bf26a6c12887462317a63dc196e7ab0e871491
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-09-16 17:04:26 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/templates/a_bitten_victim.pl
+++ trunk/quests/templates/a_bitten_victim.pl
@@ -2,7 +2,9 @@ sub EVENT_SPAWN {
 	my $range = 11;
 	my $minimum = 1;
 	my $random_race = int(rand($range)) + $minimum;
+	my $random_gender = rand();
 	
+	quest::npcgender(random_gender);
 	quest::npcrace($random_race);
 	quest::settimer(1,1);
 }

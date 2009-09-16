68a2ff6ba1f5bad0c5ec92192a814140738719a5
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-09-16 06:21:21 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poknowledge/Borik_Darkanvil.pl
+++ trunk/quests/poknowledge/Borik_Darkanvil.pl
@@ -2,7 +2,7 @@ sub EVENT_SAY
 { 
 	if($text=~/Hail/i)
 	{
-		quest::say("Greetings. Borik Darkanvil. at your service.  I was once a planar traveler like you.  Those days are behind me. now I am content to earn my keep as a blacksmith. and to experiment with some of the [special recipes] that I have picked up in these outer planes."); 
+		quest::say("Greetings, Borik Darkanvil, at your service. I was once a planar traveler like you. Those days are behind me, now I am content to earn my keep as a blacksmith, and to experiment with some of the [special recipes] that I have picked up in these outer planes."); 
 	}
 	elsif($text=~/special recipes/i)
 	{

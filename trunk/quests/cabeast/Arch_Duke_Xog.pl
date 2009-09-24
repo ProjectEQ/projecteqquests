202babce7bdd2c0885c84d972f6f6783e198b4da
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-09-24 05:51:25 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/cabeast/Arch_Duke_Xog.pl
+++ trunk/quests/cabeast/Arch_Duke_Xog.pl
@@ -4,16 +4,16 @@ sub EVENT_SAY {
  if($text=~/hail/i){
   quest::say("These are the sacred unholy grounds of the Crusaders of Greenmist and the Scaled Mystics.  If you do not belong to us, you must leave this temple at once or learn great suffering in the name of Cazic-Thule.");
   }
- elsif($text=~/greenmist/i){ #Test of the Hero (Greenmist Quest 7/8)
+ elsif($text=~/greenmist/i){ #Test of the Lord (Greenmist Quest 7/8)
   quest::emote("appears surprised at your words. 'You know of Greenmist? The Unholy Khukri of Rile? We once had knowledge of this weapon, but when our great cities were destroyed in 1056 A.G., so, too, were all the great libraries. We lost all records. An explorer named [Argest] claimed to have found one library still intact. Pure babble.'");
   }
- elsif($text=~/argest/i){ #Test of the Hero (Greenmist Quest 7/8)
+ elsif($text=~/argest/i){ #Test of the Lord (Greenmist Quest 7/8)
   quest::say("Once a Lord of Pain, Argest is now a great explorer. No lizard has seen more of Kunark than he. He returned one season ago to tell tales of an [ancient library]. He said that he believed that there he would find a tome which would reveal the location of the ancient crusader weapon, Greenmist.");
   }
- elsif($text=~/ancient library/i){ #Test of the Hero (Greenmist Quest 7/8)
+ elsif($text=~/ancient library/i){ #Test of the Lord (Greenmist Quest 7/8)
   quest::say("There are many ancient libraries yet to be discovered. Our once great cities have been decimated, if not by our foes, then by nature itself. Within the outlands are many ruins which have yet to reveal themselves. We look forward to the discovery of these ruins by such explorers as Lord Argest the Great. If only we knew [where] he was...");
   }
- elsif($text=~/where/i){ #Test of the Hero (Greenmist Quest 7/8)
+ elsif($text=~/where/i){ #Test of the Lord (Greenmist Quest 7/8)
   quest::emote("becomes despondent at your question. 'Alas, our chance of locating Greenmist is lost as long as Argest remains missing. Reports have come in from the Legion's deep range patrols that he may be in the Frontier Mountain range. At least, that is where the patrol captain found Argest's walking staff. He might have been captured, killed or even digested!!'");
   }
  elsif ($text=~/strange iksar/i) {
@@ -33,7 +33,7 @@ sub EVENT_ITEM {
  quest::faction(62,20);
  quest::faction(193,10);
 }
- elsif(plugin::check_handin(\%itemcount, 18051 =>1, 5126 =>1)){ #Test of the Hero (Greenmist Quest 7/8)
+ elsif(plugin::check_handin(\%itemcount, 18051 =>1, 5126 =>1)){ #Test of the Lord (Greenmist Quest 7/8)
  quest::say("A legible tome of the scrolls of Vok Na Zov! What a find this is!! May the unholy curses of Cazic flow through you. Please accept the weapon of a Lord of Pain. To abandon it is to abandon our ways and earn yourself the hatred of our order.");
  quest::summonitem(5128);
  quest::givecash("0","0","0","4"); 

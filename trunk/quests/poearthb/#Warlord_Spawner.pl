492a88e82928246ca056c95fe91bfe0dd3aa49c3
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-01 01:22:58 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poearthb/#Warlord_Spawner.pl
+++ trunk/quests/poearthb/#Warlord_Spawner.pl
@@ -4,7 +4,7 @@ sub EVENT_SPAWN {
 sub EVENT_SIGNAL {
     $counter += 1;
     if ($counter == 3) {
-        quest::spawn2(222019,0,0,$x,$y,$z,h);
+        quest::spawn2(222019,0,0,$x,$y,$z,$h);
         $counter=0;
    }
 }

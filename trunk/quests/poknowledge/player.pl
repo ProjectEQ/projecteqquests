d216a6b98c205146688c0e5f04f9ee5e727bdd4b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-04 20:52:20 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poknowledge/player.pl
+++ trunk/quests/poknowledge/player.pl
@@ -2,4 +2,7 @@ sub EVENT_ENTERZONE {
   if (quest::istaskcompleted(138) == 0) { #Check if completed Task: New Beginnings
     quest::assigntask(138); #Force assign Task: New Beginnings
   }
+  if(defined $qglobals{$name}) {
+    quest::delglobal("$name");
+  }
 }

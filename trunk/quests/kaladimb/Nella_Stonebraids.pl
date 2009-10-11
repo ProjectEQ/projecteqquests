805af4768c395ae9b5be9ec44b1850b004e9b948
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-11 07:39:11 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/kaladimb/Nella_Stonebraids.pl
+++ trunk/quests/kaladimb/Nella_Stonebraids.pl
@@ -1,11 +1,15 @@
 sub EVENT_SAY {
-
-if ($text=~/hail/i) {
-quest::emote("Nella Stonebraids glances your direction, then returns to what she was doing.");
- }
-if ($text=~/Do you have Jarks Dinner/i) {
+  if ($text=~/hail/i) {
+    quest::emote("Nella Stonebraids glances your direction, then returns to what she was doing.");
+  }
+  if ($text=~/jark/i || $text=~/dinner/i) {
     quest::say("Ach! I forgot me husband's dinner. I'm so sorry. I was sendin' my wishes ta Brell for health and long life. I was just floatin' coins into the wishin' pool. Usually it's for children but things haven't been so good lately. Here, please take this to him and tell him where I was.");
-  quest::summonitem(29007);
-  quest::depop();
+    quest::summonitem(29007);
+    quest::depop();
+  }
 }
+
+sub EVENT_ITEM {
+  quest::say("I do not need this.");
+  plugin::return_items(\%itemcount);
 }

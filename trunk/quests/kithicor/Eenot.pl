9965316468a0935ceb5589dd37e62ea19df9930a
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-16 19:09:41 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/kithicor/Eenot.pl
@@ -0,0 +1,32 @@
+##########################################
+# ZONE: Kithicore Forest (kithicor)
+# DATABASE: PEQ-PoP
+# LAST EDIT DATE: July 6, 2009
+# VERSION: 1.0
+# DEVELOPER: Congdar
+#
+# *** NPC IFORMATION ***
+#
+# NAME: Reggit
+# ID: 20276
+# TYPE: Druid
+# RACE: Tiger
+# LEVEL: 18
+#
+# *** ITEMS GIVEN OR TAKEN ***
+#
+# *** QUESTS INVOLVED IN ***
+#
+# Grim's Tiger Revenge
+#
+# *** QUESTS AVAILABE TO ***
+#
+# Monks with good Ashen Order Faction
+#
+############################################# 
+
+sub EVENT_SPAWN {
+    quest::shout("A loud roar is heard within the woods!!");
+}
+#END of FILE Zone:kithicor ID:20226 -- Eenot
+

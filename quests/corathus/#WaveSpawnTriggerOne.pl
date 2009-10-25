b8c281a8b64d0d64b253dd1fac3fb371b3e99cf5
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-25 19:45:52 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/corathus/#WaveSpawnTriggerOne.pl
+++ quests/corathus/#WaveSpawnTriggerOne.pl
@@ -7,23 +7,23 @@ sub EVENT_TIMER
 {
 	if($timer eq "wave")
 	{
-		quest::settimer("wave1", 1);
-		quest::settimer("wave2", 4);
-		quest::settimer("wave3", 8);
+		quest::settimer("bot1", 1);
+		quest::settimer("bot2", 4);
+		quest::settimer("bot3", 8);
 	}
-	elsif($timer eq "wave1")
+	elsif($timer eq "bot1")
 	{
-		quest::stoptimer("wave1");
+		quest::stoptimer("bot1");
 		quest::spawn2(365029, 0, 0, 140, -606, -47, 17);
 	}
-	elsif($timer eq "wave2")
+	elsif($timer eq "bot2")
 	{
-		quest::stoptimer("wave2");
+		quest::stoptimer("bot2");
 		quest::spawn2(365029, 0, 0, 140, -606, -47, 17);
 	}
-	elsif($timer eq "wave3")
+	elsif($timer eq "bot3")
 	{
-		quest::stoptimer("wave3");
+		quest::stoptimer("bot3");
 		quest::spawn2(365029, 0, 0, 140, -606, -47, 17);
 	}
 }

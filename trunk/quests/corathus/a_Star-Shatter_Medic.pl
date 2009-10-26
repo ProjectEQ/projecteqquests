2db3a5c36c990e49b7d80b041689af7856a35730
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-26 06:15:47 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/a_Star-Shatter_Medic.pl
+++ trunk/quests/corathus/a_Star-Shatter_Medic.pl
@@ -1,4 +1,35 @@
-sub EVENT_SAY
+$tic;
+sub EVENT_SPAWN
 {
-	quest::say("Insert Quest Text");
+	$tic = 0;
+}
+
+sub EVENT_WAYPOINT
+{
+	$tic++;
+	if($tic == 14)
+	{
+		my $rand_say = int(rand(5)) + 1;
+		if($rand_say == 1)
+		{
+			quest::say("So much carnage.");
+		}
+		elsif($rand_say == 2)
+		{
+			quest::say("Our soldiers can only take so much.");
+		}
+		elsif($rand_say == 3)
+		{
+			quest::say("Hang in there.");
+		}
+		elsif($rand_say == 4)
+		{
+			quest::say("These soldiers are exhausted, we must must get help soon.");
+		}
+		elsif($rand_say == 5)
+		{
+			quest::say("Hopefully these soldiers make it to see tomorrow.");
+		}
+		$tic = 0;
+	}
 }

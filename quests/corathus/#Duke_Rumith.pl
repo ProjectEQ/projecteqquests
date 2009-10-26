427b98cfd828485bc9afb4b7a74c3ef7adcb2e9c
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-26 02:57:42 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/corathus/#Duke_Rumith.pl
@@ -0,0 +1,82 @@
+$num_tics;
+
+sub EVENT_COMBAT
+{
+	if($combat_state == 0)
+	{
+		#out of combat
+		$npc->SetHP(4860000);
+	}
+	elsif($combat_state == 1)
+	{
+		#in combat
+		quest::settimer("dominate", 20);
+		quest::setnexthpevent(76);
+	}
+}
+
+sub EVENT_DEATH
+{
+	quest::stoptimer("dominate");
+	quest::stoptimer("arcane_blast");
+}
+
+sub EVENT_TIMER
+{
+	if($timer eq "dominate")
+	{
+		quest::stoptimer("dominate");
+		my @hatelist = $npc->GetHateList();
+		@hatelist = sort {$b->GetHate() <=> $a->GetHate()} @hatelist;
+		foreach $ent (@hatelist)
+		{
+			if($ent)
+			{
+				my $h_ent = $ent->GetEnt();
+				if($h_ent)
+				{
+					$npc->CastSpell(7028, $h_ent->GetID(), 10, 0);
+					last;
+				}
+			}
+		}
+		quest::settimer("dominate", 60);
+	}
+	elsif($timer eq "arcane_blast")
+	{
+		my @hatelist = $npc->GetHateList();
+		foreach $ent (@hatelist)
+		{
+			if($ent)
+			{
+				my $h_ent = $ent->GetEnt();
+				if($h_ent)
+				{
+					my $dmg = ($h_ent->GetMaxHP() * 0.21);
+					$h_ent->Damage($npc, $dmg, 1045, 4, false);
+				}
+			}
+		}
+		
+		$num_tics++;
+		
+		if($num_tics == 5)
+		{
+			quest::stoptimer("arcane_blast");
+		}
+	}
+}
+
+sub EVENT_HP
+{
+	if($hpevent == 76)
+	{
+		quest::setnexthpevent(51);
+	}
+	elsif($hpevent == 51)
+	{
+		quest::setnexthpevent(26);
+	}
+	$num_tics = 0;
+	quest::settimer("arcane_blast", 2);
+}

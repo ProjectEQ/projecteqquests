fbe7414796dac019f0b91fc4ba0dad81ad66cdc3
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-26 21:05:08 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/corathus/#Event_Control.pl
@@ -0,0 +1,44 @@
+sub EVENT_SAY
+{
+	my $is_gm = $client->GetGM();
+	if($is_gm)
+	{
+		if($text=~ /Enable/i)
+		{
+			if($text=~ /Start/i)
+			{
+				#enable spawn cond 1
+				#disable spawn cond 2
+				quest::spawn_condition("corathus", 1, 1);
+				quest::spawn_condition("corathus", 2, 0);
+			}
+			elsif($text=~ /Courtyard/i)
+			{
+				#enable spawn cond 2
+				#disable spawn cond 1
+				quest::spawn_condition("corathus", 2, 1);
+				quest::spawn_condition("corathus", 1, 0);
+			}
+		}
+		elsif($text=~ /Disable/i)
+		{
+			if($text=~ /Start/i)
+			{
+				#disable spawn cond 1
+				quest::spawn_condition("corathus", 1, 0);
+			}
+			elsif($text=~ /Courtyard/i)
+			{
+				#disable spawn cond 2
+				quest::spawn_condition("corathus", 2, 0);
+			}
+		}
+		else
+		{
+			quest::say("Options:");
+			quest::say("[Enable]/[Disable] + argument");
+			quest::say("Start");
+			quest::say("Courtyard");
+		}
+	}
+}

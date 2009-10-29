aa52bff6b4d14fa1a17d190ca33abddd910dbd98
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-29 02:34:33 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/corathus/#_.pl
@@ -0,0 +1,14 @@
+sub EVENT_SPAWN
+{
+	quest::settimer("try_sleep", 3);
+}
+
+sub EVENT_TIMER
+{
+	if($timer eq "try_sleep")
+	{
+		$npc->SetAppearance(3);
+	}
+	quest::stoptimer("try_sleep");
+	quest::settimer("try_sleep", 10);
+}

dd4ecfd85529878652fef2ebc287e6eacbbf23bb
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-30 23:56:04 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/Frenzied_Minotaur_Charger.pl
+++ trunk/quests/corathus/Frenzied_Minotaur_Charger.pl
@@ -10,7 +10,6 @@ sub EVENT_COMBAT
 	}
 	else
 	{
-		quest::say("Start Combat");
 		quest::settimer("charge", 20);
 	}
 }
@@ -19,15 +18,12 @@ sub EVENT_TIMER
 {
 	if($timer eq "charge")
 	{
-		quest::say("Charge Timer");
 		my $cur_target = $npc->GetTarget();
 		if($cur_target)
 		{
-			quest::say("Have Target");
 			my $dist = plugin::Dist($npc, $cur_target);
 			if($dist > 20)
 			{
-				quest::say("Outside range");
 				my $target_name = $cur_target->GetCleanName();
 				quest::emote("breaks his bonds and charges $target_name.");
 				quest::settimer("charge_end", 3);
@@ -35,19 +31,15 @@ sub EVENT_TIMER
 				$npc->BuffFadeByEffect(99); #root
 				quest::modifynpcstat("max_hit", 1980);
 				quest::modifynpcstat("min_hit", 840);
-				quest::modifynpcstat("runspeed", 3.75);
-			}
-			else
-			{
-				quest::say("Inside Range");
+				quest::modifynpcstat("runspeed", 2.5);
 			}
 		}
 	}
 	elsif($timer eq "charge_end")
 	{
-		quest::say("Charge End");
 		quest::modifynpcstat("max_hit", 990);
 		quest::modifynpcstat("min_hit", 420);
 		quest::modifynpcstat("runspeed", 1.25);
+		quest::stoptimer("charge_end");
 	}
 }

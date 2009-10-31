defa36b553b06a75dfe98e0109d81eecabb2a700
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-10-31 22:17:04 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/corathus/Suppression_Bot.pl
+++ trunk/quests/corathus/Suppression_Bot.pl
@@ -1,17 +1,4 @@
 sub EVENT_SPAWN
 {
 	quest::emote("begins to suppress the fires on Octo Assault Bot.");
-	quest::settimer("suppress", 1);
-}
-
-sub EVENT_TIMER
-{
-	if($timer eq "suppress")
-	{
-		my $octo = $entity_list->GetMobByNpcTypeID(365035);
-		if($octo)
-		{
-			$octo->HealDamage(2500, $npc);
-		}
-	}
 }

9ee069a7edb3cb5ca2ec635cf3f60ce5df0f0e6d
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-11-06 01:44:31 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poair/#Mist_Trigger.pl
+++ trunk/quests/poair/#Mist_Trigger.pl
@@ -1,69 +1,39 @@
-my $check = 0;
-my $counter = 0;
-
-sub EVENT_SPAWN {
-   $check = 0;
-   $counter = 0;
+sub EVENT_SPAWN 
+{
+	$counter = 0;
 }    
 
-sub EVENT_SIGNAL {
-     if ($signal == 1 && defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) {
-        $counter+=1;
-     	if ($signal == 1 && $counter == 4 && defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) {
+sub EVENT_SIGNAL 
+{
+	if ($signal == 1 && defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) 
+	{
+     		$counter+=1;
+     
+     		if ($counter == 4) 
+     		{
 			quest::spawn2(215070,0,0,323.7,-721.3,439.3,82.9);
-        }
-     }
-     if ($signal == 2 && defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) {
-        quest::spawn2(215071,0,0,-1571.6,-570.0,343.6,190.9);
+        	}
+     	}
+     	elsif ($signal == 2 && defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) 
+     	{
+        	quest::spawn2(215071,0,0,-1571.6,-570.0,343.6,190.9);
 		quest::depop(215023);
-     }
-     if ($signal == 3  && !defined($qglobals{mist_done})) {
-        quest::setglobal("mist_done",1,3,"D2");
-     }
-	 if ($signal == 4 && !defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) {
-	    $check_boss = $entity_list->GetMobByNpcTypeID(215058); #A_Phoenix_Breezewing
-		if ($check_boss) {
-		  $check = 1
+     	}
+     	elsif ($signal == 3  && !defined($qglobals{mist_done})) 
+     	{
+        	quest::setglobal("mist_done",1,3,"D2");
+     	}
+	elsif ($signal == 4 && !defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) 
+	{
+		#A_Phoenix_Breezewing, A_Wind_Pheonix X2, Servitor_of_Xegony, Spiritfrenzied_Phoenix, Ardent_Phoenix_of_Xegony, A_Phoenix_Searedwing, Calebgrothiel
+		if(!$entity_list->GetMobByNpcTypeID(215058) && !$entity_list->GetMobByNpcTypeID(215026) && !$entity_list->GetMobByNpcTypeID(215025) && !$entity_list->GetMobByNpcTypeID(215027) && !$entity_list->GetMobByNpcTypeID(215062) && !$entity_list->GetMobByNpcTypeID(215059) && !$entity_list->GetMobByNpcTypeID(215028) && !$entity_list->GetMobByNpcTypeID(215061)) 
+		{
+			quest::spawn2(215069,0,0,304.2,-792.2,441.6,15.2); 
+			quest::spawn2(215069,0,0,348.4,-632.2,442.3,140.1);
+			quest::spawn2(215069,0,0,391.7,-736.2,438.1,203.4);
+			quest::spawn2(215069,0,0,260,-699.8,444.5,75.5);
+			quest::setglobal("mist_event_start",1,3,"H2");
+			$counter=0;
 		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215026); #A_Wind_Pheonix
-		if ($check_boss) {
-		  $check = 1
-		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215025); #A_Wind_Pheonix
-		if ($check_boss) {
-		  $check = 1
-		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215027); #Servitor_of_Xegony
-		if ($check_boss) {
-		  $check = 1
-		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215062); #Spiritfrenzied_Phoenix
-		if ($check_boss) {
-		  $check = 1
-		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215059); #Ardent_Phoenix_of_Xegony
-		if ($check_boss) {
-		  $check = 1
-		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215028); #A_Phoenix_Searedwing
-		if ($check_boss) {
-		  $check = 1
-		}
-		$check_boss = $entity_list->GetMobByNpcTypeID(215061); #Calebgrothiel
-		if ($check_boss) {
-		  $check = 1
-		}
-		if($check == 0 && !defined($qglobals{mist_event_start}) && !defined($qglobals{mist_done})) {
-		quest::spawn2(215069,0,0,304.2,-792.2,441.6,15.2); 
-		quest::spawn2(215069,0,0,348.4,-632.2,442.3,140.1);
-		quest::spawn2(215069,0,0,391.7,-736.2,438.1,203.4);
-		quest::spawn2(215069,0,0,260,-699.8,444.5,75.5);
-		quest::setglobal("mist_event_start",1,3,"H2");
-		$counter=0;
-		}
-		if ($check == 1) {
-		  
-		}
-	 }
+	}
 }
-

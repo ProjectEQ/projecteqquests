98f41ed8c1af609d72244d249a72e360b5ff94b5
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-11-12 08:01:51 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/nexus/Velious_Scion.pl
+++ trunk/quests/nexus/Velious_Scion.pl
@@ -1,10 +1,9 @@
 sub EVENT_SAY { 
-if ($text=~/Hail/i){
-quest::say("Greetings $name . I am here to assist and watch over those who wish to return to the continent of Velious.  Due to the limited space I share this area with the portal to Odus. The [teleport system] back to Norrath is completely automated. I have no direct control over when the portal will turn on, although I can say that the system works on a schedule of approximately ten minutes."); 
+  if ($text=~/hail/i) {
+    quest::say("Greetings $name. I am here to assist and watch over those who wish to return to the continent of Velious. Due to the limited space I share this area with the portal to Odus. The [teleport system] back to Norrath is completely automated. I have no direct control over when the portal will turn on, although I can say that the system works on a schedule of approximately ten minutes."); 
+  }
+  if ($text=~/teleport system/i) {
+    quest::say("The teleport system is pretty simple from your perspective. Simply listen for the voice saying that the portals will become active soon. When the time nears, you should stand on the elevated pad you see next to me. Shortly after you see the message that the teleporters are active, you will be transported to your destination..");
+  }
 }
-if ($text=~/teleport system/i){
-quest::say("The teleport system is pretty simple from your perspective. Simply listen for the voice saying that the portals will become active soon. When the time nears, you should stand on the elevated pad you see next to me. Shortly after you see the message that the teleporters are active, you will be transported to your destination..");
- }
-}
-#END of FILE Zone:nexus  ID:152018 -- Velious_Scion 
-
+#END of FILE Zone:nexus  ID:152018 -- Velious_Scion

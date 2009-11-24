2cbe274489f9dfe11629f41bc1d8b332e336a152
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-11-24 04:53:52 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/kithicor/#Coercer_Q-ioul.pl
+++ trunk/quests/kithicor/#Coercer_Q-ioul.pl
@@ -1,17 +1,36 @@
 # Rogue Epic NPC -- #Coercer_Q'ioul
-
-sub EVENT_ITEM {
-  if($itemcount {28057} == 1) {
-    quest::say("The General will take care of you!");
-    quest::ding();
-    quest::exp(500);
+
+sub EVENT_SAY {
+  quest::emote("ignores you.");
+}
+
+
+sub EVENT_ITEM {
+
+  if(plugin::check_handin(\%itemcount, 28057 => 1)) {
+
+    quest::say("The General will take care of you!");
+
+    quest::ding();
+
+    quest::exp(500);
+
     quest::spawn2(20205,0,0,2316,797,275,193.5);
- }
-  else {
-    quest::emote("will not accept this item.");
-    plugin::return_items(\%itemcount);
- }
-}
-
-#END of FILE  Quest by: Solid11  Zone:kithicor  ID:20054 -- #Coercer_Q'ioul
-
+ }
+
+  else {
+
+    quest::emote("will not accept this item.");
+
+    plugin::return_items(\%itemcount);
+
+ }
+
+}
+
+
+
+#END of FILE  Quest by: Solid11  Zone:kithicor  ID:20054 -- #Coercer_Q'ioul
+
+
+

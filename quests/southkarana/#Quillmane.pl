f81fea78f504ed73bb4610547f4dcc5ecc09178e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2009-12-01 07:42:23 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/southkarana/#Quillmane.pl
+++ quests/southkarana/#Quillmane.pl
@@ -2,10 +2,12 @@
 #npc - #Quillmane
 #zone - SouthKarana
 #Angelox
+#Updated by Kilelen
 
 sub EVENT_DEATH{
- #quest::say("I'm dead");
- quest::delglobal("quill");
- quest::setglobal("quill",2,3,"F");
- $quill=undef;
-  };
+	#quest::say("I'm dead");
+	# If I was unspawnable and I died, make me spawnable again.
+	if (defined $qglobals{quill} && $qglobals{quill} == 3) {
+		quest::setglobal("quill",2,3,"F");
+	}
+}

fba9631145a18e2d2a16661a287d51e140d2344a
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-01-07 00:45:31 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poeartha/#Final_Trigger.pl
+++ trunk/quests/poeartha/#Final_Trigger.pl
@@ -6,10 +6,7 @@ sub EVENT_SPAWN {
 
 sub EVENT_TIMER {
 
-    if($timer eq "final" && defined $qglobals{vine_ring} && defined $qglobals{mud_ring} && defined $qglobals{stone_ring} && defined $qglobals{dust_ring} && !defined $qglobals{arbitor_of_earth}) {
-
-        quest::setglobal("arbitor_of_earth",1,3,"H4");
-
+    if($timer eq "final" && defined $qglobals{vine_ring} && defined $qglobals{mud_ring} && defined $qglobals{stone_ring} && defined $qglobals{dust_ring}) {
         quest::spawn2(218053,0,0,1520.9,-2745.2,6.1,188.2);
 
         }
--- trunk/quests/poknowledge/Malcolm_Thimbleton.pl
+++ trunk/quests/poknowledge/Malcolm_Thimbleton.pl
@@ -1,43 +1,48 @@
 #Malcolm_Thimbleton.pl
 #Tunic Conversion
 
+my $tunic_text;
+
+sub EVENT_SPAWN {
+  $tunic_text = "takes the tunic from you and peers at it. 'What'd you give me this for $name? It's clearly not my size! Oh! you wanted it modified that's right.' He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.";
+}
 
 sub EVENT_SAY {
-  if($text=~/Hail/i) {
-    quest::emote("reeks of elven wine and dwarven ale as you approach him. He's having a hard time putting a sentence together. \"Salut. . . sassaf. . .swee. . .erm. . .hello $name! *hic* I'm sure you're here to. . .to. . .What was I saying? *hic* oh yes! you're here to turn your silly robe into a proper tunic! And I'm just the gnome to do it! Now where'd I put my needle and thread. *hic* Give me that silly robe and I\'ll make a proper piece of clothing out of it.\'");
+  if($text=~/hail/i) {
+    quest::emote("reeks of elven wine and dwarven ale as you approach him. He's having a hard time putting a sentence together. 'Salut. . . sassaf. . .swee. . .erm. . .hello $name! *hic* I'm sure you're here to. . .to. . .What was I saying? *hic* oh yes! you're here to turn your silly robe into a proper tunic! And I'm just the gnome to do it! Now where'd I put my needle and thread. *hic* Give me that silly robe and I'll make a proper piece of clothing out of it.'");
   }
 }
   
   
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 26528 => 1)) {#Bile Stained Robes
-    quest::emote("takes the tunic from you and peers at it. \"What\'d you give me this for $name? It\'s clearly not my size! Oh! you wanted it modified that's right.\" He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.");
-    quest::summonitem(77503);#Bile Stained Jerkin
+
+  if(plugin::check_handin(\%itemcount, 26528 => 1)) { #Bile Stained Robes
+    quest::emote($tunic_text);
+    quest::summonitem(77503); #Bile Stained Jerkin
   }
-  if(plugin::check_handin(\%itemcount, 6073 => 1)) {#Cured Shade Silk Robe
-    quest::emote("takes the tunic from you and peers at it. \"What\'d you give me this for $name? It\'s clearly not my size! Oh! you wanted it modified that's right.\" He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.");
-    quest::summonitem(77501);#Cured Shade Silk Tunic
+  elsif(plugin::check_handin(\%itemcount, 6073 => 1)) { #Cured Shade Silk Robe
+    quest::emote($tunic_text);
+    quest::summonitem(77501); #Cured Shade Silk Tunic
   }
-  if(plugin::check_handin(\%itemcount, 29084 => 1)) {#Guardian Robe
-    quest::emote("takes the tunic from you and peers at it. \"What\'d you give me this for $name? It\'s clearly not my size! Oh! you wanted it modified that's right.\" He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.");
-    quest::summonitem(77504);#Guardian Jerkin
+  elsif(plugin::check_handin(\%itemcount, 29084 => 1)) { #Guardian Robe
+    quest::emote($tunic_text);
+    quest::summonitem(77504); #Guardian Jerkin
   }
-  if(plugin::check_handin(\%itemcount, 10845 => 1)) {#Robe of the Azure Sky
-    quest::emote("takes the tunic from you and peers at it. \"What\'d you give me this for $name? It\'s clearly not my size! Oh! you wanted it modified that's right.\" He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.");
-    quest::summonitem(77502);#Tunic of the Azure Sky
+  elsif(plugin::check_handin(\%itemcount, 10845 => 1)) { #Robe of the Azure Sky
+    quest::emote($tunic_text);
+    quest::summonitem(77502); #Tunic of the Azure Sky
   }
-  if(plugin::check_handin(\%itemcount, 2798 => 1)) {#Shade Silk Robe
-    quest::emote("takes the tunic from you and peers at it. \"What\'d you give me this for $name? It\'s clearly not my size! Oh! you wanted it modified that's right.\" He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.");
-    quest::summonitem(77500);#Shade Silk Tunic
+  elsif(plugin::check_handin(\%itemcount, 2798 => 1)) { #Shade Silk Robe
+    quest::emote($tunic_text);
+    quest::summonitem(77500); #Shade Silk Tunic
   }
-  if(plugin::check_handin(\%itemcount, 30858 => 1)) {#Shainai's Nightie
-    quest::emote("takes the tunic from you and peers at it. \"What\'d you give me this for $name? It\'s clearly not my size! Oh! you wanted it modified that's right.\" He turns and puts the robe on his work bench and slowly but surely goes to work on it. Half way through he spills some of his wine on it. Undaunted, he continues to work until he finally turns back to you and hands you a very well made tunic. Although it does smell of elven wine now.");
-    quest::summonitem(77505);#Shainai's Tunic
+  elsif(plugin::check_handin(\%itemcount, 30858 => 1)) { #Shainai's Nightie
+    quest::emote($tunic_text);
+    quest::summonitem(77505); #Shainai's Tunic
   }
   else {
    quest::say("I don't need this."); #text made up
    plugin::return_items(\%itemcount);
-   return 1;
   }
-    
-}#Done
+}
+#Done

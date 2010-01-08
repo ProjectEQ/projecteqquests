0d10c9c7c062a9e1b79dad8d304060abf254bd14
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-01-08 15:56:12 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poeartha/#Final_Trigger.pl
+++ trunk/quests/poeartha/#Final_Trigger.pl
@@ -3,11 +3,8 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_TIMER {
-    if($timer eq "final" && defined $qglobals{vine_ring_final} && defined $qglobals{mud_ring_final} && defined $qglobals{stone_ring_final} && defined $qglobals{dust_ring_final}) {
-        quest::delglobal("vine_ring_final");
-        quest::delglobal("mud_ring_final");
-        quest::delglobal("stone_ring_final");
-        quest::delglobal("dust_ring_final");
+    if($timer eq "final" && defined $qglobals{vine_ring_final} && defined $qglobals{mud_ring_final} && defined $qglobals{stone_ring_final} && defined $qglobals{dust_ring_final} && !defined $qglobals{arbitor_of_earth}) {
+        quest::setglobal("arbitor_of_earth", 1, 5, "H4");
         quest::spawn2(218053,0,0,1520.9,-2745.2,6.1,188.2);
         }
 }

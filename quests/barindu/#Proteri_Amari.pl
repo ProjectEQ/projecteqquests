c576a3298fd804ef08bee3ee4c3070ca87db9d6b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-01-28 22:14:40 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/barindu/#Proteri_Amari.pl
+++ quests/barindu/#Proteri_Amari.pl
@@ -2,26 +2,46 @@
 # Breakdown in communication 2
 
 # Created by gonner
+# Modified by Jaekob
 
 sub EVENT_SAY {
   if($text=~/hail/i) {
-    quest::emote(" glances around nervously. Leave me be. I can't be seen talking to outsiders.");
+  if ($faction <= 3) { # Require Warmly and greater faction
+  if (defined $qglobals{bic} && $qglobals{bic} == 5) {
+      quest::emote(" glances around nervously. Leave me be. I can't be seen talking to outsiders.");
+  }
+  }
   }
   if($text=~/talwin/i) {
+    if ($faction <= 3) { # Require Warmly and greater faction
+    if (defined $qglobals{bic} && $qglobals{bic} == 5) {
     quest::say("Yes ... Talwin. He and I were becoming fast friends until Ixvet took him. I am not sure what has happend to him, but if you would be [interested] in helping me I may be able to assist you in return.");
   }
+  }
+  }
   if($text=~/interested/i) {
+    if ($faction <= 3) { # Require Warmly and greater faction
+    if (defined $qglobals{bic} && $qglobals{bic} == 5) {
     quest::say("Good, good. As you may have noticed, the majority of these creatures are brutish types best suited for destruction. They are kept under control solely by strong [leadership].");
   }
+  }
+  }
   if($text=~/leadership/i) {
+    if ($faction <= 3) { # Require Warmly and greater faction
+    if (defined $qglobals{bic} && $qglobals{bic} == 5) {
     quest::say("The one who controls this area of the city goes by the name Ixvet Pox. She's as cruel as the rest, but considerably more intelligent which makes her even more dangerous. Fortunately, she's grown complacent and that gives us an [opportunity]. If we can destroy Ixvet, it will throw this area into chaos and we should be able to escape before order is restored.");
   }
+  }
+  }
   if($text=~/opportunity/i) {
+    if ($faction <= 3) { # Require Warmly and greater faction
+    if (defined $qglobals{bic} && $qglobals{bic} == 5) {
     quest::say("I've managed to get someone loyal to our cause placed within Ixvet Pox's entourage of personal servants and a skilled herbalist tends to her favorite fruits in the gardens. We will use this connection to poison the tyrant. Go talk with Kunimi Falade. You can find her working on the Hanging Gardens. Give her this ring as proof that you are there with my blessing and she will tell you what she needs.");
     quest::summonitem(64001); # Simple Stone Band
   }
 }
-
+}
+}
 
 sub EVENT_ITEM {
   if(plugin::check_handin(\%itemcount, 64016 => 1)) { #Crude Stone Ring
--- quests/tox/Assistant_T-os.pl
+++ quests/tox/Assistant_T-os.pl
@@ -1,12 +1,12 @@
 sub EVENT_SAY {
   if ($text=~/hail/i) {
     quest::say("Hmm. You aren't familiar. . .no matter. All is ready. Prepare yourself. Who knows what may happen. . .");
-    quest::spawn2(38173,0,0,1629,2115,-51);
+    quest::spawn2(38173,0,0,1630,2137,-54,135);
   }
   if (defined $qglobals{tsoma} && $qglobals{tsoma} == 2) {
     if ($text=~/hail/i) {
       quest::say("You're late! Never mind. Prepare yourself. I do not trust these magics T`soma has constructed.");
-      quest::spawn2(38173,0,0,1629,2115,-51);
+      quest::spawn2(38173,0,0,1630,2137,-54,135);
     }
   }
 }

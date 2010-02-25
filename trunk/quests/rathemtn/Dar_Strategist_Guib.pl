1b8fbca8762523899ab04b43d5e42d7db6ee1695
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-02-25 18:28:21 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/rathemtn/Dar_Strategist_Guib.pl
+++ trunk/quests/rathemtn/Dar_Strategist_Guib.pl
@@ -5,12 +5,27 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 67010 =>1 )) {
+  if (plugin::check_handin(\%itemcount, 67010 => 1)) { #Mug of Poisoned Ale
+    quest::me("You hand Guib the mug of ale laced with Froglok's Bane. He grins and downs the deadly syrup in one gulp.");
     quest::say("Thanks young one, that was just what I needed. Take this as a token of my gratitude. Perhaps you will join our ranks when you get older.");
-    quest::me("You hand Guib the mug of ale laced with Froglok's Bane. He grins and downs the deadly syrup in one gulp. ");
-    quest::faction( 264,-25 );
+    quest::summonitem(67009); #Insignia of the Gukta Knights
+    quest::faction(264,-25);
     quest::exp(100000);
     quest::me("The strategist shouts in pain, then there is a thud as his armor clatters to the floor. As he falls to the floor, he gasps for help in an insidious whisper. A portal opens and three frogloks appear. The poison has taken it's victim swiftly just as you were told. You grin to yourself evilly as Guktan healers rush to the aid of their fallen comrade. From this day forward Gukta will forever be changed.");
+    $npc->SetAppearance(3);
+    quest::settimer("depop",5);
+    #quest::signal(); #To be used later when quest working as intended
   }
-  plugin::return_items(\%itemcount);
+  else {
+    plugin::return_items(\%itemcount);
+  }
+}
+
+sub EVENT_TIMER {
+  quest::stoptimer("depop");
+  quest::depop();
+}
+
+sub EVENT_SIGNAL {
+  quest::depop();
 }

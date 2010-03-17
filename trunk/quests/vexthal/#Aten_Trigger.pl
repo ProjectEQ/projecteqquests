cd88eae61bf4bb3a26416cbb3d3f744b2b96898a
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-03-17 18:20:55 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/vexthal/#Aten_Trigger.pl
+++ trunk/quests/vexthal/#Aten_Trigger.pl
@@ -7,8 +7,9 @@ sub EVENT_SPAWN {
 sub EVENT_TIMER {
 	if($timer eq "aten") {
     		if (!$entity_list->GetMobByNpcTypeID(158014) && !$entity_list->GetMobByNpcTypeID(158010) && !$entity_list->GetMobByNpcTypeID(158015) && !$entity_list->GetMobByNpcTypeID(158012) && !$entity_list->GetMobByNpcTypeID(158013) && !$entity_list->GetMobByNpcTypeID(158007) && !$entity_list->GetMobByNpcTypeID(158008) && !$entity_list->GetMobByNpcTypeID(158011) && !$entity_list->GetMobByNpcTypeID(158009) &&  $entity_list->GetMobByNpcTypeID(158006) && !defined $qglobals{aten}) {
-      			quest::depop(158006);
+      			quest::depopall(158006);
       			quest::spawn2(158096,0,0,1412,0,248.63,192);
+                        quest::depop();
       		}
     	}
 }      

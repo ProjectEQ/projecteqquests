c70d3fb8de11bd16a7899d4490eb5357c316888e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-03-24 04:03:45 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/qeynos/Klieb_Torne.pl
+++ trunk/quests/qeynos/Klieb_Torne.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
 if($text=~/Hail/i){
-quest::say("Hey, $name!  Do I know you from somewhere?  Highpass?  Nah, you ain't who I thought you were.  If I were you I would just keep my trap shut and buy some drinks."); }
+quest::say("Hey, $name!  Do I know you from somewhere?  Highpass?  Nah, you ain't who I thought you were.  If I were you I would just keep my trap shut and buy some drinks."); 
+}
+if ($text=~/paw of opolla/i) {
+  quest::say("Heh? Why don't you mind your own business bub?! Buzz off!");
+}
 }
 #END of FILE Zone:qeynos  ID:1080 -- Klieb_Torne 
 

ec7e32e3123501623fca2ec0c3631f0dcddfc2a3
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-03-25 02:37:26 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/sharvahl/Historian_Sharar.pl
+++ quests/sharvahl/Historian_Sharar.pl
@@ -4,7 +4,19 @@ quest::say("It is a pleasure to meet you. friend.  Please let me know if you hav
 }
 if($text=~/yes/i){
 quest::say("You have a braver spirit than mine. friend. but I'm glad that someone is available to assist me.  Take care and keep alert.  The Khati Sha seem to be very troubled by something of late.");
-quest::summonitem("9061"); }
+quest::summonitem("9061");
+ }
 }
+
+sub EVENT_ITEM {
+  if (plugin::check_handin(\%itemcount, 8466 =>1 )) {
+  quest::say("I see that you've found the codger. He really should settle down and come in from the weather. I often find myself worrying about him these days. Adventure takes its toll on everyone. I'd like to see him settle down before the moor takes a final toll from him.");
+  quest::say("Well, I can't say that it isn't a pleasure to have his journal. He's documented the moor with such splendid detail. I cannot wait to see what he's written in these last few seasons. It is all so very helpful. Just take a look at these earplugs. They are a product of his last set of notes. He came up with the idea, while hunting owlbear in the moor. They are supposed to keep the sound they make from harming you. You can keep them, if you take this note to General Avilah for me.");
+  quest::summonitem(4461);
+  quest::summonitem(8467);
+  }
+  plugin::return_items(\%itemcount);
+}
+
 #END of FILE Zone:sharvahl  ID:155228 -- Historian_Sharar 
 

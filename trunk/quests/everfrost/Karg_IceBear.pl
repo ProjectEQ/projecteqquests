48effb962cdff4c49589dd80f2985c4f4071e8c1
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-03-30 02:14:37 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/everfrost/Karg_IceBear.pl
+++ trunk/quests/everfrost/Karg_IceBear.pl
@@ -7,6 +7,15 @@
 #   Werewolf Skin Cloak - by Darkonig
 #   Lupine Claw Gauntlets - by Darkonig
 
+sub EVENT_SPAWN {
+     quest::settimer(1,1800);
+}
+
+sub EVENT_TIMER {
+     quest::stoptimer(1);
+     quest::depop();
+}
+
 sub EVENT_SAY {
 
   if ($text=~/hail/i) {

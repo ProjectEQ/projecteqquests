71442ee1bd9d5634b86d41f663a2e214d434d6ac
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-03-31 01:21:30 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/lakerathe/Punga.pl
+++ trunk/quests/lakerathe/Punga.pl
@@ -1,6 +1,18 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Gimme duffynitey rocks! Me grindem GUUD!!!"); }
+sub EVENT_SAY {
+  if($text=~/hail/i) {
+    quest::say("Gimme duffynitey rocks! Me grindem GUUD!!!");
+  }
 }
-#END of FILE Zone:lakerathe  ID:51072 -- Punga 
 
+sub EVENT_ITEM {
+  if(plugin::check_handin(\%itemcount, 10073 => 1)) { #Dufrenite
+    quest::emote("takes the dufrenite and grinds it with his bare hands.");
+    quest::say("Here ya go!");
+    quest::summonitem(19051); #Ground Dufrenite
+  }
+  else {
+    quest::say("What dis?");
+    plugin::return_items(\%itemcount);
+  }
+}
+#END of FILE Zone:lakerathe  ID:51072 -- Punga
--- trunk/quests/lakerathe/Trug.pl
+++ trunk/quests/lakerathe/Trug.pl
@@ -1,6 +1,18 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Gimme duffynitey rocks! Me powdrem GUUD!!!"); }
+sub EVENT_SAY {
+  if($text=~/hail/i) {
+    quest::say("Gimme duffynitey rocks! Me powdrem GUUD!!!");
+  }
 }
-#END of FILE Zone:lakerathe  ID:51071 -- Trug 
 
+sub EVENT_ITEM {
+  if(plugin::check_handin(\%itemcount, 10073 => 1)) { #Dufrenite
+    quest::emote("takes the dufrenite and smashes it with her bare hands.");
+    quest::say("Here ya go!");
+    quest::summonitem(19052); #Powdered Dufrenite
+  }
+  else {
+    quest::say("What dis?");
+    plugin::return_items(\%itemcount);
+  }
+}
+#END of FILE Zone:lakerathe  ID:51071 -- Trug

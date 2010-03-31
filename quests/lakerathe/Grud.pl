f7db5e50227f05556dc34dbffad96b3a1f83146b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-03-31 01:21:30 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/lakerathe/Grud.pl
+++ quests/lakerathe/Grud.pl
@@ -1,6 +1,18 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Gimme duffynitey rocks! Me crushem GUUD!!!"); }
+sub EVENT_SAY {
+  if($text=~/hail/i) {
+    quest::say("Gimme duffynitey rocks! Me crushem GUUD!!!");
+  }
 }
-#END of FILE Zone:lakerathe  ID:51073 -- Grud 
 
+sub EVENT_ITEM {
+  if(plugin::check_handin(\%itemcount, 10073 => 1)) { #Dufrenite
+    quest::emote("takes the dufrenite and crushes it with his bare hands.");
+    quest::say("Here ya go!");
+    quest::summonitem(19050); #Crushed Dufrenite
+  }
+  else {
+    quest::say("What dis?");
+    plugin::return_items(\%itemcount);
+  }
+}
+#END of FILE Zone:lakerathe  ID:51073 -- Grud
--- quests/lakerathe/Kyralynn.pl
+++ quests/lakerathe/Kyralynn.pl
@@ -5,51 +5,47 @@
 
 
 sub EVENT_SAY {
- if($text=~/Hail/i){
-   quest::say("Hello. $name! I am the shaman Kyralynn. If you have a banded armor, my husband Vrynn and I can form it into a new shaman armor using this totem and some reagents for the ritual. I can use a banded [bracer]. [mail]. [helm] or [cloak] to form this new armor.");
- }
- if($text=~/bracer/i){
-   quest::say("The bracer requires a banded bracer. 1 dufrenite. a cauldron shell from an aqua goblin lord in Dagnor's Cauldron. and a bull elephant tooth.");
- }
- if($text=~/helm/i){
-   quest::say("The helm requires a banded helm. 1 ground dufrenite. a glacier bear pelt. and an ogreskin chamois from a lizard page.");
- }
- if($text=~/cloak/i){
-   quest::say("The cloak requires a banded cloak. 1 powdered dufrenite. basalt drake scales. and a driftwood pipe from an isle goblin chieftain.");
- }
- if($text=~/mail/i){
-   quest::say("The mail requires banded mail. 1 crushed dufrenite. a lancer swordfish fin. and Grizzleknot bark.");
- }
+  if($text=~/Hail/i){
+    quest::say("Hello. $name! I am the shaman Kyralynn. If you have a banded armor, my husband Vrynn and I can form it into a new shaman armor using this totem and some reagents for the ritual. I can use a banded [bracer], [mail], [helm] or [cloak] to form this new armor.");
+  }
+  if($text=~/bracer/i){
+    quest::say("The bracer requires a banded bracer, 1 dufrenite, a cauldron shell from an aqua goblin lord in Dagnor's Cauldron, and a bull elephant tooth.");
+  }
+  if($text=~/helm/i){
+    quest::say("The helm requires a banded helm, 1 ground dufrenite, a glacier bear pelt, and an ogreskin chamois from a lizard page.");
+  }
+  if($text=~/cloak/i){
+    quest::say("The cloak requires a banded cloak, 1 powdered dufrenite, basalt drake scales, and a driftwood pipe from an isle goblin chieftain.");
+  }
+  if($text=~/mail/i){
+    quest::say("The mail requires banded mail, 1 crushed dufrenite, a lancer swordfish fin, and Grizzleknot bark.");
+  }
 }
 
-sub EVENT_ITEM(){
- if (plugin::check_handin(\%itemcount, 3061 => 1, 10073 => 1, 19031 => 1, 19033 => 1)) {
-   quest::exp(200);
-   quest::summonitem(4945);
-   quest::say("I have crafted your bracer, use it well.");
- }
- elsif (plugin::check_handin(\%itemcount, 3053 => 1, 19051 => 1, 19042 => 1, 19040 => 1)) {
-   quest::exp(200);
-   quest::summonitem(4947);
-   quest::say("I have crafted your helm, use it well.");
- }
- elsif (plugin::check_handin(\%itemcount, 3058 => 1, 19052 => 1, 19032 => 1, 19035 => 1)) {
-   quest::exp(200);
-   quest::summonitem(4948);
-   quest::say("I have crafted your cloak, use it well.");
- }
- elsif (plugin::check_handin(\%itemcount, 3056 => 1, 19050 => 1, 19036 => 1, 19044 => 1)) {
-   quest::exp(200);
-   quest::summonitem(4946);
-   quest::say("I have crafted your breastplate, use it well.");
- }
- else {
-   quest::say("You have given me incomplete or the wrong reagents.");
-   plugin::return_items(\%itemcount);
- }
+sub EVENT_ITEM {
+  if (plugin::check_handin(\%itemcount, 3061 => 1, 10073 => 1, 19031 => 1, 19033 => 1)) {
+    quest::exp(200);
+    quest::summonitem(4945);
+    quest::say("I have crafted your bracer, use it well.");
+  }
+  elsif (plugin::check_handin(\%itemcount, 3053 => 1, 19051 => 1, 19042 => 1, 19040 => 1)) {
+    quest::exp(200);
+    quest::summonitem(4947);
+    quest::say("I have crafted your helm, use it well.");
+  }
+  elsif (plugin::check_handin(\%itemcount, 3058 => 1, 19052 => 1, 19032 => 1, 19035 => 1)) {
+    quest::exp(200);
+    quest::summonitem(4948);
+    quest::say("I have crafted your cloak, use it well.");
+  }
+  elsif (plugin::check_handin(\%itemcount, 3056 => 1, 19050 => 1, 19036 => 1, 19044 => 1)) {
+    quest::exp(200);
+    quest::summonitem(4946);
+    quest::say("I have crafted your breastplate, use it well.");
+  }
+  else {
+    quest::say("You have given me incomplete or the wrong reagents.");
+    plugin::return_items(\%itemcount);
+  }
 }
-
-#END of FILE Zone:lakerathe  ID:51058 -- Kyralynn 
-
-
-
+#END of FILE Zone:lakerathe  ID:51058 -- Kyralynn

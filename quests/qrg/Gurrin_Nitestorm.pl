c2d5ab9b46d9d1cb1a96514e94a24db4cfd87035
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-06 04:21:54 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/qrg/Gurrin_Nitestorm.pl
+++ quests/qrg/Gurrin_Nitestorm.pl
@@ -18,13 +18,13 @@ sub EVENT_SAY {
   }
  
    if ($text=~/guide/i) {
-    quest::say("'Indeed, my friend. It seems that Kaithys has chosen you to do so and I shall do what I can to abide by his wishes. I would advise that you return to Kaithys and show him the purified blade. He knows much better than I what stones must now be overturned for his intentions of you to be properly fulfilled.'");
+    quest::say("Indeed, my friend. It seems that Kaithys has chosen you to do so and I shall do what I can to abide by his wishes. I would advise that you return to Kaithys and show him the purified blade. He knows much better than I what stones must now be overturned for his intentions of you to be properly fulfilled.");
   }
 }
 
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 8908 => 1, 8911 => 1)){ # Writ of Thunder and Silver Blade of Rot
-    quest::emote("Gurrin Nitestorm examines the blade carefully, his manner seeming distant -- as if he were in a trance. The druid then turns to the ancient scroll and slowly begins to read the ancient druidic runes; his voice a soft chant and the rhythm of his incantation slow and powerful. As Gurrin weaves his spell the blade in his hands begins to bleed from the red gem in the hilt, which drains its color as the corruption is purged from its very nature. By the end of the druid's spell the gem in the blade is pure and white and the blade itself seems to have been altered -- sentient and purged of all evil. Gurrin extends the blade to Malvesti, 'The corruption is no more, although the essence of the blade has been returned to the powers that forged it. The Deathrot Knight will exist again and that I cannot change. However, the blade has been set upon a new path and all it needs now is a proper hand and will to [guide] it.'");
+    quest::emote("examines the blade carefully, his manner seeming distant -- as if he were in a trance. The druid then turns to the ancient scroll and slowly begins to read the ancient druidic runes; his voice a soft chant and the rhythm of his incantation slow and powerful. As Gurrin weaves his spell the blade in his hands begins to bleed from the red gem in the hilt, which drains its color as the corruption is purged from its very nature. By the end of the druid's spell the gem in the blade is pure and white and the blade itself seems to have been altered -- sentient and purged of all evil. Gurrin extends the blade to Malvesti, 'The corruption is no more, although the essence of the blade has been returned to the powers that forged it. The Deathrot Knight will exist again and that I cannot change. However, the blade has been set upon a new path and all it needs now is a proper hand and will to [guide] it.'");
     quest::exp(1000);
     quest::ding();
     quest::faction(271,20);  #Residents of Jaggedpine

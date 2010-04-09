eaf3add9cc9c28af835893c81ab87678faaa1192
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-09 03:57:15 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poearthb/#Warlord_Spawner.pl
+++ trunk/quests/poearthb/#Warlord_Spawner.pl
@@ -1,5 +1,5 @@
 sub EVENT_SIGNAL {
-    if (defined $qglobals{poeb_Galronor} && defined $qglobals{poeb_Awisano} && defined $qglobals{poeb_Birak}) {
+    if (defined $qglobals{poeb_Galronar} && defined $qglobals{poeb_Awisano} && defined $qglobals{poeb_Birak}) {
         quest::spawn2(222019,0,0,$x,$y,$z,$h);
    }
 }

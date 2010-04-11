70b2d7c687f11e158484796c3f38a7d173b1b4bf
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-11 03:40:53 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/poinnovation/#Researcher_Kaeon.pl
@@ -0,0 +1,11 @@
+sub EVENT_SAY {
+  if ($text=~/hail/i) {
+    quest::say("Salutations. We have been monitoring your performance in the scrap yards. Your ability seems to rival your physical capabilities. We would like to test your endurance and mental abilities further. Would you comply to endurance testing?");
+  }
+  if ($text=~/I will comply/i) {
+    quest::say("'Excellent. We would like to test a maximum of six at one time. Are you ready to begin testing?");
+  }
+  if ($text=~/ready/i) {
+    quest::say("Excellent I will now send you down to the testing bay. Assistant Kelrig will be there shortly with further instructions.");
+  }
+}

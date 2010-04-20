bd9df6d7a32b5e60aff8066bd830ad4042592691
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-20 23:57:08 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/guildhall/#Porter.pl
+++ quests/guildhall/#Porter.pl
@@ -5,92 +5,92 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_ENTER {
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 10092) { #hateplane
+	if(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 10092) { #hateplane
 		quest::selfcast(666);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 10094) { #airplane
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 10094) { #airplane
 		quest::selfcast(674);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 64191) { #dragonscale
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 64191) { #dragonscale
 		#quest::selfcast(15891);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76000) { #karana
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76000) { #karana
 		quest::selfcast(2708);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76001) { #commons
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76001) { #commons
 		quest::selfcast(4176);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76002) { #lavastorm
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76002) { #lavastorm
 		quest::selfcast(534);
-		quest::delglobal("ghport$uguild_id");
+		
 	} 
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76003) { #toxxulia
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76003) { #toxxulia
 		quest::selfcast(2707);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76004) { #faydark
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76004) { #faydark
 		quest::selfcast(2706);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76005) { #dreadlands
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76005) { #dreadlands
 		quest::selfcast(2709);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76006) { #iceclad
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76006) { #iceclad
 		quest::selfcast(2284);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76007) { #cobalt
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76007) { #cobalt
 		quest::selfcast(2031);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76009) { #twilight
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76009) { #twilight
 		quest::selfcast(3615);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76010) { #stonebrunt
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76010) { #stonebrunt
 		quest::selfcast(3794);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76011) { #slaughter
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76011) { #slaughter
 		#quest::selfcast(6180);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76012) { #barindu
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76012) { #barindu
 		#quest::selfcast(5733);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 76070) { #undershore
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 76070) { #undershore
 		#quest::selfcast(8237);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 88735) { #arcstone
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 88735) { #arcstone
 		#quest::selfcast(8967);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 88736) { #goru
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 88736) { #goru
 		#quest::selfcast();
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 88737) { #guild banner
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 88737) { #guild banner
 		#quest::selfcast();
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 88738) { #katta
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 88738) { #katta
 		quest::selfcast(11985);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 88739) { #time
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 88739) { #time
 		#quest::selfcast(20543);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
-	if(defined $qglobals{ghport$uguild_id} && $qglobals{ghport$uguild_id} == 88740) { #brell
+	elsif(defined $qglobals{"ghport$uguild_id"} && $qglobals{"ghport$uguild_id"} == 88740) { #brell
 		#quest::selfcast(21986);
-		quest::delglobal("ghport$uguild_id");
+		
 	}
 }
--- quests/guildhall/Zeflmin_Werlikanin.pl
+++ quests/guildhall/Zeflmin_Werlikanin.pl
@@ -1,106 +1,103 @@
 sub EVENT_SAY {
+$myvalue = $qglobals{"ghport$uguild_id"};
 	if ($text=~/hail/i) {
-    		quest::say("Hello $name. Come to take a look at the crystal geode? The geode has some very strong magic inside of it that can help you. I can bring out the magic of the geode for you, but a gnome has to make a living! Even if he's stuck in this little hole in the wall all the time. Not that I'm bitter. Each of the crystals I sell is linked to a location in our world. If you give me one of the crystals I can focus the geode to let you and your companions travel there in the blink of an eye!"); 
-  	}
+    		quest::say("$myvalue Hello $name. Come to take a look at the crystal geode? The geode has some very strong magic inside of it that can help you. I can bring out the magic of the geode for you, but a gnome has to make a living! Even if he's stuck in this little hole in the wall all the time. Not that I'm bitter. Each of the crystals I sell is linked to a location in our world. If you give me one of the crystals I can focus the geode to let you and your companions travel there in the blink of an eye!"); 
+	}
 }
 
 sub EVENT_ITEM {
 	
 	if(plugin::check_handin(\%itemcount, 10092 => 1)){
 		quest::setglobal("ghport$uguild_id",10092,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 10094 => 1)){
 		quest::setglobal("ghport$uguild_id",10094,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 64191 => 1)){
 		quest::setglobal("ghport$uguild_id",64191,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76000 => 1)){
 		quest::setglobal("ghport$uguild_id",76000,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76001 => 1)){
 		quest::setglobal("ghport$uguild_id",76001,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76002 => 1)){
 		quest::setglobal("ghport$uguild_id",76002,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76003 => 1)){
 		quest::setglobal("ghport$uguild_id",76003,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76004 => 1)){
 		quest::setglobal("ghport$uguild_id",76004,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76005 => 1)){
 		quest::setglobal("ghport$uguild_id",76005,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76006 => 1)){
 		quest::setglobal("ghport$uguild_id",76006,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76007 => 1)){
 		quest::setglobal("ghport$uguild_id",76007,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76009 => 1)){
 		quest::setglobal("ghport$uguild_id",76009,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76010 => 1)){
 		quest::setglobal("ghport$uguild_id",76010,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76011 => 1)){
 		quest::setglobal("ghport$uguild_id",76011,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76012 => 1)){
 		quest::setglobal("ghport$uguild_id",76012,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 76070 => 1)){
 		quest::setglobal("ghport$uguild_id",76070,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 88735 => 1)){
 		quest::setglobal("ghport$uguild_id",88735,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 88736 => 1)){
 		quest::setglobal("ghport$uguild_id",88736,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 88737 => 1)){
 		quest::setglobal("ghport$uguild_id",88737,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 88738 => 1)){
 		quest::setglobal("ghport$uguild_id",88738,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 88739 => 1)){
 		quest::setglobal("ghport$uguild_id",88739,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	elsif(plugin::check_handin(\%itemcount, 88740 => 1)){
 		quest::setglobal("ghport$uguild_id",88740,3,"H24");
-		sub EVENT_PORTTEXT;
+		quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 	}
 	else {
 		quest::say("I have no use for this, $name.");
 		plugin::return_items(\%itemcount);
 	}
 
-}
-
-sub EVENT_PORTTEXT {
-	quest::emote("takes the crystal from you and mutters some arcane words over it. 'The crystal geode is now active, just go step on the crystal platform and you'll be whisked away to your destination! I hope you don't get motion sickness!'");
 }

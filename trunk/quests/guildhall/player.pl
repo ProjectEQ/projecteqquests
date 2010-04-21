cf6f067c0fd1c085287abbd76b3f8165a95131f7
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-21 02:17:40 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/guildhall/player.pl
+++ trunk/quests/guildhall/player.pl
@@ -1,30 +1,17 @@
-sub EVENT_ENTERZONE 
-{
-	if (quest::istaskcompleted(138) == 0 && quest::istaskactive(138) == 0) #Check if completed Task: New Beginnings
-	{
-		quest::assigntask(138); #Force assign Task: New Beginnings
-	}
-	
-	if(defined $qglobals{$name}) 
-	{
-		quest::delglobal("$name");
-	}	
-}
-
 sub EVENT_CLICKDOOR {
 	my $d_id = ($doorid % 256);
-  	if($d_id == 183) {
+  	if($d_id == 2) {
         	if($uguild_id > 0) {
             		if (defined($qglobals{"glinstance$uguild_id"})) {
                 		$guildinstance = $qglobals{"glinstance$uguild_id"};
                 		quest::AssignToInstance($guildinstance);
-                		quest::MovePCInstance(344, $guildinstance, 18, -46, 6);
+                		quest::MovePCInstance(344, $guildinstance, -2.3, 582, 0.2);
             		}
             		else {
                 		$guildinstance = quest::CreateInstance("guildlobby", 0, 86400);
                 		quest::AssignToInstance($guildinstance); 
                 		quest::setglobal("glinstance$uguild_id",$guildinstance,7,"H24");
-                		quest::MovePCInstance(344, $guildinstance, 18, -46, 6);
+                		quest::MovePCInstance(344, $guildinstance, -2.3, 582, 0.2);
             		}
         	}
     	}

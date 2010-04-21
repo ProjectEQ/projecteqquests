3b3ea2b93e82ca00ab3e36391275ae36ebe1ec88
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-21 23:13:52 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/guildhall/player.pl
@@ -0,0 +1,52 @@
+# Guild Hall Regen Pool Script - Player Portion
+
+sub EVENT_SIGNAL {
+
+   if ($signal == 1)
+   {
+      quest::settimer("DoRegen", 6);
+   }
+   
+   if ($signal == 2)
+   {
+      quest::stoptimer("DoRegen");
+   }
+   
+}
+
+sub EVENT_ZONE {
+
+   #Stop the timer if they zone
+   quest::stoptimer("DoRegen");
+
+}
+
+
+
+sub EVENT_TIMER {
+
+   if ($timer eq "DoRegen")
+   {
+      quest::stoptimer("DoRegen");
+      
+      # Set Regen Percent per tic here
+      my $RegenPercent = 10;
+      
+      if ($client) {
+         my $HPRatio = $client->GetHPRatio();
+         if ($HPRatio < 100) {
+            my $HPMax = $client->GetMaxHP();
+            my $HPNew = (($HPRatio + $RegenPercent) / 100) * $HPMax;
+            $client->SetHP($HPNew);
+         }
+         my $ManaRatio = $client->GetManaRatio();
+         if ($ManaRatio < 100) {
+            my $ManaMax = $client->GetMaxMana();
+            my $ManaNew = (($ManaRatio + $RegenPercent) / 100) * $ManaMax;
+            $client->SetMana($ManaNew);
+         }
+         quest::settimer("DoRegen", 6);
+      }
+   }
+
+}

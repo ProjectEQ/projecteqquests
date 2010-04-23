99131d244026426a63bb4c334a5ac48732309693
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-23 01:13:46 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/crescent/an_injured_drake.pl
@@ -0,0 +1,11 @@
+sub EVENT_SPAWN {
+my $min = 1;
+my $range = 4;
+my $randomspawn = int(rand($range)) + $min;
+plugin::RandomRoam(250, 250);
+quest::settimer(1,$randomspawn);
+}
+
+sub EVENT_TIMER {
+plugin::RandomRoam(250, 250);
+}

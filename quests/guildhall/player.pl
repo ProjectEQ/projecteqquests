619db387ae2a49390a5b14c193da627c67db525b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-04-25 21:36:54 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/guildhall/player.pl
+++ quests/guildhall/player.pl
@@ -56,3 +56,72 @@ sub EVENT_TIMER {
    }
 
 }
+
+sub EVENT_POPUPRESPONSE {
+	if($popupid == 666) {
+		quest::selfcast(666);
+	}
+	if($popupid == 674) {
+		quest::selfcast(674);
+	}
+	if($popupid == 15891) {
+		#quest::selfcast(15891);
+	}
+	if($popupid == 2708) {
+		quest::selfcast(2708);
+	}
+	if($popupid == 4176) {
+		quest::selfcast(4176);
+	}
+	if($popupid == 534) {
+		quest::selfcast(534);
+	}
+	if($popupid == 2707) {
+		quest::selfcast(2707);
+	}
+	if($popupid == 2706) {
+		quest::selfcast(2706);
+	}
+	if($popupid == 2709) {
+		quest::selfcast(2709);
+	}
+	if($popupid == 2284) {
+		quest::selfcast(2284);
+	}
+	if($popupid == 2031) {
+		quest::selfcast(2031);
+	}
+	if($popupid == 3615) {
+		quest::selfcast(3615);
+	}
+	if($popupid == 3794) {
+		quest::selfcast(3794);
+	}
+	if($popupid == 6180) {
+		#quest::selfcast(6180);
+	}
+	if($popupid == 5733) {
+		#quest::selfcast(5733);
+	}
+	if($popupid == 8237) {
+		#quest::selfcast(8237);
+	}
+	if($popupid == 8967) {
+		#quest::selfcast(8967);
+	}
+	if($popupid == 999) {
+		#quest::selfcast();
+	}
+	if($popupid == 1000) {
+		#quest::selfcast();
+	}
+	if($popupid == 160) {
+		quest::movepc(160,-545,645,1,0);
+	}
+	if($popupid == 20543) {
+		#quest::selfcast(20543);
+	}
+	if($popupid == 21986) {
+		#quest::selfcast(21986);
+	}
+}

25f65b45f7c1ad8611b34a85107cb0025d2190a2
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-18 03:45:18 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/neriakc/Ithvol_K-Jasn.pl
+++ quests/neriakc/Ithvol_K-Jasn.pl
@@ -35,7 +35,7 @@ sub EVENT_ITEM {
     quest::faction(258, -30); #Priests of Marr
     quest::faction(260, -30); #Primordial Malice
   }
-  elsif(plugin::check_handin(\%itemcount, 12499 => 1, 1369 => 1, 19029 => 1, 1370 => 1)) { #Voucher of Service to Naythox, Swiftmoon's Head, Receipt for Provisions Crate, Disciple Symbol of Innoruuk
+  elsif(plugin::check_handin(\%itemcount, 12499 => 1, 12498 => 1, 19029 => 1, 1370 => 1)) { #Voucher of Service to Naythox, Swiftmoon's Head, Receipt for Provisions Crate, Disciple Symbol of Innoruuk
     quest::say("Your devotion to the ideals of hatred and your service to our King Naythox Thex has proven your usefulness within these spires and within our great city. I award you the Regent Symbol of Innoruuk. Congratulations, Regent $name!");
     quest::summonitem(1371);#Regent Symbol of Innoruuk
     quest::exp(8000);

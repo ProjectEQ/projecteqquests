a08ca5eec4ed299eeb800a06d18e010df2b515a0
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-21 04:30:05 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poearthb/#Awisano_Spawner.pl
+++ trunk/quests/poearthb/#Awisano_Spawner.pl
@@ -1,19 +1,19 @@
 sub EVENT_SPAWN 
 {
-	quest::settimer("depop",60);
+	quest::settimer("depop3",60);
 }
 
 sub EVENT_TIMER 
 {
-	if($timer eq "depop" && defined $qglobals{poeb_warlord}) {
-		quest::stoptimer("depop");
+	if($timer eq "depop3" && defined $qglobals{poeb_warlord}) {
+		quest::stoptimer("depop3");
 		quest::depop();
 	}
     	else {
     		$check_boss = $entity_list->GetMobByNpcTypeID(222002);#myrmadon_of_stone
     		if (!$check_boss && !defined $qglobals{poeb_Awisano}) {
 			quest::spawn2(222016,0,0,$x,$y,$z,$h); 
-        		quest::stoptimer("depop");
+        		quest::stoptimer("depop3");
         		quest::depop();
 		}
 	}

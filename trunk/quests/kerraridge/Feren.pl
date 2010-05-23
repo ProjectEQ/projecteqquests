ce9d5dc5ed2655abfaf76bd5be385f983277ec0e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-23 23:33:04 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/kerraridge/Feren.pl
+++ trunk/quests/kerraridge/Feren.pl
@@ -1,12 +1,34 @@
+# Razortooth
+
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Rarr.  Me is Feren.  Me is fisher.  That means Feren catches [fish].");
+	if($text=~/Hail/i){
+		quest::say("Rarr.  Me is Feren.  Me is fisher.  That means Feren catches [fish].");
+	}
+	
+	if($text=~/what fish/i){
+		quest::say("All kinds of fish.  Tasty fish.  All day Feren catches the fishes so all Kerran can eat.  I do for good of all Kerran.  Rrrrr.  One thing bother Feren always though.  Is bad fish.  [Razortooth].");
+	}
+
+	if($text=~/what razortooth/i){
+		quest::say("Rrrrrr!  Razortooth mean fish.  Razortooth eat fish off Feren's line.  Razortooth gnaw Feren's boat.  Razortooth smart and dangerous.  Feren never able to catch Razortooth.  This haunt Feren for months.  If you was able to catch Razortooth. Feren be forever grateful. Maybe even give you possessions."); 
+	}
+
 }
-if($text=~/what fish/i){
-quest::say("All kinds of fish.  Tasty fish.  All day Feren catches the fishes so all Kerran can eat.  I do for good of all Kerran.  Rrrrr.  One thing bother Feren always though.  Is bad fish.  [Razortooth].");
+
+sub EVENT_ITEM {
+
+	if(($itemcount{6348} == 1)){ #Razor Tooth
+		quest::say("Rarr, Feren not haunted by Razortooth no more. Feren grateful and give you this possession.");
+		quest::summonitem(1062); #Kerran Fishingpole
+		quest::faction(175,10); # Kerra Isle
+		quest::givecash(0,0,0,2);
+
+   }
+   else {
+      plugin::return_items(\%itemcount); #return items if not the ones required
+   }
 }
-if($text=~/what razortooth/i){
-quest::say("Rrrrrr!  Razortooth mean fish.  Razortooth eat fish off Feren's line.  Razortooth gnaw Feren's boat.  Razortooth smart and dangerous.  Feren never able to catch Razortooth.  This haunt Feren for months.  If you was able to catch Razortooth. Feren be forever grateful. Maybe even give you possessions."); }
-}
+
+
 #END of FILE Zone:kerraridge  ID:74088 -- Feren 
 

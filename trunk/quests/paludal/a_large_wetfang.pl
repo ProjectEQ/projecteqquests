684519791247b950971948fc35a3c011a4cf200d
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-25 21:35:11 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/paludal/a_large_wetfang.pl
+++ trunk/quests/paludal/a_large_wetfang.pl
@@ -1,9 +1,9 @@
 sub EVENT_SPAWN {
 	my $min = 1;
-        my $range = 4;
+        my $range = 50;
 	my $randomspawn = int(rand($range)) + $min;
 	plugin::RandomSwim(200, 200, -392.2);
-	quest::settimer(1,$randomspawn);
+	quest::settimer($randomspawn,$randomspawn);
 
 }
 

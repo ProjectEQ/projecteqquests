96dfd1e012d76a86e5d923d7b9f3639a16c258f0
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-26 17:04:42 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/poearthb/#Birak_Spawner.pl
+++ quests/poearthb/#Birak_Spawner.pl
@@ -1,19 +1,19 @@
 sub EVENT_SPAWN 
 {
-	quest::settimer("depop1",59);
+	quest::settimer("depop",59);
 }
 
 sub EVENT_TIMER 
 {
 	if($timer eq "depop" && defined $qglobals{poeb_warlord}) {
-		quest::stoptimer("depop1");
+		quest::stoptimer("depop");
 		quest::depop();
 	}
     	else {
     		$check_boss = $entity_list->GetMobByNpcTypeID(222001);#stonefist_clansman
     		if (!$check_boss && !defined $qglobals{poeb_Birak}) {
 			quest::spawn2(222017,0,0,$x,$y,$z,$h);  
-        		quest::stoptimer("depop1");
+        		quest::stoptimer("depop");
         		quest::depop();
 		}
 	}
--- quests/poearthb/#Galronar_Spawner.pl
+++ quests/poearthb/#Galronar_Spawner.pl
@@ -1,19 +1,19 @@
 sub EVENT_SPAWN 
 {
-	quest::settimer("depop2",61);
+	quest::settimer("depop",61);
 }
 
 sub EVENT_TIMER 
 {
 	if($timer eq "depop" && defined $qglobals{poeb_warlord}) {
-		quest::stoptimer("depop2");
+		quest::stoptimer("depop");
 		quest::depop();
 	}
     	else {
     		$check_boss = $entity_list->GetMobByNpcTypeID(222000);#rock_studded_champ
     		if (!$check_boss && !defined $qglobals{poeb_Galronar}) {
 			quest::spawn2(222018,0,0,$x,$y,$z,$h);  
-        		quest::stoptimer("depop2");
+        		quest::stoptimer("depop");
         		quest::depop();
 		}
 	}

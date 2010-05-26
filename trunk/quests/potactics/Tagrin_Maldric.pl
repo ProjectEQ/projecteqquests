c643374f7ee023985d74aa51cbdaf4dd58eb5ce1
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-26 21:11:42 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/potactics/Tagrin_Maldric.pl
+++ trunk/quests/potactics/Tagrin_Maldric.pl
@@ -1,35 +1,16 @@
 #Tagrin Maldric is the blacksmith of the Zeks, and is part of obtaining Black Insanity (Zek bane) weapons. When aggro'd, he spawns 5 animated swords, who drop the actual ore for the bane weapons.
+my $addsUp = 0;
 
-sub EVENT_SPAWN {
-  my $x = $npc->GetX();
-  my $y = $npc->GetY();
-  my $z = $npc->GetZ();
-  my $h = $npc->GetHeading();
-  my $counter = 0;
+sub EVENT_COMBAT { 
+	if ($combat_state == 1 && $addsUp == 0){
+		quest::spawn2(214119,0,0,$x-15,$y-15,$z,$h);
+		quest::spawn2(214119,0,0,$x+15,$y-15,$z,$h);
+		quest::spawn2(214119,0,0,$x-15,$y+10,$z,$h);
+		quest::spawn2(214119,0,0,$x+15,$y+10,$z,$h);
+		quest::spawn2(214119,0,0,$x,$y-15,$z,$h);
+		$addsUp = 1;		
+	} elsif ($combat_state ==0 && $addsUp == 1) {
+		quest::depopall(214119);
+		$addsUp = 0;
+	}
 }
-
-sub EVENT_AGGRO {
- quest::depopall(214119);
- quest::settimer(1,1);
- if(!defined $counter || $counter == 0){
-  quest::spawn2(214119,0,0,$x-15,$y-15,$z,$h);
-  quest::spawn2(214119,0,0,$x+15,$y-15,$z,$h);
-  quest::spawn2(214119,0,0,$x-15,$y+10,$z,$h);
-  quest::spawn2(214119,0,0,$x+15,$y+10,$z,$h);
-  quest::spawn2(214119,0,0,$x,$y-15,$z,$h);
-  $counter = 1;
- }
-}
-
-sub EVENT_TIMER {
- if($timer == 1 && $mob->IsEngaged()) {}
-  else {
-  quest::stoptimer(1);
-  $counter = 0;
- }
-}
-sub EVENT_DEATH {
-  my $counter = 0;
-}
-
-#Submitted by Jim Mills

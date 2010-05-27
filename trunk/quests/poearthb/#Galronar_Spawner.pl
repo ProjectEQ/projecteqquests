0ce0d2d97e503821890df1d8460cad3b0fcee314
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-27 04:11:57 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poearthb/#Galronar_Spawner.pl
+++ trunk/quests/poearthb/#Galronar_Spawner.pl
@@ -5,7 +5,7 @@ sub EVENT_SPAWN {
 sub EVENT_TIMER {
     $check_boss = $entity_list->GetMobByNpcTypeID(222000);#rock_studded_champ
 	
-    if (!$check_boss && !defined $qglobals{poeb_Galronar} && !defined $qglobals{poeb_warlrod}) {
+    if (!$check_boss && !defined $qglobals{poeb_Galronar} && !defined $qglobals{poeb_warlord}) {
 			quest::spawn2(222018,0,0,$x,$y,$z,$h);  
         	quest::stoptimer("depop");
         	quest::depop();

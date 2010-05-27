0b30cc0bf05dee7316abce090de547b3972a0578
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-05-27 04:11:57 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/poearthb/#Birak_Spawner.pl
+++ quests/poearthb/#Birak_Spawner.pl
@@ -5,7 +5,7 @@ sub EVENT_SPAWN {
 sub EVENT_TIMER {
     $check_boss = $entity_list->GetMobByNpcTypeID(222001);#stonefist_clansman
 	
-    if (!$check_boss && !defined $qglobals{poeb_Birak}) {
+    if (!$check_boss && !defined $qglobals{poeb_Birak} && !defined $qglobals{poeb_warlord}) {
 		quest::spawn2(222017,0,0,$x,$y,$z,$h);  
         quest::stoptimer("depop");
         quest::depop();

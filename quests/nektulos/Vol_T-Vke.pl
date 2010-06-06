edd066ab0c072d7329782bc733eac19be9431cd9
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-06-06 08:46:53 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/nektulos/Vol_T-Vke.pl
@@ -0,0 +1,4 @@
+sub EVENT_SAY
+{
+    quest::say("Welcome to the gates of Neriak, $name. Please enter the city but keep a watchful eye for we may still have some Troll guests within. Try not to assault them. For now, you are ok to pass through unharmed.");
+}

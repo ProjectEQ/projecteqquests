4d0e152384be82c934d2671385f78a7abbc80a8c
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-06-07 08:41:11 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/lavastorm/lavnaj.pl
+++ trunk/quests/lavastorm/lavnaj.pl
@@ -1,4 +1,3 @@
-
 sub EVENT_SPAWN
 {
    $x = $npc->GetX();
@@ -8,5 +7,12 @@ sub EVENT_SPAWN
 
 sub EVENT_ENTER
 {
-   quest::movepc(44,847,-28,2.19);
+    if(($client->GetClientVersionBit() & 28)!= 0 && $client->Admin() > 100)
+	{
+		quest::MovePCInstance(25, 1, 778.8, 2226.3, 22.4, 150.4);
+	}
+	else
+	{
+		quest::movepc(25,472.29,2952.41,-11.39);
+	}
 } 

c956558954c0f61b3b2f02c06945fe0ace3ef6a7
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-06-11 08:32:35 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/podisease/player.pl
+++ trunk/quests/podisease/player.pl
@@ -2,6 +2,7 @@ sub EVENT_ZONE
 {
 	if($target_zone_id == 200)
 	{
+		$level_for_tier_two = 255;
 		if($client->GetLevel() >= $level_for_tier_two || (defined $qglobals{pop_pod_alder_fuirstel} && defined $qglobals{pop_pod_grimmus_planar_projection} && defined $qglobals{pop_pod_elder_fuirstel}) || (defined $qglobals{pop_alt_access_codecay}))
 		{
 			if(quest::has_zone_flag(200) != 1)

65e2052f7e7dc6fed33ea119d5a3abebe703aa20
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-06-18 17:35:18 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/pojustice/tormented_wraith.pl
+++ trunk/quests/pojustice/tormented_wraith.pl
@@ -1,12 +1,15 @@
 sub EVENT_DEATH {
-	my $r_n = int(rand(100));
-	if($r_n < 75) {
-             if ($r_n <= 25) {
-                 quest::spawn2(201336, 0, 0, $x, $y, $z, $h);
-             } elsif ($r_n <= 50) {
-                 quest::spawn2(201440, 0, 0, $x, $y, $z, $h);
-             } elsif ($r_n < 75) {
-                 quest::spawn2(201441, 0, 0, $x, $y, $z, $h);
-             }	
-	}
+  my $r_n = int(rand(100));
+
+  if($r_n < 75) {
+    if ($r_n < 25) {
+      quest::spawn2(201336, 0, 0, $x, $y, $z, $h);
+    }
+    elsif ($r_n < 50) {
+      quest::spawn2(201440, 0, 0, $x, $y, $z, $h);
+    }
+    else {
+      quest::spawn2(201441, 0, 0, $x, $y, $z, $h);
+    }
+  }
 }

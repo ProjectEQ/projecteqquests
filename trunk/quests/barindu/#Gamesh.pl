1fc1b817592ad2bc6dcddb97b337e41bf9145bee
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-06-21 23:01:41 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/barindu/#Gamesh.pl
+++ trunk/quests/barindu/#Gamesh.pl
@@ -1,23 +1,56 @@
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("I don't know why you would want to, but if you want me to tell you the way to one of the different areas of the sewers let me know.  I know how to get to the treatment [plant], [crematory], [pools] of sludge, or the [lair] of trapped ones.");
-  }
-  if ($text=~/plant/i) {
-    quest::say("Ok, listen closely...");
-    #do instance thing
-  }
-  if ($text=~/crematory/i) {
-    quest::say("Ok, listen closely...");
-    #do instance thing
-}
-if ($text=~/pools/i) {
-    quest::say("Ok, listen closely...");
-    #do instance thing
-}
-if ($text=~/lair/i) {
-    quest::say("Ok, listen closely...");
-    #do instance thing
-}
+  	if ($text=~/hail/i) {
+    		quest::say("I don't know why you would want to, but if you want me to tell you the way to one of the different areas of the sewers let me know.  I know how to get to the treatment [plant], [crematory], [pools] of sludge, or the [lair] of trapped ones.");
+  	}
+  	if ($text=~/plant/i) {
+  	$InInstancePlant = quest::GetInstanceID("snplant",0);
+    		quest::say("Ok, listen closely...");
+    		if(!$InInstancePlant){
+    			$Instance = quest::CreateInstance("snplant", 0, 10800);
+    			quest::AssignGroupToInstance($Instance);	
+    			quest::MovePCInstance(287, $Instance, 150, 127, -7);
+    		}
+    		if($InInstancePlant){
+    			quest::MovePCInstance(287, $InInstancePlant, 150, 127, -7);
+    		}	
+  	}
+  	if ($text=~/crematory/i) {
+   	$InInstanceCrem = quest::GetInstanceID("sncrematory",0);
+	    	quest::say("Ok, listen closely...");
+	    	if(!$InInstanceCrem){
+	    		$Instance = quest::CreateInstance("sncrematory", 0, 10800);
+	    		quest::AssignGroupToInstance($Instance);	
+	    		quest::MovePCInstance(288, $Instance, 31, 175, -17);
+	    	}
+	    	if($InInstanceCrem){
+	    		quest::MovePCInstance(288, $InInstanceCrem, 31, 175, -17);
+	    	}	
+  	}
+	if ($text=~/pools/i) {
+    	$InInstancePool = quest::GetInstanceID("snpool",0);
+	    	quest::say("Ok, listen closely...");
+	    	if(!$InInstancePool){
+	    		$Instance = quest::CreateInstance("snpool", 0, 10800);
+	    		quest::AssignGroupToInstance($Instance);	
+	    		quest::MovePCInstance(285, $Instance, 137, -5, -19);
+	    	}
+	    	if($InInstancePool){
+	    		quest::MovePCInstance(285, $InInstancePool, 137, -5, -19);
+	    	}	
+  	}
+	if ($text=~/lair/i) {
+    	$InInstanceLair = quest::GetInstanceID("snlair",0);
+	    	quest::say("Ok, listen closely...");
+	    	if(!$InInstanceLair){
+	    		$Instance = quest::CreateInstance("snlair", 0, 10800);
+	    		quest::AssignGroupToInstance($Instance);	
+	    		quest::MovePCInstance(286, $Instance, 234, -70, -14);
+	    	}
+	    	if($InInstanceLair){
+	    		quest::MovePCInstance(286, $InInstanceLair, 234, -70, -14);
+	    	}	
+  	
+	}
 }
 
 sub EVENT_ITEM {

3523111928eae907a9f908bab46a76d097fe346f
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-06-25 04:35:00 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/barindu/#Taiandao.pl
+++ quests/barindu/#Taiandao.pl
@@ -7,7 +7,7 @@ sub EVENT_SAY {
   } 
   if(($text=~/muramite killed whiahdi/i) && (defined($qglobals{Soulwhisper}) && ($qglobals{Soulwhisper} == 2))) { 
     quest::say("Yes, I know the one who took her life. His name is Viarglug. I hate him with every fiber of my being. If you kill him, I will forever be in debt to you. He should be somewhere nearby making life miserable for a slave."); 
-    quest::unique_spawn(283156,0,0,-130,82,-122.5,180); #Viarglug 
+    #quest::unique_spawn(283156,0,0,-130,82,-122.5,180); #Viarglug 
   } 
 } 
 

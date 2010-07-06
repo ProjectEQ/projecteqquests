2715fd2c9b2f4fd6ad5e74338943b99ec29c7240
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-07-06 03:14:34 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/qinimi/#Executioner_Hexxt_Xocik.pl
+++ trunk/quests/qinimi/#Executioner_Hexxt_Xocik.pl
@@ -1,4 +1,4 @@
 sub EVENT_DEATH {
   quest::emote("writhes in pain as his body falls to the floor. Your valiant efforts have resulted in the rescue of Kreshin.");
-  quest::spawn2(281124,0,0,-1741,-1079,-4.8,-14.5);
+  quest::spawn2(281124,0,0,-1741,-1079,-14.5,128); #Kreshin_Silentcog
 }

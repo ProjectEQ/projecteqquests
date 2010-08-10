ec358a1544513b40b4d5ae32f3b2ce83a899885a
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-08-10 03:43:49 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/snpool/#Utandi.pl
+++ trunk/quests/snpool/#Utandi.pl
@@ -1,18 +1,29 @@
+sub EVENT_SPAWN  {
+$event = 0;
+}
+
 sub EVENT_SAY {
   if (defined $qglobals{sewers} && $qglobals{sewers} == 5) {
   if ($text=~/hail/i) {
+  if ($event == 0) {
     quest::say("I have a problem");
+    }
   }
 }
   if (defined $qglobals{sewers} && $qglobals{sewers} == 5) {
     if ($text=~/what problem/i) {
+    if ($event == 0) {
   quest::say("I have lost my map.  It shows me how to get home. Can you help me find it?");
-}
+    }
+  }
 }
   if (defined $qglobals{sewers} && $qglobals{sewers} == 5) {
   if ($text=~/I will help you find it/i) {
+      if ($event == 0) {
     quest::say("Thanks!  Try to find a big pile of slime, I think he has my map.");
     quest::spawn2(285085,0,0,1354,471,-138,187);
+    $event = 1;
+  }
   }
   }
   }

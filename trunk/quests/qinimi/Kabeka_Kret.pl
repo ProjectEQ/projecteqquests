5408a399b55cbabe5ff95c6e674130c0c858ccde
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-08-15 04:10:11 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/qinimi/Kabeka_Kret.pl
+++ trunk/quests/qinimi/Kabeka_Kret.pl
@@ -1,4 +1,4 @@
 sub EVENT_DEATH {
-  quest::signalwith(281127,1);
-  quest::signalwith(281127,2); 
+  quest::signalwith(281127,1,0);
+  quest::signalwith(281127,2,5); 
 }
---
+++ trunk/quests/tipt/#Kyv_Heartstriker_Jhiru.pl
@@ -0,0 +1,3 @@
+sub EVENT_DEATH {
+  quest::spawn2(289032,0,0,723,2239,734,244);
+}

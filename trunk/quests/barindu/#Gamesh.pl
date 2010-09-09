567e5371db63fc6a0774ab69787e5ec8dcdf7d74
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-09-09 14:08:40 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/barindu/#Gamesh.pl
+++ trunk/quests/barindu/#Gamesh.pl
@@ -1,64 +1,80 @@
+#Begin file: barindu/Gamesh.pl
+#Probably should check each player in group for active instance rather than just the client.
 sub EVENT_SAY {
-	$InInstancePlant = quest::GetInstanceID("snplant",0);
-	$InInstanceCrem = quest::GetInstanceID("sncrematory",0);
-	$InInstancePool = quest::GetInstanceID("snpool",0);
-	$InInstanceLair = quest::GetInstanceID("snlair",0);
-        $group = $client->GetGroup();
-  	if ($text=~/hail/i) {
-    		quest::say("I don't know why you would want to, but if you want me to tell you the way to one of the different areas of the sewers let me know.  I know how to get to the treatment [plant], [crematory], [pools] of sludge, or the [lair] of trapped ones.");
-  	}
-	if($group){
-  	if ($text=~/plant/i) {
-    		quest::say("Ok, listen closely...");
-    		if($InInstancePlant == 0 && $InInstanceCrem == 0 && $InInstancePool == 0 && $InInstanceLair == 0){
-    			$Instance = quest::CreateInstance("snplant", 0, 10800);
-    			quest::AssignGroupToInstance($Instance);
-    			quest::say("Instance added.");
-    		}
-    		else {
-			$client->Message(13, "You are already in an instance!");
-		}
-  	}
-  	if ($text=~/crematory/i) {
-	    	quest::say("Ok, listen closely...");
-	    	if($InInstancePlant == 0 && $InInstanceCrem == 0 && $InInstancePool == 0 && $InInstanceLair == 0){
-	    		$Instance = quest::CreateInstance("sncrematory", 0, 10800);
-	    		quest::AssignGroupToInstance($Instance);	
-	    		quest::say("Instance added.");
-	    	}	
-	    	else {
-			$client->Message(13, "You are already in an instance!");
-		}
-  	}
-	if ($text=~/pools/i) {
-	    	quest::say("Ok, listen closely...");
-	    	if($InInstancePlant == 0 && $InInstanceCrem == 0 && $InInstancePool == 0 && $InInstanceLair == 0){
-	    		$Instance = quest::CreateInstance("snpool", 0, 10800);
-	    		quest::AssignGroupToInstance($Instance);	
-	    		quest::say("Instance added.");
-	    	}	
-	    	else {
-			$client->Message(13, "You are already in an instance!");
-		}
-  	}
-	if ($text=~/lair/i) {
-	    	quest::say("Ok, listen closely...");
-	    	if($InInstancePlant == 0 && $InInstanceCrem == 0 && $InInstancePool == 0 && $InInstanceLair == 0){
-	    		$Instance = quest::CreateInstance("snlair", 0, 10800);
-	    		quest::AssignGroupToInstance($Instance);	
-	    		quest::say("Instance added.");
-	    	}
-	    	else {
-			$client->Message(13, "You are already in an instance!");
-		}
-	}
+  my $InstanceActive = (quest::GetInstanceID("snplant",0) || quest::GetInstanceID("sncrematory",0) || quest::GetInstanceID("snpool",0) || quest::GetInstanceID("snlair",0));
+  my $group = $entity_list->GetGroupByClient($client);
+
+  if ($text=~/hail/i) {
+    quest::say("I don't know why you would want to, but if you want me to tell you the way to one of the different areas of the sewers let me know. I know how to get to the treatment [plant], [crematory], [pools] of sludge, or the [lair] of trapped ones.");
   }
-         else {
-           $client->Message(13, "You are not in a group!");
-        }
-	
+  elsif ($text=~/plant/i) {
+    quest::emote("leans in closely to whisper, 'Ok, listen closely...'");
+    if ($group) {
+      if (!$InstanceActive) {
+        my $Instance = quest::CreateInstance("snplant", 0, 10800);
+        quest::AssignGroupToInstance($Instance);
+        $group->GroupMessage($npc, "You have been assigned to Sewers of Nihilia, Purifying Plant");
+      }
+      else {
+        $client->Message(13, "You are already in an instance!");
+      }
+    }
+    else {
+      $client->Message(13, "You are not in a group!");
+    }
+  }
+  elsif ($text=~/crematory/i) {
+    quest::emote("leans in closely to whisper, 'Ok, listen closely...'");
+    if ($group) {
+      if (!$InstanceActive) {
+        my $Instance = quest::CreateInstance("sncrematory", 0, 10800);
+        quest::AssignGroupToInstance($Instance);
+        $group->GroupMessage($npc, "You have been assigned to Sewers of Nihilia, the Crematory");
+      }
+      else {
+        $client->Message(13, "You are already in an instance!");
+      }
+    }
+    else {
+      $client->Message(13, "You are not in a group!");
+    }
+  }
+  elsif ($text=~/pools/i) {
+    quest::emote("leans in closely to whisper, 'Ok, listen closely...'");
+    if ($group) {
+      if (!$InstanceActive) {
+        my $Instance = quest::CreateInstance("snpool", 0, 10800);
+        quest::AssignGroupToInstance($Instance);
+        $group->GroupMessage($npc, "You have been assigned to Sewers of Nihilia, Pool of Sludge");
+      }
+      else {
+        $client->Message(13, "You are already in an instance!");
+      }
+    }
+    else {
+      $client->Message(13, "You are not in a group!");
+    }
+  }
+  elsif ($text=~/lair/i) {
+    quest::emote("leans in closely to whisper, 'Ok, listen closely...'");
+    if ($group) {
+      if (!$InstanceActive) {
+        my $Instance = quest::CreateInstance("snlair", 0, 10800);
+        quest::AssignGroupToInstance($Instance);
+        $group->GroupMessage($npc, "You have been assigned to Sewers of Nihilia, Lair of Trapped Ones");
+      }
+      else {
+        $client->Message(13, "You are already in an instance!");
+      }
+    }
+    else {
+      $client->Message(13, "You are not in a group!");
+    }
+  }
 }
 
 sub EVENT_ITEM {
+  quest::say("I do not need this.");
   plugin::return_items(\%itemcount);
 }
+#End file: barindu/Gamesh.pl

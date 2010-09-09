de310fdce30df075ddaa7d9f4eac35f0adc37b28
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-09-09 03:16:19 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/snplant/#Ansharu.pl
+++ quests/snplant/#Ansharu.pl
@@ -5,19 +5,23 @@ sub EVENT_SPAWN {
 
 sub EVENT_SAY {
 if (($text=~/hail/i) && ($event == 1))  {
+   quest::say("Don't bother me until your work is done!");
+   }
+   
+if (($text=~/hail/i) && ($event == 2))  {
    $group = $entity_list->GetGroupByClient($client);
       if ($group) {
         for ($count = 0; $count < $group->GroupCount(); $count++) {
           push (@player_list, $group->GetMember($count)->GetName());
        }
-}
+      }
 foreach $player (@player_list) {
     $pc = $entity_list->GetClientByName($player);
      $charid = $pc->CharacterID();
      quest::InsertQuestGlobal($charid, 283052,283,"temp_sewers", 1, "F");
    $pc->Message(4,"You receive a temporary flag!");
-}
-}
+        }
+     }
  elsif ($text=~/hail/i) {
     quest::say("Find the aged stonemites.  They are causing great trouble, come back and talk to me once you have solved the problem.");
     quest::spawn2(287021,0,0,-96,-1679,-89,207);
@@ -28,12 +32,13 @@ foreach $player (@player_list) {
     quest::spawn2(287021,0,0,-12,-1517,-89,159);
     quest::spawn2(287021,0,0,-56,-1484,-89,134);
     quest::spawn2(287021,0,0,-53,-1586,-87,180);
-    quest::spawn2(287021,0,0,-120,-1573,-89,135);    
+    quest::spawn2(287021,0,0,-120,-1573,-89,135);
+    $event = 1;    
   }
  }
 sub EVENT_SIGNAL {
 if($signal == 0) {
-  $event = 1;
+  $event = 2;
 }
 if($signal == 1) { 
          $counter += 1;

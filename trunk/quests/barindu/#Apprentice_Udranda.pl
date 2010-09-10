3eac41637a767640beb10bf30828346169e08f86
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-09-10 17:32:03 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/barindu/#Apprentice_Udranda.pl
@@ -0,0 +1,37 @@
+sub EVENT_SAY {
+  $InInstanceVxed = quest::GetInstanceID("vxed",0);
+  $InInstanceTipt = quest::GetInstanceID("tipt",0);
+  $group = $client->GetGroup();
+  if ($text=~/hail/i) {
+  	quest::say("Do you wish to enter the [mountains]?");
+  }
+  if ($text=~/mountains/i) {
+	if($group){
+		if(defined $qglobals{god_vxed_access} && $qglobals{god_vxed_access} == 1){			
+	    		quest::say("Your work in the sewers has been vital to our progression, please proceed with caution in the mountain pass.  Find the Stonespiritist for further advancement.");
+	    		if($InInstanceVxed == 0 && $InInstanceTipt == 0){
+	    			$Instance = quest::CreateInstance("vxed", 0, 21600);
+	    			quest::AssignGroupToInstance($Instance);	
+	    			quest::say("Instance added.");
+		    		}
+	    		else {
+				$client->Message(13, "You are already in an instance!");
+				}
+		    	}	
+                if(defined $qglobals{god_tipt_access} && $qglobals{god_tipt_access} == 1){
+		    	quest::say("Proceed with caution, you must face several trials to find your way past through this pass.");
+		    	if($InInstanceVxed == 0 && $InInstanceTipt == 0){
+		    		$Instance = quest::CreateInstance("tipt", 0, 21600);
+		    		quest::AssignGroupToInstance($Instance);	
+		    		quest::say("Instance added.");
+		    		}
+		    	else {
+				$client->Message(13, "You are already in an instance!");
+				}
+	    		}
+           else {
+		           $client->Message(13, "You are not in a group!");
+        	}
+     }
+}
+}
--- trunk/quests/qinimi/Pixtt_Xictic_Krvne.pl
+++ trunk/quests/qinimi/Pixtt_Xictic_Krvne.pl
@@ -1,4 +1,3 @@
 sub EVENT_DEATH {
-  quest::signalwith(281127,1,5);
   quest::signalwith(281127,3,0);
 } 

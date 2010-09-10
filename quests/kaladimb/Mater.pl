b1497bc80c2277bfd78e975a18b63a2418d916bf
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-09-10 17:32:03 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/kaladimb/Mater.pl
+++ quests/kaladimb/Mater.pl
@@ -1,18 +1,20 @@
-sub EVENT_SAY { 
-if ($text=~/hail/i) {
-  if ($class eq "Rogue") {
-    quest::say("Ah, welcome! We could use some fresh blood around here. The name's Mater, and I run this little outfit. Work hard for me, and I will reward you well. Cross me, and you'll find yourself buried under the mine cap. Once you are ready to begin your training please make sure that you see Crovsar Dirkbringer, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+sub EVENT_SAY {
+  if ($text=~/hail/i) {
+    if ($class eq "Rogue") {
+      quest::say("Ah, welcome! We could use some fresh blood around here. The name's Mater, and I run this little outfit. Work hard for me, and I will reward you well. Cross me, and you'll find yourself buried under the mine cap. Once you are ready to begin your training please make sure that you see Crovsar Dirkbringer, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+    }
+    else {
+      quest::say("Welcome to the mines of Kaladim!"); 
+    }
   }
-  else {
-    quest::say("Welcome to the mines of Kaladim!"); 
-  }
-  if (($text=~/mining pick 628/i) && ($class eq "Rogue")) {
-    if (plugin::check_hasitem($client, 12166) { #Parrying Pick 628
+  if (($text=~/pick 628/i) && ($class eq "Rogue")) {
+    if (plugin::check_hasitem($client, 12166)) { #Parrying Pick 628
       quest::say("Ah, so Diggins sent you, eh? You can prove you are worthy of the Miners Pick 628 if you bring me the head of that filthy Boog Mudtoe... oh, and 300 gold pieces should cover the cost of the pick."); #Text made up
     }
     else {
       quest::say("I do not think you are ready for such power. You need to prove yourself first. Maybe you should go talk with Diggins."); #Text made up
     }
+  }
 }
 
 sub EVENT_ITEM {
@@ -27,6 +29,11 @@ sub EVENT_ITEM {
       quest::summonitem(13316); #Ogre Head
       quest::givecash($copper, $silver, $gold, $platinum);
     }
+  }
+  elsif (plugin::check_handin(\%itemcount, 18767 => 1)) { #Small, Folded Note
+    quest::say("Very well then, take this and get to work.");
+    quest::summonitem(13516); #Ruined Miner's Tunic
+    quest::exp(500);
   }
   else {
     #do all other handins first with plugin, then let it do disciplines

792a3b085acd1b3ff703ca816e3a4bd8ce511452
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-09-24 02:26:04 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/bothunder/Agnarr_the_Storm_Lord.pl
+++ quests/bothunder/Agnarr_the_Storm_Lord.pl
@@ -3,13 +3,7 @@ quest::settimer(1,7200);
 }
 
 sub EVENT_AGGRO {
-quest::spawn2(209104,0,0,-888,-1623,2252,100);
-quest::spawn2(209105,0,0,-888,-1890,2252,9);
-quest::spawn2(209106,0,0,-1017,-1717,2252,66);
-quest::stoptimer(1);
-quest::settimer(1,7200);
-quest::settimer(2,31);
-quest::signalwith(209033,1,1);
+ quest::settimer(3,1);
 }
 
 sub EVENT_TIMER {
@@ -28,6 +22,29 @@ quest::settimer(1,7200);
 quest::settimer(2,31);
 }
 
+if($timer == 3) {
+ if($npc->IsEngaged()) {
+quest::spawn2(209104,0,0,-888,-1623,2252,100);
+quest::spawn2(209105,0,0,-888,-1890,2252,9);
+quest::spawn2(209106,0,0,-1017,-1717,2252,66);
+quest::stoptimer(1);
+quest::stoptimer(3);
+quest::settimer(1,7200);
+quest::settimer(2,31);
+quest::settimer(4,60);
+quest::signalwith(209033,1,1);
+ }
+ else {
+  quest::signalwith(209033,2,1);
+  quest::stoptimer(3);
+    }
+  }
+ if($timer == 4) {
+  if(!$npc->IsEngaged()) {
+   quest::signalwith(209033,2,1);
+   quest::stoptimer(4);
+    }
+  }
 }
 
 sub EVENT_DEATH {

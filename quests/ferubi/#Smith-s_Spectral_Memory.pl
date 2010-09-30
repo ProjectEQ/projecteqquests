25f28db768e49508882e5fbe264e68f0ee699c45
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-09-30 02:47:02 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/ferubi/#Smith-s_Spectral_Memory.pl
+++ quests/ferubi/#Smith-s_Spectral_Memory.pl
@@ -1,22 +1,18 @@
 sub EVENT_SPAWN  {
-quest::settimer(1,600);
+  quest::settimer(1,600);
 }
 
 sub EVENT_SAY {
-if(($text=~/hail/) && (defined $qglobals{bic} && $qglobals{bic} == 10)) {
-  quest::summonitem(67526);
-  quest::setglobal("bic",11,5,"F");
-  quest::setglobal("god_vxed_access", 1, 5, "F");
+  if($text=~/hail/i) {
+    if(defined($qglobals{bic}) && ($qglobals{bic} == 10)) {
+      quest::summonitem(67526);
+      quest::setglobal("bic", 11, 5, "F");
+    }
+    quest::setglobal("god_vxed_access", 1, 5, "F");
     $client->Message(4,"You receive a character flag!");
-}
-elsif($text=~/hail/)	{
-	quest::setglobal("god_vxed_access", 1, 5, "F");
-	$client->Message(4,"You receive a character flag!");
-	}
+  }
 }
 
 sub EVENT_TIMER {
-  if($timer == 1) {
-	quest::depop;
-  }
+  quest::depop();
 }

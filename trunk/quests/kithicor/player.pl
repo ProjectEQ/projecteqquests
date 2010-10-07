9e24056b859bd32abe0bb1e21883d160798f93b9
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-10-07 19:29:37 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/kithicor/player.pl
+++ trunk/quests/kithicor/player.pl
@@ -2,7 +2,14 @@ sub EVENT_TASK_STAGE_COMPLETE {
 	if($task_id == 212) {
 		quest::summonitem(87319,5);
 		quest::exp(100);
-		quest::setglobal("halloween_zomebie",1,0,"H3");
+		quest::setglobal("halloween_zomebie",1,5,"H3");
 		$client->Message(15,"The zombie presence seems somewhat lessened, and perhaps they have been quelled . . . for the time being.");
 	}
+}
+
+sub EVENT_SIGNAL {
+        if($signal == 666){
+                quest::updatetaskactivity(212,0,1);
+                quest::faction(98,-30);
+        }
 }

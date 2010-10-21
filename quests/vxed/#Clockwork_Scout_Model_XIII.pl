04fb6904a9bd97b3c009fa9eab0bfcab85e133de
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-10-21 16:22:34 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/vxed/#Clockwork_Scout_Model_XIII.pl
+++ quests/vxed/#Clockwork_Scout_Model_XIII.pl
@@ -1,24 +1,23 @@
-sub EVENT_SPAWN {
-  quest::emote("With the final piece in place, the clockwork scout's eyes light up as it comes to life.");
-}
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    if (defined $qglobals{bic} && $qglobals{bic} == 13) {
+  if (defined $qglobals{bic} && $qglobals{bic} == 13) {
+    if ($text=~/hail/i) {
       quest::say("Access code?");
       quest::setglobal("clockwork",1,4,"H2");
-}
-}
- if ($text=~/040703/i) {
-    if (defined $qglobals{bic} && $qglobals{bic} == 13) {
-          if (defined $qglobals{clockwork} && $qglobals{clockwork} == 1) {
-    quest::say("Verifying password and user identity . . .");
-    quest::say("Identity verified as $name. Processing scout report. Please return report to Vaifan.");
-   quest::summonitem(67536);
-   quest::settimer(60);
-   }
+    }
+    if ($text=~/040703/i) {
+      if (defined($qglobals{clockwork}) && ($qglobals{clockwork} == 1)) {
+        quest::say("Verifying password and user identity . . .");
+        quest::say("Identity verified as $name. Processing scout report. Please return report to Vaifan.");
+        quest::summonitem(67536);
+        quest::settimer(60);
+      }
+    }
   }
- }
+  else {
+    quest::emote("does not seem to respond to your command.");
+  }
 }
+
 sub EVENT_TIMER {
   quest::say("Power cells low. Shutting down to reserve power.");
   quest::depop();

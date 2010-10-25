83258bf4af4ffed08d8957d452af7ddfaddfb00c
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-10-25 01:32:05 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/highpass/Prak.pl
+++ trunk/quests/highpass/Prak.pl
@@ -1,6 +1,24 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Yeah. hello. I'm Prak. co-owner of the Golden Rooster. If you're thirsty. we have some great imported ales at our bar. If you're looking for a little excitement. try your hand at a little King's Court. We aim to please. my friend."); }
+sub EVENT_SAY {
+  if($text=~/hail/i) {
+    quest::say("Yeah, hello. I'm Prak, co-owner of the Golden Rooster. If you're thirsty, we have some great imported ales at our bar. If you're looking for a little excitement, try your hand at a little King's Court. We aim to please, my friend.");
+  }
+  if($text=~/get rid of stald/i) {
+    quest::say("Good.. Zannsin said you had a special talent for this sort of thing, so here's your chance to prove it. What you need to do is follow Stald on his patrol, and when the coast is clear, take him down. Good luck. Oh yeah.. not that I don't trust you or anything, but bring me back some sorta proof that Stald is dead, got it?");
+  }
 }
-#END of FILE Zone:highpass  ID:5053 -- Prak 
 
+sub EVENT_ITEM {
+  if(plugin::check_handin(\%itemcount, 18795 => 1)) {
+    quest::say("Hmm, I see. We think we've found out who the mole is in Carson's guards, some guy named Stald. We need to get rid of this guy as quickly, and as quietly, as possible. Carson doesn't want to cause a stink by eliminating one of his own men, so he asked us to do it. What about you? Do you think [you could get rid of Stald] for us?");
+  }
+  elsif(plugin::check_handin(\%itemcount, 13793 => 1)) {
+    quest::say("Great! Thanks for taking care of this 'problem' for us $name. Please return to Zannsin with this note and he should reward you for your assistance."); #Made up text
+    quest::summonitem(18028);
+  }
+  else { 
+    quest::say("I have no use for this, please take it back so I don't have to carry it around with me!");
+    plugin::return_items(\%itemcount);
+  }
+}
+
+#END of FILE Zone:highpass  ID:5053 -- Prak
--- trunk/quests/misty/#a_tired_farmer.pl
+++ trunk/quests/misty/#a_tired_farmer.pl
@@ -8,10 +8,10 @@
 ## Tear-stained Poem of the Storms: 62633
 
 sub EVENT_SAY {
-  if(plugin::hasitem($client, 62632)) { #Poem of the Storms
+  if(plugin::check_hasitem($client, 62632)) { #Poem of the Storms
     if($text=~/hail/i) {
       quest::emote("looks worn out. His boots are stained with mud and his clothing is torn and frayed.");
-      quest::say("Hello there. I hope things are going well for you, at least better than they are for me. I wish I had time to be more social, but I need rest before I go out and battle with the weather again.");   
+      quest::say("Hello there. I hope things are going well for you, at least better than they are for me. I wish I had time to be more social, but I need rest before I go out and battle with the weather again.");
     }
     if($text=~/friend of the storm lord/i) {
       quest::say("Please, not another lecture. I know, I know, I need to accept that it will rain when it rains. Rain is good, it grows my crops. I understand all that. But now the rain is rotting my crops on the vine. It's flooding out my seeds. It's ruining me. I want to remain true to Karana, but I can't be quiet while my family and friends starve. I understand why Karana is worthy of worship. I want to have faith, but I just can't afford to feed my family without my crops!");
@@ -23,7 +23,7 @@ sub EVENT_ITEM {
   my $cash = $copper + 10 * $silver + 100 * $gold + 1000 * $platinum;
 
   if(($cash >= 300000) && plugin::check_handin(\%itemcount, 62632 => 1)) { #Poem of the Storms
-    quest::me("The farmer looks at you with a stunned expression on his face. He carefully sets the coins down in a neat stack on the ground and slowly unfolds the paper. As he reads the poem, heavy tears form in the corners of his eyes and drop onto the page. When he is done reading he carefully folds it back up and hands the page back to you. He then wipes his nose with his sleeve.");
+    $client->Message(0,"The farmer looks at you with a stunned expression on his face. He carefully sets the coins down in a neat stack on the ground and slowly unfolds the paper. As he reads the poem, heavy tears form in the corners of his eyes and drop onto the page. When he is done reading he carefully folds it back up and hands the page back to you. He then wipes his nose with his sleeve.");
     quest::spawn2(33159, 0, 0, -1152, -362, 1.19, 0); #a_faithful_farmer
     quest::say("I think I understand a little better about faith. Maybe my faith brought you here, maybe yours did, I don't know. I don't think it matters. You've shown me something about kindness as well. Thank you so much!");
     quest::summonitem(62633); #Tear-stained Poem of the Storms
@@ -33,7 +33,7 @@ sub EVENT_ITEM {
     quest::say("I have no use for this.");
     quest::givecash($copper, $silver, $gold, $platinum);
     plugin::return_items(\%itemcount);
-  }      
+  }
 }
 
 sub EVENT_SPAWN {

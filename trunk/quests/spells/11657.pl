ae10b67ee1b780013aedaac62d14762873885592
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-10-28 04:43:19 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/spells/11657.pl
@@ -0,0 +1,8 @@
+sub EVENT_SPELL_EFFECT_CLIENT {
+$newrace = quest::ChooseRandom(469,473,464,82,456,367,485,454,471,46,63,339,342,338,341,340,331);
+	
+	quest::playerrace($newrace);
+	if($newrace > 330 && $newrace < 343){
+		quest::playergender(quest::ChooseRandom(0,1));
+	}
+}

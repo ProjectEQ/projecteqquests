17c95a60b6ef2cad4f5aea6ffcd44951422830cc
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-11-26 04:56:48 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/ikkinz/#Priest_of_Righteousness.pl
+++ trunk/quests/ikkinz/#Priest_of_Righteousness.pl
@@ -4,13 +4,19 @@ sub EVENT_SPAWN {
 
 sub EVENT_HP {
   if($hpevent == 50) {
-     quest::modifynpcstat("special_attacks",r);
+     quest::modifynpcstat("special_attacks",EFQUCDr);
+     quest::setnexthpevent(40);
+     }
+  if($hpevent == 40) {
+     quest::modifynpcstat("special_attacks",EFQUCD);
      quest::setnexthpevent(10);
      }
+
   if($hpevent == 10) {
-     quest::modifynpcstat("special_attacks",r);
+     quest::modifynpcstat("special_attacks",EFQUCDr);
      }
 }
+
 
 sub EVENT_AGGRO {
   quest::settimer(1,1);
--- trunk/quests/ponightmare/Aid_Eino.pl
+++ trunk/quests/ponightmare/Aid_Eino.pl
@@ -1,13 +1,16 @@
 #All text made up
 
+my $wave;
+
 sub EVENT_SPAWN {
+  $wave = 0;
   quest::say("Well, well! I was not sure if you would show. I hope you and your friends are strong enough to help me while I search for these new research components.");
   quest::emote("looks around. 'Looks like the way is safe. Let's go before we are noticed by the creatures of the night.'");
   quest::start(124);
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
-  if ($wp == 4) {
+  if (($wp == 4) && ($wave == 0)) {
     $wave1_1 = quest::spawn2(204072,0,0,$x+20,$y+20,$z,$h);
     $wave1_2 = quest::spawn2(204072,0,0,$x-20,$y-20,$z,$h);
     $wave1_3 = quest::spawn2(204072,0,0,$x+20,$y-20,$z,$h);
@@ -24,8 +27,9 @@ sub EVENT_WAYPOINT_ARRIVE {
     $wave1_npc2->AddToHateList($npc,1);
     $wave1_npc3->AddToHateList($npc,1);
     $wave1_npc4->AddToHateList($npc,1);
+    $wave = 1;
   }
-  if ($wp == 10) {
+  if (($wp == 10) && ($wave == 1)) {
     $wave2_1 = quest::spawn2(204073,0,0,$x+20,$y+20,$z,$h);
     $wave2_2 = quest::spawn2(204073,0,0,$x-20,$y-20,$z,$h);
     $wave2_obj1 = $entity_list->GetMobID($wave2_1);
@@ -34,8 +38,9 @@ sub EVENT_WAYPOINT_ARRIVE {
     $wave2_npc2 = $wave2_obj2->CastToNPC();
     $wave2_npc1->AddToHateList($npc,1);
     $wave2_npc2->AddToHateList($npc,1);
+    $wave = 2;
   }
-  if ($wp == 19) {
+  if (($wp == 19) && ($wave == 2)) {
     $wave3_1 = quest::spawn2(204074,0,0,$x+20,$y+20,$z,$h);
     $wave3_2 = quest::spawn2(204074,0,0,$x-20,$y-20,$z,$h);
     $wave3_3 = quest::spawn2(204074,0,0,$x+20,$y-20,$z,$h);
@@ -52,8 +57,9 @@ sub EVENT_WAYPOINT_ARRIVE {
     $wave3_npc2->AddToHateList($npc,1);
     $wave3_npc3->AddToHateList($npc,1);
     $wave3_npc4->AddToHateList($npc,1);
+    $wave = 3;
   }
-  if ($wp == 24) {
+  if (($wp == 24) && ($wave == 3)) {
     $wave4_1 = quest::spawn2(204072,0,0,$x+20,$y,$z,$h);
     $wave4_2 = quest::spawn2(204072,0,0,$x-20,$y,$z,$h);
     $wave4_3 = quest::spawn2(204072,0,0,$x,$y-20,$z,$h);
@@ -78,16 +84,21 @@ sub EVENT_WAYPOINT_ARRIVE {
     $wave4_npc4->AddToHateList($npc,1);
     $wave4_npc5->AddToHateList($npc,1);
     $wave4_npc6->AddToHateList($npc,1);
+    $wave = 4;
   }
   if ($wp == 25) {
     quest::say("What a tiring evening. Perhaps we should rest here for a while?");
     $npc->SetAppearance(1);
   }
-  if ($wp == 26) {
+  if (($wp == 26) && ($wave == 4)) {
     $wave5_1 = quest::spawn2(204076,0,0,$x+20,$y+20,$z,$h);
     $wave5_obj1 = $entity_list->GetMobID($wave5_1);
     $wave5_npc1 = $wave5_obj1->CastToNPC();
     $wave5_npc1->AddToHateList($npc,1);
+    $wave = 5;
+  }
+  if ($wp == 27) {
+    quest::say("We made it! No doubt I could not have made it without you. Did you find something useful during our trip? May I have it?");
   }
 }
 
@@ -103,13 +114,14 @@ sub EVENT_WAYPOINT_DEPART {
 }
 
 sub EVENT_ITEM {
-  if (($x == 1690) && ($y == 285) && plugin::check_handin(\%itemcount, 16261 => 1)) {
-    quest::say("Excellent work. Thank you for your help. Here, take this. I must get back to the Plane of Knowledge. Come and see me when you get a chance.");
+  if (($x == 1690) && ($y == 285) && ($wave == 5) && plugin::check_handin(\%itemcount, 16261 => 1)) {
+    quest::say("Excellent work, $name. That is exactly why I came to this dreadful place. Thank you for your help. Here, take this. I must get back to the Plane of Knowledge. Come and see me when you get a chance.");
     quest::summonitem(16260); #Tiny Gold Fist
     quest::setglobal("AidEinoDone",1,5,"F");
     quest::depop();
   }
   else {
+    quest::say("I am busy. Maybe later.");
     plugin::return_items(\%itemcount);
   }
 }

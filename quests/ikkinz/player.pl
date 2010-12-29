44fa96e564411edd4e9a56e446e0ef33f3aa2d2a
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2010-12-29 06:59:30 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

---
+++ quests/ikkinz/player.pl
@@ -0,0 +1,34 @@
+sub EVENT_CLICKDOOR {
+  my $d_id = ($doorid);
+
+  if($d_id == 515) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 1)) {
+       quest::forcedooropen(515);
+	}
+     }
+  if($d_id == 516) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 1)) {
+       quest::forcedooropen(516);
+	}
+     }
+  if($d_id == 510) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 2)) {
+       quest::forcedooropen(510);
+	}
+     }
+  if($d_id == 512) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 2)) {
+       quest::forcedooropen(512);
+	}
+     }
+  if($d_id == 508) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 3)) {
+       quest::forcedooropen(508);
+	}
+     }
+  if($d_id == 509) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 3)) {
+       quest::forcedooropen(509);
+	}
+     }
+}

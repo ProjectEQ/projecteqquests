738fb258e3be61c2741b0f8d50c3c27389680244
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-01-07 07:10:11 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/ikkinz/player.pl
+++ trunk/quests/ikkinz/player.pl
@@ -2,22 +2,22 @@ sub EVENT_CLICKDOOR {
   my $d_id = ($doorid % 256);
 
   if($d_id == 3) {
-    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 1)) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} >= 1)) {
        quest::forcedooropen(515);
 	}
      }
   if($d_id == 4) {
-    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 1)) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} >= 1)) {
        quest::forcedooropen(516);
 	}
      }
   if($d_id == 254) {
-    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 2)) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} >= 2)) {
        quest::forcedooropen(510);
 	}
      }
   if($d_id == 0) {
-    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} == 2)) {
+    if((defined $qglobals{ikkydoor}) && ($qglobals{ikkydoor} >= 2)) {
        quest::forcedooropen(512);
 	}
      }

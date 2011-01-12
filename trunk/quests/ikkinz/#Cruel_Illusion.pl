80607034535c5174d82bc007a812e58a2d8b99eb
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-01-12 15:27:59 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/ikkinz/#Cruel_Illusion.pl
+++ trunk/quests/ikkinz/#Cruel_Illusion.pl
@@ -1,3 +1,12 @@
+sub EVENT_AGGRO {
+  quest::settimer(1,1);
+}
+sub EVENT_TIMER {
+  if(($x > 500) || ($x < 200) || ($y > -30) || ($y < -250)) {
+    $npc->GMMove(442,-141,11,192);
+  }
+}
+
 sub EVENT_DEATH {
   quest::setglobal("ikkylockout0",1,3,"H17");
 }
--- trunk/quests/ikkinz/#Dire_Illusion.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_AGGRO {
-  quest::settimer(1,1);
-}
-sub EVENT_TIMER {
-  if(($x > 500) || ($x < 200) || ($y > -30) || ($y < -250)) {
-    $npc->GMMove(442,-141,11,192);
-  }
-}

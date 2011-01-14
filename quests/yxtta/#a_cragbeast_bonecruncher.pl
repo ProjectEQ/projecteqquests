c9c126a8ab9ba6e38c5c212c66b7bd5b92986586
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-01-14 05:07:26 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- quests/yxtta/#a_cragbeast_bonecruncher.pl
+++ quests/yxtta/#a_cragbeast_bonecruncher.pl
@@ -2,8 +2,9 @@ sub EVENT_SPAWN {
    quest::start(537896);
 }
 
-sub EVENT_WAYPOINT {
+sub EVENT_WAYPOINT_ARRIVE {
   if($wp == 11) {
+  quest::stop();
   quest::spawn2(291123,0,0,$x,$y,$z,$h);
   quest::depop();
   }
--- quests/yxtta/#a_voracious_beetle.pl
+++ quests/yxtta/#a_voracious_beetle.pl
@@ -2,8 +2,9 @@ sub EVENT_SPAWN {
    quest::start(537899);
 }
 
-sub EVENT_WAYPOINT {
+sub EVENT_WAYPOINT_ARRIVE {
   if($wp == 9) {
+  quest::stop();
   quest::spawn2(291050,0,0,$x,$y,$z,$h);
   quest::depop();
   }

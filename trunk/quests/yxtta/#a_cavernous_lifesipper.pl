419f23d18053c9339f915661aaae6f519043d774
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-01-14 05:07:26 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/yxtta/#a_cavernous_lifesipper.pl
+++ trunk/quests/yxtta/#a_cavernous_lifesipper.pl
@@ -2,8 +2,9 @@ sub EVENT_SPAWN {
    quest::start(537898);
 }
 
-sub EVENT_WAYPOINT {
+sub EVENT_WAYPOINT_ARRIVE {
   if($wp == 10) {
+  quest::stop();
   quest::spawn2(291125,0,0,$x,$y,$z,$h);
   quest::depop();
   }

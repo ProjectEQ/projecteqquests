05445aa2bac81cd3f233263b0807d407ff2c49b5
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-02-02 13:00:31 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/tutorialb/Frizznik_Hailstorm.pl
+++ trunk/quests/tutorialb/Frizznik_Hailstorm.pl
@@ -2,7 +2,7 @@
 #Angelox
 #rewrite by site3op
 
-sub EVENT_SAY{
+sub EVENT_SAY {
   if($text=~/hail/i) {
     quest::say("Hello there $name. The name's Frizznik and I'm in charge of making food for the revolt. Most of what I make starts with the basics. I don't have the time to make the basic items and the final products. Let me know if you are interested in [assisting] me with making dough.");
   }
@@ -10,25 +10,27 @@ sub EVENT_SAY{
     quest::emote("eyes you up and down.");
     quest::say("Make sure you buy a mixing bowl and come see me when you are [ready].");
   }
-  if($text=~/ready/i && !defined $qglobals{frizznik}) {
-    quest::say("Good enough. Now, combine these ingredients in your mixing bowl and return to me all the clumps of dough you can. If you give me four clumps of dough, I'll reward your assistance.");
-    quest::summonitem(13087,20);
-    quest::summonitem(13193,20);
-    quest::summonitem(13088,20);
-    quest::setglobal("frizznik",1,0,"F");
-  }
-  elsif($text=~/ready/i && defined $qglobals{frizznik}) {
+  if($text=~/ready/i) {
+    if (!defined($qglobals{frizznik})) {
+      quest::say("Good enough. Now, combine these ingredients in your mixing bowl and return to me all the clumps of dough you can. If you give me four clumps of dough, I'll reward your assistance.");
+      quest::summonitem(13087,20);
+      quest::summonitem(13193,20);
+      quest::summonitem(13088,20);
+      quest::setglobal("frizznik",1,0,"D60");
+    }
+    else {
      quest::say("I'm sorry, but the boss has instructed me to hand out the items only once per player!");
+    }
  }
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 13090 => 4) && !defined $qglobals{frizznikfinal}) {
+  if (plugin::check_handin(\%itemcount, 13090 => 4) && defined($qglobals{frizznik}) && ($qglobals{frizznik} == 1)) {
     quest::say("I appreciate your assistance in getting everyone fed. Please take this as a small token of my eternal appreciation.");
     quest::summonitem(3395);
-    quest::setglobal("frizznikfinal",1,0,"F");
+    quest::setglobal("frizznik",2,0,"D60");
   }
   else { 
       plugin::return_items(\%itemcount); 
- }
-   } 
+  }
+}

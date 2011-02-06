daba2443697682cfe68cec411e8d4e9a0b966b0e
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-02-06 05:34:10 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/kodtaz/Sentinel_of_the_Altar.pl
+++ trunk/quests/kodtaz/Sentinel_of_the_Altar.pl
@@ -1,24 +1,25 @@
 sub EVENT_ITEM {
   $InInstanceIkky7 = quest::GetInstanceID("ikkinz",6);
-if ((plugin::check_handin(\%itemcount, 60173 =>1 )) && (defined $qglobals{ikky} && $qglobals{ikky} == 14)) {
-   if($raid){
-     if(!defined $qglobals{ikkylockout6}) {
-	if(defined $qglobals{ikky} && $qglobals{ikky} == 14) { 
-           if($InInstanceIkky7 == 0){
-		 $Instance = quest::CreateInstance("ikkinz", 6, 21600);
-		 quest::AssignRaidToInstance($Instance);	
-		 quest::say("Instance added.");
-                 quest::summonitem(60173);
-             } else {
-		$client->Message(13, "You are already in an instance!");
-	 	  }
-           }
-         } else {
-           $client->Message(13,"You have recently completed a raid.");
-           }
+  if ((plugin::check_handin(\%itemcount, 60173 =>1 )) && (defined $qglobals{ikky} && $qglobals{ikky} == 14)) {
+    $raid = $client->GetRaid();
+    if($raid){
+      if(!defined $qglobals{ikkylockout6}) {
+        if(defined $qglobals{ikky} && $qglobals{ikky} == 14) { 
+          if($InInstanceIkky7 == 0){
+            $Instance = quest::CreateInstance("ikkinz", 6, 21600);
+            quest::AssignRaidToInstance($Instance);	
+            quest::say("Instance added.");
+          } else {
+            $client->Message(13, "You are already in an instance!");
+          }
+        }
       } else {
-	 $client->Message(13, "You are not in a raid!");
-         }
-   }
-   plugin::return_items(\%itemcount);
+        $client->Message(13,"You have recently completed a raid.");
+      }
+    } else {
+      $client->Message(13, "You are not in a raid!");
+    }
+    quest::summonitem(60173);
+  }
+  plugin::return_items(\%itemcount);
 }   

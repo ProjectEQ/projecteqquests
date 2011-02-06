cc475f73744ad5bb1924823ab38fd5c795b8ec71
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-02-06 07:00:44 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/kodtaz/Sentinel_of_the_Altar.pl
+++ trunk/quests/kodtaz/Sentinel_of_the_Altar.pl
@@ -1,10 +1,10 @@
 sub EVENT_ITEM {
-  $InInstanceIkky7 = quest::GetInstanceID("ikkinz",6);
-  if ((plugin::check_handin(\%itemcount, 60173 =>1 )) && (defined $qglobals{ikky} && $qglobals{ikky} == 14)) {
-    $raid = $client->GetRaid();
-    if($raid){
-      if(!defined $qglobals{ikkylockout6}) {
-        if(defined $qglobals{ikky} && $qglobals{ikky} == 14) { 
+  if (plugin::check_handin(\%itemcount, 60173 =>1)) {
+    if(defined $qglobals{ikky} && $qglobals{ikky} == 14) { 
+      $raid = $client->GetRaid();
+      if($raid){
+        if(!defined $qglobals{ikkylockout6}) {
+          $InInstanceIkky7 = quest::GetInstanceID("ikkinz",6);
           if($InInstanceIkky7 == 0){
             $Instance = quest::CreateInstance("ikkinz", 6, 21600);
             quest::AssignRaidToInstance($Instance);	

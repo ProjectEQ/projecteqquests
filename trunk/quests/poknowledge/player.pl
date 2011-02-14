9528dc229a12c3b3a5ba8d743384c7a8185bf7e8
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-02-14 05:18:22 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/poknowledge/player.pl
+++ trunk/quests/poknowledge/player.pl
@@ -2,11 +2,4 @@ sub EVENT_ENTERZONE {
   if (quest::istaskcompleted(138) == 0 && quest::istaskactive(138) == 0) { #Check if completed Task: New Beginnings
     quest::assigntask(138); #Force assign Task: New Beginnings
   }
-  #Remove old unneeded globals
-  if (defined($qglobals{frizznik}) && ($ulevel > 15)) {
-    quest::delglobal("frizznik");
-  }
-  if (defined($qglobals{frizznikfinal}) && ($ulevel > 15)) {
-    quest::delglobal("frizznikfinal");
-  }
 }

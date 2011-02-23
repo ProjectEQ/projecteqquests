f33b801b3e753cc5c1f24a7cc5534dcf1ea586eb
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-02-23 00:25:35 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- quests/barindu/player.pl
+++ quests/barindu/player.pl
@@ -9,4 +9,66 @@ sub EVENT_LOOT {
     quest::spawn2(283157,0,0,$x,$y,$z,0); #a chest (Viarglug)
   }
 }
+sub EVENT_COMBINE_SUCCESS {
+ if (($recipe_id == 10904) || ($recipe_id == 10905) || ($recipe_id == 10906) || ($recipe_id == 10907)) {
+   $client->Message("The gem resonates with power as the shards placed within glow unlocking some of the stone's power. You were successful in assembling most of the stone but there are four slots left to fill, where could those four pieces be?");
+   }
+  if ($recipe_id == 10903) {
+      if (($class eq "Bard") || ($class eq "Beastlord") || ($class eq "Paladin") || ($class eq "Ranger") ||  ($class eq "Shadowknight")) {
+    	   quest::summonitem(67666);
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Warrior") || ($class eq "Monk") || ($class eq "Berserker")  || ($class eq "Rogue"))  {
+           quest::summonitem(67665);
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Cleric") || ($class eq "Shaman") || ($class eq "Druid")) {
+           quest::summonitem(67667);  
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Necromancer") || ($class eq "Wizard") || ($class eq "Enchanter")  || ($class eq "Magician")) {
+           quest::summonitem(67668);
+	   quest::summonitem(67704);
+        }
+    $client->Message(1,"Success");
+    }
+  if ($recipe_id == 10346) {
+     if (($class eq "Bard") || ($class eq "Beastlord") || ($class eq "Paladin") || ($class eq "Ranger") ||  ($class eq "Shadowknight")) {
+    	   quest::summonitem(67661);
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Warrior") || ($class eq "Monk") || ($class eq "Berserker")  || ($class eq "Rogue"))  {
+           quest::summonitem(67660);
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Cleric") || ($class eq "Shaman") || ($class eq "Druid")) {
+           quest::summonitem(67662);  
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Necromancer") || ($class eq "Wizard") || ($class eq "Enchanter")  || ($class eq "Magician")) {
+           quest::summonitem(67663);
+	   quest::summonitem(67704);
+        }
+    $client->Message(1,"Success");
+    }
+  if ($recipe_id == 10334) {
+     if (($class eq "Bard") || ($class eq "Beastlord") || ($class eq "Paladin") || ($class eq "Ranger") ||  ($class eq "Shadowknight")) {
+    	   quest::summonitem(67654);
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Warrior") || ($class eq "Monk") || ($class eq "Berserker")  || ($class eq "Rogue"))  {
+           quest::summonitem(67653);
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Cleric") || ($class eq "Shaman") || ($class eq "Druid")) {
+           quest::summonitem(67655);  
+	   quest::summonitem(67704);
+        }
+        elsif (($class eq "Necromancer") || ($class eq "Wizard") || ($class eq "Enchanter")  || ($class eq "Magician")) {
+           quest::summonitem(67656);
+	   quest::summonitem(67704);
+        }
+    $client->Message(1,"Success");
+    }
+}
 #EndFile: barindu\player.pl

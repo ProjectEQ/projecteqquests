982b0900023bfbcf8404520d95eeffb9acfc524f
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-04-27 13:54:19 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/yxtta/#Jerimo_Jiao.pl
+++ trunk/quests/yxtta/#Jerimo_Jiao.pl
@@ -7,6 +7,7 @@ sub EVENT_SAY {
   quest::say("Beware traveler for behind me lies the way to an ancient temple long since forgotten about. It is within this temple that the trusik have begun a ritual similar to the one that brought the menacing beings known as Muramites. While it may appear to be nothing what you see behind me is a complex illusion placed here by those inside to trick travelers like you. But after losing my life and that of the one I loved to the invading army I am bound here to help those who wish to stop the trusik from damning us again. If you are truly a brave adventurer then just tell me you are [ready] and I will tell you how to get through the facade behind me.");
   }
   if ($text=~/ready/i) {
+  $raid = $client->GetRaid();
   if($raid){
        if(!defined $qglobals{uqualockout}) {
            if($InInstanceUqua == 0){

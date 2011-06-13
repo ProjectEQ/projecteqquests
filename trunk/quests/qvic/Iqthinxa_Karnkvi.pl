28812726e27c69ebea80a6fd33459d1ea94e9123
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-06-13 05:28:31 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/qvic/Iqthinxa_Karnkvi.pl
+++ trunk/quests/qvic/Iqthinxa_Karnkvi.pl
@@ -4,19 +4,29 @@ sub EVENT_SPAWN {
 
 sub EVENT_HP {
   if($hpevent == 75) {
-  quest::spawn2(295131,0,0,84,423,-410,84);
-  quest::spawn2(295132,0,0,126,310,-422,232);
-  quest::spawn2(295133,0,0,267,283,-410,124);
-  quest::modifynpcstat("special_attacks",ABfHG);
+    quest::spawn2(295131,0,0,84,423,-410,84);
+    quest::spawn2(295132,0,0,126,310,-422,232);
+    quest::spawn2(295133,0,0,267,283,-410,124);
+    quest::modifynpcstat("special_attacks",ABfHG);
+    #safety check for lost/missed signal from ravs.
+    quest::settimer(1,60);
     $npc->WipeHateList();
-    }
+  }
 }
 
 sub EVENT_SIGNAL {
   if($signal == 1) {
-    if((!$entity_list->GetMobByNpcTypeID(295131)) && (!$entity_list->GetMobByNpcTypeID(295132)) && (!$entity_list->GetMobByNpcTypeID(295133))) {
-       quest::modifynpcstat("special_attacks",SRTMCNIDf);
-    }   
+    CheckForRavs();
   }  
 } 
 
+sub EVENT_TIMER {
+  CheckForRavs();
+}
+
+sub CheckForRavs {
+  if((!$entity_list->GetMobByNpcTypeID(295131)) && (!$entity_list->GetMobByNpcTypeID(295132)) && (!$entity_list->GetMobByNpcTypeID(295133))) {
+     quest::modifynpcstat("special_attacks",SRTMCNIDf);
+	 quest::stoptimer(1);
+  }
+}
--- trunk/quests/qvic/player.pl
+++ trunk/quests/qvic/player.pl
@@ -1,46 +1,11 @@
 #BeginFile: qvic\player.pl
-my $whimsy_count = 1;
 
 sub EVENT_ENTERZONE {
   if (defined $qglobals{god_qvic_access} && $qglobals{god_qvic_access} == 1 && quest::has_zone_flag(295) == 1) {
     quest::setglobal("god_qvic_access",2,5,"F");
   }
-  if(plugin::check_hasitem($client, 69059)) { #whimsy of the masters
-    quest::settimer(1,800); #timer * 9 = time to full power in this case 7200 seconds (120 minutes)
-	quest::setglobal("whimsy",1,5,"F");
-  }
 }
-sub EVENT_TIMER {
-  $whimsy_count++;
-  if($whimsy_count <= 2) {
-    quest::setglobal("whimsy",2,5,"F");
-  }
-  elsif($whimsy_count == 3) {
-    quest::setglobal("whimsy",3,5,"F");
-  }
-  elsif($whimsy_count == 4) {
-    quest::setglobal("whimsy",4,5,"F");
-  }
-  elsif($whimsy_count == 5) {
-    quest::setglobal("whimsy",5,5,"F");
-  }
-  elsif($whimsy_count == 6) {
-    quest::setglobal("whimsy",6,5,"F");
-  }
-  elsif($whimsy_count == 7) {
-    quest::setglobal("whimsy",7,5,"F");
-  }
-  elsif($whimsy_count == 8) {
-    quest::setglobal("whimsy",8,5,"F");
-  }
-  elsif($whimsy_count == 9) {
-    quest::setglobal("whimsy",9,5,"F");
-  }
-  elsif($whimsy_count >= 10) {
-   quest::setglobal("whimsy",10,5,"F");
-   quest::stoptimer(1);
-  }
-}
+
 
 sub EVENT_COMBINE_SUCCESS {
  if (($recipe_id == 10904) || ($recipe_id == 10905) || ($recipe_id == 10906) || ($recipe_id == 10907)) {

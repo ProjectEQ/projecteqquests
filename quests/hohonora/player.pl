4ca023da910ef8a27b318402439eaa84c39db8b1
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-06-16 05:27:38 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- quests/hohonora/player.pl
+++ quests/hohonora/player.pl
@@ -1,57 +1,18 @@
 sub EVENT_ENTERZONE {
-if (defined $qglobals{whimsy}) {
-quest::delglobal("whimsy");
-}
-if($hasitem{69059}) { 
-  quest::settimer(1,72);
+  if(($class eq "Enchanter") && plugin::check_hasitem($client, 52963)) { #Sullied Gold Filigree
+    if(!$entity_list->GetNPCByNPCTypeID(211047) && !defined($qglobals{anthone})) {
+      quest::spawn2(211047,0,0,-1853,2479,-110,20); ##Anthone_Chapin
+    }
   }
-}
-sub EVENT_TIMER  {
-$whimsy_count++;
-if($whimsy_count == 1) {
- quest::setglobal("whimsy",1,5,"F");
- }
-if($whimsy_count == 2) {
- quest::setglobal("whimsy",2,5,"F");
- }
-if($whimsy_count == 3) {
- quest::setglobal("whimsy",3,5,"F");
- }
-if($whimsy_count == 4) {
- quest::setglobal("whimsy",4,5,"F");
- }
-if($whimsy_count == 5) {
- quest::setglobal("whimsy",5,5,"F");
- }
-if($whimsy_count == 6) {
- quest::setglobal("whimsy",6,5,"F");
- }
-if($whimsy_count == 7) {
- quest::setglobal("whimsy",7,5,"F");
- }
-if($whimsy_count == 8) {
- quest::setglobal("whimsy",8,5,"F");
- }
-if($whimsy_count == 9) {
- quest::setglobal("whimsy",9,5,"F");
- }
-if($whimsy_count == 10) {
- quest::setglobal("whimsy",10,5,"F");
- quest::stoptimer(1);
- }
 }
 
 sub EVENT_CLICKDOOR {
-	my $d_id = ($doorid % 256);
-	if($d_id == 19 || $d_id == 20)
-	{
-		if(defined $qglobals{pop_poj_mavuin} && defined $qglobals{pop_poj_tribunal} && defined $qglobals{pop_poj_valor_storms} && defined $qglobals{pop_pov_aerin_dar} && defined $qglobals{pop_hoh_faye} && defined $qglobals{pop_hoh_trell} && defined $qglobals{pop_hoh_garn})
-		{
-			if(quest::has_zone_flag(220) != 1)
-			{
-				quest::set_zone_flag(220);
-				$client->Message(15, "You have received a character flag!");
-			}
-		}
-	}
+  if($doorid == 19 || $doorid == 20) {
+    if(defined($qglobals{pop_poj_mavuin}) && defined($qglobals{pop_poj_tribunal}) && defined($qglobals{pop_poj_valor_storms}) && defined($qglobals{pop_pov_aerin_dar}) && defined($qglobals{pop_hoh_faye}) && defined($qglobals{pop_hoh_trell}) && defined($qglobals{pop_hoh_garn})) {
+      if(quest::has_zone_flag(220) != 1) {
+        quest::set_zone_flag(220);
+        $client->Message(15, "You have received a character flag!");
+      }
+    }
+  }
 }

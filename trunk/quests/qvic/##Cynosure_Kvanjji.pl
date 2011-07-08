1dd915c42cfc1c55d479a92e3c6955e8777de34c
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-07-08 15:17:01 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- trunk/quests/qvic/##Cynosure_Kvanjji.pl
+++ trunk/quests/qvic/##Cynosure_Kvanjji.pl
@@ -1,5 +1,18 @@
+sub EVENT_AGGRO {
+  quest::settimer(1,5);
+}
+
 sub EVENT_SIGNAL {
   if($signal == 1) {
     $npc->SetHP($npc->GetHP() + ($npc->GetMaxHP() / 10));
-    }
+  }
+}
+
+sub EVENT_TIMER {
+  if(!$entity_list->GetMobByNpcTypeID(295145) && !$entity_list->GetMobByNpcTypeID(295146) && !$entity_list->GetMobByNpcTypeID(295147) && !$entity_list->GetMobByNpcTypeID(295148)) {
+    quest::spawn2(295145,0,0,-216,-191,-467,192);
+    quest::spawn2(295146,0,0,-316,-181,-464,64);
+    quest::spawn2(295147,0,0,-243,-250,-439,248);
+    quest::spawn2(295148,0,0,-357,-275,-435,25);
+  }
 }
--- trunk/quests/qvic/#Svi-pral_the_Second_Arbitor.pl
+++ trunk/quests/qvic/#Svi-pral_the_Second_Arbitor.pl
@@ -1,22 +1,7 @@
 sub EVENT_AGGRO {
   quest::settimer(1,20);
-  }
+}
   
 sub EVENT_TIMER {
   quest::signalwith(295149,1);
-  }
-
-sub EVENT_DEATH {
-  $check_cyno = $entity_list->GetMobByNpcTypeID(295149);
-  $check_named = $entity_list->GetMobByNpcTypeID(295145);
-  $check_named = $entity_list->GetMobByNpcTypeID(295147);
-  $check_named = $entity_list->GetMobByNpcTypeID(295148);
-  if($check_cyno == 0) {
-      }
-  elsif($check_named == 0) {
-    quest::spanw2(295145,0,0,-216,-191,-467,192);
-    quest::spanw2(295146,0,0,-316,-181,-464,64);
-    quest::spanw2(295147,0,0,-243,-250,-439,248);
-    quest::spanw2(295148,0,0,-357,-275,-435,25);
-  }
 }

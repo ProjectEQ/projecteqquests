d42c17f58f0d221d042959c7febf9f19c1f55fe7
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-08-02 08:59:36 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- quests/torgiran/Gandak_Whisperwind.pl
+++ quests/torgiran/Gandak_Whisperwind.pl
@@ -1,10 +1,15 @@
+sub EVENT_SAY {
+  quest::say("I do not have time to talk right now."); #need real text
+}
+
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 21986 => 1)) {#need correct text
-    quest::say("What is.... By Tunare! This is from Faerik!  I knew I was not forgotten.  $name, I need your help to save my wife, I would do this myself, however I cannot return just yet.  I need you to assist me.  There is something amiss here, I believe that these trolls are planning something.  I need you to gather insignias from each of the four different factions that are found in the place called Dulak's Harbor, then return them to me.")
+  if (plugin::check_handin(\%itemcount, 21986 => 1)) { #Medallion of the Emerald warrior
+    quest::say("What is.... By Tunare! This is from Faerik! I knew I was not forgotten, $name. I need your help to save my wife. I would do this myself, however I cannot return just yet. I need you to assist me. There is something amiss here, I believe that these trolls are planning something. I need you to gather insignias from each of the four different factions that are found in the place called Dulak's Harbor, then return them to me."); #need correct text
+    quest::setglobal("Gandak",1,0,"F");
   }
-  
-  if (plugin::check_handin(\%itemcount, 21989 => 1, 21990 => 1, 21992=> 1, 	21991 => 1 )) {#need correct text
-    quest::say("Excellent work, $name!  Now that I have these I need one final favor from you.  I need you to continue on to the farmer in the Karanas to obtain the remedy for my wife.  Take him this bag and he will fill it with what you need.");
-    quest::summonitem(21987);
+  if (defined($qglobals{Gandak}) && ($qglobals{Gandak} == 1) && plugin::check_handin(\%itemcount, 21989 => 1, 21990 => 1, 21991=> 1, 21992 => 1)) { #Insignia of Hatred, Insignia of Slaying, Insignia of the Blackblood, Insignia of Suffering
+    quest::say("Excellent work, $name! Now that I have these I need one final favor from you. I need you to continue on to the farmer in the Karanas to obtain the remedy for my wife. Take him this bag and he will fill it with what you need."); #need correct text
+    quest::summonitem(21987); #Elven Medicine Bag
+    quest::delglobal("Gandak");
   }
 }

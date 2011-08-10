7d141f255c7193c8415dec05b3b15ce52ea47db2
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-08-10 23:30:36 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- quests/barindu/#Apprentice_Udranda.pl
+++ quests/barindu/#Apprentice_Udranda.pl
@@ -4,16 +4,21 @@ sub EVENT_SAY {
   $group = $client->GetGroup();
 
   if ($text=~/hail/i) {
-  	quest::say("Do you wish to enter the [mountains], or are you ready for [tipt]?");
+      quest::say("Do you wish to enter the [mountains], or are you ready for [tipt]?");
   }
   if ($text=~/mountains/i) {
-    if ($group) {
+    if ($group || $status > 79) {
       if (defined($qglobals{god_vxed_access}) && ($qglobals{god_vxed_access} == 1)) {
         quest::say("Your work in the sewers has been vital to our progression, please proceed with caution in the mountain pass. Find the Stonespiritist for further advancement.");
         if ($InInstanceVxed == 0 && $InInstanceTipt == 0) {
           $Instance = quest::CreateInstance("vxed", 0, 21600);
-          quest::AssignGroupToInstance($Instance);
-          quest::say("Instance added.");
+          if($group) {
+            quest::AssignGroupToInstance($Instance);
+            quest::say("Instance added.");
+          } elsif ($status >79){
+            quest::AssignToInstance($Instance);
+            quest::say("Instance added.");
+          }
         }
         else {
           $client->Message(13, "You are already in an instance!");
@@ -25,13 +30,18 @@ sub EVENT_SAY {
     }
   }
   if ($text=~/tipt/i) {
-    if ($group) {
+    if ($group || $status > 79) {
       if (defined($qglobals{god_tipt_access}) && ($qglobals{god_tipt_access} == 1)) {
         quest::say("Proceed with caution, you must face several trials to find your way through this pass.");
         if($InInstanceVxed == 0 && $InInstanceTipt == 0) {
           $Instance = quest::CreateInstance("tipt", 0, 21600);
-          quest::AssignGroupToInstance($Instance);
-          quest::say("Instance added.");
+          if($group) {
+            quest::AssignGroupToInstance($Instance);
+            quest::say("Instance added.");
+          } elsif ($status >79){
+            quest::AssignToInstance($Instance);
+            quest::say("Instance added.");
+          }
         }
         else {
           $client->Message(13, "You are already in an instance!");

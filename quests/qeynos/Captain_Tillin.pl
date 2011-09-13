3840d5b3208c974e2395256ae303d5757d6b72fe
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-09-13 17:25:24 +0000

    Submitting quests from HTTP submission area; verified by gaeorn

--- quests/qeynos/Captain_Tillin.pl
+++ quests/qeynos/Captain_Tillin.pl
@@ -14,30 +14,30 @@ sub EVENT_ITEM {
   if(plugin::check_handin(\%itemcount, 13915 => 1)) { #Gnoll Fang
     quest::say("Very good! One less gnoll the people of Qeynos need to fear. Here is your bounty as promised.");
     quest::summonitem(10070); #Moonstone
-    quest::faction(135,1);
-    quest::faction(9,1);
-    quest::faction(53,-1);
-    quest::faction(33,-1);
-    quest::faction(217,1);
+    quest::faction(135,1); #Guards of Qeynos
+    quest::faction(9,1);   #Antonius Bayle
+    quest::faction(53,-1); #Corrupt Qeynos Guards
+    quest::faction(33,-1); #Circle of Unseen Hands
+    quest::faction(217,1); #Merchants of Qeynos
     quest::exp(500);
   }
   elsif(plugin::check_handin(\%itemcount, 18811 => 1)) { #Tattered Note
     quest::say("I heard you were on your way. I have called for the state executioner. She should be on her way now. She will deal with our friend, McNeal Jocub. Thank you for your help, citizen.");
     quest::summonitem(13305); #Medal of Merit
-    quest::faction(9,10);
-    quest::faction(33,-10);
-    quest::faction(53,-10);
-    quest::faction(135,10);
-    quest::faction(217,10);
+    quest::faction(9,10);   #Antonius Bayle
+    quest::faction(33,-10); #Circle of Unseen Hands
+    quest::faction(53,-10); #Corrupt Qeynos Guards
+    quest::faction(135,10); #Guards of Qeynos
+    quest::faction(217,10); #Merchants of Qeynos
     quest::givecash(int(rand(10)),int(rand(10)),int(rand(10)),int(rand(10)));
   }
   elsif(plugin::check_handin(\%itemcount, 18912 => 1)) {
     quest::say("So, an assassin has been sent to Qeynos! I shall have my guards keep an eye out for any suspicious looking visitors. As for you... you should speak with the Surefall Glade ambassador. Ambassador Gash is staying at the Lion's Mane Inn here in South Qeynos. Inform him that [an assassin has been sent to kill] him. Do not let the assassin near him!");
-    quest::faction(9,10);
-    quest::faction(33,-10);
-    quest::faction(53,-10);
-    quest::faction(135,10);
-    quest::faction(217,10);
+    quest::faction(9,10);   #Antonius Bayle
+    quest::faction(33,-10); #Circle of Unseen Hands
+    quest::faction(53,-10); #Corrupt Qeynos Guards
+    quest::faction(135,10); #Guards of Qeynos
+    quest::faction(217,10); #Merchants of Qeynos
     quest::givecash(int(rand(10)),int(rand(10)),int(rand(10)),int(rand(10)));
     quest::spawn2(1138,0,0,-86.88,368.88,3.38,75.5);
   }

3d66c0d0bcca6fc764edbada072cafb8c754f12b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-10-09 06:14:59 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/poinnovation/Manaetic_Behemoth.pl
+++ trunk/quests/poinnovation/Manaetic_Behemoth.pl
@@ -1,6 +1,11 @@
 my $goactive = 0;
 my $first_signal = 0;
 
+sub EVENT_SPAWN {
+  #5 minute safety check that will kick begin event.
+  quest::settimer(2,300);
+}
+
 sub EVENT_SIGNAL {
   #signal 1 is from the spiders.
   if($signal == 1 && $first_signal == 0) {
@@ -10,6 +15,8 @@ sub EVENT_SIGNAL {
   } elsif($signal == 1 && $first_signal == 1 && $goactive > 0) {
     #if we received a signal and it was not the first signal and $goactive is > 0 reset go active back to 0
     $goactive = 0;
+	#restart safety check timer
+	quest::settimer(2,300);
   }
 }
 
@@ -20,10 +27,16 @@ sub EVENT_TIMER {
     #now check if we have been incrementing for 3 minutes.
     #increments at +1 per 5 seconds means $goactive == 60 is 3 minutes.
     if($goactive >= 60) {
-      #spawn the targetable version and depop untargetable version.
-      quest::spawn2(206074,0,0,$x,$y,$z,0);
-      #depop with respawn timer active.
-      $npc->Depop(1);
+      BEGIN_MB_EVENT();
     }
+  } elsif($timer == 2) {
+    BEGIN_MB_EVENT();
   }
+}
+
+sub BEGIN_MB_EVENT {
+  #spawn the targetable version and depop untargetable version.
+  quest::spawn2(206074,0,0,$x,$y,$z,0);
+  #depop with respawn timer active.
+  $npc->Depop(1);
 }

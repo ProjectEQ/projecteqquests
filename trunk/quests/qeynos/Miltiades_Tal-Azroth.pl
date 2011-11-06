f13397cda498cf8aef6c127a7fb0e817e47ae649
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-11-06 15:42:00 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/qeynos/Miltiades_Tal-Azroth.pl
+++ trunk/quests/qeynos/Miltiades_Tal-Azroth.pl
@@ -1,6 +1,4 @@
 
-
-
 sub EVENT_SAY { 
 if($text=~/Hail/i){
 quest::say("Greetings and salutations $name. I am Miltiades Tal`Azroth Crusader of Karana. I have spent all of my years here in Qeynos and gathered infinite knowledge from my teachers on my path to enlightenment. I have also started training new recruits to help them find their calling as a Holy Warrior of Karana. If you are a [Paladin of Karana] I might have some tasks for you to complete.");
@@ -8,45 +6,45 @@ quest::say("Greetings and salutations $name. I am Miltiades Tal`Azroth Crusader 
 if($text=~/paladin of karana/i){
 quest::say("That is excellent news $name. As a Knight in training of Karana I would like to offer you a series of [tests] what will test your devotion to yourself and your god.");
 }
-if($text=~/what tests/i){
+if($text=~/tests/i){
 quest::say("I have put together a series of tests that will enable our new recruits to create their own armor. You will use a Rainkeepers Assembly Kit to combine assorted items that you will collect from both the hunting areas surround Qeynos and the city itself. Combining these magical items in your kit will create a material that you will place in a forge along with molds that I will present to you to create your armor. Are you ready to start [fashioning your materials]?");
 }
 if($text=~/fashioning your materials/i){
-quest::say("Excellent $name. Please take this Rainkeepers Assembly Kit. Now for your materials you will fashion you will obviously need to know what items are used for what armor materials. Simply tell me what armor piece you [want] to create and I will present you with the recipe for that specific armor material. I can present you with the molds and recipes necessary for creating Rainkeeper [Helms], [Bracers], [Armguards], [Boots], [Greaves], [Gauntlets] and [Breastplates].");
+quest::say("Excellent $name. Please take this Rainkeepers Assembly Kit. Now for your materials you will fashion you will obviously need to know what items are used for what armor materials. Simply tell me what armor piece you [want] to create and I will present you with the recipe for that specific armor material. I can present you with the molds and recipes necessary for creating Rainkeeper [Helm], [Bracers], [Armguards], [Boots], [Greaves], [Gauntlets] and [Breastplate].");
 quest::summonitem(17264); 
 }
-if($text =~ /I want to craft armguards/i) {
+if($text =~ /armguards/i) {
 	quest::say("Here is your Rainkeepers Armguards Mold. To create your armguard material you will need to combine 2 Bricks of Crude Iron, 1 Snake Fang and 1 Burned Out Lightstone and 1 Fishing Bait in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Armguards of the Rainkeeper.");
 quest::summonitem(22613);
 }
-if($text =~ /I want to craft boots/i) {
+if($text =~ /boots/i) {
 	quest::say("Here is your Rainkeepers Boot Mold. To create your boot material you will need to combine 3 Bricks of Crude Iron, 1 Patch of Gnoll Fur, 1 Giant Snake Fang and 1 Bandage in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Boots of the Rainkeeper.");
 quest::summonitem(22612);
 }
-if($text =~ /I want to craft bracers/i) {
+if($text =~ /bracers/i) {
 	quest::say("Here is your Rainkeepers Bracer Mold. To create your bracer material you will need to combine 1 Brick of Crude Iron, 2 Gnoll Pup Scalps, 1 Patch of Gnoll Fur and 1 Water Flask in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Bracer of the Rainkeeper.");
 quest::summonitem(22611);
  }
- if($text =~ /I want to craft a breastplate/i) {
-	quest::say("Here is your Rainkeepers Breastplate Mold. To create your breastplate material you will need to combine 5 Bricks of Crude Iron, 1 Coyote Skull, 1 Golden Bandit Tooth, 1 Young Plains Cat Scalp, 1 Diseased Wolf Pelt and 1 Lightstone in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Breastplate of the Rainkeeper. Please come back to see me after you");
+ if($text =~ /breastplate/i) {
+	quest::say("Here is your Rainkeepers Breastplate Mold. To create your breastplate material you will need to combine 5 Bricks of Crude Iron, 1 Coyote Skull, 1 Golden Bandit Tooth, 1 Young Plains Cat Scalp, 1 Diseased Wolf Pelt and 1 Lightstone in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Breastplate of the Rainkeeper. Please come back to see me after you have created your breastplate because I have one [final favor] to ask of you.");
 quest::summonitem(22616);
  }
- if($text =~ /I want to craft gauntlets/i) {
+ if($text =~ /gauntlets/i) {
 	quest::say("Here is your Rainkeepers Gauntlets Mold. To create your gauntlet material you will need to combine 3 Bricks of Crude Iron, 1 Chunk of Meat, 1 Shadow Wolf Paw, and 2 Giant Fire Beetle Eyes in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Gauntlets of the Rainkeeper.");
 quest::summonitem(22615);
  }
- if($text =~ /I want to craft greaves/i) {
+ if($text =~ /greaves/i) {
 	quest::say("Here is your Rainkeepers Greaves Mold. To create your greaves material you will need to combine 4 Bricks of Crude Iron, 2 Ruined Lion Skins, 1 Spider Legs and 1 Bottle in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Greaves of the Rainkeeper.");
 quest::summonitem(22614);
 }
-if($text =~ /I want to craft a helm/i) {
+if($text =~ /helm/i) {
 	quest::say("Here is your Rainkeepers Helm Mold. To create your helm material you will need to combine 2 Bricks of Crude Iron, 1 Patch of Gnoll Fur, 1 Large Snake Skin and 1 Fish Wine in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Helm of the Rainkeeper.");
 quest::summonitem(22610);
  }
- if($text =~ /What final favor/i) {
+ if($text =~ /final favor/i) {
 	quest::say("In the past I have sent some of my finest messengers to Qeynos Hills to deliver messages to the Sayers about current events in Qeynos. However the last few I have sent did not return so I began to look into what might have happened when I received word that the Bloodsabers had claimed responsibility for the slayings. I must ask that you bring me the head of a certain Bloodsaber who I think is [responsible] for the slayings.");
  }
- if($text =~ /Who is responsible for the slayings/i) {
+ if($text =~ /responsible/i) {
 	quest::say("I have reason to believe that Tovax Vmar is behind this, a dreaded necromancer that has been known to prowl the hills. Having Tovax dead will surely send a message to the Bloodsabers to tread lightly for their days are numbered. If you are able to find Tovax return to me his necklace along with 2 Enchanted Wisp Globes and I will reward you with a weapon suitable for a true knight of Karana.");
  }
 }

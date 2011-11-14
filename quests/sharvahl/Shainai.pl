31f91de87292f293c87dd2c5af69c67c5b9fe90b
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-11-14 05:01:36 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/sharvahl/Shainai.pl
+++ quests/sharvahl/Shainai.pl
@@ -1,10 +1,33 @@
-sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 4460 => 1)) {
-    plugin::return_items(\%itemcount); #Merged from Not_Found.pl. Incomplete quest script.
-  }
-  else {
-    quest::say("I have no need for this, $name.");
-    plugin::return_items(\%itemcount);
-  }
+sub EVENT_SPAWN {
+	quest::disable_spawn2(32803);
 }
-#END of FILE Zone:sharvahl  ID:Not_Found -- #Shainai
+
+sub EVENT_WAYPOINT_ARRIVE {
+	if($wp==21) {
+		quest::say("Yup yup, I definitely know the way from here.");
+	}
+	if($wp==36) {
+		quest::say("Ahhh, home at last.");
+		quest::signalwith(155041,1,10);
+		$npc->SetAppearance(3);
+	}
+}
+
+sub EVENT_SIGNAL {
+	if($signal==1) {
+		quest::say("Daddy I'm sleepy, could you come tuck me in ?");
+		quest::signalwith(155340,1,10);
+	}
+}
+
+sub EVENT_ITEM {
+	if(plugin::check_handin(\%itemcount, 4478 => 1)) {
+		quest::emote("settles comfortably in for restful afternoon nap, 'Thank you for walking me home, it was ever so nice of you.  Goodnight.'");
+		quest::signalwith(155340,2,10);
+		quest::depop();
+	}
+	else {
+		quest::say("I have no need for this, $name.");
+		plugin::return_items(\%itemcount);
+	}
+}

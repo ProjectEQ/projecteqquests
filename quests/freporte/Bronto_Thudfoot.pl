0202c8259245131d8dfb96bcd437fb5f5ff36275
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-11-24 05:32:34 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/freporte/Bronto_Thudfoot.pl
+++ quests/freporte/Bronto_Thudfoot.pl
@@ -3,3 +3,7 @@ sub EVENT_COMBAT{
 		quest::say("That was not a very intelligent thing to do.");
 	}
 }
+
+sub EVENT_SIGNAL {
+	quest::say("You said it, boss!  Stay clear of taking sides and you should be just fine, young one.");
+}

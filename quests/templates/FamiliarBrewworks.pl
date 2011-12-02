437863cb48dca6b9e02adad2c1833a7da8a383bc
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-12-02 07:22:03 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/templates/FamiliarBrewworks.pl
@@ -0,0 +1,16 @@
+##Brewworks familiar from Station Store. Converts water flasks to ale.##
+
+sub EVENT_SPAWN{
+   quest::say("Brewworks Unit ready to convert Water Flasks to Ale, Master.");
+}
+
+sub EVENT_ITEM{
+   if(plugin::check_handin(\%itemcount, 13006 => 1)){
+      quest::say("Ale brewed, Master.");
+      quest::summonitem(13039);
+   }
+   else{
+      plugin::return_items(\%itemcount);
+      quest::say("I cannot brew anything from this.");
+   }
+}

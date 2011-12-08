06600f659461c39e42164282860bf0173d603f23
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-12-08 06:38:56 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ trunk/quests/tenebrous/a_grol_baku_soldier.pl
@@ -0,0 +1,7 @@
+sub EVENT_AGGRO {
+	quest::emote("speaks in a voice like grinding stones, 'Mountain Tegi made of stone will crush this being of flesh and bone!'");
+}
+
+sub EVENT_DEATH {
+	quest::say("Uuuuhhh...arg!");
+}

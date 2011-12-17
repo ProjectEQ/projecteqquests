a914d994d191eb3629a9d60995067100b1bac3d4
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2011-12-17 02:47:26 +0000

    Submitting quests from HTTP submission area; verified by cavedude

---
+++ quests/katta/Guard_Lennon.pl
@@ -0,0 +1,16 @@
+sub EVENT_SPAWN {
+	quest::settimer(1,320);
+}
+
+sub EVENT_TIMER {
+	$npc->SetAppearance(4); 
+}
+
+sub EVENT_COMBAT {
+	if($combat_state == 0) {
+		quest::say("Don't think you'll get away from me so easily!");
+	}
+	if($combat_state == 1) {
+		quest::say("Serves you right, knave.  This is what happens to those who break the law.");
+	}
+}

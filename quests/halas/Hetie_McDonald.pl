2ed31844a6d5a90e71e1a730c10912706de34ceb
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-02-10 06:28:36 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/halas/Hetie_McDonald.pl
+++ quests/halas/Hetie_McDonald.pl
@@ -1,6 +1,6 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Welcome back $name. Why don't you purchase a few things a few things before you head out into the tundra again?"); }
+sub EVENT_SAY {
+  if ($text=~/hail/i) {
+    quest::say("Welcome back, $name. Why don't you purchase a few things before you head out into the tundra again?");
+  }
 }
-#END of FILE Zone:halas  ID:29038 -- Hetie_McDonald 
-
+#END of FILE Zone:halas  ID:29038 -- Hetie_McDonald

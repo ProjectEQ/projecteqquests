c4b0eaff9996f613282dc069ba2d7c6696b1744e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-03-27 19:46:29 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/oggok/Bouncer_Gronk.pl
+++ quests/oggok/Bouncer_Gronk.pl
@@ -1,14 +1,3 @@
-sub EVENT_SPAWN {
-	quest::settimer("1",5);
-}
-
-sub EVENT_TIMER {
-	if ($timer == "1" && $zonetime > 799 && $zonetime < 1201) {
-		quest::unique_spawn(49092,0,0,$x,$y,$z,$h);
-		$npc->Depop(1);
-	}
-}
-
 sub EVENT_AGGRO {
   quest::say("Bouncer smash you!!");
 }

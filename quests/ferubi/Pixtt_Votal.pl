ef6343ee516c8317554e1074bcca9dd37195e551
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-04-08 03:07:57 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/ferubi/Pixtt_Votal.pl
+++ quests/ferubi/Pixtt_Votal.pl
@@ -1,25 +1,3 @@
-sub EVENT_SPAWN {
-  quest::settimer("already_up",1);
-}
-
-sub EVENT_COMBAT {
-  quest::settimer("depop_me",300);
-}
-
-sub EVENT_TIMER {
-  if($timer eq "already_up") {
-    if($entity_list->GetMobByNpcTypeID(284079)) {
-      $npc->Depop(1);
-      quest::stoptimer("already_up");
-    }
-  } elsif($timer eq "depop_me") {
-    if($combat_state == 0) { 
-      $npc->Depop(1);
-      quest::stoptimer("depop_me");
-    }
-  }
-}
-
 sub EVENT_DEATH {
   if($entity_list->GetMobByNpcTypeID(284080)) {
     quest::spawn2(284079,0,0,$x,$y,$z,$h);

ffd7521d142c3eaed41cd8813ca1a0efbd1a1e03
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-04-22 06:18:43 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/wakening/Korzak_Stonehammer.pl
+++ trunk/quests/wakening/Korzak_Stonehammer.pl
@@ -21,4 +21,10 @@ sub EVENT_ITEM {
 	plugin::return_items(\%itemcount);
 }
 
+sub EVENT_COMBAT {
+	if($combat_state==1) {
+		quest::say("Puny creature, feel the might of the Kromrif as I crush you into mortar for our walls!");
+	}
+}
+
 
---
+++ trunk/quests/wakening/a_frost_giant_laborer.pl
@@ -0,0 +1,6 @@
+sub EVENT_COMBAT {
+	if($combat_state==1) {
+		quest::say("Puny creature, feel the might of the Kromrif as I crush you into mortar for our walls!");
+	}
+}
+

84cf694fb44d3d80d5cd2c30f628a443eb762586
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-05-01 02:24:05 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/butcher/Nyzil_Bloodforge.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_COMBAT {
-	if($combat_state == 1) {
-	quest::say("Now you shall know the skill of a member of the Bloodforge Brigade!!");
-	}
-	if($combat_state == 0) {
-	quest::say("Yet another victory for Kaladim and the Bloodforge Brigade!");
-	}
-}

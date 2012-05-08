a1172d0c2c68d9adaeb2886708bbc244fc89236d
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-05-08 14:32:57 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/crystal/#The_Fabled_Foreman_Smason.pl
+++
@@ -1,6 +0,0 @@
-sub EVENT_COMBAT {
-	if($combat_state == 1){
-		quest::say("AARRGGHH!  Thieves!  Your blood shall run like the veins of velium in these caverns!");
-	}
-}
-
--- quests/crystal/Guard_Slacey.pl
+++
@@ -1,6 +0,0 @@
-sub EVENT_COMBAT {
-	if($combat_state == 1){
-		quest::say("We have defended these walls against the vermin from below, you will be shown that very same defeat!");
-	}
-}
-

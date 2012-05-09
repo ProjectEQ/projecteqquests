88497ab8d0d5ff6678d70d94bed4350543d8ed9e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-05-09 14:44:09 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/eastkarana/Theoris_Bladespur.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_COMBAT {
-	if($combat_state==1) {
-		quest::emote("curls his lip and draws his blades in a professional, intimidating quickness, 'You will soon know to regret the arrogance of your ways, little one -- and how unfortunate that your regret will be so short lived in the last seconds of your life.'");
-	}
-}
-
-sub EVENT_DEATH {
-	quest::emote("drops his weapons and gives a brief, muffled growl that sprays a light mist of blood from his lips as he falls dead to the ground.");
-}

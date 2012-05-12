76b6047d0174616fc07a2472ee4c8b4f7874eeb2
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-05-12 05:15:39 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/feerrott/a_lizardman_scout.pl
+++
@@ -1,7 +0,0 @@
-sub EVENT_COMBAT {
- if($combat_state == 1){
-  quest::say("Errrrrr.  Die soft skin!");
- }
-}
-
-#Submitted by: Jim Mills
--- quests/fieldofbone/Crusader_Quarg.pl
+++ quests/fieldofbone/Crusader_Quarg.pl
@@ -41,13 +41,3 @@ sub EVENT_ITEM
    }
 }
 
-sub EVENT_COMBAT {
-	if($combat_state==1) {
-		quest::say("Hiss.. You cannot hope defeat a Crusader of Greenmist!!");
-	}
-}
-
-sub EVENT_DEATH {
-	quest::say("Ha ha!!  Fool..  The Crusaders of Greenmist shall torture you to death! Ha ha!!");
-}
-

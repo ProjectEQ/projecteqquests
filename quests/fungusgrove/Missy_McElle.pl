eae539737005143002dddc9e35f2c3065e899644
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-09-02 04:13:18 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- quests/fungusgrove/Missy_McElle.pl
+++
@@ -1,7 +0,0 @@
-sub EVENT_AGGRO {
-	quest::say("That's the last mistake you'll make!");
-}
-
-sub EVENT_DEATH {
-	quest::say("Guards! Guards!!");
-}
--- quests/inktuta/296024.pl
+++ quests/inktuta/296024.pl
@@ -1,3 +1,4 @@
+# #Kelekdrix,_Herald_of_Trushar
 
 sub EVENT_SPAWN {
   quest::setnexthpevent(99);
@@ -10,12 +11,12 @@ sub EVENT_SIGNAL {
 }
 
 sub EVENT_TIMER {
-  if($timer eq "SpawnAdds") }
+  if($timer eq "SpawnAdds") {
     if(!$entity_list->GetMobByNpcTypeID(296025) && !$entity_list->GetMobByNpcTypeID(296025)) {
 	  quest::modifynpcstat("special_attacks","SQUMCNIDf");
 	  SPAWN_MY_ADDS();
 	  quest::stoptimer("SpawnAdds");
-	}
+    }
   }
 }
 
@@ -32,7 +33,7 @@ sub EVENT_DEATH {
   quest::say("Your victory here is hollow. With my death, the way to the lower reaches is unsealed. The denizens below destroy everything they touch. You will suffer their wrath!");
   quest::ze(15,"The sound of moving gears and grinding stone reverberates throughout the temple. A door has been unlocked.");
   $entity_list->FindDoor(41)->SetLockPick(0);
-  quest::spawn2(296027,0,0,165,-496,-27,64);
+  quest::spawn2(296027,0,0,90,-515,-27,64);
   my $instid = quest::GetInstanceID("inktuta",0);
   quest::setglobal($instid.'_inktuta_status',2,3,"H6");
   quest::depopall(296025);

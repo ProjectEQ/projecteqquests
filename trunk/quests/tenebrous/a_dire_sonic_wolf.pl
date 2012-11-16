ae868ee57ad76f4a37f368fe493870d56718cc13
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-11-16 19:59:04 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/tenebrous/a_dire_sonic_wolf.pl
+++
@@ -1,7 +0,0 @@
-sub EVENT_AGGRO {
-	quest::emote("emits an eerie screech and charges ferociously!");
-}
-
-sub EVENT_DEATH {
-	quest::emote("squeals in pain and falls to its side as the life fades from its eyes");
-}

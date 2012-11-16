717180838a5d4bad66783dd79e4853ce449ce27e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2012-11-16 19:56:34 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/tenebrous/Tymoz.pl
+++ trunk/quests/tenebrous/Tymoz.pl
@@ -1,13 +1,6 @@
 #Tymoz.pl
 #Custodus Helms
 
-sub EVENT_AGGRO { 
- quest::emote("hisses revealing menacing fangs and lunges ferociously at its prey!");
-}
-
-sub EVENT_DEATH {
-	quest::emote("collapses to the ground and begins to decompose at an alarming rate.");
-}
 
 sub EVENT_SAY { 
 	if($text=~/Hail/i){

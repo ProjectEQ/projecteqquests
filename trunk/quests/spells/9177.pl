af793ee345cbf922fd561a791b70e6ff69b3b16e
cavedude00 <cavedude00@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-01-09 20:10:46 +0000

    Submitting quests from HTTP submission area; verified by cavedude

--- trunk/quests/spells/9177.pl
+++ trunk/quests/spells/9177.pl
@@ -2,10 +2,10 @@ sub EVENT_SPELL_EFFECT_CLIENT
 {
 	if(($client->GetClientVersionBit() & 4294967264)!= 0)
 	{
-		quest::MovePCInstance(344,5,375,0,2,256);
+		quest::MovePCInstance(344,5,0,375,2,256);
 	}
 	else
 	{
-		quest::movepc(344,375,0,2,256);
+		quest::movepc(344,0,375,2,256);
 	}
 }

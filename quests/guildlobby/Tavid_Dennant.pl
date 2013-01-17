66a4b8e3fa6635cd59a4b7f0e55c9f11047f9310
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-01-17 05:26:05 +0000

    adding script for RoF guild lobby NPC and testing commit status.

---
+++ quests/guildlobby/Tavid_Dennant.pl
@@ -0,0 +1,9 @@
+#status: initial copy/paste of ZAM info
+
+sub EVENT_SAY {
+  if($text=~/hail/i) {
+    quest::say("Well hello! What brings a _____ like you to a place like this? Me? I seem to have misplaced my scarf, I don't suppose you've seen it? Long, wool, colorful? I can't imagine where I might have left it. Speaking of misplaced things, I've got quite a number of who's its and what's its that I've picked up during my travels. You're welcome to take a look if you think you might find any of it useful. All except for this [watch] of course. I also have a limited amount of [items] that work with armor from beyond the void. These I will even give for free, but you must ask me for them.");
+  } elsif($text=~/luminescent/i) {
+    quest::summonitem(429420);
+  }
+}

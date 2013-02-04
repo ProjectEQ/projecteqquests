1849c2b088aee3f15c92e0c45b72e085120414c0
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-04 03:29:37 +0000

    added missing text and missing faction hits as well as some corrections to guildmaster newbie scripts.

--- trunk/quests/oggok/Guntrik.pl
+++ trunk/quests/oggok/Guntrik.pl
@@ -1,30 +1,38 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Mmmph!!  Who you?  Oh. you $name.  You supposed to be promising Craknek.  Me need you.  There bad things in swamp.  You want [help Crakneks] or you [want Guntrik bash your face]!!?");
+	if($text=~/hail/i){
+		quest::say("Mmmph!!  Who you?  Oh. you $name.  You supposed to be promising Craknek.  Me need you.  There bad things in swamp.  You want [help Crakneks] or you [want Guntrik bash your face]!!?");
+	}
+	if($text=~/what help crakneks/i){
+		quest::say("Crakneks warriors!!  They be far back.  Through last caves.  They smash and bash.  They no like Greenbloods.  Bouncers keep thems from bashing.");
+	}
+	if($text=~/want gruntrik to bash my face/i){
+		quest::say("OK!! Me bash!!  You hurt!!");
+	}
+	if ($text=~/help crakneks/i) {
+		quest::say("Go to swamps. Find noble hunters of humans and elves. All have house crests. Return to me and I give things. If you find special item says where hunters sleep, give to me. If meat founded then give to Clurg's cook. She make fine stew and give coins for meat.");
+	}
 }
-if($text=~/what help crakneks/i){
-quest::say("Crakneks warriors!!  They be far back.  Through last caves.  They smash and bash.  They no like Greenbloods.  Bouncers keep thems from bashing.");
-}
-if($text=~/i want gruntrik to bash my face/i){
-quest::say("OK!! Me bash!!  You hurt!!");
-quest::say("Me strongest. You weak. Craknek best alwayss."); }
-}
-if ($text=~/me wanna help crakneks/i) {
-  quest::say("Go to swamps. Find noble hunters of humans and elves. All have house crests. Return to me and I give things. If you find special item says where hunters sleep, give to me. If meat founded then give to Clurg's cook. She make fine stew and give coins for meat.");
-}
+
 sub EVENT_ITEM { 
- if($itemcount{18787} == 1){
-	quest::say("Ahh..  good..  good..  here. take..  now you Craknek! You bash good. Bash lizards. Bash froggies. Bash mushrooms. Remember. you no help Greenbloods. Crakneks stronger than Greenbloods.");
-	quest::summonitem("13525");
- }
- if (plugin::check_handin(\%itemcount, 13361 => 1)) {
-   quest::say("One less trouble. Hunhuh!! You do good work. Keep up. Remember to bring any special things to Guntrik. Here junk for good work. Go away now.");
-   quest::summonitem(quest::ChooseRandom(5032,5029, 5033, 5031, 13355));
- } else {
-  #do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
-  plugin::return_items(\%itemcount);
- }
+	if (plugin::check_handin(\%itemcount, 18787 => 1)) {
+		quest::say("Ahh..  good..  good..  here. take..  Now you Craknek! You bash good. Bash lizards. Bash froggies. Bash mushrooms. Remember. you no help Greenbloods. Crakneks stronger than Greenbloods. When you [want to help crakneks] you tell Guntrik.");
+		quest::summonitem(13525);
+		quest::ding();
+		quest::faction(57,5);  # Craknek Warrior
+		quest::faction(46,5);  # Clurg
+		quest::faction(128,-10); # Green Blood Knight
+		quest::exp(100);
+	}
+	if (plugin::check_handin(\%itemcount, 13361 => 1)) {
+		quest::say("One less trouble. Hunhuh!! You do good work. Keep up. Remember to bring any special things to Guntrik. Here junk for good work. Go away now.");
+		quest::summonitem(quest::ChooseRandom(5032,5029, 5033, 5031, 13355));
+	} 
+	else {
+		#do all other handins first with plugin, then let it do disciplines
+		plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
+		plugin::return_items(\%itemcount);
+	}
 }
+
 #END of FILE Zone:oggok  ID:49043 -- Guntrik 
 
--- trunk/quests/oggok/Soonog.pl
+++ trunk/quests/oggok/Soonog.pl
@@ -2,17 +2,30 @@ sub EVENT_SAY {
   if ($text=~/hail/i) {
     quest::say("You [Greenblood]?");
   }
+  if ($text=~/greenblood/i) {
+    quest::say("Good, you mine.  We tuffest, bestest, scariest warriors in da Norrath.  Not like dem wimpy Craknek hoomie wannabees.  You Greenblood, you my toy.  You lives or dies at my will.  Go, brings me many lizard meats cause I Greenblood leader and I berry hungry lots.  So hungry dat I eats four lizards.  Go bring meat or I eats you.");
+  }  
 }
 
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 18789 => 1)) {
-    quest::say("Soonog own you now.. fight for Soonog.. Soonog make you power.. Soonog army rule all!");
+    quest::say("Soonog own you now.. fight for Soonog.. Soonog make you strong.. When you ready for task you tell Soonog.  Soonog army rule all!");
     quest::summonitem(13527);
+	quest::faction(128,5);  # Green Blood Knight
+	quest::faction(46,5);   # Clurg
+	quest::faction(314,-10); # Storm Guard
+	quest::faction(292,-10); # Shadowknight of Night Keep
+	quest::ding();
     quest::exp(100);
   }
   elsif (plugin::check_handin(\%itemcount, 13410 => 4)) {
-    quest::say("You do good... take this... you fight again for Soonog!");
+    quest::say("Small meats but is good nuff.  You take dis and go kill eberyting.  Make all scared of da Greenblood knights.  You get ready for next inb.. ins.. invat.. you get ready for next war.  You learning stuff, you come bak here and me teach you more Greenblood stuf so we be more tuffest.");
     quest::summonitem(5023);
+	quest::faction(128,5);  # Green Blood Knight
+	quest::faction(46,5);   # Clurg
+	quest::faction(314,-10); # Storm Guard
+	quest::faction(292,-10); # Shadowknight of Night Keep
+	quest::ding();
     quest::exp(50);
   }
   else {

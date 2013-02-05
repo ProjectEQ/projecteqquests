c5f9716cf953d292eb920c1c8286df308d2a9bf1
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-05 06:00:35 +0000

    Rewrote and Fixed for Arantir Karondor / Dargon script for Wizard Epic.

--- trunk/quests/halas/Arantir_Karondor.pl
+++ trunk/quests/halas/Arantir_Karondor.pl
@@ -3,17 +3,15 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    if($wizepicB == 1) {
-      quest::say("Ah yes, you again. Do you have the items? Give me the three you possess and I'll combine them with my own.");
-    }
-    elsif($wizepicA == 1) {
+  if (($text=~/hail/i) && (defined $qglobals{wizepicB})) {
+    quest::say("Ah yes, you again. Do you have the items? Give me the three you possess and I'll combine them with my own.");
+  }
+  if(($text=~/hail/i) && (defined $qglobals{wizepicA})) {
       quest::say("Ah, but it pains my heart to see this. How I could love a women like that is beyond me. And yet, I still do love her. It was on the day I was to ask her to marry me that I lost my powers. When I was about to cast my greatest spell to prove my love to her, my magic failed. She ran out on me that day. But enough of me, do you wish to hear my story?");
-    }
-    else {
-      quest::say("Before I tell you anything, I require you to help me. Seek a woman named Challice. Give her this letter and then return to me.");
+  }
+  if(($text=~/hail/i) && (!defined $qglobals{wizepicA}) && (!defined $qglobals{wizepicB})) {
+      quest::say("Before I tell you anything, I require you to help me. Seek a woman named Challice. Give her this ring and then return to me.");
       quest::summonitem(14334);
-    }
   }
   if($text =~ /story/i) {
     quest::say("I once was one of four true followers of Solusek Ro. So great were the four of us that Solusek Ro himself made us each a special item. We did not know what the others' items were, but we realized that was for our own safety. Each one of us, after receiving the item, began to plot against Solusek Ro. Each of us believed we had enough power to dominate Norrath. Solusek Ro saw this and at the time we needed our powers most, he stripped us of them. Now I know two of the other wizards by name. One was called Sylen Tyrn, the other's name was Demunir Scry. The last wizard was a gnome, but I can't think of his name.");
@@ -38,28 +36,19 @@ sub EVENT_ITEM {
     quest::setglobal("wizepicA",1,0,"F");
   }
   #Return Oil Note handin:
-  if (plugin::check_handin(\%itemcount, 18168 => 1)) {
+  elsif (plugin::check_handin(\%itemcount, 18168 => 1)) {
     quest::delglobal("wizepicA");
     quest::setglobal("wizepicB",1,0,"F");
   }
   #Staff of Gabstik, Blue Crystal Staff, Gnarled Staff 
-if ($wizepicB == 1) {
-  if (plugin::check_handin(\%itemcount, 14337 => 1, 14338 => 1, 14339 => 1)) {
+  elsif ((defined $qglobals{wizepicB}) &&  plugin::check_handin(\%itemcount, 14337 => 1, 14338 => 1, 14339 => 1)){
       quest::say("Here, this pack contains all of our items. You will never be able to open it again, so you must deliver the pack, intact, to Solomen. He will then reward you. Now that I have helped you, leave me in peace.");
       quest::summonitem(14340);
       quest::delglobal("wizepicB");
-    }
   }
-  plugin::return_items(\%itemcount);
-}
-
-sub EVENT_SIGNAL {
-  if ($signal == 101) {
-    quest::setglobal("wizepicA",1,0,"F");
-  }
-  if ($signal == 102) {
-    quest::delglobal("wizepicA");
-    quest::setglobal("wizepicB",1,0,"F");
+  else {
+	quest::say("I have no need for this $name, you can have it back");
+	plugin::return_items(\%itemcount);
   }
 }
 

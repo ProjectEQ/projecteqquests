c5d18cee754e103a44da29c79663221acd19a3a0
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-13 03:27:53 +0000

    Update Oggok newbie GM scripts to include client emotes also added missing texts.

--- trunk/quests/oggok/Soonog.pl
+++ trunk/quests/oggok/Soonog.pl
@@ -1,31 +1,51 @@
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER {
+  if (($ulevel == 1) && ($class == "Shadowknight")) { 
+		$client->Message(15,"You have entered what appears to be the guild hall of the Greenbloods. A large, brutish Ogre seems to be annoyed by your appearance. 'Wut? Where you come from? Oh, you here to train? Read note in inventory and then hand to me to start. Me make you strong Greenblood sum day!'");
+  }
+}
+
 sub EVENT_SAY {
   if ($text=~/hail/i) {
     quest::say("You [Greenblood]?");
   }
   if ($text=~/greenblood/i) {
     quest::say("Good, you mine.  We tuffest, bestest, scariest warriors in da Norrath.  Not like dem wimpy Craknek hoomie wannabees.  You Greenblood, you my toy.  You lives or dies at my will.  Go, brings me many lizard meats cause I Greenblood leader and I berry hungry lots.  So hungry dat I eats four lizards.  Go bring meat or I eats you.");
-  }  
+  }
+  if ($text=~/trades/i) {
+    quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+    quest::summonitem(51121); # Tradeskill Basics Volume 1
+  }
+  if ($text=~/second book/i) {
+    quest::say("Here is the second volume of the book you requested, may it serve you well!");
+    quest::summonitem(51122); # Tradeskill Basics Volume 2
+  }
 }
 
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 18789 => 1)) {
-    quest::say("Soonog own you now.. fight for Soonog.. Soonog make you strong.. When you ready for task you tell Soonog.  Soonog army rule all!");
+    quest::say("Soonog own you now.. fight for Soonog.. Soonog make you strong.. When you ready for task you tell Soonog.  Soonog army rule all!! You return to me when you stronger, I teach you many things I know, including how to start working in the various [trades].");
     quest::summonitem(13527);
-	quest::faction(128,5);  # Green Blood Knight
-	quest::faction(46,5);   # Clurg
-	quest::faction(314,-10); # Storm Guard
-	quest::faction(292,-10); # Shadowknight of Night Keep
-	quest::ding();
+		quest::faction(128,5);  # Green Blood Knight
+		quest::faction(46,5);   # Clurg
+		quest::faction(314,-10); # Storm Guard
+		quest::faction(292,-10); # Shadowknight of Night Keep
+		quest::ding();
     quest::exp(100);
   }
   elsif (plugin::check_handin(\%itemcount, 13410 => 4)) {
     quest::say("Small meats but is good nuff.  You take dis and go kill eberyting.  Make all scared of da Greenblood knights.  You get ready for next inb.. ins.. invat.. you get ready for next war.  You learning stuff, you come bak here and me teach you more Greenblood stuf so we be more tuffest.");
     quest::summonitem(5023);
-	quest::faction(128,5);  # Green Blood Knight
-	quest::faction(46,5);   # Clurg
-	quest::faction(314,-10); # Storm Guard
-	quest::faction(292,-10); # Shadowknight of Night Keep
-	quest::ding();
+		quest::faction(128,5);  # Green Blood Knight
+		quest::faction(46,5);   # Clurg
+		quest::faction(314,-10); # Storm Guard
+		quest::faction(292,-10); # Shadowknight of Night Keep
+		quest::ding();
     quest::exp(50);
   }
   else {
--- trunk/quests/oggok/Zulort.pl
+++ trunk/quests/oggok/Zulort.pl
@@ -1,3 +1,15 @@
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER {
+  if (($ulevel == 1) && ($class == "Shaman")) { 
+		$client->Message(15,"You stand in a large alcove. Next to you is a huge Ogre, with an evil grin. 'You come for training? You join us or me will smash you! I am Zulort. I train you! Read note in inventory and hand to me when ready to start!'");
+  }
+}
+
 sub EVENT_SAY {
   if($text=~/hail/i) {
     quest::say("Um, you. Hi.. You Shaman of War now, right? You gotta learns war and war spells. We fights all and makes the Warlord likes us. You [gonna help] or me gonna feed you fat stoopid boddie to doggies.");
@@ -7,6 +19,14 @@ sub EVENT_SAY {
   }
   if($text=~/bedder shaman/i) {
     quest::say("Yoo want to be bedda shaman? Them lizards in de Feerrott tink de bedda den us and bodder us wit der majiks like fleas on a dog. Kill dem and bring me, um, one of dem bags dey wear around dem necks and, um, three of dem dolls dey hold.");
+  }
+  if ($text=~/trades/i) {
+    quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+    quest::summonitem(51121); # Tradeskill Basics Volume 1
+  }
+  if ($text=~/second book/i) {
+    quest::say("Here is the second volume of the book you requested, may it serve you well!");
+    quest::summonitem(51122); # Tradeskill Basics Volume 2
   }
 }
 
@@ -24,11 +44,11 @@ sub EVENT_ITEM {
     quest::exp(100);
   }
   elsif (plugin::check_handin(\%itemcount, 18788 => 1)) { #Tattered Note
-    quest::say("Take, take.. You now Shaman of War.. Zulort make you majik, too.  When you [gonna help] you tell Zulort.");
+    quest::say("Take, take.. You now Shaman of War.. Zulort make you majik, too.  When you [gonna help] you tell Zulort. You return to me and when you stronger, I teach you many things I know, including how to start working in the various [trades].");
     quest::summonitem(13526); #Dirty Patched Fur Tunic*
-	quest::ding();
-	quest::faction(295, 5); # Shaman of War
-	quest::exp(100);
+		quest::ding();
+		quest::faction(295, 5); # Shaman of War
+		quest::exp(100);
   }
   else {
     quest::say("Me not need dis."); #text made up

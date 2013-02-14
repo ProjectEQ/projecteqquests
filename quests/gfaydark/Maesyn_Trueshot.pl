5fb9d91d8cdcf56eb7c28f12907b84de951909d9
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-14 22:16:57 +0000

    Update the Kelethin newbie GM scripts and added missing texts.

--- quests/gfaydark/Maesyn_Trueshot.pl
+++ quests/gfaydark/Maesyn_Trueshot.pl
@@ -1,28 +1,45 @@
-#BeginFile: gfaydark\Maesyn_Trueshot.pl (54084)
-#Quest file for Greater Faydark - Maesyn Trueshot: Trueshot Longbow, Rain Caller and Guild Summons
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER {
+  if (($ulevel == 1) && ($class == "Ranger")) { 
+		$client->Message(15,"Maesyn Trueshot strings his bow as you orient yourself amongst the treetop city of Kelethin. 'Greetings, young recruit. Should you wish to learn the ways of the Ranger, read the note in your inventory and hand it to me when you are ready to begin your training.'");
+  }
+}
 
 sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("Welcome to Kelethin, $name! I am Maesyn Trueshot, commander of Faydark's Champions. We are the finest marksmen in all of Norrath. With our trusty [Trueshot longbows] we can miss no target regardless of the distance or the conditions.");
-  }
-  if($text=~/trueshot longbows/i) {
-    quest::say("The Trueshot Longbow was created by my famed father. Eldin Trueshot. It is quite accurate and takes a ranger's skill to wield. We use our new recruits to [gather materials] needed by my father.  We shall soon begin to release the formula to good elves so all may fletch such a bow.");
-  }
-  if(($text=~/gather materials/i) && ($class eq "Ranger")) {
-    if($faction < 4) { #Needs better than indifferent
-      quest::say("Take this pack. Go to Kaladim, find Trantor Everhot and ask for dwarven wire. Then go to Freeport to meet Jyle Windshot. Search the inns for him and ask him for treant wood. Then, collect some spiderling silk from spiderlings and finally, in Steamfont, we have the permission of the gnomes to use any micro servos we find while destroying rogue spiders. Combine them all and return the pack to me.");
-      quest::summonitem(17951); #Material Pack
-    }
-    else {
-      quest::say("Faydark's Champions cannot call you foe, but you have yet to earn our trust.");
-    }
-  }
-  if($text=~/correct components/i) {
-    quest::say("Now that I have crafted the Treant Bow Staff, you shall need one Planing Tool, one Treant Bow Staff, one Micro Servo and one spool of Dwarven Wire. These items will be used with your Fletching Kit as all other bows. Be forewarned, only a Master Fletcher can create such a bow and even a master fails from time to time. Good Luck.");
-  }
-  if($text=~/next incarnation/i) {
-    quest::say("The Trueshot Longbow was once enchanted by the Koada'Dal enchanters.  Once it was enchanted now it is no more.  I am sure if you were ask the Koada'Dal [where the enchanted bows] are you will get an answer.");
-  }
+	if($text=~/hail/i) {
+		quest::say("Welcome to Kelethin, $name! I am Maesyn Trueshot, commander of Faydark's Champions. We are the finest marksmen in all of Norrath. With our trusty [Trueshot longbows] we can miss no target regardless of the distance or the conditions.");
+	}
+	if($text=~/trueshot longbows/i) {
+		quest::say("The Trueshot Longbow was created by my famed father. Eldin Trueshot. It is quite accurate and takes a ranger's skill to wield. We use our new recruits to [gather materials] needed by my father.  We shall soon begin to release the formula to good elves so all may fletch such a bow.");
+	}
+	if(($text=~/gather materials/i) && ($class eq "Ranger")) {
+		if($faction < 4) { #Needs better than indifferent
+			quest::say("Take this pack. Go to Kaladim, find Trantor Everhot and ask for dwarven wire. Then go to Freeport to meet Jyle Windshot. Search the inns for him and ask him for treant wood. Then, collect some spiderling silk from spiderlings and finally, in Steamfont, we have the permission of the gnomes to use any micro servos we find while destroying rogue spiders. Combine them all and return the pack to me.");
+			quest::summonitem(17951); #Material Pack
+		}
+		else {
+			quest::say("Faydark's Champions cannot call you foe, but you have yet to earn our trust.");
+		}
+	}
+	if($text=~/correct components/i) {
+		quest::say("Now that I have crafted the Treant Bow Staff, you shall need one Planing Tool, one Treant Bow Staff, one Micro Servo and one spool of Dwarven Wire. These items will be used with your Fletching Kit as all other bows. Be forewarned, only a Master Fletcher can create such a bow and even a master fails from time to time. Good Luck.");
+	}
+	if($text=~/next incarnation/i) {
+		quest::say("The Trueshot Longbow was once enchanted by the Koada'Dal enchanters.  Once it was enchanted now it is no more.  I am sure if you were ask the Koada'Dal [where the enchanted bows] are you will get an answer.");
+	}
+	if($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		quest::summonitem(51121);
+	}
+	if($text=~/second book/i)	{
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		quest::summonitem(51122);
+	}
 }
 
 sub EVENT_ITEM {
@@ -38,12 +55,13 @@ sub EVENT_ITEM {
   }
   elsif (plugin::check_handin(\%itemcount, 18785 => 1)) { #A tattered note
     quest::say("Hail, $name, and welcome.. I am Maesyn Trueshot, leader of Faydark's Champions. I will teach and train you, as I have done for many others. Let's get started.. Here, put this on.. it'll help protect you from the elements. Once you are ready to begin your training please make sure that you see Samatansyn Flamecaller, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		quest::ding();
     quest::faction(99,10);  # Faydark's Champions
     quest::faction(178,10); # King Tearis Thex
     quest::faction(43,10);  # Clerics of Tunare
     quest::faction(304,10); # Soldiers of Tunare
     quest::faction(63,-30); # Crushbone Orcs
-    quest::exp(24); # 3/5 blue bubble at level 1
+    quest::exp(100);
     quest::summonitem(13536); # Dirty Green Tunic* 
   }
   else {
@@ -52,4 +70,5 @@ sub EVENT_ITEM {
     plugin::return_items(\%itemcount);
   }
 }
+
 #END of FILE Zone:gfaydark  ID:54084 -- Maesyn_Trueshot
--- quests/gfaydark/Tylfon.pl
+++ quests/gfaydark/Tylfon.pl
@@ -1,31 +1,59 @@
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER {
+  if (($ulevel == 1) && ($class == "Rogue")) { 
+		$client->Message(15,"Before you cn fully get your bearings, a sly looking Wood Elf named Tylfon whispers in your direction. 'Welcome. I knew you would come. If you are still interested in joining the rogues, read the note in your inventory and hand it to me when you wish to begin your training.'");
+  }
+}
+
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("So. you think you have what it takes to be a Scout of Tunare? Come back with 2 [gold] and 2 [rusty dagger]s and I'll make it worth your while."); }
+	if($text=~/hail/i) {
+		quest::say("So. you think you have what it takes to be a Scout of Tunare? Come back with two gold and two rusty daggers and I'll make it worth your while.");
+	}
+	if($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		quest::summonitem(51121);
+	}
+	if($text=~/second book/i)	{
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		quest::summonitem(51122);
+	}
 }
 
-sub EVENT_ITEM { 
-  if (plugin::check_handin(\%itemcount, 54019 => 1)) {
-    quest::emote("scans the note and sizes you up with a look.");
-    quest::say("Very well then, I thank you for assisting us. You have shown great bravery in aiding in this mission. Keep this as a symbol of our friendship.");
-    quest::summonitem(54022);
-    quest::exp(100);  
-  }
-  if(($gold >= 2) && (plugin::check_handin(\%itemcount, 7007 => 1))){
-	quest::say("Well, well. I didn't think you could do it. Here's your cut and don't be surprised that it's not much because it's your first lesson. Remember. the smaller the operation. the bigger the share, and the richest rogues have the tightest lips.");
-	quest::summonitem("7021");
-	quest::givecash("0","5","0","0");
-        quest::faction(283,5);
-        quest::exp(100);
-  }
-  if (plugin::check_handin(\%itemcount,  13322=> 1)) {
-  quest::say("Excellent job, $name, we will turn you into a rogue of Tunare yet.  Here this is for your trouble.");
-  quest::summonitem(3315);
-    
-  }
-  #do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Rogue');
-  plugin::return_items(\%itemcount);
+sub EVENT_ITEM {
+	if(plugin::check_handin(\%itemcount, 18784 => 1)){ #Tattered Note
+		quest::say("Hmm.. I hope you can prove yourself a lot more valuable than you look. Here, throw this on.. it'll help protect you a little. Once you have become a bit more experienced, return to me, I have many things to teach you about advanced combat and some of the various [trades] you will have available to you.");
+		quest::summonitem(13535); #Old Worn Gray Tunic*
+		quest::ding();
+		quest::faction(283,10); #Tunare's Scouts
+		quest::exp(100);
+	}
+	elsif(plugin::check_handin(\%itemcount, 54019 => 1)) {
+		quest::emote("scans the note and sizes you up with a look.");
+		quest::say("Very well then, I thank you for assisting us. You have shown great bravery in aiding in this mission. Keep this as a symbol of our friendship.");
+		quest::summonitem(54022);
+		quest::exp(100);  
+	}
+	elsif(($gold >= 2) && (plugin::check_handin(\%itemcount, 7007 => 1))){
+		quest::say("Well, well. I didn't think you could do it. Here's your cut and don't be surprised that it's not much because it's your first lesson. Remember. the smaller the operation. the bigger the share, and the richest rogues have the tightest lips.");
+		quest::summonitem("7021");
+		quest::givecash("0","5","0","0");
+		quest::faction(283,5);
+		quest::exp(100);
+	}
+	elsif(plugin::check_handin(\%itemcount,  13322=> 1)) {
+		quest::say("Excellent job, $name, we will turn you into a rogue of Tunare yet.  Here this is for your trouble.");
+		quest::summonitem(3315);
+	}
+	else {
+		#do all other handins first with plugin, then let it do disciplines
+		plugin::try_tome_handins(\%itemcount, $class, 'Rogue');
+		plugin::return_items(\%itemcount);
+	}
 }
 
-#END of FILE Zone:gfaydark  ID:54087 -- Tylfon 
-
+#END of FILE Zone:gfaydark  ID:54087 -- Tylfon

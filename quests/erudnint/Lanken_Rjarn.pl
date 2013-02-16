7cfcc46841c601ae7a267a064c6e2ebd3d39cb0a
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-16 03:49:16 +0000

    fix one script to make it more live like.

--- quests/erudnint/Lanken_Rjarn.pl
+++ quests/erudnint/Lanken_Rjarn.pl
@@ -3,7 +3,7 @@
 sub EVENT_SPAWN {
   $x = $npc->GetX();
   $y = $npc->GetY();
-  quest::set_proximity($x - 20, $x + 20, $y - 20, $y + 20);
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
 
 sub EVENT_ENTER {
@@ -13,29 +13,29 @@ sub EVENT_ENTER {
 }
 
 sub EVENT_SAY {
-  if($text=~/hail/i){
-    quest::emote("looks up irritably. 'I do not have time to speak with thee. I have a problem on my hands. Feel free to speak with any of my trainers.'");
-  }
-  if($text=~/problem/i){
-    quest::say("My problems are of no concern of yours. but if you must know it deals with Nolusia's brother. I can tell you no more. Leave me be.");
-  }
+	if($text=~/hail/i){
+		quest::emote("looks up irritably. 'I do not have time to speak with thee. I have a problem on my hands. Feel free to speak with any of my trainers.'");
+	}
+	if($text=~/problem/i){
+		quest::say("My problems are of no concern of yours. but if you must know it deals with Nolusia's brother. I can tell you no more. Leave me be.");
+	}
 }
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 18729 => 1)){
-    quest::say("Welcome to the Craft Keepers! You have much to learn. and I'm sure you are anxious to get started. Here's your training robe. Go see Nolusia. she'll give you your first lesson.");
-    quest::ding();
-    quest::exp(100);
-    quest::summonitem(13549);
-    quest::faction(56,10);   #Craftkeepers
-    quest::faction(145,10);  #High Council of Erudin
-    quest::faction(143,-15); #Heretics
-    quest::faction(147,10);  #High Guard of Erudin
-  }
-  else {
-    quest::say("I do not need this.");
-    plugin::return_items(\%itemcount);
-  }
+	if(plugin::check_handin(\%itemcount, 18729 => 1)){
+		quest::say("Welcome to the Craft Keepers! You have much to learn. and I'm sure you are anxious to get started. Here's your training robe. Go see Nolusia. she'll give you your first lesson.");
+		quest::ding();
+		quest::summonitem(13549);
+		quest::faction(56,10);   #Craftkeepers
+		quest::faction(145,10);  #High Council of Erudin
+		quest::faction(143,-15); #Heretics
+		quest::faction(147,10);  #High Guard of Erudin
+		quest::exp(100);
+	}
+	else {
+		quest::say("I do not need this.");
+		plugin::return_items(\%itemcount);
+	}
 }
+
 #END of FILE Zone:erudnint  ID:24032 -- Lanken_Rjarn
-#Quest by Solid11

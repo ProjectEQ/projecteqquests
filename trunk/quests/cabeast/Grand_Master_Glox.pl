eb2727a1167501982ab57dba9b73d0fa64274cd9
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-18 05:02:07 +0000

    Update Cabilis newbie GM scripts.

--- trunk/quests/cabeast/Grand_Master_Glox.pl
+++ trunk/quests/cabeast/Grand_Master_Glox.pl
@@ -1,37 +1,51 @@
-
-sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote('shows no reaction to your greeting.');
-  }
- elsif($text=~/trial of agility/i){
-  quest::say('I knew you were not the whiff others claimed you to be, are you sure you are [ready] to be tested in agility?');
-  }
- elsif($text=~/ready/i){
-  quest::say('We shall spar then, I hope you are as prepared as you think you are.');
-  quest::movepc(106,-190.9,1089.8,-25.2);
-  }
- }
-
-sub EVENT_ITEM {
- if(plugin::check_handin(\%itemcount, 18204 => 1)){
-  quest::emote('breaks his meditation and quickly grabs your forearms. You feel the raw power in his heavily callused hands. Out of nowhere, his tail sweeps forward and places a shackle upon your wrist. He then points south towards Master Bain and returns to his meditation.');
-  quest::summonitem('4190');
- }
-  if (plugin::check_handin(\%itemcount, 14788 =>1 )) {
-    quest::emote("sits whispering incoherently for a long moment before even regarding the note you dropped in front of him. The Grand Master suddenly snatches up the note from Xlixinar, removes a small charcoal marker from a belt pouch, and scrawls some intructions upon it. He drops the note immediately after finishing and resumes his meditation without a word.");
-    quest::summonitem(18980);
-  }
-  if (plugin::check_handin(\%itemcount, 14782 =>1, 10032 =>1 )) {
-   quest::emote("nods slightly");
-    quest::summonitem(14783);
-    quest::ding();
-    quest::exp(10000);
-  }
-  plugin::try_tome_handins(\%itemcount, $class, 'Monk');
-  plugin::return_items(\%itemcount);
-
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
 
- #Submitted by: Senzo aka Fatty Beerbelly
- #Item_IDs : taken from : http://eqitems.13th-floor.org/
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 18204)) { 
+		$client->Message(15,"As you enter the Court of Pain, Grand Master Glox, sits in mediation. Without even looking up, he addresses you. 'The time has come for discipline. Read the note in your inventory and then hand it to me to begin your training.'");
+  }
+}
+
+sub EVENT_SAY {
+	if($text=~/hail/i){
+		quest::emote('shows no reaction to your greeting.');
+	}
+	if($text=~/trial of agility/i){
+		quest::say('I knew you were not the whiff others claimed you to be, are you sure you are [ready] to be tested in agility?');
+	}
+	if($text=~/ready/i){
+		quest::say('We shall spar then, I hope you are as prepared as you think you are.');
+		quest::movepc(106,-190.9,1089.8,-25.2);
+	}
+}
+
+sub EVENT_ITEM {
+	if(plugin::check_handin(\%itemcount, 18204 => 1)){ # Guild Summons
+		quest::emote('breaks his meditation and quickly grabs your forearms. You feel the raw power in his heavily callused hands. Out of nowhere, his tail sweeps forward and places a shackle upon your wrist. He then points south towards Master Bain and returns to his meditation.');
+		quest::summonitem(4190); # Shackle of Dust
+		quest::ding();
+		quest::faction(317,10); #Swift Tails
+		quest::faction(193,10); #Legion of Cabilis
+		quest::exp(100)
+	}
+	elsif (plugin::check_handin(\%itemcount, 14788 =>1 )) {
+		quest::emote("sits whispering incoherently for a long moment before even regarding the note you dropped in front of him. The Grand Master suddenly snatches up the note from Xlixinar, removes a small charcoal marker from a belt pouch, and scrawls some intructions upon it. He drops the note immediately after finishing and resumes his meditation without a word.");
+		quest::summonitem(18980);
+	}
+	elsif (plugin::check_handin(\%itemcount, 14782 =>1, 10032 =>1 )) {
+		quest::emote("nods slightly");
+		quest::summonitem(14783);
+		quest::ding();
+		quest::exp(10000);
+	}
+	else {
+		plugin::try_tome_handins(\%itemcount, $class, 'Monk');
+		plugin::return_items(\%itemcount);
+	}
+}
+
  #END of FILE : Zone : cabeast : NPC_ID : 2023 : Grand_Master_Glox
--- trunk/quests/cabeast/Prime_Hierophant_Vek.pl
+++ trunk/quests/cabeast/Prime_Hierophant_Vek.pl
@@ -1,55 +1,69 @@
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 18206)) { 
+		$client->Message(15,"As your reptilian eyes adjust to the darkness of the room, an imposing Iksar turns towards you, addressing you with a sharp hiss. 'I am Prime Hierophant Vek. The time has come young one. You have chosen the path of the Shadowknight. Open your inventory and read the note within. Once you are ready to begin your training, hand the note to me and we will continue.'");
+  }
+}
+
 sub EVENT_SAY { 
- if($text=~/hail/i){
-  quest::say("Praise to the ancients, my friend! There is much suffering to be given unto the world. I am the Prime Hierophant of the Temple of Terror.  Through me and my fellow priests flows the wisdom of our ancestors.");
- }
- elsif($text=~/trades/i){
-  quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
-  quest::summonitem(51121);
- }
- elsif($text=~/second book/i){
-  quest::say("Here is the second volume of the book you requested, may it serve you well!");
-  quest::summonitem(51122);
- }
+	if($text=~/hail/i){
+		quest::say("Praise to the ancients, my friend! There is much suffering to be given unto the world. I am the Prime Hierophant of the Temple of Terror.  Through me and my fellow priests flows the wisdom of our ancestors.");
+	}
+	if($text=~/trades/i){
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		quest::summonitem(51121);
+	}
+	if($text=~/second book/i){
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		quest::summonitem(51122);
+	}
 }
 
 sub EVENT_ITEM {
- if(plugin::check_handin(\%itemcount, 18206 => 1)){
-  quest::say("Welcome, young petitioner. You have been summoned to this temple to join with the Scaled Mystics. Ours is the way of pain and suffering. Here is the weapon of a petitioner. Through you the power of our ancestors shall flow. As your wisdom grows, so shall your power. Someday you may be a vessel for the supreme might of the ancients. Seek knowldege with Oxyn, he is here to help you. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
-  quest::summonitem(5140);
-  quest::exp(100);
-  quest::ding();
-  quest::faction(282,20);
-  quest::faction(193,10);
- }
- elsif(plugin::check_handin(\%itemcount, 18462 => 1, 22917 => 1)){
-  quest::emote("smiles at your dedication to Cazic Thule and hands you a small gem.");
-  quest::summonitem(7881);
-  quest::exp(20000);
-  quest::ding();
-  quest::faction(317,20);
-  quest::faction(193,10);
- }
- elsif(plugin::check_handin(\%itemcount, 18359 => 1, 22916 => 1)){
-  quest::emote("smiles at your dedication to Cazic Thule and hands you a small gem.");
-  quest::summonitem(7881);
-  quest::exp(20000);
-  quest::ding();
-  quest::faction(317,20);
-  quest::faction(193,10);
- }
- if (plugin::check_handin(\%itemcount, 14790 =>1 )) {
-   quest::emote("stares down at the note, confused, until he recognizes the writing and gasps in shock!");
-   quest::say("You spoke to the Brothers? You, a simple Legionnaire? Well, no matter, you are here to test your virtue of sacrifice and we will take pleasure in it as we have for generations even before the Fallen Brothers. One of our greatest allies in pain is disease. In the woods outside our fair city is a small tribe of goblins. There is a certain goblin famed for his [unique weapon]. Take this knife and use it to cut this from the goblin. Then return his weapon, the knife, a sapphire, and an opal to me.");
-   quest::summonitem(14784);
-    }
-    if (plugin::check_handin(\%itemcount, 14784 =>1, 14785 =>1, 10034 =>1, 10030 =>1 )) {
-    quest::say("Well done");
-    quest::ding();
-    quest::summonitem(14786);
-    quest::exp(10000);
-      
-    }
+	if(plugin::check_handin(\%itemcount, 18206 => 1)){ #guild summon
+		quest::say("Welcome, young petitioner. You have been summoned to this temple to join with the Scaled Mystics. Ours is the way of pain and suffering. Here is the weapon of a petitioner. Through you the power of our ancestors shall flow. As your wisdom grows, so shall your power. Someday you may be a vessel for the supreme might of the ancients. Seek knowldege with Oxyn, he is here to help you. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		quest::summonitem(5140); #Iron Cudgel of the Petitioner
+		quest::ding();
+		quest::faction(282,20); #Scaled Mystics
+		quest::faction(193,10); #Legion of Cabilis
+		quest::exp(100);
+	}
+	elsif(plugin::check_handin(\%itemcount, 18462 => 1, 22917 => 1)){
+		quest::emote("smiles at your dedication to Cazic Thule and hands you a small gem.");
+		quest::summonitem(7881);
+		quest::exp(20000);
+		quest::ding();
+		quest::faction(317,20);
+		quest::faction(193,10);
+	}
+	elsif(plugin::check_handin(\%itemcount, 18359 => 1, 22916 => 1)){
+		quest::emote("smiles at your dedication to Cazic Thule and hands you a small gem.");
+		quest::summonitem(7881);
+		quest::exp(20000);
+		quest::ding();
+		quest::faction(317,20);
+		quest::faction(193,10);
+	}
+	elsif (plugin::check_handin(\%itemcount, 14790 =>1 )) {
+		quest::emote("stares down at the note, confused, until he recognizes the writing and gasps in shock!");
+		quest::say("You spoke to the Brothers? You, a simple Legionnaire? Well, no matter, you are here to test your virtue of sacrifice and we will take pleasure in it as we have for generations even before the Fallen Brothers. One of our greatest allies in pain is disease. In the woods outside our fair city is a small tribe of goblins. There is a certain goblin famed for his [unique weapon]. Take this knife and use it to cut this from the goblin. Then return his weapon, the knife, a sapphire, and an opal to me.");
+		quest::summonitem(14784);
+	}
+	elsif (plugin::check_handin(\%itemcount, 14784 =>1, 14785 =>1, 10034 =>1, 10030 =>1 )) {
+		quest::say("Well done");
+		quest::ding();
+		quest::summonitem(14786);
+		quest::exp(10000);
+	}
+	else {
+		quest::say("I have no need for this $name, you can have it back");
     plugin::return_items(\%itemcount);
+	}
 }
 
 #Submitted by Jim Mills, with thanks to BWStripes

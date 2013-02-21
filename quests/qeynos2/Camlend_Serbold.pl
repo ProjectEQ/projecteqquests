fdbc3efce2803c88217479faefc87b43f10da9bc
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-21 22:45:38 +0000

    Update the rest of Qeynos newbie GM scripts.

--- quests/qeynos2/Camlend_Serbold.pl
+++ quests/qeynos2/Camlend_Serbold.pl
@@ -1,61 +1,78 @@
-sub EVENT_SAY
-{
-  if($faction <= 3)
-  {
-    if($text=~/What about Lord Grimrot/i)
-    {
-      quest::say("You have heard of Lord Grimrot? He is a shadowknight working with the Bloodsabers. We have word he is trying to build an army of undead. Only a powerful knight can rid the lands of Grimrot. So, did you come here to [defend life]?");
-    }
-    if($text=~/defend life/i)
-    {
-      quest::say("Then go to the Plains of Karana. Somewhere there, Lord Grimrot is hatching his little plan for the Bloodsabers. Bring me his evil scythe and his heart. It is said that upon his death, his soul will be absorbed into this weapon of darkness. Do this and be rewarded");
-    }
-  }
-  if($text=~/Hail/i)
-  {
-    quest::say("I say, good to meet you, $name !  I am Camlend Serbold.  Some call me Serbold the Great.  I am the leader of the righteous order of paladins which serves the Temple of Life.  Together with the clerics of this temple, we defend the will of the great [Rodcet Nife] and smite all who dare to oppose us.");
-  }  
-  if($text=~/Rodcet Nife/i)
-  {
-    quest::say("Do you feel your heart beating within your chest?  It is Rodcet Nife, the Prime Healer, whose breath keeps it in perfect rhythm.  If you do not pray to His glory then I assure you, someday when your heart begins to slow, you shall be crying out loud to Rodcet Nife.");
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
+
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 18711)) { 
+		$client->Message(15,"As you get your bearings, a healing energy seems to sweep over you and an almost etheral voice seems to echo in your mind. 'Welcome young paladin of Rodcet. I am Guild Master Camlend Serbold. Read the note in your inventory and hand it to me to begin your training.'");
   }
 }
 
-sub EVENT_ITEM
-{
-  if($faction <= 3)  #Require kindly or greater faction
-  {
-    # Pestilence Scythe ID: 13324
-    # Decaying Heart ID: 13325
-    if(plugin::check_handin(\%itemcount, 13324 => 1, 13325 => 1))
-    {
-      quest::ding();
-      # Aegis of Life ID: 12238
-      quest::summonitem(12238);
-      quest::say("Thank you for your service to the Knights of Thunder. Here is your reward.");
-    }
-    else
-    {
-        quest::say("I do not know what these are for.");
-    }
-  }
-  else
-  {
-    # If items are handed in without proper faction
-    if($itemcount{13324} == 1 && $itemcount{13325} == 1)
-    {
-      quest::say("You have not yet proven yourself.");
-    }
-  }
-  if(plugin::check_handin(\%itemcount, 18710 => 1))
-  {
-    quest::say("Welcome to the Temple of Life. You have much to learn and we have much work to do. Pain, hurt, disease and death lurk everywhere, our job is seemingly endless, but none is more gratifying. Here is our guild tunic, represent us well. Suuspa Clanim shall help you to get started. Good health be with you.");
-    quest::summonitem("13506");
-    quest::exp("100");
-  }
-  # Do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Paladin');
-  plugin::return_items(\%itemcount);
+sub EVENT_SAY {
+	if($faction <= 3) {
+		if($text=~/lord grimrot/i) {
+			quest::say("You have heard of Lord Grimrot? He is a shadowknight working with the Bloodsabers. We have word he is trying to build an army of undead. Only a powerful knight can rid the lands of Grimrot. So, did you come here to [defend life]?");
+		}
+		if($text=~/defend life/i) {
+			quest::say("Then go to the Plains of Karana. Somewhere there, Lord Grimrot is hatching his little plan for the Bloodsabers. Bring me his evil scythe and his heart. It is said that upon his death, his soul will be absorbed into this weapon of darkness. Do this and be rewarded");
+		}
+	}
+	if($text=~/hail/i) {
+		quest::say("I say, good to meet you, $name !  I am Camlend Serbold.  Some call me Serbold the Great.  I am the leader of the righteous order of paladins which serves the Temple of Life.  Together with the clerics of this temple, we defend the will of the great [Rodcet Nife] and smite all who dare to oppose us.");
+	}  
+	if($text=~/rodcet nife/i) {
+		quest::say("Do you feel your heart beating within your chest?  It is Rodcet Nife, the Prime Healer, whose breath keeps it in perfect rhythm.  If you do not pray to His glory then I assure you, someday when your heart begins to slow, you shall be crying out loud to Rodcet Nife.");
+	}
+	if($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		quest::summonitem(51121);
+	}
+	if($text=~/second book/i)	{
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		quest::summonitem(51122);
+	}
 }
+
+sub EVENT_ITEM {
+	if(plugin::check_handin(\%itemcount, 18711 => 1)) { #Tattered Note
+		quest::say("Welcome to the Temple of Life. You have much to learn and we have much work to do. Pain, hurt, disease and death lurk everywhere, our job is seemingly endless, but none is more gratifying. Here is our guild tunic, represent us well. Davloran Girionlis will help you to get started. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available for you.");
+		quest::summonitem(13506); #Faded Tunic*
+		quest::ding();
+		quest::faction(257,10); #Priest of Life
+		quest::faction(183,10); #Knight of Thunder
+		quest::faction(135,10); #Guards of Qeynos
+		quest::faction(21,-15); #Bloodsabers
+		quest::faction(9,10); #Antonius Bayle
+		quest::exp(100);
+	}
+	elsif($faction <= 3) {  #Require kindly or greater faction 
+		if(plugin::check_handin(\%itemcount, 13324 => 1, 13325 => 1)) { # Pestilence Scythe ID: 13324 # Decaying Heart ID: 13325
+			quest::ding();
+			quest::summonitem(12238); # Aegis of Life ID: 12238
+			quest::say("We thank you for your service. With Lord Grimrot's evil soul trapped in this scythe, all but the truly evil shadowknights would be able to wield it. I would like to present you with this as a token of our appreciation. Your devotion to life is supreme. Go now, and serve life.");
+			quest::faction(257,30); #Priest of Life
+			quest::faction(183,30); #Knight of Thunder
+			quest::faction(135,30); #Guards of Qeynos
+			quest::faction(21,-55); #Bloodsabers
+			quest::faction(9,30); #Antonius Bayle
+			quest::exp(1000);
+			quest::givecash(0,0,0,9);
+		}
+		else {
+			quest::say("I do not know what these are for.");
+		}
+	}
+	elsif($itemcount{13324} == 1 && $itemcount{13325} == 1){
+		quest::say("You have not yet proven yourself.");
+	}
+	else {
+		# Do all other handins first with plugin, then let it do disciplines
+		plugin::try_tome_handins(\%itemcount, $class, 'Paladin');
+	}
+	plugin::return_items(\%itemcount);
+}
+
 #END of FILE Zone:qeynos2 ID:2092 -- Camlend_Serbold
 
--- quests/qeynos2/Hanns_Krieghor.pl
+++ quests/qeynos2/Hanns_Krieghor.pl
@@ -1,48 +1,66 @@
-#Hanns accepts Stanos' head for yet another alternate ending to the rogue epic quest (the other being to give Stanos' head to General V`ghera and receive the high elf illusion mask).
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
 
-sub EVENT_SIGNAL {
-  if($signal == 1) {
-    quest::say("Yes, I just got some new information from Prak this evening. He says they found out the spy is working for some bandit in the Karanas named Rujahn Tahslek. You used to work out there. do you recognize the name?");
-    quest::signalwith(2085,2,1);
-	}
-  if($signal == 2) {
-quest::say("Hmmff.. Anyway, send someone to Prak. I want this job done quickly. Let's show these pathetic bandits that they've been nosing around in the wrong places.");
-    quest::signalwith(2085,3, 1);
-	}
-  if($signal == 3) {
-    quest::say("Lomarc is already back. He sent word that the guards are on to him, so he's laying low for a bit. He also said the cost for delivery just went up, because he had to pay off the owner of the Mermaid's Lure.");
-    quest::signalwith(2033,1,1);
-	}
-  if($signal == 4) {
-	quest::say("Finally, the rat comes out of his hole! Send some men, no no, go yourself, and track him down. Then kill him. I don't care who sees you, or who gets in your way. Finish the job. And bring me whatever you find on his carcass. I'm sure he took some valuables from us when he escaped. And don't worry about Malka, she will keep until you finish Stanos, then she is yours. Shame to waste such talent, but you don't steal from the Circle and live.");
-    quest::signalwith(2033,2, 1);
-	}	
-  }
-
-
-
-
-sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("My name is Hanns..  Do as I say. and we shall get along just fine.");
-  }
-  if($text=~/carson has a mole in the highpass/i) {
-    quest::say("Arrgg, that Carson can't control anything. Sometimes he's practically useless. Go tell Zannsin that [I want him to send some of his men to Prak in Highpass], to help Carson get back on track.");
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 18708)) { 
+		$client->Message(15,"As your eyes adjust to the dark interior of the room, a sly looking man turns towards you. 'Shh! Not too loudly. I am Hanns if you are here to start your training, read the note in your inventory and hand it to me.'");
   }
 }
 
+sub EVENT_SIGNAL {
+	if($signal == 1) {
+		quest::say("Yes, I just got some new information from Prak this evening. He says they found out the spy is working for some bandit in the Karanas named Rujahn Tahslek. You used to work out there. do you recognize the name?");
+		quest::signalwith(2085,2,1);
+	}
+	if($signal == 2) {
+		quest::say("Hmmff.. Anyway, send someone to Prak. I want this job done quickly. Let's show these pathetic bandits that they've been nosing around in the wrong places.");
+		quest::signalwith(2085,3, 1);
+	}
+	if($signal == 3) {
+		quest::say("Lomarc is already back. He sent word that the guards are on to him, so he's laying low for a bit. He also said the cost for delivery just went up, because he had to pay off the owner of the Mermaid's Lure.");
+		quest::signalwith(2033,1,1);
+	}
+	if($signal == 4) {
+		quest::say("Finally, the rat comes out of his hole! Send some men, no no, go yourself, and track him down. Then kill him. I don't care who sees you, or who gets in your way. Finish the job. And bring me whatever you find on his carcass. I'm sure he took some valuables from us when he escaped. And don't worry about Malka, she will keep until you finish Stanos, then she is yours. Shame to waste such talent, but you don't steal from the Circle and live.");
+		quest::signalwith(2033,2, 1);
+	}
+}
+
+sub EVENT_SAY {
+	if($text=~/hail/i) {
+		quest::say("My name is Hanns..  Do as I say. and we shall get along just fine.");
+	}
+	if($text=~/carson has a mole in the highpass/i) {
+		quest::say("Arrgg, that Carson can't control anything. Sometimes he's practically useless. Go tell Zannsin that [I want him to send some of his men to Prak in Highpass], to help Carson get back on track.");
+	}
+}
+
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 28058 => 1)) {
-    quest::say("Well done, $name."); #Text made up
-    quest::summonitem(7501);
-    quest::exp(500000);
-    quest::ding();
-    quest::faction(33,20);
-  }
-  else{
-    plugin::try_tome_handins(\%itemcount, $class, 'Rogue');
-    plugin::return_items(\%itemcount);
-  }
+	if(plugin::check_handin(\%itemcount, 18708 => 1)) { #Napkin From Crow's
+		quest::say("Lucky thing you weren't followed. or you'd be breathing through holes in your back right now. Go find Knargon, maybe you can help run the next [shipment]. Remember, we keep our business quiet, so watch yourself, sewer crawler.");
+		quest::summonitem(13501); #Second Hand Tunic*
+		quest::ding();
+		quest::faction(33,10); #Circle of Unseen Hands
+		quest::faction(217,-15); #Merchants of Qeynos
+		quest::faction(53,10); #Corrupt Qeynos Guards
+		quest::faction(135,-15); #Guards of Qeynos
+		quest::faction(164,10); #Kane Bayle
+		quest::exp(100);
+	}
+	elsif(plugin::check_handin(\%itemcount, 28058 => 1)) {
+		quest::say("Well done, $name."); #Text made up
+		quest::summonitem(7501);
+		quest::faction(33,20);
+		quest::exp(500000);
+		quest::ding();
+	}
+	else{
+		plugin::try_tome_handins(\%itemcount, $class, 'Rogue');
+	}
+	plugin::return_items(\%itemcount);
 }
 
 #Submitted by Jim Mills

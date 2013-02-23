6e619af3e37cabbe52de7833ffe654c2d4477d3c
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-02-23 00:44:09 +0000

    Update Freeport newbie GM scripts.

--- quests/freeporteast/Pietro_Zarn.pl
+++ quests/freeporteast/Pietro_Zarn.pl
@@ -1,96 +1,42 @@
-############################################
-# ZONE: East Freeport (freporte)
-# DATABASE: PEQ-Velious
-# LAST EDIT DATE: May 25,2005
-# VERSION: 1.0
-# BASE QUEST BY: PEQ Team
-# DEVELOPER: MWMDRAGON
-#
-# *** NPC INFORMATION ***
-#
-# NAME: Pietro_Zarn
-# ID: 10102
-# TYPE: Shadowknight Guild Master
-# RACE: Human
-# LEVEL: 61
-#
-# *** ITEMS GIVEN OR TAKEN ***
-#
-# A tattered note ID- 18743
-# Faded Crimson Tunic ID- 13561
-# Translated Parchment ID- 18961
-# Rage War Maul ID-12153
-#
-# *** QUESTS INVOLVED IN ***
-#
-#1 - Shadowknight Newbie Note
-#2 - The Bayle List
-#
-# *** QUESTS AVAILABLE TO ***
-#
-#1 - Shadowknight
-#2 - Shadowknight
-#
-# *** NPC NOTES ***
-#
-#
-#
-############################################
-
-######## EVENT_SAY AREA ####################
-### Called when the NPC is spoken to by a PC
-
-sub EVENT_SAY
-{
-   if($text=~/Hail/i)
-   {
-   quest::say("Who are you that you dare to speak with the great Pietro Zarn!! I see nothing more than a whelp in front of me. Innoruuk himself speaks with me. And now you expect me to waste words on you, a common. Begone $name . You have nothing to peak my interest, only my rage.");
-   }
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
 
-######## EVENT_ITEM AREA ###################
-### Called when the NPC is handed items
-
-sub EVENT_ITEM
-{
-
-   # A tattered note ID- 18743
-   if(plugin::check_handin(\%itemcount, 18743 => 1))
-   {
-   quest::ding();
-   quest::say("A new member to carry the rage of Innoruuk into the city and beyond. How wonderful. I must admit that you do not appear to carry the rage within. Hopefully you shall color the battlefields with the blood of many knights from the Hall of Truth. Here. Wear this tunic with pride. Ha ha ha!! We shall see young one. We shall see.");
-   quest::exp("50");
-
-   # Faded Crimson Tunic ID- 13561
-   quest::summonitem("13561");
-
-   # Opal Dark Briar Faction ID - 235
-   quest::faction("235","1");
-   # Dismal Rage Faction ID - 86
-   quest::faction("86","1");
-   # Knights of Truth Faction ID - 184
-   quest::faction("184","-1");
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 18743)) { 
+		$client->Message(15,"You find yourself in a small, dimly lit room. A dark figure steps from the shadows and greets you. 'Welcome young apprentice. I am Pietro Zarn your new Guild Master. I am glad you found us. Read the note in your inventory and hand it to me when you wish to begin your training.'");
   }
-
-  # Translated Parchment ID- 18961
-  elsif(plugin::check_handin(\%itemcount, 18961 => 1))
-  {
-   quest::ding();
-   quest::say("I will deal with this $name. Take this and know that you are welcomed by the Dismal Rage.");
-   quest::exp("100");
-   # Rage War Maul ID-12153 
-   quest::summonitem("12153");
-
-  }
-
-  #do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Shadowknight');
-  plugin::return_items(\%itemcount);
 }
 
-### EQEmu Quest Template By: MWMDRAGON
-### The End of this script must contain 2 empty lines for the EQ Quest System
-#END of FILE Zone:freporte  ID:10102 -- Pietro_Zarn
+sub EVENT_SAY {
+	if($text=~/hail/i) {
+		quest::say("Who are you that you dare to speak with the great Pietro Zarn!! I see nothing more than a whelp in front of me. Innoruuk himself speaks with me. And now you expect me to waste words on you, a common. Begone $name . You have nothing to peak my interest, only my rage.");
+	}
+}
 
+sub EVENT_ITEM {
+	if(plugin::check_handin(\%itemcount, 18743 => 1)) { # A tattered note
+		quest::say("A new member to carry the rage of Innoruuk into the city and beyond. How wonderful. I must admit that you do not appear to carry the rage within. Hopefully you shall color the battlefields with the blood of many knights from the Hall of Truth. Here. Wear this tunic with pride. Once you are ready to begin your training please make sure that yo see Gunex Eklar, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		quest::summonitem(13561); # Faded Crimson Tunic
+		quest::ding();
+		quest::faction(86,10); # Dismal Rage
+		quest::faction(184,-15); # Knights of Truth
+		quest::faction(235,10); # Opal Dark Briar
+		quest::exp(100);
+	}
+	elsif(plugin::check_handin(\%itemcount, 18961 => 1)) { # Translated Parchment ID- 18961
+		quest::ding();
+		quest::say("I will deal with this $name. Take this and know that you are welcomed by the Dismal Rage.");
+		quest::exp("100");
+		quest::summonitem("12153"); # Rage War Maul ID-12153 
+	}
+	else {
+		#do all other handins first with plugin, then let it do disciplines
+		plugin::try_tome_handins(\%itemcount, $class, 'Shadowknight');
+	}
+	plugin::return_items(\%itemcount);
+}
 
-
+#END of FILE Zone:freporte  -- Pietro_Zarn
--- quests/freeportwest/Valeron_Dushire.pl
+++ quests/freeportwest/Valeron_Dushire.pl
@@ -1,57 +1,75 @@
-# 2nd part of paladin quest spell - divine might
-# the way the original quest works on live is that you mention the name of
-# Diggs Duggun to this npc and he responds about how the tunic and the leg bone
-# are from a lost paladin hero which makes no sense. changing events slightly to
-# have the player hand the legbone to the npc to trigger the dialogue. npc will
-# hand the bone back to the player.
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+}
 
-sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("I am Sir Valeron Dushire, leader of the order of the Knights of Truth. If your soul shines with purity and strength, I urge you to join our order. The might of Mithaniel Marr stands behind all who join. Soon that might will help us free this city from the tyranny of the [Freeport Militia].");
-  }
-  if ($text=~/freeport militia/i) {
-    quest::say("The Freeport Militia took control of this city long ago. They are nothing more than a group of thugs not worthy of respect. They follow the words of [Sir Lucan] D'Lere. He has been a thorn in this city's side for too long. If you wish to aid us in the fight against tyranny, go speak with Jemoz or Sir Theron. May the Truthbringer shine upon your soul.");
-  }
-  if ($text=~/sir lucan/i) {
-    quest::say("Lucan is a bad seed. You see, I trained Sir Lucan. He was nothing more than a street rat who was taken in by the Temple of Marr. The priests taught him and found him to be very agile and strong. They requested either the Priests of Marr or the Knights of Truth take him on as a squire. Gygus could not spare the trainers at the time and so we took the boy. The boy became a man, and a very formidable knight. Then something [went awry].");
-  }
-  if ($text=~/went awry/i) {
-    quest::say("During one of our crusades into the lands of Norrath, we left Sir Lucan in charge of the remaining knights. He did much good and the people respected him. He was overtaken by power. He soon began to hire mercenaries to guard the city, calling them the Freeport Militia. His true downfall began when he killed Sentry Dillius, a paladin of the Priests of Marr. His divine powers were stripped from his soul. He was a paladin no longer. He and his militia now control the city. They treat it as their playground and bully the populace. Someone must [stop Sir Lucan].");
-  }
-  if ($text=~/stop sir lucan/i) {
-    quest::say("That would be good, but I doubt you can. We have been trying for years and are unable to get him apart from the militia. If you do the impossible and destroy Sir Lucan, bring me his testimony. Every knight of the Hall of Truth carries a signed testimony. Sir Lucan still holds his.");
-  }
-  if ($text=~/hero/i) {
-    quest::say("His name has been long lost but his legend lives on in every young knight who aspires to continue his fight. He attempted to quell an evil that resided within Kithicor, perverting nature itself. Much to his relief, it was an earthly foe he fought against and not a denizen summoned from the planes of evil. Then, suddenly, he was surrounded by dozens of dark elves. He was captured and taken to the Dead to be experimented upon and had his soul twisted by vile necromantic magics. Then the Tier'Dal cast his remains to the winds. Where they landed, no one knew, until now. You have found one of his bones. I hereby charge you with a Holy Quest to bring this Hero's bones back for a burial with honors. Do you [accept], knight?");
-  }
-  if ($text=~/i accept/i) {
-    quest::say("Good! I knew you would. It is one of the highest honors one can receive. Your quest will not be easy for I know not where his other bones lay. A spy of ours within Neriak was able to surmise that six of his bones along with his sword and shield were scattered throughout Norrath. You must search far and wide to find the remaining pieces, as you already have one.. Do not give up. It is now your sacred duty. Once you have retrieved all eight pieces, come back to me and inform me that you [have all the bones].");
-  }
-  if ($text=~/have all the bones/i) {
-    quest::say("Excellent! You are indeed worthy to bear the title of Holy Knight! Now, about the coffin. I have spoken with Ping Fuzzlecutter, outside the western gates. Ask him about the coffin and once it is made, bring it to Simon Aldicott of Marr. I shall have Simon meet you by the lake in the Commonlands after Ping gives you the coffin.");
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 18737)) { 
+		$client->Message(15,"As you bask in the majesty of the Temple of Marr, an important looking individual, clad in shining metal armor turns to greet you. 'I can spot a new recruit a mile away. Welcome to the Temple of Marr. I am Valeron Dushire. Read the note in your inventory and hand it to me to begin your training. I wish you the best of luck.'");
   }
 }
 
+sub EVENT_SAY {
+	if($text=~/hail/i) {
+		quest::say("I am Sir Valeron Dushire, leader of the order of the Knights of Truth. If your soul shines with purity and strength, I urge you to join our order. The might of Mithaniel Marr stands behind all who join. Soon that might will help us free this city from the tyranny of the [Freeport Militia].");
+	}
+	if($text=~/freeport militia/i) {
+		quest::say("The Freeport Militia took control of this city long ago. They are nothing more than a group of thugs not worthy of respect. They follow the words of [Sir Lucan] D'Lere. He has been a thorn in this city's side for too long. If you wish to aid us in the fight against tyranny, go speak with Jemoz or Sir Theron. May the Truthbringer shine upon your soul.");
+	}
+	if($text=~/sir lucan/i) {
+		quest::say("Lucan is a bad seed. You see, I trained Sir Lucan. He was nothing more than a street rat who was taken in by the Temple of Marr. The priests taught him and found him to be very agile and strong. They requested either the Priests of Marr or the Knights of Truth take him on as a squire. Gygus could not spare the trainers at the time and so we took the boy. The boy became a man, and a very formidable knight. Then something [went awry].");
+	}
+	if($text=~/went awry/i) {
+		quest::say("During one of our crusades into the lands of Norrath, we left Sir Lucan in charge of the remaining knights. He did much good and the people respected him. He was overtaken by power. He soon began to hire mercenaries to guard the city, calling them the Freeport Militia. His true downfall began when he killed Sentry Dillius, a paladin of the Priests of Marr. His divine powers were stripped from his soul. He was a paladin no longer. He and his militia now control the city. They treat it as their playground and bully the populace. Someone must [stop Sir Lucan].");
+	}
+	if($text=~/stop sir lucan/i) {
+		quest::say("That would be good, but I doubt you can. We have been trying for years and are unable to get him apart from the militia. If you do the impossible and destroy Sir Lucan, bring me his testimony. Every knight of the Hall of Truth carries a signed testimony. Sir Lucan still holds his.");
+	}
+	if($text=~/hero/i) {
+		quest::say("His name has been long lost but his legend lives on in every young knight who aspires to continue his fight. He attempted to quell an evil that resided within Kithicor, perverting nature itself. Much to his relief, it was an earthly foe he fought against and not a denizen summoned from the planes of evil. Then, suddenly, he was surrounded by dozens of dark elves. He was captured and taken to the Dead to be experimented upon and had his soul twisted by vile necromantic magics. Then the Tier'Dal cast his remains to the winds. Where they landed, no one knew, until now. You have found one of his bones. I hereby charge you with a Holy Quest to bring this Hero's bones back for a burial with honors. Do you [accept], knight?");
+	}
+	if($text=~/i accept/i) {
+		quest::say("Good! I knew you would. It is one of the highest honors one can receive. Your quest will not be easy for I know not where his other bones lay. A spy of ours within Neriak was able to surmise that six of his bones along with his sword and shield were scattered throughout Norrath. You must search far and wide to find the remaining pieces, as you already have one.. Do not give up. It is now your sacred duty. Once you have retrieved all eight pieces, come back to me and inform me that you [have all the bones].");
+	}
+	if($text=~/have all the bones/i) {
+		quest::say("Excellent! You are indeed worthy to bear the title of Holy Knight! Now, about the coffin. I have spoken with Ping Fuzzlecutter, outside the western gates. Ask him about the coffin and once it is made, bring it to Simon Aldicott of Marr. I shall have Simon meet you by the lake in the Commonlands after Ping gives you the coffin.");
+	}
+	if($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		quest::summonitem(51121);
+	}
+	if($text=~/second book/i)	{
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		quest::summonitem(51122);
+	}
+}
+
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 18737 => 1)) { #guild note
-    quest::summonitem(13554);
-    quest::faction(258,100); #priests of marr
-    quest::faction(105,-50); #freeport militia
-    quest::faction(184,100); #knights of truth
-  }
-  elsif (plugin::check_handin(\%itemcount, 6701 => 1)) { #a dusty old leg bone
-    quest::say("According to this crest on the tunic, you have recovered one of the bones of a lost Paladin [Hero].");
-    quest::summonitem(6701);
-  }
-  elsif (plugin::check_handin(\%itemcount, 18827 => 1)) { #a testimony of truth
-    quest::say("Praise be to Marr!! You have done the impossible!! Sir Lucan is finally sent to the higher courts of the Tribunal. The city now has a chance to prosper. The Hall of Truth has been redeemed and gives you thanks. Take this, it is the Sword of Faith. May you wield it with righteousness. Beware of the remainder of the militia. They will be hunting for your head.");
-    # Brilliant Sword of Faith
-    quest::summonitem(13947);
-  }
-  #do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Paladin');
+	if(plugin::check_handin(\%itemcount, 18737 => 1)) { #Tattered Note
+		quest::say("Welcome to the Guild, here's your guild tunic. Once you are ready to begin your training please make sure that yo see Edwardian Holyblade, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		quest::summonitem(13554); #Faded Purple Tunic*
+		quest::ding();
+		quest::faction(184,10); #Knight of Truth
+		quest::faction(86,-15); #Dimal Rage
+		quest::faction(105,-15); #The Freeport Militia
+		quest::faction(258,10); #Priests of Marr
+		quest::faction(311,10); #Steel Warriors
+		quest::exp(100);
+	}
+	elsif(plugin::check_handin(\%itemcount, 6701 => 1)) { #a dusty old leg bone
+		quest::say("According to this crest on the tunic, you have recovered one of the bones of a lost Paladin [Hero].");
+		quest::summonitem(6701);
+	}
+	elsif(plugin::check_handin(\%itemcount, 18827 => 1)) { #a testimony of truth
+		quest::say("Praise be to Marr!! You have done the impossible!! Sir Lucan is finally sent to the higher courts of the Tribunal. The city now has a chance to prosper. The Hall of Truth has been redeemed and gives you thanks. Take this, it is the Sword of Faith. May you wield it with righteousness. Beware of the remainder of the militia. They will be hunting for your head.");
+		quest::summonitem(13947); # Brilliant Sword of Faith
+	}
+	else {
+		#do all other handins first with plugin, then let it do disciplines
+		plugin::try_tome_handins(\%itemcount, $class, 'Paladin');
+	}
   plugin::return_items(\%itemcount);
 }
 
 # EOF zone: freportn ID: 8077 NPC: Valeron_Dushire
-

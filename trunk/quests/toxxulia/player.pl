c2b6efa193fc4808a4d0cb18c4ce1fe93dfee2b4
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-03-20 10:33:50 +0000

    Split qglobals from original tox

--- trunk/quests/toxxulia/player.pl
+++ trunk/quests/toxxulia/player.pl
@@ -1,31 +1,33 @@
+#ToDo: Check task compatibility for revamped tox
+
 sub EVENT_ENTERZONE {
   quest::settimer("spires",10);
-  $qglobals{nexus_tox} = undef;
-  $qglobals{spire_tox} = undef;
-  $qglobals{message_tox} = undef;
+  $qglobals{nexus_toxxulia} = undef;
+  $qglobals{spire_toxxulia} = undef;
+  $qglobals{message_toxxulia} = undef;
 }
 
 sub EVENT_TIMER {
-  if (defined $qglobals{nexus_tox} && defined $qglobals{spire_tox} && $qglobals{spire_tox} == 1 && plugin::check_hasitem($client, 19720)) {
+  if (defined $qglobals{nexus_toxxulia} && defined $qglobals{spire_toxxulia} && $qglobals{spire_toxxulia} == 1 && plugin::check_hasitem($client, 19720)) {
     quest::selfcast(2433); #self only to avoid AE
     quest::setglobal("spire_tox",0,1,"F");
     quest::delglobal("message_tox");
     $client->NukeItem(19720); #removes stone from inventory.
-    $qglobals{nexus_tox} = undef;
-    $qglobals{spire_tox} = undef;
-    $qglobals{message_tox} = undef;
+    $qglobals{nexus_toxxulia} = undef;
+    $qglobals{spire_toxxulia} = undef;
+    $qglobals{message_toxxulia} = undef;
   }
-  elsif (defined $qglobals{nexus_tox} && defined $qglobals{spire_tox} && $qglobals{spire_tox} == 1 && !defined $qglobals{message_tox} && !plugin::check_hasitem($client, 19720)) {
+  elsif (defined $qglobals{nexus_toxxulia} && defined $qglobals{spire_toxxulia} && $qglobals{spire_toxxulia} == 1 && !defined $qglobals{message_toxxulia} && !plugin::check_hasitem($client, 19720)) {
     $client->Message(15,"You don't have the correct component to travel to Luclin.");
     quest::setglobal("message_tox",1,1,"M20"); #prevent component message from being spammed.
-    $qglobals{nexus_tox} = undef;
-    $qglobals{spire_tox} = undef;
-    $qglobals{message_tox} = undef;
+    $qglobals{nexus_toxxulia} = undef;
+    $qglobals{spire_toxxulia} = undef;
+    $qglobals{message_toxxulia} = undef;
   }
-  elsif (defined $qglobals{nexus_tox} && defined $qglobals{spire_tox} && $qglobals{spire_tox} == 1 && defined $qglobals{message_tox} && !plugin::check_hasitem($client, 19720)) {
-    $qglobals{nexus_tox} = undef;
-    $qglobals{spire_tox} = undef;
-    $qglobals{message_tox} = undef;
+  elsif (defined $qglobals{nexus_toxxulia} && defined $qglobals{spire_toxxulia} && $qglobals{spire_toxxulia} == 1 && defined $qglobals{message_toxxulia} && !plugin::check_hasitem($client, 19720)) {
+    $qglobals{nexus_toxxulia} = undef;
+    $qglobals{spire_toxxulia} = undef;
+    $qglobals{message_toxxulia} = undef;
   }
 }
 

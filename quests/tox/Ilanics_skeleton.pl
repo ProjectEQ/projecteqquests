ed9e79db91d88e8b8a45c3b29638d2ea3fd4e6fc
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-03-20 10:10:16 +0000

    Added missing subs for item code.

--- quests/tox/Ilanics_skeleton.pl
+++ quests/tox/Ilanics_skeleton.pl
@@ -1,21 +1,16 @@
 # Ilanic's Scroll
 
 sub EVENT_SAY {
-
-   if($text=~/Hail/i){
-
-   quest::say("My scroll must find my [scroll] so I may finish my research.");
-
-}
-
-   if($text=~/scroll/i){
-
+  if($text=~/hail/i) {
+    quest::say("My scroll must find my [scroll] so I may finish my research.");
+  }
+  if($text=~/scroll/i) {
     quest::say("Kobold! Kobold!");
-
+  }
 }
- }
-      plugin::return_items(\%itemcount);
-      quest::say("What am I supposed to do with these?");
 
-
+sub EVENT_ITEM {
+  quest::say("What am I supposed to do with these?");
+  plugin::return_items(\%itemcount);
+}
 #END of FILE Zone:erudnint  ID:38150 --  Ilanics_skeleton

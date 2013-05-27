31e49660db65b4a1441c27b0bbda3676fe678d57
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-05-27 22:49:25 +0000

    redo the Wall Squad Ring script

--- trunk/quests/rivervale/Mayor_Gubbin.pl
+++ trunk/quests/rivervale/Mayor_Gubbin.pl
@@ -1,17 +1,32 @@
 sub EVENT_SAY {
   if($text=~/hail/i) {
-    quest::say("Greetings. $name!  Welcome to Rivervale!  I hope you plan to stay on and assist us in any way you can.  We all have talents to aid in the prosperity of the hollow.");
+    quest::say("Greetings! Allow me to introduce myself. I am Marshal Ghobber Truppack, Marshal of the Great Wall Patrol. Are you [new to the squad]?");
+  }
+  if (($text=~/new to the squad/i) {
+    if ($faction <= 5) {
+	  quest::say("Great!!! We required more deputies in this squad. We are suffering great losses to the [new threat]. Welcome to the Great Wall Patrol where our motto is, 'United we shall defend.' It will be some time before you can earn your squad ring - first you must prove your strength in battle. Report to the squad leader. He is Deputy Budo. You can find him at the Great Wall of Serilis in the Misty Thicket. Go to him at once and tell him you are reporting for duty.");
+	}
+  }
+  if($text=~/new threat/i) {
+    if ($faction <= 5) {
+	  quest::say("The new threat I refer to is the community of goblins which was found beyond the Great Wall of Serilis. They are called Clan Runnyeye. They have begun to sneak over the wall. The deputies have reported seeing them launch themselves up the wall in a matter of seconds using ornate grappling hooks. We have attempted to obtain of these devices for further inspection, but we have yet to find one of any of the dead goblins. If you should ever find one, be sure to bring it to me immediately.");
+	}
+  }
+  if(($text=~/new to the squad/i) || ($text=~/new threat/i)) {
+    if ($faction => 6) {
+	  quest::say("'How can you expect to just waltz into Guardian Stronghold and expect to gather information?! Consider yourself lucky I don't command the Guardian of the Vale deputies to show you the sharpness of their blades!");
+	}
   }
   if ($text=~/one with the wall/i) {
     if ($faction == 1){
       quest::say("Yes!! Welcome, Deputy $name! You are now an elite member of the Great Wall Division. Wear this ring with pride. You may take it to Hendi Mrubble of the Clerics of Mischief for healing at any time. Wear it with pride.");
       quest::summonitem(13936);
-	  quest::faction(133, -350); #Guardian of the Vale
+	  quest::faction(133, -399); #Guardian of the Vale
       #quest::enabletitle(1);
     }
-    else{
-      quest::say("Please serve the Vale more.  I am sure you will earn our complete trust in time, $name");
-    }  
+    else {
+      quest::say("You are in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
+    }
   }
 }
 

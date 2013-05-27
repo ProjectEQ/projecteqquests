cc09dc2ee3540ba03fb5fe8a3b6eb7100fdffbd2
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-05-27 23:04:46 +0000

    added return_item plugin.

--- trunk/quests/rivervale/Mayor_Gubbin.pl
+++ trunk/quests/rivervale/Mayor_Gubbin.pl
@@ -9,6 +9,7 @@ sub EVENT_ITEM {
     quest::say("Ah, thank you. I was a bit parched.' Mayor Gubbin unstops the flask and takes a healthy swig of the brew. He makes a strange face and licks his lips while staring at the bottle. 'Wow, this is delicious. Like no other drink I've ever tried. This is even better than that Spiced Jumjum that Lanena is so fond of. Wait a moment . . . Is this the brew that Deeppockets keeps trying to hawk in my town? Well now, I suppose it's not so bad. Hmm. Why don't you tell him I've changed my mind. Tell Lendel he has my blessing and is welcome to bring in as much of this as he can sell.");
     quest::summonitem(13588);
   }
+  plugin::return_items(\%itemcount);
 }
 
 

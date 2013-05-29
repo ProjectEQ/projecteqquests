6fdc8dc4fbc8e8926a8fae7e83debc839919f07d
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-05-29 20:52:17 +0000

    splits like an essence harvester npc when killed.  confirmed on live.

--- quests/airplane/Eternal_Spirit.pl
+++ quests/airplane/Eternal_Spirit.pl
@@ -1,3 +1,32 @@
+# on death of Eternal Spirit, two a soul harvester spawn
+#
+
+my $x;
+my $y;
+my $z;
+my $h;
+my $entid1;
+my $entid2;
+my $mob1;
+my $mob2;
+my $mobnpc1;
+my $mobnpc2;
+
+sub EVENT_DEATH {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  $z = $npc->GetZ();
+  $h = $npc->GetHeading();
+  $entid1 = quest::spawn2(71110,0,0,$x+10,$y,$z,$h);
+  $entid2 = quest::spawn2(71110,0,0,$x-10,$y,$z,$h);
+  $mob1 = $entity_list->GetMobID($entid1);
+  $mob2 = $entity_list->GetMobID($entid2);
+  $mobnpc1 = $mob1->CastToNPC();
+  $mobnpc2 = $mob2->CastToNPC();
+  $mobnpc1->AddToHateList($npc->GetHateTop());
+  $mobnpc2->AddToHateList($npc->GetHateTop());
+}
+
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 57081 =>1)) {
     quest::emote("beams and looks at you with pride.");

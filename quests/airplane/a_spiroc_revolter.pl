12ad35255fb786391e6eb1b215cf152b3256c7ca
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-05-30 16:34:56 +0000

    change the comments on PoSky spiroc scripts.

--- quests/airplane/a_spiroc_revolter.pl
+++ quests/airplane/a_spiroc_revolter.pl
@@ -1,5 +1,5 @@
 sub EVENT_DEATH {
   if($entity_list->GetMobByNpcTypeID(71009)) {
-    quest::updatespawntimer(2628,1000); #a_spiroc_revolter
+    quest::updatespawntimer(2628,1000); #update to respawn in 1 sec if vanquisher are still up
   }
 }

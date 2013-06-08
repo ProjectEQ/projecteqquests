1744901c86531fdb052da2f018493e97be0439ef
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-08 18:17:40 +0000

    added decay timer to uqua creategroundobject.

--- trunk/quests/airplane/an_essence_carrier.pl
+++ trunk/quests/airplane/an_essence_carrier.pl
@@ -6,18 +6,15 @@ my $y;
 my $z;
 my $h;
 my $entid1;
-my $entid2;
 my $mob1;
-my $mob2;
 my $mobnpc1;
-my $mobnpc2;
 
 sub EVENT_DEATH {
   $x = $npc->GetX();
   $y = $npc->GetY();
   $z = $npc->GetZ();
   $h = $npc->GetHeading();
-  $entid1 = quest::spawn2(71071,0,0,$x+10,$y,$z,$h);
+  $entid1 = quest::spawn2(71071,0,0,$x,$y,$z,$h);
   $mob1 = $entity_list->GetMobID($entid1);
   $mobnpc1 = $mob1->CastToNPC();
   $mobnpc1->AddToHateList($npc->GetHateTop());

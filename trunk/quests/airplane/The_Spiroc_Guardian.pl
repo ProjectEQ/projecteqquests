2eefb7a9046471641c5fbd951920d2ba898abe95
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-09 01:49:48 +0000

    added to include the other spiroc mobs to respawn if still up.

--- trunk/quests/airplane/The_Spiroc_Guardian.pl
+++ trunk/quests/airplane/The_Spiroc_Guardian.pl
@@ -1,5 +1,5 @@
 sub EVENT_DEATH {
-  if($entity_list->GetMobByNpcTypeID(71009)) {
-    quest::updatespawntimer(2631,1000); #update to respawn in 1 sec if vanquisher are still up
+  if($entity_list->GetMobByNpcTypeID(71009) || $entity_list->GetMobByNpcTypeID(71008) || $entity_list->GetMobByNpcTypeID(71007) || $entity_list->GetMobByNpcTypeID(71015) || $entity_list->GetMobByNpcTypeID(71011) || $entity_list->GetMobByNpcTypeID(71010) || $entity_list->GetMobByNpcTypeID(71014)) {
+    quest::updatespawntimer(2631,1000); #update to respawn in 1 sec if any spiroc except for lord are still up
   }
 }

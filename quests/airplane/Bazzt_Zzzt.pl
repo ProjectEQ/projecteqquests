4bed590137215f7e519b6f708584108ad17f7769
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-11 02:52:37 +0000

    update for more accurate spawnpoint

--- quests/airplane/Bazzt_Zzzt.pl
+++ quests/airplane/Bazzt_Zzzt.pl
@@ -1,5 +1,5 @@
 #Bazzt_Zzzt
 sub EVENT_DEATH {
   quest::setglobal("sirran",6,3,"M20");
-  quest::spawn2(71058,0,0,206,-980,803,0); #Sirran the Lunatic
+  quest::spawn2(71058,0,0,234,-1078,812,192); #Sirran the Lunatic
 }

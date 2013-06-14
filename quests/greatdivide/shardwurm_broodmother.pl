c1c200cc77c86921867108abc725e69c1b9c336a
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-14 03:49:36 +0000

    fixed formatting

--- quests/greatdivide/shardwurm_broodmother.pl
+++ quests/greatdivide/shardwurm_broodmother.pl
@@ -2,8 +2,8 @@ sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 30272=> 1)) { #Harness of Control
     quest::emote("reels in pain as the harness snaps around its neck tightly. A strange gleam enters its eyes as it slowly begins to shamble towards the exit and the giants fortress.");
     quest::exp(20000);
-	quest::ding();
-	quest::start(57);
+    quest::ding();
+    quest::start(57);
   }
   plugin::return_items(\%itemcount);
 }
@@ -11,21 +11,22 @@ sub EVENT_ITEM {
 sub EVENT_WAYPOINT_ARRIVE {
   if ($wp == 16) {
     $x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x-25,$x+25,$y-25,$y+25);
+    $y = $npc->GetY();
+    quest::set_proximity($x-25,$x+25,$y-25,$y+25);
   }
   if ($wp == 19) {
     quest::clear_proximity();
-	if($entity_list->GetMobByNpcTypeID(118208)){
+    if($entity_list->GetMobByNpcTypeID(118208)){
       quest::attacknpctype(118208);
-	  quest::signal(118207);
-	}
+      quest::signal(118207);
+    }
   }
 }
 
 sub EVENT_ENTER {
   quest::spawn2(118207,0,0,-2397,-2615,294,0);
   quest::spawn2(118208,0,0,-2391,-2569,299,0);
+  quest::clear_proximity();
 }
 
 sub EVENT_SIGNAL {
--- quests/thurgadina/Erdarf_Restil.pl
+++ quests/thurgadina/Erdarf_Restil.pl
@@ -23,15 +23,15 @@ sub EVENT_ITEM {
     quest::emote("smirks at you. 'Hrm, you again eh? Did you come back to bother me with more...Wait a minute, let me see that. This is... This must be, yes it is. This is one of the ancient coldain priests sceptres of channeling. This is too good to be true. Haha! Oh, what I can do with this. Here, take this gem from the sceptre. If ancient lore proves right it will allow you to persuade beasts of many kinds.' He grins at you strangely and then leaves.");
     quest::summonitem(30270); #Gem of Persuasion
     quest::exp(25000);
-	quest::ding();
-	quest::depop_withtimer();
+    quest::ding();
+    quest::depop_withtimer();
   }
   if(($faction <= 3) && (plugin::check_handin(\%itemcount, 30271 => 1))) { #Bottle of Karsin Acid
     quest::emote("turns his head long enough for you to slip the contens of the karsin bottle into his mug. As he drinks heavily his face turns even more pale until he looks as if in pain. He rushes from the bar and as he does a trinket slips from his pocket.");
     quest::summonitem(30270); #Gem of Persuasion
     quest::exp(25000);
-	quest::ding();
-	quest::depop_withtimer();
+    quest::ding();
+    quest::depop_withtimer();
   }
   plugin::return_items(\%itemcount);
 }

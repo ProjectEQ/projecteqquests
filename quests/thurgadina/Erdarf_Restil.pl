7ed042cafb487a3eed7e5f93c2da319830a3ac97
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-14 03:41:28 +0000

    Upload missing scripts for a quest "Crystal Caverns' Ancient Artifacts"

--- quests/thurgadina/Erdarf_Restil.pl
+++ quests/thurgadina/Erdarf_Restil.pl
@@ -2,25 +2,36 @@
 #
 
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("Huh? Don't you know better than to interrupt a Coldain enjoying his ale? I may be a user of priestly magic but that doesnt mean I can't bash your head in.");
-  }
-  if ($text=~/priestly magic/i) {
-    quest::say("Aye. An old one not much taken with idle chit chat.");
-  }
-  if ($text=~/old one/i) {
-    quest::say("Very old, very wise, and on and on. You must have some point to bothering me. I'm not a merchant and I don't contract myself out for work. I've performed works for the Dain. There isn't much I haven't done..");
-  }
-  if ($text=~/have you done/i) {
-    quest::emote("chuckles at you. 'If you want to impress me, and let me make certain you understand I doubt you can, then you can do what I never was able. For years I adventured and attempted to venture to the bottom of the Crystal Caverns in search of ancient artifacts from my people. Do that, show me that you have, and I will be impressed.'");
+  if ($faction <= 3) {
+    if ($text=~/hail/i) {
+      quest::say("Huh? Don't you know better than to interrupt a Coldain enjoying his ale? I may be a user of priestly magic but that doesnt mean I can't bash your head in.");
+    }
+    if ($text=~/priestly magic/i) {
+      quest::say("Aye. An old one not much taken with idle chit chat.");
+    }
+    if ($text=~/old one/i) {
+      quest::say("Very old, very wise, and on and on. You must have some point to bothering me. I'm not a merchant and I don't contract myself out for work. I've performed works for the Dain. There isn't much I haven't done..");
+    }
+    if ($text=~/haven't done/i) {
+      quest::emote("chuckles at you. 'If you want to impress me, and let me make certain you understand I doubt you can, then you can do what I never was able. For years I adventured and attempted to venture to the bottom of the Crystal Caverns in search of ancient artifacts from my people. Do that, show me that you have, and I will be impressed.'");
+    }
   }
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 30269 => 1)) {
+  if(($faction <= 3) && (plugin::check_handin(\%itemcount, 30269 => 1))) { #Sceptre of the Coldain Ancients
     quest::emote("smirks at you. 'Hrm, you again eh? Did you come back to bother me with more...Wait a minute, let me see that. This is... This must be, yes it is. This is one of the ancient coldain priests sceptres of channeling. This is too good to be true. Haha! Oh, what I can do with this. Here, take this gem from the sceptre. If ancient lore proves right it will allow you to persuade beasts of many kinds.' He grins at you strangely and then leaves.");
-    quest::summonitem(30270);
+    quest::summonitem(30270); #Gem of Persuasion
     quest::exp(25000);
+	quest::ding();
+	quest::depop_withtimer();
+  }
+  if(($faction <= 3) && (plugin::check_handin(\%itemcount, 30271 => 1))) { #Bottle of Karsin Acid
+    quest::emote("turns his head long enough for you to slip the contens of the karsin bottle into his mug. As he drinks heavily his face turns even more pale until he looks as if in pain. He rushes from the bar and as he does a trinket slips from his pocket.");
+    quest::summonitem(30270); #Gem of Persuasion
+    quest::exp(25000);
+	quest::ding();
+	quest::depop_withtimer();
   }
   plugin::return_items(\%itemcount);
 }

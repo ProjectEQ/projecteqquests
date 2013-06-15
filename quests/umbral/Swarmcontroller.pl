2c990ec45335a77ff6ee1ef8162ff0240be63d51
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-15 22:41:23 +0000

    change the trigger event and timing for swarmcaller.

--- quests/umbral/Swarmcontroller.pl
+++ quests/umbral/Swarmcontroller.pl
@@ -1,22 +1,17 @@
 my $count = 0;
 
 sub EVENT_SIGNAL {
-  quest::settimer(1,300);
+  quest::settimer(1,90);
 }
 
 sub EVENT_TIMER {
   $count++;
   if($count==1) { #4x a netherbian ravager
-    if(!$entity_list->GetMobByNpcTypeID(176007)) {
-	  quest::spawn2(176113,0,0,487,2281,302,135);
-	  quest::spawn2(176113,0,0,455,2253,305,64);
-	  quest::spawn2(176113,0,0,475,2229,305,14);
-	  quest::spawn2(176113,0,0,504,2258,304,184);
-	}
-	else {
-	  quest::stoptimer(1);
-	  $count=0;
-	}
+    quest::spawn2(176113,0,0,487,2281,302,135);
+    quest::spawn2(176113,0,0,455,2253,305,64);
+    quest::spawn2(176113,0,0,475,2229,305,14);
+    quest::spawn2(176113,0,0,504,2258,304,184);
+	quest::settimer(1,300);
   }
   if($count==2) {  #4x a netherbian ravager
     quest::spawn2(176113,0,0,487,2281,302,135);
@@ -43,7 +38,7 @@ sub EVENT_TIMER {
     quest::spawn2(176113,0,0,504,2258,304,184);
   }	  
   if($count==6) {  #3x a netherbian ravager and 1x Netherbian Swarmlord
-    quest::spawn2(176093,0,0,487,2281,302,135);
+    quest::spawn2(176093,0,0,482,2254,306,128);
     quest::spawn2(176113,0,0,455,2253,305,64);
     quest::spawn2(176113,0,0,475,2229,305,14);
     quest::spawn2(176113,0,0,504,2258,304,184);

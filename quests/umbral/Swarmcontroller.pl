108a86a893735b196348a87290eb6a343e653f66
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-16 00:27:30 +0000

    fix the timer.

--- quests/umbral/Swarmcontroller.pl
+++ quests/umbral/Swarmcontroller.pl
@@ -11,7 +11,8 @@ sub EVENT_TIMER {
     quest::spawn2(176113,0,0,455,2253,305,64);
     quest::spawn2(176113,0,0,475,2229,305,14);
     quest::spawn2(176113,0,0,504,2258,304,184);
-	quest::settimer(1,300);
+    quest::stoptimer(1);
+    quest::settimer(1,300);
   }
   if($count==2) {  #4x a netherbian ravager
     quest::spawn2(176113,0,0,487,2281,302,135);

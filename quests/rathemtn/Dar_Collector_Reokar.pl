1777d8ff13ae8061257d6971c67c9bf697ab1685
cavedude00@gmail.com <cavedude00@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-25 14:27:26 +0000

    Reokar's Collection correction by noudess.

--- quests/rathemtn/Dar_Collector_Reokar.pl
+++ quests/rathemtn/Dar_Collector_Reokar.pl
@@ -1,13 +1,13 @@
 sub EVENT_SAY{
   if ($text=~/hail/i){
-  quest::say("Greetings! Have you anything for me? Don't look at me like that, you know what I mean. Odds and ends from out there in the swamp. Rat parts are fine, and I could always use some freshly caught fish.");
+  quest::say("Greetings! Have you anything for me? Don't look at me like that, you know what I mean. Odds and ends from out there in the swamp. Rat parts are fine, and I could always use some freshly caught fish. If you've not done work for me before, perhaps we can start with bone chips?  Always bring me 4 of everything.  I hate not being able to use them immediately!");
   }
 }
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 13073 => 4) && $qglobals{darcollect} == undef){
+  if($qglobals{darcollect} == undef && plugin::check_handin(\%itemcount, 13073 => 4)){
   quest::say("Thank you!");
-  quest::setglobal("darcollect",1,2,"M30");
+  quest::setglobal("darcollect",1,2,"D30");
   quest::faction(35, 4);
   quest::faction(136, 4);
   quest::faction(146, 4);
@@ -16,9 +16,9 @@ sub EVENT_ITEM {
   quest::exp(100);
   quest::ding();
   }
-  if(plugin::check_handin(\%itemcount, 13073 => 4) && $qglobals{darcollect} == 1){
+  if($qglobals{darcollect} == 1 && plugin::check_handin(\%itemcount, 13073 => 4)){
   quest::say("I'm going to need some fish now.");
-  quest::setglobal("darcollect",2,2,"M30");
+  quest::setglobal("darcollect",2,2,"D30");
   quest::faction(35, 4);
   quest::faction(136, 4);
   quest::faction(146, 4);
@@ -27,9 +27,9 @@ sub EVENT_ITEM {
   quest::exp(100);
   quest::ding();
   }
-  if(plugin::check_handin(\%itemcount, 13019 => 4) && $qglobals{darcollect} == 2){
+  if($qglobals{darcollect} == 2 && plugin::check_handin(\%itemcount, 13019 => 4)){
   quest::say("Thank you!");
-  quest::setglobal("darcollect",3,2,"M30");
+  quest::setglobal("darcollect",3,2,"D30");
   quest::faction(35, 4);
   quest::faction(136, 4);
   quest::faction(146, 4);
@@ -38,9 +38,9 @@ sub EVENT_ITEM {
   quest::exp(100);
   quest::ding();
   }
-  if(plugin::check_handin(\%itemcount, 13019 => 4) && $qglobals{darcollect} == 3){
+  if($qglobals{darcollect} == 3 && plugin::check_handin(\%itemcount, 13019 => 4)){
   quest::say("I'm going to need some rat ears now.");
-  quest::setglobal("darcollect",4,2,"M30");
+  quest::setglobal("darcollect",4,2,"D30");
   quest::faction(35, 4);
   quest::faction(136, 4);
   quest::faction(146, 4);
@@ -49,9 +49,9 @@ sub EVENT_ITEM {
   quest::exp(100);
   quest::ding();
   }
-  if(plugin::check_handin(\%itemcount, 13050 => 4) && $qglobals{darcollect} == 4){
+  if($qglobals{darcollect} == 4 && plugin::check_handin(\%itemcount, 13050 => 4)){
   quest::say("Thank you!");
-  quest::setglobal("darcollect",5,2,"M30");
+  quest::setglobal("darcollect",5,2,"D30");
   quest::faction(35, 4);
   quest::faction(136, 4);
   quest::faction(146, 4);
@@ -60,7 +60,7 @@ sub EVENT_ITEM {
   quest::exp(100);
   quest::ding();
   }
-  if(plugin::check_handin(\%itemcount, 13050 => 4) && $qglobals{darcollect} == 5){
+  if($qglobals{darcollect} == 5 && plugin::check_handin(\%itemcount, 13050 => 4)){
   quest::say("I'm going to need some bone chips now.");
   quest::delglobal("darcollect");
   quest::faction(35, 4);
@@ -71,6 +71,8 @@ sub EVENT_ITEM {
   quest::exp(100);
   quest::ding();
   }
+  else{
   plugin::return_items(\%itemcount);
+  }
   $qglobals{darcollect} = undef;
 }

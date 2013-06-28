25a63dc07b6b579e7162088897b833e691e4bb18
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-06-28 04:29:10 +0000

    fixed a few files with EVENT_DEATH_COMPLETE

--- quests/airplane/a_spiroc_banisher.pl
+++ quests/airplane/a_spiroc_banisher.pl
@@ -1,4 +1,4 @@
-sub EVENT_DEATH_COMPLETE_COMPLETE {
+sub EVENT_DEATH_COMPLETE {
   if($entity_list->GetMobByNpcTypeID(71009)) {
     quest::updatespawntimer(2623,1000); #update to respawn in 1 sec if vanquisher are still up
   }

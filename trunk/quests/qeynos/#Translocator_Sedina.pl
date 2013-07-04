5d18dd614d0822a8a3d824489321abdbb4034ef3
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-04 00:26:39 +0000

    convert Translocator Sedina to lua

--- trunk/quests/qeynos/#Translocator_Sedina.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Hello there. There seem to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to] Erud's [Crossing] in the meantime, I can transport you to my companion there."); 
-}
-if($text=~/travel to crossing/i){
-quest::selfcast("2289"); }
-}
- 
-

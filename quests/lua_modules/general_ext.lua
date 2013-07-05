a6c1aaffac48d64f3250ea2f48862dfaca1a616d
KimmySprite <KimmySprite@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-05 07:36:44 +0000

    Support for new eq constructs...

--- quests/lua_modules/general_ext.lua
+++ quests/lua_modules/general_ext.lua
@@ -1,3 +1,14 @@
 function eq.ChooseRandom(tbl)
 	return tbl[math.random(#tbl)];
 end
+
+function eq.SelfCast(spell_id)
+	local init = eq.get_initiator();
+	local sp = Spell(spell_id);
+	
+	if(init.null or sp.null) then
+		return;
+	end
+	
+	init:SpellFinished(spell_id, init, 10, 0, -1, sp:ResistDiff());
+end

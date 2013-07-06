b1ca8b9cdde6d8f55801e15522e94b86bdf4efc9
cavedude00@gmail.com <cavedude00@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-06 05:23:00 +0000

    More work on Rivervale

---
+++ quests/rivervale/Daleen_Leafsway.lua
@@ -0,0 +1,25 @@
+function event_spawn(e)
+	eq.set_timer("blurt",600000);
+end
+
+function event_say(e) 
+	local faction = e.other:GetFaction(e.self);
+	
+	if(e.message:findi("Hail")) then
+		e.self:Say(string.format("Greetings. %s!  Welcome to Tagglefoot's Farm.  We grow nothing but the finest vegetables in our field.  We even manage to harvest the mystical jumjum stalk in our fields.  Karana has blessed us indeed.", e.other:GetCleanName()));  
+	elseif(faction == 1 and e.message:findi("starving")) then
+		e.self:Say("Deputy Eigon! I forgot! I was supposed to bring him some turnips to eat while he is on patrol! Oh... He asked so nicely, too. I feel bad that I forgot. If only someone would take these [turnips] to the Deputy..");
+	elseif(faction == 1 and e.message:findi("turnips")) then
+		e.self:Say("Oh, thank you so much! You can keep any payment he gives you. Be sure to tell him I'm sorry.");
+		e.other:SummonItem(16165);
+	end
+end
+
+function event_timer(e)
+	if (timer == blurt) then
+		e.self:Say("Oh Dear.. I can't believe I forgot.. He must be [starving]!");
+	end
+end
+
+--END of FILE Zone:rivervale  ID:19082 -- Daleen_Leafsway 
+

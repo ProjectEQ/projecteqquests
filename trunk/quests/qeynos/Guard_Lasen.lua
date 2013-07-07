b70d92aed9d51914951edc97ed4348f3a353c292
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-07 00:25:06 +0000

    remove the if item return statement.

--- trunk/quests/qeynos/Guard_Lasen.lua
+++ trunk/quests/qeynos/Guard_Lasen.lua
@@ -16,7 +16,5 @@ function event_trade(e)
     e.other:Faction(217,5,0);
     e.other:GiveCash(5,0,0,0);
   end
-  if(item_lib.return_items(e.self, e.other, e.trade)) then
-   	e.self:Say(string.format("I have no need for this %s, you can have it back.",e.other:GetName()));
-  end
+  item_lib.return_items(e.self, e.other, e.trade)
 end

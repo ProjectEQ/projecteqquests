ae42f4f959721c9570f00a46288e082ed07ae5ff
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-07 00:19:01 +0000

    lua for qeynos guards.

--- quests/qeynos/Guard_Lasen.lua
+++ quests/qeynos/Guard_Lasen.lua
@@ -1,9 +1,3 @@
-function event_combat(e)
-  if(e.joined == true) then
-    e.self:Say(string.format("Time to die %s!",e.other:GetCleanName()));
-  end
-end
-
 function event_say(e)
   if(e.message:findi("hail")) then
     e.self:Say("I wish it wasn't so blasted hot, I could really use some water.");

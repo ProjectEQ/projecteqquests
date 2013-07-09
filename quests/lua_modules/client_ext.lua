659469d84da1506d12aabd672965d6409da84cf9
darkkehl@gmail.com <darkkehl@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-09 01:06:01 +0000

    Updated client_ext.lua with a function for class checking that uses strings ie) "Warrior" instead of "1"

--- quests/lua_modules/client_ext.lua
+++ quests/lua_modules/client_ext.lua
@@ -65,6 +65,33 @@ function Client:GetFaction(npc)
 	return self:GetFactionLevel(self:CharacterID(), npc:GetID(), self:GetRace(), self:GetClass(), self:GetDeity(), npc:GetPrimaryFaction(), npc);
 end
 
+function Client:Class()
+	local class = self:GetClass();
+	
+	do
+		local c = {
+		 [1] = "Warrior",
+		 [2] = "Cleric",
+		 [3] = "Paladin",
+		 [4] = "Ranger",
+		 [5] = "Shadow Knight",
+		 [6] = "Druid",
+		 [7] = "Monk",
+		 [8] = "Bard",
+		 [9] = "Rogue",
+		 [10] = "Shaman",
+		 [11] = "Necromancer",
+		 [12] = "Wizard",
+		 [13] = "Magician",
+		 [14] = "Enchanter",
+		 [15] = "Beastlord",
+		 [16] = "Berserker"
+		 }
+
+	return c[class];
+	end
+end
+
 function Client:Race()
 	local race = self:GetRace();
 	

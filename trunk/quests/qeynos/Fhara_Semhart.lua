565fc9291c401f9c9c75a4454a76ab2a53ccd8fc
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-09 06:55:59 +0000

    fix a qglobal conditional

--- trunk/quests/qeynos/Fhara_Semhart.lua
+++ trunk/quests/qeynos/Fhara_Semhart.lua
@@ -1,7 +1,7 @@
 function event_say(e)
 	local tax = eq.get_qglobals(e.self);
 	
-	if(e.message:findi("tax collection") and tax.tax_collection ~= not nil) then
+	if(e.message:findi("tax collection") and tax.tax_collection ~= nil) then
 		e.self:Say("Taxes? How can I afford more taxes? You tax us poor merchants right out of Qeynos. Freeport is looking better and better every day.");
 		e.other:SummonItem(13175);
 		e.other:Faction(217,-10,0);

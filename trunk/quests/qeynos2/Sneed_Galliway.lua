bdaaeb5df898812a8f9b15eb039430461a2b5a7e
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-09 07:57:38 +0000

    add a missing text to Sneed Galliway

--- trunk/quests/qeynos2/Sneed_Galliway.lua
+++ trunk/quests/qeynos2/Sneed_Galliway.lua
@@ -6,6 +6,8 @@ function event_say(e)
 		e.self:Say(string.format("Hello, %s. I am Sneed Galliway, merchant and citizen. I run the only general store in all of Qeynos, serving the needs of the many adventurers who call Qeynos home. If only one of them would take the time to help me with my er... [problem]...",e.other:GetName()));
 	elseif(e.message:findi("problem")) then
 		e.self:Say("Shhh.. Keep it down. It seems I have a little rodent problem in my store. A mangy rat keeps digging into my crates of rations. He looks as if he is diseased. If you help me by killing this pest, I will make it owrth your while. Bring me his head as proff it is done.");
+	elseif(e.message:findi("ronn castekin")) then
+		e.self:Say("Ronn? I saw him a few days ago. He was with someone dressed in black I didn't recognize .. I think I saw them later that day, swimming in the north pond. They really should stay away from the aqueducts. They can be quite dangerous. Full of rats, ya know.");
 	elseif(e.message:findi("tax collection")and tax.tax_collection ~= nil) then
 		e.self:Say(string.format("Hey, %s, how the heck did Vicus convince you to do his job? He better be paying you well. Here you go.",e.other:GetName()));
 		e.other:SummonItem(13172);

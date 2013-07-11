4efe1adcddd1bac81297c423a04ceeccf9c0abd0
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-11 04:50:29 +0000

    more Qeynos2 converts. Added new scripts.  Added a partial "The Package" quest.

--- trunk/quests/qeynos2/Pelshia_Thuxpire.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_SIGNAL {
-  if($signal == 1) {
-    quest::say("Um, just fine, Knargon. What do you want now, huh?");
-    quest::signalwith(2072,1,1);
-	}
-  if($signal == 2) {
-	quest::say("How about you go, and I'll catch up with you... never?");
-    }
-}

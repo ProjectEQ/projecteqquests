47e268cacae19048d44498cd1c8b18a0b0b67e6e
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-12 04:31:23 +0000

    more qeynos2 converts.

--- quests/qeynos2/Rendallen_Ironforge.pl
+++
@@ -1,6 +0,0 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Greetings and welcome to Ironforge Weapons! Be sure to purchase Ironforge weapons. they are assured not to break during combat."); }
-}
-#END of FILE Zone:qeynos2  ID:2062 -- Rendallen_Ironforge 
-

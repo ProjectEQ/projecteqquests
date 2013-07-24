07c85fb899df878ea87ffb1abe477f28c1f5a24a
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-24 06:32:45 +0000

    add faction requirement to initiate the "Sentry Xyrin" quest.

--- trunk/quests/freportn/Serna_Tasknon.lua
+++ trunk/quests/freportn/Serna_Tasknon.lua
@@ -1,11 +1,17 @@
 function event_say(e)
+	local fac = e.other:GetFaction(e.self);
+	
 	if(e.message:findi("hail")) then
 		e.self:Say("It is a good thing to see new faces visiting our temple. We are the source of strength within this city. Do not be fooled by the Freeport Militia. They are not warriors of valor. [Join the fight against the Freeport Militia].");
 	elseif(e.message:findi("join the fight")) then
 		e.self:Say("You are wise. If you are a paladin, either speak with Theron of this temple or visit the Hall of Truth here in North Freeport. Clerics should concetrate on keeping the knights strong and healthy. Would you care to [assist the Temple of Marr]?");
 	elseif(e.message:findi("assist")) then
-		e.self:Say("Good. Take this Potion of Marr to the Sentries of Passion. They are the protectors of this temple. Start in alphabetical order and the first shall take but a sip then you shall take it to the next in order of the alphabet. There are but eight sentries. Sentry Andlin to Sentry Xyrin. Go.");
-		e.other:SummonItem(12127);
+		if(fac < 5) then
+			e.self:Say("Good. Take this Potion of Marr to the Sentries of Passion. They are the protectors of this temple. Start in alphabetical order and the first shall take but a sip then you shall take it to the next in order of the alphabet. There are but eight sentries. Sentry Andlin to Sentry Xyrin. Go.");
+			e.other:SummonItem(12127);
+		else
+			e.self:Say("The path you walk is correct, but you have further to travel before you need worry about this.");
+		end
 	end
 end
 

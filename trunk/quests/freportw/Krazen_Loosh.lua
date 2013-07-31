3c1f113a8c214d24e61b03124745494367057837
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-07-31 00:36:45 +0000

    fixed and convert some scripts that are related to soulfire quest.

---
+++ trunk/quests/freportw/Krazen_Loosh.lua
@@ -0,0 +1,25 @@
+function event_spawn(e)
+	e.self:SetRunning(true);
+end
+
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("The towering wall of stone is clearly unmovable at this point, being held in place by collection of magical energies.");
+	end
+end
+
+function event_waypoint_arrive(e)
+	if(e.wp == 8) then
+		e.self:Say("Hello, Alayle. We just got a message from Qeynos. I think you should come with me.");
+		eq.signal(9141,1);
+		e.self:SetRunning(false);
+	end
+end
+
+function event_signal(e)
+		e.self:Say("As you try to open penetrate the stone wall it is clearly being held in place by a powerful force.");
+		eq.attack_npc_type(9141);
+		eq.signal(9106,1);
+end	
+
+--END of FILE Zone:freportw  Krazen_Loosh
--- trunk/quests/paw/Blazemaster_Arnab.pl
+++
@@ -1,3 +0,0 @@
-sub EVENT_SPAWN {
-  quest::shout("Who dares to destroy the denizens of flame? You shall feel my fiery wrath!");
-}

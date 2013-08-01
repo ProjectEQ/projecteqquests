712395169a4bb782fddb254122627f44055ca56a
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-08-01 22:03:34 +0000

    convert and adjust script for overhaul freeport.

---
+++ trunk/quests/freeporteast/Guard_Jup.lua
@@ -0,0 +1,26 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Hail, Citizen. In this time of piracy on the high seas, your [duty] is clear, is it not?");
+	elseif(e.message:findi("duty")) then
+		e.self:Say("What duty? To your state and your fellow man, that is what duty! These attacks must be stopped, before all order is lost and the city falls to [vermin]! We must strike them down first, with strength and determination; this could get out of hand.");
+	elseif(e.message:findi("vermin")) then
+		e.self:Say("The pirates of Broken Skull Rock are uncouth and relentless. They take whatever they may want, ruining trade lines without regard to the might of such established cities as Freeport. The rotten thieves are all thrown and mixed together in filthy shantytowns from what I hear, so striking at their heart should not be terribly difficult. Your [mission] is clear.");
+	elseif(e.message:findi("mission")) then
+		e.self:Say("Go to Broken Skull Rock! Slay these impudent pirates and wreak havoc on their plans. Some of them carry a seal that grants passage about their society, bring me ten of these such seals and your duty to your city shall be fulfilled. We might even have some form of [reward] for you.");
+	elseif(e.message:findi("reward")) then
+		e.self:Say("First the seals, then the reward! Take this to transport them in.");
+		e.other:SummonItem(17174);
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+
+	if(item_lib.check_turn_in(e.trade, {item1 = 54010})) then
+		e.self:Say(string.format("Excellent work, %s!  This will teach the pirates of Broken Skull a lesson and help our cause in repelling them.  Here is your reward.",e.other:GetName()));
+		e.other:SummonItem(54024);
+		e.other:Ding();
+		e.other:AddEXP(10000);
+	end
+	item_lib.return_items(e.self, e.other, e.trade)
+end
--- trunk/quests/freeporteast/Jyle_Windshot.lua
+++ trunk/quests/freeporteast/Jyle_Windshot.lua
@@ -1,19 +1,15 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hello. Might I ask what you are looking for?");
+	e.self:Say("Greetings, friend!! Nice to meet you. I see you enjoy barging into other people's rooms. No problem. I enjoy the company. What are you looking for?");
 	elseif(e.message:findi("treant wood")) then
-		if(e.other:Class() == "Ranger") then
-			e.self:Say("You are a new courier from the Faydarks. I am sorry I did not stay closer to the docks. I hear that part of Freeport is dangerous and is filled with many rogues. I shall require a new Small Lantern in trade for the Treant Wood.");
-		else
-			e.self:Say("Your ways are considered vile to Faydark's Champions. Leave before my rage overcomes my restraint."
-		end
+		e.self:Say("So you want a treant shards? I have them. If you want them I will trade you for them. Hmm.. I will take 2 gold and how about a nifty Faydark Champion Tunic. That is all I want. No more, no less.");
 	end
 end
 
 function event_trade(e)
 	local item_lib = require("items");
 
-	if(item_lib.check_turn_in(e.trade, {item1 = 13003})) then -- Small Lantern
+	if(item_lib.check_turn_in(e.trade, {item1 = 13536, gold = 2})) then -- dirty green tunic and 2 gold.
 		e.self:Say("Thanks, friend. I have run a long way to get here in time. Mostly at night. I lost my lantern in a card game in Highkeep.");
 		e.other:SummonItem(eq.ChooseRandom(13824, 13824, 13824, 13824, 13824, 13824, 13824, 13824, 13824, 12334)); -- 1/10 chance of wooden heart 9/10 chance of Wooden Shards
 		e.other:Ding();
@@ -27,4 +23,4 @@ function event_trade(e)
 	item_lib.return_items(e.self, e.other, e.trade)
 end
 
---eof freportw - Jyle Windshot
+-- END of FILE Zone: -- Jyle_Windshot *(FREEPORTEAST VERSION)*

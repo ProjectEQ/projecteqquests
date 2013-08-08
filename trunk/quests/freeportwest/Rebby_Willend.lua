3097d5c79add3ad0be2ce37eab133470e093dbcb
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-08-08 03:53:42 +0000

    convert for west freeport and adjust for overhaul west freeport.

---
+++ trunk/quests/freeportwest/Rebby_Willend.lua
@@ -0,0 +1,18 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Hey friend. nice wallet ya got there.  Bet it's fulla money. huh?  Why don't ya let me take a quick look. huh?");
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+
+	if(item_lib.check_turn_in(e.trade, {item1 = 18923})) then
+		e.other:SummonItem(13158);
+		e.other:Ding();
+		e.other:AddEXP(50);
+	end
+	item_lib.return_items(e.self, e.other, e.trade)
+end
+
+-- END of FILE Zone:freportw -- Rebby_Willend

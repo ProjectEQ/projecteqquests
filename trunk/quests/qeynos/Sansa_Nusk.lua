9f39fbdfc66c30e696b2f1d0d2f4f5a0bfabd468
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-08-15 02:16:24 +0000

    forgot to update a couple of scripts for South Qeynos.

--- trunk/quests/qeynos/Sansa_Nusk.lua
+++ trunk/quests/qeynos/Sansa_Nusk.lua
@@ -14,6 +14,7 @@ function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.trade, {item1 = 13703,platinum = 1})) then
 		e.self:Say("I am not even going to bother asking where you got that. Just take this set I already made and hurry them back to Fabian.");
+		e.other:SummonItem(13709);
 		e.other:Ding();
 		e.other:Faction(192,30,0);
 		e.other:Faction(184,30,0);

419b0484d1b5d004ae9282c8cd0e1cd0da1828e1
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-08-16 04:56:01 +0000

    converts for befallen

---
+++ trunk/quests/befallen/Wraps_McGee.lua
@@ -0,0 +1,17 @@
+function event_spawn(e)
+	eq.set_timer("shout",45000);
+}
+
+function event_say(e)
+	if(e.message:findi("trick or treat")) then
+		e.self:Say("Here I'll just wrap this up for you. . . Get it? Hahaha!");
+		e.other:SummonItem(eq.ChooseRandom(84091,84092,84093,6800,6800,6800,6800,6800,6800,6800));
+		eq.update_task_activity(220,9);
+	end
+end
+
+function event_timer(e)
+	if(e.timer == "shout") then
+		e.self:Shout("Trick or treat! Smell my feet! Give me something good to eat!");
+	end
+end

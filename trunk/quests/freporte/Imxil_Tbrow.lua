26a2792b28dc8b963bcc119ae18a1876d2d20c53
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-09-29 04:49:42 +0000

    added bar text flavors in old east freeport.

--- trunk/quests/freporte/Imxil_Tbrow.lua
+++ trunk/quests/freporte/Imxil_Tbrow.lua
@@ -4,6 +4,14 @@ function event_say(e)
 	end
 end
 
+function event_signal(e)
+	if(e.signal == 2) then
+		e.self:Say("Those halfwit ogres make good pets. If they are housebroken.");
+	elseif(e.signal == 4) then
+		e.self:Say("The Teir'Dal cower to no one. We are the only true force to be reckoned with.");
+	end
+end
+
 function event_trade(e)
 	local item_lib = require("items");
 	item_lib.return_items(e.self, e.other, e.trade)

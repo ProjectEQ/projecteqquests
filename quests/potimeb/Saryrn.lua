efb8cae0b45718c3ed07c72217de337191327bfd
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-10-08 03:40:12 +0000

    typo in Saryrn.lua

--- quests/potimeb/Saryrn.lua
+++ quests/potimeb/Saryrn.lua
@@ -11,22 +11,22 @@ function event_hp(e)
 	if (e.hp_event == 91) then
 		-- spawn Saryrn's 4 random mobs from A_Mouth_of_Insanity, A_Tormentor, A_Mouth_of_Dementia, A_Twisted_Tormentor
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0);
-		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348.0);
+		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0);
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0);
-		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348.0);
+		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0);
 		eq.set_next_hp_event(51);
 	elseif (e.hp_event == 51) then
 		-- spawn Saryrn's 4 random mobs from A_Mouth_of_Insanity, A_Tormentor, A_Mouth_of_Dementia, A_Twisted_Tormentor
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0);
-		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348.0);
+		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0);
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0);
-		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348.0);
+		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0);
 		eq.set_next_hp_event(11);
 	elseif (e.hp_event == 11) then
 		-- spawn Saryrn's 4 random mobs from A_Mouth_of_Insanity, A_Tormentor, A_Mouth_of_Dementia, A_Twisted_Tormentor
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0);
-		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348.0);
+		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0);
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0);
-		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348.0);
+		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0);
 	end
 end

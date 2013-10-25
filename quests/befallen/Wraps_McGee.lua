87caae6926eef5522a4d57a3c003f000cc3bdf83
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-10-25 13:31:24 +0000

    Leere: Update Halloween task properly.

--- quests/befallen/Wraps_McGee.lua
+++ quests/befallen/Wraps_McGee.lua
@@ -6,7 +6,7 @@ function event_say(e)
 	if(e.message:findi("trick or treat")) then
 		e.self:Say("Here I'll just wrap this up for you. . . Get it? Hahaha!");
 		e.other:SummonItem(eq.ChooseRandom(84091,84092,84093,6800,6800,6800,6800,6800,6800,6800));
-		eq.update_task_activity(220,9);
+		eq.update_task_activity(220,9,1);
 	end
 end
 

1aa9f2c6930d062a15adf0334fede1fd9f9f1458
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-11-16 15:59:17 +0000

    Change to only allow 1 corrupted shaman.

--- quests/lakerathe/#tainted_aquagoblin.pl
+++ quests/lakerathe/#tainted_aquagoblin.pl
@@ -1,12 +1,7 @@
-my $x;
-my $y;
-my $z;
-my $h;
-
 sub EVENT_DEATH_COMPLETE {
   my $x = $npc->GetX();
   my $y = $npc->GetY();
   my $z = $npc->GetZ();
   my $h = $npc->GetHeading();
-  quest::spawn2(51149,0,0,$x,$y,$z,$h);
+  quest::unique_spawn(51149,0,0,$x,$y,$z,$h);
 }

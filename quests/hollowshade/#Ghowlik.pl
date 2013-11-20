4101d8137153bf76d2889bbcff70554181e6b15e
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-11-20 01:38:21 +0000

    Change to perl comments.

--- quests/hollowshade/#Ghowlik.pl
+++ quests/hollowshade/#Ghowlik.pl
@@ -1,9 +1,9 @@
 #Sonic Wolf Leader in the East
 
 sub EVENT_DEATH_COMPLETE 
-	{
-	# leader is down - send signal after timer.
-  	//quest::ze(15,"The Sonic Wolf leader is dead, putting the Sonic Wolves in the East in a panic!");
-	quest::signalwith(166257, 3, 0);
-	}
+  {
+  # leader is down - send signal after timer.
+    #quest::ze(15,"The Sonic Wolf leader is dead, putting the Sonic Wolves in the East in a panic!");
+  quest::signalwith(166257, 3, 0);
+  }
 
--- quests/hollowshade/#a_grimling_chieftain.pl
+++ quests/hollowshade/#a_grimling_chieftain.pl
@@ -1,9 +1,9 @@
 #Griming Leader in the South
 
 sub EVENT_DEATH_COMPLETE 
-	{
-	# leader is down - send signal after timer.
-  	//quest::ze(15,"The Grimling leader is dead, putting the Grimlings in the South in a panic!");
-	quest::signalwith(166257, 4, 0);
-	}
+  {
+  # leader is down - send signal after timer.
+    #quest::ze(15,"The Grimling leader is dead, putting the Grimlings in the South in a panic!");
+  quest::signalwith(166257, 4, 0);
+  }
 

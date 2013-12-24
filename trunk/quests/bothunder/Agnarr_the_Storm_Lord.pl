c5ed3caa331441c1affb1116309e3e958369cbff
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-12-24 07:05:23 +0000

    Submission from Bobaski: Agnarr event rewrote

--- trunk/quests/bothunder/Agnarr_the_Storm_Lord.pl
+++ trunk/quests/bothunder/Agnarr_the_Storm_Lord.pl
@@ -1,11 +1,13 @@
 # NPCID: 209026 Agnarr_the_Storm_Lord
+
 sub EVENT_SPAWN {
-	quest::settimer(1,7200);
 	quest::setnexthpevent(76); #spawn first giant at 75%
 }
 
 sub EVENT_AGGRO {
-	quest::settimer(3,1);
+	quest::settimer(1,7200); #fail timer
+	quest::settimer(2,60); #check for aggro
+	quest::signalwith(209033,1,1); #portals start spawning
 }
 
 sub EVENT_HP {
@@ -21,43 +23,21 @@ sub EVENT_HP {
 }
 
 sub EVENT_TIMER {
-	if($timer == 1) {
+	if($timer == 1) { #took too long. fail
 		quest::stoptimer(1);
-		quest::spawn2(209026,0,0,$x,$y,$z,$h);
-		quest::signalwith(209033,2,1);
-		quest::depop();
-	} elsif($timer == 2) {
-		quest::signalwith(209033,1,1);
-		quest::stoptimer(1);
-		quest::stoptimer(2);
-		quest::settimer(1,7200);
-		quest::settimer(2,31);
-	} elsif($timer == 3) {
-		if($npc->IsEngaged()) {
-			quest::stoptimer(1);
-			quest::stoptimer(3);
-			quest::settimer(1,7200);
-			quest::settimer(2,31);
-			quest::settimer(4,60);
-			quest::signalwith(209033,1,1);
-		} else {
-			quest::signalwith(209033,2,1);
-			quest::stoptimer(3);
-		}
-	} elsif($timer == 4) {
+		quest::signalwith(209033,2,1); #portals stop spawning
+		quest::depop_withtimer();
+	} elsif ($timer == 2) {
 		if(!$npc->IsEngaged()) {
-			quest::signalwith(209033,2,1);
-			quest::stoptimer(4);
+			quest::signalwith(209033,2,1); #if not aggro'd, stop portals from spawning
 		}
 	}
+	
 }
 
 sub EVENT_DEATH_COMPLETE {
-	# spawn planar projection
-	quest::spawn2(202367,0,0,$x,$y,$z,$h);
-	# spawn Karana up the stairs
-	quest::spawn2(209108,0,0,-469,-1754,2351.2,197.6); 
+	quest::spawn2(209114,0,0,$x,$y,$z,$h); #Planar Projection
+	quest::spawn2(209108,0,0,-469,-1754,2351.2,197.6); #Karana upstairs
 	quest::stoptimer(1);
-	quest::stoptimer(2);
-	quest::signalwith(209033,2,1);
+	quest::signalwith(209033,2,1); #stop portals from spawning
 }
--- trunk/quests/bothunder/Emmerik_Skyfury.pl
+++ trunk/quests/bothunder/Emmerik_Skyfury.pl
@@ -1,22 +1,26 @@
-sub EVENT_DEATH_COMPLETE {
-quest::spawn2(209102,0,0,$x,$y,$z,$h);
-quest::signalwith(209034,2,1);
+#NPCID: Bastion of Thunder > Emmerik_Skyfury (209053)
+sub EVENT_AGGRO {
+	#tell A_storm_portal_ I have engaged
+	quest::signalwith(209034,1,1);
+	# create a timer to check if I have lost all aggro and need to reset the event
+	quest::settimer("emmerik_aggro_check",60);
 }
 
-sub EVENT_AGGRO {
-quest::signalwith(209034,1,1);
-quest::settimer(1,31);
+sub EVENT_DEATH_COMPLETE {
+	#spawn #Askr_the_Lost_ to port players to the next floor
+	quest::spawn2(209102,0,0,$x,$y,$z,$h); 
+	#tell the A_storm_portal_ I have died
+	quest::signalwith(209034,2,1); 
 }
 
 sub EVENT_TIMER {
-
-quest::signalwith(209034,1,1);
-
-}
-
-
-sub_EVENT_DEATH_COMPLETE {
-
-quest::stoptimer(1);
-
+	if ($timer == "emmerik_aggro_check") {
+		if(!$npc->IsEngaged()) {
+			# if I have no aggro stop the portals from spawning adds
+			quest::signalwith(209034,2,1); 
+			#depop the portal adds
+			quest::depopall(209103);
+			quest::stoptimer("emmerik_aggro_check");
+		}
+	}
 }

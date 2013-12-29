ca352eefde69bfebad58be6f213d8daa0b64f2eb
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2013-12-29 23:01:20 +0000

    change the level requirement for Dain's head turn ins.

--- trunk/quests/kael/King_Tormax.pl
+++ trunk/quests/kael/King_Tormax.pl
@@ -16,7 +16,7 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  if ($faction == 1) { # Require ally faction
+  if ($faction < 3) { # Require kindly or better faction
     if (plugin::check_handin(\%itemcount, 30501 => 1)) { ## Dain head
       quest::summonitem(25858);
       quest::faction(49,-100); # -coldain

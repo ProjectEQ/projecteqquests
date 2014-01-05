a38ed13e491849d06360825c138f28bec37e8601
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-05 06:40:47 +0000

    Some code tweaks in case troubleshooting is required.

--- trunk/quests/kodtaz/Pixtt_Grand_Summoner.pl
+++ trunk/quests/kodtaz/Pixtt_Grand_Summoner.pl
@@ -1,6 +1,7 @@
 sub EVENT_SPAWN {
   $npc->SetHP(100000);
 }
+
 sub EVENT_DEATH_COMPLETE {
-  quest::signalwith(293218,1);
+  quest::signalwith(293218,1,0);
 }
--- trunk/quests/kodtaz/Pixtt_High_Summoner.pl
+++ trunk/quests/kodtaz/Pixtt_High_Summoner.pl
@@ -1,24 +1,20 @@
 sub EVENT_AGGRO {
   quest::settimer(1,60);
 }
+
 sub EVENT_TIMER {
-  {
-   quest::stoptimer(1);
-   quest::settimer(1,3);
-      $named_up = 0;
-	}
-$check_named = $entity_list->GetMobByNpcTypeID(293120);
-$check_named = $entity_list->GetMobByNpcTypeID(293213);
-	if ($check_named) {
-		$named_up = 1;
+  quest::stoptimer(1);
+  quest::settimer(1,3);
+  $check_1 = $entity_list->GetMobByNpcTypeID(293120);
+  $check_2 = $entity_list->GetMobByNpcTypeID(293213);
+  if (!$check_1 && !$check_2) {
+    quest::spawn2(293212,0,0,$x,$y,$z,$h);
+    quest::signalwith(293218,0,0);
+    quest::stoptimer(1);
+    quest::depop_withtimer();
+  }
 }
-if ($timer == 1 && $named_up == 0)  {
-   quest::spawn2(293212,0,0,$x,$y,$z,$h);
-   quest::depop_withtimer();
-   quest::signalwith(293218,0);
-   quest::stoptimer(1);
-}
-}
+
 sub EVENT_DEATH_COMPLETE {
   quest::spawn2(293211,0,0,$x,$y,$z,$h);
 }

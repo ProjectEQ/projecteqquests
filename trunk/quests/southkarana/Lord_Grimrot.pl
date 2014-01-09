a07043c113c59022cc990c9ca47597214ea44038
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-09 05:41:01 +0000

    finished converts to lua for south karana

--- trunk/quests/southkarana/Lord_Grimrot.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Welcome to my field of decay. Won't you be so kind as to attack me? I have need of more bodies to join my diseased legion. Try it. maybe you shall win and gain my trusty [Pestilence].");
-}
-if($text=~/what pestilence/i){
-quest::say("My beloved scythe. Upon my death. my soul shall live withn her.  Such is the pact."); }
-}
-#END of FILE Zone:southkarana  ID:3312 -- Lord_Grimrot 
-
--- trunk/quests/southkarana/player.pl
+++
@@ -1,9 +0,0 @@
-#BeginFile: southkarana\player.pl
-#Quest file for South Karana: Paladin message during Necromancer Epic 1.5 (Soulwhisper)
-
-sub EVENT_LOOT {
-  if(($looted_id == 22892) && plugin::check_hasitem($client, 26896) && plugin::check_hasitem($client, 14344) && plugin::check_hasitem($client, 11430)) { #All 4 Paladin Heads
-    $client->Message(15,"With his last breath, the paladin says, 'You are too late. The last paladin has fled to Natimbi with the staff and is on his way to destroy it!'");
-  }
-}
-#EndFile: southkarana\player.pl

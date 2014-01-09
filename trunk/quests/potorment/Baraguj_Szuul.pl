b0ab9f994d31e65ccf9f431897d2538a803d7245
RSchmertz@gmail.com <RSchmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-09 18:38:35 +0000

    Add step to stomach event.

--- trunk/quests/potorment/Baraguj_Szuul.pl
+++ trunk/quests/potorment/Baraguj_Szuul.pl
@@ -1,39 +1,17 @@
-my $raid = undef;
-my $group = undef;
-my $pc = undef;
-my $count = undef;
 
-sub EVENT_ATTACK {
-	$raid = $entity_list->GetRaidByClient($client);
-	$group = $entity_list->GetGroupByClient($client);
-	if ($raid) {
-		for ($count = 0; $count < $raid->RaidCount(); $count++) {
-			$pc = $raid->GetMember($count);
-			#distance restriction so the members need to be reasonably close.
-			if ($pc->CalculateDistance($npc->GetX(),$npc->GetY(),$npc->GetZ()) <= 100) {
-				$pc->MovePC(207,-1094,910,-746,0);
-			}
+sub EVENT_COMBAT {
+	my @clientlist = $entity_list->GetClientList();
+	foreach $ent (@clientlist) {
+		#distance restriction so the members need to be reasonably close.
+		if ($ent->CalculateDistance($npc->GetX(),$npc->GetY(),$npc->GetZ()) <= 100) {
+		$ent->MovePC(207,-1094,910,-746,0);
 		}
 	}
-	elsif ($group) {
-		for ($count = 0; $count < $group->GroupCount(); $count++) {
-			$pc = $group->GetMember($count);
-			#distance restriction so the members need to be reasonably close.
-			if ($pc->CalculateDistance($npc->GetX(),$npc->GetY(),$npc->GetZ()) <= 100) {
-				$pc->MovePC(207,-1094,910,-746,0);
-			}
-		}
-	}
-	else {
-		#If player is not grouped or in raid, just move the one.
-		quest::movepc(207,-1094,910,-746);
-	}
-	# 207031 (An Unimaginable Horror) will depop after 30 minutes.
-	$client->Message(15,"You have 30 minutes");
 	#signal mouth_trigger that spawns all the mobs in stomach
 	quest::signal(207071);
+	quest::settimer(1,5);
+}
+
+sub EVENT_TIMER {
 	quest::depop_withtimer();
-	#update spawn timer after depop or mob will repop when zone resets
-	#quest::updatespawntimer(42246,172800000); #no variance
-	quest::updatespawntimer(42246,(int(rand(34560))+155520)*1000); #respawn with variance
 }

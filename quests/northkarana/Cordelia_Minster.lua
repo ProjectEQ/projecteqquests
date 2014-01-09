f28a95c64708382dc5fd910087e6996a4c2bea72
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-09 03:40:40 +0000

    rewrote the interrogator badge quest and added missing texts for unkempt druids / winds of karana quests.

--- quests/northkarana/Cordelia_Minster.lua
+++ quests/northkarana/Cordelia_Minster.lua
@@ -11,9 +11,9 @@ end
 
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("I can not find my flute, my favorite possesion. Is there anyone who cam [help] me?");
-	elseif(e.message:findi("help")) then
-		e.self:Say("My flute is missing, I think that old dirty hermit took off with it. He is always taking off with my things.");
+		e.self:Say("Greetings!  I am Cordelia, a traveling piper. I am afraid I cannot play a tune for you, however, as my [flute] is gone.");
+	elseif(e.message:findi("flute")) then
+		e.self:Say("I traded my flute to a hermit in the southern plains of Karana. I had a spare flute, but that was taken from me by some bandits. If you could find this hermit and ask him for my flute back, I would be most appreciative.");
 	end
 end
 
@@ -21,7 +21,7 @@ function event_trade(e)
 	local item_lib = require("items");
 
 	if(item_lib.check_turn_in(e.trade, {item1 = 13310})) then -- A cracked Flute
-		e.self:Say("Ohh!! I see you got my flute back. For that I can not be thankful enough but while sitting here I spotted what looks to be a sheet of music but it looks incomplete. Maybe you can find out who it belongs to.");
+		e.self:Say("Why thank you, kind adventurer! Here is a little something to keep food in your belly. Now back to practice. La la la..");
 		e.other:SummonItem(13119); -- Winds of Karana sheet 2
 		e.other:Ding();
 		e.other:Faction(135,2,0);
@@ -30,6 +30,7 @@ function event_trade(e)
 		e.other:Faction(207,-5,0);
 		e.other:Faction(273,-5,0);
 		e.other:AddEXP(1000);
+		e.other:GiveCash(0,2,0,0);
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end

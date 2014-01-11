c7ae31ab3a9b61fa66821fd4a21ccaf043cc4ec3
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-11 19:12:46 +0000

    Magus' converted to lua and added say_link

---
+++ quests/nro/Magus_Arindri.lua
@@ -0,0 +1,31 @@
+-- nro\Magus_Arindri.lua NPCID 34129
+
+function event_say(e)
+	--Adventurers Stone
+	if(e.other:KeyRingCheck(41000) or e.other:HasItem(41000)) then
+		if(e.message:findi("hail")) then
+			e.self:Say("You endured the burning heat of the desert to come and use our magic!  I'm so excited.  We have been getting a lot of customers.  We've only lost a few.  I sometimes wonder if Vayzl has incorrectly used the spell on purpose to see the odd traveler explode into a spray of magic.  I do respect her, but I just don't understand dark elves, I guess.  I am far too precise and careful to make any mistakes... Well, not a second time.  Tell me where you would like to go and I will send you there.  I can send you to any of the other camps in [" .. eq.say_link("Butcherblock",false,"Butcherblock") .. "], [" .. eq.say_link("Commonlands",false,"Commonlands") .. "], [" .. eq.say_link("Everfrost",false,"Everfrost") .. "], [" .. eq.say_link("Nedaria's Landing",false,"Nedaria's Landing") .. "], or [" .. eq.say_link("South Ro",false,"South Ro") .. "].");
+		elseif(e.message:findi("commonlands")) then
+			e.self:CastSpell(4176,e.other:GetID(),0,1);
+		elseif(e.message:findi("everfrost")) then
+			e.self:CastSpell(4180,e.other:GetID(),0,1);
+		elseif(e.message:findi("nedaria")) then
+			e.self:CastSpell(4580,e.other:GetID(),0,1);
+		elseif(e.message:findi("butcherblock")) then
+			e.self:CastSpell(4179,e.other:GetID(),0,1);
+		elseif(e.message:findi("south ro")) then
+			e.self:CastSpell(4178,e.other:GetID(),0,1);
+		end
+	else --no Adventurers Stone
+		if(e.message:findi("hail")) then
+			e.self:Say("You will have to excuse me, I am quite busy studying this Farstone and the possibility of using the magic stored inside of it.  Perhaps you should talk to those at the Wayfarer camps to see if they have any use for you.  I have enough time to send you to [" .. eq.say_link("Nedaria's Landing") .. "], if you'd like.");
+		elseif(e.message:findi("nedaria")) then
+			e.self:CastSpell(4580,e.other:GetID(),0,1);
+		end
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+	item_lib.return_items(e.self, e.other, e.trade);
+end
---
+++ quests/sro/Magus_Jerira.lua
@@ -0,0 +1,31 @@
+-- sro\Magus_Jerira.lua NPCID 35066
+
+function event_say(e)
+	--Adventurers Stone
+	if(e.other:KeyRingCheck(41000) or e.other:HasItem(41000)) then
+		if(e.message:findi("hail")) then
+			e.self:Say("Nice to see you, $name.  Am I safe in assuming you wish to travel today?  I do admire the adventurers that so willingly take the risk involved with this spell.  I enjoy knowing all of my hard work is not in vain.  There's only been a few unfortunate folk that haven't, uhm, been so lucky.  Frightful mess.  I'll be sure to be extra careful where you are concerned.  Honestly.  A dark elf's word is... Well, forget that.  He he.  Tell me where you would like to go and I will send you there.  I can send you to any of the other camps in [" .. eq.say_link("Butcherblock",false,"Butcherblock") .. "], [" .. eq.say_link("Commonlands",false,"Commonlands") .. "], [" .. eq.say_link("Everfrost",false,"Everfrost") .. "], [" .. eq.say_link("Nedaria's Landing",false,"Nedaria's Landing") .. "], or [" .. eq.say_link("North Ro",false,"North Ro") .. "].");
+		elseif(e.message:findi("commonlands")) then
+			e.self:CastSpell(4176,e.other:GetID(),0,1);
+		elseif(e.message:findi("everfrost")) then
+			e.self:CastSpell(4180,e.other:GetID(),0,1);
+		elseif(e.message:findi("nedaria")) then
+			e.self:CastSpell(4580,e.other:GetID(),0,1);
+		elseif(e.message:findi("north ro")) then
+			e.self:CastSpell(4177,e.other:GetID(),0,1);
+		elseif(e.message:findi("butcherblock")) then
+			e.self:CastSpell(4179,e.other:GetID(),0,1);
+		end
+	else --no Adventurers Stone
+		if(e.message:findi("hail")) then
+			e.self:Say("You will have to excuse me, I am quite busy studying this Farstone and the possibility of using the magic stored inside of it.  Perhaps you should talk to those at the Wayfarer camps to see if they have any use for you.  I have enough time to send you to [" .. eq.say_link("Nedaria's Landing") .. "], if you'd like.");
+		elseif(e.message:findi("nedaria")) then
+			e.self:CastSpell(4580,e.other:GetID(),0,1);
+		end
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+	item_lib.return_items(e.self, e.other, e.trade);
+end

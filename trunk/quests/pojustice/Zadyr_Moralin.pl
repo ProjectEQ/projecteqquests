1476770f38b291387a265bf8f725010921b57230
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-17 03:50:42 +0000

    Again more Guise of Horror updates.

--- trunk/quests/pojustice/Zadyr_Moralin.pl
+++ trunk/quests/pojustice/Zadyr_Moralin.pl
@@ -1,28 +1,30 @@
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("By the Prince, have you come to [free] me from this place?", 17);
-  }
-  if ($text=~/free/i) {
-    quest::say("I must find a way out of this prison, my trial is approaching, so says the warden. Time is running out for me. There is little chance of reprieve, given my [experiments].", 17);
-  }
-  if ($text=~/experiments/i) {
-    quest::say("My people are fleeing, fleeing underground away from the traitor-elves. We are taking to the tunnels and to the caverns, where we can rebuild our strength, beneath the surface. The Prince will protect our people. My experiments were dire indeed, and yet in times of war one must take every opportunity one can get to defeat one's foe, as I am certain that you would agree. I experimented upon the souls of my [slaves].", 17);
-  }
-  if ($text=~/slaves/i) {
-    quest::say("My House always kept dwarves; they are sturdy, hard-working and durable. It has always been the law of our House never to abuse the slaves, however. The war with the traitors lasted so long, and I saw an opportunity to end it. My research into the binding of mortal souls, and to use that harnessed spiritual power for further incantations, had proceeded to the point at which I required actual living subjects upon which to perform my [rituals].", 17);
-  }
-  if ($text=~/rituals/i) {
-    quest::say("The ritual of binding requires over two whole days of preparation, and the separation and captivation of the subject's soul takes a further two days of rigorous effort. I succeeded, but the price was high. Upon three of my slaves did I perform the rituals, and thrice damned am I for it. I wielded the power of their life's energies with decisiveness. My [victories] were the stuff of legends.", 17);
-  }
-  if ($text=~/victories/i) {
-    quest::say("Against the traitor-elves, my new-found energies from the life-force of my three slaves' trapped souls provided me with the ability to lay waste to whole legions of our enemies. Yet in their foolishness, my peers of the Prince's Elves sought to stop me, and bring me to [justice].", 17);
-  }
-  if ($text=~/justice/i) {
-    quest::say("They took me in my sleep, and for my 'crimes', I was banished here. Banished here like a criminal, rather than a savior of our species! Without my rituals, without my incantations, our people are doomed - they flee to the underworld and hide, like vermin. Even now, I sit here, accused, and were that not torment enough I am constantly reminded of my deeds by the souls of the three that I damned with me.", 17);
-  }
-  if ($text=~/i remember/i) {
-    if (($class eq "Rogue") || ($class eq "Bard")) {
-      quest::summonitem(11043); #Tears of the Forgotten
+  if ($langid == 17) { #Elder Teir'Dal
+    if ($text=~/hail/i) {
+      quest::say("By the Prince, have you come to [free] me from this place?", 17);
+    }
+    if ($text=~/free/i) {
+      quest::say("I must find a way out of this prison, my trial is approaching, so says the warden. Time is running out for me. There is little chance of reprieve, given my [experiments].", 17);
+    }
+    if ($text=~/experiments/i) {
+      quest::say("My people are fleeing, fleeing underground away from the traitor-elves. We are taking to the tunnels and to the caverns, where we can rebuild our strength, beneath the surface. The Prince will protect our people. My experiments were dire indeed, and yet in times of war one must take every opportunity one can get to defeat one's foe, as I am certain that you would agree. I experimented upon the souls of my [slaves].", 17);
+    }
+    if ($text=~/slaves/i) {
+      quest::say("My House always kept dwarves; they are sturdy, hard-working and durable. It has always been the law of our House never to abuse the slaves, however. The war with the traitors lasted so long, and I saw an opportunity to end it. My research into the binding of mortal souls, and to use that harnessed spiritual power for further incantations, had proceeded to the point at which I required actual living subjects upon which to perform my [rituals].", 17);
+    }
+    if ($text=~/rituals/i) {
+      quest::say("The ritual of binding requires over two whole days of preparation, and the separation and captivation of the subject's soul takes a further two days of rigorous effort. I succeeded, but the price was high. Upon three of my slaves did I perform the rituals, and thrice damned am I for it. I wielded the power of their life's energies with decisiveness. My [victories] were the stuff of legends.", 17);
+    }
+    if ($text=~/victories/i) {
+      quest::say("Against the traitor-elves, my new-found energies from the life-force of my three slaves' trapped souls provided me with the ability to lay waste to whole legions of our enemies. Yet in their foolishness, my peers of the Prince's Elves sought to stop me, and bring me to [justice].", 17);
+    }
+    if ($text=~/justice/i) {
+      quest::say("They took me in my sleep, and for my 'crimes', I was banished here. Banished here like a criminal, rather than a savior of our species! Without my rituals, without my incantations, our people are doomed - they flee to the underworld and hide, like vermin. Even now, I sit here, accused, and were that not torment enough I am constantly reminded of my deeds by the souls of the three that I damned with me.", 17);
+    }
+    if ($text=~/i remember/i) {
+      if (($class eq "Rogue") || ($class eq "Bard")) {
+        quest::summonitem(11043); #Tears of the Forgotten
+      }
     }
   }
 }
--- trunk/quests/rivervale/Shakey_Scarecrow.lua
+++ trunk/quests/rivervale/Shakey_Scarecrow.lua
@@ -7,12 +7,13 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
+    local class = e.other:GetClass();
 
 	if(item_lib.check_turn_in(e.trade, {item1 = 14321})) then
 		e.other:SummonItem(eq.ChooseRandom(13980, 13980, 14031));
 		e.other:Ding();
 		e.other:AddEXP(500);
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 11139})) then
+	elseif((class == 8 or class == 9) and item_lib.check_turn_in(e.trade, {item1 = 11139})) then
 		e.other:Message(0,"You remove the dilapidated pumpkin from Shakey's wooden neck post and replace it with the new one. A haunting voice rings in your ears");
 		e.self:Say("Who has given me a voice once again? I can sense your presence but cannot see your form. Allow me but a moment to explain. I have been trapped in this golem of hay for many years. Ever since the horrible bloodbath in the forest I have been bound to this horrible fate, and I cannot find rest until the person responsible for my murder has been dealt with! Our fates are intertwined, you and I. We can both find peace if you listen to what I have to say. Find the blue orb. It yet retains its dark power. Go into the woods and seek out the one with a fragment.");
 		e.other:SummonItem(52355,2); -- Dilapidated Head of Shakey

11db8980c7d1cecb7ce7f27dc6f811e3af9fe45e
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-22 19:28:10 +0000

    Tagrin mechanics modified.

--- trunk/quests/potactics/Animated_Decorin_Blade.pl
+++ trunk/quests/potactics/Animated_Decorin_Blade.pl
@@ -1,27 +1,31 @@
 #Five Animated Decorin Blades spawn when Tagrin Maldric is aggro'd. This quest file will depop them if they aren't aggro'd for fifteen minutes after spawning.
 
 sub EVENT_SPAWN {
-  my $x = $npc->GetX();
-  my $y = $npc->GetY();
-  my $z = $npc->GetZ();
-  my $h = $npc->GetHeading();
-  quest::settimer("depop",900);
+	my $x = $npc->GetX();
+	my $y = $npc->GetY();
+	my $z = $npc->GetZ();
+	my $h = $npc->GetHeading();
+	quest::settimer("depop",900);
 }
 
-sub EVENT_AGGRO {
-  quest::stoptimer("depop");
+sub EVENT_COMBAT { 
+	if ($combat_state == 1) {
+		quest::stoptimer("depop");
+	} else {
+		quest::settimer("depop",900)
+	}
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::stoptimer("depop");
-  if($entity_list->GetMobByNpcTypeID(214054)) {
-    quest::spawn2(214119,0,0,$x,$y,$z,$h);
-  }
+	quest::stoptimer("depop");
+	if($entity_list->GetMobByNpcTypeID(214054)) {
+		quest::spawn2(214119,0,0,$x,$y,$z,$h);
+	}
 }
 
 sub EVENT_TIMER {
-  if ($timer eq "depop") {
-    quest::stoptimer("depop");
-    quest::depop();
-  }
+	if ($timer eq "depop") {
+		quest::stoptimer("depop");
+		quest::depop();
+	 }
 }

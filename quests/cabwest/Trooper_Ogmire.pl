2f826435ad9fc1588d3778e0a6f117d5b1aab938
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-23 03:46:17 +0000

    Continued work on Restraining Order quest.

--- quests/cabwest/Trooper_Ogmire.pl
+++ quests/cabwest/Trooper_Ogmire.pl
@@ -1,6 +1,18 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Greetings. citizen.  Looking for healing or other such services?  If so. then I urge you to seek out the Temple of Terror.  The hierophants will see to it for you."); }
+sub EVENT_SAY {
+  if ($text=~/hail/i) { #Did not respond to hail on live 01/22/2014
+    quest::say("Greetings, citizen. Looking for healing or other such services? If so, then I urge you to seek out the Temple of Terror. The hierophants will see to it for you.");
+  }
+  if ($text=~/sign the restraining order/i) {
+    quest::say("I knew my wicked ways would eventually come to an end. Very well, then. Hand me the legion order and I shall sign it.");
+  }
 }
-#END of FILE Zone:cabwest  ID:5133 -- Trooper_Ogmire 
 
+sub EVENT_ITEM {
+  if (plugin::check_handin(\%itemcount, 18245 => 1)) { #Legion Order (5 signed)
+    quest::emote("signs the order and hands it back to you.");
+    quest::summonitem(18246); #Legion Order (6 signed)
+    quest::exp(200);
+  }
+  plugin::return_items(\%itemcount);
+}
+#END of FILE Zone:cabwest  ID:5133 -- Trooper_Ogmire

c2c30783cf91ccf76bd7dd69d949cef66550f145
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-31 00:31:07 +0000

    update from peqmacquest and included say_link from a few scripts.  credit to speedz for converting to lua.

---
+++ quests/innothuleb/Lynuga.lua
@@ -0,0 +1,24 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Hrrump?! Who you? " .. e.other:GetName() .. "? What you want with Lynuga? Me am out [" .. eq.say_link("collecting",false,"collecting") .. "]. Leave me be!");
+	elseif(e.message:findi("collecting")) then
+		e.self:Say("I collect all sort's of thing's. Gem's be my favorite. Me really like rubies. If you have ruby, give to me! Why you want to know? You want to steal my collection?!? I squish you if you do!!");
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+
+	if(item_lib.check_turn_in(e.trade, {item1 = 10035})) then -- Handin: Ruby
+		e.self:Say("Mmm. Ruby!! Me thank's you! Here take this, me got it off dead someone who try take my collection. Me think's this valuable thing..");
+		e.other:SummonItem(eq.ChooseRandom(10080, 10081, 10082)); -- Random: Brutechopper, Midnight Mallet, Ivandyr's Hoop
+		e.other:Ding();
+		e.other:Faction(22,5,0);
+		e.other:Faction(292,-5,0);
+		e.other:Faction(66,-5,0);
+		e.other:AddEXP(500);
+	end
+	item_lib.return_items(e.self, e.other, e.trade)
+end
+
+-- EOF
---
+++ quests/oasis/a_Dervish_Thug.lua
@@ -0,0 +1,7 @@
+-- Converted to .lua by Speedz
+
+function event_combat(e)
+	if(e.joined == true) then
+		e.self:Say(string.format("Time to die %s!",e.other:GetCleanName()));
+	end
+end

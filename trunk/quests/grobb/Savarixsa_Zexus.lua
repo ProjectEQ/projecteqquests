5997c49adce76bd8c3f00e92964cc608120eed11
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-01-31 05:00:34 +0000

    updated from peqmacquest and added saylink,

---
+++ trunk/quests/grobb/Savarixsa_Zexus.lua
@@ -0,0 +1,9 @@
+-- Converted to .lua by Speedz
+
+function event_say(e)
+	if(e.message:findi("Hail")) then
+		e.self:Say("Salutations. " .. e.other:GetName() .. "! I am here as a favor to my brother. High Priest Perrir. We seek loyal followers of Innoruuk to earn their place among the hierarchy of the Spires. Are you [" .. eq.say_link("devoted to Innoruuk",false,"devoted to Innoruuk") .. "]?");
+	elseif(e.message:findi("devoted to innoruuk")) then
+		e.self:Say("My brother requests your presence in Neriak at the Spires of Innoruuk. Take this note with you. " .. e.other:GetName() .. ". It will inform Perrir of who sent you.");
+	end
+end

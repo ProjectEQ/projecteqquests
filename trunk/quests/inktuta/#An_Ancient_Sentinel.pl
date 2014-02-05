bfe12a29f8191056dfae0d86d6bd185ad403ab75
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-02-05 05:07:43 +0000

    opening inktuta w/o flags

--- trunk/quests/inktuta/#An_Ancient_Sentinel.pl
+++ trunk/quests/inktuta/#An_Ancient_Sentinel.pl
@@ -1,7 +1,7 @@
 sub EVENT_SAY {
   if (($text=~/hail/i) && (!defined $qglobals{ink_final})) {
     $client->Message(4,"The sentinel nods solemnly and reaches toward you with a closed palm. His clenched fist opens and an unusual splinter of stone falls into your possession.");
-    quest::summonitem(60253);
-    quest::setglobal("ink_final",1,0,"F");
+    #quest::summonitem(60253); add back when zone is full live
+    #quest::setglobal("ink_final",1,0,"F");
   }
 }

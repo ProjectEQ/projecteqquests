0641a9c62e2afff485a67c981b6c68180a3a4d75
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-02-13 09:49:45 +0000

    No idiots

--- quests/inktuta/#Nijacka_the_Scourge.pl
+++ quests/inktuta/#Nijacka_the_Scourge.pl
@@ -1,4 +1,11 @@
+my $instid;
+
+sub EVENT_AGGRO {
+	$instid = quest::GetInstanceID('inktuta',0); #get the instance id
+}
+	
 sub EVENT_DEATH_COMPLETE {
-  quest::emote("grasps at his throat as he chokes out a few final words");
-  quest::say("I foresaw this. My death. Our deaths. We are all going to perish!");
+	quest::crosszonemessageplayerbyname(5, "GMBobaski", "Inktuta Nijacka Dead. InstID: $instid");
+	quest::emote("grasps at his throat as he chokes out a few final words");
+	quest::say("I foresaw this. My death. Our deaths. We are all going to perish!");
 }

a4b53cacd02ad0e42d752bd8d975f7d5dd13096f
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-02-14 03:06:28 +0000

    Some additions and cleanup.

--- trunk/quests/firiona/Lenka_Stoutheart.pl
+++ trunk/quests/firiona/Lenka_Stoutheart.pl
@@ -1,6 +1,10 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("We're just kidding with you. kid. Heh.  Although they are making heroes kinda small these days.  Hey. you [want to see something interesting]?"); }
+sub EVENT_SAY {
+  if ($text=~/hail/i) {
+    quest::say("We're just kidding with you, kid. Heh. Although they are making heroes kinda small these days. Hey, you [want to see something interesting]?");
+  }
 }
-#END of FILE Zone:firiona  ID:84122 -- Lenka_Stoutheart 
 
+sub EVENT_ITEM {
+  plugin::return_items(\%itemcount);
+}
+#END of FILE Zone:firiona  ID:84122 -- Lenka_Stoutheart
--- trunk/quests/firiona/Tracker_Azeal.pl
+++ trunk/quests/firiona/Tracker_Azeal.pl
@@ -5,23 +5,24 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote("looks over at you and gives you an awkward smile, then quickly looks away. You notice he isn't like most of the elves you've seen. You can't quite put your finger on it, but for one thing he smells like he hasn't bathed in weeks.");
- }
+  if ($text=~/hail/i) {
+   quest::emote("looks over at you and gives you an awkward smile, then quickly looks away. You notice he isn't like most of the elves you've seen. You can't quite put your finger on it, but for one thing he smells like he hasn't bathed in weeks.");
+  }
 }
 
 sub EVENT_TIMER {
- if ($timer eq "ZoneTime") {
-  if ($zonehour == 21) {
-   my $x = $npc->GetX();
-   my $y = $npc->GetY();
-   my $z = $npc->GetZ();
-   my $h = $npc->GetHeading();
-   quest::spawn2(84028,0,0,$x,$y,$z,$h);
-   quest::stoptimer("ZoneTime");
-   quest::depop_withtimer();
+  if (($timer eq "ZoneTime") && ($zonehour == 21)) {
+    my $x = $npc->GetX();
+    my $y = $npc->GetY();
+    my $z = $npc->GetZ();
+    my $h = $npc->GetHeading();
+    quest::spawn2(84028,0,0,$x,$y,$z,$h);
+    quest::stoptimer("ZoneTime");
+    quest::depop_withtimer();
   }
- }
 }
 
+sub EVENT_ITEM {
+  plugin::return_items(\%itemcount);
+}
 #Submitted by Jim Mills

75a1b519a8a97eeb3a9b284c032c7578b84e4e4f
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-02-25 16:14:06 +0000

    fix for doors not opening from signal not working anymore with entity list checks.

--- quests/ikkinz/#Trigger_Ikkinz_1.pl
+++ quests/ikkinz/#Trigger_Ikkinz_1.pl
@@ -1,16 +1,25 @@
 sub EVENT_SIGNAL {
-  if($signal == 1) {
-    if(!$entity_list->GetMobByNpcTypeID(294574) && !$entity_list->GetMobByNpcTypeID(294338)) { #Priest of Righteousness
-      $entity_list->FindDoor(2)->SetLockPick(0);
-      $entity_list->FindDoor(3)->SetLockPick(0);
-      $entity_list->FindDoor(4)->SetLockPick(0);
-      $entity_list->FindDoor(5)->SetLockPick(0);
+	if($signal == 1) {
+		quest::settimer(1,10);
+	} elsif($signal == 2) {
+		quest::settimer(2,10);
+	}
+}
+
+sub EVENT_TIMER {
+	if($timer == 1) {
+		if(!$entity_list->GetMobByNpcTypeID(294574) && !$entity_list->GetMobByNpcTypeID(294338)) { #Priest of Righteousness
+			quest::stoptimer(1);
+			$entity_list->FindDoor(2)->SetLockPick(0);
+			$entity_list->FindDoor(3)->SetLockPick(0);
+			$entity_list->FindDoor(4)->SetLockPick(0);
+			$entity_list->FindDoor(5)->SetLockPick(0);
+		}
+    } elsif($timer == 2) {
+		if(!$entity_list->GetMobByNpcTypeID(294339) && !$entity_list->GetMobByNpcTypeID(294575)) { #Defender of Righteousness
+			quest::stoptimer(2);
+			$entity_list->FindDoor(6)->SetLockPick(0);
+			$entity_list->FindDoor(7)->SetLockPick(0);
+		}
     }
-  }
-  if($signal == 2) {
-    if(!$entity_list->GetMobByNpcTypeID(294339) && !$entity_list->GetMobByNpcTypeID(294575)) { #Defender of Righteousness
-      $entity_list->FindDoor(6)->SetLockPick(0);
-      $entity_list->FindDoor(7)->SetLockPick(0);
-    }
-  }
 }

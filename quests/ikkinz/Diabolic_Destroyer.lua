fd5c97bb84634b37e320f8a03e2f44864cbe4518
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-02-28 15:48:33 +0000

    changed to event_combat and added handling to reset event.

--- quests/ikkinz/Diabolic_Destroyer.lua
+++ quests/ikkinz/Diabolic_Destroyer.lua
@@ -2,8 +2,7 @@
 -- Diabolic_Destroyer NPCID: 294043
 
 function event_spawn(e)
-	eq.depop_all(294137);
-	eq.set_next_hp_event(76);
+	set_event_start();
 end
 
 function event_hp(e)
@@ -22,11 +21,18 @@ function event_hp(e)
 	end
 end
 
-function event_aggro(e)
+function event_combat(e)
+	if (e.joined) then
 	-- /loc updated from MySEQ capture 2014/01/26
 	eq.spawn2(294137,0,0,418.5914,-179.3205,10.502,192);
 	eq.spawn2(294137,0,0,418.5914,-94.3507,10.502,192);
 	eq.spawn2(294137,0,0,379.2154,-186.3592,1.502002,256);
+	else
+		if (e.self:GetHP() < e.self:GetMaxHP()) then
+			e.self:SetHP(e.self:GetMaxHP());
+		end
+		set_event_start();
+	end
  end
  
 function event_death_complete(e)
@@ -34,4 +40,9 @@ function event_death_complete(e)
 	eq.depop_all(294137);
 	-- spawn the Cruel_Illusion
 	eq.spawn2(294136,0,0,442,-141,11,192);
+end
+
+function set_event_start()
+	eq.depop_all(294137);
+	eq.set_next_hp_event(76);
 end

e7851823842ec97a7d9e383f14b4b1849ecd68ea
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-01 06:57:47 +0000

    Cleaning up nasty scripts and began conversion to Lua while I was at it.

---
+++ quests/templeveeshan/A_deafening_roar.lua
@@ -0,0 +1,11 @@
+-- A_deafening_roar NPCID: 124135
+function event_spawn(e)
+	local ex = e.self:GetX();
+	local ey = e.self:GetY();
+	local ez = e.self:GetZ();
+	eq.set_proximity(ez - 30, ex + 30, ey - 30, ey + 30, ez - 30, ez + 30);
+end
+
+function event_enter(e)
+	e.self:Emote("fills the halls.");
+end

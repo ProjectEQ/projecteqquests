250f07b0f588978e9081d791a67d91902d446613
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-01 17:02:42 +0000

    veeshantemple cleaning up nasty scripts and more conversion to Lua while I was at it.

---
+++ trunk/quests/templeveeshan/Gra-Vloren.lua
@@ -0,0 +1,15 @@
+-- Gra`Vloren NPCID: 124091
+function event_combat(e)
+	if (e.joined) then
+		-- grab the entity list
+		local entity_list = eq.get_entity_list();
+		-- also aggro #Lady_Mirenilla (124077) and Wel`Wnas (124092) if they are up
+		local npc_table = {124077,124092};
+		for k,v in pairs(npc_table) do
+			local npc = entity_list:GetMobByNpcTypeID(v);
+			if (npc.valid) then
+				npc:AddToHateList(e.other,1);
+			end
+		end
+	end
+end

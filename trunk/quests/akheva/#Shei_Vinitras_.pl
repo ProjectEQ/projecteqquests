bc223af4552d06664f9b84c751062d6fbaa7a4d1
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-01 16:50:24 +0000

    akheva scripts converted to lua provided by Stryd's conversion tool

--- trunk/quests/akheva/#Shei_Vinitras_.pl
+++
@@ -1,8 +0,0 @@
-#This version of Shei is a simple non-aggro merchant. When killed, the true version of Shei will spawn.
-
-sub EVENT_DEATH_COMPLETE {
- quest::spawn2(179032,0,0,-1736,1082,22.6,63);
-}
-
-#Submitted by Jim Mills
-#Modified by Reno

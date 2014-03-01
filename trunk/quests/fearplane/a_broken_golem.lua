1da28724a9f6dd418013c487d3a534f09aba2530
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-01 03:10:03 +0000

    fixed the broken golem script.

--- trunk/quests/fearplane/a_broken_golem.lua
+++ trunk/quests/fearplane/a_broken_golem.lua
@@ -1,6 +1,7 @@
 function event_trade(e)
 	local item_lib = require("items");
-	-- [FLAG: Manual conversion needed: $itemcount{] if($itemcount{14319} == 1) then{
+	
+	if(item_lib.check_turn_in(e.trade, {item1 = 14319})) then
 		e.self:Say("Error! Malfunction! Destroy!");
 		e.self:Say("A " .. e.other:Class() .. " like you always bring out the worst in me.");
 		eq.spawn2(72106,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());

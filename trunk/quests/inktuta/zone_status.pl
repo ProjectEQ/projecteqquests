80cff84248759e68e653b80ae5d34c3acdf87401
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-07 00:10:52 +0000

    Different pile of bones for curse callers

--- trunk/quests/inktuta/zone_status.pl
+++ trunk/quests/inktuta/zone_status.pl
@@ -46,7 +46,7 @@ sub EVENT_SIGNAL {
 		quest::ze(15,"The sound of moving gears and grinding stone reverberates throughout the temple. A door has been unlocked.");
 		$entity_list->FindDoor(42)->SetLockPick(0);
 		$entity_list->FindDoor(43)->SetLockPick(0);
-		quest::spawn2(296073,0,0,-198,-908,-126,0); #a_pile_of_bones_
+		quest::spawn2(296072,0,0,-198,-908,-126,0); #a_pile_of_bones
 		quest::spawn2(296075,0,0,-79,-635,-126,0); #noqufiel_trigger	
 	#Noqufiel win
 	} elsif ($signal == 296065) { 

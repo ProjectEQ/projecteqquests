7fd6867cf536da7198bca6ae27efe064691c8357
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-07 21:35:03 +0000

    moved function

--- trunk/quests/uqua/#Specter_of_Rage.pl
+++ trunk/quests/uqua/#Specter_of_Rage.pl
@@ -3,7 +3,6 @@ my $instid = 0;
 sub EVENT_ITEM {
 	$instid = quest::GetInstanceID('uqua',0);
 	if (plugin::check_handin(\%itemcount, 67705 => 1)) {
-		quest::settimer(1,30);
 		quest::signalwith(292008,$instid);
 	}
 }

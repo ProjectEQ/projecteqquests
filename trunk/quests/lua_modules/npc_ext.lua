83ddf26b22a479c53053dd5f04c3e02f2b8bfc77
cavedude00@gmail.com <cavedude00@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-10 20:44:01 +0000

    Converted RandomRoam to lua, and a couple of NPCs in crescent to use it.

--- trunk/quests/lua_modules/npc_ext.lua
+++ trunk/quests/lua_modules/npc_ext.lua
@@ -22,3 +22,39 @@ function NPC:CountHateList(cond)
 	
 	return ret;
 end
+
+function NPC:RandomRoam(maxx,maxy,maxz,los)
+	maxz = maxz or 15;
+	los = los or 5;
+
+	if(self:IsEngaged() ~= 1) then
+		local curx = self:GetX();
+		local cury = self:GetY();
+		local origx = self:GetSpawnPointX();
+		local origy = self:GetSpawnPointY();
+		local origz = self:GetSpawnPointZ();
+		local guardx = self:GetGuardPointX();
+		local guardy = self:GetGuardPointY();
+
+		if(curx == guardx and cury == guardy) then
+			local randomx = math.random(maxx) + 1;
+			local randomy = math.random(maxy) + 1;
+			local posx = origx + randomx;
+			local posy = origy + randomy;
+			local negx = origx - randomx;
+			local negy = origy - randomy;
+			local newx = math.random(posx,negx);
+			local newy = math.random(posy,negy);
+			local newz = self:FindGroundZ(newx,newy, 5) + 1;
+
+			if(newz > -999999 and origz > (newz - maxz + 1) and origz < (newz + maxz - 1)) then
+				local loscheck = self:CheckLoSToLoc(newx, newy, newz, los);
+				if(loscheck) then
+					self:MoveTo(newx,newy,newz,-1,true);
+				end
+			end
+		end
+	end
+end
+			
+			

ba5c277b4996dfa944bbabcf50b00511b58b3ab1
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-16 22:33:35 +0000

    declaration fix

--- quests/inktuta/lockout_inktuta.lua
+++ quests/inktuta/lockout_inktuta.lua
@@ -1,9 +1,13 @@
 -- Temp use for setting lockout
 -- lockout_inktuta NPCID: 296076
+
+local instance_id;
+local charid_list;
+
 function event_spawn(e)
 	-- get the zone instance id
-	local instance_id = eq.get_zone_instance_id();
-	local charid_list = eq.get_characters_in_instance(instance_id);
+	instance_id = eq.get_zone_instance_id();
+	charid_list = eq.get_characters_in_instance(instance_id);
 	eq.signal(296070, instance_id);
 	
 end

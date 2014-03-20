e2b5c910e5cf91d2e8fc1735f1ca7e2f89035521
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-20 04:17:13 +0000

    Removed qglobal from Quillmane logic.

--- trunk/quests/southkarana/an_elephant_calf.lua
+++ trunk/quests/southkarana/an_elephant_calf.lua
@@ -1,25 +1,19 @@
--- #Quillmane spawn
--- #npc - #a_lioness - randomly spawns 1 of 3 Quilmane locs around the zone)
--- #zone - SouthKarana
--- #Angelox
--- # Updated by Kilelen
--- # 2nd Updated and converted to Lua by robregen
-
+-- an_elephant_calf NPCID: 14013, 14027, 14029, 14052
+-- #an_elephant_calf or #a_lioness or an_elephant_calf or a_lioness can potentially spawn Quillmane
 function event_death_complete(e)
-	local random_result = math.random(100);
+	--#Quillmane (14139)
 	local quillmane = 14139;
-	local qglobals = eq.get_qglobals(e.self);
-	
-	if(qglobals.quill == "2") then
+	-- Do nothing unless Quillmane is not spawned
+	if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(quillmane) == false)
+		-- percentile roll to determine if Quillmane will be spawned
+		local random_result = math.random(100);
+		-- He can spawn in 1 of 3 places.
 		if(random_result < 4) then
 			eq.spawn2(quillmane,101,0,3210,-6821.6,38.3,193.3);
-			eq.set_global("quill","3",3,"F");
 		elseif(random_result >= 4 and random_result < 8) then
 			eq.spawn2(quillmane,102,0,-2761.8,-6076,0.7,4.0);
-			eq.set_global("quill","3",3,"F");
 		elseif(random_result >= 8 and random_result < 12) then
 			eq.spawn2(quillmane,103,0,-2333.8,1296.5,38.7,247.3);
-			eq.set_global("quill","3",3,"F");
 		end
 	end
 end

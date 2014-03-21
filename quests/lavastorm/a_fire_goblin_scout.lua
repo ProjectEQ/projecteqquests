616842ddb87c71aac46992e57d27cff6ddfc0056
sorvani@gmail.com <sorvani@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-21 04:51:47 +0000

    updated with npcid

--- quests/lavastorm/a_fire_goblin_scout.lua
+++ quests/lavastorm/a_fire_goblin_scout.lua
@@ -1,10 +1,9 @@
--- a_fire_goblin_scout.lua NPCID: PENDING
--- 10% chance to spawn Scorchfist on death
+-- a_fire_goblin_scout.lua NPCID: 27126
+-- 10% chance to spawn #Scorchfist on death
 function event_death_complete(e)
 	if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(scorchfist)==false) then
 		if(math.random(100) < 10) then
-			-- pending NPCID
-			-- eq.spawn2(PENDING,101,0,820,1270,-77.9,193);
+			eq.spawn2(27126,101,0,820,1270,-77.9,193);
 		end
 	end
 end

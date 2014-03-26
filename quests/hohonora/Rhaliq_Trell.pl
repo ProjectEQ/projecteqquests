a75e7def802933712f933f7cca0a26688d94d95e
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-03-26 19:56:50 +0000

    Fix for lots of misused variables that were messing up the re-do on the trials

--- quests/hohonora/Rhaliq_Trell.pl
+++ quests/hohonora/Rhaliq_Trell.pl
@@ -1,36 +1,11 @@
 sub EVENT_SAY {
-		if($text=~/Hail/i && $villager == undef) {
+		if($text=~/Hail/i) {
 			quest::say("Weakling! How dare you approach me.  Access to Lord Marrs temple is reserved only for the honorable!  You will never be [ready]...");
 		}
 
-		if($text=~/ready/i && $villager == undef) {
+		if($text=~/ready/i) {
 			quest::say("Be warned, $name, if you believe you are ready, you will fail, even if you can kill Lord Marrs servants!");
-			$villager=1;
-			quest::settimer(1,14400);
 			quest::spawn2(211088,0,0,526,1375,-115,$h);
+			quest::depopwithtimer();
 		}
-
-                if($text=~/hail/i && $flag == 1) {
-                        quest::say("Congratulations... Two other trials and you may have proven yourself worthy to stand before Lord Marr.");
-                    	quest::setglobal("pop_hoh_trell", 1, 5, "F");
-	                $client->Message(4,"You receive a character flag!");
-                         }
-}
-
-sub EVENT_SIGNAL {
-if($signal == 8) {
-quest::settimer(9,600);
-quest::shout("Clever ones you are!");
-$flag=1;
-}}
-
-
-sub EVENT_TIMER {
-if($timer == 1) {
-$villager=undef;
-}
-
-if($timer == 9) {
-$flag=undef;
-}
 }
--- quests/hohonora/Trydan_Faye.pl
+++ quests/hohonora/Trydan_Faye.pl
@@ -1,19 +1,11 @@
 sub EVENT_SAY {
-		if($text=~/Hail/i && $dragon == undef) {
+		if($text=~/Hail/i) {
 			quest::say("Weakling! How dare you approach me.  Access to Lord Marrs temple is reserved only for the honorable!  You will never be [ready]...");
 		}
 
-		if($text=~/ready/ && $dragon == undef) {
+		if($text=~/ready/) {
 			quest::say("Be warned, $name, if you believe you are ready, you will fail, even if you can kill Lord Marrs servants!");
-			$dragon=1;
-			quest::settimer(1,7200);
-			quest::spawn2(211075,0,0,$x,$y,$z,$h);
+			quest::spawn2(211075,0,0,$x,$y,$z,$h); #Ryda`Dar
 			quest::depop_withtimer();
 		}
-}
-
-sub EVENT_TIMER {
-if($timer == 1) {
-$dragon=undef;
-}
 }

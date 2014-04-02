686f538faa8d8b0620b616b47c3283a1cc8044b4
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-04-02 02:38:41 +0000

    fixed Scribe Gurru and redo the south qeynos camp event to use spawn condition instead of timer.  Also first used of e.self:MerchantOpenShop() and e.self:MerchantCloseShop().

--- quests/qeynos/Gahna_Salbeen.lua
+++ quests/qeynos/Gahna_Salbeen.lua
@@ -1,21 +1,8 @@
-local count = 0;
-
-function event_spawn(e)
-	eq.set_timer("1",1000);
-end
-
-function event_timer(e)
-	count = count + 1;
-	local ztime = eq.get_zone_time();
-	if((count == 1) and (ztime.zone_time > 599 and ztime.zone_time < 1800)) then
-		eq.start(57);
-	elseif((count == 1) and (ztime.zone_time < 600 or ztime.zone_time > 1799)) then
+function event_signal(e)
+	if(e.signal == 1) then
 		eq.stop();
-		eq.move_to(-269,-24,3.75,131);
-	elseif(count == 2) then
-		count = 0;
-		eq.stop_timer("1")
-		eq.set_timer("1",30000);
+	elseif(e.signal == 2) then
+		eq.start(57);
 	end
 end
 
--- quests/qeynos/Solani_Dayadil.lua
+++ quests/qeynos/Solani_Dayadil.lua
@@ -1,22 +1,12 @@
-local count = 0;
-
 function event_spawn(e)
-	eq.set_timer("1",1000);
 	e.self:SetRunning(true);
 end
 
-function event_timer(e)
-	count = count + 1;
-	local ztime = eq.get_zone_time();
-	if((count == 1) and (ztime.zone_time > 599 and ztime.zone_time < 1800)) then
-		eq.start(59);
-	elseif((count == 1) and (ztime.zone_time < 600 or ztime.zone_time > 1799)) then
+function event_signal(e)
+	if(e.signal == 1) then
 		eq.stop();
-		eq.move_to(-260,-26,3.75,176);
-	elseif(count == 2) then
-		count = 0;
-		eq.stop_timer("1")
-		eq.set_timer("1",34000);
+	elseif(e.signal == 2) then
+		eq.start(59);		
 	end
 end
 

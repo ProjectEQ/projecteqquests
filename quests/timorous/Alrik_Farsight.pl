2599b15f1b38e982453f0985ebf468fea74452b8
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-04-13 05:35:22 +0000

    correct the text in event_say for Alrik Farsight.

--- quests/timorous/Alrik_Farsight.pl
+++ quests/timorous/Alrik_Farsight.pl
@@ -6,10 +6,10 @@ sub EVENT_SAY {
     if ($text=~/hail/i) {
       quest::say("Ah. Hello there, adventurer. Come to search for ancient artifacts with Fizzlebik and myself?");
     }
-    if ($text=~/ancient artifacts/i) {
+    if ($text=~/ancient artifact/i) {
       quest::say("Oh. All types of things. This area is great. So many artifacts and remains of things all over. It's starting to fall into the pattern of things.");
     }
-    if ($text=~/ancient type of bowl/i) {
+    if ($text=~/ancient bowl/i) {
       quest::emote("rummages through his tattered bag and throws aside a towel. 'Too many towels. Oh well, someone told me they were good to have once. Here it is, a bowl pattern. Interesting bowl. Looks almost like a magical bowl but I'm no potter. You need it, you say? Well, it is interesting. Part of my research here is for the wizard guild in Felwithe. I'm sure they'd put a high price on this one, the enchanters guild being so interested in trading for magical paraphernalia and all. Of course if you helped me out on a chore I have, I'd be happy to give it to you. I need to deliver an artifact to the guild and I'd much rather stay here exploring. If you wish, you can take the artifact and return with the receipt and I'll give you the pattern.'");
     }
     if ($text=~/take the artifact/i) {

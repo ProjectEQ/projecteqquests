2db2b5c01735ca34481365f5cf8b014edf16374e
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-04-24 23:58:12 +0000

    fix spawn

--- trunk/quests/lavastorm/a_fire_sprite.lua
+++ trunk/quests/lavastorm/a_fire_sprite.lua
@@ -2,7 +2,7 @@ function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Emote("ignores you.");
 	elseif(e.message:findi("challenge eejag")) then
-		eq.spawn(27119,0,0,126,1789,-42);
+		eq.spawn2(27119,0,0,126,1789,-42,0);
 		eq.depop_withtimer();
 	end
 end

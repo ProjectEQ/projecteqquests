1fe4048c446ab1f60638f257f981b834bd88e003
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-04-25 00:15:08 +0000

    Saw another issue

--- trunk/quests/lavastorm/a_fire_sprite.lua
+++ trunk/quests/lavastorm/a_fire_sprite.lua
@@ -3,6 +3,6 @@ function event_say(e)
 		e.self:Emote("ignores you.");
 	elseif(e.message:findi("challenge eejag")) then
 		eq.spawn2(27119,0,0,126,1789,-42,0);
-		eq.depop_withtimer();
+		eq.depop_with_timer();
 	end
 end

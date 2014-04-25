7149ebb11a038db080b9701aeffc74e0d077a9d1
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-04-25 17:53:21 +0000

    typo _

--- quests/hohonora/Alekson_Garn.pl
+++ quests/hohonora/Alekson_Garn.pl
@@ -7,6 +7,6 @@ sub EVENT_SAY {
 		quest::say("Be warned, $name. If you believe you are ready, you will fail, even if you can kill Lord Marr's servants!");
 		quest::spawn2(211076,0,0,($x - 50),$y,$z,$h); # A_Custodian_of_Marr_
 		quest::signalwith(211081,4,1);
-		quest::depopwithtimer();
+		quest::depop_withtimer();
 	}
 }

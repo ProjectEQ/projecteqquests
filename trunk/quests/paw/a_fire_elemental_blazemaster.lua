96989ac3cc96cdc954359ccf038de8fd6fe5ad51
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-05-11 20:41:33 +0000

    change to unique_spawn

--- trunk/quests/paw/a_fire_elemental_blazemaster.lua
+++ trunk/quests/paw/a_fire_elemental_blazemaster.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
 	if(math.random(100) < 10) then
-		eq.spawn2(18010,0,0,183,1217,5,192); -- Blazemaster Arnab
+		eq.unique_spawn(18010,0,0,183,1217,5,192); -- Blazemaster Arnab
 	end
 end
--- trunk/quests/paw/an_earth_elemental_intruder.lua
+++ trunk/quests/paw/an_earth_elemental_intruder.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
 	if(math.random(100) < 10) then
-		eq.spawn2(18015,0,0,-130,871,-21,128); -- Earthmaster Grundag
+		eq.unique_spawn(18015,0,0,-130,871,-21,128); -- Earthmaster Grundag
 	end
 end

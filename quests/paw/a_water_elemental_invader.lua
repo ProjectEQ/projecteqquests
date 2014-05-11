f5afdf353125e2dc5690b859bde7e9465752dd7d
rregenn@gmail.com <rregenn@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-05-11 20:41:33 +0000

    change to unique_spawn

--- quests/paw/a_water_elemental_invader.lua
+++ quests/paw/a_water_elemental_invader.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
 	if(math.random(100) < 10) then
-		eq.spawn2(18013,0,0,213,734,-19,206); -- Tidemaster Aquinius
+		eq.unique_spawn(18013,0,0,213,734,-19,206); -- Tidemaster Aquinius
 	end
 end

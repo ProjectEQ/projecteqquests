b176aa6b0d2698cccd9e0a3998379d7c190abac7
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-06-23 21:40:43 +0000

    Cleanup for troubleshooting "Thieves Aboard the Ship".

--- trunk/quests/hatesfury/#languish_spell.pl
+++ trunk/quests/hatesfury/#languish_spell.pl
@@ -1,14 +1,14 @@
 sub EVENT_SIGNAL {
-  if ($signal == 666){
-     quest::settimer("languish",32); #cast every 32 seconds
-}
+  if ($signal == 666) {
+    quest::settimer("languish", 32); #cast every 32 seconds
+  }
   if ($signal == 333){
-     quest::stoptimer("languish");
+    quest::stoptimer("languish");
+  }
 }
- }
- 
+
 sub EVENT_TIMER {
- if ($timer eq "languish") {
-     $npc->CastSpell(3783,228118);
-}    
- } 
+  if ($timer eq "languish") {
+    $npc->CastSpell(3783, 228118); #Languish of the Sea, spell_target
+  }
+}

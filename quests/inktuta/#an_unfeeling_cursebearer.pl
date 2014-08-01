4e80020ee71dd97e5c7042819dd1467d384430ab
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-08-01 03:35:39 +0000

    wipe mechanics - inktuta

--- quests/inktuta/#an_unfeeling_cursebearer.pl
+++ quests/inktuta/#an_unfeeling_cursebearer.pl
@@ -15,6 +15,9 @@ sub EVENT_TIMER {
 	$new_speed = $npc->GetRunspeed();
 	$new_speed = ($new_speed * 1.1);
 	quest::modifynpcstat("runspeed", $new_speed);
+	if (!$npc->GetTarget()) {
+		quest::depop();
+	}
 }
 
 sub EVENT_SLAY {

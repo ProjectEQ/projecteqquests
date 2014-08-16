9ddf454869e8d20de591a329b9c0a812e8342881
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-08-16 07:18:05 +0000

    Txevu

--- quests/qvic/player.pl
+++ quests/qvic/player.pl
@@ -1,20 +1,26 @@
 #BeginFile: qvic\player.pl
 
 sub EVENT_ENTERZONE {
-  if (defined $qglobals{god_qvic_access} && $qglobals{god_qvic_access} == 1 && quest::has_zone_flag(295) == 1) {
-    quest::setglobal("god_qvic_access",2,5,"F");
-  }
+	if (defined $qglobals{god_qvic_access} && $qglobals{god_qvic_access} == 1 && quest::has_zone_flag(295) == 1) {
+		quest::setglobal("god_qvic_access",2,5,"F");
+	}
 }
 
 sub EVENT_CLICKDOOR {
-  if ($doorid == 3) { #Inktu`Ta zone in
-    my $InstanceID = quest::GetInstanceID("inktuta",0);
-    if($InstanceID > 0){
-      quest::MovePCInstance(296,$InstanceID,0,65,-1);
-    } else {
-      $client->Message(13, "You are not a part of an instance!");
-    }
-  }
+	if ($doorid == 2) { #Txevu
+		if (plugin::check_hasitem($client, 60253)) {
+			if(!quest::has_zone_flag(297) {
+				quest::set_zone_flag(297);
+			}
+		}
+	} elsif ($doorid == 3) { #Inktu`Ta
+		my $InstanceID = quest::GetInstanceID("inktuta",0);
+		if($InstanceID > 0){
+		quest::MovePCInstance(296,$InstanceID,0,65,-1);
+		} else {
+		$client->Message(13, "You are not a part of an instance!");
+		}
+	}
 }
 
 #EndFile: qvic\player.pl

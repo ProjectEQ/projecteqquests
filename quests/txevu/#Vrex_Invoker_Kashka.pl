3689e04f54e65823490e3c5903dd9baeb1397629
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-08-21 21:14:08 +0000

    Txevu high priest event

--- quests/txevu/#Vrex_Invoker_Kashka.pl
+++ quests/txevu/#Vrex_Invoker_Kashka.pl
@@ -1,3 +1,12 @@
+sub EVENT_SPAWN {
+	quest::depopall(297064);
+	quest::spawn2(297064,0,0,-570,-930,-506,248);
+	quest::spawn2(297064,0,0,-553,-919,-506,207);
+	quest::spawn2(297064,0,0,-553,-905,-506,184);
+	quest::spawn2(297064,0,0,-562,-890,-506,150);
+	quest::spawn2(297064,0,0,-574,-886,-503,126);
+}
+
 sub EVENT_DEATH_COMPLETE {
 	quest::spawn2(297215,74,0,$x,$y,$z,$h); #Shade
 }

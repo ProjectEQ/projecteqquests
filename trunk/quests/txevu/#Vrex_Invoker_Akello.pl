2e7d820de28450755668167d7462d1a97b3c70ef
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-08-21 21:14:08 +0000

    Txevu high priest event

--- trunk/quests/txevu/#Vrex_Invoker_Akello.pl
+++ trunk/quests/txevu/#Vrex_Invoker_Akello.pl
@@ -1,3 +1,12 @@
+sub EVENT_SPAWN {
+	quest::depopall(297062);
+	quest::spawn2(297062,0,0,-538,-56,-476,111);
+	quest::spawn2(297062,0,0,-548,-64,-477,86);
+	quest::spawn2(297062,0,0,-552,-80,-480,64);
+	quest::spawn2(297062,0,0,-550,-94,-480,38);
+	quest::spawn2(297062,0,0,-537,-104,-478,16);
+}
+
 sub EVENT_DEATH_COMPLETE {
 	quest::spawn2(297213,73,0,$x,$y,$z,$h); #Shade
 }

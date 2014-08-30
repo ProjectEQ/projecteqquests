a1ed59a7f00d2e36d28f5d78e34c1b6ab4018739
mcook@mackal.net <mcook@mackal.net@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-08-30 20:37:14 +0000

    Fix summonitem issue with Specter of Barxt

--- trunk/quests/uqua/#Specter_of_Barxt.lua
+++ trunk/quests/uqua/#Specter_of_Barxt.lua
@@ -14,7 +14,7 @@ function event_say(e)
 	if(e.message:findi("hail")) then
 		if (e.other:HasItem(60252) == false) then
 			e.self:Say("Thank you for helping me see the truth. When you slew the construct, I was driven to the brink of insanity. By destroying my mortal self, you freed my tortured spirit. Please accept this offering. It is a fragment of a master key. Once you have the first two pieces of the High Temple key, you will be able to bypass the wards that protect Qvic. When you have located all three fragments you may find a way to gain entrance into the lair of Txevu. The Muramites have taken control of it and I fear that even now they may be defacing our most sacred temple. Please remember that while you have come far, this is but one small step in your quest to free Taelosia. There are many challenges that yet await you.");
-			e.otherSummonItem(60252);
+			e.other:SummonItem(60252);
 		end
 	end
 end

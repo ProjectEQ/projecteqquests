205c3492fbd8d68016c70a26c23214125206cdde
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-09-01 21:46:04 +0000

    provacating

--- quests/grobb/Savarixsa_Zexus.lua
+++ quests/grobb/Savarixsa_Zexus.lua
@@ -5,5 +5,6 @@ function event_say(e)
 		e.self:Say("Salutations. " .. e.other:GetName() .. "! I am here as a favor to my brother. High Priest Perrir. We seek loyal followers of Innoruuk to earn their place among the hierarchy of the Spires. Are you [" .. eq.say_link("devoted to Innoruuk",false,"devoted to Innoruuk") .. "]?");
 	elseif(e.message:findi("devoted to innoruuk")) then
 		e.self:Say("My brother requests your presence in Neriak at the Spires of Innoruuk. Take this note with you. " .. e.other:GetName() .. ". It will inform Perrir of who sent you.");
+		e.other:SummonItem(18756); -- Tattered Note
 	end
 end

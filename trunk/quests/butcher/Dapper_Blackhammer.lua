43530f43716dc79cfdc9ece860a6e791b171b4af
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-09-10 22:48:18 +0000

    Only one equals

--- trunk/quests/butcher/Dapper_Blackhammer.lua
+++ trunk/quests/butcher/Dapper_Blackhammer.lua
@@ -9,7 +9,7 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 12880, item2 = 6552, platinum == 500})) then
+	if(item_lib.check_turn_in(e.trade, {item1 = 12880, item2 = 6552, platinum = 500})) then
 		e.self:Say("I thank ya for ya patronage. Here is what ya came here for. Now be careful who ya point this at. 'Tis quite sharp, " .. e.other:Race() .. ". Be off with ya now as I must get back ta workin here. Good day!");
 		e.other:SummonItem(6551);
 		e.other:AddEXP(25000);

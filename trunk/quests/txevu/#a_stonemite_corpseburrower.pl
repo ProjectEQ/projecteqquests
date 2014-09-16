8522b262abfb357b046b20a5b328ce3038d35808
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-09-16 03:49:35 +0000

    Mites add no matter what

---
+++ trunk/quests/txevu/#a_stonemite_corpseburrower.pl
@@ -0,0 +1,11 @@
+# #a_stonemite_corpseburrower (297207)
+# Stonemite adds during Bloodfeaster event
+
+sub EVENT_SPAWN {
+	if ($entity_list->IsMobSpawnedByNpcTypeID(297082)) {
+		my $BF = $entity_list->GetNPCByNPCTypeID(297082);
+		my $TopHate = $BF->GetHateTop();
+		my $attack_name = $entity_list->GetClientByName($TopHate->GetName());
+		quest::attack($attack_name->GetName());
+	}
+}

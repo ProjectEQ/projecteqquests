4dafeca5d25cf6afaad1d46bc57a49f99480ca93
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-11-10 05:12:54 +0000

    Return item fixes.

--- quests/guildhall/#Healer.pl
+++ quests/guildhall/#Healer.pl
@@ -4,26 +4,23 @@
 my $ProxDist = 30;
 
 sub EVENT_SPAWN {
-
-   my $x = $npc->GetX();
-   my $y = $npc->GetY();
-   my $z = $npc->GetZ();
-   quest::set_proximity($x - $ProxDist, $x + $ProxDist, $y - $ProxDist, $y + $ProxDist, $z - $ProxDist, $z + $ProxDist);
-   
+  my $x = $npc->GetX();
+  my $y = $npc->GetY();
+  my $z = $npc->GetZ();
+  quest::set_proximity($x - $ProxDist, $x + $ProxDist, $y - $ProxDist, $y + $ProxDist, $z - $ProxDist, $z + $ProxDist);
 }
 
 
 sub EVENT_ENTER {
-
-   $npc->SignalClient($client, 1);
-   $client->Message(4,"Your wounds begin to heal.");
+  $npc->SignalClient($client, 1);
+  $client->Message(4,"Your wounds begin to heal.");
 }
 
 sub EVENT_EXIT {
+  $npc->SignalClient($client, 2);
+  $client->Message(4,"Your healing slows.");
+}
 
-   if($client) {   # Probably not required to check this here
-      $npc->SignalClient($client, 2);
-      $client->Message(4,"Your healing slows.");
-   }
-   
+sub EVENT_ITEM {
+  plugin::return_items(\%itemcount);
 }

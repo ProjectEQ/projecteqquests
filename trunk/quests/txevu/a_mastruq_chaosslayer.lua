0c822fbb2427f9abd692f59009cfac5ef74a2500
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-11-12 22:56:04 +0000

    Update some leashing mechanics. can't use the DB leash values because it's not the same distances in each direction that they leash.

---
+++ trunk/quests/txevu/a_mastruq_chaosslayer.lua
@@ -0,0 +1,12 @@
+-- a_mastruq_chaosslayer 297159  part of ZMTZ event
+
+function event_spawn(e) 
+	eq.set_timer("leash", 1000)
+end
+
+function event_timer(e)
+	if (e.self:GetX() < 1218) then
+		e.self:GMMove(1306, 0, -304, 192)
+		e.self:WipeHateList()
+	end
+end
---
+++ trunk/quests/txevu/an_ikaav_binder_.lua
@@ -0,0 +1,12 @@
+-- 297223 an_ikaav_binder part of ZMTZ event
+
+function event_spawn(e) 
+	eq.set_timer("leash", 1000)
+end
+
+function event_timer(e)
+	if (e.self:GetX() < 1218) then
+		e.self:GMMove(1306, 0, -304, 192)
+		e.self:WipeHateList()
+	end
+end

3e88487ee0e4e51d4201e06f83fcc530c3188fc9
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-11-13 02:26:08 +0000

    Zombie event should only update by using the wand.

--- quests/kithicor/zombie_trooper.pl
+++ quests/kithicor/zombie_trooper.pl
@@ -1,11 +1,19 @@
 sub EVENT_SPAWN {
-my $min = 1;
-my $range = 4;
-my $randomspawn = int(rand($range)) + $min;
-	plugin::RandomRoam(250, 250);
-	quest::settimer(1,$randomspawn);
+  my $min = 1;
+  my $range = 4;
+  my $randomspawn = int(rand($range)) + $min;
+
+  plugin::RandomRoam(250, 250);
+  quest::settimer(1, $randomspawn);
 }
-	
+
 sub EVENT_TIMER {
-	plugin::RandomRoam(250, 250);
+  plugin::RandomRoam(250, 250);
+}
+
+sub EVENT_CAST_ON {
+  if (quest::istaskactive(212) && ($spell_id = 2089)) {
+    quest::updatetaskactivity(212, 0, 1);
+    quest::faction(98, -30); #Fallen of Bloody Kithicor
+  }
 }

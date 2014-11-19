aecce1fb63e84455be2ad78061453e16c4e21a51
mcook@mackal.net <mcook@mackal.net@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-11-19 02:34:57 +0000

    Guild hall door for RoF

--- trunk/quests/guildhall/player.pl
+++ trunk/quests/guildhall/player.pl
@@ -116,3 +116,16 @@ sub EVENT_POPUPRESPONSE {
     #quest::movepc(480,-23,-619,36);
   }
 }
+
+sub EVENT_CLICKDOOR
+{
+# This will move RoF clients to their instance, the rest are handled in the DB
+  if (($client->GetClientVersionBit() & 4294967264) != 0) {
+    if ($doorid == 2 || $doorid == 5) {
+# Do we really need the check here?
+#      if ($client->CalculateDistance(-3.4, -162.9, 6.9) <= 30) {
+        quest::MovePCInstance(344, 5, 0, 588, 1.75, 130);
+#      }
+    }
+  }
+}

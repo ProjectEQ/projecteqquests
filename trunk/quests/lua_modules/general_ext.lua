4423718bc52789d393f5599cf8cd91d9aec512ad
KimmySprite@gmail.com <KimmySprite@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-11-19 04:56:39 +0000

    Added some command stuff, converted player global quest to lua

--- trunk/quests/lua_modules/general_ext.lua
+++ trunk/quests/lua_modules/general_ext.lua
@@ -13,3 +13,17 @@ function eq.SelfCast(spell_id)
 	
 	init:SpellFinished(spell_id, init, 10, 0, -1, sp:ResistDiff());
 end
+
+function eq.ClassType(class)
+	if(class == 8 or class == 15 or class == 3 or class == 4 or class == 5) then
+		return "hybrid";
+	elseif(class == 1 or class == 7 or class == 16 or class == 9) then
+		return "melee";
+	elseif(class == 11 or class == 12 or class == 13 or class == 14) then
+		return "caster"
+	elseif(class == 2 or class == 10 or class == 6) then
+		return "priest";
+	end
+	
+	return "other";
+end

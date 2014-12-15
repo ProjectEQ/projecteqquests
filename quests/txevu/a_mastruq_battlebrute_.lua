ee31b58b3dcd85f546f3101dc91e0fc826a886a2
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-12-15 22:00:58 +0000

    zmtz

--- quests/txevu/a_mastruq_battlebrute_.lua
+++ quests/txevu/a_mastruq_battlebrute_.lua
@@ -1,10 +1,28 @@
 -- 297222 a_mastruq_battlebrute part of ZMTZ event
 
-function event_spawn(e) 
-	eq.set_timer("leash", 1000)
+function event_spawn(e)
+	eq.set_timer("checks", 1000)
 end
 
+function event_combat(e)
+	if (e.joined) then
+		eq.set_timer("checks", 1000)	
+	else
+		eq.stop_all_timers();
+	end
+end
+
+
 function event_timer(e)
+	local rClient = nil
+	if (e.self:GetHateTop() == nil) then
+		rClient = eq.get_entity_list():GetRandomClient(1445,0,-295,150)
+	end
+
+	if (rClient ~= nil) then
+		e.self:AddToHateList(rClient, 1)
+	end
+
 	if (e.self:GetX() < 1218) then
 		e.self:GMMove(1306, 0, -304, 192)
 		e.self:WipeHateList()

f528ecc96912c160fcc4557f6208ece305a73bf9
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2014-12-27 18:52:35 +0000

    change location of where poeb warlord spawns

--- trunk/quests/poearthb/#Warlord_Spawner.lua
+++ trunk/quests/poearthb/#Warlord_Spawner.lua
@@ -1,8 +1,8 @@
 function event_signal(e)
-	-- get quest globals
-	local qglobals = eq.get_qglobals();
+  -- get quest globals
+  local qglobals = eq.get_qglobals();
 
-	if (qglobals["poeb_Galronar"] and qglobals["poeb_Birak"] and qglobals["poeb_Awisano"] ) then
-		eq.spawn2(222019, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
-	end
+  if (qglobals["poeb_Galronar"] and qglobals["poeb_Birak"] and qglobals["poeb_Awisano"] ) then
+    eq.spawn2(222019, 0, 0, 415,161,-55,191.5);
+  end
 end

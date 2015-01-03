5c03b5ce19e53ad36979743a5b26d34798c3da20
rschmertz@gmail.com <rschmertz@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2015-01-03 23:51:22 +0000

    ZMTZ mechanics

--- trunk/quests/txevu/an_ikaav_binder_.lua
+++ trunk/quests/txevu/an_ikaav_binder_.lua
@@ -1,8 +1,5 @@
 -- 297223 an_ikaav_binder part of ZMTZ event
 
-function event_spawn(e)
-	eq.set_timer("checks", 1000)
-end
 
 function event_combat(e)
 	if (e.joined) then
@@ -14,15 +11,6 @@ end
 
 
 function event_timer(e)
-	local rClient = nil
-	if (e.self:GetHateTop() == nil) then
-		rClient = eq.get_entity_list():GetRandomClient(1445,0,-295,150)
-	end
-
-	if (rClient ~= nil) then
-		e.self:AddToHateList(rClient, 1)
-	end
-
 	if (e.self:GetX() < 1218) then
 		e.self:GMMove(1306, 0, -304, 192)
 		e.self:WipeHateList()

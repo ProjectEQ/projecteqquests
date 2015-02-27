78e94c875758548a1e7fcc32559fb021175f7146
jeremiah.oligario@gmail.com <jeremiah.oligario@gmail.com@858f6fc0-2484-11de-a087-198e6c7179d9>
2015-02-27 03:33:26 +0000

    (noudess) Faction and handin fixes.

--- trunk/quests/qeynos2/Priestess_Caulria.lua
+++ trunk/quests/qeynos2/Priestess_Caulria.lua
@@ -25,11 +25,11 @@ function event_trade(e)
 			e.self:Say("I see you have rid our land of a beast tainted with the blood of Bertoxxulous. The Prime Healer shall look favorably upon your soul. Here is our thanks and just in case the beast may have bitten you...");
 			e.self:CastSpell(213,e.other:GetID());
 			e.other:Ding();
-			e.other:Faction(257,5,0);  -- Priests of Life
-			e.other:Faction(183,5,0);  -- Knights of Thunder
-			e.other:Faction(135,5,0);  -- Guards of Qeynos
-			e.other:Faction(21,-10,0); -- Bloodsabers
-			e.other:Faction(9,5,0);    -- Antonius Bayle
+			e.other:Faction(9,1,0);   -- Antonius Bayle
+			e.other:Faction(135,2,0); -- Guards of Qeynos
+			e.other:Faction(21,-1,0); -- Bloodsabers
+			e.other:Faction(183,1,0); -- Knights of Thunder
+			e.other:Faction(257,5,0); -- Priests of Life
 		else
 			e.self:Say("The Temple of Life smiles upon you, friend.. but such a delicate matter can only be entrusted to our most loyal members.");
 			e.other:SummonItem(13949);
@@ -40,11 +40,11 @@ function event_trade(e)
 			e.self:CastSpell(213,e.other:GetID());
 			e.other:SummonItem(15226);
 			e.other:Ding();
-			e.other:Faction(257,5,0);  -- Priests of Life
-			e.other:Faction(183,5,0);  -- Knights of Thunder
-			e.other:Faction(135,5,0);  -- Guards of Qeynos
-			e.other:Faction(21,-10,0); -- Bloodsabers
-			e.other:Faction(9,5,0);    -- Antonius Bayle
+			e.other:Faction(9,1,0);   -- Antonius Bayle
+			e.other:Faction(135,2,0); -- Guards of Qeynos
+			e.other:Faction(21,-1,0); -- Bloodsabers
+			e.other:Faction(183,1,0); -- Knights of Thunder
+			e.other:Faction(257,5,0); -- Priests of Life
 		else
 			e.self:Say("The Temple of Life smiles upon you, friend.. but such a delicate matter can only be entrusted to our most loyal members.");
 			e.other:SummonItem(13298);
@@ -53,11 +53,11 @@ function event_trade(e)
 		e.self:Say("This is odd. This could explain the increase in the number of beasts within the lands surrounding Qeynos. You must take this to Freeport. Here. You must speak with Lorme Tredore. Show him this pellet. You had best gear up for the trip. Freeport is very far away. Here is some gold. Outfit yourself at our expense.");
 		e.other:SummonItem(13951);
 		e.other:Ding();
-		e.other:Faction(257,5,0);  -- Priests of Life
-		e.other:Faction(183,5,0);  -- Knights of Thunder
-		e.other:Faction(135,5,0);  -- Guards of Qeynos
-		e.other:Faction(21,-10,0); -- Bloodsabers
-		e.other:Faction(9,5,0);    -- Antonius Bayle
+		e.other:Faction(9,1,0);   -- Antonius Bayle
+		e.other:Faction(135,2,0); -- Guards of Qeynos
+		e.other:Faction(21,-1,0); -- Bloodsabers
+		e.other:Faction(183,1,0); -- Knights of Thunder
+		e.other:Faction(257,5,0); -- Priests of Life
 		e.other:AddEXP(500);
 		e.other:GiveCash(0,0,6,0);
 	end

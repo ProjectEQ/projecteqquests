c063b9eb82e4f5897dcf01d9e1b4766dce8a7f2c
Athrogate <Shadrach145@gmail.com>
2015-06-13 17:02:32 -0700

    Tavci PRT Encounter

---
+++ tacvi/an_unstable_construct.lua
@@ -0,0 +1,16 @@
+-- an_unstable_construct (298045)
+-- add during Pixtt_Riel_Tavas fight
+-- mini golems that cast a AE DD after a while, and when they die
+
+function event_spawn(e)
+	eq.set_timer("AddSpell", 10000)
+end
+
+function event_timer(e)
+	--Cataclysm of Stone
+	e.self:AddAISpell(0, 4661, 1, -1, -1, -350)
+end
+
+function event_death_complete(e)
+	e.self:CastSpell(4661)
+end

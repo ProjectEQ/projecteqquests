33b6f648266a0bac22c71092d9ec92e421aa970c
Athrogate <Shadrach145@gmail.com>
2015-06-18 19:49:29 -0700

    Removing Zone_Emote Debugging

--- tacvi/player.lua
+++ tacvi/player.lua
@@ -20,16 +20,11 @@ function event_click_door(e)
   setup();
   local door_id = e.door:GetDoorID();
   
-eq.zone_emote(14, "Door: " .. door_id .. " clicked.  ");
-
   if ( Tacvi_Doors[ door_id ] ) then
     -- If the Door Clicked is tied to a mob.
-eq.zone_emote(14, " Mob: " .. Tacvi_Doors[ door_id ]);
     if (eq.get_entity_list():IsMobSpawnedByNpcTypeID( Tacvi_Doors[ door_id ] ) ) then 
-eq.zone_emote(14, " Locking Door: " .. door_id .. " mob " .. Tacvi_Doors[ door_id ] .. " is still up.");
       eq.get_entity_list():FindDoor(door_id):SetLockPick(-1);
     else
-eq.zone_emote(14, " Unlocking Door: " .. door_id );
       eq.get_entity_list():FindDoor(door_id):SetLockPick(0);
     end
   end

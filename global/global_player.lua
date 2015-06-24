f32747bd3ec33c34c71bb7105dc68f99a9a4282d
KimLS <KLS@peqtgc.com>
2015-06-24 15:03:17 -0700

    VetAA stuff

--- global/global_player.lua
+++ global/global_player.lua
@@ -43,3 +43,35 @@ end
 function event_command(e)
 	return eq.DispatchCommands(e);
 end
+
+--[[ the main key is the ID of the AA
+--   the first set is the age required in seconds
+--   the second is if to ignore the age and grant anyways live test server style
+--   the third is enabled
+--]]
+vet_aa = {
+    [481]  = { 31536000, true, true}, -- Lesson of the Devote 1 yr
+    [482]  = { 63072000, true, true}, -- Infusion of the Faithful 2 yr
+    [483]  = { 94608000, true, true}, -- Chaotic Jester 3 yr
+    [484]  = {126144000, true, true}, -- Expedient Recovery 4 yr
+    [485]  = {157680000, true, true}, -- Steadfast Servant 5 yr
+    [486]  = {189216000, true, true}, -- Staunch Recovery 6 yr
+    [487]  = {220752000, true, true}, -- Intensity of the Resolute 7 yr
+    [511]  = {252288000, true, true}, -- Throne of Heroes 8 yr
+    [2000] = {283824000, true, true}, -- Armor of Experience 9 yr
+    [8081] = {315360000, true, true}, -- Summon Resupply Agent 10 yr
+    [8130] = {346896000, true, true}, -- Summon Clockwork Banker 11 yr
+    [453]  = {378432000, true, true}, -- Summon Permutation Peddler 12 yr
+    [182]  = {409968000, true, true}, -- Summon Personal Tribute Master 13 yr
+    [600]  = {441504000, true, true}, -- Blessing of the Devoted 14 yr
+}
+
+function event_connect(e)
+    local age = e.self:GetAccountAge();
+    for aa, v in pairs(vet_aa) do
+        if v[3] and (v[2] or age >= v[1]) then
+            e.self:GrantAlternateAdvancementAbility(aa, 1)
+        end
+    end
+end
+

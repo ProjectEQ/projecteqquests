3ab8d38cb389be03c16b9a5acb53597d82163a96
Michael Cook (mackal) <mcook@mackal.net>
2015-07-21 01:39:16 -0400

    add ZMMD 30% dmg boost

--- tacvi/encounters/zmmd.lua
+++ tacvi/encounters/zmmd.lua
@@ -3,40 +3,40 @@
 -- 298020
 -- http://everquest.allakhazam.com/db/quest.html?quest=4265
 --
--- Zun`Muram Mordl Delt says 'Come you fools! Show me your strongest warrior and I will show you my first victim.' 
+-- Zun`Muram Mordl Delt says 'Come you fools! Show me your strongest warrior and I will show you my first victim.'
 --
 --
--- The Encounter 
+-- The Encounter
 --
--- Zun`Muram Mordl Delt hits for a max ~4,800 and single-target rampages. 
+-- Zun`Muram Mordl Delt hits for a max ~4,800 and single-target rampages.
 --
--- During the encounter, he splits into multiple versions of himself (wouldn't this be convenient in everyday life). 
+-- During the encounter, he splits into multiple versions of himself (wouldn't this be convenient in everyday life).
 --
--- At 90% health, the real "Zun`Muram Mordl Delt" feigns death and two clones of him spawn and attack: 
+-- At 90% health, the real "Zun`Muram Mordl Delt" feigns death and two clones of him spawn and attack:
 --
--- Zun'Muram Mordl Delt's skin begins to bulge and split open, releasing a wave of energy. As his body falls to the floor, two smaller images of the massive creature appear before you. 
+-- Zun'Muram Mordl Delt's skin begins to bulge and split open, releasing a wave of energy. As his body falls to the floor, two smaller images of the massive creature appear before you.
 --
--- The clones each hit for a max ~3,700 and should be kited until they despawn (this takes a few minutes): 
+-- The clones each hit for a max ~3,700 and should be kited until they despawn (this takes a few minutes):
 --
--- Zun'Muram Mordl Delt's body begins to glow as the images before you merge back into one. The wounds upon Mordl's body quickly heal as he's infused by the energy, but new wounds appear where his other forms were injured. 
+-- Zun'Muram Mordl Delt's body begins to glow as the images before you merge back into one. The wounds upon Mordl's body quickly heal as he's infused by the energy, but new wounds appear where his other forms were injured.
 --
--- At 70% health, he feigns death again and spawns three clones which, again, should be kited until they despawn. 
+-- At 70% health, he feigns death again and spawns three clones which, again, should be kited until they despawn.
 --
--- He does this again at 50% (four clones to be kited until despawn). 
+-- He does this again at 50% (four clones to be kited until despawn).
 --
--- At 30%, he spawns two clones (weaker in power), but this time, doesn't feign death. Here, he needs to be killed while you kite and/or offtank the two clones. 
+-- At 30%, he spawns two clones (weaker in power), but this time, doesn't feign death. Here, he needs to be killed while you kite and/or offtank the two clones.
 --
--- In a final act of desperation, Mordl splits two smaller forms off from himself but looks visibly drained by the effort. 
+-- In a final act of desperation, Mordl splits two smaller forms off from himself but looks visibly drained by the effort.
 --
--- NOTE: In all cases, and if your raid force can withstand it, you can kill the clones instead of kiting them. 
+-- NOTE: In all cases, and if your raid force can withstand it, you can kill the clones instead of kiting them.
 --
 --
--- Completion & Loot 
+-- Completion & Loot
 --
--- Zun`Muram Mordl Delt has been slain by _____! 
--- The creature's two heads face each other just before it falls to the floor, shaking the very foundation of the temple. Now there is nothing that stands between you and the being in charge of this invading army. 
+-- Zun`Muram Mordl Delt has been slain by _____!
+-- The creature's two heads face each other just before it falls to the floor, shaking the very foundation of the temple. Now there is nothing that stands between you and the being in charge of this invading army.
 --
--- The chamber is filled with the sound of a grinding stone. The path leading into the final chamber has been opened and it awaits your arrival. You hear a voice that sounds oddly familiar. 'Come now, fools, enter my chamber and learn the meaning of suffering from one bred to destroy and conquer. You have beaten the best of my army, but you have yet to see true power. Step into the abyss and cower at what stares back at you.' 
+-- The chamber is filled with the sound of a grinding stone. The path leading into the final chamber has been opened and it awaits your arrival. You hear a voice that sounds oddly familiar. 'Come now, fools, enter my chamber and learn the meaning of suffering from one bred to destroy and conquer. You have beaten the best of my army, but you have yet to see true power. Step into the abyss and cower at what stares back at you.'
 --
 --]]
 local ZMMD_Active = "SERFQMCNIDf"
@@ -47,12 +47,12 @@ local kite_time = 180;
 
 function ZMMD_Spawn(e)
   eq.set_next_hp_event(90);
-end 
+end
 
 function ZMMD_Combat(e)
   if (e.joined == true) then
 
-    e.self:Say("Come you fools! Show me your strongest warrior and I will show you my first victim."); 
+    e.self:Say("Come you fools! Show me your strongest warrior and I will show you my first victim.");
   else
     eq.set_timer("wipecheck", 1 * 1000);
   end
@@ -72,11 +72,11 @@ function ZMMD_Inactivate(e)
 end
 
 function ZMMD_Timer(e)
-  if (e.timer == "zmmd_kite") then 
+  if (e.timer == "zmmd_kite") then
     eq.stop_timer(e.timer);
-      eq.depop_all(298050); 
-      eq.depop_all(298051); 
-      eq.depop_all(298052); 
+      eq.depop_all(298050);
+      eq.depop_all(298051);
+      eq.depop_all(298052);
     ZMMD_Activate(e);
   elseif ("wipecheck") then
     -- Check to see if there are any Clients in the room with ZMKP
@@ -84,10 +84,10 @@ function ZMMD_Timer(e)
     if (client:IsClient() == false) then
       -- Wipe Mechanics
       eq.get_entity_list():FindDoor(13):SetLockPick(0);
-      eq.depop_all(298050); 
-      eq.depop_all(298051); 
-      eq.depop_all(298052); 
-      eq.depop_all(298053); 
+      eq.depop_all(298050);
+      eq.depop_all(298051);
+      eq.depop_all(298052);
+      eq.depop_all(298053);
       eq.spawn2(298020, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
       eq.depop();
     end
@@ -95,7 +95,7 @@ function ZMMD_Timer(e)
 end
 
 function ZMMD_Hp(e)
-  if (e.hp_event == 90) then 
+  if (e.hp_event == 90) then
     eq.get_entity_list():FindDoor(13):SetLockPick(-1);
 
     e.self:Emote("skin begins to bulge and split open, releasing a wave of energy. As his body falls to the floor, two smaller images of the massive creature appear before you. ");
@@ -119,7 +119,7 @@ function ZMMD_Hp(e)
 
   elseif (e.hp_event == 50) then
     eq.set_next_hp_event(30);
-    
+
     ZMMD_Inactivate(e);
     eq.spawn2(298052,0,0, 367, 130, -6.7, 185);
     eq.spawn2(298052,0,0, 367, 151, -6.7, 197);
@@ -128,12 +128,13 @@ function ZMMD_Hp(e)
     eq.set_timer("zmmd_kite", kite_time * 1000);
 
   elseif (e.hp_event == 30) then
-
+    eq.modify_npc_stat("min_hit", "1670");
+    eq.modify_npc_stat("max_hit", "4900");
     eq.spawn2(298053,0,0, 367, 151, -6.7, 197);
     eq.spawn2(298053,0,0, 367, 171, -6.7, 197);
 
   end
-    
+
 end
 
 function ZMMD_Signal(e)
@@ -149,8 +150,8 @@ function ZMMD_Death(e)
 end
 
 function ZMMD_Add_Death(e)
-  if (eq.get_entity_list():IsMobSpawnedByNpcTypeID( 298050 ) == false 
-    and eq.get_entity_list():IsMobSpawnedByNpcTypeID( 298051 ) == false 
+  if (eq.get_entity_list():IsMobSpawnedByNpcTypeID( 298050 ) == false
+    and eq.get_entity_list():IsMobSpawnedByNpcTypeID( 298051 ) == false
     and eq.get_entity_list():IsMobSpawnedByNpcTypeID( 298052 ) == false) then
 
     -- Wake ZMMD Main Mob back up.

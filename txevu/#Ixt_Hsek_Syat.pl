4c43b297f471136be9eeaef801fab308c11b9d5b
Athrogate <Shadrach145@gmail.com>
2015-07-22 07:04:27 -0700

    Txevu: Champion Encounter - set reset timer if Syat goes out of combat to 30min.

--- txevu/#Ixt_Hsek_Syat.pl
+++ txevu/#Ixt_Hsek_Syat.pl
@@ -1,5 +1,8 @@
 # #Ixt_Hsek_Syat (297211)
-
+# 
+# If the Raid wipes after Ixt Hsek Syat spawns
+# - If Syat leaves combat and stays out of combat for 30min; depsawn
+# - Respawn MC (and event) 30min after Syat despawn.
 
 sub EVENT_SIGNAL {
 	#Champion is at 15%, need to assist
@@ -7,4 +10,21 @@ sub EVENT_SIGNAL {
 	my $TopHate = $champion->GetHateTop();
 	my $attack_name = $entity_list->GetClientByName($TopHate->GetName());
 	quest::attack($attack_name->GetName());
+}
+
+sub EVENT_COMBAT {
+  if ($combat_state==1){
+    quest::stoptimer(1);
+  } else {
+    quest::settimer(1, 1800);
+  }
+}
+
+sub EVENT_TIMER {
+  if ($timer == 1) {
+    quest::updatespawntimer(51505, 1800 * 1000); #Update MC Spawn Timer for 30min since there was a failure.
+    quest::depopall(297209); #runt
+    quest::depopall(297034); #MC
+    quest::depopall(297211); #Hsek Syat
+  }
 }

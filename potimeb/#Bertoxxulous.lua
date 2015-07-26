3eab75a49aa300f3a89b87533ae0d31d577426d2
Michael Cook (mackal) <mcook@mackal.net>
2015-07-25 22:58:49 -0400

    Fix emotes

--- potimeb/#Bertoxxulous.lua
+++ potimeb/#Bertoxxulous.lua
@@ -18,31 +18,31 @@ function event_hp(e)
     if (e.hp_event == 85) then
         eq.modify_npc_stat("min_hit", "1040")
         eq.modify_npc_stat("max_hit", "1800")
-        eq.zone_emote("Blisters and festering sores covering Bertoxxulous's hide burst. A noxious ooze bleeds down his scarred flesh, strengthening his physical body.")
+        eq.zone_emote(15, "Blisters and festering sores covering Bertoxxulous's hide burst. A noxious ooze bleeds down his scarred flesh, strengthening his physical body.")
         eq.set_next_hp_event(70)
     elseif (e.hp_event == 70) then
         eq.modify_npc_stat("min_hit", "1145")
         eq.modify_npc_stat("max_hit", "2000")
-        eq.zone_emote("Blisters and festering sores covering Bertoxxulous's hide burst. A noxious ooze bleeds down his scarred flesh, strengthening his physical body.")
+        eq.zone_emote(15, "Blisters and festering sores covering Bertoxxulous's hide burst. A noxious ooze bleeds down his scarred flesh, strengthening his physical body.")
         eq.set_next_hp_event(55)
     elseif (e.hp_event == 55) then
         eq.modify_npc_stat("min_hit", "1250")
         eq.modify_npc_stat("max_hit", "2200")
-        eq.zone_emote("Blisters and festering sores covering Bertoxxulous's hide burst. A noxious ooze bleeds down his scarred flesh, strengthening his physical body.")
+        eq.zone_emote(15, "Blisters and festering sores covering Bertoxxulous's hide burst. A noxious ooze bleeds down his scarred flesh, strengthening his physical body.")
         eq.set_next_hp_event(40)
     elseif (e.hp_event == 40) then
         eq.modify_npc_stat("min_hit", "1145")
         eq.modify_npc_stat("max_hit", "2000")
-        eq.zone_emote("Bertoxxulous falters, nearly imperceptibly.")
+        eq.zone_emote(15, "Bertoxxulous falters, nearly imperceptibly.")
         eq.set_next_hp_event(25)
     elseif (e.hp_event == 25) then
         eq.modify_npc_stat("min_hit", "1040")
         eq.modify_npc_stat("max_hit", "1800")
-        eq.zone_emote("Bertoxxulous falters, nearly imperceptibly.")
+        eq.zone_emote(15, "Bertoxxulous falters, nearly imperceptibly.")
         eq.set_next_hp_event(10)
     elseif (e.hp_event == 10) then
         eq.modify_npc_stat("min_hit", "940")
         eq.modify_npc_stat("max_hit", "1700")
-        eq.zone_emote("Bertoxxulous falters, nearly imperceptibly.")
+        eq.zone_emote(15, "Bertoxxulous falters, nearly imperceptibly.")
     end
 end

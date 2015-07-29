c5a4e67b4277d8468862e41a1b4350b829175750
Athrogate <Shadrach145@gmail.com>
2015-07-28 21:15:54 -0700

    Tacvi: Tunat first draft

---
+++ tacvi/encounters/tmcv.lua
@@ -0,0 +1,317 @@
+--[[
+-- Initial Fight 
+--
+-- Tunat`Muram Cuu Vauax says 'You have defiled my chambers and destroyed my officers. I will crush your soul and suck the marrow from your bones.' 
+--
+-- Tunat`Muram Cuu Vauax hits for a max ~4,400 (no rampage; no flurry; no special effects). He is permanently rooted in place and summons when damaged. He has an extra summon for players who try to get away from him: 
+--
+-- Tunat`Muram Cuu Vauax says 'You can not evade my reach so easily, coward!' 
+-- Tunat`Muram Cuu Vauax begins to cast a spell. <Immobilizing Spikes> 
+--
+-- Immobilizing Spikes: Single Target, Unresistable (0) 
+-- 1: Root 
+-- 2: Decrease Hitpoints by 200 per tick 
+--
+-- He is surrounded by nine aneuks. Their sole purpose is to regenerate the Tunat`Muram throughout the event, prolonging his death: 
+--
+-- Tunat`Muram Cuu Vauax pauses for a moment as a portion of his spirit is transferred into one of the phylacteries. 
+--
+-- You can attack and kill these aneuks; however, their DPS output and health are comparable to that of the mastruq mobs encountered in the zone, and they also cast a DOT on whoever is on their hate list. 
+--
+-- Once Tunat`Muram Cuu Vauax reaches 40%, 2x "an ukun biledrinker" become active. You can either kill them or offtank them (they will despawn when Tunat`Muram does). They can be stunned, but not mezzed. 
+--
+-- When Tunat`Muram Cuu Vauax reaches 0%, he despawns and respawns behind the altar area (NOT auto-aggro). This version will be much meaner: 
+--
+-- In an explosion of energy, Tunat'Muram Cuu Vauax disappears while ancient pebbles pelt against your armor. 
+--
+-- The room is filled with an eerie laugh. 'You have done well to defeat my doppelganger and have shown great strength by making it this far, but I'm afraid I must end your struggle here. Your days have been numbered since you first set foot upon this continent and your time is up. Kneel before me and I will grant you a quick death, but resist and you will suffer in ways that will be spoken about in hushed tones for eons to come. 
+--
+--
+-- That Was Too Easy; Now the Main Event 
+--
+-- You can take as much time to rez, med, rebuff, etc. as necessary as there is no apparent timer to engage. (This also acts as a reset point for the event. If you fail this part, you don't have to repeat the initial fight.) 
+--
+-- When ready, engage him again. Here, he hits for a max ~4,800 and buffs himself with "Haste of the Tunat'Muram": 
+--
+-- Haste of the Tunat'Muram: Self 0', Unresistable (0) 
+-- 1: Increase Attack Speed by 50% 
+-- 2: Increase Damage Shield by 60 
+--
+-- At 90% health and every 10% health after (down to 20%), he transforms into one of his lieutenants: 
+--
+-- Tunat`Muram Cuu Vauax shimmers and changes before your eyes. 
+--
+-- 90%: Pixtt Xxeric Kex (flurries; immediately spawns four ukun adds - stunnable, but not mezzable) 
+-- 80%: Pixtt Kretv Krakxt (mitigated AE rampage; spawns 4x "an ikaav hatchling" adds if you take too long) 
+-- 70%: Pixtt Riel Tavas (unstable construct adds if you take too long) 
+-- 60%: Zun`Muram Kvxe Pirik (single-target rampage; straight melee) 
+-- 50%: Zun`Muram Yihst Vor (single-target rampage; straight melee) 
+-- 40%: Zun`Muram Mordl Delt (single-target rampage; flurries; spawns 2x "Zun`Muram Mordl Delt" adds) 
+-- 30%: Zun`Muram Shaldn Boc (single-target rampage; straight melee) 
+--
+-- At 20%, he reforms as "Tunat`Muram Cuu Vauax" once again... 
+--
+-- Zun`Muram Shaldn Boc shimmers and changes before your eyes. 
+--
+-- Tunat`Muram Cuu Vuaux says 'You are stronger than I anticipated. While you have exhausted the fleeting spirit of my underlings, you have yet to face my true fury!' 
+--
+-- For now, he hits for a max ~5,300 and remains simple enough. By 15% health, however, things start to pick up... 
+--
+-- Below 15% health, he starts hitting for a max ~6,500 and frequently single-target rampages and flurries. He also increases his attack speed and uses an arsenal of spells that are cast with some frequency: 
+--
+-- Haste of the Tunat'Muram: Self 0', Unresistable (0) 
+-- 1: Increase Attack Speed by 50% 
+-- 2: Increase Damage Shield by 60 
+--
+-- Bellow of Tunat'Muram: NPC Hatelist 1000', Chromatic (-200) 
+-- 1: Silence 
+-- 2: Decrease Hitpoints by 800 
+-- 3: Increase Curse Counter by 16 
+-- 4: Decrease Accuracy by 20% 
+--
+-- Discord's Rebuke: PB AE 100', Chromatic (-250) 
+-- 1: Decrease Hitpoints by 3000 
+--
+-- Gaze of the Tunat'Muram: Targeted AE 30', Prismatic (-300) 
+-- 1: Decrease Spell Haste by 50% 
+-- 2: Decrease HP when cast by 1500 
+-- 3: Decrease Hitpoints by 1000 per tick 
+-- 4: Decrease WIS by 50 
+-- 5: Decrease INT by 50 
+-- 6: Decrease Mana by 150 per tick 
+-- 7: Limit: Combat Skills Not Allowed 
+--
+-- Ikaav's Venom: PB AE 300', Magic (-300) 
+-- 1: Decrease Attack Speed by 50% 
+-- 2: Decrease HP when cast by 4050 
+-- 3: Increase Poison Counter by 36 
+--
+-- Spirit Cleaver: Single Target, Prismatic (-350) 
+-- 1: Decrease ATK by 500 
+-- 2: Decrease Hitpoints by 1225 per tick 
+-- 3: Decrease Mana by 400 per tick 
+-- 4: Decrease Endurance by 400 per tick 
+-- 5: Decrease Stats by 350 
+-- 6: Increase Poison Counter by 99 
+-- 7: Increase Poison Counter by 99 
+-- 8: Increase Poison Counter by 99 
+--
+-- Touch of the Tunat'Muram: Targeted AE 30', Prismatic (-300) 
+-- 1: Decrease HP when cast by 2000 
+-- 2: Decrease Hitpoints by 1500 per tick 
+-- 3: Decrease Mana by 100 per tick 
+-- 4: Decrease Endurance by 100 per tick 
+-- 5: Decrease STR by 50 
+-- 6: Decrease ATK by 300 
+--
+-- Wave of Rage: PB AE 100', Prismatic (-350) 
+-- 1: Decrease Spell Mana Cost by 0% 
+-- 2: Decrease HP when cast by 4050 
+-- 3: Decrease Hitpoints by 500 per tick 
+-- 4: Decrease INT by 400 
+-- 5: Decrease WIS by 400 
+-- 6: Increase Poison Counter by 99 
+-- 7: Increase Poison Counter by 99 
+-- 8: Increase Poison Counter by 99 
+-- 9: Increase Poison Counter by 99 
+-- 10: Limit: Combat Skills Not Allowed 
+--
+-- Kill him to complete the event. 
+--
+--
+-- Event Completion & Loot 
+--
+-- Tunat`Muram Cuu Vuaux has been slain by _____! 
+--
+-- Tunat`Muram Cuu Vuaux's corpse says 'Impossible! You have not been tested in the fires of discord, yet you have devastated the best the legion could muster. How can a world untouched by the crucible of chaos breed warriors of such strength? Mata Muram must be warned! He must prepare for your coming!' 
+--
+-- Loot from this event includes 2 "Cracked Shard of Power" (Breakdown in Communication item) + 1 "Tongue of the Tunat'muram" + 2 augmentations (first list) + 2 other items (second list): 
+--
+-- Mobs:
+--  298014 Tunat (1st)
+--  298055 Tunat (2nd)
+--  298209 an ukun biledrinker (spawns at 40% of Tunat (1st)'s health)
+--  298113 Living Phylactery (Tunat 1st) Guards
+--  An ukun juxta pincer: 60k hp, doubles,flurries, 635-2820 min/max
+--  an ukun manasipper:double, 600-2120min/max, 116k hp
+--  an ukun lifebleeder: doubles, flurries, 80k hp, 600-2120min/max
+--  an ukun ragehound: double, flurries, 114k hp, 650-2990
+--  
+--
+--]]
+function Tunat_Second_Spawn()
+  eq.set_next_hp_event(90);
+end
+
+function Tunat_Second_Death(e)
+
+end
+
+function Tunat_Second_HP(e)
+  eq.zone_emote(14, "Tunat HP Event: " .. e.hp_event);
+  if (e.hp_event == 90) then
+    -- 90%: Pixtt Xxeric Kex (flurries; immediately spawns four ukun adds - stunnable, but not mezzable) 
+    e.self:ModifyNPCStat("flurry", "1");
+    eq.modify_npc_stat("min_hit", "1270");
+    eq.modify_npc_stat("max_hit", "4500");
+
+    e.self:SendIllusionPacket({race=393,gender=2,texture=11});
+    e.self:TempName("Pixtt Xxeric Kex");
+
+    -- Spawn Adds
+    eq.spawn2(298044, 0, 0, 334, -117, 21, 140);
+    eq.spawn2(298043, 0, 0, 356, -154, 21, 178);
+    eq.spawn2(298042, 0, 0, 353, -201, 21, 217);
+    eq.spawn2(298041, 0, 0, 322, -215, 21, 248);
+
+    eq.set_next_hp_event(80);
+  elseif (e.hp_event == 80) then 
+    -- 80%: Pixtt Kretv Krakxt (mitigated AE rampage; spawns 4x "an ikaav hatchling" adds if you take too long) 
+    e.self:ModifyNPCStat("flurry", "0");
+    e.self:ModifyNPCStat("area_rampage", "1");
+    eq.modify_npc_stat("min_hit", "1272");
+    eq.modify_npc_stat("max_hit", "4432");
+
+    e.self:SendIllusionPacket({race=394,gender=2,texture=11});
+    e.self:TempName("Pixtt Kretv Kakxt");
+
+    eq.set_timer("pkk_adds", 30 * 1000);
+
+    eq.set_next_hp_event(70);
+    
+  elseif (e.hp_event == 70) then
+    -- 70%: Pixtt Riel Tavas (unstable construct adds if you take too long) 
+    e.self:ModifyNPCStat("area_rampage", "0");
+    eq.modify_npc_stat("min_hit", "1560");
+    eq.modify_npc_stat("max_hit", "4600");
+
+    e.self:SendIllusionPacket({race=394,gender=2,texture=11});
+    e.self:TempName("Pixtt Riel Tavas");
+
+    eq.stop_timer("pkk_adds");
+    eq.set_timer("prt_adds", 30 * 1000);
+
+    eq.set_next_hp_event(60);
+
+  elseif (e.hp_event == 60) then
+    -- 60%: Zun`Muram Kvxe Pirik (single-target rampage; straight melee) 
+    e.self:SendIllusionPacket({race=400,gender=2,texture=11});
+    e.self:TempName("Zun`Muram Kvxe Pirik");
+    eq.modify_npc_stat("min_hit", "1430");
+    eq.modify_npc_stat("max_hit", "3900");
+
+    eq.stop_timer("prt_adds");
+
+    eq.set_next_hp_event(50);
+
+  elseif (e.hp_event == 50) then
+    -- 50%: Zun`Muram Yihst Vor (single-target rampage; straight melee) 
+    e.self:ModifyNPCStat("flurry", "1");
+    e.self:ModifyNPCStat("area_rampage", "1");
+    eq.modify_npc_stat("min_hit", "1650");
+    eq.modify_npc_stat("max_hit", "4500");
+    
+    e.self:SendIllusionPacket({race=400,gender=2,texture=11});
+    e.self:TempName("Zun`Muram Yihst Vor");
+
+    eq.set_next_hp_event(40);
+
+  elseif (e.hp_event == 40) then
+    -- 40%: Zun`Muram Mordl Delt (single-target rampage; flurries; spawns 2x "Zun`Muram Mordl Delt" adds) 
+    e.self:ModifyNPCStat("area_rampage", "0");
+    e.self:ModifyNPCStat("rampage", "1");
+    e.self:ModifyNPCStat("flurry", "1");
+    eq.modify_npc_stat("min_hit", "1350");
+    eq.modify_npc_stat("max_hit", "4200");
+
+    e.self:SendIllusionPacket({race=400,gender=2,texture=11});
+    e.self:TempName("Zun`Muram Mordl Delt");
+
+    eq.set_timer("zmmd_adds", 12 * 1000);
+    eq.set_next_hp_event(30);
+
+  elseif (e.hp_event == 30) then
+    -- 30%: Zun`Muram Shaldn Boc (single-target rampage; straight melee) 
+    e.self:ModifyNPCStat("flurry", "0");
+    eq.modify_npc_stat("min_hit", "1470");
+    eq.modify_npc_stat("max_hit", "4700");
+
+    e.self:SendIllusionPacket({race=400,gender=2,texture=11});
+    e.self:TempName("Zun`Muram Shaldn Boc");
+
+    eq.set_next_hp_event(20);
+  elseif (e.hp_event == 20) then 
+    -- 20%: he reforms as Tunat`Muram Cuu Vauax once again...
+    e.self:ModifyNPCStat("flurry", "1");
+    eq.modify_npc_stat("min_hit", "1450");
+    eq.modify_npc_stat("max_hit", "4300");
+
+    e.self:SendIllusionPacket({race=399,gender=2,texture=11});
+    e.self:TempName("Tunat`Muram Cuu Vauax");
+
+    eq.stop_timer("zmmd_adds");
+  end
+end
+
+function Tunat_First_Death(e)
+	eq.spawn2(298055,0,0, 309, -170.8, 21.3, 59.4);
+
+  eq.depop_all(298113);
+  eq.depop_all(298209);
+end
+
+function Tunat_First_Spawn(e)
+  eq.set_next_hp_event(40);
+
+  -- Spawn the Living 
+  eq.spawn2(298113, 0, 0,500.00, -152.00, 23.75, 56.00);
+  eq.spawn2(298113, 0, 0,507.00, -172.00, 23.75, 56.00);
+  eq.spawn2(298113, 0, 0,498.00, -193.00, 23.75, 56.00);
+  eq.spawn2(298113, 0, 0,476.00, -242.00, 23.75, 123.00);
+  eq.spawn2(298113, 0, 0,428.00, -242.00, 23.75, 123.00);
+  eq.spawn2(298113, 0, 0,454.00, -242.00, 23.75, 123.00);
+  eq.spawn2(298113, 0, 0,478.00, -100.00, 23.75, 14.00);
+  eq.spawn2(298113, 0, 0,454.00, -100.00, 23.75, 14.00);
+  eq.spawn2(298113, 0, 0,431.00, -100.00, 23.75, 14.00);
+
+end
+
+function Tunat_First_HP(e)
+  if (e.hp_event == 40) then
+    eq.spawn2(298209, 0, 0, 445, -203, 25, 17);
+    eq.spawn2(298209, 0, 0, 447, -139, 25, 99);
+  end
+end
+
+function Tunat_Second_Timer(e)
+
+  if (e.timer == "pkk_adds") then
+    eq.spawn2(298048, 0, 0, 334, -117, 21, 140);
+    eq.spawn2(298048, 0, 0, 356, -154, 21, 178);
+    eq.spawn2(298048, 0, 0, 353, -201, 21, 217);
+    eq.spawn2(298048, 0, 0, 322, -215, 21, 248);
+
+  elseif (e.timer == "prt_adds") then
+    eq.spawn2(298045, 0, 0, 334, -117, 21, 140);
+    eq.spawn2(298045, 0, 0, 356, -154, 21, 178);
+    eq.spawn2(298045, 0, 0, 353, -201, 21, 217);
+    eq.spawn2(298045, 0, 0, 322, -215, 21, 248);
+    eq.spawn2(298045, 0, 0, 322, -225, 21, 248);
+
+  elseif (e.timer == "zmmd_adds") then
+    eq.spawn2(298053, 0, 0, 334, -117, 21, 140);
+    eq.spawn2(298053, 0, 0, 356, -154, 21, 178);
+
+  end
+end
+
+function event_encounter_load(e)
+  eq.register_npc_event('tmcv', Event.spawn,          298014, Tunat_First_Spawn);
+  eq.register_npc_event('tmcv', Event.death_complete, 298014, Tunat_First_Death);
+  eq.register_npc_event('tmcv', Event.hp,             298014, Tunat_First_HP);
+
+  eq.register_npc_event('tmcv', Event.spawn,          298055, Tunat_Second_Spawn);
+  eq.register_npc_event('tmcv', Event.death_complete, 298055, Tunat_Second_Death);
+  eq.register_npc_event('tmcv', Event.hp,             298055, Tunat_Second_HP);
+  eq.register_npc_event('tmcv', Event.timer,          298055, Tunat_Second_Timer);
+end

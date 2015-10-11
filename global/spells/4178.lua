4e77da9077b664c4c3fea1cf468681b98fcc8f00
Athrogate <Shadrach145@gmail.com>
2015-10-11 13:38:48 -0700

    Magus TL to sro correcting location.

--- global/spells/4178.lua
+++ global/spells/4178.lua
@@ -1,7 +1,7 @@
 -- Teleport: South Ro
 function event_spell_effect(e)
   local client = eq.get_entity_list():GetClientByID(e.target:GetID());
-  client:MovePC(35, 137.41, -1043.16, 10.34, 0);
+  client:MovePC(35, 1053.41, -1461.16, -25.96, 0);
 
   return 1;
 end

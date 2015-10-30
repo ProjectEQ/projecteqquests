4a7f02d515e291b341e9d2036a3de9b45d0bcd97
Athrogate <Shadrach145@gmail.com>
2015-10-29 17:49:20 -0700

    Ikaav correct spells based on player feedback from test.

--- txevu/#Ikaav_Nysf_Lleiv.lua
+++ txevu/#Ikaav_Nysf_Lleiv.lua
@@ -11,9 +11,6 @@ function event_combat(e)
 	if (e.joined == true) then
 		--spawn golem adds in middle of room, starting every 30 seconds
 		eq.set_timer("spawn_golem", add_timer)
-		--scripted AEs
-		eq.set_timer("malicious_intent",20000)
-		eq.set_timer("curse_of_flames",10000)
 	else
 		--wipe
 		eq.stop_all_timers()
@@ -58,26 +55,25 @@ end
 function event_hp(e)
 	-- 75/50/25 each of the stonemasters becomes killable
 	if (e.hp_event == 75) then
+    e.self:AddAISpell(0, 1236, 1, -1, 45, -350)
 		if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(297089)) then
 			eq.get_entity_list():MessageClose(e.self,false,120,0,"The shielding on one of the stonemasters falters as Ikaav Nysf Lliev focuses her energies inward, intensifying her own powers.")
 			eq.signal(297089,1)
 			eq.set_next_hp_event(50)
 		end
 	elseif (e.hp_event == 50) then
+		eq.set_timer("malicious_intent",20000)
 		if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(297088)) then
 			eq.get_entity_list():MessageClose(e.self,false,120,0,"The shielding on one of the stonemasters falters as Ikaav Nysf Lliev focuses her energies inward, intensifying her own powers.")
 			eq.signal(297088,1)
 			eq.set_next_hp_event(25)
 		end
 	elseif (e.hp_event == 25) then
+		eq.set_timer("curse_of_flames",10000)
 		if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(297087)) then
 			eq.get_entity_list():MessageClose(e.self,false,120,0,"The shielding on the final stonemaster falters as Ikaav Nysf Lliev focuses her energies inward, intensifying her own powers.")
 			eq.signal(297087,1)
-			eq.set_next_hp_event(20)
 		end
-	elseif (e.hp_event == 20) then
-	--add Charm spell at 20%
-	e.self:AddAISpell(0, 1236, 1, -1, 45, -350)
 	end
 end
 

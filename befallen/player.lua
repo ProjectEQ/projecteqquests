fdf868e608024f097b6c331558958b7d051e9c80
Athrogate <Shadrach145@gmail.com>
2015-11-06 17:36:43 -0800

    Befallen Rogue/Bard quest was referenceing other instead of self.

--- befallen/player.lua
+++ befallen/player.lua
@@ -1,6 +1,6 @@
 function event_enter_zone(e)
 	if(e.self:Class() == "Bard" or e.self:Class() == "Rogue") then
-		if(e.other:HasItem( 52355) == true and e.other:HasItem( 52357) == false) then --Shakey's Dilapidated Noggin, Pulsing Rose Lightstone
+		if(e.self:HasItem( 52355) == true and e.self:HasItem( 52357) == false) then --Shakey's Dilapidated Noggin, Pulsing Rose Lightstone
 			eq.unique_spawn(36102, 2, 0, 165, -170, -10); --Flighty_Rose_Wisp
 			e.self:Message(15, "Out of the corner of your eye, you catch a glimpse of a brightly colored wisp as it darts by. It's moving at an incredible speed.");
 		end

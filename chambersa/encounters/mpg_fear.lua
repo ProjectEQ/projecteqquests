601d287f6fcc6e1f7fcbd4a926e9dcc07979433e
Athrogate <Shadrach145@gmail.com>
2015-12-15 15:45:20 -0800

    MPG Fear Trial Beta Update

--- chambersa/encounters/mpg_fear.lua
+++ chambersa/encounters/mpg_fear.lua
@@ -5,13 +5,14 @@ local minutes_remaining;
 local lockout_name = 'MPG_fear';
 local lockout_win = 72;
 local lockout_loss = 2;
+local warnings = 0;
 
 function Fear_Spawn(e)
-  instance_id = eq.get_instance_id('chambersa', 1);
-  eq.zone_emote(13, "instance_id: " .. instance_id);
+  --instance_id = eq.get_instance_id('chambersa', 1);
+  instance_id = eq.get_zone_instance_id();
   mobs_died = 0;
-  mobs_must_die = 1;
-  minutes_remaining = 1;
+  mobs_must_die = 15;
+  minutes_remaining = 15;
   player_list = eq.get_characters_in_instance(instance_id);
 
   eq.spawn_condition('chambersa', instance_id, 1, 0 );
@@ -92,16 +93,23 @@ function Fearable_Tick(e)
 end
 
 function Deathtouch_Tick(e)
-  local box = require("aa_box");
-  local dt_box = box(325, 105, -90, -58);
-  local my_id = eq.get_instance_id("chambers", 1);
-  eq.zone_emote(13, "tick my_id: " .. my_id);
+  local my_id = eq.get_zone_instance_id();
   local my_list = eq.get_characters_in_instance(my_id);
 
-
   for k,v in pairs(my_list) do
-    eq.zone_emote(13, "k: " .. k .. " v: " .. v);
-    if (dt_box:contains(client:GetX(), client:GetY()) ) then
+    local client = eq.get_entity_list():GetClientByCharID(v);
+    if (client.valid) then 
+      if (client:GetX() > -64 or client:GetY() < 122 ) then
+        client:Message(13, "A deep voice booms in your head, 'This breach of the rules will not be tolerated. You must face the trials. Return to the arena or be subjected to pain.'");
+        if ( warnings >= 10 ) then
+          client:Message(13, "A deep voice booms in your head, 'You have been warned.  You did not heed the warnings.  Now you Die!'");
+          if (client:Admin() < 80) then 
+            client:Kill();
+          end
+        else 
+          warnings = warnings + 1;
+        end
+      end
     end
 
   end

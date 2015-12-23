2e8923886378a1f19adbc4f9081c15959bd13cbf
Athrogate <Shadrach145@gmail.com>
2015-12-23 10:15:24 -0800

    MPG Trials Update

--- chambersb/304022.lua
+++ chambersb/304022.lua
@@ -1,8 +1,8 @@
 function event_say(e)
   if (e.message:findi("hail")) then
-    e.self:Say("Do you wish to [" .. eq.say_link('leave', false, 'leave') .. "] this trial?");
+    e.self:Say("Do you wish to [ " .. eq.say_link('leave', false, 'leave') .. " ] this trial?");
   elseif (e.message:findi("leave")) then
-    e.other:MovePC(316,-928, 404, -306, 135); 
+    e.other:MovePC(316, 1093, -4825, -307, 130);
   end
 end
 

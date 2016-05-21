b75aa92a7b5eec2c2dad437ef2d3a63bf29b3c71
AsmoTiC <asmotic@hotmail.com>
2016-05-21 10:17:09 -0600

    Update Chamberlain_Krystorf.pl
    Revised Faction Reward/Hit for Giant Helmets quest to match Live

--- thurgadinb/Chamberlain_Krystorf.pl
+++ thurgadinb/Chamberlain_Krystorf.pl
@@ -7,9 +7,9 @@ sub EVENT_ITEM {
 if(plugin::check_handin(\%itemcount, 29062 => 4)){
   quest::say("Well done, $name, the Dain is pleased with your efforts. With a few more brave allies like you, we'll soon be tearing down the halls of Kael Drakkel.");
   quest::summonitem(quest::ChooseRandom(30215,30219,30212,30263));
-  quest::faction(49, 10);
-  quest::faction(67, 10);
-  quest::faction(179, -30);
+  quest::faction(49, 50);
+  quest::faction(67, 50);
+  quest::faction(179, -25);
   quest::exp(64000);
   quest::ding();
   quest::givecash(0,0,0,20);

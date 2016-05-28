85a15ca6deb860c344f9b9b5b96c0590a28dc0b4
AsmoTiC <asmotic@hotmail.com>
2016-05-28 12:01:57 -0600

    Fixed naming in quest text & minor bug
    %t was returning Dain Frostreaver IV instead of the person interacting with him.  Also, the dirk turnin event was failing on quest:ze since no text color was identified.

--- thurgadinb/#Dain_Frostreaver_IV.pl
+++ thurgadinb/#Dain_Frostreaver_IV.pl
@@ -44,7 +44,7 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
 #Handin for the 9th ring. Needs correct dialogue
   if(plugin::check_handin(\%itemcount, 1500 => 1, 30164 => 1) && ($faction <= 5 || $faction >= 8)) {
-    quest::say("%t, you have done a great service to my people. I had not imagined the treachery had run so deeply within our ranks. Here. Take this ring as your reward. From this day forth, you shall be known as the Hero of the Coldain. Take my Dirk as well, and if you wish to further aid us in our cause, then return it to me.");
+    quest::say("$name, you have done a great service to my people. I had not imagined the treachery had run so deeply within our ranks. Here. Take this ring as your reward. From this day forth, you shall be known as the Hero of the Coldain. Take my Dirk as well, and if you wish to further aid us in our cause, then return it to me.");
     quest::summonitem(30369); #9th ring
     quest::summonitem(1465); #dirk of the Dain
     quest::faction(49,50); #coldain
@@ -55,8 +55,8 @@ sub EVENT_ITEM {
   }
   #Tormax's head
   elsif(plugin::check_handin(\%itemcount, 30516 => 1) && $faction == 1) {
-    quest::say("You have done what no Coldain could do, %t! This is indeed a glorious say in our people's history. In return for your invaluable service I present you with the Tri-plated Golden Hackle Hammer. Its magic is powerful and I am sure it will serve you well.");
-    quest::ze("Let it be know from this day forth that %t and their companions are Heros of the Coldain Kingdom. King Tormax has been slain, it is a time for celebration. Let no tankard go unfilled!");
+    quest::say("You have done what no Coldain could do, $name! This is indeed a glorious say in our people's history. In return for your invaluable service I present you with the Tri-plated Golden Hackle Hammer. Its magic is powerful and I am sure it will serve you well.");
+    quest::ze(2, "Let it be know from this day forth that $name and their companions are Heros of the Coldain Kingdom. King Tormax has been slain, it is a time for celebration. Let no tankard go unfilled!");
     quest::summonitem(30502);
     quest::faction(49,100);
     quest::faction(67,100);
@@ -64,8 +64,8 @@ sub EVENT_ITEM {
   }
 #Dirk handin for the 10th ring
   elsif(plugin::check_handin(\%itemcount, 1465 => 1) && $faction == 1) {
-    quest::say("My good %t, you have served me well. You have flushed out all who sought to oppose me and my people. I am afraid I need to call upon you and your friends one final time. The dissention and treason ran deeper than I had anticipated. Our population has been cleansed, but we lost a full third of our army to the poisonous words of those rebels. In retaliation for your deeds, the Kromrif have made plans to attack us in this, our weakest hour. Can I count on your help outlander?");
-    quest::say("Several of our greatest officers, including a few veterans from the war on Yesterwinter are assembling just outside our city. Gather your army at once and give this parchment and the ninth ring to Sentry Badian. I will remain inside the city with a few of my troops to defend it against any who might penetrate your defense. May Brell be with you, %t.");
+    quest::say("My good $name, you have served me well. You have flushed out all who sought to oppose me and my people. I am afraid I need to call upon you and your friends one final time. The dissention and treason ran deeper than I had anticipated. Our population has been cleansed, but we lost a full third of our army to the poisonous words of those rebels. In retaliation for your deeds, the Kromrif have made plans to attack us in this, our weakest hour. Can I count on your help outlander?");
+    quest::say("Several of our greatest officers, including a few veterans from the war on Yesterwinter are assembling just outside our city. Gather your army at once and give this parchment and the ninth ring to Sentry Badian. I will remain inside the city with a few of my troops to defend it against any who might penetrate your defense. May Brell be with you, $name.");
     quest::summonitem(1567);
   }
   # Runed Coldain Prayer Shawl (7th shawl)

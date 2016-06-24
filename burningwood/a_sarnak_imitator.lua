f31c3b662ac66cfbf95b3dfc32dbd4da6688ff20
cream24 <pmh24@aol.com>
2016-06-23 21:24:19 -0400

    ench epic

--- burningwood/a_sarnak_imitator.lua
+++ burningwood/a_sarnak_imitator.lua
@@ -2,7 +2,18 @@
 
 
 function event_say(e)
-	if(e.message:findi("are you jeb lumsed")) then
+	local qglobals = eq.get_qglobals(e.other);	
+	if(e.message:findi("hail") and (e.other:HasItem(10650) or qglobals["ench_pre"] == "3")) then --has 1.0 or prequest (prequest not done)
+		e.self:Say("Much time has passed since the creation of the Staff of the Serpent. The power of discord is seeping into our fair world, and only the most powerful of us shall stand to force it back. The time has come for a new tool, and a new breed of enchanter. [" .. eq.say_link("Are you prepared") .. "] to meet this challenge?");
+		if(qglobals["ench_epic"] == nil) then
+			eq.set_global("ench_epic","1",5,"F"); --Flagged to start epic
+		end
+	elseif(e.message:findi("prepared") and qglobals["ench_epic"] >= "1") then	
+		e.self:Say("I have recently received this ancient tome; it contains knowledge that may prove invaluable to the construction of a new staff. Unfortunately, time has had its way with it. I need you to find the missing pages so we can get to work. There should be ten, unless they have been destroyed by time and decay.");
+		e.other:SummonItem(57918);		
+	elseif(e.message:findi("chest") and qglobals["ench_epic"] >= "3") then	
+		e.other:SummonItem(52960); --Ornate Staff Chest						
+	elseif(e.message:findi("are you jeb lumsed")) then
 		e.self:Say("Yes, I am. Use your most enlightened magic to seek the truth of what is around you. When that is done, you will know what you seek.");
 	elseif(e.message:findi("the serpent")) then
 		e.self:Say("The first truly powerful enchanter, Krilan Pedin, crafted the Serpent years ago. He trained his students to carry on the trade in a five-step process. Each student then became a master and chose one of their own students to pass the legacy on to. Four others and I, myself, are current practitioners of the crafting.");
@@ -32,6 +43,22 @@ function event_trade(e)
 		e.other:Faction(342,30);
 		e.other:AddEXP(1500000);
 		e.other:SummonItem(10650);
+	elseif(item_lib.check_turn_in(e.trade, {item1 = 54267})) then
+		e.self:Say("As I suspected, this is something extraordinary. You must go speak to Maelin at once. It has been many moons since I have entered the Plane of Knowledge. He may well have some new information for me. Go at once and tell him I sent you.");
+		eq.set_global("ench_epic","2",5,"F");
+	elseif(item_lib.check_turn_in(e.trade, {item1 = 52959})) then
+		e.self:Say("Interesting indeed. I see that I was not wrong in my instinct to begin this as quickly as possible. I have created a list of the items that you will need to begin the construction of this tool. Mind you, it will not be a simple thing. When you have gathered them all return to me. Would you like a [" .. eq.say_link("chest") .. "] to carry them in?");
+		e.other:SummonItem(54286); --Assembling the Staff
+		eq.set_global("ench_epic","3",5,"F");
+	elseif(item_lib.check_turn_in(e.trade, {item1 = 52966})) then
+		e.self:Say("You have done well, my student. The staff is now complete. In order to stand up to the true fury of discord, it will need purifying, but even in this state it shall protect you well.");
+		e.other:SummonItem(52952); --Oculus 1.5
+		e.other:AddAAPoints(5);
+		e.other:Ding();
+		e.other:Message(15,'You have gained 5 ability points!');
+		e.other:SetAATitle('Coercer');		
+		eq.set_global("ench_epic","7",5,"F");
+		
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end

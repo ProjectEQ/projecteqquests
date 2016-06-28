2d6e84e6583cc063725d05a5fc65bae849e66b57
cream24 <pmh24@aol.com>
2016-06-28 13:20:35 -0400

    serker fix

--- kodtaz/player.lua
+++ kodtaz/player.lua
@@ -3,7 +3,7 @@ function event_click_door(e)
   local door_id = e.door:GetDoorID();
 	if(door_id == 25 or door_id == 26 or door_id == 27) then
 	  local qglobals = eq.get_qglobals(e.self);
-		if(e.self:Class() == "Berserker" and qglobals["kt_fern"] == nil) then
+		if(e.self:Class() == "Berserker" and qglobals["kt_" .. door_id .. "fern"] == nil) then
 				e.self:SummonItem(16949); --Taelosian Fern Sample 
 				eq.set_global("kt_" .. door_id .. "fern","1",3,"H1")
 		end

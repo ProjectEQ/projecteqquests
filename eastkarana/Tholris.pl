e00db9e635bcb447a70d1e5c40404cc8d0352ab9
cream24 <pmh24@aol.com>
2016-06-29 07:04:06 -0400

    ranger fix

--- eastkarana/Tholris.pl
+++ eastkarana/Tholris.pl
@@ -1,5 +1,6 @@
 # Tholris
 #
+my $saydisease;
 
 sub EVENT_SIGNAL {
   if ($signal == 99) {
@@ -7,6 +8,36 @@ sub EVENT_SIGNAL {
   }
 }
 
+sub EVENT_SPAWN {
+  $x = $npc->GetX();
+  $y = $npc->GetY();
+  quest::set_proximity($x - 10, $x + 10, $y - 10, $y + 10);
+}
 
+sub EVENT_ENTER {
+  if(plugin::check_hasitem($client, 62846)) { 
+		$client->Message(15,"The crystal glows warm in the presence of Althele and Tholris.");
+		$saydisease=0;
+  }
+}
+
+sub EVENT_SAY {
+	if(($text=~/you are diseased/i) && plugin::check_hasitem($client, 62846)) {
+		$saydisease=$saydisease+1;
+		if($saydisease==1) {
+			quest::emote("looks at you indignantly");
+		}
+		elsif($saydisease==2) {
+			quest::emote("kicks dirt at your feet");
+		}
+		elsif($saydisease==3) {
+			$saydisease=0;
+			quest::emote("shakes his head. Then he catches a glimpse of the Mind Crystal and looks thoughtful for a moment. Then he reaches into his pocket and produces two unusual crystals and hands them to you.");
+			quest::summonitem(62618);
+			quest::summonitem(62619);
+			quest::signalwith(15044,11111,1);
+		}
+	}
+}
 # EOF zone: eastkarana ID: 15043 NPC: Tholris
 

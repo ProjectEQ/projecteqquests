0958ac5a5a1fdf5576fd065bed2e23e22f9d032a
cream24 <pmh24@aol.com>
2016-07-20 10:11:11 -0400

    warr 1.5 fix

--- dranik/player.lua
+++ dranik/player.lua
@@ -1,7 +1,6 @@
 function event_loot(e)
 	if(e.self:Class() == "Warrior" and e.item:GetID() == 60321 and e.corpse:GetNPCTypeID()==336091) then
 		local qglobals = eq.get_qglobals(e.self);
-		e.self:Say("You are too weak for me! Die now by my hand!");
 		if(qglobals["warrior_epic"] == "15" and qglobals["warrior_epic_dranik"] == nil ) then
 			eq.spawn2(283157,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- #a chest (Epic 1.5)
 			eq.set_global("warrior_epic_dranik","1",5,"F");

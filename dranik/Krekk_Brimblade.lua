ccfd9c407f6242750e6617cdc3fcde8ca1dcd0be
cream24 <pmh24@aol.com>
2016-07-20 10:04:28 -0400

    warr 1.5 fix

--- dranik/Krekk_Brimblade.lua
+++ dranik/Krekk_Brimblade.lua
@@ -9,6 +9,7 @@ function event_say(e)
 		e.self:SetSpecialAbility(35, 0);
 		e.self:AddToHateList(e.other,1);
 	end
+	eq.set_global("warrior_epic","14",5,"F");
 end
 
 function event_combat(e)
@@ -25,9 +26,9 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 	local qglobals = eq.get_qglobals(e.other);
-	if(item_lib.check_turn_in(e.trade, {item1 = 60320, item2 = 60306, item3 = 60303}) and qglobals["warrior_epic"] == "14") then --Blackfall Blade, Potion of Blackfall Spirit and Hilt of Eternal Power
+	if(qglobals["warrior_epic"] == "14" and item_lib.check_turn_in(e.trade, {item1 = 60320, item2 = 60306, item3 = 60303})) then --Blackfall Blade, Potion of Blackfall Spirit and Hilt of Eternal Power
 		e.self:Say("You've been working very closely with my brother, I see. No doubt he wants my help to finish da sword for you. I'll put da sword together, you can rest assured of that. But I won't be giving it back to you. Oh no, I think I'll keep it for myself. I'll be da one to continue da Brimblade legacy and strike fear into da hearts of men! Not him!");
-		eq.set_global("warrior_epic","15",5,"F");
+		--eq.set_global("warrior_epic","15",5,"F");
 		e.self:SetSpecialAbility(19, 0);
 		e.self:SetSpecialAbility(20, 0);
 		e.self:SetSpecialAbility(24, 0);

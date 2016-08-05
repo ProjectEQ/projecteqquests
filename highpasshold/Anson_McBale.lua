bd53b5ddec9209d4d26cff6814ebbdbe986c4629
cream24 <pmh24@aol.com>
2016-08-05 14:45:52 -0400

    rogue epic fixes

--- highpasshold/Anson_McBale.lua
+++ highpasshold/Anson_McBale.lua
@@ -17,10 +17,24 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-
-	if(item_lib.check_turn_in(e.trade, {item1 = 28014}) and e.other:GetClass() == "Rogue" and e.other:GetLevel() >= 50) then
-		e.self:Say("Oh, I see.");
+	if(item_lib.check_turn_in(e.trade, {item1 = 28014}) and e.other:Class() == "Rogue" and e.other:GetLevel() >= 50) then
+		e.self:Say("Ah, we have been expecting this. Let me get Stanos, he will want to inspect it first, but here are your coins.");
+		e.other:Ding();
+		e.other:Faction(149,5,0);
+		e.other:Faction(31,10,0);
+		e.other:Faction(214,5,0);
+		e.other:Faction(53,5,0);
+		e.other:Faction(105,5,0);
+		e.other:AddEXP(500);
+		e.other:Message(15,"You receive 100 gold from Anson McBale.");
+		e.other:Message(15,"You receive 25 platinum from Anson McBale.")
+		e.other:AddMoneyToPP(0, 0, 100, 25, true);
 		eq.spawn2(5088,0,0,336,10,45,225);
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
+end
+
+function event_signal(e)
+	e.self:Say("Vilnius has always had a good eye for talent. I think we can trust this one. But will he trust us? You have to wonder if he even knows who we are...");
+	eq.signal(5088, 0); --stanos
 end

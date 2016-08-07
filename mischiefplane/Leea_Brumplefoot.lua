49f6c87656b23a71fa0bd6389536bb8fc0312cc9
cream24 <pmh24@aol.com>
2016-08-07 19:26:38 -0400

    rogue 1.5

--- mischiefplane/Leea_Brumplefoot.lua
+++ mischiefplane/Leea_Brumplefoot.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	local qglobals = eq.get_qglobals(e.other);
-	if( qglobals["Fatestealer_shak"] == "1" and e.message:findi("blue orb")) then
+	if( (qglobals["Fatestealer_shak"] == "1" or e.other:HasItem(52355) == true or e.other:HasItem(52356) == true) and e.message:findi("blue orb")) then
 		e.other:SummonItem(9822); 
 	end
 end

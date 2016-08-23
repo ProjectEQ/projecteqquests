bc30b13661f1eb86e835e5202f24b29c9798183a
Michael Cook (mackal) <mcook@mackal.net>
2016-08-22 22:43:05 -0400

    Dire/Dragon stuff for bard 1.5

---
+++ stonebrunt/#a_mountain_dragon.lua
@@ -0,0 +1,17 @@
+-- bard 1.5
+-- no pre-reqs
+local dire_id = 250029 -- spawn2 ID
+
+function event_combat(e)
+    if (e.joined == false and e.self:GetHPRatio() < 97) then
+        eq.set_timer("depop", 12000)
+    end
+end
+
+function event_timer(e)
+    if (e.self:GetHPRatio() == 100) then
+        eq.update_spawn_timer(dire_id, 0)
+        eq.depop()
+    end
+end
+

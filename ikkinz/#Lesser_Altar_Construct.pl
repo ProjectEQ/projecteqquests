0f4bfa78829ff5d0f33ad2d1ede5cf5719f417d7
Michael Cook (mackal) <mcook@mackal.net>
2016-08-31 13:29:39 -0400

    more leash fixes

--- ikkinz/#Lesser_Altar_Construct.pl
+++ ikkinz/#Lesser_Altar_Construct.pl
@@ -3,7 +3,7 @@ sub EVENT_AGGRO {
 }
 
 sub EVENT_TIMER {
-  if($x > 720 || $x < 500 || $y < -300 || $y > -120) {
+  if($x > 690 || $x < 500 || $y < -300 || $y > -120) {
     #$npc->GMMove(531,-157,-49,126);
 	#send it back to one of the 4 spawn points.
 	my $c = quest::ChooseRandom(1,2,3,4);

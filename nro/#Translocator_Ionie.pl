6e36108cdb9a35fff3026a73f2801d5d913da0bd
cream24 <pmh24@aol.com>
2016-10-16 23:06:45 -0400

    use saylink

--- nro/#Translocator_Ionie.pl
+++ nro/#Translocator_Ionie.pl
@@ -1,5 +1,5 @@
 sub EVENT_SAY { 
-if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Iceclad] in the meantime, I can transport you to my companion there."); }
+if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [" . quest::saylink("travel to Iceclad") . "] in the meantime, I can transport you to my companion there."); }
 if ($text=~/travel to iceclad/i){
 quest::selfcast("2284"); }
 }

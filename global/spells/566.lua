5e9f68c56409e16f86a429963fe18f1ef49fc449
cream24 <pmh24@aol.com>
2016-10-16 23:04:11 -0400

    revert to old zones for all clients

--- global/spells/566.lua
+++ global/spells/566.lua
@@ -5,11 +5,11 @@ function event_spell_effect(e)
     local client = e.target:CastToClient();
 
     -- If Rof2 client; move them to east commonland ldon camp.
-    if (bit.band(client:GetClientVersionBit(), 4294967264) ~= 0) then
-      client:MovePC(22, -144, -1543, 2, 122);
-    else
+    --if (bit.band(client:GetClientVersionBit(), 4294967264) ~= 0) then
+    --  client:MovePC(22, -144, -1543, 2, 122);
+    --else
       client:MovePC(21, 1839, 2, -15, 60);
-    end
+    --end
   end
 
   return 1;

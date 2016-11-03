a6336bbfc783436b39b8bd94512ea28a16b7d859
huffin <acg1089@aol.com>
2016-11-03 10:34:12 -0500

    Update Paruek_the_Strong.pl
    add swarms every 55s

--- postorms/Paruek_the_Strong.pl
+++ postorms/Paruek_the_Strong.pl
@@ -1,5 +1,22 @@
 #Notifies Jeplak_Lord_of_Srerendi that npc was killed in the camp.
+sub EVENT_AGGRO {
+    quest::settimer("tumadds",55);
+}
 
 sub EVENT_DEATH_COMPLETE {
-  quest::signal(210179,0);
+    quest::signal(210179,0);
+}
+
+sub EVENT_TIMER {
+  if($timer == "tumadds") {
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    quest::spawn2(210247,0,0,($x + 10),$y,$z,$h);
+    }
 }

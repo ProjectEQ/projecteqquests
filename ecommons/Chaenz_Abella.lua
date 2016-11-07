ceaf4cd615098514849ab4fe701b3e27b985c00d
Athrogate <Shadrach145@gmail.com>
2016-11-07 12:06:48 -0800

    Fixing a typo

--- ecommons/Chaenz_Abella.lua
+++ ecommons/Chaenz_Abella.lua
@@ -12,7 +12,7 @@ function event_say(e)
         {'LDON_rujg', 'LDoN Raid: Rujarkian Hills: Hidden Vale of Deceit'}
       }
 
-      local request = instance_requests.ValidateRequest('raid', "urjg", 50, 6, 54, 61, {any = {41000, 40999}}, e.other, lockout_globals);
+      local request = instance_requests.ValidateRequest('raid', "rujg", 50, 6, 54, 61, {any = {41000, 40999}}, e.other, lockout_globals);
       if (request.valid and request.flags == 1) then
         instance_requests.DisplayLockouts(e.other, e.other, lockout_globals)
       elseif (request.valid and request.flags == 0) then

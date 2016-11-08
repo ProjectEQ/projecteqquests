28040973bb4bf7d93a17b039bbda5d5bc3581545
Michael Cook (mackal) <mcook@mackal.net>
2016-11-08 14:26:47 -0500

    CLR epic is in, this isn't needed anymore

--- WorkInProgress/Omens of War Cleric Epic/Skyfire/Zordak_Ragefire.lua
+++
@@ -1,5 +0,0 @@
--- 1.0 Cleric Epic // Water Sprinkler of Nem Ankh // -- Drogerin
-
-function event_death_complete(e)
-		eq.spawn2(91090,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- Spawns Zordakalicus Ragefire
-end
--- WorkInProgress/Omens of War Cleric Epic/Wakening/Player.lua
+++
@@ -1,8 +0,0 @@
-
-
-function event_loot(e)
-	-- Cleric is on current step of 1.5
-	if(e.item:GetID() == 9954)
-		eq.spawn(A chest); -- need information for this chest.
-	end
-end

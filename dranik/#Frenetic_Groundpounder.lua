f69f74585a5fb09deae056543a63224267bc0277
huffin <acg1089@aol.com>
2016-11-15 06:00:16 -0600

    Update #Frenetic_Groundpounder.lua
    fix

--- dranik/#Frenetic_Groundpounder.lua
+++ dranik/#Frenetic_Groundpounder.lua
@@ -1,8 +1,12 @@
 function event_death_complete(e)
   
+  local qglobals = eq.get_qglobals();
+  
   local entity_list = eq.get_entity_list();
   
-  if (eq.get_entity_list():GetMobByNpcTypeID(336239) == false) then
+  local check_frenetic = entity_list:GetMobByNpcTypeID(336239);
+  
+  if (not check_frenetic.valid and qglobals["frenetic_br"] == nil ) then
     eq.set_global("frenetic_br","1",3,"H2");
     eq.signal(336240,0);
   end  

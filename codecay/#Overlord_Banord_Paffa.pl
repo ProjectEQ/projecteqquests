7959684a2eb19a82a06ce5be21d0e8288d923f72
huffin <acg1089@aol.com>
2016-11-15 06:21:28 -0600

    Update #Overlord_Banord_Paffa.pl
    re arrange timers

--- codecay/#Overlord_Banord_Paffa.pl
+++ codecay/#Overlord_Banord_Paffa.pl
@@ -8,8 +8,6 @@ sub EVENT_SPAWN {
 sub EVENT_ENTER {
   quest::emote("A deep voice bellows from just ahead saying, 'Come fools see if you can match the power of Banord and his puslings.  Puslings come your master has need of you!' Wretched howls of the twisted puslings begin to echo throughout the room.");
   quest::settimer("paffa_1",190);
-  quest::settimer("paffa_2",380);
-  quest::settimer("paffa_3",570);
 }
 
 sub EVENT_TIMER {
@@ -22,6 +20,8 @@ sub EVENT_TIMER {
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::stoptimer("paffa_1");
     
+    quest::settimer("paffa_2",190);
+    
   } elsif ($timer == "paffa_2") {
     quest::emote("A crazed chant echoes through the room as Banord calls for more minions to attack.");
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
@@ -30,6 +30,8 @@ sub EVENT_TIMER {
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::stoptimer("paffa_2");
+    
+    quest::settimer("paffa_3",190);
     
   } elsif ($timer == "paffa_3") {
     quest::emote("A loud and maddened scream of rage is heard as Banord prepares to attack.");
@@ -40,7 +42,7 @@ sub EVENT_TIMER {
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(200059,0,0,$x,$y,$z,$h);
     
-    quest::depop_withtimer();
     quest::stoptimer("paffa_3");
+    quest::depop_withtimer();
   }
 }

4de4b1ccb9fec99227eaeb3ac9077024d9e98faa
rosspulliam <ross.pulliam@outlook.com>
2016-11-16 21:41:19 -0600

    Update Battlemaster_Rhorious

--- dranik/Ikaav_Salisa_Mexmielk.lua
+++ dranik/Ikaav_Salisa_Mexmielk.lua
@@ -1,4 +1,6 @@
-function event_death_complete(e)
-  eq.set_global("ikaav_br","1",3,"H2");
-  eq.signal(336240,0);
+--dranik/Ikaav_Salisa_Mexmielk.lua
+--Spawned as part of #Battlemaster_Rhorious.lua event
+
+function event_death(e)
+  eq.signal(336240,91); --Signal to Rhorious_Chest_Spawner.lua
 end

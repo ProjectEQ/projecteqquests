53ea89b248697fda3a84e1f7708de6b3df5d734f
Athrogate <Shadrach145@gmail.com>
2016-11-18 05:14:26 -0800

    Fixing the oh so great perl syntax error == v/s eq

--- codecay/#Overlord_Banord_Paffa.pl
+++ codecay/#Overlord_Banord_Paffa.pl
@@ -10,13 +10,14 @@ sub EVENT_SPAWN {
 sub EVENT_ENTER {
     if ($repeat_flag == 0) {
       quest::emote("A deep voice bellows from just ahead saying, 'Come fools see if you can match the power of Banord and his puslings.  Puslings come your master has need of you!' Wretched howls of the twisted puslings begin to echo throughout the room.");
-      quest::settimer("paffa_1",190);
+      quest::settimer("paffaa",190);
       $repeat_flag = 1;
     }
 }
 
 sub EVENT_TIMER {
-  if($timer == "paffa_1") {
+  if($timer eq "paffaa") {
+    quest::stoptimer($timer);
     quest::emote("A crazed chant echoes through the room as Banord calls for more minions to attack.");
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
@@ -24,10 +25,10 @@ sub EVENT_TIMER {
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     
-    quest::settimer("paffa_2",190);
+    quest::settimer("paffab",190);
   }
     
-  if ($timer == "paffa_2") {
+  if ($timer eq "paffab") {
     quest::emote("A crazed chant echoes through the room as Banord calls for more minions to attack.");
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
@@ -35,10 +36,10 @@ sub EVENT_TIMER {
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     
-    quest::settimer("paffa_3",190);
+    quest::settimer("paffac",190);
   }
     
-  if ($timer == "paffa_3") {
+  if ($timer eq "paffac") {
     quest::emote("A loud and maddened scream of rage is heard as Banord prepares to attack.");
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);
     quest::spawn2(quest::ChooseRandom(200003,200004),0,0,$x,$y,$z,$h);

ee9eba71c3a5b18e4abda833350bcee2bfba5b88
cream24 <pmh24@aol.com>
2016-11-19 12:16:17 -0500

    beast 1.5

--- draniksscar/player.lua
+++ draniksscar/player.lua
@@ -21,3 +21,13 @@ function event_timer(e)
 		end
 	end
 end
+
+function event_loot(e)
+	if(e.self:Class() == "Beastlord" and e.item:GetID() == 52907) then
+		local qglobals = eq.get_qglobals(e.self);
+		if(qglobals["beast_epic"] == "15" and qglobals["beast_epic_ds"] == nil ) then
+			eq.spawn2(283157,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- #a chest (Epic 1.5)
+			eq.set_global("beast_epic_ds","1",5,"F");
+		end
+	end	
+end

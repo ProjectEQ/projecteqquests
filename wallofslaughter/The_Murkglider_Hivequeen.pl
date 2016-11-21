10d2e385b63c5557b866dbbcb815de99b191f67a
huffin <acg1089@aol.com>
2016-11-20 19:20:12 -0600

    Update The_Murkglider_Hivequeen.pl
    spaces make the world go round

--- wallofslaughter/The_Murkglider_Hivequeen.pl
+++ wallofslaughter/The_Murkglider_Hivequeen.pl
@@ -1,14 +1,14 @@
 sub EVENT_AGGRO {
-  quest::settimer("eggs",40);
+  quest::settimer("eggs", 40);
 }
 
 sub EVENT_TIMER {
   if($timer eq "eggs") {
     if($npc->IsEngaged()) {
       quest::emote("The Murkglider Hivequeen  spews several slimy eggs unto the ground.  They begin to quiver almost immediately.");
-      quest::spawn2(300083,0,0,($x + 10),$y,$z,$h);
-      quest::spawn2(300083,0,0,($x + 10),$y,$z,$h);
-      quest::spawn2(300083,0,0,($x + 10),$y,$z,$h);
+      quest::spawn2(300083,0,0,$x,$y,$z,$h);
+      quest::spawn2(300083,0,0,$x,$y,$z,$h);
+      quest::spawn2(300083,0,0,$x,$y,$z,$h);
       } else {
       quest::stoptimer("eggs");
     }

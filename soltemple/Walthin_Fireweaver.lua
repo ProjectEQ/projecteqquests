87c99aab167a46a3b351c950f8ae2f9ecc997e31
Michael Cook (mackal) <mcook@mackal.net>
2016-11-27 17:34:26 -0500

    Add live text for Walthin Fireweaver

--- soltemple/Walthin_Fireweaver.lua
+++ soltemple/Walthin_Fireweaver.lua
@@ -5,14 +5,16 @@ function event_say(e)
         e.self:Say("Good day to you!  I am Walthin Fireweaver of the League of Antonican Bards.  My friend Cryssia and I are here while we deal in [" .. eq.say_link("lambent stones") .. "] with the followers of Solusek Ro.  If you are interested, Cryssia and I also deal in [" .. eq.say_link("lambent armor") .. "], the perfect armor for a bard of standing.  I also dabble in some research from time to time.  I'm a bit of a historian.  I study the history of [" .. eq.say_link("dragons") .. "].")
     elseif e.message:findi("dragons") then
         e.self:Say("I'm utterly fascinated by all things draconic.  There are many tomes in the temple's libraries that catalog much of their species' history.  Some day I'll have to travel to the frozen land of Velious and see what other secrets I can learn there.")
+    elseif e.message:findi("lambent stones") then
+        e.self:Say("Lambent stones are gemstones of great power.  They are midnight blue in color - if you want more information on them you should ask Genni.")
     elseif e.message:findi("lambent armor") then
         e.self:Say("Lambent armor is custom-crafted armor, made especially for bards. It is forged by the MeadowGreen brothers and then enchanted by the followers of Solusek Ro. If you are interested, I can tell you about [" .. eq.say_link("lambent gauntlets") .. "], [" .. eq.say_link("lambent greaves") .. "] and [" .. eq.say_link("lambent boots") .. "]. Cryssia can tell you about other lambent armor.")
     elseif e.message:findi("lambent gauntlets") then
-        e.self:Say("Alright, I need you to retrieve for me some Black Silk Gloves, a Lambent Star Ruby, and Shin Gauntlets. Return to me with these and I will craft your gauntlets.")
+        e.self:Say("Lambent gauntlets are exceptionally well made gauntlets.  If you are interested, I will give you a pair, but you will have to bring me the following items that I need for a trade with the followers of Solusek Ro.  I need shin gauntlets from a froglok knight and black silk gloves from Castle Mistmoore.  You will also need to talk to Genni about getting me a lambent star ruby.  Bring me these items and you will earn lambent gauntlets.")
     elseif e.message:findi("lambent greaves") then
-        e.self:Say("Very well, return to me with these items and I will craft them. A set of Icy Greaves, a Lambent Fire Opal, and Shin Greaves.")
+        e.self:Say("There are no finer leggings for a bard than lambent greaves.  Collect for me two pairs of lesser greaves and I will give you some.  Bring me icy greaves from the caverns of Permafrost and shin greaves from a ghoul knight, as well as a lambent fire opal and I will bestow a pair of lambent greaves upon you .")
     elseif e.message:findi("lambent boots") then
-        e.self:Say("Alright, I will require some items. Go and get me some Firewalker Boots, a Lambent Sapphire, and the middle piece of the Rune of the One Eye. Return to me with these items and I will craft your boots.")
+        e.self:Say("Lambent boots are the boots of choice for most bards.  If you can bring me firewalker boots from a Solusek Mage and the middle portion of the rune of the One Eye from Froon then I ill give you a pair of lambent boots.  Oh, I almost forgot! I will need a lambent sapphire as well.")
     end
 end
 

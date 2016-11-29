20ddd6b521fbd0b151b67c92681782f8cd94eb64
Michael Cook (mackal) <mcook@mackal.net>
2016-11-29 17:26:47 -0500

    Add Metala Von Korach spawn

--- potorment/#a_bruised_woman.lua
+++ potorment/#a_bruised_woman.lua
@@ -6,5 +6,15 @@ end
 
 function event_trade(e)
     local item_lib = require("items")
+    if item_lib.check_turn_in(e.trade, {item1 = 77629}) then -- Vhalen's Necklace
+        local entity_list = eq.get_entity_list()
+        e.self:Emote("'s eyebrows shift into the shadow of a quizzical expression as you show her the necklace.  A flash of recognition crosses her face and she jerks back across the floor in horror.  A screeching wail tears through your mind 'You are a fool Vahlen!  You send these peasants to do your dirty work, you never were much for getting your hands dirty!  The path of pain is the only way to true power.  I will never turn from it!")
+        entity_list:MessageClose(e.self, true, 120, 1, "You notice a distinct change in the air around you as something inside your packs grows hot and heavy.  Black tendrils sprout from your pack as the words of Hsagra echo in your mind.  Beware the orb!")
+        entity_list:MessageClose(e.self, true, 120, 1, "Black ooze begins to float out from " .. e.other:GetName() .. "'s body and wrap around Metala's body as she twitches and spasms violently.")
+        entity_list:MessageClose(e.self, true, 120, 1, "As you struggle to regain your balance and equilibrium you look on in horror as Metala's body begins to twitch and jerk.  Bones snapping and sinew stretching.  Her face seems to struggle against her bones as they contort into a horrific new visage of torment.")
+        eq.spawn2(207084, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading())
+        e.other:QuestReward(e.self, 0, 0, 0, 0, 77629, 1); -- Vhalen's Necklace
+        eq.depop_with_timer()
+    end
     item_lib.return_items(e.self, e.other, e.trade)
 end

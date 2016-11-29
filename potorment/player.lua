c40f746ad423b005861d1a22a0b6813d0eb67fde
Michael Cook (mackal) <mcook@mackal.net>
2016-11-29 18:14:48 -0500

    Fix Metala chest

--- potorment/player.lua
+++ potorment/player.lua
@@ -1,6 +1,6 @@
 function event_loot(e)
     -- bard 1.5 Metala chest
-    if e.self:GetClass() == 8 and e.item:GetID() == 77613 then
+    if e.self:GetClass() == 8 and e.item:GetID() == 77630 then
         local qglobals = eq.get_qglobals(e.self)
         if e.self:HasItem(77629) and qglobals["bard15_metalahest"] == nil then
             eq.spawn2(283157, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading())

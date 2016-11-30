40a3f99b5a2c5d605cb59c36aef8f1e9d3767ef7
Michael Cook (mackal) <mcook@mackal.net>
2016-11-30 16:04:40 -0500

    Add drac event cool down
    Also add possibility to respawn event if someone deletes the orb (need for 2.0)

--- necropolis/player.lua
+++ necropolis/player.lua
@@ -19,7 +19,9 @@ function event_enter_zone(e)
         if qglobals["hsagra_cooldown"] == nil and qglobals["bard15"] == "2" then
             eq.unique_spawn(123172, 0, 0, -2716, -293, 12, 41)
         end
-        if qglobals["bard15"] == "3" then
+        -- we want to let them respawn the event if they deleted the orb so they can do 2.0
+        -- Should probably add a 2.0 check here eventually
+        if qglobals["drac_cooldown"] == nil and qglobals["bard15"] ~= nil and qglobals["bard15"] >= "3" and not e.self:HasItem(77613) then
             local entity_list = eq.get_entity_list()
             -- we can probably get away with just doing unique_spawn but lets just be safe
             if entity_list:IsMobSpawnedByNpcTypeID(123173) == false and entity_list:IsMobSpawnedByNpcTypeID(123174) == false then

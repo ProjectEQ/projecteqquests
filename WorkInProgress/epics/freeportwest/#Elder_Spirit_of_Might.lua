2fd90639bae5c0df40ae268970c3b4720e508613
Michael Cook (mackal) <mcook@mackal.net>
2016-12-15 13:31:06 -0500

    Nuke WIP folder, see #112

--- WorkInProgress/epics/freeportwest/#Elder_Spirit_of_Might.lua
+++
@@ -1,7 +0,0 @@
---freeportwest/#Elder_Spirit_of_Might.lua NPCID 
---Shaman Epic 2.0
-function event_signal(e)
-	if(e.signal == 1) then
-		e.self:Say("How can you forgive me? I turned on you and I do remember trying to harm the very one you asked to save me. That shaman risked all to take part in saving me.");
-	end
-end	

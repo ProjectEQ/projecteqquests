08ac133012c006d3de5f90b567813751cb77f7ef
cream24 <pmh24@aol.com>
2016-12-17 07:43:24 -0500

    monk 1.5 fix again

--- harbingers/Windrush.lua
+++ harbingers/Windrush.lua
@@ -5,7 +5,7 @@ function event_say(e)
 		e.self:Say("Leave before I send you to your god.");
 	elseif(qglobals["monk_epic"] >= "7" and e.message:findi("body")) then
 		e.other:Message(6,"As you speak you can see that you have stirred something from within this creature");	
-		e.self:Say("I know what you seek but I now hold all the power that is Kaiaren's mind!");
+		e.self:Say("I will teach you to disturb what you cannot change!");
 		e.self:SetSpecialAbility(19, 0);
 		e.self:SetSpecialAbility(20, 0);
 		e.self:SetSpecialAbility(24, 0);
@@ -17,10 +17,19 @@ end
 function event_combat(e)
   if (e.joined == true) then
 	eq.set_timer("depop", 1800 * 1000);
-	eq.set_timer("stun", 6 * 1000);
-	eq.set_timer("spectrum", 3 * 1000);
-	eq.set_timer("say",60*1000);
+	eq.set_next_hp_event(70);
   end
+end
+
+function event_hp(e)
+	if(e.hp_event == 70) then
+		e.self:Say("I know what you seek but I now hold all the power that is Kaiaren's mind!");
+		eq.set_next_hp_event(40);
+		eq.set_timer("stun", 1 * 1000);
+	elseif(e.hp_event == 40) then
+		e.self:Say("Fools! You cannot fathom the power an enlightened mind has.");
+		eq.set_timer("spectrum", 1 * 1000);
+	end
 end
 
 function event_timer(e)
@@ -33,8 +42,6 @@ function event_timer(e)
 	elseif(e.timer=="stun") then
 		eq.stop_timer("stun");
 		e.self:CastSpell(1968, e.self:GetTarget():GetID());	
-		eq.set_timer("stun", 20 * 1000);	
-	elseif(e.timer=="say") then
-		e.self:Say("Fools! You cannot fathom the power an enlightened mind has.");
+		eq.set_timer("stun", 20 * 1000);
 	end
 end

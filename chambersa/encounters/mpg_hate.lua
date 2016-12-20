c908c068e20eb61d02f8fd8db8bcbcebd91b399d
Athrogate <Shadrach145@gmail.com>
2016-12-20 06:53:46 -0800

    Moved deathtouch tick from encounter to a lua file so it doesn't double up in the 2 versions.

--- chambersa/encounters/mpg_hate.lua
+++ chambersa/encounters/mpg_hate.lua
@@ -255,28 +255,6 @@ function Feran_Death(e)
   eq.signal(304017, 1);
 end
 
-function Deathtouch_Tick(e)
-  local my_id = eq.get_zone_instance_id();
-  local my_list = eq.get_characters_in_instance(my_id);
-
-  for k,v in pairs(my_list) do
-    local client = eq.get_entity_list():GetClientByCharID(v);
-    if (client.valid) then 
-      if (client:GetX() > -64 or client:GetY() < 122 ) then
-        client:Message(13, "A deep voice booms in your head, 'This breach of the rules will not be tolerated. You must face the trials. Return to the arena or be subjected to pain.'");
-        if ( warnings >= 10 ) then
-          client:Message(13, "A deep voice booms in your head, 'You have been warned.  You did not heed the warnings.  Now you Die!'");
-          if (client:Admin() < 80) then 
-            client:Kill();
-          end
-        else 
-          warnings = warnings + 1;
-        end
-      end
-    end
-  end
-end
-
 function Feran_Spawn(e)
   -- 24 Will Not Aggro
   -- 25 Immune to Aggro
@@ -304,8 +282,6 @@ function event_encounter_load(e)
   eq.register_npc_event('mpg_hate', Event.death_complete, 304017, Hate_Death);
   eq.register_npc_event('mpg_hate', Event.signal,         304017, Hate_Signal);
   eq.register_npc_event('mpg_hate', Event.combat,         304017, Hate_Combat);
-
-  eq.register_npc_event('mpg_hate', Event.tick,           304021, Deathtouch_Tick);
 
   eq.register_npc_event('mpg_hate', Event.death_complete, 304019, Feran_Death);
   eq.register_npc_event('mpg_hate', Event.death_complete, 304023, Feran_Death);

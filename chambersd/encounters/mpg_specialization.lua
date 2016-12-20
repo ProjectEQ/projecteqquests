b71352e1c110f70fe8cf0df4dc57dbb76beb7fcf
Athrogate <Shadrach145@gmail.com>
2016-12-20 07:06:47 -0800

    Moved deathtouch tick from encounter to a lua file so it doesn't double up in the 2 versions.

--- chambersd/encounters/mpg_specialization.lua
+++ chambersd/encounters/mpg_specialization.lua
@@ -75,6 +75,7 @@ function Boss_Say(e)
 
     -- Enable the Death Toucher
     eq.spawn_condition('chambersd', instance_id, 5, 1 );
+    eq.spawn2(307006,0,0,0,0,0,0);
 
     CastSpells(e);
     eq.set_timer('spell_timer', spell_timer  * 1000);
@@ -149,6 +150,7 @@ function Boss_Death(e)
 
   -- Disable the Death Toucher
   eq.spawn_condition('chambersd', instance_id, 5, 0 );
+  eq.depop_all(307006);
 
   -- Spawn a Greedy Dwarf to distribute loot until all MPG Raid Trials are complete
   eq.spawn2(304028, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
@@ -158,35 +160,12 @@ function Boss_Death(e)
   mpg_helper.UpdateRaidTrialLockout(player_list, this_bit, lockout_name);
 end
 
-function Deathtouch_Tick(e)
-  local my_id = eq.get_zone_instance_id();
-  local my_list = eq.get_characters_in_instance(my_id);
-
-  for k,v in pairs(my_list) do
-    local client = eq.get_entity_list():GetClientByCharID(v);
-    if (client.valid) then 
-      if (client:GetX() > -64 or client:GetY() < 122 ) then
-        client:Message(13, "A deep voice booms in your head, 'This breach of the rules will not be tolerated. You must face the trials. Return to the arena or be subjected to pain.'");
-        if ( warnings >= 10 ) then
-          client:Message(13, "A deep voice booms in your head, 'You have been warned.  You did not heed the warnings.  Now you Die!'");
-          if (client:Admin() < 80) then 
-            client:Kill();
-          end
-        else 
-          warnings = warnings + 1;
-        end
-      end
-    end
-  end
-end
-
 function event_encounter_load(e)
   eq.register_npc_event('mpg_specialization', Event.say,            307007, Boss_Say);
   eq.register_npc_event('mpg_specialization', Event.spawn,          307007, Boss_Spawn);
   eq.register_npc_event('mpg_specialization', Event.timer,          307007, Boss_Timer);
   eq.register_npc_event('mpg_specialization', Event.death_complete, 307007, Boss_Death);
 
-  eq.register_npc_event('mpg_specialization', Event.tick,           304021, Deathtouch_Tick);
 end
 
 function event_encounter_unload(e)

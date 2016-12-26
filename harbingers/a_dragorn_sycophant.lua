d5353184c9d79af405ed8f71e3cafbd48001c74e
huffin <acg1089@aol.com>
2016-12-26 07:18:49 -0600

    Update a_dragorn_sycophant.lua

--- harbingers/a_dragorn_sycophant.lua
+++ harbingers/a_dragorn_sycophant.lua
@@ -1,5 +1,5 @@
-function event_death(e)
-  if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(335051) == false) then
+function event_death_complete(e)
+  if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(335051) == false) then
     eq.spawn2(335051, 0, 0, -1403, 391.99, 570.58, 134.6);
     eq.spawn2(335014, 0, 0, -1422, 383, 570.58, 134.6);
     eq.spawn2(335014, 0, 0, -1422, 383, 570.58, 134.6);

ecc5c52b4252ae93b31bc04c8bef2947216b7a1b
Michael Cook (mackal) <mcook@mackal.net>
2016-12-27 17:52:32 -0500

    Huffers

--- harbingers/a_dragorn_sycophant.lua
+++ harbingers/a_dragorn_sycophant.lua
@@ -1,7 +1,7 @@
 function event_death_complete(e)
   eq.signal(335062,10);
   if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(335051) == false) then
-    eq.spawn2(335051, 0, 0, -1403, 391.99, 570.58, 134.6);
+    eq.spawn2(335051, 0, 0, -1403, 391.99, 570.58, 134.6);
     eq.spawn2(335014, 0, 0, -1422, 383, 570.58, 134.6);
     eq.spawn2(335014, 0, 0, -1422, 383, 570.58, 134.6);
     eq.spawn2(335014, 0, 0, -1422, 383, 570.58, 134.6);

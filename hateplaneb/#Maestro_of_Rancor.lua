b7e64da070f513fa94dd6a965d83ac1ab52f747e
huffin <acg1089@aol.com>
2017-01-10 07:11:28 -0600

    Update #Maestro_of_Rancor.lua

--- hateplaneb/#Maestro_of_Rancor.lua
+++ hateplaneb/#Maestro_of_Rancor.lua
@@ -8,6 +8,8 @@ function event_hp(e)
     eq.spawn2(186191,0,0,-117.37,-483,35.88,64.6);
     e.self:ModifyNPCStat("hp_regen", "10000");
     eq.set_next_hp_event(40);
+    eq.zone_emote(10, "A haunting tune floats through the music hall.  Someone or SOMETHING has begun playing the pipe organ.");
+    eq.zone_emote(10, "Maestro of Rancor chortles, 'This song is sweet to my senses. Keep up with the dance mortals... IF YOU CAN!");
   elseif (e.hp_event == 40) then
     eq.spawn2(186192,0,0,-291,-421,23.23,128);
     eq.spawn2(186193,0,0,-333,-480,23.23,64.6);

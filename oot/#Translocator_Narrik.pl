3f52de7dbf7cd946a380ba749717f95543905929
cream24 <pmh24@aol.com>
2017-01-15 12:47:58 -0500

    translocate fix

--- oot/#Translocator_Narrik.pl
+++ oot/#Translocator_Narrik.pl
@@ -1,5 +1,8 @@
 sub EVENT_SAY { 
 if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [" . quest::saylink("travel to Freeport") . "] or [" . quest::saylink("travel to Butcherblock") . "] in the meantime, I can transport you to my companion there."); }
-if ($text=~/travel to freeport/i){quest::selfcast("2283"); }
+if ($text=~/travel to freeport/i){
+  quest::say("Off you go!");
+  quest::movepc(10,-1006.66,-14.65,-53.47,64);
+}
 if ($text=~/travel to butcherblock/i){quest::selfcast("2281"); } 
 } 

b14238525b5c632ef16eaee75b21568ced71fdfa
cream24 <pmh24@aol.com>
2017-02-28 15:30:26 -0500

    remove emotes

--- rujd/a_Rujarkian_blackhand.lua
+++
@@ -1,9 +0,0 @@
-function event_combat(e)
-	if e.joined then
-		e.self:Say("You dare confront me?!  All attack!")
-	end
-end
-
-function event_death_complete(e)
-	e.self:Emote("'s corpse draws a final breath while clawing at its fatal wounds.")
-end
--- rujd/a_Rujarkian_shaman.lua
+++
@@ -1,9 +0,0 @@
-function event_combat(e)
-	if e.joined then
-		e.self:Say("You will steal no more!  All of this is ours!")
-	end
-end
-
-function event_death_complete(e)
-	e.self:Emote("'s corpse mumbles a fading, final prayer.")
-end

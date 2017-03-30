4cc7d6cc172db0ac7de176d458386fbaa49ede84
Athrogate <Shadrach145@gmail.com>
2017-03-30 07:22:48 -0700

    Abstracted lockouts to lockouts_def.lua

---
+++ lua_modules/lockouts_def.lua
@@ -0,0 +1,51 @@
+local Lockouts_Def = {}
+
+function Lockouts_Def.Lockout_Globals()
+	return {			
+			{ "potimeb_lockout", "Plane of Time"},
+			{ "lockout_ikky_g1", "Ikkinz Group Trial #1: Singular Might"},
+			{ "lockout_ikky_g2", "Ikkinz Group Trial #2: Twin Struggles"},
+			{ "lockout_ikky_g3", "Ikkinz Group Trial #3: Tri-Fates"},
+			{ "ikkylockout3", "Ikkinz Raid #1: Chambers of Righteousness"},
+			{ "ikkylockout4", "Ikkinz Raid #2: Chambers of Glorification"},
+			{ "ikkylockout5", "Ikkinz Raid #3: Chambers of Transcendence"},
+			{ "ikkylockout6", "Ikkinz Raid #4: Chambers of Destruction"},
+			{ "uqualockout", "Uqua, the Ocean God Chantry"},
+			{ "Ink_Kelekdrix", "Kelekdrix, Herald of Trushar" },
+			{ "Ink_Mites", "Stonemite Event" },
+			{ "Ink_Golems", "Inktu`Ta Golems" },
+			{ "Ink_Callers", "Cursecaller Event" },
+			{ "Ink_Noqufiel", "Noqufiel Event" },
+      { "Tacvi_PXK", "Tacvi: Pixtt Xxeric Kex" },
+      { "Tacvi_PKK", "Tacvi: Pixtt Kretv Krakxt" },
+      { "Tacvi_PRT", "Tacvi: Pixtt Riel Tavas" },
+      { "Tacvi_ZMKP", "Tacvi: Zun`Muram Kvxe Pirik" },
+      { "Tacvi_ZMSB", "Tacvi: Zun`Muram Shaldn Boc" },
+      { "Tacvi_ZMMD", "Tacvi: Zun`Muram Mordl Delt" },
+      { "Tacvi_ZMYV", "Tacvi: Zun`Muram Yihst Vor" },   
+      { "Tacvi_TMCV", "Tacvi: Tunat`Muram Cuu Vauax" },
+	    { "LDON_rujd", "LDoN Raid: Rujarkian Hills: Prison Break"},
+      { "LDON_rujg", "LDoN Raid: Rujarkian Hills: Hidden Vale of Deceit"},
+	    { "mirb_50_lockout", "LDoN Raid: Miragul's Menagerie: Frozen Nightmare"},
+	    { "LDON_mirg", "LDoN Raid: Miragul's Menagerie: Folly of Miragul's Ambition"},	  
+      { "LDON_mmcc", "LDoN Raid: Mistmoore Catacombs: Struggles within the Progeny"},
+      { "LDON_takc", "LDoN Raid: Takish-Hiz: Within the Compact" },
+	    { "LDON_guke", "LDoN Raid: Deepest Guk: The Curse Reborn"},
+      { "LDON_gukg", "LDoN Raid: Deepest Guk: Ritualist of Hate"},
+      { "MPG_fear", "MPG: The Mastery of Fear"},
+      { "MPG_weaponry", "MPG: The Mastery of Weaponry"},
+      { "MPG_subversion", "MPG: The Mastery of Subversion"},
+      { "MPG_efficiency", "MPG: The Mastery of Efficiency"},
+      { "MPG_ingenuity", "MPG: The Mastery of Ingenuity"},
+      { "MPG_destruction", "MPG: The Mastery of Destruction"},
+      { "MPG_hatred", "MPG: Trial of Hatred" },
+      { "MPG_specialization", "MPG: Trial of Specialization" },
+      { "MPG_endurance", "MPG: Trial of Endurance" },
+      { "MPG_foresight", "MPG: Trial of Foresight" },
+      { "MPG_adaptation", "MPG: Trial of Adaptation" },
+      { "MPG_corruption", "MPG: Trial of Corruption" },
+	    { "paladinEpicDranikCata","Dranik Catacombs A" }
+  }
+end
+
+return Lockouts_Def;

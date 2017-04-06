e7bdb6afc61c5c1a0e9473750db33774275e484a
huffin <acg1089@aol.com>
2017-04-06 17:31:06 -0500

    Update 301076.lua
    flavor flav

--- bloodfields/301076.lua
+++ bloodfields/301076.lua
@@ -1,4 +1,5 @@
 function event_death_complete(e)
 eq.spawn2(301077,0,0,-1257,144,-913,0);
+eq.zone_emote(15, "Attracted by the recent commotion, a reclusive Girplan emerges from its hiding spot.");
 end
 -- 5th girplan spawns 6th (named)

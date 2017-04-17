977651761076a4992d590a3725046ace6e874455
Michael Cook (mackal) <mcook@mackal.net>
2017-04-17 16:49:46 -0400

    MNK 2.0 WIP

---
+++ povalor/#Disciple_of_Tranquility.lua
@@ -0,0 +1,40 @@
+function event_say(e)
+    local qglobals = eq.get_qglobals(e.other);
+
+    if (qglobals["mnk_epic20"] == "3") then
+        if (e.message:findi("hail")) then
+            e.self:Say("I see you bear the Sign of the Order. I already know why you are here, " .. e.other:GetName() .. ". If it is my seal you wish to obtain you must [" .. eq.say_link("battle") .. "] me.");
+        elseif (e.message:findi("challenge")) then
+            e.self:Say("Prepare yourself then " .. e.other:GetName() .. ", for this will not be a battle you will want to lose.");
+            e.self:SetSpecialAbility(SpecialAbility.immune_aggro, 0);
+            e.self:SetSpecialAbility(SpecialAbility.immune_aggro_on, 0);
+            e.self:SetSpecialAbility(SpecialAbility.no_harm_from_client, 0);
+        end
+    end
+end
+
+function event_combat(e)
+    if (e.joined == true) then
+        eq.set_timer("start_strike", 1000);
+        eq.set_timer("start_blinding", 2000);
+    else
+        eq.stop_all_timers();
+    end
+end
+
+function event_timer(e)
+    if (e.timer == "start_strike") then
+        e.self:CastSpell(3168, e.self:GetID());
+        eq.stop_timer(e.timer);
+        eq.set_timer("cast_strike", 45000);
+    elseif (e.timer == "start_blinding") then
+        e.self:CastSpell(3166, e.self:GetID());
+        eq.stop_timer(e.timer);
+        eq.set_timer("cast_blinding", 60000);
+    elseif (e.timer == "cast_strike") then
+        e.self:CastSpell(3168, e.self:GetID());
+    elseif (e.timer == "cast_blinding") then
+        e.self:CastSpell(3166, e.self:GetID());
+    end
+end
+
---
+++ tenebrous/#Disciple_of_Focus.lua
@@ -0,0 +1,33 @@
+function event_say(e)
+    local qglobals = eq.get_qglobals(e.other);
+
+    if (qglobals["mnk_epic20"] == "3") then
+        if (e.message:findi("hail")) then
+            e.self:Say("I see you have finally come. I have sensed someone with the I see you have finally come. I have sensed someone with the must know if this is the case. If so, you will [" .. eq.say_link("challenge") .. "] me.");
+        elseif (e.message:findi("challenge")) then
+            e.self:Say("Prepare yourself then " .. e.other:GetName() .. ", for this will not be a battle you will want to lose.");
+            e.self:SetSpecialAbility(SpecialAbility.immune_aggro, 0);
+            e.self:SetSpecialAbility(SpecialAbility.immune_aggro_on, 0);
+            e.self:SetSpecialAbility(SpecialAbility.no_harm_from_client, 0);
+        end
+    end
+end
+
+function event_combat(e)
+    if (e.joined == true) then
+        eq.set_timer("startcast", 1000);
+    else
+        eq.stop_all_timers();
+    end
+end
+
+function event_timer(e)
+    if (e.timer == "startcast") then
+        e.self:CastSpell(3751, e.self:GetID());
+        eq.stop_timer(e.timer);
+        eq.set_timer("cast", 24000);
+    elseif (e.timer == "cast") then
+        e.self:CastSpell(3751, e.self:GetID());
+    end
+end
+

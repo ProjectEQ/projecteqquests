308365bf58a65cc9a3696688cc236f9140af7582
cream24 <pmh24@aol.com>
2017-04-19 18:25:36 -0400

    monk 2.0 fix, anguish aug spawn msg

--- povalor/#Disciple_of_Tranquility.lua
+++ povalor/#Disciple_of_Tranquility.lua
@@ -4,7 +4,7 @@ function event_say(e)
     if (qglobals["mnk_epic20"] == "3") then
         if (e.message:findi("hail")) then
             e.self:Say("I see you bear the Sign of the Order. I already know why you are here, " .. e.other:GetName() .. ". If it is my seal you wish to obtain you must [" .. eq.say_link("battle") .. "] me.");
-        elseif (e.message:findi("challenge")) then
+        elseif (e.message:findi("battle")) then
             e.self:Say("Prepare yourself then " .. e.other:GetName() .. ", for this will not be a battle you will want to lose.");
             e.self:SetSpecialAbility(SpecialAbility.immune_aggro, 0);
             e.self:SetSpecialAbility(SpecialAbility.immune_aggro_on, 0);

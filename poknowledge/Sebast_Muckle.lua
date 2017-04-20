f9015a1b289368d65bc57eb6c14b9af9d799fbbf
Michael Cook (mackal) <mcook@mackal.net>
2017-04-20 00:43:29 -0400

    Fix MNK 2.0

--- poknowledge/Sebast_Muckle.lua
+++ poknowledge/Sebast_Muckle.lua
@@ -12,7 +12,7 @@ function event_trade(e)
         e.other:AddAAPoints(10);
         e.other:Message(15, "You have gained 10 ability point(s)!")
         e.self:Say("I see you have found a way to bring order and balance back to the world. You should have also learned of the need for such evils in the world. There cannot be good without evil just as there cannot be truth without deception. I will take the discordant globe now with your fistwraps and make something wonderful for you from them. They will help guide you through the most difficult of times as a reminder of the knowledge you now possess for without the balance between good and evil we are but lost.");
-        e.other::QuestReward(e.self, 0, 0, 0, 0, 67742, 1000); -- Transcended Fistwraps of Immortality
+        e.other:QuestReward(e.self, 0, 0, 0, 0, 67742, 1000); -- Transcended Fistwraps of Immortality
     end
 end
 

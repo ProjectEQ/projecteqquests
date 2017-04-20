e5b42c932727b35ebde7723a01d7c2bfda893e7c
Michael Cook (mackal) <mcook@mackal.net>
2017-04-19 23:51:41 -0400

    Update #Disciple_of_Tranquility.lua

--- povalor/#Disciple_of_Tranquility.lua
+++ povalor/#Disciple_of_Tranquility.lua
@@ -25,7 +25,9 @@ end
 
 function event_timer(e)
     if (e.timer == "start_strike") then
-        e.self:CastSpell(3168, e.self:GetID());
+        if (e.self:GetTarget().valid) then
+            e.self:CastSpell(3168, e.self:GetTarget():GetID());
+        end
         eq.stop_timer(e.timer);
         eq.set_timer("cast_strike", 45000);
     elseif (e.timer == "start_blinding") then
@@ -35,9 +37,13 @@ function event_timer(e)
         eq.stop_timer(e.timer);
         eq.set_timer("cast_blinding", 60000);
     elseif (e.timer == "cast_strike") then
-        e.self:CastSpell(3168, e.self:GetID());
+        if (e.self:GetTarget().valid) then
+            e.self:CastSpell(3168, e.self:GetTarget():GetID());
+        end
     elseif (e.timer == "cast_blinding") then
-        e.self:CastSpell(3166, e.self:GetID());
+        if (e.self:GetTarget().valid) then
+            e.self:CastSpell(3166, e.self:GetTarget():GetID());
+        end
     end
 end
 

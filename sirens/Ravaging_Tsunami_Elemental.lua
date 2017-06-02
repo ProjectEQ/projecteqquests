6ba01cd0d836b94a5a58cf401855335a67a59710
Kilzpeq <slickpitt@gmail.com>
2017-06-02 10:03:01 -0500

    Create Ravaging_Tsunami_Elemental.lua
    Mage 1.5 pre-quest

---
+++ sirens/Ravaging_Tsunami_Elemental.lua
@@ -0,0 +1,9 @@
+function event_trade(e)
+   local item_lib = require("items");
+   item_lib.check_turn_in(e.trade, {item1 = 11304}) then -- Vial of Elemental Binding
+      e.self:SetSpecialAbility(SpecialAbility.immune_aggro, 0);
+           e.self:SetSpecialAbility(SpecialAbility.no_harm_from_client, 0);
+      e.self:AddToHateList(e.other,1);
+   end
+   item_lib.return_items(e.self, e.other, e.trade)
+end

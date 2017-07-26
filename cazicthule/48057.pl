3d387ebe0b9ecac0bc56184a6ee8c9cfe1463fec
huffin <acg1089@aol.com>
2017-07-26 13:18:43 -0500

    Update 48057.pl

--- cazicthule/48057.pl
+++ cazicthule/48057.pl
@@ -13,10 +13,6 @@ sub EVENT_SIGNAL {
     # signal 1 = Wake Tahia Up 
     if ($signal == 1) {
         quest::say("Brave adventurers, I am Tahia Felwah, and I owe you my life. I was to be a sacrifice to the faceless. Listen to my words carefully, there isn't much time now.");
-
-        quest::pause(9);
-        
-        quest::SetRunning(0);
         quest::pathto(573, 1005, -94);
         quest::say("You have interrupted a complicated ritual and soon the rift to the Faceless will become unstable. I must collect three runes of the Faceless for my research. I have been appointed to uncover the source of these lizards' newfound power. If I fail, their kind will surely spread across the land and threaten the balance of Norrath.");
 
@@ -34,11 +30,8 @@ sub EVENT_TIMER {
 
         quest::say("This failed ritual presents a unique opportunity to gather the runes I desperately need. Recover the runes from the fallen minions of the faceless. When your need is greatest, hand me a rune and I will replenish you and your companions.");
 
-        quest::pause(14);
 
         quest::say("Prepare youselves! You must repel the denizens of Fear itself! Good luck brave souls, I will remain here until my task is fulfilled.");
-
-        quest::pause(4);
         
         quest::ze(1, "The walls of the temple begin to crack as the ground beneath your feet shakes violently.");
 

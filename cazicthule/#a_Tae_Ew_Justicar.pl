7e66608745b457e782749de58108f0537bcb75fa
huffin <acg1089@aol.com>
2017-08-03 09:22:54 -0500

    Update #a_Tae_Ew_Justicar.pl

--- cazicthule/#a_Tae_Ew_Justicar.pl
+++ cazicthule/#a_Tae_Ew_Justicar.pl
@@ -12,11 +12,13 @@ sub EVENT_SPAWN {
 sub EVENT_SIGNAL {
     # signal 1 = emote and blow up lizards
     if ($signal == 1) {
+        quest::settimer(1, 15);
+    
         quest::emote("looks paniced as planar forces shoot through it's body.");
 
         $npc->CastSpell($tragedy_at_cazic_thule_spell_id, 48029);
 
-        quest::settimer(1, 15);
+        
 
     }
 }
@@ -25,10 +27,11 @@ sub EVENT_TIMER {
 
     # timer 1 = waiting for Justicars to finish casting
     if ($timer == 1) {
-        quest::stoptimer(1);
+        
 
         # spawn unstable rift
         quest::spawn2($unstable_rift_id, 0,0, 587, 1090, -98, 180);
+        quest::stoptimer(1);
 
         quest:depopall(48029);
     }

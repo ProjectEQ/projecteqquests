d3bc6b2b53a01b2b14bb7020e3dc5ccca6d79ee9
huffin <acg1089@aol.com>
2017-08-03 08:59:55 -0500

    Update 48057.pl

--- cazicthule/48057.pl
+++ cazicthule/48057.pl
@@ -12,21 +12,22 @@ sub EVENT_SPAWN {
 sub EVENT_SIGNAL {
     # signal 1 = Wake Tahia Up 
     if ($signal == 1) {
+        quest::settimer(1, 12);
+        
         quest::say("Brave adventurers, I am Tahia Felwah, and I owe you my life. I was to be a sacrifice to the faceless. Listen to my words carefully, there isn't much time now.");
-        quest::pathto(573, 1005, -94);
+        
         quest::say("You have interrupted a complicated ritual and soon the rift to the Faceless will become unstable. I must collect three runes of the Faceless for my research. I have been appointed to uncover the source of these lizards' newfound power. If I fail, their kind will surely spread across the land and threaten the balance of Norrath.");
 
-        quest::settimer(1, 12);
+        quest::pathto(573, 1005, -94);
 
-    }
+    } 
     
 }
 
 sub EVENT_TIMER {
     if ($timer == 1) {
-        quest::stoptimer(1);
-
-        # Tahia has walked to waypoint 1 (start Justicars)
+        # signal Justicar
+        quest::signalwith($a_tae_ew_justicar_id, 1, 0);
 
         quest::say("This failed ritual presents a unique opportunity to gather the runes I desperately need. Recover the runes from the fallen minions of the faceless. When your need is greatest, hand me a rune and I will replenish you and your companions.");
 
@@ -35,13 +36,13 @@ sub EVENT_TIMER {
         
         quest::ze(1, "The walls of the temple begin to crack as the ground beneath your feet shakes violently.");
 
-        # signal Justicars
-        quest::signalwith($a_tae_ew_justicar_id, 1, 0);
+        
 
 
         # spawn the 2nd version of Tahia Felwah
         quest::spawn2($tahia_felwah_2_id, 0,0, 573, 1005, -94, 0);
-
+        quest::stoptimer(1);
+        # Tahia has walked to waypoint 1 (start Justicars)
         # depop version 1 of Tahia Felwah
         quest::depop();
     }

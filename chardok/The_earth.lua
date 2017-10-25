9f721939454279ad47221e205538c3c08f36b4a6
huffin <acg1089@aol.com>
2017-10-25 06:53:18 -0500

    Create The_earth.lua
    traps essay

---
+++ chardok/The_earth.lua
@@ -0,0 +1,9 @@
+function event_spawn(e)
+eq.set_proximity(e.self:GetX() - 30, e.self:GetX() + 30, e.self:GetY() - 30, e.self:GetY() + 30);
+end
+
+function event_enter(e)
+e.self:Emote("at your feet erupts.");
+eq.spawn2(103093, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
+eq.depop_with_timer();
+end

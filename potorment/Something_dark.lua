543bee9a6998030dbed37a1de5d6f9cac900ca7d
huffin <acg1089@aol.com>
2017-11-02 17:07:12 -0500

    Update Something_dark.lua

--- potorment/Something_dark.lua
+++ potorment/Something_dark.lua
@@ -1,5 +1,6 @@
 function event_combat(e)
 if (e.joined == true) then
+e.self:Emote("and metallic drops from the mist!");
 eq.spawn2(207079, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
 eq.depop_with_timer();
 end

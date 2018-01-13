9172d9d9bf9b53790c521724f43fbd2882b7c19e
Akkadius <akkadius1@gmail.com>
2018-01-13 17:38:34 -0600

    Change plugins/MySQL.pl config loader method to JSON

--- plugins/MySQL.pl
+++ plugins/MySQL.pl
@@ -1,41 +1,36 @@
 #::: Akkadius
 #::: Description: These plugins are MySQL loaders in use with Perl DBI
-#::: For the best speed, manually define your variables instead of opening the XML file for every single connection establishment
-#::: 	This means using LoadMysql_Fast and renaming it to LoadMysql and manually installing the credentials, name the other one to LoadMysql_Old otherwise the subroutines will conflict
-#::: For plug and play, LoadMysql will load your database credentials from eqemu_config.xml
+#::: For plug and play, LoadMysql will load your database credentials from eqemu_config.json
 
-sub LoadMysql{	
+sub LoadMysql {	
 	use DBI;
 	use DBD::mysql;
-	# CONFIG VARIABLES
-	my $confile = "eqemu_config.xml"; #default
-	open(F, "<$confile") or die "Unable to open config: $confile\n";
-	my $indb = 0;
-	while(<F>) {
-		s/\r//g;
-		if(/<database>/i) { $indb = 1; }
-		next unless($indb == 1);
-		if(/<\/database>/i) { $indb = 0; last; }
-		if(/<host>(.*)<\/host>/i) { $host = $1; } 
-		elsif(/<username>(.*)<\/username>/i) { $user = $1; } 
-		elsif(/<password>(.*)<\/password>/i) { $pass = $1; } 
-		elsif(/<db>(.*)<\/db>/i) { $db = $1; }
-	}
-	#::: DATA SOURCE NAME
-	$dsn = "dbi:mysql:$db:localhost:3306";
-	#::: PERL DBI CONNECT
+	use JSON;
+	
+    my $json = new JSON();
+
+	#::: Load Config
+    my $content;
+    open(my $fh, '<', "eqemu_config.json") or die "cannot open file $filename"; {
+        local $/;
+        $content = <$fh>;
+    }
+    close($fh);
+
+	#::: Decode
+    $config = $json->decode($content);
+    
+	#::: Set MySQL Connection vars
+    $db = $config->{"server"}{"database"}{"db"};
+    $host = $config->{"server"}{"database"}{"host"};
+    $user = $config->{"server"}{"database"}{"username"};
+    $pass = $config->{"server"}{"database"}{"password"};
+	
+	#::: Map DSN
+	$dsn = "dbi:mysql:$db:$host:3306";
+	
+	#::: Connect and return
 	$connect = DBI->connect($dsn, $user, $pass);
+	
 	return $connect;
-}
-
-sub LoadMysql_Fast{	
-	use DBI;
-	use DBD::mysql;
-	# CONFIG VARIABLES
-	$host = 127.0.0.1;
-	$user = "user";
-	$pass = "pass";
-	$db = "databasename";
-	$dsn = "dbi:mysql:$db:localhost:3306";
-	return DBI->connect($dsn, $user, $pass);
 }

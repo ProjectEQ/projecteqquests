7dff64b7d0b6d173477519e5b5f04afb3ac11d14
Kurt Gilpin <kurt@Gilpin.info>
2018-01-14 13:39:47 -0600

    Updated with saylinks

--- pojustice/201078.pl
+++ pojustice/201078.pl
@@ -26,11 +26,11 @@ sub EVENT_SAY
 {   
    if(defined $qglobals{pop_poj_mavuin}) {
       if($text=~/Hail/i) {
-         quest::emote(" fixes you with a dark, peircing gaze. 'What do you want, mortal? Are you [prepared]?");
+	 quest::emote(" fixes you with a dark, peircing gaze. 'What do you want, mortal? Are you [" . quest::saylink("prepared") . "]?");
       }
       
       elsif($text=~/prepared/i) {
-         quest::say("Very well. When you are ready, you may begin the trial of execution. The victim will perish should the hooded executioner reach him. Its life will end only when all of the nemeses which accompany it also perish. We shall judge the mark of your success.");
+         quest::say("Very well. When you are ready, you may [" . quest::saylink("begin the trial of execution", 0, "begin the trial of execution") . "]. The victim will perish should the hooded executioner reach him. Its life will end only when all of the nemeses which accompany it also perish. We shall judge the mark of your success.");
       }
       
       elsif($text=~/begin the trial of execution/i) {

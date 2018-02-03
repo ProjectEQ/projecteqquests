bca2442b00e9bfec641a6364e3bcb05f61ae9791
Michael Cook (mackal) <mcook@mackal.net>
2018-02-03 18:28:09 -0500

    Add a reset to Zun, not sure if correct

--- dranik/336141.lua
+++ dranik/336141.lua
@@ -7,6 +7,11 @@ function event_hp(e)
     if (e.hp_event == 85) then
         e.self:SetPseudoRoot(false);
         eq.stop_timer("terror");
+        eq.set_next_inc_hp_event(99);
+    elseif (e.inc_hp_event == 99) then -- not sure on how to reset ... probably should :P
+        e.self:GotoBind();
+        e.self:SetPseudoRoot(true);
+        eq.set_next_hp_event(85);
     end
 end
 

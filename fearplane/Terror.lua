740e3452d59bb76ae25b8d7fa5ad9944b4d14c37
Michael Cook (mackal) <mcook@mackal.net>
2018-02-20 13:37:22 -0500

    Move fear golems to spell tables

--- fearplane/Terror.lua
+++ fearplane/Terror.lua
@@ -1,18 +1,3 @@
-function event_timer(e)
-	if e.timer=="dt" then
-		e.self:CastSpell(982, e.self:GetTarget():GetID()); --100k cazic touch
-	end
-end
-
-function event_combat(e)
-	if e.joined then
-		e.self:CastSpell(982, e.other:GetID()); --100k cazic touch
-		eq.set_timer("dt",30*1000);
-	else
-		eq.stop_timer("dt");
-	end
-end
-
 function event_signal(e)
 	entity_list = eq.get_entity_list();
 

7e58f41698deea2846d18740d3efb4a13b76c33f
JJ <jeremiah.oligario@gmail.com>
2018-02-23 10:56:02 -0500

    Order of precedence for Necro Skullcap #5.

--- overthere/93182.pl
+++ overthere/93182.pl
@@ -7,17 +7,18 @@
 # Necro skull cap 5, returns A metal key, you have to give it to Tin Banker Assistant
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 12848 => 1)){
-    quest::emote('a skeleton worker stops working and begins to open his creaking jaw.');
-    quest::say('I live to study and quench my thirst. I live to Bash the Faces of Pariah and entangle myself in the ivy of evergreen. I live. I want to remember.');
-    quest::summonitem(12848);
-  }
-  if (plugin::check_handin(\%itemcount, 12848 => 1, 12850 => 1, 12851 => 1, 12610 => 1)) { 
-    if ($ulevel>20) {
+  if (plugin::check_handin(\%itemcount, 12848 => 1, 12850 => 1, 12851 => 1, 12610 => 1)) {
+    if ($ulevel > 20) {
       quest::summonitem(12849);
-    } else {
-      quest::say('You will have to be more skilled to complete that quest.');
     }
+    else {
+      quest::say("You will have to be more skilled to complete that quest.");
+    }
+  }
+  elsif (plugin::check_handin(\%itemcount, 12848 => 1)) {
+    quest::emote("a skeleton worker stops working and begins to open his creaking jaw.");
+    quest::say("I live to study and quench my thirst. I live to Bash the Faces of Pariah and entangle myself in the ivy of evergreen. I live. I want to remember.");
+    quest::summonitem(12848);
   }
   plugin::return_items(\%itemcount);
 }

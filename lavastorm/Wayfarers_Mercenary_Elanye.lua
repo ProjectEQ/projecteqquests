9b1c26b70d0859ec0b47cff413f7201a1000529b
Michael Cook (mackal) <mcook@mackal.net>
2018-05-15 13:24:18 -0400

    This should be more accurate

--- lavastorm/Wayfarers_Mercenary_Elanye.lua
+++ lavastorm/Wayfarers_Mercenary_Elanye.lua
@@ -17,8 +17,11 @@ function event_say(e)
 		local NKF=e.other:GetFactionLevel(e.other:GetID(), e.self:GetID(), e.other:GetBaseRace(), e.other:GetClass(), e.other:GetDeity(), 429, e.self);
 		if (NKF >= 5) then
 			e.self:Say("I'm not that interested in becoming a servant of those followers of Firiona Vie myself, but I don't mind the cash they pay me. I keep most for me and throw a little back to the Wayfarers crew. If you're looking to get on their good side, I can make some money and you can gain their audience.");
-			eq.task_selector({273, 274}); --Blood of the Winged, Goblin Broth -- this isn't actually correct, you get one assigned based on something ...
-		else
+			-- so we get a random task here if we don't have one. Should we do like even/odd in game minute so everyone in the group can get the same?
+			-- Blood of the Winged, Goblin Broth
+			if (not e.other:IsTaskActive(273) and e.other:IsTaskActive(274)) then
+				e.other:AssignTask(eq.ChooseRandom(273, 274), e.self:GetID(), true);
+			end
 			e.self:Say("I've gotten acquainted with Norrath's Keepers well enough to know they might be interested in speaking with you.");
 			--e.self:Say("You can probably go try to speak to the Dark Reign for work. I've got nothing for you.");
 		end

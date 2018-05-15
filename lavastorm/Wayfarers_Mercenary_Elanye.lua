30361f9a686df4598cd0d1338fff51cf1f8b015d
Michael Cook (mackal) <mcook@mackal.net>
2018-05-15 01:09:06 -0400

    Update comment

--- lavastorm/Wayfarers_Mercenary_Elanye.lua
+++ lavastorm/Wayfarers_Mercenary_Elanye.lua
@@ -17,7 +17,7 @@ function event_say(e)
 		local NKF=e.other:GetFactionLevel(e.other:GetID(), e.self:GetID(), e.other:GetBaseRace(), e.other:GetClass(), e.other:GetDeity(), 429, e.self);
 		if (NKF >= 5) then
 			e.self:Say("I'm not that interested in becoming a servant of those followers of Firiona Vie myself, but I don't mind the cash they pay me. I keep most for me and throw a little back to the Wayfarers crew. If you're looking to get on their good side, I can make some money and you can gain their audience.");
-			eq.task_selector({273, 274}); --Blood of the Winged, Goblin Broth
+			eq.task_selector({273, 274}); --Blood of the Winged, Goblin Broth -- this isn't actually correct, you get one assigned based on something ...
 		else
 			e.self:Say("I've gotten acquainted with Norrath's Keepers well enough to know they might be interested in speaking with you.");
 			--e.self:Say("You can probably go try to speak to the Dark Reign for work. I've got nothing for you.");

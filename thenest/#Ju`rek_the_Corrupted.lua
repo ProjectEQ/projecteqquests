886f3740b3b92600fb06fd2566952de36773d030
huffin <acg1089@aol.com>
2018-08-31 17:56:06 -0500

    Create #Ju`rek_the_Corrupted.lua
    add jurek

---
+++ thenest/#Ju`rek_the_Corrupted.lua
@@ -0,0 +1,33 @@
+function event_combat(e)
+	if (e.joined == true) then
+		eq.set_next_hp_event(95);
+  end
+end
+
+function event_hp(e)
+	if (e.hp_event == 95) then
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.set_next_hp_event(80);
+  elseif (e.hp_event == 80) then
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.set_next_hp_event(60);
+  elseif (e.hp_event == 60) then
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.set_next_hp_event(40);
+  elseif (e.hp_event == 40) then
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.set_next_hp_event(20);
+  elseif (e.hp_event == 20) then
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+  end
+end

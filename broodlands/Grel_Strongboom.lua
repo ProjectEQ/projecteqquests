16368e8b802fe10581b24cbbc02d5901cc847073
huffin <acg1089@aol.com>
2018-10-08 10:40:01 -0500

    Update Grel_Strongboom.lua
    oh the disparity

--- broodlands/Grel_Strongboom.lua
+++ broodlands/Grel_Strongboom.lua
@@ -1,15 +1,15 @@
 function event_waypoint_arrive(e)
-	if(e.wp == 2) then
+	if(e.wp == 1) then
 		e.self:Say("Where's me bloody hat? I need it a'fore I can leave this despicable place!");
-	elseif(e.wp == 12) then
+	elseif(e.wp == 11) then
 		e.self:Say("Where's me bloody hat? I need it a'fore I can leave this despicable place!");
-  	elseif(e.wp == 20) then
+  	elseif(e.wp == 19) then
 		e.self:Say("Grr! I'll get me hat if it's the last thing I do! Minions! Search about to find me hat!");
-  	elseif(e.wp == 25) then
+  	elseif(e.wp == 24) then
 		e.self:Say("Filthy, disgusting, despicable place! I hope i can find me hat so I can get out of here quickly!");
-  	elseif(e.wp == 36) then
+  	elseif(e.wp == 35) then
 		e.self:Say("Where's me bloody hat? I need it a'fore I can leave this despicable place!");
-  	elseif(e.wp == 41) then
+  	elseif(e.wp == 40) then
 		e.self:Say("Arr! I'll look for it later around here. I'll be back you whelps! Grel will find his hat, count on it! Minions, stay here and guard my retreat!");
 	end
 end

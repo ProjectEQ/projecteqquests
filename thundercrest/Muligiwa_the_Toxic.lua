d5a30be8f8f9957f172ced05604a8266433e5727
huffin <acg1089@aol.com>
2019-01-06 18:39:42 -0600

    Create  Muligiwa_the_Toxic.lua

---
+++ thundercrest/Muligiwa_the_Toxic.lua
@@ -0,0 +1,21 @@
+function event_combat(e)
+if (e.joined == true) then
+eq.stop_timer('depop');
+else
+eq.set_timer('depop', 10 * 1000);
+end
+end
+
+function event_timer(e)
+if (e.timer == 'spawn') then
+eq.stop_timer('depop');
+eq.signal(2000903,1);
+end
+end
+
+function event_death_complete(e)
+if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000902) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000901) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000900) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000899) == false eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000903) == true) then
+eq.spawn2(340032, 0, 0, -2844, 3143, 307, 0);
+eq.depop_with_timer(2000903);
+end
+end

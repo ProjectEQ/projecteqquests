7a48c7d73956d2f57d1150382990f34b0a5f6767
huffin <acg1089@aol.com>
2019-01-06 19:25:09 -0600

    Update Clayforged_Protector.lua

--- thundercrest/Clayforged_Protector.lua
+++ thundercrest/Clayforged_Protector.lua
@@ -14,7 +14,7 @@ end
 end
 
 function event_death_complete(e)
-if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000902) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000901) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000900) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000899) == false eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000903) == true) then
+if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000902) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000901) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000900) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000899) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(2000903) == true) then
 eq.spawn2(340032, 0, 0, -2844, 3143, 307, 0);
 eq.depop_with_timer(2000903);
 end

ace64a4969587ec55189217c901106b623e23f4f
huffin <acg1089@aol.com>
2019-01-07 11:37:00 -0600

    Create #Stormfront.lua

---
+++ thundercrest/#Stormfront.lua
@@ -0,0 +1,5 @@
+function event_death_complete(e)
+if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(340397) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(340399) == false) then
+eq.signal(340398,1);
+end
+end

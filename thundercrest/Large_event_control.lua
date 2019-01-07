daf99a642d1e04eee7af3cf3cedddb6b837b0dfd
huffin <acg1089@aol.com>
2019-01-07 07:54:46 -0600

    Update Large_event_control.lua
    change ids

--- thundercrest/Large_event_control.lua
+++ thundercrest/Large_event_control.lua
@@ -5,22 +5,22 @@ end
 function event_timer(e)
   if (e.timer == 'spawn') then
     eq.stop_timer('spawn');
-    eq.spawn2(2000902, 0, 0, -2969, 3380, 275, 199);
-    eq.spawn2(2000900, 0, 0, -2970, 3002, 272, 61);
-    eq.spawn2(2000901, 0, 0, -2715, 3002, 275, 457);
-    eq.spawn2(2000899, 0, 0, -2715, 3380, 272, 325);
+    eq.spawn2(340389, 0, 0, -2969, 3380, 275, 199);
+    eq.spawn2(340388, 0, 0, -2970, 3002, 272, 61);
+    eq.spawn2(340387, 0, 0, -2715, 3002, 275, 457);
+    eq.spawn2(340386, 0, 0, -2715, 3380, 272, 325);
   end
 end
 
 function event_signal(e)
   if (e.signal==1) then
-    eq.depop_all(2000902);
-    eq.depop_all(2000900);
-    eq.depop_all(2000901);
-    eq.depop_all(2000899);
-    eq.unique_spawn(2000902, 0, 0, -2969, 3380, 275, 199);
-    eq.unique_spawn(2000900, 0, 0, -2970, 3002, 272, 61);
-    eq.unique_spawn(2000901, 0, 0, -2715, 3002, 275, 457);
-    eq.unique_spawn(2000899, 0, 0, -2715, 3380, 272, 325);
+    eq.depop_all(340389);
+    eq.depop_all(340388);
+    eq.depop_all(340387);
+    eq.depop_all(340386);
+    eq.unique_spawn(340389, 0, 0, -2969, 3380, 275, 199);
+    eq.unique_spawn(340388, 0, 0, -2970, 3002, 272, 61);
+    eq.unique_spawn(340387, 0, 0, -2715, 3002, 275, 457);
+    eq.unique_spawn(340386, 0, 0, -2715, 3380, 272, 325);
   end
 end

2625c276061d06501c018124247a090da9139f40
huffin <acg1089@aol.com>
2019-01-07 11:36:17 -0600

    Create #Thunderhead.lua

---
+++ thundercrest/#Thunderhead.lua
@@ -0,0 +1,5 @@
+function event_death_complete(e)
+eq.spawn2(340399,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+eq.spawn2(340399,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+eq.spawn2(340399,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+end

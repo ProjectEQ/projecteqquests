963de2a817d9a4a197725f8255b4bdd11b2dae8e
huffin <acg1089@aol.com>
2019-01-08 19:23:07 -0600

    Update #Raging_Thunderhead.lua
    flavor flav

--- thundercrest/#Raging_Thunderhead.lua
+++ thundercrest/#Raging_Thunderhead.lua
@@ -4,3 +4,7 @@ e.self:SetSpecialAbility(24, 0);
 e.self:SetSpecialAbility(35, 0);
 end
 end
+
+function event_spawn(e)
+eq.zone_emote(15,"A storm rages to the west, building up strength as it devours lesser storms.");
+end

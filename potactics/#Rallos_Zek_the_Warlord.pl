d693d02c37d322fe6253ee4d48b34e4e189fbe17
huffin <acg1089@aol.com>
2019-04-11 16:19:51 -0500

    Update #Rallos_Zek_the_Warlord.pl
    moar flavor

--- potactics/#Rallos_Zek_the_Warlord.pl
+++ potactics/#Rallos_Zek_the_Warlord.pl
@@ -6,6 +6,7 @@
 my @spawn2list = (44462,44639,44640,44641,44642,44646,44654,44655,44656,44657,44664,44665,44666,44667,44674,44675,44676,44677,44678,44679,44681,44682,44687,44688,44702,44703,44705,44708,44709,44710,44711,44712,44713,44715,44716,44719,44720,44721,44722,44723,44724,44727,44728,44729,44737,44738,44742,44743,44744,44745,44746,44747,44748,44749,44758,44759,44760,44761,44762,44763,44764,44765,44775,44776,44780,44781,44782,44680);
 
 sub EVENT_SPAWN {
+	quest::ze(0, "A great cry echoes across the field of blood and through the halls of Drunder. The creatures in the arena flee to avoid the impending doom.");
 	quest::settimer(2,1800);   # 30 min to kill him
 	foreach my $spawn2 (@spawn2list) {   # Depsawn all the mobs in the pit
 		#disable the spawn2 entry for all the pit mobs.

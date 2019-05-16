5609e852aa40b167368bf517b2e81937a7deb742
Paul Coene <pcoene1@twc.com>
2019-05-16 10:38:16 -0400

    Add required faction checks.  Fix some text to match live.

--- grobb/Treskar.lua
+++ grobb/Treskar.lua
@@ -2,13 +2,17 @@
 -- Quest for SK GM Treskar in Grobb
 
 function event_say(e)
+    local pfaction = e.other:GetFaction(e.self);
+
 	if(e.message:findi("hail")) then
 		e.self:Say("You dare speak to Master Treskar!! You be [sent by Hukulk] or you be hurtin'!! Me have no time to waste with ugly one like you!!");
+	elseif(pfaction > 4)
+		reject(e, pfaction);
 	elseif(e.message:findi("sent by hukulk")) then
 		e.self:Say("Ha!! Hukulk accept puny troll now?!! Ha!! You join us and you join fight. Nightkeep enemy is " .. e.other:GetName() .. " enemy!! You help smash [other weak shadowknights]. Them weak. We true power!! You bash good and maybe you do [secret mission] for Treskar. Or I has sum other [work] fer you.");
 	elseif(e.message:findi("other weak shadowknights")) then
-		e.self:Say("Ha! Dem Ogre Greenbloods are weaklings. Dems your enemy. Frogloks are your enemy. Smash all Greenbloods! Smash all Frogloks!");
-	elseif(e.message:findi("secret mission")) then -- Should have a faction check. However, since the Trolls haven't eaten you, we'll let you do it.
+		e.self:Say("Other shadowknights is ogre shadowknights!  They name is Greenbloods.  They no match for Nightkeep!  We tear arms from ogre bodies.  You not as powerful as Treskar so you smash young Greenbloods.  You hunt YOUNG members of Greenblood shadowknights.  You bring me black shadow tunic and me share treasures from Nightkeep vault with puny " .. e.other:GetName() .. ". Go!");
+	elseif(e.message:findi("secret mission")) then
 		e.self:Say("Nightkeep hear bashers see fungus man spore guardian at Innothule. Him protect young fungus man spores so they grow big, BIG an' attack trolls!! You go find spore guardian. Smash hi big red head!! Bring Treskar that huge mushroom head of his to prove he walk no more. You do this and me give you froglok skin mask.");
 	elseif (e.message:findi("work")) then
 		e.self:Say("So you not want to smash the weak Greenbloods. Dats ok, you can bring me three ruined wolf hides and a fire beetle eye. Dat help you git big enuff to smash dem.");
@@ -23,7 +27,11 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 12190})) then
+	local pfaction = e.other:GetFaction(e.self);
+
+	if (pfaction > 4) then
+        e.self:Say("You no friend to Treskar!");
+	elseif(item_lib.check_turn_in(e.trade, {item1 = 12190})) then
 		e.self:Say("Ha! Dats one big dead head. You smashed him guud. Take dis mask, Shadowknight uv Nightkeep - I seez great tings for you."); -- Guessed Dialog as quest was revised when removed to Neriak
 		e.other:SummonItem(2308); -- Item: Froglok Skin Mask
 		e.other:Ding();
@@ -54,6 +62,14 @@ function event_trade(e)
 		e.other:AddEXP(1000);
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
+end
+
+function reject(e, pfaction)
+    if (pfaction < 6) then
+        e.self:Say("Me talk to you 'bout dat when you do more to help Nightkeep!  Kill all Greenbloods!  Kill all froggies!");
+    else
+        e.self:Say("'<..Sniff, sniff, sniff..>  Me smell the blood of enemy in you.  You fool to talk to Nightkeep shadowknight!  Me bleed you if you no run!");
+    end
 end
 
 -- End - SK GM Treskar, Grobb

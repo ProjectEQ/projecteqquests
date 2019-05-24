882fe527ccaaf6f124bf08a6e72b5d5b27dc8e2c
Paul Coene <pcoene1@twc.com>
2019-05-24 10:54:15 -0400

    Apply faction changes and made some text more accurate.

--- neriakc/Perrir_Zexus.pl
+++ neriakc/Perrir_Zexus.pl
@@ -11,27 +11,34 @@ sub EVENT_ENTER {
 }
 
 sub EVENT_SAY {
-  if($text=~/hail/i) {
+  if($faction >5) {
+    quest::say("You are worthless and pathetic! You could never be of service to our temple!  Begone before your innards decorate our walls!");
+  }
+  elsif($faction == 5) {
+    quest::say("Although I sense the hate building within you, you have not yet done enough service to this temple to be trusted!");
+  }
+  elsif($text=~/hail/i) {
     quest::say("Salutations, $name! Your devotion to our Lord Innoruuk is evident in your actions and on your soul. As a member of the Spires of Innoruuk, you are required to obtain and wear the symbol of your station among the clergy. I can award you the [hematite symbol of Innoruuk] worn by the initiates of the Spires.");
   }
-  if($text=~/hematite symbol of innoruuk/i) {
+  elsif($text=~/hematite symbol of innoruuk/i) {
     quest::say("The forest beyond the gates of our grand city has been shaped by the magic of our Lord Innoruuk to be more hospitable to our kind. There are halfling druids in the service of Karana who have set up shrines there in hopes of restoring the forest to its former state. They could never succeed in their task with their feeble powers granted by their insignificant god, yet still, they are a nuisance. Bring me four of the holy symbols worn by the druids and I shall grant you the initiate symbol of Innoruuk.");
   }
-  if($text=~/trades/i) {
+  elsif($text=~/trades/i) {
     quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
     quest::summonitem(51121); # Item: Tradeskill Basics : Volume I
   }
-  if($text=~/second book/i)  {
+  elsif($text=~/second book/i)  {
     quest::say("Here is the second volume of the book you requested, may it serve you well!");
     quest::summonitem(51122); # Item: Tradeskill Basics : Volume II
   }
 }
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 14550 => 4)) { #Woven Grass Amulets
+  if($faction < 5 && plugin::check_handin(\%itemcount, 14550 => 4)) { #Woven Grass Amulets
     quest::emote("laughs. 'Here, take this then!  You are now and initiate of Innoruuk.  Speak with Ithvol K`Jasn for further advancement.'"); #text made up
     quest::summonitem(1369); #Initiate Symbol of Innoruuk
     quest::exp(2000);
+    quest::ding();
     quest::faction(278, 7);  #King Naythox Thex
     quest::faction(340, 50);  #Priests of Innoruuk
     quest::faction(226, -12);  #Clerics of Tunare

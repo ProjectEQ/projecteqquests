65f8b1920d9a8655c4868600d890e47485cc0885
Paul Coene <pcoene1@twc.com>
2019-05-25 11:41:46 -0400

    Faction value corrections and tasks are grob specific again (were neriak)

--- grobb/Gargh.lua
+++ grobb/Gargh.lua
@@ -1,61 +1,76 @@
 -- Converted to .lua by Speedz
 -- added saylink by robregen
+-- faction and converting to grobb version of quests by noudess
 
 function event_say(e)
+	local pfaction = e.other:GetFaction(e.self);
+
 	if(e.message:findi("hail")) then
 		e.self:Say("You [" .. eq.say_link("friend of Gardunk",false,"friend of Gardunk") .. "]? Meez friend of Gardunk.");
+	elseif(pfaction > 4) then
+		e.self:Say("Meez don't trust you.");
 	elseif(e.message:findi("friend of Gardunk")) then
 		e.self:Say("Dat good. He tell meez you was comin'. Meez learn you to fight like alligator. Dominate through fear. Dat is what we do. Strike fear into heart of enemies. You [" .. eq.say_link("ready to start",false,"ready to start") .. "]?");
 	elseif(e.message:findi("ready to start")) then
-		e.self:Say("Good. We start easy. You go and kill snakes. Skin da snake and bring me da Snake Scales and a Fang. Dat your first task.");
+		e.self:Say("Good. We start easy. Little froggies a good place. They our enemies. You go to da swamp and kill da tadpoles. Bring back to me four pieces of Froglok Tadpole Flesh. Strip da flesh from our enemies. Dat your first task.");
 	elseif (e.message:findi("learn about da animals")) then
-		e.self:Say("We start with da wolf. You go to da forest and kill wolfies. Bring back to me four of da Ruined Wolf Pelts to prove what yooz done.");
+		e.self:Say("We start with da rat. You go and kill rats. Tear da rat apart and bring me a Giant Rat Ear and a Giant Rat Pelt.");
 	elseif (e.message:findi("fight like da alligator")) then
-		e.self:Say("Best way to do dat is to fight alligators. But dere no alligators around, so we improvise. Hunt down da spiders in the forest and tear dere little legs off. Bring back two of those to Gargh");
+		e.self:Say("Best way to do dat is to fight alligators. You go out and fight da alligators in da swamp. Bring back to meez an alligator tooth and meez believe you.");
 	elseif (e.message:findi("next part of your training")) then
-		e.self:Say("For da next part of your training you go and strike terror into da snakes again. Only not the little ones. You go and you skin the scales from a garter snake. You bring meez back one large snake skin. Meez be waiting for you here if you make it back alive.");
+		e.self:Say("For da next part of your training you go and strike fear into da hearts of da froggies again. Only not the little ones. You go and you drain da blood from one of da full grown ones. You bring meez back da Froglok Blood. Meez be waiting for you here if you make it back alive.");
 	elseif (e.message:findi("one more task")) then
-		e.self:Say("You is getting stronger. It time for you to spread fear to da little halflings. Scour the forest until you find dem. You go and spread fear to them. Fight them. You peel off dere hides. Bring me four Matted Halfling Hides.");
+		e.self:Say("'You is getting stronger. It time for you to venture out some and spread fear to other parts of da world. You go over to da Feerrott. There you find lots of lizard men. You go and spread fear to them. Fight them. You cut off their tails. Bring me four Lizard Tail.");
 	end
 end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if (item_lib.check_turn_in(e.trade, {item1 = 13070, item2 = 13067})) then
+	local pfaction = e.other:GetFaction(e.self);
+
+	if (pfaction > 4) then
+		e.self:Say("No trust.  You take dis back");
+	elseif (item_lib.check_turn_in(e.trade, {item1 = 13187, item2 = 13187, item3 = 13187, item4 = 13187})) then
 		e.self:Say("You done good. You begin to know fear by causing fear. Next you learn about da animals. You learn about da animals by fighting dem and tearing them apart. You ready to [" .. eq.say_link("learn about da animals",false,"learn about da animals") .. "]?");
 		e.other:SummonItem(7380); -- Item: Dark One's Bracer
 		e.other:Ding();
-		e.other:Faction(237,10,0); -- Faction: Dark Ones
-		e.other:Faction(308,10,0); -- Faction: Shadowknights of Night Keep
-		e.other:Faction(251,-10,0); -- Faction: Frogloks of Guk
-	elseif (item_lib.check_turn_in(e.trade, {item1 = 13782, item2 = 13782, item3 = 13782, item4 = 13782})) then
-		e.self:Say("So you know all about da wolf now. Dat is good. Next step is important. You need to learn to [" .. eq.say_link("fight like da alligator",false,"fight like da alligator") .. "].");
+		e.other:Faction(237,1,0); -- Faction: Dark Ones
+		e.other:Faction(308,1,0); -- Faction: Shadowknights of Night Keep
+		e.other:Faction(251,-1,0); -- Faction: Frogloks of Guk
+		e.other:AddEXP(210);
+	elseif (item_lib.check_turn_in(e.trade, {item1 = 13050, item2 = 13054})) then
+		e.self:Say("'So you know all about da rat now. Dat is good. Next step is important. You need to learn to [" .. eq.say_link("fight like da alligator",false,"fight like da alligator") .. "].");
 		e.other:SummonItem(7381); -- Item: Dark One's Cap
 		e.other:Ding();
-		e.other:Faction(237,10,0); -- Faction: Dark Ones
-		e.other:Faction(308,10,0); -- Faction: Shadowknights of Night Keep
-		e.other:Faction(251,-10,0); -- Faction: Frogloks of Guk
-	elseif (item_lib.check_turn_in(e.trade, {item1 = 13417, item2 = 13417})) then
+		e.other:Faction(237,2,0); -- Faction: Dark Ones
+		e.other:Faction(308,1,0); -- Faction: Shadowknights of Night Keep
+		e.other:Faction(251,-1,0); -- Faction: Frogloks of Guk
+		e.other:AddEXP(210);
+	elseif (item_lib.check_turn_in(e.trade, {item1 = 13725})) then
 		e.self:Say("Ok meez believe you. Here your reward. Meez think you just about ready for [" .. eq.say_link("next part of your training",false,"next part of your training") .. "].");
 		e.other:SummonItem(7382); -- Item: Dark One's Leggings
 		e.other:Ding();
-		e.other:Faction(237,10,0); -- Faction: Dark Ones
-		e.other:Faction(308,10,0); -- Faction: Shadowknights of Night Keep
-		e.other:Faction(251,-10,0); -- Faction: Frogloks of Guk
-	elseif (item_lib.check_turn_in(e.trade, {item1 = 13060})) then
+		e.other:Faction(237,6,0); -- Faction: Dark Ones
+		e.other:Faction(308,1,0); -- Faction: Shadowknights of Night Keep
+		e.other:Faction(251,-1,0); -- Faction: Frogloks of Guk
+		e.other:AddEXP(210);
+	elseif (item_lib.check_turn_in(e.trade, {item1 = 22524})) then
 		e.self:Say("You did it! You cause lots of fear for them. Dat good. Even da spirits are starting to tremble. Soon you will be able to make dem do what you want. But there is [" .. eq.say_link("one more task",false,"one more task") .. "] meez want you to do.");
 		e.other:SummonItem(7383); -- Item: Dark One's Claw
 		e.other:Ding();
-		e.other:Faction(237,10,0); -- Faction: Dark Ones
-		e.other:Faction(308,10,0); -- Faction: Shadowknights of Night Keep
-		e.other:Faction(251,-10,0); -- Faction: Frogloks of Guk
-	elseif (item_lib.check_turn_in(e.trade, {item1 = 22572, item2 = 22572, item3 = 22572, item4 = 22572})) then
-		e.self:Say("You did it! You cause lots of fear for them. Dat good. Even da spirits are starting to tremble. Soon you will be able to make dem do what you want. You have passed your final test.");
+		e.other:Faction(237,27,0); -- Faction: Dark Ones
+		e.other:Faction(308,7,0); -- Faction: Shadowknights of Night Keep
+		e.other:Faction(251,-4,0); -- Faction: Frogloks of Guk
+		e.other:AddEXP(210);
+	elseif (item_lib.check_turn_in(e.trade, {item1 = 13354, item2 = 13354, item3 = 13354, item4 = 13354})) then
+		e.self:Say("Meez impressed. Da spirits, they tremble in terror before you. Meez can't teach you no more. Go and dominate through fear. Fight like da alligator and tear your enemies apart.");
 		e.other:SummonItem(7384); -- Item: Dark One's Leather
 		e.other:Ding();
 		e.other:Faction(237,10,0); -- Faction: Dark Ones
-		e.other:Faction(308,10,0); -- Faction: Shadowknights of Night Keep
-		e.other:Faction(251,-10,0); -- Faction: Frogloks of Guk
+		e.other:Faction(308,2,0); -- Faction: Shadowknights of Night Keep
+		e.other:Faction(251,-1,0); -- Faction: Frogloks of Guk
+		e.other:AddEXP(210);
 	end
+
 	item_lib.return_items(e.self, e.other, e.trade)
 end

d435c15aff23cb1f106d24a20fdeabac5cf37039
Paul Coene <pcoene1@twc.com>
2019-05-30 14:52:55 -0400

    Donation from Nilbog
    I'll check for faction implications.

---
+++ everfrost/Granin_O`Gill.pl
@@ -0,0 +1,23 @@
+#nilbog
+
+sub EVENT_SAY 
+{
+    if($text=~/Hail/i)
+    {
+        quest::say("Brrr... Good to meet another upon this wasteland.  You would not by any chance have any iron rations on you, would you?  If so, I shall heal you in trade.  I am in need of provisions.");
+    }
+}
+
+sub EVENT_AGGRO 
+{ 
+    quest::say("Great Tribunal, help me smite this foe!");
+}
+
+sub EVENT_ITEM 
+{
+    if(plugin::check_handin(\%itemcount, 13005 => 1)) #iron ration
+    {
+        quest::say("Great Tribunal... Give me strength to bind this good person's wounds.");
+        $npc->CastSpell(200,$userid); #minor healing
+    }
+}

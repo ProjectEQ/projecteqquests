8d0c66a311b7920609a0aaf8618766e61ef2c817
huffin <acg1089@aol.com>
2019-06-08 09:08:33 -0500

    Update #Birak_Spawner.lua

--- poearthb/#Birak_Spawner.lua
+++ poearthb/#Birak_Spawner.lua
@@ -1,20 +1,13 @@
 function event_spawn(e)
-	eq.set_timer("depop", 59000);
+	eq.set_timer("spawn", 6000);
 end
 
 function event_timer(e)
-	-- get quest globals
-	local qglobals = eq.get_qglobals();
-
-	-- get the list of NPCs in the zone
-   local entity_list = eq.get_entity_list();
-
-	-- Check to see if the Stonefist Clansman are up
-	local check_stonefist = entity_list:GetMobByNpcTypeID(222001);
-
-	if (not check_stonefist.valid and qglobals["poeb_Birak"] == nil and qglobals["poeb_warlord"] == nil ) then
-		eq.spawn2(222017, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
-		eq.stop_timer("depop");
-		eq.depop_with_timer();
-	end
+	if (e.timer == "spawn") then
+			eq.stop_timer("spawn");
+			eq.spawn2(222001, 0, 0, -12, 352, -14.5, 193);
+			eq.spawn2(222001, 0, 0, 52, 285, -14.5, 450);
+			eq.spawn2(222001, 0, 0, 58, 356, -14.5, 320);
+			eq.spawn2(222001, 0, 0, -13, 286, -14.5, 65);
+ 	end
 end

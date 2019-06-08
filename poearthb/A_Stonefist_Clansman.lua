0e0d21f38feea79718810ef54e0c45efc49bcb78
huffin <acg1089@aol.com>
2019-06-08 09:09:10 -0500

    Create A_Stonefist_Clansman.lua

---
+++ poearthb/A_Stonefist_Clansman.lua
@@ -0,0 +1,6 @@
+function event_death_complete(e)
+  if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(222001) == false) then
+ 	  eq.spawn2(222017, 0, 0, 21, 322, -20, 127);
+ 	  eq.depop_with_timer(222021);
+  end
+end

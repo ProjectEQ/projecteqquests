1d0457aeb54b99f05d6de58124aee61233310438
Paul Coene <pcoene1@twc.com>
2019-06-10 13:03:11 -0400

    Create Basher_Avisk.lua
    New support for Urako's Big Mistake

---
+++ grobb/Basher_Avisk.lua
@@ -0,0 +1,8 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Ranjor tell shaddernites to gets rids of spore guardian. Ha! Gud.");
+	elseif(e.message:findi("where the minstrel")) then
+		e.self:Say("Dere stewpid skeleton singing by Basher Ganbaku. His post be up high.");
+		eq.unique_spawn(52119,0,0,-182,313,26.7,459);	
+	end
+end

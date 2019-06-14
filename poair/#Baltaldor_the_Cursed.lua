e6e983d8a00e52077a170bd5d22ad3f7dbdda81c
huffin <acg1089@aol.com>
2019-06-14 07:16:14 -0500

    Create #Baltaldor_the_Cursed.lua
    add blur

---
+++ poair/#Baltaldor_the_Cursed.lua
@@ -0,0 +1,19 @@
+function event_combat(e)
+if (e.joined == true) then
+eq.set_timer('Hardblur', 180 * 1000);
+eq.set_timer('Softblur', 6 * 1000);
+else
+eq.stop_timer('Hardblur');
+eq.stop_timer('Softblur');
+end
+end
+
+function event_timer(e)
+	if (e.timer == 'Hardblur') then
+		e.self:WipeHateList();
+	elseif (e.timer == 'Softblur') then
+		if (math.random(100)<=10) then
+			e.self:WipeHateList();
+		end
+	end
+end

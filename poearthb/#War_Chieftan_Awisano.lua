4d39c80e65e516cf4c966a20695db9d1d7637d08
huffin <acg1089@aol.com>
2019-06-15 09:58:10 -0500

    Update #War_Chieftan_Awisano.lua

--- poearthb/#War_Chieftan_Awisano.lua
+++ poearthb/#War_Chieftan_Awisano.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
-	if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(222020) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222022) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222021) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222023) == true) then
+	if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(222020) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222022) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222021) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222023) == true and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222016) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222017) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222018) == false) then
 		eq.signal(222023,0); -- NPC: #Warlord_Spawner
 	end
 end

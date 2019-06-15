3d50fef715d12ef36cd73a0f6c81023586729117
huffin <acg1089@aol.com>
2019-06-15 10:00:41 -0500

    Update #Warlord_Spawner.lua

--- poearthb/#Warlord_Spawner.lua
+++ poearthb/#Warlord_Spawner.lua
@@ -1,6 +1,6 @@
 function event_signal(e)
   if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(222020) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222022) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(222021) == false) then
-    eq.spawn2(222019, 0, 0, 415,161,-55,383); -- NPC: #Warlord_Gintolaken
+    eq.unique_spawn(222019, 0, 0, 415,161,-55,383); -- NPC: #Warlord_Gintolaken
     eq.depop_with_timer();
   end
 end

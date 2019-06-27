f47e38b47d7aede70802afb194d736a293948b7f
Justin Wienckowski <jwinky@gmail.com>
2019-06-26 17:16:05 -0700

    Fixes the Coldain Ring #7 event with Corbin Blackwell.

--- eastwastes/Corbin_Blackwell.pl
+++ eastwastes/Corbin_Blackwell.pl
@@ -1,86 +1,94 @@
-##NEEDS WORK!!!
-#Not all timers stopped before depop()
-#Timer 2 not used. Maybe final_path is supposed to use 2?
+my $corbin = undef;
 
 sub EVENT_SPAWN {
-  if(defined($ring_seven_start) && $ring_seven_start == 1) { #timer for turnin
-    quest::settimer(1,1);
-  }
-  if(defined($final_path) && $final_path == 1) { #timer for final respawn and signal for turnin
-    quest::settimer(1,1);
-  }
-  if(defined($turnin) && $turnin == 1) {
-    quest::settimer(3, 600);
-  }
+    if ($npc->GetNPCTypeID() == 116119) {
+        $corbin = $npc;
+        quest::settimer("down",1);
+    }
+}
+
+sub EVENT_HP {
+    if ($inchpevent == 50) {
+        quest::emote(" sits up with a groan of effort.");
+        $npc->SetAppearance(1);
+        quest::setnextinchpevent(75);
+    }
+    elsif ($inchpevent == 75) {
+        quest::emote(" heaves himself heavily to his feet.");
+        $npc->SetAppearance(0);
+        quest::settimer("down", 5);
+    }
 }
 
 sub EVENT_SAY { 
-  if($text=~/hail/i && !defined($event_spawn) && !defined($ring_seven_start)) {
-    quest::say("Hurry! There's not much time. Give me the key and show me proof that you are a friend sent to rescue me...");
-  }
-}
-
-sub EVENT_TIMER {
-  if($timer == 1 && $x == -2012 && $y == 197 && !defined($event_spawn) && !defined($final_path)) {
-    quest::stoptimer(1);
-    $event_spawn=1;			
-    $ring_seven_start=undef;
-    quest::say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
-    quest::spawn2(116119, 0,0, $x, $y, $z, $h); # NPC: Corbin_Blackwell
-    quest::spawn2(116569, 0,0, -2139, 168, 150, 114); # NPC: Commander_Bahreck
-    quest::depop_withtimer();
-  }
-  if($timer == 1 && $x == -3183 && $y == -586 && defined($final_path) && $final_path == 1) {
-    quest::stoptimer(1);
-    $event_spawn=undef;
-    $ring_seven_start=undef;
-    $final_path=undef;
-    $turnin=1;
-    quest::spawn2(116119, 0, 0, $x, $y, $z, 18); # NPC: Corbin_Blackwell
-    quest::say("I have escaped! With the help of our friends here I was saved from certain death. We are in their debt.");
-    quest::signalwith(116118, 1, 1000); # NPC: Dobbin_Crossaxe
-    quest::depop_withtimer();
-  }
-  if($timer == 2) { #Not sure why this is here. Timer 2 is not defined/declared
-    $event_spawn=undef;
-    quest::stoptimer(1);
-    $ring_seven_start=undef;
-    quest::depop_withtimer();
-  }
-  if($timer == 3) {
-    $event_spawn=undef;
-    $ring_seven_start=undef;
-    $final_path=undef;
-    $turnin=undef;
-    quest::signalwith(116118, 2, 0); # NPC: Dobbin_Crossaxe
-    quest::depop_withtimer();
-  }
+    if($text=~/hail/i && $npc->GetGrid() == 0) {
+        quest::say("Hurry! There's not much time. Give me the key and show me proof that you are a friend sent to rescue me...");
+    }
 }
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 1046 => 1, 30162 => 1) && !defined($ring_seven_start) && !defined($final_path)) {
-    quest::say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
-    quest::summonitem(30162); # Item: Mithril Coldain Insignia Ring
-    $ring_seven_start=1;
-    $event_spawn=undef;
-    quest::spawn2(116119,234, 0, $x, $y, $z, $h); # NPC: Corbin_Blackwell
-    quest::depop_withtimer();
-  }
-  plugin::return_items(\%itemcount);
+    if (plugin::check_handin(\%itemcount, 1046 => 1, 30162 => 1)) {
+        quest::say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
+        quest::summonitem(30162); # Item: Mithril Coldain Insignia Ring
+        $corbin2 = quest::spawn2(2000944, 234, 0, $x, $y, $z, $h); # NPC: Corbin_Blackwell
+        $mob = $entity_list->GetMobID($corbin2);
+        $mobnpc = $mob->CastToNPC();
+        $mobnpc->SetHP( $mobnpc->GetMaxHP() * 0.84 );
+        quest::depop_withtimer();
+    }
+
+    plugin::return_items(\%itemcount);
+}
+
+sub EVENT_WAYPOINT_DEPART {
+    if ($wp == 39) {
+        quest::say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
+        quest::spawn2(116569, 0, 0, -2139, 168, 150, 114); # NPC: Commander_Bahreck
+        quest::settimer("depop", 1200);
+    }
+}
+
+sub EVENT_WAYPOINT_ARRIVE {
+    if ($npc->GetGrid() == 235 && $wp == 18) {
+        quest::say("I have escaped! With the help of our friends here I was saved from certain death. We are in their debt.");
+        quest::signalwith(116118, 1, 1000); # NPC: Dobbin_Crossaxe
+        quest::settimer("depop", 30);
+    }
+}
+
+sub EVENT_TIMER {
+    if ($timer eq "depop") {
+        quest::stoptimer("depop");
+        quest::depop();
+    }
+    elsif ($timer eq "down") {
+        quest::stoptimer("down");
+
+        # Makes guards emote flavor text
+        quest::signalwith(116114, 116119, 500); # NPC: a_Ry-Gorr_prison_guard
+        quest::signalwith(116527, 116119, 500); # NPC: a_Ry-Gorr_prison_guard
+        quest::signalwith(116102, 116119, 500); # NPC: a_Ry-Gorr_prison_guard
+
+        quest::settimer("resethp", 1);
+    }
+    elsif ($timer eq "resethp") {
+        quest::stoptimer("resethp");
+
+        $npc->SetHP( $npc->GetMaxHP() * 0.30 );
+        quest::setnextinchpevent(50);
+
+        $npc->SetAppearance(3);
+    }
 }
 
 sub EVENT_SIGNAL {
-  $event_spawn=undef;
-  $final_path=1;
-  quest::spawn2(116119, 235, 0, $x, $y, $z, $h); # NPC: Corbin_Blackwell
-  quest::depop_withtimer();
+    quest::stoptimer("depop");
+    quest::spawn2(2000944, 235, 0, $x, $y, $z, $h); # NPC: Corbin_Blackwell
+    quest::depop();
 }
 
 sub EVENT_DEATH_COMPLETE {
-  $event_spawn=undef;
-  $ring_seven_start=undef;
-  $final_path=undef;
-  $turnin=undef;
+    quest::stoptimer("depop");
 }
 
-#END of FILE Zone:eastwastes  ID:116145 -- Corbin_Blackwell
+#END of FILE Zone:eastwastes  ID:116145 ID:2000944 -- Corbin_Blackwell
---
+++ eastwastes/a_Ry-Gorr_prison_guard.pl
@@ -0,0 +1,4 @@
+sub EVENT_SIGNAL {
+    quest::emote(" advances grimly on Corbin Blackwell and beats him until he collapses.");
+}
+

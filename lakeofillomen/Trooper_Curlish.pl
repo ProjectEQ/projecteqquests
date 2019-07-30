c9f87534fd5ad8c9a88695c717325c16c2715446
huffin <acg1089@aol.com>
2019-07-30 15:27:44 -0500

    Update Trooper_Curlish.pl
    comma

--- lakeofillomen/Trooper_Curlish.pl
+++ lakeofillomen/Trooper_Curlish.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY { 
 if($text=~/Hail/i){
-quest::say("Have you tried to drink from the lake?  It is poisoned with salt!  If you plan on venturing to the far side of the lake. you had best visit one of the Kloks to purchase a Water Extractor."); }
+quest::say("Have you tried to drink from the lake?  It is poisoned with salt!  If you plan on venturing to the far side of the lake, you had best visit one of the Kloks to purchase a Water Extractor."); }
 }
 
 

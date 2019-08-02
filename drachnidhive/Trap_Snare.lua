9f82e39601891638ee31667a1389061b0fcf4447
huffin <acg1089@aol.com>
2019-08-02 16:17:26 -0500

    Update Trap_Snare.lua

--- drachnidhive/Trap_Snare.lua
+++ drachnidhive/Trap_Snare.lua
@@ -1,6 +1,10 @@
 function event_combat(e)
 if (e.joined == true) then
+eq.set_timer("cast", 500);
+end
+end
+
+function event_timer(e)
 e.self:CastSpell(7740,e.self:GetTarget():GetID());
 eq.depop_with_timer();
-end
 end

ecfd675a8c5f133d8e27c71c03863785cbe365e5
huffin <acg1089@aol.com>
2019-08-03 14:06:10 -0500

    Update Trap_Snare.lua

--- drachnidhive/Trap_Snare.lua
+++ drachnidhive/Trap_Snare.lua
@@ -1,6 +1,9 @@
 function event_combat(e)
 if (e.joined == true) then
 e.self:CastSpell(7740,0); --confining web
-eq.depop_with_timer();
 end
+end
+
+function event_cast(e)
+eq.depop_with_timer(); -- add this function because spell has casting time
 end

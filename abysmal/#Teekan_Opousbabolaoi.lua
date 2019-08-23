aa031ec45f17eac569bfd51dca8289b9ad5f08ac
Drogerin <40513146+Drogerin@users.noreply.github.com>
2019-08-22 21:53:14 -0500

    Update #Teekan_Opousbabolaoi.lua
    Check qglobal before turn in to prevent items from being eaten.

--- abysmal/#Teekan_Opousbabolaoi.lua
+++ abysmal/#Teekan_Opousbabolaoi.lua
@@ -10,7 +10,7 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 	local qglobals = eq.get_qglobals(e.other);	
-		if(item_lib.check_turn_in(e.trade, {item1 = 18976, item2 = 11999}) and qglobals["berserk_epic"] == "5") then 
+		if qglobals["berserk_epic"] == "5" and item_lib.check_turn_in(e.trade, {item1 = 18976, item2 = 11999}) then 
 			e.self:Emote("takes the axe and the Soul Gem and begins the chant in low, near inaudible tones. Smoke begins to swirl around the axe as the orb twists and writhes like a serpent around the haft of the axe, melding itself to the dark wooden handle. When the smoke clears, she hands the axe back to you and says, 'This should be what you're looking for! Be sure to show this to Keras and give him my regards. Tell him he should stop by and visit sometime!");
 			e.other:SummonItem(18398); -- Raging Taelosian Alloy Axe (Epic 1.5)
 			e.other:AddAAPoints(5);

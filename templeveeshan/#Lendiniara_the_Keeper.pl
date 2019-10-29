e6754647651fb91ff49c66b2e0a5df4791697c49
huffin <acg1089@aol.com>
2019-10-29 17:36:25 -0500

    Update #Lendiniara_the_Keeper.pl

--- templeveeshan/#Lendiniara_the_Keeper.pl
+++ templeveeshan/#Lendiniara_the_Keeper.pl
@@ -29,7 +29,7 @@ sub EVENT_SAY {
     }
   }
   else {
-    quest::say("I do not know you well enough to entrust you with such a quest, yet.");
+    quest::say("You need to prove your dedication to our cause before I can discuss such matters with you.");
   }
 }
 

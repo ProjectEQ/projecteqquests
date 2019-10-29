4f3003be812c23958e57c3b2233960b2cd2e5dc2
huffin <acg1089@aol.com>
2019-10-29 18:09:45 -0500

    Update Derasinal.pl

--- westwastes/Derasinal.pl
+++ westwastes/Derasinal.pl
@@ -1,5 +1,5 @@
 sub EVENT_SAY {
-  if ($faction <= 5) {
+  if ($faction <= 3) {
     if($text=~/hail/i) {
      quest::say("It is not often that I see strangers in these dangerous lands. Perhaps you are the chosen that Jualicn speaks of that will aid us in our battle against the foul giants. I do not believe you would be here if you were not in need of my sacred rune.");
     }
@@ -10,10 +10,12 @@ sub EVENT_SAY {
      quest::say("I understand that you may know of Jualicn, but I can only deal with those that he would trust with something dear to him.");
     }
   }
+  else {
+    quest::say("You seem like a decent sort for a snack food, but there is nothing you can do for me this day.  Come back when you have done more work for the Claws.");
 }
 
 sub EVENT_ITEM {
-  if ($faction <= 5) {
+  if ($faction <= 3) {
   if(plugin::check_handin(\%itemcount, 1908 => 1)){ #Jualicn's Token
     quest::say("I have given you the Rune of Bleve, to assist you in your research for the once lost magic against the Kromzek. I hope that you can make good use of it, for it is very sacred to me. May Veeshan guide your path!");
     quest::faction(430,10);   #Claws of Veeshan

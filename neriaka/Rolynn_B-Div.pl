ae923c15011bedc77859d1f3e08278d0d80b55cc
TurmoilToad <TurmoilToad@gmail.com>
2019-11-05 15:19:18 -0500

    Update Rolynn_B-Div.pl
    Missing dialogue:  http://everquest.allakhazam.com/db/npc.html?id=2197#m105562848879620

--- neriaka/Rolynn_B-Div.pl
+++ neriaka/Rolynn_B-Div.pl
@@ -1,6 +1,11 @@
-sub EVENT_WAYPOINT_ARRIVE {
-if (($wp eq 3) || ($wp eq 8) || ($wp eq 18)) {
-	quest::say("Hear yee. Master Narex of the Indigo Brotherhood is offering one gold piece for every Leatherfoot Raider skullcap.");
+sub EVENT_SAY {
+	if ($text=~/narex/i) {
+		quest::say("Talk to Head Master Narex of the Indigo Brotherhood. He will tell you.");
 	}
 }
 
+sub EVENT_WAYPOINT_ARRIVE {
+	if (($wp eq 3) || ($wp eq 8) || ($wp eq 18)) {
+		quest::say("Hear yee. Master Narex of the Indigo Brotherhood is offering one gold piece for every Leatherfoot Raider skullcap.");
+	}
+}

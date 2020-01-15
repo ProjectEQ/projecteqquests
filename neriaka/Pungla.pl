89b2a915ce439881ad027a82bfae5ad8aaf58ed8
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-01-15 15:31:33 -0600

    Update Pungla.pl
    Flavor Flav

--- neriaka/Pungla.pl
+++ neriaka/Pungla.pl
@@ -1,9 +1,9 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Welcome. Drink you shall some of de finest drinks of da lands. [Flaming Pungla] I suggest to you. It all me sells.");
+		quest::say("Welcome! Drink you shall, some of de finest drinks of da lands! [Flaming Pungla] I suggest to you. It all me sells.");
 	}
 	elsif ($text=~/flaming pungla/i) {
-		quest::say("Greatest drink and Plunga's creation. Buy one you should. Dey so gud me gots da charge tree golds.");
+		quest::say("Greatest drink and Plunga's creation. Buy one you should. Dey so gud me gots da charge t'ree golds.");
 	}
 }
 

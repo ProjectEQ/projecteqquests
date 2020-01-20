b4baff68088c5049726604a017a5a4192e190710
huffin <acg1089@aol.com>
2020-01-20 08:54:13 -0600

    Create 209086.lua

---
+++ bothunder/209086.lua
@@ -0,0 +1,15 @@
+function event_spawn(e)
+eq.set_timer("roll",4*1000);
+end
+
+function event_timer(e)
+	if (e.timer == "roll") then
+		local rand = math.random(1,100);
+		if (rand <= 33) then
+			eq.get_entity_list():MessageClose(e.self, false, 100, 0, "The storm clouds pass harmlessly.");
+		elseif (rand <= 66 and rand > 33) then
+			e.self:SpellFinished(1032,e.self); --hailstorm
+		end
+		eq.depop();
+	end
+end

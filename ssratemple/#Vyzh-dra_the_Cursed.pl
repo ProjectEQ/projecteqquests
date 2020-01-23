7fa2fcd2818d85026c224dafaeb01f1c7b8f601e
huffin <acg1089@aol.com>
2020-01-22 18:01:42 -0600

    Update #Vyzh-dra_the_Cursed.pl

--- ssratemple/#Vyzh-dra_the_Cursed.pl
+++ ssratemple/#Vyzh-dra_the_Cursed.pl
@@ -1,9 +1,29 @@
-sub EVENT_SLAY {
-	quest::shout("Tell your gods that I will be coming for them next!");
-}
+function event_spawn(e)
+	eq.set_timer('depop', 1800 * 1000);
+end
 
-sub EVENT_DEATH_COMPLETE {
-	quest::shout("I cannot die! I am the only true god!");
-	quest::emote("crashes to the ground. A horrific sound fills the room, but vanishes as quickly as it came.");
-	quest::signalwith(162255,3,0); # NPC: #cursed_controller
-}
+function event_combat(e)
+	if (e.joined == true) then
+		if(not eq.is_paused_timer('depop')) then
+			eq.pause_timer('depop');
+		end
+	else
+		eq.resume_timer('depop');
+	end
+end
+
+function event_timer(e)
+	if (e.timer == 'depop') then
+		eq.depop();
+	end
+end
+
+function event_slay(e)
+	eq.zone_emote(0,"Vyzh-dra the Cursed shouts, 'Tell your gods that I will be coming for them next!");
+end
+
+function event_death_complete(e)
+	eq.zone_emote(0,"Vyzh-dra the Cursed shouts, 'I cannot die! I am the only true god!");
+	e.self:Emote("crashes to the ground. A horrific sound fills the room, but vanishes as quickly as it came.");
+	eq.signal(162255,3);
+end

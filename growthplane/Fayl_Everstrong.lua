de23c1d4d8ec453db35e47e0721ee26aec624548
huffin <acg1089@aol.com>
2020-01-24 09:41:58 -0600

    Update Fayl_Everstrong.lua

--- growthplane/Fayl_Everstrong.lua
+++ growthplane/Fayl_Everstrong.lua
@@ -20,7 +20,7 @@ function call_zone_to_assist(e_self,e_other)
 	-- grab the entity list
 	local entity_list = eq.get_entity_list();
 	-- move the mobs to the location that is calling to.
-	local include_npc_list = Set {127027,127103,127026}; -- entoiling mobs
+	local include_npc_list = Set {127027,127103,127026,127028,127102,127057}; -- entoiling mobs
 	local npc_list = entity_list:GetNPCList();
 	if (npc_list ~= nil) then
 		for npc in npc_list.entries do

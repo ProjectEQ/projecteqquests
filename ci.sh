c0521324c8cb524ab1b57cec1390b0193f468e1b
Michael Cook (mackal) <mcook@mackal.net>
2020-01-24 14:50:38 -0500

    It's a lot faster this way [skip ci]

--- ci.sh
+++ ci.sh
@@ -6,16 +6,19 @@ error=0
 
 echo "Checking Perl scripts..."
 # perl sadly outputs "Syntax OK" on stderr, so gotta filter that out :/
+# it's significantly faster to not save the output of the first perl -c call ... and just call it again
 while read s
 do
-    if ! errormsg=$(perl -c "$s" 2>&1); then
-        echo "$errormsg"
+    if ! perl -c "$s" 2>/dev/null; then
+        perl -c "$s"
         ((error=1))
     fi
 done < <(find . -name "*.pl")
 
 echo "Checking Lua scripts..."
 
+# this has the issue of stopping once an error happens (won't find all bad files)
+# it is a lot faster than looping like above though ...
 if ! find . -name "*.lua" -exec luac -p '{}' + ; then
     error=1
 fi

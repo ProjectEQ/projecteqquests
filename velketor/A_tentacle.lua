3d165a33dedf6b13ae61a1a15410254f334a07f9
huffin <acg1089@aol.com>
2020-02-09 11:22:14 -0600

    Create A_tentacle.lua

---
+++ velketor/A_tentacle.lua
@@ -0,0 +1,7 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("strikes out at you!");
+eq.spawn2(112047, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0); --a_crystalline_shard
+eq.depop_with_timer();
+end
+end

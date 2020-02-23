88c5f5ab410012faacac7e7b4d9e846d8efb8a35
huffin <acg1089@aol.com>
2020-02-23 06:48:39 -0600

    Create An_Undead_Inhabitant.lua

---
+++ hohonora/An_Undead_Inhabitant.lua
@@ -0,0 +1,4 @@
+function event_death_complete(e)
+eq.spawn2(211055, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- spawns multiple A_Undead_Miniature on death (211055)
+eq.spawn2(211055, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); -- spawns multiple A_Undead_Miniature on death (211055)
+end

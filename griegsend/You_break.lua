48600c0bcfe19050135c862b9d28212c05e9b34f
huffin <acg1089@aol.com>
2020-02-26 09:46:53 -0600

    Create You_break.lua

---
+++ griegsend/You_break.lua
@@ -0,0 +1,6 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("into a cold sweat as you glance around the room and are unable to focus on anything.");
+eq.depop_with_timer();
+end
+end

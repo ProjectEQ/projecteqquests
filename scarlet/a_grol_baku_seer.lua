d027fe1e993ce7b2b3e17ea8cefc020eb035e89d
huffin <acg1089@aol.com>
2020-02-28 06:49:23 -0600

    Update a_grol_baku_seer.lua

--- scarlet/a_grol_baku_seer.lua
+++ scarlet/a_grol_baku_seer.lua
@@ -1,7 +1,6 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("Gobu gobu. bakula erila [shamu eku]?");
-		end
 	elseif(e.message:findi("shamu eku")) then
 		e.self:Say("Weeee Weeee! Shamiku! Weeee!");
 		e.self:CastSpell(2063,e.other:GetID());

3a5951b16c88f8470abd4cd036fb78a1b2fd6873
huffin <acg1089@aol.com>
2020-02-28 06:49:03 -0600

    Update and rename a_grol_baku_seer.pl to a_grol_baku_seer.lua

---
+++ scarlet/a_grol_baku_seer.lua
@@ -0,0 +1,9 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Gobu gobu. bakula erila [shamu eku]?");
+		end
+	elseif(e.message:findi("shamu eku")) then
+		e.self:Say("Weeee Weeee! Shamiku! Weeee!");
+		e.self:CastSpell(2063,e.other:GetID());
+	end
+end

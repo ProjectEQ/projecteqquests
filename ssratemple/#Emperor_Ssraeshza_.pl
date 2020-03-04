2d88a71cfbcfc33e110525825fdc0943b943b36d
huffin <acg1089@aol.com>
2020-03-04 17:36:30 -0600

    Update #Emperor_Ssraeshza_.pl
    update spawn locs

--- ssratemple/#Emperor_Ssraeshza_.pl
+++ ssratemple/#Emperor_Ssraeshza_.pl
@@ -22,12 +22,11 @@ sub EVENT_COMBAT {
   
 sub EVENT_DEATH_COMPLETE {
   quest::emote("'s corpse says 'How...did...ugh...'");
-  quest::spawn2(162210,0,0,953,-332,403.1,380); #Shissar Wraith
-  quest::spawn2(162210,0,0,953,-324,403.1,380); # NPC: A_shissar_wraith
-  quest::spawn2(162210,0,0,953,-316,403.1,380); # NPC: A_shissar_wraith
-  quest::spawn2(162210,0,0,937,-332,403.1,380); # NPC: A_shissar_wraith
-  quest::spawn2(162210,0,0,937,-324,403.1,380); # NPC: A_shissar_wraith
-  quest::spawn2(162210,0,0,937,-316,403.1,380); # NPC: A_shissar_wraith
+  quest::spawn2(162210,0,0,877, -326, 408,385); # NPC: A_shissar_wraith
+  quest::spawn2(162210,0,0,953, -293, 404,385); # NPC: A_shissar_wraith
+  quest::spawn2(162210,0,0,953, -356, 404,385); # NPC: A_shissar_wraith
+  quest::spawn2(162210,0,0,773, -360, 403,128); # NPC: A_shissar_wraith
+  quest::spawn2(162210,0,0,770, -289, 403,128); # NPC: A_shissar_wraith
   quest::signalwith(162260,2,0); #EmpCycle
 }
 

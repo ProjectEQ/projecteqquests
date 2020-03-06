ed2f76fe1bc6c41380299df2c75acad7696654a4
huffin <acg1089@aol.com>
2020-03-06 07:59:41 -0600

    Update 77005.lua

--- arena/77005.lua
+++ arena/77005.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("If you would like to adjust the offensive difficulty level of this npc, let me know if you would like a [low], [medium], or [hard] difficulty. Perhaps you would like to [add flurry], [remove flurry], [add rampage], [remove rampage], [add ae rampage], [remove ae rampage], [hit slower], [hit faster], [mitigate more slow], or [mitigate less slow].");
+		e.self:Say("If you would like to adjust the offensive difficulty level of this npc, let me know if you would like a [low], [medium], or [hard] difficulty. Perhaps you would like to [add flurry], [remove flurry], [add rampage], [remove rampage], [add ae rampage], [remove ae rampage], [hit slower], [hit faster], [mitigate more slow], or [mitigate less slow]. You may also [repop] me to reset my settings.");
 	elseif(e.message:findi("low")) then
     e.self:ModifyNPCStat("accuracy", "100");
     e.self:ModifyNPCStat("atk", "200");
@@ -30,5 +30,7 @@ function event_say(e)
     e.self:ModifyNPCStat("slow_mitigation","85");
 	elseif(e.message:findi("mitigate less slow")) then
     e.self:ModifyNPCStat("slow_mitigation","15");
+	elseif(e.message:findi("repop")) then
+    eq.depop_with_timer();
 	end
 end

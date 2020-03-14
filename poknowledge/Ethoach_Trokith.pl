a226882d3fdbc5dcf55adcb1f4ac8bdfabc3fb15
huffin <acg1089@aol.com>
2020-03-14 08:35:49 -0500

    Update Ethoach_Trokith.pl

--- poknowledge/Ethoach_Trokith.pl
+++ poknowledge/Ethoach_Trokith.pl
@@ -13,6 +13,7 @@ sub EVENT_ITEM {
   if(plugin::check_handin(\%itemcount, 22503 => 1, 9423 => 1, 26709 => 1, 28793 => 1)) {#Blue Diamond, Blizzard Gem, Symbol of the Diaku, Intermediate Stealth Manual
     quest::say("I must say, this is a surprise. I was beginning to wonder if you would ever return with these. I'm glad you see you have though. Take this new book and use it well in your future journeys. You may also be interested to know that Tenada Jeried also has a few lesson plans for very advanced students. If you should see him, be sure to tell him you are interested in learning from him. Good luck to you, $name!");#Stole Text from Advanced Combat Manual quest, I have no other reference
     quest::summonitem(28794); #Advanced Stealth Manual
+    quest::exp(500000);
   }
   plugin::return_items(\%itemcount);
 }#END of FILE Zone:poknowledge  ID:202047 -- Ethoach_Trokith 

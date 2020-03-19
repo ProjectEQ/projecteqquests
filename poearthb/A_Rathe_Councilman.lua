e78d5f840936cc1eb628b0e28e425fafced6db34
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-03-19 12:16:51 -0500

    Update A_Rathe_Councilman.lua

--- poearthb/A_Rathe_Councilman.lua
+++ poearthb/A_Rathe_Councilman.lua
@@ -1,15 +1,16 @@
 function event_combat(e)
 	if not e.joined then
-	local roll = math.random(100)
+		local roll = math.random(100)
 		if roll >= 50 then
 			eq.set_timer("punt", 6* 1000);
 		end
+	else
+		eq.stop_timer("punt");
 	end
 end
 
 function event_timer(e)
 	if e.timer == "punt" then
 		e.self:GotoBind();
-		eq.stop_timer("punt");
 	end
 end

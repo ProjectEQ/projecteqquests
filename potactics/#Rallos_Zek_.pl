acbab4da88590e31227b897059386c72762ae849
huffin <acg1089@aol.com>
2020-03-21 12:37:17 -0500

    Update #Rallos_Zek_.pl

--- potactics/#Rallos_Zek_.pl
+++ potactics/#Rallos_Zek_.pl
@@ -1,6 +1,6 @@
 sub EVENT_SIGNAL {
-	quest::spawn2(214109,0,0,500,20,169.1,381.2);	# Spawn Mini Rallos and depop when told to
-	quest::depop();
+	quest::spawn2(214109,0,0,500,11,169.1,381.2);	# Spawn Mini Rallos and depop when told to
+	quest::depop_withtimer();
 }
 
 # End of File  Zone: PoTactics  ID: 214052  -- #Rallos_Zek_

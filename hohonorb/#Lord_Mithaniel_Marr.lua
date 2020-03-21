65654024586663441de12ac721a13fccf5dcd31e
huffin <acg1089@aol.com>
2020-03-21 09:09:19 -0500

    Update #Lord_Mithaniel_Marr.lua

--- hohonorb/#Lord_Mithaniel_Marr.lua
+++ hohonorb/#Lord_Mithaniel_Marr.lua
@@ -1,7 +1,3 @@
-sub EVENT_DEATH_COMPLETE {
-quest::spawn2(202368,0,0,$x,$y,$z,$h); # NPC: A_Planar_Projection
-}
-
 function event_death_complete(e)
 eq.spawn2(202368,0,0,2380,-2,444,387); --NPC: A_Planar_Projection pops in middle of room
 eq.depop_with_timer(220016); -- depop the trigger

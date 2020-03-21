480412cb1e71be0ce47072c5b55cd3f7ae30b14c
huffin <acg1089@aol.com>
2020-03-21 16:21:00 -0500

    Update A_Chaos_Wraith.lua

--- potactics/A_Chaos_Wraith.lua
+++ potactics/A_Chaos_Wraith.lua
@@ -1,13 +1,9 @@
 -- A_Chaos_Wraith (214114)) part of rztwl script
 
 function event_spawn(e)
-local rallos = eq.get_entity_list():GetMobByNpcTypeID(214113);	--#Rallos_Zek_the_Warlord (214113)
-eq.move_to(rallos:GetX(),rallos:GetY(),rallos:GetZ(),rallos:GetHeading(),true);
-e.self:SetRunning(true);
-eq.set_timer('depop', 900 * 1000);
+	eq.set_timer('timetomove', 2 * 1000);
+	eq.set_timer('depop', 900 * 1000);
 end
-
-
 
 function event_combat(e)
 	if (e.joined == true) then
@@ -30,8 +26,14 @@ function event_timer(e)
 		if (e.self:GetX() > 950) then
 			e.self:GotoBind();
 			e.self:WipeHateList();
+			eq.set_timer('timetomove', 2 * 1000);
 		else
 			eq.set_timer("OOBcheck", 6 * 1000);
 		end
+	elseif (e.timer == 'timetomove') then
+		eq.stop_timer('timetomove');
+		local rallos = eq.get_entity_list():GetMobByNpcTypeID(214113);	--#Rallos_Zek_the_Warlord (214113)
+		eq.move_to(rallos:GetX(),rallos:GetY(),rallos:GetZ(),rallos:GetHeading(),true);
+		e.self:SetRunning(true);
 	end
 end

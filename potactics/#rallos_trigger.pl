771b236ab513124847046f1326f96a6be3717167
huffin <acg1089@aol.com>
2020-03-22 18:59:50 -0500

    Update #rallos_trigger.pl
    adjust spawn locs tz vz

--- potactics/#rallos_trigger.pl
+++ potactics/#rallos_trigger.pl
@@ -30,8 +30,8 @@ sub EVENT_SIGNAL {
                     quest::ze(0, "The air of Drunder grows strangely cold as a rumble shakes through the fortress' walls. The Warlord stirs.");
 		    quest::stoptimer("Decorin");
                     quest::settimer("VTZek", 1800); # 30min to kill the Zek brothers
-                    quest::unique_spawn(214111,188,0,996,580,133.1,385); # NPC: #Vallon_Zek_
-                    quest::unique_spawn(214108,187,0,996,-559,133.1,385); # NPC: #Tallon_Zek
+                    quest::unique_spawn(214111,188,0,620,580,145,385); # NPC: #Vallon_Zek_
+                    quest::unique_spawn(214108,187,0,620,-560,145,385); # NPC: #Tallon_Zek
 					$entity_list->FindDoor(14)->SetLockPick(0);
 					$entity_list->FindDoor(15)->SetLockPick(0);
 					$entity_list->FindDoor(16)->SetLockPick(0);
@@ -50,8 +50,8 @@ sub EVENT_SIGNAL {
 		quest::ze(0, "The air of Drunder grows strangely cold as a rumble shakes through the fortress' walls. The Warlord stirs.");
                     quest::stoptimer("Decorin");
                     quest::settimer("VTZek", 1800); # 30min to kill the Zek brothers
-                    quest::unique_spawn(214111,188,0,996,580,133.1,385); # NPC: #Vallon_Zek_
-                    quest::unique_spawn(214108,187,0,996,-559,133.1,385); # NPC: #Tallon_Zek
+                    quest::unique_spawn(214111,188,0,620,580,145,385); # NPC: #Vallon_Zek_
+                    quest::unique_spawn(214108,187,0,620,-560,145,385); # NPC: #Tallon_Zek
 					$entity_list->FindDoor(14)->SetLockPick(0);
 					$entity_list->FindDoor(15)->SetLockPick(0);
 					$entity_list->FindDoor(16)->SetLockPick(0);

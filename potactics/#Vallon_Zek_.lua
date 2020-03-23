7a6b4aad6d14d37f9b0634f74485fe2e64edb883
huffin <acg1089@aol.com>
2020-03-23 14:00:51 -0500

    Update #Vallon_Zek_.lua

--- potactics/#Vallon_Zek_.lua
+++ potactics/#Vallon_Zek_.lua
@@ -1,47 +1,4 @@
-sub EVENT_SPAWN {
-	quest::setnexthpevent(51);
-	quest::SetRunning(1);
-}
-
-sub EVENT_DEATH_COMPLETE {
-	quest::stoptimer("despawn");
-	quest::signalwith(214123, 214111, 0);	# Tell trigger that Vallon is dead
-	quest::signalwith(214084,1);
-}
-
-sub EVENT_HP {
-	if($hpevent == 51) {
-    quest::spawn2(214129,0,0,($x + 10),$y,$z,$h);
-    quest::spawn2(214129,0,0,($x + 10),$y,$z,$h);
-  }
-}	
-
-sub EVENT_SIGNAL {
-  quest::stoptimer("despawn");
-  quest::depop();
-  quest::depopall(214084);
-  quest::depopall(214128);
-}
-
-sub EVENT_TIMER {
-	if ($timer eq "despawn") {
-		quest::stoptimer("despawn");
-		quest::depop();
-	}
-}
-
-sub EVENT_WAYPOINT_ARRIVE {
-	if ($wp == 9) {
-		quest::spawn2(214084,0,0,359,73,169,255); # Gindan_Flayer 214084
-		quest::spawn2(214084,0,0,383,73,169,255); # Gindan_Flayer 214084
-		quest::spawn2(214084,0,0,308,73,169,255); # Gindan_Flayer 214084
-		quest::spawn2(214084,0,0,283,73,169,255); # Gindan_Flayer 214084
-	}
-}
-
-# End of File  Zone: PoTactics  ID: 214111 -- #Vallon_Zek_ part of rztwl script
-# Used in Rallos Zek event
-
+-- Vallon Zek part of rztwl script Huffin rewrite
 function event_spawn(e)
 	eq.set_next_hp_event(51);
 	e.self:SetRunning(true);

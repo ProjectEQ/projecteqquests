315ce43e1df0a6e83683c77bad50389072cb1668
huffin <acg1089@aol.com>
2020-03-23 16:32:01 -0500

    Create ##Anival_the_Blade.lua

---
+++ potactics/##Anival_the_Blade.lua
@@ -0,0 +1,20 @@
+-- (214138) loot 2.5 min depop
+function event_spawn(e)
+eq.set_timer('depop', 150 * 1000);
+end
+
+function event_combat(e)
+if (e.joined == true) then
+if(not eq.is_paused_timer('depop')) then
+			eq.pause_timer('depop');
+		end
+	else
+		eq.resume_timer('depop');
+	end
+end
+
+function event_timer(e)
+if (e.timer == 'depop') then
+eq.depop();
+end
+end

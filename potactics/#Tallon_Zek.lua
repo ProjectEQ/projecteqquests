560af16f598580d80809458464b5e12960cf79a7
huffin <acg1089@aol.com>
2020-03-23 14:36:59 -0500

    Update #Tallon_Zek.lua

--- potactics/#Tallon_Zek.lua
+++ potactics/#Tallon_Zek.lua
@@ -1,6 +1,6 @@
--- #Tallon_Zek (214108) part of RZTWL script
+-- #Tallon_Zek (214108) part of RZTWL script Huffin rewrite
 function event_spawn(e)
-    eq.set_timer("despawn", 2700000) -- 45 mins
+    eq.set_timer('depop', 1020 * 1000); --17 minutes of no combat on either one until vz/tz depop happens
     e.self:SetRunning(true);
 end
 
@@ -9,26 +9,29 @@ function event_death_complete(e)
     eq.signal(214086,1); -- tell the Hendin's to start their timer on depop
 end
 
-function event_signal(e)
-    eq.depop();
-    eq.depop_all(214086);
-end
-
 function event_combat(e)
-    if e.joined == true then
-        eq.set_timer("barb", math.random(1, 4) * 1000)
-    else
-        eq.stop_timer("barb");
-    end
+	if (e.joined == true) then
+		if(not eq.is_paused_timer('depop')) then
+			eq.pause_timer('depop');
+		end
+	eq.set_timer("barb", math.random(1, 4) * 1000)
+	else
+		eq.resume_timer('depop');
+		eq.stop_timer("barb");
+	end
 end
 
 function event_timer(e)
     if (e.timer == "barb") then
         e.self:CastSpell(eq.ChooseRandom(2449, 2450, 2451, 2452), e.self:GetTarget():GetID())
-        eq.set_timer("barb", math.random(2, 6) * 1000)
-    elseif (e.timer == "despawn") then
-        eq.depop();
-	eq.depop_all(214068); -- depop all of my Hendin's
+        eq.set_timer("barb", math.random(1, 6) * 1000)
+    elseif (e.timer == 'depop') then
+	eq.signal(214123,777); --  tell trigger mob tz/vz failed, reset sequence
+	eq.depop_all(214129); -- depop VZ splits
+	eq.depop_all(214111); -- #Vallon_Zek_ (214111)
+	eq.depop_all(214086); --Hendin_Shadow_Master (214086)
+	eq.depop_all(214084); --Gindan_Flayer 214084
+	eq.depop();
     end
 end
 

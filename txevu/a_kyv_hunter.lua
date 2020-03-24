4816a1eef940a5d3bef288eea91a07f1e908300f
huffin <acg1089@aol.com>
2020-03-24 10:50:44 -0500

    Update a_kyv_hunter.lua

--- txevu/a_kyv_hunter.lua
+++ txevu/a_kyv_hunter.lua
@@ -16,6 +16,7 @@ function event_timer(e)
     if (e.timer == "casting") then
         e.self:CastSpell(eq.ChooseRandom(4850, 4849, 4851), e.self:GetTarget():GetID())
         eq.set_timer("casting", 10 * math.random(1, 3) * 1000)
+        e.self:Emote("releases a volley of arrows.");
     end
 end
 

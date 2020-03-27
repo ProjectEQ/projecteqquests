0fd1d531628cdabcbf85f66e148c929558cb66ba
huffin <acg1089@aol.com>
2020-03-27 09:06:00 -0500

    Update #Terris_Thule.pl
    flavor depop

--- ponightmare/#Terris_Thule.pl
+++ ponightmare/#Terris_Thule.pl
@@ -7,3 +7,14 @@ if($timer == 5) {
 quest::depop();
 }
 }
+
+sub EVENT_SIGNAL { 
+if($signal == 1) {
+quest::say("'You fool!  You did not earn this prize on your own!  The contract that has been drawn is now invalid.  You will never leave my grasp, prepare your soul for eternal torment!");
+}
+if($signal == 2) {
+quest::emote("laughs heartily and then vanishes in a swirl of incorporeal mist.");
+quest::emote("begins to cast the gate spell.");
+eq.depop();
+}
+}

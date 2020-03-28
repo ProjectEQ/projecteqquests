ce74e4c6b7c9dd741d25e2e3c9d769a00fe93e9c
huffin <acg1089@aol.com>
2020-03-27 20:05:34 -0500

    Update #Terris_Thule.pl

--- ponightmare/#Terris_Thule.pl
+++ ponightmare/#Terris_Thule.pl
@@ -10,7 +10,7 @@ quest::depop();
 
 sub EVENT_SIGNAL { 
 if($signal eq 1) {
-quest::say("'You fool!  You did not earn this prize on your own!  The contract that has been drawn is now invalid.  You will never leave my grasp, prepare your soul for eternal torment!");
+quest::say("You fool!  You did not earn this prize on your own!  The contract that has been drawn is now invalid.  You will never leave my grasp, prepare your soul for eternal torment!");
 }
 if($signal eq 2) {
 quest::emote("laughs heartily and then vanishes in a swirl of incorporeal mist.");

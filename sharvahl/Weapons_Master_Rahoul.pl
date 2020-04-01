668aaf44d86266bdf30811520ffb3859a4e18145
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-04-01 15:43:02 -0500

    Update Weapons_Master_Rahoul.pl
    parenthesis quest please work

--- sharvahl/Weapons_Master_Rahoul.pl
+++ sharvahl/Weapons_Master_Rahoul.pl
@@ -17,10 +17,10 @@ sub EVENT_SAY {
   if ($text=~/padding/i) {
     quest::say("When traversing the Deep, you will run across a shrieking fungus. Those creatures are much more durable and dangerous than the other fungi that you may encounter in the caverns of Luclin. It is for this reason that we use their flesh as padding. Take care when hunting them, for they will provide a good challenge.");
   }
-  if ($client->GetGlobal("beast_turnin") == 1) && ($text=~/willing/i) {
+  if ($client->GetGlobal("beast_turnin") == 1 && $text=~/willing/i) {
     quest::say("First, we will need you to contribute some of your own funds in order to obtain this particular piece. It is in a form of a statuette that will grant you an audience with Muada. I have used some rare components to make these that will cost you in the order of 2000 platinum pieces. Yes, it's a high cost, but we must keep our coffers healthy. You will need to create your own leather [" . quest::saylink("adornments") . "] however. When you have given me the required funds, I will give you the beastlord's statuette to work with.");
   }
-  if($client->GetGlobal("beast_turnin") == 1) && ($text=~/adornments/i) {
+  if($client->GetGlobal("beast_turnin") == 1 && $text=~/adornments/i) {
     quest::say("Yes, Muada demands that each beastlord put some care and devotion into the making of this artifact. You will need to craft your own decoration to this statue to be made from two hides of the strange flesh hounds in Taelosia. You will need to be sure your tailoring is up to par for this, or it will become a very expensive endeavor to gain favor with Muada. Take this pattern to work with when you have your hides. You will need to tan them first and acid wash will do well to remove the impurities and markings, of course. When you are finished, you may present the item to Elder Muada.");
     quest::summonitem(57056); # Item: Adornments Pattern
   }

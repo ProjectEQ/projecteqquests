b1226fc26f951bdaa11ed12a2c5bbfc9fabd3aeb
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-04-01 15:16:21 -0500

    Update Weapons_Master_Rahoul.pl
    add a global check to require the player to do the previous turn in quest,  previously saying "adornments" let them skip half the quest and that is not intentional. 
    
    add a global check upon "adornments" to check to see if player turned in the trash loot the npc wanted them to go gather.

--- sharvahl/Weapons_Master_Rahoul.pl
+++ sharvahl/Weapons_Master_Rahoul.pl
@@ -20,7 +20,7 @@ sub EVENT_SAY {
   if ($text=~/willing/i) {
     quest::say("First, we will need you to contribute some of your own funds in order to obtain this particular piece. It is in a form of a statuette that will grant you an audience with Muada. I have used some rare components to make these that will cost you in the order of 2000 platinum pieces. Yes, it's a high cost, but we must keep our coffers healthy. You will need to create your own leather [" . quest::saylink("adornments") . "] however. When you have given me the required funds, I will give you the beastlord's statuette to work with.");
   }
-  if ($text=~/adornments/i) {
+  if if($client->GetGlobal("beast_turnin") == 1) && ($text=~/adornments/i) {
     quest::say("Yes, Muada demands that each beastlord put some care and devotion into the making of this artifact. You will need to craft your own decoration to this statue to be made from two hides of the strange flesh hounds in Taelosia. You will need to be sure your tailoring is up to par for this, or it will become a very expensive endeavor to gain favor with Muada. Take this pattern to work with when you have your hides. You will need to tan them first and acid wash will do well to remove the impurities and markings, of course. When you are finished, you may present the item to Elder Muada.");
     quest::summonitem(57056); # Item: Adornments Pattern
   }
@@ -40,6 +40,9 @@ sub EVENT_ITEM {
   }
   if (plugin::check_handin(\%itemcount, 9053 => 2, 9054 => 1, 9052 => 1)) {
     quest::say("Ah, I see you have been so kind as to help us. Muada would like you take your assistance one step further. If you are [" . quest::saylink("willing") . "], of course.");
+    if($client->GetGlobal("beast_turnin") ==undef) {
+			quest::setglobal("beast_turnin", "1", 5, "F");
+    }
   }
   if ($platinum >= 2000) {
     quest::say("I wasn't sure you would part with such a great amount of coin. I am impressed. Here is your statuette to work with. You will need to tailor the leather adornments you create and work with them into the statue itself, making it your own.");

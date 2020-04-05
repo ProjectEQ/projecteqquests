1652d57abc243815f7e9325bf6511a30b3587f99
huffin <acg1089@aol.com>
2020-04-05 07:10:34 -0500

    Create The_banners.lua

---
+++ hole/The_banners.lua
@@ -0,0 +1,6 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("of this once great city are hung on the wall with pride. The tattered and torn fabric is rough to the touch, yet very brittle.");
+eq.depop_with_timer();
+end
+end

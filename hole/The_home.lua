0bc0310654bee793efcf2443439d103d22842212
huffin <acg1089@aol.com>
2020-04-05 06:56:33 -0500

    Create The_home.lua
    flavor trap

---
+++ hole/The_home.lua
@@ -0,0 +1,6 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("of the golems opens up before you. Their massive forms have made great indentations in the ground.");
+eq.depop_with_timer();
+end
+end

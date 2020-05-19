4e51ce8cbfd20593e08f9013d44087452c331d9a
huffin <acg1089@aol.com>
2020-05-18 20:27:51 -0500

    Create #Bloodspear_the_Fallen.lua

---
+++ nadox/#Bloodspear_the_Fallen.lua
@@ -0,0 +1,34 @@
+local wave_counter = 0;
+
+function event_spawn(e)
+  wave_counter = 0;
+end
+
+function event_signal(e)
+if (e.signal == 1) then
+  wave_counter = wave_counter + 1;
+	  if (wave_counter == 3) then
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+	  elseif (wave_counter == 10) then
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Broken_Skull_Spiritist (227097)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+			eq.spawn2(227098,0,0,-527,-95,51,385); --a_Cathedral_Protector (227095)
+	  elseif (wave_counter == 20) then
+			eq.spawn2(227336,0,0,-527,-95,51,385); --#Bloodspear_the_Wretched (227336)
+			eq.depop();
+	  end
+  end
+end

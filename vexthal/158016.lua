e5a9faa995c29ada6c239b6ab83b0274c34d87b6
huffin <acg1089@aol.com>
2020-05-30 09:39:16 -0500

    Update 158016.lua

--- vexthal/158016.lua
+++ vexthal/158016.lua
@@ -1,5 +1,5 @@
---spawn point IDs for south wing TVX adds to move to him when TVX is aggro'd #Thall_Va_Xakra (158016)
--- local spawnpt_list = {36546, 36547, 36545, 36518, 36519, 36516, 36517, 36520, 36521, 36512, 36513, 36522, 36523, 36540, 36541, 36660, 36661, 36524, 36525, 36526, 36527, 36548, 36549, 36550, 36552, 36553};
+--spawn group IDs for south wing TVX adds to move to him when TVX is aggro'd #Thall_Va_Xakra (158016)
+
 local spawnpt_list = {17641,17642,17640,17639,17649,17650,17646,17645,17643,17644,17617,17616,17615,17610,17609,17611,17613,17612,17614,17635,17636,17637,17638,17676,17622,17621,17677};
 
 function event_combat(e)

5ed6a298c5f5a88483cec616fce6591b3edd21e9
huffin <acg1089@aol.com>
2020-05-30 20:36:10 -0500

    Delete a_Broken_Skull_bloodguard.lua

--- torgiran/a_Broken_Skull_bloodguard.lua
+++
@@ -1,5 +0,0 @@
-function event_death(e)
-  if(eq.get_entity_list():GetNPCBySpawnID(46864) == true or eq.get_entity_list():GetNPCBySpawnID(282503) == true) then --spawn groups inside the treasurer room
-    eq.signal(226211,1); --signal The_room to add to wave counter
-  end
-end

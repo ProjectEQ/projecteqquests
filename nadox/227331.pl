5608f5652e86df5edc92c01900090bf7121b992c
huffin <acg1089@aol.com>
2020-06-02 15:36:09 -0500

    Delete 227331.pl

--- nadox/227331.pl
+++
@@ -1,8 +0,0 @@
-#BeginFile: nadox\227331.pl
-#Script for Crypt of Nadox - Innoruk (Nadox)
-
-sub EVENT_SPAWN {
-  quest::shout("You fools! I shall tear the life from your bodies for this trespass, and what you have done to my creations!");
-}
-
-#EndFile: nadox\227331.pl (Innoruk)

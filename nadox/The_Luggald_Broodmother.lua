c2949fa6aae12027b77dfe450c9b1ee873547080
huffin <acg1089@aol.com>
2020-06-03 08:56:51 -0500

    Update The_Luggald_Broodmother.lua

--- nadox/The_Luggald_Broodmother.lua
+++ nadox/The_Luggald_Broodmother.lua
@@ -3,14 +3,14 @@ function event_waypoint_arrive(e)
 		eq.spawn2(227003, 51, 0, 1257, 675, -88, 126); -- NPC: Defender_of_the_Broodmother
 		eq.spawn2(227003, 51, 0, 1257, 665, -88, 126); -- NPC: Defender_of_the_Broodmother
 	elseif(e.wp == 6) then
-		eq.spawn2(227003, 0, 0, 1206, 1000, -74, 254); -- NPC: Defender_of_the_Broodmother
-		eq.spawn2(227003, 0, 0, 1216, 1000, -74, 254); -- NPC: Defender_of_the_Broodmother
-		eq.spawn2(227002, 0, 0, 1211, 1000, -74, 254); -- NPC: a_Luggald_Archseeker
+		eq.spawn2(227003, 52, 0, 1206, 1000, -74, 254); -- NPC: Defender_of_the_Broodmother
+		eq.spawn2(227003, 52, 0, 1216, 1000, -74, 254); -- NPC: Defender_of_the_Broodmother
+		eq.spawn2(227002, 52, 0, 1211, 1000, -74, 254); -- NPC: a_Luggald_Archseeker
 	elseif(e.wp == 13) then
-		eq.spawn2(227003, 0, 0, 820, 977, -74, 254); -- NPC: Defender_of_the_Broodmother
-		eq.spawn2(227003, 0, 0, 825, 977, -74, 254); -- NPC: Defender_of_the_Broodmother
-		eq.spawn2(227002, 0, 0, 830, 977, -74, 254); -- NPC: a_Luggald_Archseeker
-		eq.spawn2(227002, 0, 0, 835, 977, -74, 254); -- NPC: a_Luggald_Archseeker
+		eq.spawn2(227003, 53, 0, 820, 977, -74, 254); -- NPC: Defender_of_the_Broodmother
+		eq.spawn2(227003, 53, 0, 825, 977, -74, 254); -- NPC: Defender_of_the_Broodmother
+		eq.spawn2(227002, 53, 0, 830, 977, -74, 254); -- NPC: a_Luggald_Archseeker
+		eq.spawn2(227002, 53, 0, 835, 977, -74, 254); -- NPC: a_Luggald_Archseeker
 	elseif(e.wp == 16) then
 		eq.spawn2(227003, 0, 0, 861, 675, -88, 126); -- NPC: Defender_of_the_Broodmother
 		eq.spawn2(227003, 0, 0, 861, 665, -88, 126); -- NPC: Defender_of_the_Broodmother

7526c8d7b6b5d067adfff4f21e3f3ff8a1cad05c
huffin <acg1089@aol.com>
2020-06-20 19:30:30 -0500

    Create As_you.lua

---
+++ chardokb/As_you.lua
@@ -0,0 +1,6 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("enter the room, you take notice of four sarnak in deep meditations. They pay no attention to you and continue on with their incantations.");
+eq.depop_with_timer();
+end
+end

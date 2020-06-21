2dd022c82950a555c511a72075ac34fd2b634d71
huffin <acg1089@aol.com>
2020-06-21 10:23:53 -0500

    Create As_you_push.lua

---
+++ chardokb/As_you_push.lua
@@ -0,0 +1,6 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("further into the haunted halls, you question your reason for adventuring here.");
+eq.depop_with_timer();
+end
+end

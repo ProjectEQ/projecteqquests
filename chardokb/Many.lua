3fea0ef9992058547203a29147e341de1608c58b
huffin <acg1089@aol.com>
2020-06-22 15:12:56 -0500

    Create Many.lua

---
+++ chardokb/Many.lua
@@ -0,0 +1,6 @@
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("books are scattered around in this room that seems to be some kind of tactical planning area.");
+eq.depop_with_timer();
+end
+end

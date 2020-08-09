9c6f4487b7f1a3cc2b69c62647fa86b1dcb7ec5e
huffin <acg1089@aol.com>
2020-08-09 14:44:53 -0500

    Update Saryrn.lua

--- potimeb/Saryrn.lua
+++ potimeb/Saryrn.lua
@@ -11,8 +11,7 @@ end
 
 function event_combat(e)
 	if (e.joined == false) then
-		e.self:SetHP(e.self:GetMaxHP());
-		eq.set_next_hp_event(91);
+		eq.set_timer('reset', 6 * 1000);
 	end
 end
 
@@ -27,6 +26,7 @@ function event_hp(e)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
+		eq.set_timer('saryrnhelp', 1 * 1000);
 		eq.set_next_hp_event(51);
 	elseif (e.hp_event == 51) then
 		-- spawn Saryrn's 4 random mobs from A_Mouth_of_Insanity, A_Tormentor, A_Mouth_of_Dementia, A_Twisted_Tormentor
@@ -34,6 +34,7 @@ function event_hp(e)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
+		eq.set_timer('saryrnhelp', 1 * 1000);
 		eq.set_next_hp_event(11);
 	elseif (e.hp_event == 11) then
 		-- spawn Saryrn's 4 random mobs from A_Mouth_of_Insanity, A_Tormentor, A_Mouth_of_Dementia, A_Twisted_Tormentor
@@ -41,5 +42,30 @@ function event_hp(e)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-219,-350,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
 		eq.spawn2(eq.ChooseRandom(223083,223084,223085,223086),0,0,-350,-230,348,0); -- NPC(s): A_Mouth_of_Insanity (223083), A_Mouth_of_Dementia (223084), A_Tormentor (223085), A_Twisted_Tormentor (223086)
+		eq.set_timer('saryrnhelp', 1 * 1000);
 	end
+end
+
+function event_timer(e)
+	if(e.timer == 'saryrnhelp') then
+		help_saryrn(e);
+		eq.stop_timer('saryrnhelp');
+	elseif(e.timer == 'reset') then
+		e.self:SetHP(e.self:GetMaxHP());
+		eq.set_next_hp_event(91);
+		eq.depop_all(223083); -- depop adds
+		eq.depop_all(223084);
+		eq.depop_all(223085);
+		eq.depop_all(223086);
+		eq.stop_timer('reset');
+	end
+end
+
+function help_saryrn(e)
+	local npc_list =  eq.get_entity_list():GetNPCList();
+	for npc in npc_list.entries do
+		if (npc.valid and (npc:GetNPCTypeID() == 223083 or npc:GetNPCTypeID() == 223084 or npc:GetNPCTypeID() == 223085 or npc:GetNPCTypeID() == 223086)) then
+		npc:CastToNPC():MoveTo(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 0, false);
+		end
+	end		
 end

810e73cdc1c69ea21ee60fffed11ae027e8dae8e
huffin <acg1089@aol.com>
2020-08-11 10:11:38 -0500

    Create 223230.lua

---
+++ potimeb/223230.lua
@@ -0,0 +1,22 @@
+-- a_summoned_boar_ (223230) part of Rallos Zek p5 adds
+function event_combat(e)
+  if (e.joined == true) then
+    eq.set_timer("OOBcheck", 6 * 1000);
+  else
+    eq.stop_timer("OOBcheck");
+  end
+end
+
+
+function event_timer(e)
+if(e.timer=="OOBcheck") then
+eq.stop_timer("OOBcheck");
+	if (e.self:GetX() < 169 or e.self:GetY() > -179) then
+		e.self:GotoBind();
+		e.self:WipeHateList();
+		e.self:CastSpell(3230,e.self:GetID()); -- Spell: Balance of the Nameless
+	else
+		eq.set_timer("OOBcheck", 6 * 1000);
+	end
+end
+end

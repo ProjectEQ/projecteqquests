a7f2db1075a4ac14ca090a328320c7d31dc1ff1a
huffin <acg1089@aol.com>
2020-08-13 08:19:42 -0500

    Create A_Nightmare_Knight_of_Terris.lua

---
+++ potimeb/A_Nightmare_Knight_of_Terris.lua
@@ -0,0 +1,21 @@
+function event_combat(e)
+	if (e.joined == true) then
+		eq.set_timer("OOBcheck", 6 * 1000);
+	else
+		eq.stop_timer("OOBcheck");
+	end
+end
+
+
+function event_timer(e)
+if(e.timer=="OOBcheck") then
+eq.stop_timer("OOBcheck");
+	if (e.self:GetX() > -110 or e.self:GetY() < 103) then
+		e.self:GotoBind();
+		e.self:WipeHateList();
+		e.self:CastSpell(3230,e.self:GetID()); -- Spell: Balance of the Nameless
+	else
+		eq.set_timer("OOBcheck", 6 * 1000);
+	end
+end
+end

10fdbdb3da8a2ddbbfbc5b5edaedbe5c05994d27
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-08-22 19:42:46 -0500

    Update #Arias.pl
    fix update activity

--- tutorialb/#Arias.pl
+++ tutorialb/#Arias.pl
@@ -12,7 +12,7 @@ sub EVENT_SAY {
 }
   elsif ($text=~/hail/i && quest::istaskactivityactive(22,6)){
     quest::say("Well done, my friend. Take this kobold skull charm. You are now ready to join ranks with the slaves and fight against the Gloomingdeep kobolds. Speak with Guard Rahtiz for your next assignment.");
-    quest::updatetaskactivity(22,6); 
+    quest::updatetaskactivity(22,7); 
 }
   elsif ($text=~/hail/i){
     quest::say("We found the other slaves! Not bad, my friend, not bad. No matter what happens in the mines, you should always be able to find your way back here.");

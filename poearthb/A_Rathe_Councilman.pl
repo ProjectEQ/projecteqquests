32a60c2c955289f22fe24c9710160dcb187ca2e5
huffin <acg1089@aol.com>
2020-08-28 11:30:53 -0500

    Update A_Rathe_Councilman.pl

--- poearthb/A_Rathe_Councilman.pl
+++ poearthb/A_Rathe_Councilman.pl
@@ -14,7 +14,7 @@ sub EVENT_DEATH_COMPLETE {
 }
 
 sub EVENT_TIMER {
-    if($timer eq "banish_random_client" && !$mob->IsMezzed()) {
+    if($timer eq "banish_random_client" && !$mob->Mesmerize()) {
 		my $hate_target = $npc->GetHateRandom();
 		if ($hate_target->IsClient())
 		{

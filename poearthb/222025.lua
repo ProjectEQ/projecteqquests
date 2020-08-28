c0c9db96f14f1c511800621afc32110cd3ea8313
huffin <acg1089@aol.com>
2020-08-28 15:18:21 -0500

    Update 222025.lua

--- poearthb/222025.lua
+++ poearthb/222025.lua
@@ -9,7 +9,7 @@ end
 function event_timer(e)
 local rand_hate = e.self:GetHateRandom()
 		eq.debug("banish selected: " ..rand_hate:GetName());
-		if (rand_hate.valid and rand_hate:IsClient()) then
+		if (rand_hate.valid and rand_hate:IsClient() and not e.self:IsMezzed()) then
 			local rand_hate_v = rand_hate:CastToClient()
 			if (rand_hate_v.valid) then
 				eq.debug(rand_hate_v:GetName());

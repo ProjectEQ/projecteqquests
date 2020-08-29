86255f70683b81a43201066404c6ea4a225f8efb
huffin <acg1089@aol.com>
2020-08-29 10:56:01 -0500

    Update 222008.lua

--- poearthb/222008.lua
+++ poearthb/222008.lua
@@ -3,12 +3,6 @@ function event_spawn(e)
     eq.set_next_hp_event(70)
 end
 
-function event_combat(e)
-    if (e.joined == false) then
-        e.self:SetHP(e.self:GetMaxHP());
-        eq.set_next_hp_event(70);
-    end
-end
 
 --[ NPC flecks down at each HP event
 --Starting damage:
@@ -44,5 +38,37 @@ function event_hp(e)
         eq.modify_npc_stat("min_hit", "185")
         eq.modify_npc_stat("max_hit", "850")
         eq.zone_emote(15, "The ground shudders beneath your feet as flecks of dirt and stone fall away from one of the Rathe.")
+    end
+end
+
+function event_combat(e)
+	if (e.joined) then
+		eq.set_timer("banish", 60000)
+		eq.stop_timer("reset")
+	else
+		eq.stop_timer("banish")
+		eq.set_timer("reset", 45000)
+	end
+end
+
+function event_timer(e)
+    if(e.timer=="banish") then
+    local rand_hate = e.self:GetHateRandom()
+		eq.debug("banish selected: " ..rand_hate:GetName());
+		if (rand_hate.valid and rand_hate:IsClient() and not e.self:IsMezzed() and not rand_hate:IsPet() and e.self:GetHPRatio() >= 11) then
+			local rand_hate_v = rand_hate:CastToClient()
+			if (rand_hate_v.valid) then
+				eq.debug(rand_hate_v:GetName());
+				e.self:Say("begone " .. rand_hate_v:GetName())
+				e.self:SetHate(rand_hate_v, 1, 1)
+				rand_hate_v:MovePC(222,1864.94, 941.05, -254.0, 0)
+			end
+		end
+    elseif(e.timer=="reset") then
+	    eq.stop_timer("reset")
+	    e.self:SetHP(e.self:GetMaxHP());
+        eq.set_next_hp_event(70);
+        eq.modify_npc_stat("min_hit", "623")
+        eq.modify_npc_stat("max_hit", "2964")
     end
 end

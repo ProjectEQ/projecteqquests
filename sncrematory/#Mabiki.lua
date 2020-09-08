bcadf076ae8d9d804c908f8a9f9b50d45723c11a
hg <4683435+hgtw@users.noreply.github.com>
2020-09-07 20:38:02 -0400

    Rewrite second sewer expedition (sncrematory)
    Replace some perl scripts with lua
    
    Add live dialogue and emotes
    
    Modify furnace room trigger to behave like live
    
    Use live spawn locations for the furnace room npcs
    
    Make ghostly essence violet like live
    
    Make ghostly essences turn kos when time runs out. Live actually spawns
    a new mob (an evil ghostly essence) but those don't exist in db
    
    Add area emotes

--- sncrematory/#Mabiki.lua
+++ sncrematory/#Mabiki.lua
@@ -1,4 +1,5 @@
+-- On Death: "Mabiki's corpse's spirit dissipates in a din of chaotic magic.  Dark clouds encircle you, engulfing you in a smothering embrace.  You are momentarily blinded.  After you come to your senses, you see that a ghostly essence has formed and is moving towards a room to the east."
 function event_death_complete(e)
-eq.spawn2(288086,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --#A_Ghostly_Essence
-e.self:Emote("s corpse's spirit dissipates in a din of chaotic magic.  Dark clouds encircle you, engulfing you in a smothering embrace.  You are momentarily blinded.  After you come to your senses, you see that a ghostly essence has formed and is moving towards a room to the east.);
+  local essence = eq.spawn2(288086, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 0) -- NPC: a_ghostly_essence
+  essence:SetEntityVariable("pylon_position", "3")
 end

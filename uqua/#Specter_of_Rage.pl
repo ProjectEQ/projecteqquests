9001796125cb09ec8203f5570c07efeefddbe850
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-09-21 16:48:18 -0500

    Update #Specter_of_Rage.pl
    update to add return items if not correct items.

--- uqua/#Specter_of_Rage.pl
+++ uqua/#Specter_of_Rage.pl
@@ -5,4 +5,5 @@ sub EVENT_ITEM {
 	if (plugin::check_handin(\%itemcount, 67705 => 1)) {
 		quest::signalwith(292008,$instid); # NPC: a_spirit_guardian
 	}
+	plugin::return_items(\%itemcount); #return items if not the ones required
 }

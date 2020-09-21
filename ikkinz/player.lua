5d8671bdf05431c3991ba5b90f85a1eea0fef56e
hg <4683435+hgtw@users.noreply.github.com>
2020-09-20 23:20:15 -0400

    Add ikkinz door messages from group trials

--- ikkinz/player.lua
+++ ikkinz/player.lua
@@ -12,6 +12,10 @@ local instance_ver = eq.get_zone_instance_version();
 			e.self:Message(15, "The markings on the Overseer's Geostone line up with the markings on the door. It rumbles to life as it opens for you.");			
 		elseif (e.self:HasItem(60225)==true and instance_ver==4) then
 			e.self:Message(15, "The glyphs on Axtekilitek and the door glow momentarily before the doors slide open.");			
+		elseif instance_ver == 2 then -- group trial 3
+			e.self:Message(15, "The stone nearby shudders as the massive doors part for you.")
+		elseif instance_ver == 0 or instance_ver == 1 then -- group trials 1 or 2
+			e.self:Message(13, "The door appears to have been sealed shut long ago. You will be unable to open it.")
 		end		
 	elseif (door_id == 13 or door_id ==14) then
 		if (e.self:HasItem(60249)==true and instance_ver==6) then
@@ -22,6 +26,14 @@ local instance_ver = eq.get_zone_instance_version();
 	elseif (door_id == 10 or door_id ==12) then
 		if (e.self:HasItem(60227)==true and instance_ver==4) then
 			e.self:Message(15, "Kruxilitek seemingly melds to the door before falling back into your hand. The door opens with ease.");
+		elseif instance_ver == 2 then -- group trial 3
+			e.self:Message(13, "This door has been sealed magically from the other side. There is no way to tell what might lie in the halls beyond.")
 		end		
+	elseif door_id == 2 or door_id == 3 or door_id == 5 then
+		e.self:Message(15, "Magic rocks rub against stone and charge the air as the door opens.")
+	elseif door_id == 6 or door_id == 7 and (instance_ver == 0 or instance_ver == 1 or instance_ver == 2) then -- group instances
+		e.self:Message(13, "This door is damaged and has been closed for some time. There is no telling who caused the damage, but it will be impossible to repair.")
+	elseif door_id == 15 or door_id == 16 then
+		e.self:Message(15, "The door gives way freely as dust falls from the cracks.")
 	end
 end

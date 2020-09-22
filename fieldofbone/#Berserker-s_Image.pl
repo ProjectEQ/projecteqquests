204258c70b19239f0d9fabc7a40ea181b2c735fc
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-09-21 22:20:11 -0500

    Update #Berserker-s_Image.pl
    fix to support other rewrite

--- fieldofbone/#Berserker-s_Image.pl
+++ fieldofbone/#Berserker-s_Image.pl
@@ -1,20 +1,24 @@
-#berserker epic
-#written:wizardanim
-#9/13/07
+--[[
+#Berserker-s_Image
 
-sub EVENT_DEATH_COMPLETE
-{
-quest::signalwith(78100,15,10); # NPC: Mardic_Crimsonsteel
-}
+Berserker 1.0 rewrite
 
-sub EVENT_SPAWN
+Drogerin
+--]]
 
-{
-quest::setnexthpevent(50);
-}
+function event_death_complete(e)
+	local qglobals = eq.get_qglobals(e.self);
+		if (qglobals['bersolo'] == nil) then
+			eq.set_global("bersolo","1",5,"F");
+			eq.signal(78100,15,10); --NPC: Mardic_Crimsonsteel
+		end
+end
 
-sub EVENT_HP
+function event_spawn(e)
+	eq.set_next_hp_event(50);
+end
 
-{
-quest::say("You cannot overcome my infinite rage!");
-}
+
+function event_hp(e)
+	e.self:Say("You cannot overcome my infinite rage!");
+end

707431b227c80b26962f2fd6962d522abfcfa914
hg <4683435+hgtw@users.noreply.github.com>
2020-09-25 18:10:39 -0400

    Use dz for destruction/corruption (power-chambersf)

--- provinggrounds/Projection_of_Power.lua
+++ provinggrounds/Projection_of_Power.lua
@@ -2,45 +2,33 @@
 -- NPC.: Projection_of_Power
 --
 
-function event_spawn(e)
-  eq.set_proximity(e.self:GetX()+10, e.self:GetY()+10, e.self:GetX()-10, e.self:GetY()-10);
-end
+local compass = { "provinggrounds", -3053.37, -1030.69, 310.11 }
+local safereturn = { "provinggrounds", -3057, -1042.375, -307.75, 0.0 }
+local zonein = { 0.0, -37.0, -0.875, 0 }
 
-function event_enter(e)
-    e.self:Say("Perception and split-second reactions are not something you can acquire in training.  It's a gift learned on the battlefield, and the penalty for being a slow learner is a quick and decisive death.  Can you see the arrow as it sails toward your heart and sidestep out of the way?  If you have this ability, now is the opportunity to prove your worth!");
-end
+local destruction = {
+  info = { "Proving Grounds: The Mastery of Destruction", 2, 6 },
+  dz = { "chambersf", 1, eq.seconds("1h"), compass = compass, safereturn = safereturn, zonein = zonein }
+}
+
+local corruption = {
+  info = { "Proving Grounds: The Mastery of Corruption", 6, 54 },
+  dz = { "chambersf", 2, eq.seconds("3h"), compass = compass, safereturn = safereturn, zonein = zonein }
+}
 
 function event_say(e)
   if (e.message:findi("hail")) then
-    e.self:Say("'Halt, " .. e.other:GetCleanName() .. ", and hear my words. Your background and your status are of no consequence. All are welcome to participate in the trials to prove their worth, or die trying. Whosoever approaches me has addressed the embodiment of Power, keeper of the trials of Destruction and Corruption. The former is a small-scale test, while the latter is large-scale. My task is to explain the rules and usher hopefuls into the battle arena. Would you like to hear the rules for [ " .. eq.say_link('small-scale', false, 'small-scale') .. " ] or [large-scale] trials, or do you wish to enroll in [ " .. eq.say_link('Destruction', false, 'Destruction') .. " ] or [ " .. eq.say_link('Corruption', false, 'Corruption') .. " ]?' ");
-
+    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Projection of Power says, 'Halt, " .. eq.get_race_name(e.other:GetRace()) .. ", and hear my words.  Your background and your status are of no consequence.  All are welcome to participate in the trials to prove their worth, or die trying.  Whosoever approaches me has addressed the embodiment of Power, keeper of the trials of Destruction and Corruption.  The former is a small-scale test, while the latter is large-scale.  My task is to explain the rules and usher hopefuls into the battle arena.  Would you like to hear the rules for [" .. eq.say_link("small-scale") .. "] or [" .. eq.say_link("large-scale") .. "] trials, or do you wish to enroll in [" .. eq.say_link("Destruction") .. "] or [" .. eq.say_link("Corruption") .. "]?'")
   elseif (e.message:findi("destruction")) then
-    local instance_requests = require("instance_requests");
-    local lockouts = { { 'MPG_destruction', 'MPG: The Mastery of Destruction' } } 
-    local requests = instance_requests.ValidateRequest('group', 'chambersf', 1, 2, 6, 65, nil, nil, e.other, lockouts);
-    if (requests.valid and requests.flags == 1) then
-      instance_requests.DisplayLockouts(e.other, e.other, lockouts);
-    elseif (requests.valid and requests.flags == 0) then
-      local instance_id = eq.create_instance('chambersf', 1, 3600);
-      eq.assign_group_to_instance(instance_id);
-
-      e.self:Say("'To conquer worlds requires the capability to annihilate vast areas. How much devastation can you inflict in as short a time as possible? Can you cut a swathe in the ranks of your enemy and destroy them with impunity? If you have this ability, now is the opportunity to prove your worth!'");
-    end
-
+    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Projection of Power says, 'To conquer worlds requires the capability to annihilate vast areas.  How much devastation can you inflict in as short a time as possible?  Can you cut a swathe in the ranks of your enemy and destroy them with impunity?  If you have this ability, now is the opportunity to prove your worth!'")
+    e.other:CreateExpedition(destruction.dz, destruction.info)
   elseif (e.message:findi("corruption")) then
-    local instance_requests = require("instance_requests");
-    local lockouts = { { 'MPG_corruption', 'MPG: Trial of Corruption' } }
-    local requests = instance_requests.ValidateRequest('raid',  'chambresf', 2, 2, 54, 65, nil, nil, e.other, lockouts);
-    if (requests.valid and requests.flags == 1) then
-      instance_requests.DisplayLockouts(e.other, e.other, lockouts);
-    elseif (requests.valid and requests.flags == 0) then 
-      local instance_id = eq.create_instance('chambersf', 2, 10800);
-      eq.assign_raid_to_instance(instance_id);
-
-      e.other:Say("Prepare yourself for the greatest challenge of all. The Mastery of Corruption will test your battle prowess as well as your strength of character. Can you see through corruption and rout it out, or are you corrupt yourself ? The spoils of war include the chance to rewrite history. The victor will decide who was right and who was wrong, but in the end - do such things ever matter?' ");
-    end
+    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Projection of Power says, 'Prepare yourself for the greatest challenge of all.  The Mastery of Corruption will test your battle prowess as well as your strength of character.  Can you see through corruption and rout it out, or are you corrupt yourself?  The spoils of war include the chance to rewrite history.  The victor will decide who was right and who was wrong, but in the end - do such things even matter?'")
+    e.other:CreateExpedition(corruption.dz, corruption.info)
   elseif (e.message:findi('small') ) then
-    e.self:Say("'Every test has its rules and these trials are no exception. The following guidelines must be followed to the letter, or else you stand to forfeit or perish. You are allowed no more than six participants in the battle. Once the trial has begun, you must remain in the combat area. Violators of this rule will be warned and then destroyed. Your [ ".. eq.say_link('progress') .. " ] will be carefully monitored. If you take too long, you will fail. These are the rules and they are not negotiable.'");
+    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Projection of Power says, 'Every test has its rules and these trials are no exception.  The following guidelines must be followed to the letter, or else you stand to forfeit or perish.  You are allowed no more than six participants in the battle.  Once the trial has begun, you must remain in the combat area.  Violators of this rule will be warned and then destroyed.  Your progress will be carefully monitored.  If you take too long, you will fail.  These are the rules and they are not negotiable.'")
+  elseif e.message:findi("large") then
+    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Projection of Power says, 'Every test has its rules and these trials are no exception.  The following guidelines must be followed to the letter, or else you stand to forfeit or perish.  You are allowed no more than fifty-four participants in the battle.  Once the trial has begun, you must remain in the combat area.  Violators of this rule will be warned and then destroyed.  Your progress will be monitored.  The longer you tarry in completing your task, the more difficult your task will become.  These are the rules and they are not negotiable.'")
   elseif (e.message:findi("progress")) then
     local mpg_helper = require("mpg_helper");
     mpg_helper.Display_Completed_Trials(e.other);

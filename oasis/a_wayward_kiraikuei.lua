fb70bcc25f6a59262d04a9190428078fa2ab19f5
hg <4683435+hgtw@users.noreply.github.com>
2020-09-27 23:25:36 -0400

    Use dz for rogue 1.5 expedition in oasis

--- oasis/a_wayward_kiraikuei.lua
+++ oasis/a_wayward_kiraikuei.lua
@@ -1,3 +1,9 @@
+local rogue_info = { "Innoruuk's Realm", 6, 36 } -- name from log pasted on safehouse
+local rogue_dz = { "hateplaneb", 1, eq.seconds("1h") }
+rogue_dz.compass = { "oasis", 411.22, -378.93, 130.52 }
+rogue_dz.safereturn = { "oasis", 411.22, -378.93, 130.52, 0 } -- unknown
+rogue_dz.zonein = { -389.93, -853.94, 2.19, 258 }
+
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("I will scour this desert until the one with the orb arrives. Leave me to my wandering, weakling!");
@@ -12,41 +18,15 @@ function event_trade(e)
 	local qglobals = eq.get_qglobals(e.self,e.other);
 	
 	if(qglobals["Fatestealer"] == "1" and item_lib.check_turn_in(e.trade, {item1 = 9599 })) then
-		local instance_requests = require("instance_requests");
-		local lockout_globals = {{'hate_rogue_1_5', 'Plane of Hate: Innoruuks Realm'}}
-		local request = instance_requests.ValidateRequest('raid', "hateplaneb", 1, 6, 36, 61, nil, nil, e.other, lockout_globals);
-		if (request.valid and request.flags == 1) then
-			instance_requests.DisplayLockouts(e.other, e.other, lockout_globals);
-			e.other:SummonItem(9599); -- Item: Pulsating Blue Metal Sphere
-		elseif (request.valid and request.flags == 0) then
-		local raid = e.other:GetRaid();
-		local instance_id = eq.create_instance("hateplaneb", 1, 3600);
-			if (raid.valid) then
-				e.other:Message(13,"The sky crackles with electricity as ominous russet clouds gather overhead, forming a churning funnel above the tower. A feeling of pure hatred washes over you. Innoruuk's Realm is now available to you.");		
-				eq.set_global(instance_id.."_1",tostring(request.flags),7,"H1")
-				eq.assign_raid_to_instance(instance_id);
-				set_compass(raid);
-			else
-				e.other:SummonItem(9599); -- Item: Pulsating Blue Metal Sphere
-			end
-		else
+		e.other:Message(13,"The sky crackles with electricity as ominous russet clouds gather overhead, forming a churning funnel above the tower. A feeling of pure hatred washes over you.")
+		local dz = e.other:CreateExpedition(rogue_dz, rogue_info)
+		if not dz.valid then
 			e.other:SummonItem(9599); -- Item: Pulsating Blue Metal Sphere
 		end
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
 
-function set_compass(raid)
-	for i = 0, raid:RaidCount() - 1, 1 do
-		local mob_v = raid:GetMember(i);
-		if (mob_v.valid and mob_v:IsClient()) then
-			local client_v = mob_v:CastToClient();
-			if (client_v.valid) then
-				client_v:MarkSingleCompassLoc(411.22,-378.93,130.52);
-			end
-		end
-	end
-end
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
 -- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--- oasis/player.lua
+++ oasis/player.lua
@@ -1,14 +1,6 @@
 function event_click_door(e)
   local door_id = e.door:GetDoorID();
   if (door_id == 20) then
-    local instance_id = eq.get_instance_id("hateplaneb", 1);
-    if (instance_id > 0) then
-	  e.self:ClearCompassMark();
-      e.self:MovePCInstance(186, instance_id, -389.93, -853.94, 2.19, 258); -- Zone: hateplaneb
-    end
+    e.self:MovePCDynamicZone("hateplaneb")
   end
-end
-
-function event_zone(e)
-  e.self:ClearCompassMark();
 end

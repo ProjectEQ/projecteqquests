b1f3b9ebcd510c93826a94807730a7430d9af769
hg <4683435+hgtw@users.noreply.github.com>
2020-09-28 21:24:21 -0400

    Use dz for paladin 1.5 expedition from nedaria

--- nedaria/#Redis_Linrok_the_Suffering.lua
+++ nedaria/#Redis_Linrok_the_Suffering.lua
@@ -1,19 +1,15 @@
+local paladin_dz = { "dranikcatacombsa", 1, eq.seconds("2h") }
+paladin_dz.compass = { "dranik", -292.35, 1049.05, -82.54 }
+paladin_dz.safereturn = { "dranik", -291.375, 1027.125, -76.25, 0.0 }
+paladin_dz.zonein = { 0, 0, -8.5, 0 }
+
 function event_say(e)
-	-- load the current qglobals
-	local qglobals = eq.get_qglobals(e.self,e.other);
-	
-	if (qglobals["paladin_epic"] >= "6") then		
-		local instance_requests = require("instance_requests");
-		local lockouts = { { 'paladinEpicDranikCata','Dranik Catacombs A' } } 
-		local requests = instance_requests.ValidateRequest('group','dranikcatacombsa', 1, 2, 6, 65, nil, nil, e.other, lockouts);
-		if (requests.valid and requests.flags == 1) then
-		  instance_requests.DisplayLockouts(e.other, e.other, lockouts);
-		elseif (requests.valid and requests.flags == 0) then
-		  e.self:Say("Auuugh. . .Tantelus. . .the. . .catacombs. . .is where. . .my essence. . .is. . .");
-		  local instance_id = eq.create_instance('dranikcatacombsa',1, 7200);
-		  eq.assign_group_to_instance(instance_id);
-		  e.other:Message(15,"A flash of light occurs as Redis disappears. An entrance to an unfamiliar place has been opened for you. Perhaps you should investigate there.");
-		  eq.depop_with_timer();
-		end		
-	end
+  local qglobals = eq.get_qglobals(e.self, e.other)
+
+  if (qglobals["paladin_epic"] >= "6") then
+    e.self:Say(("Auuugh...%s...the...catacombs...is where...my essence...is..."):format(e.other:GetCleanName()))
+    e.other:Message(MT.Emote, "A flash of light occurs as Redis disappears. An entrance to an unfamiliar place has been opened for you. Perhaps you should investigate there.")
+    e.other:CreateExpedition(paladin_dz, { "Catacombs of Dranik", 2, 6 })
+    eq.depop_with_timer();
+  end
 end

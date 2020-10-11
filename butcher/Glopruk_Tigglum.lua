2ecaf2c97630f8aae20263bfaf9e3337b0a40803
hg <4683435+hgtw@users.noreply.github.com>
2020-10-11 16:46:09 -0400

    Disable dialogue in unimplemented ldon raids

--- butcher/Glopruk_Tigglum.lua
+++ butcher/Glopruk_Tigglum.lua
@@ -4,13 +4,14 @@ mmcf_dz.compass = { "lfaydark", 3877.23, -127.446, -53.598 }
 mmcf_dz.safereturn = { "lfaydark", 3847, -56, -50, 0 }
 mmcf_dz.zonein = { -184, 399, -12, 0 }
 
-function event_say(e)
-  if e.message:findi("hail") then
-    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Glopruk Tigglum says, 'Me hate dem vampires over in doze dark catacombs. They do have some of de shiny tings though. Me seen lots of dem come through here.  I mean shiny tings, not vampires.  My new friends have lots of work to do. If youz can rally de call of your friend perhaps youz can help us wit a serious [" .. eq.say_link("problem") .. "].'")
-  elseif e.message:findi("problem") then
-    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Glopruk Tigglum says, 'One of de rogues heard dat Valdoon Kel'Novar, a Trueborn, was on his way to de rocky summoning place to make an army of gargoyles to do de Trueborn's biddin'. If he make dat stony army, de Trueborn can rule over de rest of Mayong Mistmoore's Progeny. We can't let dat happen, see? You gonna need lots of help. Are youz [" .. eq.say_link("interested") .. "]?'")
-  elseif e.message:findi("interested") then
-    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Glopruk Tigglum says, 'Bring back de shiny tings for me!'")
-    -- e.other:CreateExpedition(mmcf_dz, mmcf_info)
-  end
-end
+-- not implemented
+-- function event_say(e)
+--   if e.message:findi("hail") then
+--     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Glopruk Tigglum says, 'Me hate dem vampires over in doze dark catacombs. They do have some of de shiny tings though. Me seen lots of dem come through here.  I mean shiny tings, not vampires.  My new friends have lots of work to do. If youz can rally de call of your friend perhaps youz can help us wit a serious [" .. eq.say_link("problem") .. "].'")
+--   elseif e.message:findi("problem") then
+--     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Glopruk Tigglum says, 'One of de rogues heard dat Valdoon Kel'Novar, a Trueborn, was on his way to de rocky summoning place to make an army of gargoyles to do de Trueborn's biddin'. If he make dat stony army, de Trueborn can rule over de rest of Mayong Mistmoore's Progeny. We can't let dat happen, see? You gonna need lots of help. Are youz [" .. eq.say_link("interested") .. "]?'")
+--   elseif e.message:findi("interested") then
+--     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Glopruk Tigglum says, 'Bring back de shiny tings for me!'")
+--     e.other:CreateExpedition(mmcf_dz, mmcf_info)
+--   end
+-- end
--- sro/Scout_Miigle.lua
+++ sro/Scout_Miigle.lua
@@ -6,13 +6,14 @@ gukc_dz.safereturn = { "innothule", 1181, 1163, -12, 0 } -- made up, live uses i
 -- gukc_dz.compass = { "innothuleb", 572.351, 934.646, 19.018 }
 -- gukc_dz.safereturn = { "innothuleb", 540, 961, 15.125, 0 }
 
-function event_say(e)
-  if e.message:findi("hail") then
-    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, ("Scout Miigle says, 'Hail, %s. We are looking for a brave crew of adventures to assist us. We must act quickly to save a prominent Guktan from the depths of Deepest Guk. We haven't heard from him since he went down there to explore the odd occurrences.  If you can rally the call of your friends perhaps you can help us with this serious [" .. eq.say_link("problem") .. "].'"):format(e.other:GetCleanName()))
-  elseif e.message:findi("problem") then
-    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Scout Miigle says, 'Very bad news! Bidip Moktu, one of the leading officials in Gukta, has been captured by a Witness of Hate! We have sent a regiment to rescue him but they have not reported back. We need you to assist us in rescuing Bidip Moktu and putting a stop to whatever unnatural magic is occurring in Deepest Guk. Please say you'll return our people safely.  Are you [" .. eq.say_link("interested") .. "]?'")
-  elseif e.message:findi("interested") then
-    eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Scout Miigle says, 'I hope to so Bidip Moktu again soon, and in one piece.'")
-    -- e.other:CreateExpedition(gukc_dz, gukc_info)
-  end
-end
+-- not implemented
+-- function event_say(e)
+--   if e.message:findi("hail") then
+--     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, ("Scout Miigle says, 'Hail, %s. We are looking for a brave crew of adventures to assist us. We must act quickly to save a prominent Guktan from the depths of Deepest Guk. We haven't heard from him since he went down there to explore the odd occurrences.  If you can rally the call of your friends perhaps you can help us with this serious [" .. eq.say_link("problem") .. "].'"):format(e.other:GetCleanName()))
+--   elseif e.message:findi("problem") then
+--     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Scout Miigle says, 'Very bad news! Bidip Moktu, one of the leading officials in Gukta, has been captured by a Witness of Hate! We have sent a regiment to rescue him but they have not reported back. We need you to assist us in rescuing Bidip Moktu and putting a stop to whatever unnatural magic is occurring in Deepest Guk. Please say you'll return our people safely.  Are you [" .. eq.say_link("interested") .. "]?'")
+--   elseif e.message:findi("interested") then
+--     eq.get_entity_list():MessageClose(e.self, true, 100, MT.SayEcho, "Scout Miigle says, 'I hope to so Bidip Moktu again soon, and in one piece.'")
+--     e.other:CreateExpedition(gukc_dz, gukc_info)
+--   end
+-- end

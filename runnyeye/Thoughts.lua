e9a99c020e51236c3f5d93f69d4d4de6cc8d3f18
huffin <acg1089@aol.com>
2020-10-20 11:42:01 -0500

    Update Thoughts.lua

--- runnyeye/Thoughts.lua
+++ runnyeye/Thoughts.lua
@@ -1,9 +1,6 @@
-function event_spawn(e)
-	local xloc = e.self:GetX();
-	local yloc = e.self:GetY();
-	eq.set_proximity(xloc - 50, xloc + 50, yloc - 50, yloc + 50);
+function event_combat(e)
+if (e.joined == true) then
+e.self:Emote("of great treasures and fame spur you on.  Thoughts of certain and painful death do not deter you.");
+eq.depop_with_timer();
 end
-
-function event_enter(e)
-	e.self:Emote("of great treasures and fame spur you on.  Thoughts of certain and painful death do not deter you.");
 end

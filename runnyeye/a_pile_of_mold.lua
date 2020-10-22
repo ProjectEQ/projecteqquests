718d229c293ed41222e1a84c128e46ec9b56d97a
huffin <acg1089@aol.com>
2020-10-22 12:04:41 -0500

    Update a_pile_of_mold.lua

--- runnyeye/a_pile_of_mold.lua
+++ runnyeye/a_pile_of_mold.lua
@@ -1,6 +1,7 @@
 function event_combat(e)
 if (e.joined == true) then
 e.self:Emote("erupts in a cloud of spores!");
+eq.spawn2(11164,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 eq.depop_with_timer();
 end
 end

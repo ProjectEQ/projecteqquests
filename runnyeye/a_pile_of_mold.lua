2553db38dd2c87d58ddf99726be106f9551df262
huffin <acg1089@aol.com>
2020-10-23 16:06:53 -0500

    Update a_pile_of_mold.lua

--- runnyeye/a_pile_of_mold.lua
+++ runnyeye/a_pile_of_mold.lua
@@ -1,7 +1,11 @@
 function event_combat(e)
-if (e.joined == true) then
-e.self:Emote("erupts in a cloud of spores!");
-eq.spawn2(11164,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
-eq.depop_with_timer();
-end
+  if (e.joined == true) then
+    e.self:Emote("erupts in a cloud of spores!");
+    
+    local spawns = math.random(1,5);
+	  for n = 1, spawns do
+		mob = eq.spawn2(11164,0,0,e.self:GetX() + math.random (-5,5), e.self:GetY() + math.random(-5,5), e.self:GetZ(), e.self:GetHeading());	-- a_sporeling (11164)
+      
+    eq.depop_with_timer();
+  end
 end

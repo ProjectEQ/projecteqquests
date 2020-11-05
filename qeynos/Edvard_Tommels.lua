5c05b77d6fce3c15b8db892413f63c626370bcf9
huffin <acg1089@aol.com>
2020-11-05 08:44:10 -0600

    Update Edvard_Tommels.lua

--- qeynos/Edvard_Tommels.lua
+++ qeynos/Edvard_Tommels.lua
@@ -1,6 +1,7 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say(string.format("Greetings, %s. Thank you for stopping by. Had you heard I was looking for any and all information regarding the Tesch Val incursion? Perhaps you have come to help me gather knowledge? I seek the [Tesch Val scrolls]!",e.other:GetName()));
+		e.self:DoAnim(70); -- bowing		
 	elseif(e.message:findi("scrolls")) then
 		e.self:Say("Oh good! I can add them to my collection. We are preparing to make books you know. Bring me any of the scrolls you find in the old Splitpaw lair, and I would be happy to reward you handsomely.");
 	end
@@ -8,27 +9,44 @@ end
 
 function event_trade(e)
 	local item_lib =require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 18504,item2 = 18505,item3 = 18506,item4 = 18507})) then
-		local pp = math.random(6);
-		local gp = math.random(20);
-		local sp = math.random(60);
-		local cp = math.random(100);
-		e.self:Say(string.format("Why thank you, %s. If you do happen to come across any more of these, please bring them to me.",e.other:GetName()));
-		e.other:SummonItem(12076); -- Item: Karana's Tear
-		e.other:Ding();
-		e.other:Faction(121,30,0); -- Faction: Qeynos Citizens
-		e.other:AddEXP(100);
-		e.other:GiveCash(cp,sp,gp,pp);
-	elseif(item_lib.check_turn_in(e.trade, {item1 = (18504 or 18505 or 18506 or 18507)})) then
-		local pp = math.random(2);
-		local gp = math.random(6);
-		local sp = math.random(15);
-		local cp = math.random(25);
-		e.self:Say(string.format("Why thank you, %s. If you do happen to come across any more of these, please bring them to me.",e.other:GetName()));
-		e.other:Ding();
-		e.other:Faction(121,5,0); -- Faction: Qeynos Citizens
-		e.other:AddEXP(100);
-		e.other:GiveCash(cp,sp,gp,pp);
+	
+	-- Verified Live faction and experience.  questsay and item reward seems to be random with each item turn in.
+	local scroll = 0;
+	local Volone = item_lib.count_handed_item(e.trade, {18504});
+	local Voltwo = item_lib.count_handed_item(e.trade, {18505});
+	local Volthree = item_lib.count_handed_item(e.trade, {18506});
+	local Volfour = item_lib.count_handed_item(e.trade, {18507});
+	
+	if(Volone == 1) then
+		scroll = scroll + 1;
+		Volone = 0;
 	end
+	if(Voltwo == 1) then
+		scroll = scroll + 1;
+		Voltwo = 0;
+	end
+	if(Volthree == 1) then
+		scroll = scroll + 1;
+		Volthree = 0;
+	end
+	if(Volfour == 1) then
+		scroll = scroll + 1;
+		Volfour = 0;
+	end
+	
+	if(scroll > 0) then
+		repeat
+			if(math.random(100) < 50) then
+				e.self:Say("Why thank you, " .. e.other:GetCleanName() .. ". If you do happen to come across any more of these, please bring them to me.");
+			end
+			if(math.random(100) < 50 and not e.other:HasItem(12076)) then
+				e.other:QuestReward(e.self,{itemid = 12076});
+			end
+			e.other:Faction(e.self,121,2,0); -- Faction: Qeynos Citizens
+			e.other:QuestReward(e.self,math.random(0,15),math.random(0,8),math.random(0,8),math.random(0,2),0,5000);
+			scroll = scroll - 1;
+		until scroll == 0;
+	end
+
 	item_lib.return_items(e.self, e.other, e.trade)
 end

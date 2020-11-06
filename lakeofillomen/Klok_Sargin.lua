0b878d5e5340813c9d8044238c3402006fb3fabb
huffin <acg1089@aol.com>
2020-11-06 13:14:26 -0600

    Update Klok_Sargin.lua

--- lakeofillomen/Klok_Sargin.lua
+++ lakeofillomen/Klok_Sargin.lua
@@ -13,7 +13,7 @@ function event_trade(e)
 	if(e.other:GetFaction(e.self) <= 5 and item_lib.check_turn_in(e.trade, {item1 = 12761})) then
 		e.self:Say("Uhh. Thanks. I sort of told the lizard who bought the skull that you were asking for him, and, well, he was kinda mad, and... " .. e.other:GetCleanName() .. ", meet Bruiser.");
 		eq.unique_spawn(85226,0,0,e.self:GetX() + 5,e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- Bruiser Noz
-		eq.set_timer("emote", 60000);
+		eq.set_timer("emote", 10000);
 	end
 	item_lib.return_items(e.self, e.other, e.trade) --return items if not the ones required
 end

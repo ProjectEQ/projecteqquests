54421d92c18b90ef340d01a7a985cf281d04fc77
huffin <acg1089@aol.com>
2020-11-14 19:06:40 -0600

    Update Legionnaire_Claudius.lua

--- katta/Legionnaire_Claudius.lua
+++ katta/Legionnaire_Claudius.lua
@@ -9,15 +9,15 @@ function event_trade(e)
 	
 	if(item_lib.check_turn_in(e.trade, {item1 = 29845}, 0)) then -- Field Orders
 		e.self:Say("Huh? What? Oh, umm, let's see here... OK I best get these taken care of right away. Thank you.");
-		e.other:Faction(e.self,1503,1);  -- +Validus Custodus 
-		e.other:Faction(e.self,1502,1);  -- +Katta Castellum Citizens
-		e.other:Faction(e.self,1504,1);  -- +Magus Conlegium
-		e.other:Faction(e.self,1505,-1); -- -Nathyn Illuminious
-		e.other:Faction(e.self,1506,-1);  -- -Coterie of the Eternal Night
-		e.other:Faction(e.self,1483,-1); -- -Seru
-		e.other:Faction(e.self,1484,-1); -- -Hand of Seru
-		e.other:Faction(e.self,1485,-1);  -- -Eye of Seru
-		e.other:Faction(e.self,1541,-1); -- -Hand Legionnaries
+		e.other:Faction(1503,1);  -- +Validus Custodus 
+		e.other:Faction(1502,1);  -- +Katta Castellum Citizens
+		e.other:Faction(1504,1);  -- +Magus Conlegium
+		e.other:Faction(1505,-1); -- -Nathyn Illuminious
+		e.other:Faction(1506,-1);  -- -Coterie of the Eternal Night
+		e.other:Faction(1483,-1); -- -Seru
+		e.other:Faction(1484,-1); -- -Hand of Seru
+		e.other:Faction(1485,-1);  -- -Eye of Seru
+		e.other:Faction(1541,-1); -- -Hand Legionnaries
 		e.other:QuestReward(e.self,0,0,0,0,0,1000);
 		--eq.spawn2(160136,30,0,-2107,99,33,0);
 		eq.depop_with_timer();

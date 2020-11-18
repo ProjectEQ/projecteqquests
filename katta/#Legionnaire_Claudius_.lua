729dec94ecd1da13e0683e314c2ca7e8b743043f
huffin <acg1089@aol.com>
2020-11-18 13:23:54 -0600

    Update #Legionnaire_Claudius_.lua

--- katta/#Legionnaire_Claudius_.lua
+++ katta/#Legionnaire_Claudius_.lua
@@ -8,11 +8,11 @@ function event_timer(e)
 	if(e.timer == "attack") then
 		count = count + 1;
 		if(count == 1) then
-			eq.spawn2(160456,0,0,-1191,-6,-266,0); --#Legionnaire (160456)
+			eq.spawn2(160456,31,0,-1191,-6,-266,0); --#Legionnaire (160456)
 		elseif(count == 2) then
-			eq.spawn2(160456,0,0,-1191,-6,-266,0); --#Legionnaire (160456)
-			eq.spawn2(160456,0,0,-1191,-6,-266,0); --#Legionnaire (160456)
-			eq.spawn2(160456,0,0,-1005,54,-266,0); --#Legionnaire (160456)
+			eq.spawn2(160456,31,0,-1191,-6,-266,0); --#Legionnaire (160456)
+			eq.spawn2(160456,31,0,-1191,-6,-266,0); --#Legionnaire (160456)
+			eq.spawn2(160456,31,0,-1005,54,-266,0); --#Legionnaire (160456)
 		elseif(count == 3) then
 			e.self:Say("I have been discovered! You should have left well enough alone!!!")
 			eq.stop_timer("attack");

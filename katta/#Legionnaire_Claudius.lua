f50eddcdc76c13380f96f2c69d264aea82e594f0
huffin <acg1089@aol.com>
2020-11-18 13:30:56 -0600

    Update #Legionnaire_Claudius.lua

--- katta/#Legionnaire_Claudius.lua
+++ katta/#Legionnaire_Claudius.lua
@@ -12,17 +12,17 @@ end
 
 function event_waypoint_arrive(e)
 	local vahn = eq.get_entity_list():GetMobByNpcTypeID(160469);
-	if(e.wp == 43) then
+	if(e.wp == 46) then
 		eq.unique_spawn(160469,0,0,-1149,67,-265,100); --#Vahn (160469)
-	elseif(e.wp == 44) then
+	elseif(e.wp == 47) then
 		if(vahn.valid) then
 			vahn:Emote("speaks with Legionnaire Claudius in hushed tones. Their whispers cannot be heard, but there is certainly something suspicious in their demeanor.");
 		end
-	elseif(e.wp == 45) then
+	elseif(e.wp == 48) then
 		if(vahn.valid) then
 			vahn:Emote("passes something to Claudius and gets a rolled up parchment in return. Their business appears to be completed and both parties prepare to go about their business.");
 		end
-	elseif(e.wp == 46) then
+	elseif(e.wp == 49) then
 		if(vahn.valid) then
 			vahn:Emote("glances in either direction and nods at Claudius before disappearing back into the shadows.");
 			e.self:Emote("looks to Vahn tentatively, 'We shall meet again soon.'");

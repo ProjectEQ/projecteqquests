ace4d939d92fdc44198fd9fc5edc947d96171581
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-12-10 14:54:06 -0600

    Update #Disciple_of_Earth.lua

--- thundercrest/#Disciple_of_Earth.lua
+++ thundercrest/#Disciple_of_Earth.lua
@@ -19,6 +19,7 @@ function event_signal(e)
 	if (e.signal == 1) then
 		eq.set_timer("chat", 45000);
 		e.self:AddGrid(16);
+		eq.start(5);
 	elseif (e.signal == 2) then
 		e.self:SetSpecialAbility(19, 0);
 		e.self:SetSpecialAbility(20, 0);

32677160ad80056d740b8f21b7f7a4f555b92d23
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-12-26 14:00:52 -0600

    Update player.lua

--- hateplaneb/player.lua
+++ hateplaneb/player.lua
@@ -6,8 +6,12 @@ function event_enter_zone(e)
 		eq.set_timer("s2",10000);
 		eq.set_timer("s3",15000);
 		eq.set_global("hpb_zone_emote","1",3,"1H");
+	elseif qglobals["pre_pal"] == "2" then
+			 e.self:Message(15,"Your sword piece reverberates with a low hum. An attraction is felt within it. To the center of this plane you are drawn, towards the direction of that attraction.");
+			 e.self:MarkSingleCompassLoc(-130.00,-480.00,31.73);
 	end  
 end
+
 
 
 function event_loot(e)

4110f2ba7f2045ef2ce541dc34b3f4505dffa21f
Drogerin <40513146+Drogerin@users.noreply.github.com>
2020-12-26 15:44:54 -0600

    Update Rombus.lua

--- everfrost/Rombus.lua
+++ everfrost/Rombus.lua
@@ -8,11 +8,11 @@ function event_say(e)
 	if qglobals["pre_pal"] == "2" then
 		if (e.message:findi("hail")) and e.other:HasItem(69903) and e.other:HasItem(69904) and e.other:HasItem(69905) then
 			e.self:QuestSay(e.other, "Hail to you," .. e.other:GetName() .. ". Odd, this sword piece of mine is humming and I feel it tugging towards you. Do you happen to know why? Maybe you have [" .. eq.say_link("other pieces of this broken sword") .. "] ?");
-		elseif (e.message:findi("other pieces of this broken sword") then
+		elseif (e.message:findi("other pieces of this broken sword")) then
 			e.self:QuestSay(e.other, "Aha! That must explain it. I knew this sword must have magical powers. . .I have felt its strength. You being here proved me that. [" .. eq.say_link("Would you like the piece") .. "] of the sword that I own?");
-		elseif (e.message:findi("Would you like the piece") then
+		elseif (e.message:findi("Would you like the piece")) then
 			e.self:QuestSay(e.other, "Well, I won't be giving this to you for free. This surely must be a valuable sword. It is no doubt an extraordinary blade. I'll tell you what, I've been trying to create this new teleportation spell for a while now. Unfortunately, I have tried to create the spell multiple times but I keep failing it. I think I have very bad luck. Go acquire the [" .. eq.say_link("four runes") .. "] for me and return to me with them. Hopefully I can borrow some of your luck to create this spell.");
-		elseif (e.message:findi("four runes") then
+		elseif (e.message:findi("four runes")) then
 			e.self:QuestSay(e.other, "Yes, the four runes were scattered by the winds to four different areas: Plane of Tranquility, Mistmoore Castle, Scarlet Desert, and the Gulf of Gunthak. Please find all four of them and bring them to me.");
 		else
 			e.self:Say("How to get this spell to work!");

0d56285ae22c8ebd6c7119f7da53320236f3fbfa
huffin <acg1089@aol.com>
2020-12-29 20:01:04 -0600

    Update #Mabiki.lua

--- sncrematory/#Mabiki.lua
+++ sncrematory/#Mabiki.lua
@@ -1,4 +1,4 @@
 function event_death_complete(e)
 eq.spawn2(288086,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --#A_Ghostly_Essence
-e.self:Emote("s corpse's spirit dissipates in a din of chaotic magic.  Dark clouds encircle you, engulfing you in a smothering embrace.  You are momentarily blinded.  After you come to your senses, you see that a ghostly essence has formed and is moving towards a room to the east.);
+e.self:Emote("s corpse's spirit dissipates in a din of chaotic magic.  Dark clouds encircle you, engulfing you in a smothering embrace.  You are momentarily blinded.  After you come to your senses, you see that a ghostly essence has formed and is moving towards a room to the east.");
 end

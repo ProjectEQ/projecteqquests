1b6718bc60a4879ec9b2d924233b9c2d440ce95e
huffin <acg1089@aol.com>
2020-12-30 10:51:19 -0600

    Update and rename Packmaster_Skoiat_Pizak.pl to Packmaster_Skoiat_Pizak.lua

---
+++ ferubi/Packmaster_Skoiat_Pizak.lua
@@ -0,0 +1,34 @@
+-- NPC(s): #Rav_Pizak (284098), #Rav_Pizak (284107)
+guard_list = { 284098, 284107 };
+
+function event_combat(e)
+	-- during combat, reaggro the guards on current target every 30 seconds (ensures no idle guards)
+	if e.joined then
+		aggro_guards(e.self:GetHateTop());
+		eq.set_timer('aggro_guards', 30 * 1000);
+		eq.set_timer('spawnadds', 45 * 1000);
+	else
+		eq.stop_timer('aggro_guards');
+		eq.stop_timer('spawnadds');
+	end
+end
+
+function event_timer(e)
+	if e.timer == 'aggro_guards' then
+		aggro_guards(e.self:GetHateTop());
+	elseif e.timer == 'spawnadds' then
+		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-170,-884,3,114);
+		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-167,-792,3,136);
+		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-170,-884,3,114);
+		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-167,-792,3,136);
+	end
+end
+
+function aggro_guards(mob)
+	for _, guard in pairs(guard_list) do
+		local guard_mob = eq.get_entity_list():GetNPCByNPCTypeID(guard);
+		if guard_mob ~= nil then
+			guard_mob:AddToHateList(mob);
+		end
+	end
+end

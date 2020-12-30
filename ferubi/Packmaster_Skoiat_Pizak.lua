d9a725daa189316dd623ac58cc7c8b7fcc727e81
huffin <acg1089@aol.com>
2020-12-30 11:31:24 -0600

    Update Packmaster_Skoiat_Pizak.lua

--- ferubi/Packmaster_Skoiat_Pizak.lua
+++ ferubi/Packmaster_Skoiat_Pizak.lua
@@ -7,9 +7,11 @@ function event_combat(e)
 		aggro_guards(e.self:GetHateTop());
 		eq.set_timer('aggro_guards', 30 * 1000);
 		eq.set_timer('spawnadds', 45 * 1000);
+		eq.stop_timer('depopadds');
 	else
 		eq.stop_timer('aggro_guards');
 		eq.stop_timer('spawnadds');
+		eq.set_timer('depopadds', 60 * 1000);
 	end
 end
 
@@ -21,6 +23,10 @@ function event_timer(e)
 		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-167,-792,3,136):AddToHateList(e.self:GetHateRandom(),1);
 		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-170,-884,3,114):AddToHateList(e.self:GetHateRandom(),1);
 		eq.spawn2(eq.ChooseRandom(284098,284107),0,0,-167,-792,3,136):AddToHateList(e.self:GetHateRandom(),1);
+	elseif e.timer == 'depopadds' then
+		eq.stop_timer('depopadds');
+		eq.depop_all(284098);
+		eq.depop_all(284107);
 	end
 end
 

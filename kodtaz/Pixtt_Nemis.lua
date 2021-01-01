65bedf872fa69717d3f9e8fa458aa929308f94ad
huffin <acg1089@aol.com>
2021-01-01 13:37:10 -0600

    Update Pixtt_Nemis.lua

--- kodtaz/Pixtt_Nemis.lua
+++ kodtaz/Pixtt_Nemis.lua
@@ -2,6 +2,6 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("Did she send you? Hmmph, you obviously don't know what I'm talking about. Get out of my face!");
-		MovePC(293,-966, 153, -423, 0)
+		e.other:MovePC(293,-966, 153, -423, 0);
 	end
 end

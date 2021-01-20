fcdd4ef3d57ef0bfaf8d73920086d7151c75c162
huffin <acg1089@aol.com>
2021-01-20 10:15:23 -0600

    Update Guard_Change_Shouter.lua

--- hohonora/Guard_Change_Shouter.lua
+++ hohonora/Guard_Change_Shouter.lua
@@ -4,9 +4,10 @@ end
 
 function event_timer(e)
 	if ( e.timer == "timecheck" ) then
-		zoneTime = eq.get_zone_time();
-		if ( zoneTime == 7 or zoneTime == 19 ) then
-				
+		local zone_time = eq.get_zone_time();
+
+    		if(zone_time["zone_hour"] == 7 or zone_time["zone_hour"] == 19) then
+	
 			eq.zone_emote(0, "You hear a chime in the distance, and a voice booms in your ears, 'It is now the time for the changing of the guard!'");
 			eq.stop_timer("timecheck");
 			eq.set_timer("pause", 5 * 60 * 1000);

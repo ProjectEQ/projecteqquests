e679bb03df43382169e3e2b1f5cb296d66489f2f
huffin <acg1089@aol.com>
2021-01-21 11:33:34 -0600

    Update Guard_Change_Shouter.lua

--- hohonora/Guard_Change_Shouter.lua
+++ hohonora/Guard_Change_Shouter.lua
@@ -11,6 +11,10 @@ function event_timer(e)
 			eq.zone_emote(0, "You hear a chime in the distance, and a voice booms in your ears, 'It is now the time for the changing of the guard!'");
 			eq.stop_timer("timecheck");
 			eq.set_timer("pause", 5 * 60 * 1000);
+			eq.spawn2(eq.ChooseRandom(211112,211002),33,0,109,17,3,126);
+			eq.spawn2(eq.ChooseRandom(211112,211002),34,0,109,17,3,126);
+			eq.spawn2(eq.ChooseRandom(211113,211114),35,0,109,17,3,126);
+			eq.spawn2(eq.ChooseRandom(211113,211114),36,0,109,17,3,126);
 		end
 	elseif ( e.timer == "pause" ) then
 			eq.stop_timer("pause");

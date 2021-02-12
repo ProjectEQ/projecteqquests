9ba3b8968890f49400dbad0bc4908a9ad8f05f69
JJ <jeremiah.oligario@gmail.com>
2021-02-11 22:34:53 -0500

    Revert 41799c4 and f0fa3bd for now as items are not returning correctly

--- plugins/check_handin.pl
+++ plugins/check_handin.pl
@@ -1,67 +1,65 @@
 # plugin::check_handin($item1 => #required_amount,...);
-# Automatically returns unused items on success
+# autoreturns extra unused items on success
 sub check_handin {
-	my $itemcount = shift;
-	my %required_items = @_;
-	foreach $item_id (keys %required_items) {
-		if (!defined $itemcount->{$item_id} || $itemcount->{$item_id} != $required_items{$item_id}) {
-			return 0;
-		}
-		
-		if ($required_items{$item_id} < $itemcount->{$item_id}) {
-			$itemcount->{$item_id} -= $required_items{$item_id};
-		} else {
-			delete $itemcount->{$item_id};
-		}
+    my $hashref = shift;
+    my %required = @_;
+    foreach my $req (keys %required) {
+	if ((!defined $hashref->{$req}) || ($hashref->{$req} != $required{$req})) {
+            return(0);
+	}
     }
-	return 1;
+     foreach my $req (keys %required) {
+         if ($required{$req} < $hashref->{$req}) {
+             $hashref->{$req} -= $required{$req};
+         } else {
+             delete $hashref->{$req};
+         }
+     }
+     return 1;
 }
 
-# plugin::return_items();
-# Returns unused items in your itemcount hash
 sub return_items {    
-	my $itemcount = plugin::var('itemcount');
-	my $client = plugin::val('client');
-	my $name = plugin::val('name');
+	my $hashref = plugin::var('$itemcount');
+	my $client = plugin::val('$client');
+	my $name = plugin::val('$name');
 	my $items_returned = 0;
-	my %items_hash = (
-		0 => [ plugin::val('item1'), plugin::val('item1_charges'), plugin::val('item1_attuned') ],
-		1 => [ plugin::val('item2'), plugin::val('item2_charges'), plugin::val('item2_attuned') ],
-		2 => [ plugin::val('item3'), plugin::val('item3_charges'), plugin::val('item3_attuned') ],
-		3 => [ plugin::val('item4'), plugin::val('item4_charges'), plugin::val('item4_attuned') ],
+
+	my %ItemHash = (
+		0 => [ plugin::val('$item1'), plugin::val('$item1_charges'), plugin::val('$item1_attuned') ],
+		1 => [ plugin::val('$item2'), plugin::val('$item2_charges'), plugin::val('$item2_attuned') ],
+		2 => [ plugin::val('$item3'), plugin::val('$item3_charges'), plugin::val('$item3_attuned') ],
+		3 => [ plugin::val('$item4'), plugin::val('$item4_charges'), plugin::val('$item4_attuned') ],
 	);
 	
-	foreach $handin_item (keys %{$itemcount}) {
-		next if ($handin_item == 0);
-		$required_count = $itemcount->{$handin_item};
-		foreach $required_slot (0..3) {
-			if (
-				$required_count > 0 &&
-				$items_hash{$required_slot}[0] &&
-				$items_hash{$required_slot}[0] == $handin_item
-			) {
-				if ($client) {
-					$client->SummonItem(
-						$handin_item,
-						$items_hash{$required_slot}[1],
-						$items_hash{$required_slot}[2]
-					);
-					$items_returned = 1;
-				} else {
-					quest::summonitem($handin_item);
+	foreach my $k (keys(%{$hashref}))
+	{
+		next if($k == 0);
+		my $rcount = $hashref->{$k};
+		my $r;
+		for ($r = 0; $r < 4; $r++)
+		{
+			if ($rcount > 0 && $ItemHash{$r}[0] && $ItemHash{$r}[0] == $k)
+			{
+				if ($client)
+				{
+					$client->SummonItem($k, $ItemHash{$r}[1], $ItemHash{$r}[2]);
+					quest::say("I have no need for this $name, you can have it back.");
 					$items_returned = 1;
 				}
-				$required_count--;
+				else
+				{
+					# This shouldn't be needed, but just in case
+					quest::summonitem($k, 0);
+					$items_returned = 1;
+				}
+				$rcount--;
 			}
-			$required_slot++;
 		}
-		delete $itemcount->{$handin_item};
+		delete $hashref->{$k};
 	}
-	
-	if ($items_returned) {
-		quest::say("I have no need for this $name, you can have it back.");
-	}
+	# Return true if items were returned
 	return $items_returned;
+
 }
 
 sub mq_process_items {

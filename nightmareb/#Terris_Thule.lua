601ed6a9bd6391f728ae5c17d1fd0e5b9dff25c0
huffin <acg1089@aol.com>
2021-02-24 10:00:17 -0600

    Update #Terris_Thule.lua

--- nightmareb/#Terris_Thule.lua
+++ nightmareb/#Terris_Thule.lua
@@ -4,6 +4,7 @@ local DEFILER_LARGE_TYPE = 221012;
         
  function event_spawn(e)
 	eq.set_next_hp_event(95);
+	eq.set_timer("gargs", 1000); --pop untargettable gargs
 end
 
 function event_combat(e)
@@ -58,20 +59,26 @@ function event_hp(e)
 	elseif ( e.hp_event == 40 ) then
 		e.self:Shout("You will not escape my realm so easily!");
 		eq.get_entity_list():MessageClose(e.self, true, 250, 0, "The air grows thick with the smell of burning mana.  A rumbling sound draws your attention to the massive statues that rest above the ancient monoliths.  The statues begin to crumble, as they shift their attention from the heavens to you!");
-    eq.spawn2(221007,0,0,-1993,-102,134,478); --a grotesque statue
-    eq.spawn2(221007,0,0,-1798,-102,134,402); --a grotesque statue
-   eq.spawn2(221007,0,0,-1993,78,134,318); --a grotesque statue
-    eq.spawn2(221007,0,0,-1798,78,134,382); --a grotesque statue
+		eq.spawn2(221007,0,0,-1993,-102,134,478); --a grotesque statue
+		eq.spawn2(221007,0,0,-1798,-102,134,402); --a grotesque statue
+		eq.spawn2(221007,0,0,-1993,78,134,318); --a grotesque statue
+		eq.spawn2(221007,0,0,-1798,78,134,382); --a grotesque statue
 	end
  end
         
  function event_timer(e)
-          if ( e.timer == "oobcheck" ) then
-            if ( e.self:GetX() > -1580 or e.self:GetX() < -2090 or e.self:GetY() > 250 or e.self:GetY() < -280 ) then
+	if ( e.timer == "oobcheck" ) then
+            	if ( e.self:GetX() > -1580 or e.self:GetX() < -2090 or e.self:GetY() > 250 or e.self:GetY() < -280 ) then
 			eq.get_entity_list():MessageClose(e.self, true, 200, 0, "disappears into the ether and reforms at the center of her chamber, cleansed of your magic!");
 			e.self:GMMove(e.self:GetSpawnPointX(), e.self:GetSpawnPointY(), e.self:GetSpawnPointZ(), e.self:GetSpawnPointH());
 			e.self:CastSpell(3230, e.self:GetID()); -- Balance of the Nameless
 		end
+	elseif ( e.timer == "gargs" ) then
+		eq.stop_timer("gargs");
+		eq.spawn2(221013,0,0,-1954,99,202,191); --untargettable statue
+		eq.spawn2(221013,0,0,-1748,91,202,330); --untargettable statue
+		eq.spawn2(221013,0,0,-1736,-125,202,454); --untargettable statue
+		eq.spawn2(221013,0,0,-1958,-104,202,67); --untargettable statue
 	end
 end
         

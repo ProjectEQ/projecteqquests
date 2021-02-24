4484bf8120ca93b983d8847eaba751c1985a6413
huffin <acg1089@aol.com>
2021-02-24 17:21:36 -0600

    Update #Aerin-Dar.lua

--- povalor/#Aerin-Dar.lua
+++ povalor/#Aerin-Dar.lua
@@ -1,24 +1,33 @@
 
 function event_spawn(e)
-  mob:SetFlyMode(1);
+	mob:SetFlyMode(1);
 	eq.set_next_hp_event(80);
-  eq.set_timer("respawn", 2 * 1000);
+	eq.set_timer("respawn", 2 * 1000);
 end
 
 function event_hp(e)
 	if e.hp_event == 80 then
-    eq.signal(208172,1);
+		eq.signal(208172,1);
+		eq.spawn2(208170,0,0,485,2744,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
+		eq.spawn2(208170,0,0,234,2744,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
 		eq.set_next_hp_event(60);
 		eq.set_next_inc_hp_event(98); --to reset on event failure
 	elseif e.hp_event == 60 then
-    eq.signal(208169,1);
+		eq.signal(208169,1);
+		eq.spawn2(208170,0,0,212,2668,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
+		eq.spawn2(208170,0,0,511,2668,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
 		eq.set_next_hp_event(40);
 	elseif e.hp_event == 40 then
-    eq.signal(208174,1);
+		eq.signal(208174,1);
+		eq.spawn2(208170,0,0,481,2581,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
+		eq.spawn2(208170,0,0,239,2581,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
 		eq.set_next_hp_event(20);
 	elseif e.hp_event == 20 then
-    eq.signal(208171,1);
-    eq.signal(208022,1);
+		eq.signal(208171,1);
+		eq.signal(208022,1);
+		eq.spawn2(208170,0,0,291,2518,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
+		eq.spawn2(208170,0,0,427,2518,39,257):AddToHateList(e.self:GetHateTop(),1); --A_Mindless_Minion
+		eq.spawn2(208176,0,0,359,2500,39,257):AddToHateList(e.self:GetHateTop(),1); --rahlgon
 	end
 	
 	if e.inc_hp_event == 98 then

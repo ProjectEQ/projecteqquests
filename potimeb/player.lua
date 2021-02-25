8c85896eeb1ada577541943a4b0aab2f0db98b87
Trust <fryguy503@gmail.com>
2021-02-24 19:22:53 -0500

    [DZ] PoTime Remove non-peq functions

--- potimeb/player.lua
+++ potimeb/player.lua
@@ -71,19 +71,9 @@ local phaselockouts = {
 
 
 function event_enter_zone(e)
-	-- load the current qglobals
-	local qglobals = eq.get_qglobals(e.self);
-	local discs = require('disciplines');
-	discs:update_discs(e, e.self:GetLevel());
-	
 	if(instance_id ~= 0) then
         e.self:Message(15,"You have entered an Instanced Version of the zone.");
     end
-end
-
-function event_level_up(e)
-	local discs = require('disciplines');
-	discs:train_discs(e, e.self:GetLevel());
 end
 
 function event_click_door(e)

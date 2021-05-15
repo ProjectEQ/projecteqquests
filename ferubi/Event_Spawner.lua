109249375c5a05cf509b25b8a27cd77b453c2409
huffin <acg1089@aol.com>
2021-05-15 17:43:41 -0500

    Update Event_Spawner.lua

--- ferubi/Event_Spawner.lua
+++ ferubi/Event_Spawner.lua
@@ -15,12 +15,12 @@ function event_signal(e)
     blademaster_counter = blademaster_counter + 1;
 	end
 	if (blademaster_counter == 100) then
-     eq.unique_spawn(284039,0,0,443,428,102.5,0); -- Pxet_Elite_Blademaster (284039)
+     	eq.unique_spawn(284039,0,0,443,428,102.5,0); -- Pxet_Elite_Blademaster (284039)
+	end
   if (e.signal == 2) then
     spearmaster_counter = spearmaster_counter + 1;
 	end
 	if (spearmaster_counter == 100) then
      eq.unique_spawn(284091,0,0,-771,354,98,240); -- Pxet_Elite_Spearmaster (284091)
-
-  end
+ 	 end
 end

7292b27f70bae284608838d3f1b6ffdcfd5c240b
huffin <acg1089@aol.com>
2021-06-14 09:49:22 -0500

    Create a_Wayfarers_Brotherhood_supplier.lua

---
+++ nedaria/a_Wayfarers_Brotherhood_supplier.lua
@@ -0,0 +1,5 @@
+function event_say(e)
+	if(e.message:findi("Hail")) then
+		e.self:Say("I will continue to gather supplies for the Abysmal Sea.  There's a lot of food and other supplies needed there to keep everyone fed and warm.");	
+	end
+end

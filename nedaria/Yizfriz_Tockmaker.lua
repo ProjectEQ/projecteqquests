2c0f13b412ad5ba2a70a2ace6bb3d886c1859471
huffin <acg1089@aol.com>
2021-06-14 09:40:35 -0500

    Create Yizfriz_Tockmaker.lua

---
+++ nedaria/Yizfriz_Tockmaker.lua
@@ -0,0 +1,5 @@
+function event_say(e)
+	if(e.message:findi("Hail")) then
+		e.self:Say("I've been workin' with Ippledop for years buildin' all kinds of machines. We've created all kinds of waterwheels and steam propulsion systems, so far. We're goin' to hang about and keep creatin' stuff in case more machines are needed to get the ship operatin'.");	
+	end
+end

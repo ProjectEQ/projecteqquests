286627c4c7aa7f2b02905a8cab8c5a9fadc1979e
huffin <acg1089@aol.com>
2021-06-14 09:16:29 -0500

    Update #Redis_Linrok_the_Suffering.lua

--- nedaria/#Redis_Linrok_the_Suffering.lua
+++ nedaria/#Redis_Linrok_the_Suffering.lua
@@ -9,7 +9,7 @@ local paladin_dz = {
 function event_say(e)
   local qglobals = eq.get_qglobals(e.self, e.other)
 
-  if (qglobals["paladin_epic"] >= "6") then
+  if (qglobals["paladin_epic"] ~= nil and qglobals["paladin_epic"] >= "6") then
     e.self:Say(("Auuugh...%s...the...catacombs...is where...my essence...is..."):format(e.other:GetCleanName()))
     e.other:Message(MT.Emote, "A flash of light occurs as Redis disappears. An entrance to an unfamiliar place has been opened for you. Perhaps you should investigate there.")
     e.other:CreateExpedition(paladin_dz)

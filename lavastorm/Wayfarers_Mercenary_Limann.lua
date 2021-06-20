4f55fd0345aa13fe89641f139b3bc7fc12095d27
huffin <acg1089@aol.com>
2021-06-20 11:14:11 -0500

    Create Wayfarers_Mercenary_Limann.lua

---
+++ lavastorm/Wayfarers_Mercenary_Limann.lua
@@ -0,0 +1,5 @@
+function event_say(e)
+	if (e.message:findi("hail")) then
+		e.self:Say("Me and me sister have got a great thing going here.  We easily can play both sides of the fence with these camps and earn a good amount of coin. As long as neither knows we are relations, we can continue this forever! I've been bringing tokens of the Dark Reign to them and they seem to enjoy getting them, and pay us fairly well.  So, if you give me one, I will be able to spin a tale that will put you in a favorable light.");
+	end
+end

1c5ec8e5cda0220fb5ad8320e05fb2a8213cad72
huffin <acg1089@aol.com>
2021-06-20 09:54:05 -0500

    Update Wayfarers_Mercenary_Elanye.lua

--- lavastorm/Wayfarers_Mercenary_Elanye.lua
+++ lavastorm/Wayfarers_Mercenary_Elanye.lua
@@ -19,8 +19,5 @@ function event_say(e)
 			e.self:Say("I'm not that interested in becoming a servant of those followers of Firiona Vie myself, but I don't mind the cash they pay me. I keep most for me and throw a little back to the Wayfarers crew. If you're looking to get on their good side, I can make some money and you can gain their audience.");
 			-- Blood of the Winged, Goblin Broth
 			eq.task_selector({273, 274});
-		else
-			e.self:Say("I've gotten acquainted with Norrath's Keepers well enough to know they might be interested in speaking with you.");
-		end
 	end
 end

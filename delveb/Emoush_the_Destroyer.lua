4463f4903ca8e9d79d616d30231b94fe1ba37117
huffin <acg1089@aol.com>
2021-06-20 15:09:33 -0500

    Update Emoush_the_Destroyer.lua

--- delveb/Emoush_the_Destroyer.lua
+++ delveb/Emoush_the_Destroyer.lua
@@ -2,7 +2,7 @@ function event_signal(e)
   if(e.signal == 1) then
     e.self:SetSpecialAbility(24, 0);
     e.self:SetSpecialAbility(35, 0);
-    e.self:ModifyNPCStat("hp_regen", "1");
+    e.self:ModifyNPCStat("hp_regen", "220");
   elseif(e.signal == 2) then
     e.self:SetSpecialAbility(24, 1);
     e.self:SetSpecialAbility(35, 1);

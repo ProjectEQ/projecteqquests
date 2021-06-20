52d0a902fd1bc5ade2947fc0d5040774c8b5334f
huffin <acg1089@aol.com>
2021-06-20 14:23:20 -0500

    Update unconscious_mystic.lua

--- delveb/unconscious_mystic.lua
+++ delveb/unconscious_mystic.lua
@@ -1,20 +1,25 @@
 function event_spawn(e)
-eq.set_timer('reset', 180 * 1000);
-e.self:SetAppearance(3)
+eq.set_timer("reset", 120 * 1000);
+eq.set_timer("liedown", 1 * 1000);
+e.self:SetAppearance(3);
 end
 
 function event_timer(e)
-eq.stop_timer('reset');
+if (e.timer == "reset") then
+eq.stop_timer("reset");
 eq.depop_all(342060);
 eq.spawn2(342053,0,0,23,2025,2,401); -- NPC: #Mystic_Braggle
 eq.spawn2(342054,0,0,-161,2064,-11,149); -- NPC: #Mystic_Shiflor
 eq.spawn2(342055,0,0,-85,1961,-.23,35); -- NPC: #Mystic_Griknok
 eq.signal(342052,2); --emoush goes back to sleep
+elseif (e.timer == "liedown") then
+e.self:SetAppearance(3);
+eq.stop_timer("liedown");
 end
 
 function event_signal(e)
   if(e.signal == 1) then
-    eq.stop_timer('reset');
+    eq.stop_timer("reset");
     eq.depop_all(342060);
   end
 end

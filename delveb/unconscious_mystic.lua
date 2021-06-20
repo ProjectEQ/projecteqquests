aeb548ad8b75ac2f4c44f30ad07d557ed0cb4b47
huffin <acg1089@aol.com>
2021-06-20 15:22:10 -0500

    Update unconscious_mystic.lua

--- delveb/unconscious_mystic.lua
+++ delveb/unconscious_mystic.lua
@@ -6,27 +6,19 @@ end
 function event_timer(e)
     if (e.timer == "reset") then
         eq.stop_timer("reset");
+        eq.signal(342052,2); --emoush goes back to sleep
+        eq.depop_all(342060); --depop unconscious mystics
         local el = eq.get_entity_list();
         if ( el:IsMobSpawnedByNpcTypeID(342054) == false) then
             eq.spawn2(342054,0,0,-161,2064,-11,149); -- NPC: #Mystic_Shiflor
-        end
-        if ( el:IsMobSpawnedByNpcTypeID(342053) == false) then
+        elseif ( el:IsMobSpawnedByNpcTypeID(342053) == false) then
             eq.spawn2(342053,0,0,23,2025,2,401); -- NPC: #Mystic_Braggle
-        end
-        if ( el:IsMobSpawnedByNpcTypeID(342055) == false) then
+        elseif ( el:IsMobSpawnedByNpcTypeID(342055) == false) then
             eq.spawn2(342055,0,0,-85,1961,-.23,35); -- NPC: #Mystic_Griknok
         end
-        eq.signal(342052,2); --emoush goes back to sleep
-        eq.depop_all(342060); --depop unconscious mystics
+
     elseif (e.timer == "liedown") then
         e.self:SetAppearance(3);
         eq.stop_timer("liedown");
     end
-end
-
-function event_signal(e)
-  if(e.signal == 1) then
-    eq.stop_timer("reset");
-    eq.depop_all(342060);
-  end
 end

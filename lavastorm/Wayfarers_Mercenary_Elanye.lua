63033eb71fd74c4a4e384794da06b64c7da4074b
huffin <acg1089@aol.com>
2021-06-20 09:59:23 -0500

    Update Wayfarers_Mercenary_Elanye.lua
    doesnt require any min faction to do these

--- lavastorm/Wayfarers_Mercenary_Elanye.lua
+++ lavastorm/Wayfarers_Mercenary_Elanye.lua
@@ -1,21 +1,6 @@
---only get quest if faction <= indifferent
---1	Ally
---2	Warmly
---3	Kindly
---4	Amiably
---5	Indifferent
---6	Apprehensive
---7	Dubious
---8 Threateningly
---9	Scowls
-
---404 Dark Reign
---429 Norrath's Keepers
 
 function event_say(e)
 	if(e.message:findi("hail")) then
---		local NKF=e.other:GetFactionLevel(e.other:GetID(), e.self:GetID(), e.other:GetBaseRace(), e.other:GetClass(), e.other:GetDeity(), 1023, e.self); dont need this 
---		if (NKF >= 5) then
 			e.self:Say("I'm not that interested in becoming a servant of those followers of Firiona Vie myself, but I don't mind the cash they pay me. I keep most for me and throw a little back to the Wayfarers crew. If you're looking to get on their good side, I can make some money and you can gain their audience.");
 			-- Blood of the Winged, Goblin Broth
 			eq.task_selector({273, 274});

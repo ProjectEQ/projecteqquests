114120cbebac580136128d973e1207c8866a22af
huffin <acg1089@aol.com>
2021-06-21 15:06:33 -0500

    Update #Ju-rek_the_Corrupted.lua
    flavor flav

--- thenest/#Ju-rek_the_Corrupted.lua
+++ thenest/#Ju-rek_the_Corrupted.lua
@@ -8,30 +8,35 @@ function event_combat(e)
 end
 
 function event_hp(e)
-	if (e.hp_event == 95) then
+  if (e.hp_event == 95) then
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
+    eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"Harsh wind is kicked up from Ju`Rek's wings.");
     eq.set_next_hp_event(80);
   elseif (e.hp_event == 80) then
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
+    eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"Harsh wind is kicked up from Ju`Rek's wings.");
     eq.set_next_hp_event(60);
   elseif (e.hp_event == 60) then
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
+    eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"Harsh wind is kicked up from Ju`Rek's wings.");
     eq.set_next_hp_event(40);
   elseif (e.hp_event == 40) then
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
+    eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"Harsh wind is kicked up from Ju`Rek's wings.");
     eq.set_next_hp_event(20);
   elseif (e.hp_event == 20) then
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
     eq.spawn2(343027,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: a_harsh_wind
+    eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"Harsh wind is kicked up from Ju`Rek's wings.");
   end
 end
 

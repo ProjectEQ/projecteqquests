ca1f6cd2b29f1ac06c4cd7703a87d4ce2d52384a
huffin <acg1089@aol.com>
2021-06-23 14:37:42 -0500

    Create Invis_Man_Ambush_Trap.lua

---
+++ thundercrest/Invis_Man_Ambush_Trap.lua
@@ -0,0 +1,7 @@
+function event_combat(e)
+	if (e.joined == true) then
+		eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 15, 100,"Hidden panels open from the ceiling as a pair of assassins drop from above.");
+    eq.spawn2(340502, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading()); -- an_Autumn_Rose_assassin (340502)
+    eq.spawn2(340502, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading()); -- an_Autumn_Rose_assassin (340502)
+	end
+end

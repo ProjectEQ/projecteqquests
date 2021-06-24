f599df0c6a69357b53999d56e0f7be7f687d53cf
huffin <acg1089@aol.com>
2021-06-24 13:59:08 -0500

    Update #Shala_Belen.lua
    IDS

--- thenest/#Shala_Belen.lua
+++ thenest/#Shala_Belen.lua
@@ -3,7 +3,7 @@ function event_combat(e)
 
 		local npc_list =  eq.get_entity_list():GetNPCList();
 		for npc in npc_list.entries do
-			if (npc.valid and (npc:GetNPCTypeID() == 343644)) then
+			if (npc.valid and (npc:GetNPCTypeID() == 343664)) then
 			npc:AddToHateList(e.self:GetHateRandom(),1);
 			end
 		end

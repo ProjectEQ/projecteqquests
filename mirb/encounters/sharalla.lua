e2d30a4c9554c9e9de97b7d613464b35187b22bb
huffin <acg1089@aol.com>
2021-06-25 15:50:59 -0500

    Update sharalla.lua

--- mirb/encounters/sharalla.lua
+++ mirb/encounters/sharalla.lua
@@ -22,11 +22,8 @@ local animals = {
 }
 
 local function make_attackable(mob, attackable)
-  mob:SetSpecialAbility(SpecialAbility.immune_melee, attackable and 0 or 1)
-  mob:SetSpecialAbility(SpecialAbility.immune_magic, attackable and 0 or 1)
   mob:SetSpecialAbility(SpecialAbility.immune_aggro, attackable and 0 or 1)
   mob:SetSpecialAbility(SpecialAbility.immune_aggro_on, attackable and 0 or 1)
-  mob:SetSpecialAbility(SpecialAbility.no_harm_from_client, attackable and 0 or 1)
 end
 
 local function depop_event()
@@ -97,7 +94,7 @@ function animal_combat(e)
 end
 
 -- deaggro condition is a random check every 5s (not sure on exact chance)
--- once deaggro, the animal is non-aggroable and non-hittable until the next tick
+-- once deaggro, the animal is non-aggroable
 function animal_timer(e)
   if e.timer == "process" then
     local in_combat = (e.self:GetEntityVariable("combat") == "1")

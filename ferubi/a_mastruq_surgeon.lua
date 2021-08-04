c1f7c461d3b84630f81e4904282c4e4a4e1d2f89
huffin <acg1089@aol.com>
2021-08-04 11:53:23 -0500

    Update a_mastruq_surgeon.lua

--- ferubi/a_mastruq_surgeon.lua
+++ ferubi/a_mastruq_surgeon.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
-    if(e.self:GetSp2() == 287779 or e.self:GetSp2() == 287780) then
+    if(e.self:GetSp2() == 287779 or e.self:GetSp2() == 287826) then
         eq.signal(284110,1); --signal Event_Spawner to add to blademaster counter
     end
     if(e.self:GetSp2() == 287782) then

ccb378e02ac57ba19a97ddba34df7fb8b20ae1c2
huffin <acg1089@aol.com>
2021-08-04 10:03:42 -0500

    Update #Pixtt_Kekken.lua

--- riwwi/#Pixtt_Kekken.lua
+++ riwwi/#Pixtt_Kekken.lua
@@ -1,9 +1,12 @@
+local TM_minimum_players = 18; --changed to 6 much later (2017)
+
+
 function event_say(e)
 	if(e.message:findi("hail")) then
 			e.self:Say("It's a good thing Taskmistress Krisz doesn't know what we're up to.  Wow, does she have a temper, and enough skill with a blade to back it up.  No one would [" .. eq.say_link("dare") .. "] cross her.");
 	elseif(e.message:findi("dare")) then
-		if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(282115) == true and eq.get_entity_list():IsMobSpawnedByNpcTypeID(282105) == false) then
-		-- if TM trigger is up and TM is not currently spawned, spawn TM
+		if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(282115) == true and eq.get_entity_list():IsMobSpawnedByNpcTypeID(282105) == false and e.other:GetRaidMemberCountInZone() >= TM_minimum_players) then
+		-- if TM trigger is up and TM is not currently spawned and 18 or more in raid, spawn TM
 			e.self:Emote("laughs, 'Think you could teach that old goat a lesson!?");
 			eq.unique_spawn(282105,21,0,-1117,-408,33,502); -- #Taskmistress_Krisz
 		else
@@ -11,3 +14,5 @@ function event_say(e)
 		end
 	end
 end
+
+--need to add random emotes

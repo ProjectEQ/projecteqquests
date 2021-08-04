042574ccce6c5b544c4a49113f4c4c024276987b
huffin <acg1089@aol.com>
2021-08-03 20:26:50 -0500

    Update and rename #Pixtt_Kekken.pl to #Pixtt_Kekken.lua

---
+++ riwwi/#Pixtt_Kekken.lua
@@ -0,0 +1,13 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+			e.self:Say("It's a good thing Taskmistress Krisz doesn't know what we're up to.  Wow, does she have a temper, and enough skill with a blade to back it up.  No one would [" .. eq.say_link("dare") .. "] cross her.");
+	elseif(e.message:findi("dare")) then
+		if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(282115) == true and eq.get_entity_list():IsMobSpawnedByNpcTypeID(282105) == false) then
+		-- if TM trigger is up and TM is not currently spawned, spawn TM
+			e.self:Emote("laughs, 'Think you could teach that old goat a lesson!?");
+			eq.unique_spawn(282105,21,0,-1117,-408,33,502); -- #Taskmistress_Krisz
+		else
+			e.self:Say("She hasn't been around much lately, but I'm sure she'll be back.");
+		end
+	end
+end

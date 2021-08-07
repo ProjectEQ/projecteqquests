7339002eed20d78aa396d89d5002598763970c1c
huffin <acg1089@aol.com>
2021-08-07 17:23:18 -0500

    Update Spirit_of_Tunik_Tamuik.lua

--- ikkinz/Spirit_of_Tunik_Tamuik.lua
+++ ikkinz/Spirit_of_Tunik_Tamuik.lua
@@ -1,7 +1,8 @@
 --Ikkinz Raid #1: Chambers of Righteousness   
 function event_say(e)
+	local el = eq.get_entity_list();
 	if(e.message:findi("hail")) then
-		local el = eq.get_entity_list();
+
 		if ( el:IsMobSpawnedByNpcTypeID(294574) == false and el:IsMobSpawnedByNpcTypeID(294575) == false and el:IsMobSpawnedByNpcTypeID(294339) == false and el:IsMobSpawnedByNpcTypeID(294338) == false and el:IsMobSpawnedByNpcTypeID(294341) == false) then
 		e.self:Emote("can't see you nor does he know your name.  He blinks as he regards you as a trusik. 'You have made it through the trials of this sanctuary. The Guardian of Righteousness was my creation and will be forevermore. It is a ward against those who are not worthy and a test to those trusik priests who believe they can become the highest among priests -- the Vrex. You are most talented to have made it this far. Now, though, it is time for me to rest. I left something with my creations that you should have with you, correct? Hand it to me now.");
 		else

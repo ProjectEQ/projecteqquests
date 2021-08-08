852988934d91e5fea22d29adad698eb424cb2cb6
huffin <acg1089@aol.com>
2021-08-07 21:05:41 -0500

    Update and rename #Defender_of_Righteousness.pl to #Defender_of_Righteousness.lua

---
+++ ikkinz/#Defender_of_Righteousness.lua
@@ -0,0 +1,27 @@
+--Ikkinz Raid #1: Chambers of Righteousness  
+
+function event_combat(e)
+	if (e.joined == true) then
+		eq.set_timer("OOBcheck", 3 * 1000);	
+	else
+		eq.stop_timer("OOBcheck");
+	end
+end
+
+
+function event_timer(e)
+if(e.timer=="OOBcheck") then
+eq.stop_timer("OOBcheck");
+	if (e.self:GetX() > 115 or e.self:GetX() < -180 or e.self:GetY() > -360 or e.self:GetY() < -470) then
+		e.self:CastSpell(3791,e.self:GetID()); -- Spell: Ocean's Cleansing
+		e.self:GotoBind();
+		e.self:WipeHateList();
+	else
+		eq.set_timer("OOBcheck", 3 * 1000);
+	end
+end
+end
+
+function event_death_complete(e)
+eq.signal(294141,2); -- NPC: #Trigger_Ikkinz_1
+end

ba1fcebdb5ddd0e864afdb410b709ae7f600314a
huffin <acg1089@aol.com>
2021-08-08 20:25:50 -0500

    Update and rename #Guardian_of_Glorification.pl to #Guardian_of_Glorification.lua

---
+++ ikkinz/#Guardian_of_Glorification.lua
@@ -0,0 +1,68 @@
+--Ikkinz Raid #2: Chambers of Glorification
+--2nd guardian of glorification in the final sequence
+local reduce_regen = 0;
+local reduce_dmg = 0;
+local reduce_hp = 0;
+
+function event_spawn(e)
+	eq.set_next_hp_event(55);
+	reduce_regen = 0;
+	reduce_dmg = 0;
+	reduce_hp = 0;
+end
+
+function event_hp(e)
+	if(e.hp_event == 55) then
+		eq.zone_emote(0, "Guardian of Glorification splits into two smaller stone workers!");
+		eq.spawn2(294583,0,0,1038,-484,-68,384); -- NPC: ##Guardian_of_Glorification
+		eq.spawn2(294583,0,0,1039,-431,-68,384); -- NPC: ##Guardian_of_Glorification
+		eq.depop();
+	end
+end
+				
+function event_signal(e)
+	if (e.signal == 1) then
+ 		if (reduce_regen == 0) then
+			reduce_regen = reduce_regen + 1;
+			e.self:ModifyNPCStat("hp_regen", "1000");
+		elseif (reduce_regen == 1) then
+			reduce_regen = reduce_regen + 1;
+			e.self:ModifyNPCStat("hp_regen", "1");
+		end
+	elseif (e.signal == 2) then
+		eq.modify_npc_stat("max_hp", tostring(e.self:GetMaxHP() - 70000)); --reduce max hp by 70k per signal (2 total)
+	elseif (e.signal == 3) then
+		 if (reduce_dmg == 0) then
+			reduce_dmg = reduce_dmg + 1;
+			e.self:ModifyNPCStat("max_hit", "2912")
+  			e.self:ModifyNPCStat("min_hit", "1126")
+		elseif (reduce_dmg == 1) then
+			reduce_dmg = reduce_dmg + 1;
+			e.self:ModifyNPCStat("max_hit", "2538")
+   			e.self:ModifyNPCStat("min_hit", "980")
+		end
+	end
+end
+
+function event_combat(e)
+	if(e.joined) then
+		eq.set_timer("random", 5 * 1000);
+	else
+		eq.stop_timer("random");
+	end
+end
+
+function event_timer(e)
+	if (e.timer == "random") then
+		local rand = math.random(1,100);
+		if (rand >= 85) then -- 15 % to cast throw
+		e.self:Emote("tosses its foes away wildly!");
+				e.self:CastedSpellFinished(4185, e.self:GetHateRandom());	-- Spell: Throw
+				e.self:CastedSpellFinished(4185, e.self:GetHateRandom());	-- Spell: Throw
+				e.self:CastedSpellFinished(4185, e.self:GetHateRandom());	-- Spell: Throw
+		elseif (rand < 85) and (rand >= 70) then -- 15 % to cast manabolt
+			e.self:Emote("lets loose a bolt of energy toward his enemy!");
+				e.self:CastedSpellFinished(1046, e.self:GetHateRandom());	-- Spell: Manabolt
+		end
+	end
+end

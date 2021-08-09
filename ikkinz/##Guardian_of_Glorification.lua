7ae3906048957bdcfa61d1a1e056e39cd3a7d07a
huffin <acg1089@aol.com>
2021-08-08 21:43:37 -0500

    Update and rename ##Guardian_of_Glorification.pl to ##Guardian_of_Glorification.lua

---
+++ ikkinz/##Guardian_of_Glorification.lua
@@ -0,0 +1,39 @@
+--Ikkinz Raid #2: Chambers of Glorification
+--2nd form of split mobs that will split into their final form
+
+function event_spawn(e)
+	eq.set_next_hp_event(55);
+
+end
+
+function event_hp(e)
+	if(e.hp_event == 55) then
+		eq.zone_emote(0, "Guardian of Glorification splits into two smaller stone workers!");
+		eq.spawn2(294584,0,0,1038,-484,-68,384); -- NPC: #Guardian_of_Glorification_
+		eq.spawn2(294584,0,0,1039,-431,-68,384); -- NPC: #Guardian_of_Glorification_
+		eq.depop();
+	end
+end
+
+function event_combat(e)
+	if(e.joined) then
+		eq.set_timer("random", 5 * 1000);
+	else
+		eq.stop_timer("random");
+	end
+end
+
+function event_timer(e)
+	if (e.timer == "random") then
+		local rand = math.random(1,100);
+		if (rand >= 85) then -- 15 % to cast throw
+		e.self:Emote("tosses its foes away wildly!");
+				e.self:CastedSpellFinished(4185, e.self:GetHateRandom());	-- Spell: Throw
+				e.self:CastedSpellFinished(4185, e.self:GetHateRandom());	-- Spell: Throw
+				e.self:CastedSpellFinished(4185, e.self:GetHateRandom());	-- Spell: Throw
+		elseif (rand < 85) and (rand >= 70) then -- 15 % to cast manabolt
+			e.self:Emote("lets loose a bolt of energy toward his enemy!");
+				e.self:CastedSpellFinished(1046, e.self:GetHateRandom());	-- Spell: Manabolt
+		end
+	end
+end

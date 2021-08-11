2aead0705b34b4f39a2eda53b49ab49a28675996
huffin <acg1089@aol.com>
2021-08-11 11:39:28 -0500

    Update 294478.lua

--- ikkinz/294478.lua
+++ ikkinz/294478.lua
@@ -9,4 +9,6 @@ end
 
 function event_death_complete(e)
 eq.signal(294582,2); -- NPC: #Guardian_of_Glorification (294582) signal to reduce max hp
+eq.signal(294583,2); -- ##Guardian_of_Glorification (294583) signal to reduce max hp
+eq.signal(294584,2); -- #Guardian_of_Glorification_ (294584) signal to reduce max hp
 end

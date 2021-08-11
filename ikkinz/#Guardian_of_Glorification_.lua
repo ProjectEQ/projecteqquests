3ebf9d1fe5829e48dab55c2e610da9c04eab175e
huffin <acg1089@aol.com>
2021-08-11 11:21:28 -0500

    Update #Guardian_of_Glorification_.lua

--- ikkinz/#Guardian_of_Glorification_.lua
+++ ikkinz/#Guardian_of_Glorification_.lua
@@ -1,5 +1,11 @@
 --Ikkinz Raid #2: Chambers of Glorification
 --final split form that drops loot
+local hp_regen = 15;
+
+function event_spawn(e)
+	eq.set_timer("checknpc", 1 * 1000);
+end
+
 function event_combat(e)
 	if(e.joined) then
 		eq.set_timer("random", 5 * 1000);
@@ -45,10 +51,33 @@ function event_timer(e)
 			else
 				eq.set_timer("OOBcheck", 3 * 1000);
 			end
+	elseif(e.timer=="checknpc") then
+		eq.stop_timer("checknpc");
+		local npc_list = eq.get_entity_list():GetNPCList();
+
+		if (npc_list ~= nil) then
+			-- loop through the list entries
+			for npc in npc_list.entries do
+			
+				if (npc:GetNPCTypeID() == 294474) then
+					-- if any Glorified_Bolsterer is alive, add hp
+					eq.modify_npc_stat("max_hp", tostring(e.self:GetMaxHP() + 70000)); --increase max hp by 70k per npc (2 total)
+				end
+				if (npc:GetNPCTypeID() == 294475) then
+					-- if any Glorified_Bolsterer is alive, add regen
+					eq.modify_npc_stat("hp_regen", tostring(hp_regen + 1100)); --increase hp regen by 1100 per npc (2 total)
+				end
+				if (npc:GetNPCTypeID() == 294478) then
+					-- if any Glorified_Bolsterer is alive, add min and max dmg
+					eq.modify_npc_stat("min_hit", tostring(e.self:GetMinDMG() + 70)); --increase min dmg by 70 per npc (2 total)
+					eq.modify_npc_stat("max_hit", tostring(e.self:GetMaxDMG() + 241)); --increase max dmg by 241 per npc (2 total)
+				end
+			end
+		end
 	end
 end
 
 function event_death_complete(e)
-eq.signal(294631,4); -- NPC: lockout_ikkinz
-eq.spawn2(294585, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); --NPC: a_pile_of_bones
+	eq.signal(294631,4); -- NPC: lockout_ikkinz
+	eq.spawn2(294585, 0, 0, e.self:GetX(), e.self:GetY(),  e.self:GetZ(),  e.self:GetHeading()); --NPC: a_pile_of_bones
 end

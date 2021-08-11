86d7630d30598ba0d362b08331737d5a85756b1c
huffin <acg1089@aol.com>
2021-08-10 19:24:18 -0500

    Update an_enthralled_listener.lua

--- ikkinz/an_enthralled_listener.lua
+++ ikkinz/an_enthralled_listener.lua
@@ -1,5 +1,5 @@
 function event_signal(e)
 if (e.signal == 1) then
-e.self:CastedSpellFinished(8277, e.self:GetHateRandom());	-- Spell: Fling
+e.self:CastedSpellFinished(8277, e.self);	-- Spell: Fling
 end
 end

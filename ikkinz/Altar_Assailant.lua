3c31ce70dcd1646755f2cc7db4475b6ac8e52005
huffin <acg1089@aol.com>
2021-08-13 16:30:08 -0500

    Update Altar_Assailant.lua

--- ikkinz/Altar_Assailant.lua
+++ ikkinz/Altar_Assailant.lua
@@ -13,5 +13,23 @@ function event_timer(e)
 		eq.set_timer("depop", 5 * 1000); -- guessing 5 sec until depop at this point
 	elseif (e.timer == "depop") then
 		eq.depop();
+	elseif(e.timer=="OOBcheck") then
+		eq.stop_timer("OOBcheck");
+			if (e.self:GetY() < -70) then
+				e.self:CastSpell(3791,e.self:GetID()); -- Spell: Ocean's Cleansing
+				e.self:GotoBind();
+				e.self:WipeHateList();
+			else
+				eq.set_timer("OOBcheck", 3 * 1000);
+			end
+	end
+end
+
+function event_combat(e)
+	if (e.joined == true) then
+		eq.set_timer("OOBcheck", 3 * 1000);
+		
+	else
+		eq.stop_timer("OOBcheck");
 	end
 end

241537c086e54e83195ac9d8d657ae22480d637f
huffin <acg1089@aol.com>
2021-08-13 15:38:09 -0500

    Update Altar_Assailant.lua

--- ikkinz/Altar_Assailant.lua
+++ ikkinz/Altar_Assailant.lua
@@ -9,7 +9,7 @@ function event_timer(e)
 		e.self:WipeHateList();
 		eq.stop_timer("deactivate");
 		eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"The assailant runs out of energy and crumbles to the ground.");
-		e.Self:SetAppearance(3);
+		e.self:SetAppearance(3);
 		eq.set_timer("depop", 5 * 1000); -- guessing 5 sec until depop at this point
 	elseif (e.timer == "depop") then
 		eq.depop();

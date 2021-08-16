166655e86c2287a9ef33e9ec947c713e5e135058
huffin <acg1089@aol.com>
2021-08-16 14:31:51 -0500

    Update Constrained_Servitor.lua

--- ikkinz/Constrained_Servitor.lua
+++ ikkinz/Constrained_Servitor.lua
@@ -2,5 +2,6 @@
 
 function event_death_complete(e)
 eq.zone_emote(0,"The constrained servitor is allowed to rest at last. The priests he was serving have been weakened by his death.");
-eq.signal(294085,1); -- NPC: Malevolent_Priest
+eq.signal(294138,1); --signal #Malevolent_Priest (294138) to reduce hp
+eq.signal(294629,1); --signal #Malevolent_Priest_ (294629) to reduce hp
 end

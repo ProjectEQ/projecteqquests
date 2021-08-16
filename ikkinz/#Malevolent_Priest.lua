186c4c403dbc6606598d1984a392fcdeb853d6b2
huffin <acg1089@aol.com>
2021-08-16 15:20:58 -0500

    Update #Malevolent_Priest.lua

--- ikkinz/#Malevolent_Priest.lua
+++ ikkinz/#Malevolent_Priest.lua
@@ -69,10 +69,12 @@ end
 
 
 function event_death_complete(e)
-if(eq.get_entity_list():GetNPCBySpawnID(294629) == false) then --#Malevolent_Priest_ (294629)
-Instance_Win();
---check for other malevolent priest, if both are down, instance is won
-end
+	local el = eq.get_entity_list();
+  	
+	if (el:IsMobSpawnedByNpcTypeID(294086) == false) then --#Malevolent_Priest_ (294629)
+		Instance_Win();
+		--check for other malevolent priest, if both are down, instance is won
+	end
 end
 
 function Instance_Win(e)

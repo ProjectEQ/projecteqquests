4be32016c272985804dfc051b41977fd398d77ce
huffin <acg1089@aol.com>
2021-08-17 10:23:59 -0500

    Update #Trigger_Ikkinz_2.lua

--- ikkinz/#Trigger_Ikkinz_2.lua
+++ ikkinz/#Trigger_Ikkinz_2.lua
@@ -4,9 +4,19 @@ local counter = 0
 function event_signal(e)
   if e.signal == 1 then -- signal from death of Flesh_Hunter
     counter = counter + 1
+   eq.set_timer("kyv1", math.random(450,630) * 1000); --7.5-10.5 min RNG timer to respawn
+  elseif e.signal == 2 then -- signal from death of Flesh_Hunter
+    counter = counter + 1
+   eq.set_timer("kyv2", math.random(450,630) * 1000); --7.5-10.5 min RNG timer to respawn
+  elseif e.signal == 3 then -- signal from death of Flesh_Hunter
+    counter = counter + 1
+   eq.set_timer("kyv3", math.random(450,630) * 1000); --7.5-10.5 min RNG timer to respawn
   end
 
   if counter == 3 then
+    eq.stop_timer("kyv1");
+    eq.stop_timer("kyv2");
+    eq.stop_timer("kyv3");
     local dz = eq.get_expedition()
     if dz.valid then
       dz:SetLocked(true, ExpeditionLockMessage.Close)
@@ -18,5 +28,21 @@ function event_signal(e)
       eq.zone_emote(0, "The Tri-Fate Hunters have been defeated! Though the legion may send replacements, you have finished what you sought out to do and delayed their progress for a time! Congratulations!")
       eq.ZoneMarquee(10, 510, 1, 1, 6000, "The Tri-Fate Hunters have been defeated! Congratulations!");
     end
+  end
+end
+
+function event_timer(e)
+  if(e.timer=="kyv1") then
+    eq.stop_timer("kyv1");
+    counter = counter - 1
+    eq.spawn2(294624, 0, 0, 623, -559, -46, 384)
+  elseif(e.timer=="kyv2") then
+    eq.stop_timer("kyv2");
+    counter = counter - 1
+    eq.spawn2(294135, 0, 0, 623, -483, -46, 384)
+  elseif(e.timer=="kyv3") then
+    eq.stop_timer("kyv3");
+    counter = counter - 1
+    eq.spawn2(294625, 0, 0, 623, -634, -46, 384)
   end
 end

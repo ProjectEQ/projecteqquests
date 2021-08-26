9b0a091b137d009e485299fbb027c04e4b390f33
huffin <acg1089@aol.com>
2021-08-26 14:27:49 -0500

    Create 186152.lua

---
+++ hateplaneb/186152.lua
@@ -0,0 +1,24 @@
+--_Insanity (186152) emotes random things zonewide
+function event_spawn(e)
+  eq.set_timer("emote", 45 * 60 * 1000); -- 45 min
+end
+
+function event_timer(e)
+  if(e.timer=="emote") then
+  local rand = math.random(1,7);
+    if (rand ==1) then
+      eq.zone_emote(0, "A blood chilling cry echoes through the darkened streets");
+    elseif (rand == 2) then
+      eq.zone_emote(0, "A wise man would not find himself invading the realm of Innoruuk.");
+    elseif (rand == 3) then
+      eq.zone_emote(0, "Filled with rage, you can hardly wait until you are able to spill blood again.  In fact, you might not wait!  You never did like the way that person over there looked at you!");
+    elseif (rand == 4) then
+      eq.zone_emote(0, "Suddenly paranoid,  you look at each of your party members.  Which one will be corrupted first?  Who will die next?  Will one of them fail me?");
+    elseif (rand == 5) then
+      eq.zone_emote(0, "The stench of decay is overpowering");
+    elseif (rand == 6) then
+      eq.zone_emote(0, "You really hate this place.");
+    elseif (rand == 7) then
+      eq.zone_emote(0, "You would much rather be in a safe and warm inn than this hellish dimension.");
+  end
+end

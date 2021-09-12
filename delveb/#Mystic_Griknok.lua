57b7876d221432f06128282800591c3cb7d33af0
huffin <acg1089@aol.com>
2021-09-12 10:08:54 -0500

    Delete #Mystic_Griknok.lua

--- delveb/#Mystic_Griknok.lua
+++
@@ -1,6 +0,0 @@
-function event_death_complete(e)
-eq.spawn2(342060,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: unconscious_mystic
-  if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(342053) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(342054) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(342055) == false) then
-    eq.signal(342052,1); --signal emoush to wake up
-  end
-end

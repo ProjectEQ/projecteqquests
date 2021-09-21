a4fa41c6486a0e3847ee80a6d99eb8317ca56794
huffin <acg1089@aol.com>
2021-09-21 10:53:45 -0500

    Update Guardian_of_the_Past.lua

--- thundercrest/Guardian_of_the_Past.lua
+++ thundercrest/Guardian_of_the_Past.lua
@@ -1,8 +1,8 @@
 function event_death_complete(e)
-if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(340413) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(340412) == false) then
-eq.signal(340395,1); -- NPC: #Lore_Warden
-eq.signal(340391,1); -- NPC: Messenger_Drake
-end
+	if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(340413) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(340412) == false) then
+		eq.signal(340395,1); -- NPC: #Lore_Warden
+		eq.signal(340391,1); -- NPC: Messenger_Drake
+	end
 end
 
 function event_combat(e)
@@ -17,7 +17,7 @@ end
 function event_timer(e)
 if(e.timer=="OOBcheck") then
 eq.stop_timer("OOBcheck");
-	if (e.self:GetX() < 1712 or e.self:GetY() < 2433) then
+	if (e.self:GetX() < 1712 and e.self:GetY() < 2433) then
 		e.self:GotoBind();
 		e.self:WipeHateList();
 		e.self:CastSpell(3230,e.self:GetID()); -- Spell: Balance of the Nameless

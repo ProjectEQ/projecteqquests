9ba8e3cd53334e86ac5396a897085686ee97621f
huffin <acg1089@aol.com>
2021-09-25 21:39:52 -0500

    Update a_kyv_heartpiercer.lua

--- barindu/a_kyv_heartpiercer.lua
+++ barindu/a_kyv_heartpiercer.lua
@@ -4,7 +4,7 @@ end
 
 function event_hp(e)
     if (e.hp_event == 45) then
-        eq.set_timer("casting", 1000)
+        eq.set_timer("casting", 20 * 1000) -- chance to fire every 20 sec
         eq.set_next_inc_hp_event(45)
     elseif (e.inc_hp_event == 45) then
         eq.stop_timer("casting")
@@ -14,8 +14,12 @@ end
 
 function event_timer(e)
     if (e.timer == "casting") then
+        local roll = math.random(100)
+        if (roll >= 50) then
+        e.self:Emote("releases a volley of arrows.");
         e.self:CastSpell(eq.ChooseRandom(4850, 4849, 4851), e.self:GetTarget():GetID())
-        eq.set_timer("casting", 10 * math.random(1, 3) * 1000)
+        eq.set_timer("casting", 20  * 1000)
+        end
     end
 end
 

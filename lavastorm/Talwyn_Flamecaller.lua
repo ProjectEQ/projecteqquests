c8312b5922dc02407f91709505426705134668ed
JJ <jeremiah.oligario@gmail.com>
2021-10-21 06:08:51 -0400

    Allow for "recharge" of Spell: Electrical Charge. http://www.projecteq.net/forums/index.php?threads/wiz-1-5-pre-quest-recharge-spell-electrical-charge.17206/

--- lavastorm/Talwyn_Flamecaller.lua
+++ lavastorm/Talwyn_Flamecaller.lua
@@ -165,8 +165,8 @@ function event_trade(e)
   elseif (qglobals["wiz_epic_pre"] == "2" and item_lib.check_turn_in(e.trade, {item1 = 16675})) then -- Item: Cursed Talisman
     e.self:Say("Your speed and concentration impress even me. The Test of Channeling is no easy feat to accomplish, especially with so many dangerous distractions nearby.");
     e.other:SummonItem(16675); -- Item: Cursed Talisman
-    eq.set_global("wiz_epic_pre","3",5,"F"); -- Test of Channeling comlete
-  elseif (qglobals["wiz_epic_pre"] == "3" and item_lib.check_turn_in(e.trade, {item1 = 16623})) then -- Item: Spell: Electrical Charge
+    eq.set_global("wiz_epic_pre","3",5,"F"); -- Test of Channeling complete
+  elseif ((qglobals["wiz_epic_pre"] == "3" or qglobals["wiz_epic_pre"] == "4") and item_lib.check_turn_in(e.trade, {item1 = 16623})) then -- Item: Spell: Electrical Charge
     e.self:Say("The Test of Research wears thin upon you, but with this parchment I hold in my hand, you have proven your worth as a contributor to the cause!");
     e.other:SummonItem(16623); -- Item: Spell: Electrical Charge
     eq.set_global("wiz_epic_pre","4",5,"F"); -- Test of Research complete

596a6614a0971396ca51c5a60abd91e41cf27570
huffin <acg1089@aol.com>
2021-11-10 10:15:50 -0600

    Delete a_decaying_corpse.pl

--- txevu/a_decaying_corpse.pl
+++
@@ -1,6 +0,0 @@
-sub EVENT_SIGNAL {
-	if ($signal == 1) { #from bloodfeaster saying spawn my stonemite adds
-		quest::spawn2(297207,0,0,plugin::RandomRange(215,280), plugin::RandomRange(405,445), -419, 0);
-		quest::spawn2(297207,0,0,plugin::RandomRange(215,280), plugin::RandomRange(405,445), -419, 0);
-	}
-}

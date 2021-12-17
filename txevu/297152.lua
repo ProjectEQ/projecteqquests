4534856b7175e77a8770cb78742ea97e6e795dd0
huffin <acg1089@aol.com>
2021-12-17 09:42:31 -0600

    Create 297152.lua

---
+++ txevu/297152.lua
@@ -0,0 +1,21 @@
+--south jail mobs
+function event_combat(e)
+	if (e.joined == true) then
+		eq.set_timer("OOBcheck", 3000)
+	else
+		eq.stop_timer("OOBcheck")
+	end
+end
+
+function event_timer(e)
+	if(e.timer=="OOBcheck") then
+		eq.stop_timer("OOBcheck");
+			if (e.self:GetY() > -175) then
+				e.self:CastSpell(3791,e.self:GetID()); -- Spell: Ocean's Cleansing
+				e.self:GotoBind();
+				e.self:WipeHateList();
+			else
+				eq.set_timer("OOBcheck", 3 * 1000);
+			end
+	end
+end

c7c167f859849831cd3608dab520df13ae92dfee
huffin <acg1089@aol.com>
2021-12-20 12:15:00 -0600

    Update keymaster.lua

--- riwwi/keymaster.lua
+++ riwwi/keymaster.lua
@@ -5,7 +5,7 @@ end
 function event_timer(e)
 if (e.timer == "replenish") then
 eq.stop_timer("replenish");
-	local rand = math.random(1,4);
+	local rand = math.random(1,5);
 		if (rand == 1) then
       eq.create_ground_object(52154,-1,-201,1,0);
 		elseif (rand == 2) then
@@ -14,6 +14,8 @@ eq.stop_timer("replenish");
       eq.create_ground_object(52154,-736,-327,0,0);
 		elseif (rand == 4) then
       eq.create_ground_object(52154,-172,-233,18,0);
+		elseif (rand == 5) then
+      eq.create_ground_object(52154,-678,495,-36,0);
 		end
 end
 end

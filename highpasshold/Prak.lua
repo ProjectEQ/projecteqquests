9031ca6c1e0258c928c6c8cd9377c771f1735291
huffin <acg1089@aol.com>
2022-01-31 14:19:26 -0600

    Update Prak.lua

--- highpasshold/Prak.lua
+++ highpasshold/Prak.lua
@@ -33,3 +33,13 @@ function event_trade(e)
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
+
+function event_signal(e)
+	if(e.signal == 1) then
+		e.self:Say("The boss might need some help!");
+		local stanos = eq.get_entity_list():GetMobByNpcTypeID(5088); -- Stanos_Herkanor
+		if ( stanos.valid ) then
+			e.self:MoveTo(stanos:GetX(), stanos:GetY(), stanos:GetZ(), -1, false);
+		end
+	end
+end

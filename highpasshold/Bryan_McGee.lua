eb25bdf88b6564bef56f22cb7411fd9b0cf76d6c
huffin <acg1089@aol.com>
2022-02-02 20:05:54 -0600

    Update Bryan_McGee.lua

--- highpasshold/Bryan_McGee.lua
+++ highpasshold/Bryan_McGee.lua
@@ -2,7 +2,7 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("I do not know what you are doing up here, but I am a busy man. Please return to the bar downstairs and enjoy the atmosphere.");
-		eq.signal(5055,2) -- NPC: Beef
+		eq.signal(407027,2) -- NPC: Beef new highpasshold
 	elseif(e.message:findi("oblong bottle")) then
 		e.self:Say("The oblong bottle is a legend. They say one drink and you're off to oblivion, but just before that you feel the best you've ever felt and relive all your finest memories in the blink of an eye. The last I heard, some guy by the name of Turgin Swillfod turned up in Freeport spouting that he had found it. He was never heard from again.");
 	elseif(e.message:findi("never stop chopping")) then
@@ -25,7 +25,7 @@ function event_trade(e)
 		e.other:AddEXP(15000);
 		e.other:GiveCash(0,0,0,0);
 		
-		eq.signal(5055,3) -- NPC: Beef
+		eq.signal(407027,3) -- NPC: Beef new highpasshold
 	elseif(item_lib.check_turn_in(e.trade, {item1 = 12365})) then
 		e.self:Say("I cannot believe you actually acquired all those drinks!! You do good work, kid. Here is the gem as I promised. And a few plat for good measure. Don't let it be said that the Axe doesn't treat his friends right.");
 		e.other:SummonItem(12348); -- Item: Gem of Stamina

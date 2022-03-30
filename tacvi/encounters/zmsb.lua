d4d23d18397a96bfb25311328dd40df19d0048d3
huffin <acg1089@aol.com>
2022-03-30 09:41:45 -0500

    Update zmsb.lua

--- tacvi/encounters/zmsb.lua
+++ tacvi/encounters/zmsb.lua
@@ -10,7 +10,9 @@ function ZMSB_Combat(e)
     
   else
     -- Wipe Mechanics
+	eq.stop_timer("check");
     eq.stop_timer("rage");
+	-- door should only unlock when he reaches 100%
     eq.get_entity_list():FindDoor(8):SetLockPick(0);
     eq.spawn2(298018, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading()); -- NPC: Zun`Muram_Shaldn_Boc
     eq.depop();
@@ -33,46 +35,59 @@ function ZMSB_Timer(e)
     eq.zone_emote(15,"Zun`Muram Shaldn Boc starts to foam at the mouth as he enters a blind rage.");
     if (e.self:GetHPRatio() >= 90) then
         --need to parse 100% rage
-				e.self:ModifyNPCStat("min_hit", "1520");
+	e.self:ModifyNPCStat("min_hit", "1520");
         e.self:ModifyNPCStat("max_hit", "4850");
         e.self:ModifyNPCStat("attack_delay","16");
     elseif (e.self:GetHPRatio() < 90 and e.self:GetHPRatio() >= 80) then
-				e.self:ModifyNPCStat("min_hit", "1558");
+	e.self:ModifyNPCStat("min_hit", "1558");
         e.self:ModifyNPCStat("max_hit", "4978");
         e.self:ModifyNPCStat("attack_delay","15");
     elseif (e.self:GetHPRatio() < 80 and e.self:GetHPRatio() >= 70) then
-				e.self:ModifyNPCStat("min_hit", "1612");
+	e.self:ModifyNPCStat("min_hit", "1612");
         e.self:ModifyNPCStat("max_hit", "5127");
         e.self:ModifyNPCStat("attack_delay","14");
     elseif (e.self:GetHPRatio() < 70 and e.self:GetHPRatio() >= 60) then
-				e.self:ModifyNPCStat("min_hit", "1666");
+	e.self:ModifyNPCStat("min_hit", "1666");
         e.self:ModifyNPCStat("max_hit", "5276");
         e.self:ModifyNPCStat("attack_delay","13");
     elseif (e.self:GetHPRatio() < 60 and e.self:GetHPRatio() >= 50) then
-				e.self:ModifyNPCStat("min_hit", "1721");
+	e.self:ModifyNPCStat("min_hit", "1721");
         e.self:ModifyNPCStat("max_hit", "5426");
         e.self:ModifyNPCStat("attack_delay","12");
     elseif (e.self:GetHPRatio() < 50 and e.self:GetHPRatio() >= 40) then
-				e.self:ModifyNPCStat("min_hit", "1775");
+	e.self:ModifyNPCStat("min_hit", "1775");
         e.self:ModifyNPCStat("max_hit", "5575");
         e.self:ModifyNPCStat("attack_delay","12");
     elseif (e.self:GetHPRatio() < 40 and e.self:GetHPRatio() >= 30) then
-				e.self:ModifyNPCStat("min_hit", "1829");
+	e.self:ModifyNPCStat("min_hit", "1829");
         e.self:ModifyNPCStat("max_hit", "5724");
         e.self:ModifyNPCStat("attack_delay","11");
     elseif (e.self:GetHPRatio() < 30 and e.self:GetHPRatio() >= 20) then
-				e.self:ModifyNPCStat("min_hit", "1883");
+	e.self:ModifyNPCStat("min_hit", "1883");
         e.self:ModifyNPCStat("max_hit", "5873");
         e.self:ModifyNPCStat("attack_delay","10");
     elseif (e.self:GetHPRatio() < 20 and e.self:GetHPRatio() >= 10) then
-				e.self:ModifyNPCStat("min_hit", "1938");
+	e.self:ModifyNPCStat("min_hit", "1938");
         e.self:ModifyNPCStat("max_hit", "6023");
         e.self:ModifyNPCStat("attack_delay","9");
     elseif (e.self:GetHPRatio() < 10) then
-				e.self:ModifyNPCStat("min_hit", "1992");
+	e.self:ModifyNPCStat("min_hit", "1992");
         e.self:ModifyNPCStat("max_hit", "6172");
         e.self:ModifyNPCStat("attack_delay","8");
-		end
+	end
+elseif (e.timer == "check") then
+		
+		local instance_id = eq.get_zone_instance_id();
+		e.self:ForeachHateList(
+		  function(ent, hate, damage, frenzy)
+			if(ent:IsClient() and ent:GetX() < 293 or ent:GetX() > 448 or ent:GetY() < 270) then
+			  local currclient=ent:CastToClient();
+				--e.self:Shout("You will not evade me " .. currclient:GetName())
+				currclient:MovePCInstance(298,instance_id, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0); -- Zone: tacvi
+				currclient:Message(5,"Zun`Muram Shaldn Boc says, 'You cannot run from your fate, you must accept it.");
+			end
+		  end
+		);
   end
 end
 
@@ -81,6 +96,7 @@ function ZMSB_Hp(e)
   if (e.hp_event == 90) then
     eq.get_entity_list():FindDoor(8):SetLockPick(-1);
     -- he should start checking for players on hate list outside room here
+	eq.set_timer("check", 1 * 1000);
   end
 end
 

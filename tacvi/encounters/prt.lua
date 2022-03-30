f6db2670bb8f6f6d9d0037440227139e43226d6a
huffin <acg1089@aol.com>
2022-03-30 17:53:25 -0500

    Update prt.lua

--- tacvi/encounters/prt.lua
+++ tacvi/encounters/prt.lua
@@ -1,47 +1,4 @@
--- Pixtt_Riel_Tavas
---[[
---Each Golem hits for 2500. Start FD I think
---??and must be tanked both at the same time, and killed within a few minutes of each other.
--- 5 minutes and nothing happened only killing one
--- 
--- Spells:
--- xDelusional Visions from the start.
--- xprocs Wrath of the Ikaav
--- 
--- At 30% she spawns an unstable construct, low hp fast hitting golems who, if killed or left mobile for too long explode into a 3kae.
--- Cataclysm of Stone 4661
--- 
--- At 25% the Pixxt begins her first and only AE.
--- Ikaav's Venom
--- 
--- notes
--- This final 30% is the danger moment.
--- 
--- At 30% the 'An Instable Construct' are spawned and must be contained. To do this,
--- have your Enchanters set up a Circle of Dreams (Word of Morell works too but more resists...) chain,
--- with each CoD landing at just after another Enchanters has landed.
--- The secondary spell should be PBAE stun, single mezz, AE mezz(wake of felicty for example)
--- 
---  26% have Clerics switch to spam heals. Thiis is due to the AE that starts at 25% which has a knockback effect to it
--- 
---  Once the Pixxt is dead, you still have the small matter of the 3k exploding Golems to deal with. Once Pixxt dies,
---  have your raid force sprint back to zone in, leaving in Pixxt room, the Enchanter chain and a wizard or two.
---  Once all celar of the room, have the wizard AE nuke and wipe the Enchanters and himself out.
--- 
---  "You dare enter my chambers and then try to leave? Your punishment will be quite severe"
--- 
---  he says, So it seems you are not so easily defeated after all.  I am through toying with you fools. Prepare for the reality of death.
--- 
---  Riel's body begins to speed up as her attacks become increasingly vicious
--- 
---  he says, You and your friends are starting to annoy me.  Come forth my little experiments.  Choose one of these fools and show them the surprise you have waiting.
--- 
---  Thats it!  You have past the point of being bothersome. I grow weary of this encounter. It is time for it to end
--- 
--- 
---  Riel's body falls to the stone floor in a puddle of blackened blood. You step back as she slashes one last time, connecting with nothing but the stale air of the room. 'This is not over. My commander will destroy you for this and when he does I hope it is my power he is weilding'
--- 
---]]
+
 
 local golems_spawn = false;
 ---
@@ -84,12 +41,14 @@ function PRT_HP(e)
     --lock door behind her
     eq.get_entity_list():FindDoor(23):SetLockPick(-1)
     eq.set_next_hp_event(50)
+	eq.set_timer("check", 1 * 1000); -- start checking for clients outside room
+	eq.zone_emote(0,"Riel raises her hands to the sky and laughs as the door behind you seals itself.");
 
   elseif (e.hp_event == 50) then
-    -- Hits a little harder and Flurry
-    --quest::modifynpcstat("max_hit",4500);
+    --add flurry, reduce atk delay
+    e.self:ModifyNPCStat("attack_delay","12");
     e.self:SetSpecialAbility(SpecialAbility.flurry, 1)
-    e.self:ModifyNPCStat("max_hit",tostring(e.self:GetMaxDMG()*1.1))
+    --e.self:ModifyNPCStat("max_hit",tostring(e.self:GetMaxDMG()*1.1))
     e.self:Say("So it seems you are not so easily defeated after all. I am through toying with you fools. Prepare for the reality of death.' Riel's body begins to speed up as her attacks become increasingly vicious")
     e.self:Emote("'s body begins to speed up as her attacks become increasingly vicious")
     eq.set_next_hp_event(30)
@@ -151,7 +110,19 @@ function PRT_Timer(e)
     eq.depop();
 
     eq.spawn2(298032, 0, 0, 202.0, -586.0, -4.125, 380); -- NPC: Pixtt_Riel_Tavas
-
+elseif (e.timer == "check") then
+		
+		local instance_id = eq.get_zone_instance_id();
+		e.self:ForeachHateList(
+		  function(ent, hate, damage, frenzy)
+			if(ent:IsClient() and ent:GetX() < 100 or ent:GetX() > 244 or ent:GetY() < 192 or ent:GetY() > -535 or ent:GetY() < -636) then
+			  local currclient=ent:CastToClient();
+				--e.self:Shout("You will not evade me " .. currclient:GetName())
+				currclient:MovePCInstance(298,instance_id, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0); -- Zone: tacvi
+				currclient:Message(5,"Pixtt Riel Tavas says, 'You dare enter my chambers and then try to leave? Your punishment will be quite severe.");
+			end
+		  end
+		);
   end
 end
 

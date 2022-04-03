262fc2994757ad6af35ab9f79b5f06c6225f5122
huffin <acg1089@aol.com>
2022-04-03 14:52:55 -0500

    Update zmyv.lua

--- tacvi/encounters/zmyv.lua
+++ tacvi/encounters/zmyv.lua
@@ -7,11 +7,11 @@ end
 function ZMYV_Combat(e)
   if (e.joined == true) then
     e.self:Say("The weak willed and the idle will serve my cause.");
-    eq.set_timer("allure", 90 * 1000);
+    --eq.set_timer("allure", 90 * 1000);
   else
     eq.set_timer("wipecheck", 1 * 1000);
     eq.stop_timer("check");
-		eq.stop_timer("allure");
+	--eq.stop_timer("allure");
   end
 end
 

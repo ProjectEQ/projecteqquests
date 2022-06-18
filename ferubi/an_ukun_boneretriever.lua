d8b0b05687c1639f80c463555265a228e42be409
huffin <acg1089@aol.com>
2022-06-17 18:22:24 -0600

    Update an_ukun_boneretriever.lua

--- ferubi/an_ukun_boneretriever.lua
+++ ferubi/an_ukun_boneretriever.lua
@@ -6,3 +6,14 @@ function event_death_complete(e)
         eq.signal(284110,3); --signal Event_Spawner to add to hammermaster counter
     end
 end
+
+--ukun mods
+
+function event_spawn(e)
+e.self:ModSkillDmgTaken(0, 10); -- 1h blunt
+e.self:ModSkillDmgTaken(2, 10); -- 2h blunt
+e.self:ModSkillDmgTaken(36,-10); -- piercing
+e.self:ModSkillDmgTaken(77,-10); -- 2h piercing
+e.self:ModSkillDmgTaken(28, 10) -- hand to hand
+e.self:ModSkillDmgTaken(7, 10); -- archery
+end

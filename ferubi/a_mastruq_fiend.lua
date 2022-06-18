e7e5fbcee6f5f56c063f1fd24011e81c95e50a81
huffin <acg1089@aol.com>
2022-06-17 18:27:08 -0600

    Update a_mastruq_fiend.lua

--- ferubi/a_mastruq_fiend.lua
+++ ferubi/a_mastruq_fiend.lua
@@ -6,3 +6,13 @@ function event_death_complete(e)
         eq.signal(284110,2); --signal Event_Spawner to add to spearmaster counter
     end
 end
+
+--mastruq mods
+
+function event_spawn(e)
+e.self:ModSkillDmgTaken(0, -20); -- 1h blunt
+e.self:ModSkillDmgTaken(2, -20); -- 2h blunt
+e.self:ModSkillDmgTaken(1, 20); -- 1h slashing
+e.self:ModSkillDmgTaken(3, 20); -- 2h slashing
+e.self:ModSkillDmgTaken(7, 20); -- archery
+end

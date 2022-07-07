6eda97e4555df545955972da6de1f9984127eb59
huffin <acg1089@aol.com>
2022-07-07 15:02:52 -0600

    Update a_quivering_egg.lua

--- wallofslaughter/a_quivering_egg.lua
+++ wallofslaughter/a_quivering_egg.lua
@@ -4,6 +4,8 @@ end
 
 function event_timer(e)
 	if (e.timer == "hatch") then
+		e.self:Emote("shatters as several small murkgliders emerge.");
+		
 		eq.spawn2(300084, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
 		eq.spawn2(300084, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
 		-- 2 a_murk_hatchling per egg

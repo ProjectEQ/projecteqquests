3d05c14167df2e0115cdd639421d696f34547bae
huffin <acg1089@aol.com>
2022-07-07 14:52:54 -0600

    Update a_quivering_egg.lua

--- wallofslaughter/a_quivering_egg.lua
+++ wallofslaughter/a_quivering_egg.lua
@@ -1,11 +1,12 @@
 function event_spawn(e)
-eq.set_timer("hatch", 30 * 1000);
+	eq.set_timer("hatch", 30 * 1000);
 end
 
 function event_timer(e)
-if (e.timer == "hatch") then
-eq.spawn2(300084, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
---NPC: a_murk_hatchling
-eq.depop();
-end
+	if (e.timer == "hatch") then
+		eq.spawn2(300084, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
+		eq.spawn2(300084, 0, 0, e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
+		-- 2 a_murk_hatchling per egg
+		eq.depop();
+	end
 end

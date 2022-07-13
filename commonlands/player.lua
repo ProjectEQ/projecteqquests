4d837700018d13590154ca46d2572984b5f1213d
Trent <trentdm@gmail.com>
2022-07-12 22:13:41 -0600

    fix move method

--- commonlands/player.lua
+++ commonlands/player.lua
@@ -2,6 +2,6 @@ function event_enter_zone(e)
   local client = eq.get_entity_list():GetClientByID(e.self:GetID());
   
   if(($client->GetClientVersionBit() & 3) != 0) { #062/Titanium
-    client:MovePCInstance(22, -144, -1543, 2, 244); -- Zone: ecommons
+    client:MovePC(22, -144, -1543, 2, 244); -- Zone: ecommons
   }
 end

678fb05a493e724413e552daaee812734c939412
Trent <trentdm@gmail.com>
2022-07-12 22:51:26 -0600

    fix syntax and rof2 client comparison

--- commonlands/player.lua
+++ commonlands/player.lua
@@ -1,7 +1,8 @@
 function event_enter_zone(e)
   local client = eq.get_entity_list():GetClientByID(e.self:GetID());
   
-  if((client.GetClientVersionBit() & 3) != 0) { -- 062/Titanium
-    client:MovePCInstance(22, -144, -1543, 2, 244); -- Zone: ecommons
-  }
+-- If not Rof2 client; move them to east commonland ldon camp. Commons not yet implemented
+  if (bit.band(client:GetClientVersionBit(), 4294967264) == 0) then
+    client.MovePC(22, -144, -1543, 2, 244); -- Zone: ecommons
+  end
 end

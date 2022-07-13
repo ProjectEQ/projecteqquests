4f5d60f6252e332548aaa6516e133eebc93c2f8b
Trent <trentdm@gmail.com>
2022-07-12 22:17:07 -0600

    Update player.lua

--- commonlands/player.lua
+++ commonlands/player.lua
@@ -1,7 +1,7 @@
 function event_enter_zone(e)
   local client = eq.get_entity_list():GetClientByID(e.self:GetID());
   
-  if(($client->GetClientVersionBit() & 3) != 0) { #062/Titanium
-    client:MovePC(22, -144, -1543, 2, 244); -- Zone: ecommons
+  if((client.GetClientVersionBit() & 3) != 0) { #062/Titanium
+    client:MovePCInstance(22, -144, -1543, 2, 244); -- Zone: ecommons
   }
 end

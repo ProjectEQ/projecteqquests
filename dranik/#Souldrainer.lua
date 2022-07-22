7ccd308f5aaca014620e91ee3ac05c17e758d51e
huffin <acg1089@aol.com>
2022-07-22 07:58:19 -0600

    Update #Souldrainer.lua

--- dranik/#Souldrainer.lua
+++ dranik/#Souldrainer.lua
@@ -1,7 +1,7 @@
 -- will despawn while in combat if timer is met
 
 function event_spawn(e)
-  eq.set_timer("depop", 1200 * 1000); -- 20 min guess for now
+  eq.set_timer("depop", 2700 * 1000); -- 45 min uptime
 end
 
 

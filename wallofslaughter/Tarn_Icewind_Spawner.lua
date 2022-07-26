6dc79cdcb902ff76513120c319275e659ccb853f
huffin <acg1089@aol.com>
2022-07-26 07:39:37 -0600

    Update Tarn_Icewind_Spawner.lua

--- wallofslaughter/Tarn_Icewind_Spawner.lua
+++ wallofslaughter/Tarn_Icewind_Spawner.lua
@@ -11,7 +11,7 @@ function event_signal(e)
   	if (ice_crystal == 4) then
     	eq.depop_all(300023);
     	eq.zone_emote(0, "The crystals pulse with energy as a creature steps from the portal.");
-    	eq.spawn2(300063, 0, 0, 554,138,-6,510); -- NPC: #Tarn_Icewind
+    	eq.unique_spawn(300063, 0, 0, 554,138,-6,510); -- NPC: #Tarn_Icewind
     	eq.spawn2(300086, 0, 0, 634, 64, 4.22,0); -- NPC: #a_portal_crystal
     	eq.spawn2(300086, 0, 0, 631, 216, 4.22,0); -- NPC: #a_portal_crystal
     	eq.spawn2(300086, 0, 0, 483, 211, 4.22,0); -- NPC: #a_portal_crystal

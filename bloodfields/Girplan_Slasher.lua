89698f6f4dc4cb288bf5cae4069f11cb66dc4651
huffin <acg1089@aol.com>
2022-07-27 07:48:11 -0600

    Update Girplan_Slasher.lua

--- bloodfields/Girplan_Slasher.lua
+++ bloodfields/Girplan_Slasher.lua
@@ -1,6 +1,6 @@
 --Warrior Epic 1.5
 function event_death_complete(e)
-	if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(301027) == false) then -- check other slashers
+	if (eq.get_entity_list():IsMobSpawnedByNpcTypeID(301027) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(301063) == false) then -- check other slashers
 		eq.unique_spawn(301063  ,0,0,835,-650,-755,60);	-- NPC: Girplan Devourer
 		eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"A massive girplan devourer emerges nearby to challenge those who have destroyed its kind.");
 	elseif (eq.get_entity_list():IsMobSpawnedByNpcTypeID(301027) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(301063) == true) then -- check other slashers and devourer

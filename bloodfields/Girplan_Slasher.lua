0ef0fb28cfc2a724815525c704f04efea4064ccc
huffin <acg1089@aol.com>
2022-07-27 09:29:12 -0600

    Update Girplan_Slasher.lua

--- bloodfields/Girplan_Slasher.lua
+++ bloodfields/Girplan_Slasher.lua
@@ -5,5 +5,6 @@ function event_death_complete(e)
 		eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"A massive girplan devourer emerges nearby to challenge those who have destroyed its kind.");
 	elseif (eq.get_entity_list():IsMobSpawnedByNpcTypeID(301027) == false and eq.get_entity_list():IsMobSpawnedByNpcTypeID(301063) == true) then -- check other slashers and devourer
 		eq.spawn2(301085  ,0,0,835,-650,-755,60);	-- NPC: Girplan_Lacerator (301085)
+		eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 0, 100,"There is no sign of the girplan devourer, but a lesser lacerator has appeared nearby instead.");
 	end
 end

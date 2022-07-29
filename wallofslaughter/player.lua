a3f6750bc075c674eb35d2b2f6bb17efcb274859
huffin <acg1089@aol.com>
2022-07-29 13:54:13 -0600

    Update player.lua
    add qglobal flag requirement for Stone of Eternal Power: Northeast Eye and  Fluxing Rod

--- wallofslaughter/player.lua
+++ wallofslaughter/player.lua
@@ -23,15 +23,23 @@ end
 function event_loot(e)
 	if(e.self:Class() == "Warrior" and e.item:GetID() == 60312) then -- Stone of Eternal Power: Northeast Eye
 		local qglobals = eq.get_qglobals(e.self);
-		if(qglobals["warrior_epic"] == "10" and qglobals["warr_epic_wos"] == nil ) then
-			eq.spawn2(283157,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- #a chest (Epic 1.5)
-			eq.set_global("warr_epic_wos","1",5,"F");
+		if(qglobals["warrior_epic"] == "10") then
+			if (qglobals["warr_epic_wos"] == nil ) then
+				eq.spawn2(283157,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- #a chest (Epic 1.5)
+				eq.set_global("warr_epic_wos","1",5,"F");
+			end
+		else
+			return 1;
 		end
 	elseif(e.self:Class() == "Wizard" and e.item:GetID() == 11445) then  --Fluxing Rod
 		local qglobals = eq.get_qglobals(e.self);
-		if(qglobals["wiz_epic"] == "1" and qglobals["wiz_wos_chest"] == nil) then
-			eq.spawn2(283157,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- #a chest (Epic 1.5)
-			eq.set_global("wiz_wos_chest","1",5,"F");
+		if(qglobals["wiz_epic"] == "1") then
+			if (qglobals["wiz_wos_chest"] == nil) then
+				eq.spawn2(283157,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- #a chest (Epic 1.5)
+				eq.set_global("wiz_wos_chest","1",5,"F");
+			end
+		else
+			return 1;
 		end
 	elseif(e.self:Class() == "Paladin" and e.item:GetID() == 69971) then
 		local qglobals = eq.get_qglobals(e.self);

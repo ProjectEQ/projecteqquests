ba6e1f8107f252b71960c4d5ad37a5e2f86852a4
huffin <acg1089@aol.com>
2022-09-13 15:26:25 -0600

    Update Zov_Va_Liako.lua

--- vexthal/Zov_Va_Liako.lua
+++ vexthal/Zov_Va_Liako.lua
@@ -1,9 +1,34 @@
+--Vex Thal Shade Reanimation Script on Death  (Note  Va Liakos(SKs) respawn as Liakos (paladins) in shade form
+
+function event_timer(e)
+    if e.timer == 'depop' then
+        eq.stop_timer(e.timer);
+        eq.depop();
+    end
+end
+
 function event_death_complete(e)
-local xloc = e.self:GetX();
-local yloc = e.self:GetY();
-local zloc = e.self:GetZ();
-local heading = e.self:GetHeading();
-local add=eq.ChooseRandom(158119,158120,158121,158122,158123,158124)
-eq.spawn2(add,0,0,xloc,yloc,zloc,heading);
-eq.debug("death: " .. e.self:GetName() .. " add: " .. add);
+   local ran = math.random(1, 100);
+   local mob;
+   local x, y, z , h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
+   if ran <= 30 then
+      --qua 
+      mob = 158019;
+   elseif ran <= 50 then
+      --zov
+      mob = 158055;
+   elseif ran <= 70 then
+      --zun
+      mob = 158071;
+   elseif ran <= 85 then
+      --pli
+      mob = 158037;
+   else
+      -- eom
+      mob = 158073;
+   end
+
+   local spawned = eq.spawn2(mob, 0, 0, x, y, z, h);
+   eq.set_timer('depop', 30 * 60 * 1000, spawned);
+    
 end

2197c7f8fd8f2ac24332fed4fecf4a8f4912427e
huffin <acg1089@aol.com>
2022-09-18 08:43:16 -0600

    Update Guard_Kliknaw.lua

--- hollowshade/Guard_Kliknaw.lua
+++ hollowshade/Guard_Kliknaw.lua
@@ -32,7 +32,7 @@ function event_trade(e)
     local item_lib = require("items");
     
     if quests then
-        if item_lib.check_turn_in(e.self, e.trade, {item1 = 6220}) then -- Daily Status Report (2)
+        if item_lib.check_turn_in(e.trade, {item1 = 6220}) then -- Daily Status Report (2)
             e.self:Emote("yells, 'Head count! Head count time!' A moment later he says 'I'm pretty sure that they're all here. Just take this back and act like your didn't just see that.' He winks and hands you the report.");
             e.other:Faction( 132,10 ); -- Guardians of Shar Vahl
             e.other:QuestReward(e.self,0,0,0,0,6221,1000); -- Daily Status Report (3)

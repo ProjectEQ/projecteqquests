f4cf34b293ed2db929b2b7026f14757953cd215f
Michael Cook (mackal) <277429+mackal@users.noreply.github.com>
2022-09-24 11:39:49 -0400

    Add some missing return_items

--- greatdivide/Gavon_Morant.lua
+++ greatdivide/Gavon_Morant.lua
@@ -10,18 +10,19 @@
 
 function event_trade(e)
     local item_lib = require("items");
-        
-        if (item_lib.check_turn_in(e.trade, {item1 = 56010})) then --Check for "Talisman of the Plasmatic Priests"
-			e.other:SummonItem(56010); --return "Talisman of the Plasmatic Priests"
-            -- Spawn High Priest Maltan
-            eq.spawn2(118211, 0, 0, -489, -6115, -557, 0); -- NPC: High_Priest_Maltan
-            -- Spawn 2 A Plasmatic Priest
-            eq.spawn2(27129, 0, 0, -510, -6104, -557, 124); -- NPC: A_Plasmatic_Priest
-            eq.spawn2(27129, 0, 0, -501, -6085, -555, 204); -- NPC: A_Plasmatic_Priest
-            -- Spawn 3 A Plasmatic Priestess
-            eq.spawn2(27130, 0, 0, -476, -6084, -555, 296); -- NPC: A_Plasmatic_Priestess
-            eq.spawn2(27130, 0, 0, -469, -6104, -557, 376); -- NPC: A_Plasmatic_Priestess
-            eq.depop_with_timer();
-        end
-		
+
+    if (item_lib.check_turn_in(e.trade, {item1 = 56010})) then --Check for "Talisman of the Plasmatic Priests"
+        e.other:SummonItem(56010); --return "Talisman of the Plasmatic Priests"
+        -- Spawn High Priest Maltan
+        eq.spawn2(118211, 0, 0, -489, -6115, -557, 0); -- NPC: High_Priest_Maltan
+        -- Spawn 2 A Plasmatic Priest
+        eq.spawn2(27129, 0, 0, -510, -6104, -557, 124); -- NPC: A_Plasmatic_Priest
+        eq.spawn2(27129, 0, 0, -501, -6085, -555, 204); -- NPC: A_Plasmatic_Priest
+        -- Spawn 3 A Plasmatic Priestess
+        eq.spawn2(27130, 0, 0, -476, -6084, -555, 296); -- NPC: A_Plasmatic_Priestess
+        eq.spawn2(27130, 0, 0, -469, -6104, -557, 376); -- NPC: A_Plasmatic_Priestess
+        eq.depop_with_timer();
     end
+    item_lib.return_items(e.self, e.other, e.trade)
+end
+
--- lavastorm/Laura_Rako.lua
+++ lavastorm/Laura_Rako.lua
@@ -10,19 +10,20 @@
 
 function event_trade(e)
     local item_lib = require("items");
-        
-        if (item_lib.check_turn_in(e.trade, {item1 = 56010})) then --Check for "Talisman of the Plasmatic Priests"
-			e.other:SummonItem(56010); --return "Talisman of the Plasmatic Priests"
-            -- Spawn High Priestess Shima
-            eq.spawn2(27128, 0, 0, 113, 1672, 56, 462); -- NPC: High_Priestess_Shima
-            -- Spawn 2 A Plasmatic Priest
-            eq.spawn2(27129, 0, 0, 132, 1669, 55, 0); -- NPC: A_Plasmatic_Priest
-            eq.spawn2(27129, 0, 0, 89, 1672, 55, 40); -- NPC: A_Plasmatic_Priest
-            -- Spawn 3 A Plasmatic Priestess
-            eq.spawn2(27130, 0, 0, 113, 1650, 56, 502); -- NPC: A_Plasmatic_Priestess
-            eq.spawn2(27130, 0, 0, 95, 1706, 54, 206); -- NPC: A_Plasmatic_Priestess
-            eq.spawn2(27130, 0, 0, 125, 1709, 55, 274); -- NPC: A_Plasmatic_Priestess
-            eq.depop_with_timer();
-        end
-		
+
+    if (item_lib.check_turn_in(e.trade, {item1 = 56010})) then --Check for "Talisman of the Plasmatic Priests"
+        e.other:SummonItem(56010); --return "Talisman of the Plasmatic Priests"
+        -- Spawn High Priestess Shima
+        eq.spawn2(27128, 0, 0, 113, 1672, 56, 462); -- NPC: High_Priestess_Shima
+        -- Spawn 2 A Plasmatic Priest
+        eq.spawn2(27129, 0, 0, 132, 1669, 55, 0); -- NPC: A_Plasmatic_Priest
+        eq.spawn2(27129, 0, 0, 89, 1672, 55, 40); -- NPC: A_Plasmatic_Priest
+        -- Spawn 3 A Plasmatic Priestess
+        eq.spawn2(27130, 0, 0, 113, 1650, 56, 502); -- NPC: A_Plasmatic_Priestess
+        eq.spawn2(27130, 0, 0, 95, 1706, 54, 206); -- NPC: A_Plasmatic_Priestess
+        eq.spawn2(27130, 0, 0, 125, 1709, 55, 274); -- NPC: A_Plasmatic_Priestess
+        eq.depop_with_timer();
     end
+    item_lib.return_items(e.self, e.other, e.trade)
+end
+

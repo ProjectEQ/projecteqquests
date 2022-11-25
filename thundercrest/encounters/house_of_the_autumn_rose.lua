6e2bb5414fa359852790cd0e927197c9f79bf378
hg <4683435+hgtw@users.noreply.github.com>
2022-11-25 09:56:30 -0500

    Fix assassin spawns in House of the Autumn Rose
    Use spawngroup id for Autumn Rose assassin traps and combat aggro to
    trigger them instead of proximity.
    
    Offset z location of assassin spawns which breaks on emu since they get
    stuck in the ceiling
    
    Remove the previous npc script and just uses the encounter

--- thundercrest/encounters/house_of_the_autumn_rose.lua
+++ thundercrest/encounters/house_of_the_autumn_rose.lua
@@ -1,21 +1,19 @@
-local function ambush_spawn(e)
-  local x, y, z = e.self:GetX(), e.self:GetY(), e.self:GetZ()
-  eq.set_proximity(x - 100, x + 100, y - 100, y + 100, z - 100, z + 100) -- should be los aggro
-end
+local function ambush_combat(e)
+  if e.joined then
+    eq.zone_emote(MT.Yellow, "Hidden panels open from the ceiling as a pair of assassins drop from above.")
+    eq.depop()
 
-local function ambush_proximity(e)
-  eq.zone_emote(MT.Yellow, "Hidden panels open from the ceiling as a pair of assassins drop from above.")
-  eq.depop()
-
-  if e.self:CalculateDistance(-2, 482, 181) < 50 then -- entrance
-    eq.spawn2(340502, 0, 0, 0.0,    657.0,  176.0, 244.0) -- an_Autumn_Rose_assassin
-    eq.spawn2(340502, 0, 0, 13.0,   419.0,  231.0, 510.0)
-  elseif e.self:CalculateDistance(308, 160, 227) < 50 then -- west hallway
-    eq.spawn2(340502, 0, 0, 307.0,  108.0,  281.0, 8.0)
-    eq.spawn2(340502, 0, 0, 310.0,  162.0,  287.0, 294.0)
-  elseif e.self:CalculateDistance(-131, -489, 295) < 50 then -- dojo leading to bridge
-    eq.spawn2(340502, 0, 0, -215.0, -486.0, 384.0, 126.0)
-    eq.spawn2(340502, 0, 0, -54.0,  -483.0, 386.0, 373.0)
+    -- all z positions offset from live values to fix getting stuck in ceiling
+    if e.self:GetSp2() == 280371 then -- entrance
+      eq.spawn2(340502, 0, 0, 0.0,    657.0,  156.0, 244.0) -- an_Autumn_Rose_assassin
+      eq.spawn2(340502, 0, 0, 13.0,   419.0,  211.0, 510.0)
+    elseif e.self:GetSp2() == 280392 then -- west hallway
+      eq.spawn2(340502, 0, 0, 307.0,  108.0,  261.0, 8.0)
+      eq.spawn2(340502, 0, 0, 310.0,  162.0,  267.0, 294.0)
+    elseif e.self:GetSp2() == 280440 then -- dojo leading to bridge
+      eq.spawn2(340502, 0, 0, -215.0, -486.0, 334.0, 126.0)
+      eq.spawn2(340502, 0, 0, -54.0,  -483.0, 336.0, 373.0)
+    end
   end
 end
 
@@ -25,7 +23,6 @@ local function poison_brewer_death(e)
 end
 
 function event_encounter_load(e)
-  eq.register_npc_event(Event.spawn,          340501, ambush_spawn) -- Invis_Man_Ambush_Trap
-  eq.register_npc_event(Event.enter,          340501, ambush_proximity)
+  eq.register_npc_event(Event.combat,         340501, ambush_combat) -- Invis_Man_Ambush_Trap
   eq.register_npc_event(Event.death_complete, 340506, poison_brewer_death) -- Poison_Brewer_Ishi
 end

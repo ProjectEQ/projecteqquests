0f15a5887087bdcf4d6f6e7cdf8f3772b0758cdd
hg <4683435+hgtw@users.noreply.github.com>
2022-11-29 18:32:56 -0500

    Use aggro not proximity in Tracking the Kirin
    Remove the non-encounter footprints script

--- stillmoona/Unusual_footprints.lua
+++
@@ -1,6 +0,0 @@
-function event_combat(e)
-	if (e.joined == true) then
-		e.self:Emote("can be seen on the ground before you. Perhaps if you keep searching you will find more.");
-	  eq.depop_with_timer();
-	end
-end

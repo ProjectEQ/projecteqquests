fa7452723b22c399eb4a5d158f3bab0fc1bc8fca
catapultam-habeo <catapultam@pyrelight.net>
2022-11-29 17:54:08 -0600

    format

--- nexus/Magus_Asorin_Obine.pl
+++ nexus/Magus_Asorin_Obine.pl
@@ -39,12 +39,7 @@ sub EVENT_SAY {
 sub POPUP_DISPLAY {
     my $Break = plugin::PWBreak();
     
-    quest::popup('', "<c \"#FF0000\">The Rules:</c>
-                      <br>
-                      1. Don't be a dick. (Don't be unnecessarily rude or vulgar in global channels. Don't go out of your way to make other players have a bad time.)<br>
-                      2. MQ2 is not allowed. The server files include several MQ2 features, with potentially more to come.<br>
-                      <br>$Break<br>
-                      Pyrelight is a solo-balanced progression server, meant to offer a challenging experience for adventurers willing to develop both their character and their personal skills.
+    quest::popup('', "Pyrelight is a solo-balanced progression server, meant to offer a challenging experience for adventurers willing to develop both their character and their personal skills.
                       <br>
                       Progression does not exactly map to original expansion releases, but focuses more on level and overall difficulty. Currently, progression through ROUGHLY <c \"#00FFFF\">Scars of Velious</c> is available, though several Luclin zones are available and Veeshan's Peak and the Classic planes are not. This is <c \"#00FFFF\">Tier 1</c>, and levels are capped at <c \"#00FFFF\">60</c>. Additional progression tiers will be added in the future.
                       <br>$Break<br>              

f4d49edf67102bb7d8f8332479ce039f4da13b46
catapultam-habeo <catapultam@pyrelight.net>
2022-11-29 18:01:01 -0600

    better

--- nexus/Lyrei_Helemaer.pl
+++ nexus/Lyrei_Helemaer.pl
@@ -19,15 +19,12 @@ sub EVENT_SAY {
     if (quest::get_data($MAOcharKey) <= 0) {
       plugin::NPCTell("Hail, traveler. You'll need to get permission to access the Nexus teleportation network. Speak to Magus Asorin on the central platform.");
     } else {
-      plugin::NPCTell("Hail, traveler. I can transport you to the Great Spires of Faydwer. Speak to Magus Obine on the central spire if you would like to travel to other locations.");
-    }
-    $client->Message(257, " ------- Select a Destination ------- ");
-    
-    foreach my $t (sort keys %teleport_zones) {
-      $client->Message(257, "-> ".quest::saylink($teleport_zones{$t}[1],0,$t));
-    }
-    
-    
+      plugin::NPCTell("Hail, traveler. I can transport you to the Great Spires of Faydwer. Speak to Magus Obine on the central spire if you would like to travel to other locations.");    
+      $client->Message(257, " ------- Select a Destination ------- ");      
+      foreach my $t (sort keys %teleport_zones) {
+        $client->Message(257, "-> ".quest::saylink($teleport_zones{$t}[1],0,$t));
+      }
+    }    
   } elsif (exists($teleport_zones{$text}[1])) {
     $client->MovePC(quest::GetZoneID($teleport_zones{$text}[0]),$teleport_zones{$text}[2],$teleport_zones{$text}[3],$teleport_zones{$text}[4],$teleport_zones{$text}[5]);
   }

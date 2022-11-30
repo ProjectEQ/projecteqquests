e798bb5cd15840b38c0486100185a33bb3e018a2
catapultam-habeo <catapultam@pyrelight.net>
2022-11-29 18:56:05 -0600

    typo

--- bazaar/Fedoro_the_Mysterious.pl
+++ bazaar/Fedoro_the_Mysterious.pl
@@ -12,7 +12,7 @@ sub EVENT_SAY {
     } elsif ($text=~/cad1a/i) { #I'm right
         plugin::NPCTell("Perfect! I have a unique talent - I can grant you the ability to shift your spirit, allowing you to pick up a different set of skills. You'll be as weak as 
                         newborn kitten at first, but only at first. Better yet, as you flex your new skills and regain experience, you'll find that many of your new abilities will carry
-                        over to different aspects of your spirit. I can open up new [". quest::saylink("Classes",1) ."] for you to explore, but you'll need to some [". quest::saylink("favors",1) .
+                        over to different aspects of your spirit. I can open up new [". quest::saylink("Classes",1) ."] for you to explore, but you'll need to do some [". quest::saylink("favors",1) .
                         "] for me, first.");
     } 
 }

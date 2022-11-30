624d097d3a43d38bd15a57640c16f77e151a612c
catapultam-habeo <catapultam@pyrelight.net>
2022-11-30 09:50:17 -0600

    debug

--- global/spells/17779.pl
+++ global/spells/17779.pl
@@ -1,8 +1,10 @@
 sub EVENT_SPELL_EFFECT_CLIENT {
+    quest::debug("started warrior class change");
     if ($client->GetClassName() neq "Warrior") {
         $client->BuffFadeAll();
         quest::permaclass(1);
     } else {
         $client->Message(13, "Ability Failed. You are already a Warrior.");
     }
+    quest::debug("Completed warrior class change");
 }

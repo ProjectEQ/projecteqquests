1093a435c5342747a3b855c87a8aae1856c0ab44
Chris Miles <akkadius1@gmail.com>
2023-01-12 19:20:40 -0600

    Push up changes

--- lua_modules/items.lua
+++ lua_modules/items.lua
@@ -7,6 +7,26 @@ function items.check_turn_in(trade, trade_check)
 	for key, value in pairs(trade) do
 		trade_return[key] = value;
 	end
+
+	-- Handin table for source
+	local handin_data = {};
+	for x = 1, 4 do
+		local inst = trade["item" .. x];
+		if (inst.valid) then
+			local is_attuned = 0;
+			if inst:IsInstNoDrop() then
+				is_attuned = 1;
+			end
+
+			handin_data[x] = string.format("%d|%d|%d", inst:GetID(), inst:GetCharges(), is_attuned);
+		else
+			handin_data[x] = "0|0|0";
+		end
+	end
+
+	trade.other:SetEntityVariable("HANDIN_ITEMS", items.get_handin_items_serialized(handin_data))
+
+	trade.other:SetEntityVariable("HANDIN_MONEY", string.format("%d-%d-%d-%d", trade.copper, trade.silver, trade.gold, trade.platinum))
 	
 	--for every item in trade_check check trade_return
 		--if item exists in trade_return then 
@@ -83,16 +103,27 @@ end
 function items.return_items(npc, client, trade, text)
 	text = text or true;
 	local returned = false;
+
+	-- Handin table for source
+	local return_data = {};
 	for i = 1, 4 do
 		local inst = trade["item" .. i];
 		if(inst.valid) then
 			-- remove delivered task items from return for this slot
 			local return_count = inst:RemoveTaskDeliveredItems()
+<<<<<<< Updated upstream
+=======
+			local is_attuned = 0;
+			if inst:IsInstNoDrop() then
+				is_attuned = 1;
+			end
+>>>>>>> Stashed changes
 
 			if(eq.is_disc_tome(inst:GetID()) and npc:GetClass() > 19 and npc:GetClass() < 36) then
 				if(client:GetClass() == npc:GetClass() - 19) then
 					client:TrainDisc(inst:GetID());
 				else
+					return_data[i] = string.format("%d|%d|%d", inst:GetID(), inst:GetCharges(), is_attuned);
 					npc:Say(string.format("You are not a member of my guild. I will not train you!"));
 					if return_count > 0 then
 						client:PushItemOnCursor(inst);
@@ -100,6 +131,10 @@ function items.return_items(npc, client, trade, text)
 					end
 				end
 			elseif return_count > 0 then
+<<<<<<< Updated upstream
+=======
+				return_data[i] = string.format("%d|%d|%d", inst:GetID(), inst:GetCharges(), is_attuned);
+>>>>>>> Stashed changes
 				client:PushItemOnCursor(inst);
 				if(text == true) then
 					npc:Say(string.format("I have no need for this %s, you can have it back.", client:GetCleanName()));
@@ -108,6 +143,8 @@ function items.return_items(npc, client, trade, text)
 			end
 		end
 	end
+
+	client:SetEntityVariable("RETURN_ITEMS", items.get_handin_items_serialized(return_data))
 	
 	local money = false;
 	if(trade.platinum ~= 0) then
@@ -133,6 +170,8 @@ function items.return_items(npc, client, trade, text)
 	if(money == true) then
 		client:AddMoneyToPP(trade.copper, trade.silver, trade.gold, trade.platinum, true);
 	end
+
+	eq.send_player_handin_event();
 	
 	return returned;
 end
@@ -168,6 +207,7 @@ function items.return_bot_items(bot, client, trade, text)
 		returned = true;
 		money = true;
 	end
+<<<<<<< Updated upstream
 	
 	if (money) then
 		client:AddMoneyToPP(trade.copper, trade.silver, trade.gold, trade.platinum, true);
@@ -265,3 +305,106 @@ function items.check_bot_turn_in(trade, trade_check)
 end
 
 return items;
+=======
+	
+	if (money) then
+		client:AddMoneyToPP(trade.copper, trade.silver, trade.gold, trade.platinum, true);
+	end
+
+	if (text and returned) then
+		bot:OwnerMessage("I have no need for this, you can have it back.")
+	end
+	
+	return returned;
+end
+
+function items.check_bot_turn_in(trade, trade_check)
+	--create trade_return table == trade
+	--shallow copy
+	local trade_return = {};
+	for key, value in pairs(trade) do
+		trade_return[key] = value;
+	end
+	
+	--for every item in trade_check check trade_return
+		--if item exists in trade_return then 
+			--remove that item from trade_return
+		--else
+			--failure
+	for i = 1, 8 do
+		local key = "item" .. i;
+		if(trade_check[key] ~= nil and trade_check[key] ~= 0) then
+			local found = false;
+			for j = 1, 8 do
+				local inst = trade_return["item" .. j];			
+				if(inst.valid and trade_check[key] == inst:GetID()) then
+					trade_return["item" .. j] = ItemInst();
+					found = true;
+					break;
+				end
+			end
+			
+			if(not found) then
+				return false;
+			end
+		end
+	end
+	
+	--convert our money into copper then check that we have enough then convert change back
+	local trade_check_money = 0;
+	local return_money = 0;
+	
+	if(trade_check["platinum"] ~= nil and trade_check["platinum"] ~= 0) then
+		trade_check_money = trade_check_money + (trade_check["platinum"] * 1000);
+	end
+	
+	if(trade_check["gold"] ~= nil and trade_check["gold"] ~= 0) then
+		trade_check_money = trade_check_money + (trade_check["gold"] * 100);
+	end
+	
+	if(trade_check["silver"] ~= nil and trade_check["silver"] ~= 0) then
+		trade_check_money = trade_check_money + (trade_check["silver"] * 10);
+	end
+	
+	if(trade_check["copper"] ~= nil and trade_check["copper"] ~= 0) then
+		trade_check_money = trade_check_money + trade_check["copper"];
+	end
+	
+	return_money = return_money + trade_return["platinum"] * 1000 + trade_return["gold"] * 100;
+	return_money = return_money + trade_return["silver"] * 10 + trade_return["copper"];
+	
+	if(return_money < trade_check_money) then
+		return false;
+	else
+		return_money = return_money - trade_check_money;
+	end
+	
+	--replace trade with trade_return
+	trade.item1 = trade_return.item1;
+	trade.item2 = trade_return.item2;
+	trade.item3 = trade_return.item3;
+	trade.item4 = trade_return.item4;
+	trade.item5 = trade_return.item5;
+	trade.item6 = trade_return.item6;
+	trade.item7 = trade_return.item7;
+	trade.item8 = trade_return.item8;
+	
+	trade.platinum = math.floor(return_money / 1000);
+	return_money = return_money - (trade.platinum * 1000);
+	
+	trade.gold = math.floor(return_money / 100);
+	return_money = return_money - (trade.gold * 100);
+	
+	trade.silver = math.floor(return_money / 10);
+	return_money = return_money - (trade.silver * 10);
+	
+	trade.copper = return_money;
+	return true;
+end
+
+function items.get_handin_items_serialized(handin_table)
+	return table.concat(handin_table, ",")
+end
+	
+return items;
+>>>>>>> Stashed changes
--- plugins/check_handin.pl
+++ plugins/check_handin.pl
@@ -1,72 +1,81 @@
 # plugin::check_handin($item1 => #required_amount,...);
 # autoreturns extra unused items on success
 sub check_handin {
-    my $hashref = shift;
-    my %required = @_;
-    foreach my $req (keys %required) {
-	if ((!defined $hashref->{$req}) || ($hashref->{$req} != $required{$req})) {
-            return(0);
+	my $client = plugin::val('client');
+	my $copper = plugin::val('copper');
+	my $silver = plugin::val('silver');
+	my $gold = plugin::val('gold');
+	my $platinum = plugin::val('platinum');
+	my $hashref = shift;
+
+	$client->SetEntityVariable("HANDIN_MONEY", "$copper-$silver-$gold-$platinum");
+	$client->SetEntityVariable("HANDIN_ITEMS", plugin::GetHandinItemsSerialized("Handin", %$hashref));
+
+	my %required = @_;
+	foreach my $req (keys %required) {
+		if (!defined $hashref->{$req} || $hashref->{$req} != $required{$req}) {
+			return 0;
+		}
 	}
-    }
-     foreach my $req (keys %required) {
-         if ($required{$req} < $hashref->{$req}) {
-             $hashref->{$req} -= $required{$req};
-         } else {
-             delete $hashref->{$req};
-         }
-     }
-     return 1;
+
+	foreach my $req (keys %required) {
+		if ($required{$req} < $hashref->{$req}) {
+			$hashref->{$req} -= $required{$req};
+		} else {
+			delete $hashref->{$req};
+		}
+	}
+
+	return 1;
 }
 
-sub return_items {    
+sub return_items {
 	my $hashref = plugin::var('$itemcount');
 	my $client = plugin::val('$client');
 	my $name = plugin::val('$name');
 	my $items_returned = 0;
 
-	my %ItemHash = (
-		0 => [ plugin::val('$item1'), plugin::val('$item1_charges'), plugin::val('$item1_attuned'), plugin::val('$item1_inst') ],
-		1 => [ plugin::val('$item2'), plugin::val('$item2_charges'), plugin::val('$item2_attuned'), plugin::val('$item2_inst') ],
-		2 => [ plugin::val('$item3'), plugin::val('$item3_charges'), plugin::val('$item3_attuned'), plugin::val('$item3_inst') ],
-		3 => [ plugin::val('$item4'), plugin::val('$item4_charges'), plugin::val('$item4_attuned'), plugin::val('$item4_inst') ],
+	my %item_data = (
+		0 => [ plugin::val('$item1'), plugin::val('$item1_charges'), plugin::val('$item1_attuned') ],
+		1 => [ plugin::val('$item2'), plugin::val('$item2_charges'), plugin::val('$item2_attuned') ],
+		2 => [ plugin::val('$item3'), plugin::val('$item3_charges'), plugin::val('$item3_attuned') ],
+		3 => [ plugin::val('$item4'), plugin::val('$item4_charges'), plugin::val('$item4_attuned') ],
 	);
 
-	foreach my $k (keys(%{$hashref}))
-	{
-		next if($k == 0);
+	my %return_data = ();	
+
+	foreach my $k (keys(%{$hashref})) {
+		next if ($k == 0);
 		my $rcount = $hashref->{$k};
 		my $r;
-		for ($r = 0; $r < 4; $r++)
-		{
-			if ($rcount > 0 && $ItemHash{$r}[0] && $ItemHash{$r}[0] == $k)
-			{
-				if ($client)
-				{
-					# remove delivered task items from return for this slot
-					my $inst = $ItemHash{$r}[3];
-					my $return_count = $inst->RemoveTaskDeliveredItems();
-
-					if ($return_count > 0)
-					{
-						$client->SummonItem($k, $inst->GetCharges(), $ItemHash{$r}[2]);
-						quest::say("I have no need for this $name, you can have it back.");
-						$items_returned = 1;
-					}
-				}
-				else
-				{
-					# This shouldn't be needed, but just in case
+		for ($r = 0; $r < 4; $r++) {
+			if ($rcount > 0 && $item_data{$r}[0] && $item_data{$r}[0] == $k) {
+				if ($client) {
+					$return_data{$r} = [$k, $item_data{$r}[1], $item_data{$r}[2]];
+					$client->SummonItem($k, $item_data{$r}[1], $item_data{$r}[2]);
+					$items_returned = 1;
+				} else {
+					$return_data{$r} = [$k, $item_data{$r}[1], $item_data{$r}[2]];
 					quest::summonitem($k, 0);
 					$items_returned = 1;
 				}
 				$rcount--;
 			}
 		}
+
 		delete $hashref->{$k};
 	}
+
+	$client->SetEntityVariable("RETURN_ITEMS", plugin::GetHandinItemsSerialized("Return", %return_data));
+
+	if ($items_returned) {
+		quest::say("I have no need for this $name, you can have it back.");
+	}
+
+	quest::send_player_handin_event();
+
 	# Return true if items were returned
 	return $items_returned;
-
 }
 
 sub return_bot_items {
@@ -81,8 +90,8 @@ sub return_bot_items {
 	my $items_returned = 0;
 
 	my %item_data = (
-		1 => [ plugin::val('item2'), plugin::val('item2_charges'), plugin::val('item2_attuned'), plugin::val('item2_inst') ],
 		0 => [ plugin::val('item1'), plugin::val('item1_charges'), plugin::val('item1_attuned'), plugin::val('item1_inst') ],
+		1 => [ plugin::val('item2'), plugin::val('item2_charges'), plugin::val('item2_attuned'), plugin::val('item2_inst') ],
 		2 => [ plugin::val('item3'), plugin::val('item3_charges'), plugin::val('item3_attuned'), plugin::val('item3_inst') ],
 		3 => [ plugin::val('item4'), plugin::val('item4_charges'), plugin::val('item4_attuned'), plugin::val('item4_inst') ],
 		4 => [ plugin::val('item5'), plugin::val('item5_charges'), plugin::val('item5_attuned'), plugin::val('item5_inst') ],
@@ -124,101 +133,20 @@ sub return_bot_items {
 	return $items_returned;
 }
 
-sub mq_process_items {
-	my $hashref = shift;
-	my $npc = plugin::val('$npc');
-	my $trade = undef;
-	
-	if($npc->EntityVariableExists("_mq_trade")) {
-		$trade = decode_eqemu_item_hash($npc->GetEntityVariable("_mq_trade")); 
-	} else {
-		$trade = {};
-	}
-	
-	foreach my $k (keys(%{$hashref})) {
-		next if($k == 0);
-		
-		if(defined $trade->{$k}) {
-			$trade->{$k} = $trade->{$k} + $hashref->{$k};
-		} else {
-			$trade->{$k} = $hashref->{$k};
-		}
-	}
-	
-	my $str = encode_eqemu_item_hash($trade);
-	$npc->SetEntityVariable("_mq_trade", $str);
-}
+sub GetHandinItemsSerialized {
+	my $type = shift;
+    my %hash = @_;
+    my @variables = ();
 
-sub check_mq_handin {
-	my %required = @_;
-	my $npc = plugin::val('$npc');
-	my $trade = undef;
-	
-	if($npc->EntityVariableExists("_mq_trade")) {
-		$trade = decode_eqemu_item_hash($npc->GetEntityVariable("_mq_trade"));
-	} else {
-		return 0;
-	}
-	
-	foreach my $req (keys %required) {
-		if((!defined $trade->{$req}) || ($trade->{$req} < $required{$req})) {
-				return 0;
-		}
-	}
-	
-	foreach my $req (keys %required) {
-		if ($required{$req} < $trade->{$req}) {
-			$trade->{$req} -= $required{$req};
-		} else {
-			delete $trade->{$req};
-		}
-	}
-	
-	$npc->SetEntityVariable("_mq_trade", encode_eqemu_item_hash($trade));
-	return 1;
-}
+    if ($type eq "Handin") {
+        foreach my $key (keys %hash) {
+            push(@variables, $key . "|" . $hash{$key} . "|0");
+        }
+    } else {
+        foreach my $key (keys %hash) {
+            push(@variables, $hash{$key}[0] . "|" . $hash{$key}[1] . "|" . $hash{$key}[2]);
+        }
+    }
 
-sub clear_mq_handin {
-	my $npc = plugin::val('$npc');
-	$npc->SetEntityVariable("_mq_trade", "");
-}
-
-sub encode_eqemu_item_hash {
-	my $hashref = shift;
-	my $str = "";
-	my $i = 0;
-	
-	foreach my $k (keys(%{$hashref})) {
-		if($i != 0) {
-			$str .= ",";
-		} else {
-			$i = 1;
-		}
-		
-		$str .= $k;
-		$str .= "=";
-		$str .= $hashref->{$k};
-	}
-	
-	return $str;
-}
-
-sub decode_eqemu_item_hash {
-	my $str = shift;
-	my $hashref = { };
-	
-	my @vals = split(/,/, $str);
-	my $val_len = @vals;
-	for(my $i = 0; $i < $val_len; $i++) {
-		my @subval = split(/=/, $vals[$i]);
-		my $subval_len = @subval;
-		if($subval_len == 2) {
-			my $key = $subval[0];
-			my $value = $subval[1];
-			
-			$hashref->{$key} = $value;
-		}
-	}
-	
-	return $hashref;
+    return join(",", @variables);
 }

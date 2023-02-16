29e946d840ff67c33f67de8c1d9053c78063a6cb
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-15 23:29:23 -0600

    test

--- nexus/Grand_Arcanist_Eithan.pl
+++ nexus/Grand_Arcanist_Eithan.pl
@@ -11,16 +11,14 @@ sub POPUP_DISPLAY {
 
   my $yellow = plugin::PWColor("Yellow");
 
-  my $discord = "Server Discord: " . plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA");
-  my $header = $yellow . plugin::PWAutoCenter("Welcome to Pyrelight!") . "</c><br>";
+  my $discord = "Server Discord: " . plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA") . "<br>";
+  my $header = $yellow . plugin::PWAutoCenter("Welcome to Pyrelight!") . "</c><br><br>";
 
   my $desc = "Pyrelight is a solo-balanced server, meant to offer a challenging experience for veteran players and an alternative take on the 'solo progression' mold.";
 
 
   my $text = $header .
-             "<br>" .
              $discord .
-             "<br>" . 
              $desc;  
   quest::popup('', $text);
 }

ca272ac586438d3b88f0c824f506210c44972de2
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-15 23:22:57 -0600

    test

--- nexus/Grand_Arcanist_Eithan.pl
+++ nexus/Grand_Arcanist_Eithan.pl
@@ -2,20 +2,8 @@ sub EVENT_SAY {
   POPUP_DISPLAY();
   my $charKey = $client->CharacterID() . "-MAO-Progress";
   my $progress = quest::get_data($charKey);
-  if ($text=~/hail/i) {  
-    
-    my $acctMoneyFlagKey = $client->AccountID() . "-InitMoneyFlag";
-    my $acctMoneyFlagValue = quest::get_data($acctMoneyFlagKey);
-    if ($acctMoneyFlagValue <= 10) {
-      plugin::NPCTell("Also, take these coins. You'll need them to get started, and you can pay me back sometime if your pride demands it.");
-      quest::givecash(0, 0, 5, 1);
-      quest::set_data($acctMoneyFlagKey, ++$acctMoneyFlagValue);
-    }
-    
-    quest::set_data($charKey, "1");
-    quest::message(15,"You have gained the ability to use the Nexus teleportation network.");
-    quest::ding();
-    quest::exp(1500);    
+  if ($text=~/hail/i) {     
+      
   }
 }
 
@@ -27,5 +15,5 @@ sub POPUP_DISPLAY {
   my $header = $yellow . plugin::PWAutoCenter("Welcome to Pyrelight!") . "</c>>br>" .
                plugin::PWAutoCenter("Discord: " . plugin::PWHyperLink("https://discord.com/invite/5cFCA7TVgA","5cFCA7TVgA"));
     
-  quest::popup('', $text);
+  quest::popup('', $header);
 }

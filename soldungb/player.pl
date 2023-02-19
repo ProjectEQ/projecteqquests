9d9a19f03d45ab3f2815eadd057b3697d389fa34
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-02-19 11:14:07 -0500

    Update player.pl

--- soldungb/player.pl
+++ soldungb/player.pl
@@ -1,6 +1,11 @@
 sub EVENT_TARGET_CHANGE {
-  if (($client->GetTarget()->GetCleanName() eq "Lord Nagafen") && ($ulevel > 52) && ($status < 80)) {
-    quest::ze(0,"I will not fight you, but I will banish you!");
-    $client->MovePC(27,-64,262,-93.96,0); # Zone: lavastorm
-  }
+	if (
+		$client->GetTarget() &&
+		$client->GetTarget()->GetCleanName() eq "Lord Nagafen" &&
+		$ulevel > 52 &&
+		$status < 80
+	) {
+		quest::ze(0, "I will not fight you, but I will banish you!");
+		$client->MovePCInstance(27, $instanceid, -64, 262, -93.96, 0); # Zone: lavastorm
+	}
 }

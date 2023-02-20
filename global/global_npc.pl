27b30e70ec3b93e8639a3d13b12fe455ed9e01db
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 17:20:29 -0600

    ???

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,12 +1,6 @@
 sub EVENT_DEATH {
 
     # Global Custom Loot For Non-Greys
-    my @hate_list = $npc->GetHateListClients();
-    my $hate_count = @hate_list;
-    if ($hate_count > 0) {
-        foreach $ent (@hate_list) {
-            quest::debug($ent->GetCleanName() . " killed a " . $npc->GetCleanName());
-        }
-    }
+    quest::debug($npc->$GetHateDamageTop->GetCleanName() . " killed a " . $npc->GetCleanName());
     
 }

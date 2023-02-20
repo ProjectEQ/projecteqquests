f0d5082d6047f6c81c595a9fbd2c68cd4f3419fa
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 17:12:24 -0600

    lmao

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,7 +1,7 @@
 sub EVENT_DEATH_COMPLETE {
 
     # Global Custom Loot For Non-Greys
-    my @hate_list = $mob->GetHasteListClients();
+    my @hate_list = $mob->GetHateListClients();
     my $hate_count = @hate_list;
     if ($hate_count > 0) {
         foreach $ent (@hate_list) {

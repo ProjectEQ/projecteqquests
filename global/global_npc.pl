bc60a8df2b6887b984d991a87aaa944c8748733e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 17:17:46 -0600

    wrong syntax?

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,11 +1,11 @@
 sub EVENT_DEATH {
 
     # Global Custom Loot For Non-Greys
-    my @hate_list = $mob->GetHateListClients();
+    my @hate_list = $npc->GetHateListClients();
     my $hate_count = @hate_list;
     if ($hate_count > 0) {
         foreach $ent (@hate_list) {
-            quest::debug($ent->GetCleanName() . " killed a " . $mob->GetCleanName());
+            quest::debug($ent->GetCleanName() . " killed a " . $npc->GetCleanName());
         }
     }
     

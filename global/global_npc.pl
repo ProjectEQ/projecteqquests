052347d888d325488d9153af33ab88845d476203
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 17:48:24 -0600

    COMPLETE

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,12 +1,19 @@
-sub EVENT_DEATH {
+sub EVENT_DEATH_COMPLETE {
     my @hate_list = $npc->GetHateListClients();
     my $hate_count = @hate_list;
+    my $max_level = 0;
 
     if ($hate_count > 0) {
         foreach $ent (@hate_list) {
             quest::debug($npc->GetCleanName() . " was killed by " . $ent->GetEnt()->CastToClient()->GetCleanName());
+            my $level = $ent->GetEnt()->CastToClient()->GetLevel();
+            if ($level > $max_level) {
+                $max_level = $level
+            }
         }
     } else {
         quest::debug("No Hate List");
     }
+
+
 }

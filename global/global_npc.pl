d9379935547822c9b73e087954defccafe90cbd6
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 18:00:03 -0600

    test

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,19 +1,26 @@
 sub EVENT_DEATH_COMPLETE {
-    my @hate_list = $npc->GetHateListClients();
-    my $hate_count = @hate_list;
-    my $max_level = 0;
+   
 
-    if ($hate_count > 0) {
-        foreach $ent (@hate_list) {
-            quest::debug($npc->GetCleanName() . " was killed by " . $ent->GetEnt()->CastToClient()->GetCleanName());
-            my $level = $ent->GetEnt()->CastToClient()->GetLevel();
-            if ($level > $max_level) {
-                $max_level = $level
+   my $killer = $entity_list->GetMobByID($killer_id);
+
+   if ($killer->IsClient() or $kill->IsBot()) {
+        my $level = $killer->GetLevel();
+        if ($killer->GetGroup()) {
+            for (my $count = 0; $count < $killer->GetGroup()->GroupCount(); $count++) {                
+                if ($killer->GetGroup()->GetMember($count)->GetLevel() > $level) {
+                    $level = $killer->GetGroup()->GetMember($count)->GetLevel();
+                }
             }
         }
-    } else {
-        quest::debug("No Hate List");
-    }
+   } elsif ($killer->IsPet() and ($killer->GetOwner()->IsClient() or $killer->GetOwner()->IsBot()) {
+        if ($killer->GetOwner()->GetGroup()) {
+            for (my $count = 0; $count < $killer->GetOwner()->GetGroup()->GroupCount(); $count++) {
+                if ($killer->GetOwner()->GetGroup()->GetMember($count)->GetLevel() > $level) {
+                    $level = $killer->GetOwner()->GetGroup()->GetMember($count)->GetLevel();
+                }
+            }
+        }
+   }
 
-
+    quest::debug("My Highest Level Killer Was: " . $level);
 }

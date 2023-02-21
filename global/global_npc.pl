8f54430ccc0a7c5f8e6dd419896ddef760e8f182
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 18:03:57 -0600

    test

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,26 +1,7 @@
-sub EVENT_DEATH_COMPLETE {
-   
-
+sub EVENT_DEATH_COMPLETE { 
    my $killer = $entity_list->GetMobByID($killer_id);
 
-   if ($killer->IsClient() or $kill->IsBot()) {
-        my $level = $killer->GetLevel();
-        if ($killer->GetGroup()) {
-            for (my $count = 0; $count < $killer->GetGroup()->GroupCount(); $count++) {                
-                if ($killer->GetGroup()->GetMember($count)->GetLevel() > $level) {
-                    $level = $killer->GetGroup()->GetMember($count)->GetLevel();
-                }
-            }
-        }
-   } elsif ($killer->IsPet() and ($killer->GetOwner()->IsClient() or $killer->GetOwner()->IsBot())) {
-        if ($killer->GetOwner()->GetGroup()) {
-            for (my $count = 0; $count < $killer->GetOwner()->GetGroup()->GroupCount(); $count++) {
-                if ($killer->GetOwner()->GetGroup()->GetMember($count)->GetLevel() > $level) {
-                    $level = $killer->GetOwner()->GetGroup()->GetMember($count)->GetLevel();
-                }
-            }
-        }
-   }
+   quest::debug("I was killed by " . $killer->GetCleanName());
 
-    quest::debug("My Highest Level Killer Was: " . $level);
 }
+   

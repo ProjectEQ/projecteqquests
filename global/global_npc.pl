01520ae6ee3788e4d94afec92b0d8a6fbbb9a841
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-20 18:32:44 -0600

    test

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,10 +1,14 @@
 sub EVENT_DEATH_COMPLETE { 
-   my $killer = $entity_list->GetMobByID($killer_id);   
+   my $killer = $entity_list->GetMobByID($killer_id);
+
+   if ($killer->IsPet()) {
+        $killer = $killer->GetOwner();
+   }
 
    if ($killer->IsClient() or $killer->IsBot()) {
-        quest::debug("I was killed by a client or bot");
-   } elsif ($killer->IsPet() and ($killer->GetOwner()->IsBot() or $killer->GetOwner()->IsClient())) {
-        quest::debug("I was killed by the pet of a client or bot");
+        my $level = $killer->GetLevel();
+        quest::debug("My killer's level is: " . $level);
    }
+
 }
    

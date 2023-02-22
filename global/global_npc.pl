d82290a632b88cb7db52a4b86a59d5b8900ae1dd
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-22 08:02:33 -0800

    Update global_npc.pl

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,34 +1,3 @@
-sub EVENT_DEATH_COMPLETE { 
-   my $killer = $entity_list->GetMobByID($killer_id);
-
-   if ($killer->IsPet()) {
-        $killer = $killer->GetOwner();
-   }
-
-   if ($killer->IsClient() or $killer->IsBot()) {
-        my $group = $killer->CastToClient()->GetGroup();
-        my $level = $killer->GetLevel();
-        if ($group) {
-            for (my $count = 0; $count < $group->GroupCount(); $count++) {
-                if ($group->GetMember($count).GetLevel() > $level) {
-                    $level = $group->GetMember($count).GetLevel();
-                    $killer = $group->GetMember($count)->CastToClient();
-                }
-            }        
-        }
-
-        # The mob was not grey
-        if ($killer->GetLevelCon($npc->GetLevel()) != 6) {
-            quest::debug("I was not grey to my killer");
-        }
-   }
+sub EVENT_ATTACK {
+    quest::debug("EVENT_ATTACK happened: " . $npc->GetCleanName());
 }
-   
-#define CON_GREEN		2
-#define CON_LIGHTBLUE	18
-#define CON_BLUE		4
-#define CON_WHITE		10
-#define CON_WHITE_TITANIUM		20
-#define CON_YELLOW		15
-#define CON_RED			13
-#define CON_GRAY		6

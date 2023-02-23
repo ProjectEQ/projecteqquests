42ae356bea8d5c93d5f8b03472d35288d462b38b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-23 14:21:33 -0800

    lowered plat cost

--- nexus/Arcanist_Evanora.pl
+++ nexus/Arcanist_Evanora.pl
@@ -10,7 +10,7 @@ sub EVENT_SAY {
     # This will need to be reworked for POP eventually.
     plugin::NPCTell("Hail, ". $client->GetCleanName() .". I can assist you in operating the main teleportation spires in order to [".quest::saylink("ae1",1,"reach other planes")."]. How can I help you?");
   } elsif ($text=~/ae1/i) {
-    plugin::NPCTell("I'm capable of sending you to either [".quest::saylink("ae_hateplaneb",1,"The Plane of Hate")."] or [".quest::saylink("ae_airplane",1,"The Plane of Sky")."]. If you've already visited them using their permanent portals elsewhere in Norrath, I can also transport you to [".quest::saylink("ae_mischiefplane",1,"The Plane of Mischief")."], [".quest::saylink("ae_growthplane",1,"The Plane of Growth")."], or [".quest::saylink("ae_fearplane",1,"The Plane of Fear")."]. This transportation is not free, though; I require a fee of 5,000 platinum pieces for my time.");
+    plugin::NPCTell("I'm capable of sending you to either [".quest::saylink("ae_hateplaneb",1,"The Plane of Hate")."] or [".quest::saylink("ae_airplane",1,"The Plane of Sky")."]. If you've already visited them using their permanent portals elsewhere in Norrath, I can also transport you to [".quest::saylink("ae_mischiefplane",1,"The Plane of Mischief")."], [".quest::saylink("ae_growthplane",1,"The Plane of Growth")."], or [".quest::saylink("ae_fearplane",1,"The Plane of Fear")."]. This transportation is not free, though; I require a fee of 500 platinum pieces for my time.");
   } elsif ($text=~/ae_hateplaneb/i) {
     plugin::NPCTell("Are you sure that you wish to be transported to [".quest::saylink("ae_transport",1,"The Plane of Hate")."]?");
     $client->SetBucket("evanora_target", "hateplaneb", 60);
@@ -27,7 +27,7 @@ sub EVENT_SAY {
     plugin::NPCTell("Are you sure that you wish to be transported to [".quest::saylink("ae_transport",1,"The Plane of Mischief")."]?");
     $client->SetBucket("evanora_target", "mischiefplane", 60);
   } elsif ($text=~/ae_transport/i) { 
-    if ($client->GetGM() or $client->TakeMoneyFromPP(5000000, 1)) {
+    if ($client->GetGM() or $client->TakeMoneyFromPP(50000, 1)) {
         my $target = $client->GetBucket("evanora_target");
         if ($target eq 'hateplaneb') {
             $client->MovePC(186, -393, 656, 4, 0);
@@ -53,7 +53,7 @@ sub EVENT_SAY {
             }
         }
     } else {
-        plugin::NPCTell("I'm sorry. You don't have enough money to pay my fee. Please return when you have 5,000 platinum pieces");
+        plugin::NPCTell("I'm sorry. You don't have enough money to pay my fee. Please return when you have 500 platinum pieces");
     }
   }
 }
--- soldungb/Lord_Nagafen.pl
+++ soldungb/Lord_Nagafen.pl
@@ -1,5 +1,4 @@
 sub EVENT_KILLED_MERIT {
-	quest::debug("Test:" . $client->GetCleanName());
 }
 
 sub EVENT_SPAWN {

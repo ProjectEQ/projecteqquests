89edf1df672e2f3f36124295e6a4c217cec49749
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-23 09:57:42 -0800

    Update global_npc.pl

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -6,9 +6,8 @@ sub EVENT_DAMAGE_GIVEN {
 
 sub EVENT_SPAWN
 {
-    if ($npc->IsPet()) {
-        quest::debug("I'm a pet, I spawned: " . $npc->GetCleanName());
-        $npc->ModifyNPCStat("max_hp", $npc->GetMaxHP() * 2);
+    if ($npc->IsPet()) {  
+        EVAL_PET($npc);
     }
 }
 
@@ -28,21 +27,10 @@ sub EVAL_PET
                 my $owner_spelldmg = $owner->GetSpellDmg();
                 my $owner_healamt = $owner->GetHealAmount();
 
-                $pet->ModifyNPCStat("max_hp", $owner_level * 100 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("ac", $owner_level * 3 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("atk", $owner_level * 1 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("max_hit", $owner_level * 2 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("min_hit", $owner_level * 0.5 * ($owner_spelldmg));
-
-                $pet->ModifyNPCStat("fr", $owner->GetFR()/2 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("cr", $owner->GetCR()/2 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("dr", $owner->GetDR()/2 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("pr", $owner->GetPR()/2 * ($owner_spelldmg));
-                $pet->ModifyNPCStat("mr", $owner->GetMR()/2 * ($owner_spelldmg));
-
                 $pet->ModifyNPCStat("spellscale",$owner->GetSpellDmg());
                 $pet->ModifyNPCStat("healscale",$owner->GetHealAmt());
-                $pet->ModifyNPCStat("runspeed", 2.5);
+
+                $pet->ModifyNPCStat("max_hp", $pet->GetMaxHP() * $owner_spelldmg);
 
                 $pet->SetMaxHP();
         }

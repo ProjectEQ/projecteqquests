7b2499e850b2d07ae33bf6e841458a5dd86f574d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-22 19:40:08 -0800

    wtf

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -6,7 +6,7 @@ sub EVENT_DAMAGE_GIVEN {
 
 sub EVENT_DAMAGE_TAKEN {
     quest::debug("My Race is: " . $npc->GetRace(). " My Name is: " . $npc->GetCleanName());
-    if ($npc->GetRace() == 356) {
+    if ($npc->GetRace() == 42) {
         quest::debug("Attempting to reset Scaled Wolf Size: " . $npc->GetSize() . ":" . $npc->GetBaseSize());
         $npc->ChangeSize($npc->GetBaseSize(), 1);
     }

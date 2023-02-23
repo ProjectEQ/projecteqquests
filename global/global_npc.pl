efad52eb5c621b22a06c8eccdc739f642fece8ac
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-22 19:07:06 -0800

    this is dumb

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -3,3 +3,10 @@ sub EVENT_DAMAGE_GIVEN {
         $entity_list->GetMobByID($entity_id)->AddToHateList($npc->GetOwner());
     }        
 }
+
+sub EVENT_DAMAGE_TAKEN {
+    if ($npc->GetRace() == 356) {
+        $npc->ChangeSize($npc->GetSize());
+        quest::debug("Attempting to reset Scaled Wolf Size");
+    }
+}

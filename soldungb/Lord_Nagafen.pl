deaa48e3cdb145037605efab72f6886d50940711
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-23 16:10:22 -0800

    test

--- soldungb/Lord_Nagafen.pl
+++ soldungb/Lord_Nagafen.pl
@@ -1,5 +1,12 @@
 sub EVENT_KILLED_MERIT {
-}
-
-sub EVENT_SPAWN {
+    if ($instanceversion = 1 and $instanceid) {
+        if ($client->GetBucket("FOS-Nagafen")) {
+            $client->Message(15,"You have completed this Feat of Strength! You have already been awarded for this achivement.");
+        } else {
+            $client->SetBucket("FOS-Nagafen", 1);
+            $client->Message(15,"You have completed this Feat of Strength! You have been awarded with five Alternate Advancement Points");
+            $client->AddAAPoints(5);
+            quest::worldwidemessage(15, "The brave adventurer " . $client->GetCleanName() . " has completed the Feat of Strength - Slay Lord Nagafen.");
+        }
+    }
 }

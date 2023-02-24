493a2df8821dabf505c646d22b09623fe581fad3
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-02-23 16:53:53 -0800

    kedge scripts

---
+++ cauldron/Phinigel_Autropos.pl
@@ -0,0 +1,12 @@
+sub EVENT_KILLED_MERIT {
+    if ($instanceversion = 1 and $instanceid) {
+        if ($client->GetBucket("FOS-Phinigel")) {
+            $client->Message(15,"You have completed this Feat of Strength! You have already been awarded for this achivement.");
+        } else {
+            $client->SetBucket("FOS-Phinigel", 1);
+            $client->Message(15,"You have completed this Feat of Strength!");
+            $client->AddAAPoints(5);
+            quest::worldwidemessage(15, "The brave adventurer " . $client->GetCleanName() . " has completed the Feat of Strength - Slay Phinigel Autropos.");
+        }
+    }
+}

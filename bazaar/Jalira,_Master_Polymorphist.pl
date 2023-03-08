7a88a35ae6f1d7d6b510788e32d17c8dc116ffe4
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-03-08 02:18:27 -0600

    stuff

--- bazaar/Jalira,_Master_Polymorphist.pl
+++ bazaar/Jalira,_Master_Polymorphist.pl
@@ -1,8 +1,8 @@
 
 sub EVENT_SAY {
     if ($text=~/Hail/i) {
-        plugin::Whisper("A test subject! What would you like to " . quest::saylink("become", 1) . " ?");
-    } elsif ($text=~/List/i) {
+        plugin::Whisper("A test subject! What would you like to " . quest::saylink("become", 1) . "?");
+    } elsif ($text=~/Become/i) {
         plugin::RaceChanger("List");
     } else {
         plugin::RaceChanger($text);

a1dc52c2d5220b5e3cbfcea05786c34d5ccebdbb
Natan <natanx@gmail.com>
2023-03-18 00:41:26 -0700

    Updates the Monk epic class check for `Deep` in `lakerathe`
    This step isn't required on live servers / isn't in the guide here https://www.eqprogression.com/monk-1-0-epic-quest/ so I didn't think it was necessary, but, it's required for the PEQ version of the quest.

--- lakerathe/Deep.pl
+++ lakerathe/Deep.pl
@@ -6,7 +6,7 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  if((plugin::check_handin(\%itemcount, 1686 => 1)) && ($ulevel >= 46) && ($class eq "Monk")) { #Trunt's Head
+  if((plugin::check_handin(\%itemcount, 1686 => 1)) && ($ulevel >= 46) && ($class eq "Beastlord")) { #Trunt's Head
     quest::emote("slowly opens her eyes and looks up at you. She stares at you a long while and then closes her eyes and lowers her head again.");
     quest::say("Very well, $name, if you wish death so greatly, we will be happy to oblige. My master projects part of himself in the wilder lands known as the Overthere. He has granted you an audience. Find him and show him the head of our earth brother. At that point, we will discuss how we will end your life.");
     quest::summonitem(1686); #Trunt's Head

3d59d047b6af7cb2959df6f02de5e305b8b13298
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-03-28 14:21:26 -0500

    Did this just work in one go?
    test
    
    test
    
    test
    
    asd
    
    test
    
    test
    
    test
    
    test
    
    asd
    
    kms
    
    test
    
    if this fucking works I want to kms
    
    wtf
    
    test
    
    test
    
    wtf
    
    runspeed
    
    test regen

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,105 +1,212 @@
+use List::Util qw(max);
+use POSIX;
+
 sub EVENT_SPAWN
 {
     if ($npc->IsPet() and $npc->GetOwner()->IsClient()) {  
-        EVAL_PET($npc);
+       SAVE_PET_STATS($npc);
+       UPDATE_PET_STATS($npc);
+       APPLY_EPIC_FOCI($npc);
     }
 }
 
-sub EVENT_DAMAGE_GIVEN {
-    if ($npc->IsPet()) {
+sub EVENT_TICK 
+{
+    if ($npc->IsPet() and $npc->GetOwner()->IsClient()) {  
+        UPDATE_PET_STATS($npc);
+        APPLY_EPIC_FOCI($npc);
+    }
+}
+
+sub EVENT_DAMAGE_GIVEN 
+{
+    if ($npc->IsPet() and $npc->GetOwner()->IsClient() and not $npc->IsTaunting()) {
         $entity_list->GetMobByID($entity_id)->AddToHateList($npc->GetOwner());
     }        
 }
 
-sub EVAL_PET
+sub SAVE_PET_STATS
 {
-        my $pet = shift;
-        if ($pet->GetOwnerID()) {
-                my $owner = $entity_list->GetClientByID($pet->GetOwnerID());
-                
-                my $owner_spelldmg = ($owner->GetSpellDamage() / 100);
-                my $owner_healamt = ($owner->GetHealAmount() / 100);
+    my $pet = shift;
+    my $owner = $pet->GetOwner()->CastToClient();
 
-                $pet->ModifyNPCStat("spellscale",$owner->GetSpellDamage() + 100);
-                $pet->ModifyNPCStat("healscale",$owner->GetHealAmount() + 100);
+    if ($owner) {     
+        $owner->SetBucket("hp_regen", $pet->GetNPCStat("hp_regen"));        
+        $owner->SetBucket("min_hit", $pet->GetNPCStat("min_hit"));
+        $owner->SetBucket("max_hit", $pet->GetNPCStat("max_hit"));
+        $owner->SetBucket("max_hp", $pet->GetNPCStat("max_hp"));
+        $owner->SetBucket("atk", $pet->GetNPCStat("atk"));
+        $owner->SetBucket("str", $pet->GetNPCStat("str"));
+        $owner->SetBucket("sta", $pet->GetNPCStat("sta"));
+        $owner->SetBucket("dex", $pet->GetNPCStat("dex"));
+        $owner->SetBucket("agi", $pet->GetNPCStat("agi"));
+        $owner->SetBucket("ac", $pet->GetNPCStat("ac"));
+        $owner->SetBucket("mr", $pet->GetNPCStat("mr"));
+        $owner->SetBucket("fr", $pet->GetNPCStat("fr"));
+        $owner->SetBucket("cr", $pet->GetNPCStat("cr"));
+        $owner->SetBucket("dr", $pet->GetNPCStat("dr"));
+        $owner->SetBucket("pr", $pet->GetNPCStat("pr"));
 
-                $pet->ModifyNPCStat("max_hp", $pet->GetMaxHP() * ($owner_healamt + 1));
-                $pet->ModifyNPCStat("ac", $pet->GetNPCStat("ac") * ($owner_spelldmg + 1));
-                $pet->ModifyNPCStat("atk", $pet->GetNPCStat("atk") * ($owner_spelldmg + 1));
-                $pet->ModifyNPCStat("hp_regen", $pet->GetNPCStat("hp_regen") * ($owner_healamt + 1));
-                $pet->ModifyNPCStat("min_hit", $pet->GetNPCStat("min_hit") * (($owner_spelldmg / 2) + 1));
-                $pet->ModifyNPCStat("max_hit", $pet->GetNPCStat("max_hit") * (($owner_spelldmg / 2) + 1));
+        # We only arrive here on initial summoning.
+        $owner->DeleteBucket("epic_proc");
+    }
+}
 
-                $pet->ModifyNPCStat("fr", $owner->GetFR());
-                $pet->ModifyNPCStat("cr", $owner->GetCR());
-                $pet->ModifyNPCStat("mr", $owner->GetMR());
-                $pet->ModifyNPCStat("pr", $owner->GetDR());
-                $pet->ModifyNPCStat("dr", $owner->GetPR());
+sub UPDATE_PET_STATS
+{
+    my $pet = shift;
+    my $owner = $pet->GetOwner()->CastToClient();
 
-                $pet->ModifyNPCStat("accuracy", $pet->GetNPCStat("accuracy") * (($owner_spelldmg / 2) + 1));
-                $pet->ModifyNPCStat("atk", $pet->GetNPCStat("atk") * (($owner_spelldmg / 2) + 1));                
+    if ($owner) {
+        # Create Scalar. The max() probably isn't needed but just to be safe
+        my $pet_scalar = max(($owner->GetSpellDamage() / 125) + 1, 1);
 
-                $pet->ModifyNPCStat("avoidance", $pet->GetNPCStat("avoidance") * (($owner_healamt / 2) + 1));
+        # Do max HP adjustment and readjust current hp to original ratio
+        my $hp_ratio = $pet->GetHP() / $pet->GetMaxHP();
+        my $max_hp = ceil($owner->GetBucket("max_hp") * $pet_scalar);
+        my $cur_hp = ceil($max_hp * $hp_ratio);
+        $pet->ModifyNPCStat("max_hp", $max_hp);
+        $pet->SetHP($cur_hp);
 
-                $pet->SetMaxHP();
+        # Set spellscale and healscale for the pet
+        $pet->ModifyNPCStat("spellscale", ($owner->GetSpellDamage()) + 100 . "");
+        $pet->ModifyNPCStat("healscale", ($owner->GetHealAmount()) + 100 . "");
 
-                if ($owner->GetClass() == 13) {
-                    # Handling for Mage Epic Bonus
-                    my @items = ($owner->GetItemIDAt(quest::getinventoryslotid("primary")),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),0),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),1),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),2),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),3),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),4),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),5));
-                    #TODO - Add the id of the 1.0 stat augment here
-                    foreach (@items) {
-                        quest::debug($_);
-                        if ($_ == 28034) {
-                            # TODO: Is 100 the right effect chance value here?
-                            $pet->AddMeleeProc(848, 100);
-                            $pet->ModifyNPCStat("max_hp", $pet->GetMaxHP() + 3000);
-                            $pet->ModifyNPCStat("min_hit", $pet->GetNPCStat("min_hit") * 1.1);
-                            $pet->ModifyNPCStat("max_hit", $pet->GetNPCStat("max_hit") * 1.1);
-                            $pet->SetMaxHP();
+        # Set Resists
+        my $mr = ceil($owner->GetBucket("mr") * $pet_scalar) . "";
+        my $fr = ceil($owner->GetBucket("fr") * $pet_scalar) . "";
+        my $cr = ceil($owner->GetBucket("cr") * $pet_scalar) . "";
+        my $dr = ceil($owner->GetBucket("dr") * $pet_scalar) . "";
+        my $pr = ceil($owner->GetBucket("pr") * $pet_scalar) . "";
 
-                            quest::debug("Applying Mage Epic Pet Buffs");
-                            last;
-                        }
-                    }
-                }
-                elsif ($owner->GetClass() == 15) {
-                    # Handling for Beastlord Epic Bonus
-                    my @items = ($owner->GetItemIDAt(quest::getinventoryslotid("primary")),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),0),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),1),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),2),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),3),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),4),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("primary"),5),
-                                 $owner->GetItemIDAt(quest::getinventoryslotid("secondary")),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("secondary"),0),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("secondary"),1),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("secondary"),2),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("secondary"),3),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("secondary"),4),
-                                 $owner->GetAugmentAt(quest::getinventoryslotid("secondary"),5));
-                    foreach (@items) {
-                        quest::debug($_);
-                        #TODO - Add the ids of the 1.0 stat augment here
-                        if ($_ == 8495 || $_ == 8496) {
-                            # TODO: This is a group heal proc -- is that what we want it to be?
-                            # TODO: Is 30 the right effect chance value here?
-                            $pet->AddMeleeProc(3577, 30);
-                            $pet->ModifyNPCStat("max_hp", $pet->GetMaxHP() + 3000);
-                            $pet->ModifyNPCStat("min_hit", $pet->GetNPCStat("min_hit") * 1.1);
-                            $pet->ModifyNPCStat("max_hit", $pet->GetNPCStat("max_hit") * 1.1);
-                            $pet->SetMaxHP();
+        $pet->ModifyNPCStat("mr", $mr);
+        $pet->ModifyNPCStat("fr", $fr);
+        $pet->ModifyNPCStat("cr", $cr);
+        $pet->ModifyNPCStat("dr", $dr);
+        $pet->ModifyNPCStat("pr", $pr);
 
-                            quest::debug("Applying Beastlord Epic Pet Buffs");
-                            last;
-                        }
-                    }
-                }
+        # Set Primary Stats
+        my $str = ceil($owner->GetBucket("str") * $pet_scalar) . "";
+        my $sta = ceil($owner->GetBucket("sta") * $pet_scalar) . "";
+        my $dex = ceil($owner->GetBucket("dex") * $pet_scalar) . "";
+        my $agi = ceil($owner->GetBucket("agi") * $pet_scalar) . "";
+        my $atk = ceil($owner->GetBucket("atk") * $pet_scalar) . "";
+
+        $pet->ModifyNPCStat("str", $str);
+        $pet->ModifyNPCStat("sta", $sta);
+        $pet->ModifyNPCStat("dex", $dex);
+        $pet->ModifyNPCStat("agi", $agi);
+        $pet->ModifyNPCStat("atk", $atk);
+
+        # Set Min\Max Hits and Regen
+        my $min_hit = ceil($owner->GetBucket("min_hit") * $pet_scalar) . "";
+        my $max_hit = ceil($owner->GetBucket("max_hit") * $pet_scalar) . "";
+        my $hp_regen = ceil($owner->GetBucket("hp_regen") * $pet_scalar) . "";
+
+        $pet->ModifyNPCStat("min_hit", $min_hit);
+        $pet->ModifyNPCStat("max_hit", $max_hit);
+        $pet->ModifyNPCStat("hp_regen", $hp_regen);
+
+        # Set Runspeed
+        my $runspeed = $owner->GetRunspeed() / 17 . "";
+        $pet->ModifyNPCStat("runspeed", $runspeed);
+    }
+}
+
+sub APPLY_EPIC_FOCI 
+{
+    my $pet = shift;
+    my $owner = $pet->GetOwner()->CastToClient();
+
+    #Magician
+    if ($owner->GetClass() == 13) {
+        my $mag_epic_scalar = 1.5;
+        
+        # Look for Epic 1.0\Augment
+        my $epic1 = $owner->CountItemEquippedByID(28034) || $owner->CountAugmentEquippedByID(2028034);
+        if ($epic1 > 0) {
+            # Check if we applied the proc buff already
+            if ($owner->GetBucket("epic_proc") ne "true") {                
+                $owner->SetBucket("epic_proc", "true"); 
+                $pet->AddMeleeProc(849, 115);
+                $pet->AddMeleeProc(848, 25);                           
+            }
+            if (!($pet->FindBuff(847))) {
+                $pet->CastSpell(847, $pet->GetID());
+            }
+
+            my $min_hit = ceil($pet->GetNPCStat("min_hit") * $mag_epic_scalar) . "";
+            my $max_hit = ceil($pet->GetNPCStat("max_hit") * $mag_epic_scalar) . "";
+
+            $pet->ModifyNPCStat("min_hit", $min_hit);
+            $pet->ModifyNPCStat("max_hit", $max_hit);
+
+            # Do max HP adjustment and readjust current hp to original ratio
+            my $hp_ratio = $pet->GetHP() / $pet->GetMaxHP();
+            my $max_hp = ceil($pet->GetNPCStat("max_hp") * $mag_epic_scalar);
+            my $cur_hp = ceil($max_hp * $hp_ratio);
+            $pet->ModifyNPCStat("max_hp", $max_hp);
+            $pet->SetHP($cur_hp);
+        } elsif ($owner->GetBucket("epic_proc") eq "true") {
+            $pet->RemoveMeleeProc(849);
+            $pet->RemoveMeleeProc(848);
+            $pet->BuffFadeBySpellID(847);
         }
+    }
+
+    #Beastlord
+    if ($owner->GetClass() == 13) {
+        my $bst_epic_scalar = 1.25;
+        
+        # Look for Epic 1.0\Augment
+        my $epic1 = ($owner->CountItemEquippedByID(8495) && $owner->CountItemEquippedByID(8496)) || $owner->CountAugmentEquippedByID(208495);
+        if ($epic1 > 0) {
+            # Check if we applied the proc buff already
+            if ($owner->GetBucket("epic_proc") ne "true") {                
+                $owner->SetBucket("epic_proc", "true");
+                # TODO CUSTOM PROC            
+            }
+
+            my $min_hit = ceil($pet->GetNPCStat("min_hit") * $bst_epic_scalar) . "";
+            my $max_hit = ceil($pet->GetNPCStat("max_hit") * $bst_epic_scalar) . "";
+
+            $pet->ModifyNPCStat("min_hit", $min_hit);
+            $pet->ModifyNPCStat("max_hit", $max_hit);
+
+            # Do max HP adjustment and readjust current hp to original ratio
+            my $hp_ratio = $pet->GetHPRatio();
+            my $max_hp = ceil($pet->GetNPCStat("max_hp") * $bst_epic_scalar);
+            my $cur_hp = ceil($max_hp * $hp_ratio);
+            $pet->ModifyNPCStat("max_hp", $max_hp);
+            $pet->SetHP($cur_hp);
+        }
+    }
+
+    #Necromancer
+    if ($owner->GetClass() == 11) {
+        my $nec_epic_scalar = 1.25;
+        
+        # Look for Epic 1.0\Augment
+        my $epic1 = $owner->CountItemEquippedByID(20544) || $owner->CountAugmentEquippedByID(2020544);
+        if ($epic1 > 0) {
+            # Check if we applied the proc buff already
+            if ($owner->GetBucket("epic_proc") ne "true") {                
+                $owner->SetBucket("epic_proc", "true");
+                # TODO CUSTOM PROC            
+            }
+
+            my $min_hit = ceil($pet->GetNPCStat("min_hit") * $nec_epic_scalar) . "";
+            my $max_hit = ceil($pet->GetNPCStat("max_hit") * $nec_epic_scalar) . "";
+
+            $pet->ModifyNPCStat("min_hit", $min_hit);
+            $pet->ModifyNPCStat("max_hit", $max_hit);
+
+            # Do max HP adjustment and readjust current hp to original ratio
+            my $hp_ratio = $pet->GetHPRatio();
+            my $max_hp = ceil($pet->GetNPCStat("max_hp") * $nec_epic_scalar);
+            my $cur_hp = ceil($max_hp * $hp_ratio);
+            $pet->ModifyNPCStat("max_hp", $max_hp);
+            $pet->SetHP($cur_hp);
+        }
+    }
 }

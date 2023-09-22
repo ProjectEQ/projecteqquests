debd9874ce2ab7993035ed7681caca9cbeaa0afc
Trust <fryguy503@gmail.com>
2023-09-22 14:54:54 -0400

    [DoN] Fixing Spawngroup Checks

--- stillmoonb/encounters/drake_eggs.lua
+++ stillmoonb/encounters/drake_eggs.lua
@@ -1,13 +1,13 @@
 local function drake_spawn(e)
   -- the drake egg always drops from same drake at 1004, 2052, 419.75 (spawngroup: 277936)
-  if e.self:GetSp2() == 277936 then -- spawngroup id of drake egg dropper
+  if e.self:GetSp2() == 5277936 then -- spawngroup id of drake egg dropper
     e.self:AddItem(49022, 1) -- Stillmoon Drake Egg
   end
 end
 
 local function drake_death(e)
   -- drake can respawn and re-trigger duplicate spawns
-  if e.self:GetSp2() == 277936 then -- spawngroup id of drake egg dropper
+  if e.self:GetSp2() == 5277936 then -- spawngroup id of drake egg dropper
     eq.spawn2(339114, 0, 0, 169, 1101, 43, 9)   -- a_Goblin_Guard
     eq.spawn2(339297, 0, 0, 191, 1135, 45, 494) -- a_guardian_statue
     eq.spawn2(339297, 0, 0, 138, 1130, 44, 38)  -- a_guardian_statue

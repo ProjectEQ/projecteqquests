53946764bd694e5b3561edfaed41bd638c42dc31
JJ <jeremiah.oligario@gmail.com>
2023-10-03 18:15:44 -0400

    Change Class() to GetClassName() in text.

--- burningwood/Azdalin.lua
+++ burningwood/Azdalin.lua
@@ -4,17 +4,11 @@ function event_combat(e)
 	if(e.joined) then
 		e.self:Say("Foolish adventurer!!  You shall die.  You are nothing to Azdalin. ");
 		if(random_result <=20) then
-			e.self:Say("" .. e.other:Class() .. " like you always bring out the worst in me!");
+			e.self:Say("" .. e.other:GetClassName() .. " like you always bring out the worst in me!");
 		elseif(random_result <=40) then
-			e.self:Say("I really hate " .. e.other:Class() .. " like you!");
+			e.self:Say("I really hate " .. e.other:GetClassName() .. " like you!");
 		else
-			e.self:Say("" .. e.other:Class() .. " like you are an affront to my senses!");
+			e.self:Say("" .. e.other:GetClassName() .. " like you are an affront to my senses!");
 		end
 	end
 end
-
---by: Qadar
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------

634461bf5fc08f82eaa227e26d16e7c41b4b76ff
JJ <jeremiah.oligario@gmail.com>
2023-10-28 15:23:05 -0400

    Some cleanup in velk scripts

--- velketor/Khlem_Alestone.lua
+++ velketor/Khlem_Alestone.lua
@@ -1,11 +1,11 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("I am not the begining.");
+		e.self:Say("I am not the beginning.");
 	end
 end
 
 function event_signal(e)
 	if(e.signal == 1) then
-		e.self:Say("Middle and end.");
+		e.self:Say("middle and end.");
 	end
 end

124a84b1b4bff09e0080f19e35a34299d85adfaa
Trust <fryguy503@gmail.com>
2023-11-27 00:24:35 -0500

    [Bug] Start Timer should be Set Timer

--- wallofslaughter/#Herrian_Warfrost.lua
+++ wallofslaughter/#Herrian_Warfrost.lua
@@ -1,27 +1,28 @@
 function event_spawn(e)
-	eq.set_timer("depop",30*60*1000);
+	eq.set_timer("depop",30 * 60 * 1000);
 end
 
 function event_combat(e)
 	if e.joined then
-		eq.set_timer("freeze", math.random(3,15)*1000);
-		eq.set_timer("hoar",   math.random(3,15)*1000);
+		eq.set_timer("freeze", math.random(3,15) * 1000);
+		eq.set_timer("hoar",   math.random(3,15) * 1000);
 		eq.stop_timer("depop");
 	else
 		eq.stop_timer("freeze");
 		eq.stop_timer("hoar");
-		eq.start_timer("depop",5*60*1000);
+		eq.set_timer("depop",5 * 60 * 1000);
 	end
 end
 
 function event_timer(e)
-	if (e.timer == "freeze") then
+	if e.timer == "freeze" then
 		e.self:CastSpell(5819, e.self:GetTarget():GetID()); -- Spell: Winter's Freeze
-		eq.set_timer("freeze",20*1000);
-	elseif e.timer=="hoar" then
+		eq.set_timer("freeze",20 * 1000);
+	elseif e.timer == "hoar" then
 		e.self:CastSpell(5818, e.self:GetTarget():GetID()); -- Spell: Hoarfrost
-		eq.set_timer("hoar",20*1000);
-	elseif e.timer=="depop" then
+		eq.set_timer("hoar",20 * 1000);
+	elseif e.timer == "depop" then
+		eq.depop_all(300058);
 		eq.depop();
 	end
 end

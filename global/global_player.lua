767a691fa78979a7a84609a02e528211eb8e12a1
Trust <fryguy503@gmail.com>
2023-12-08 10:38:07 -0500

    Adjustment to Wayfarers Message

--- global/global_player.lua
+++ global/global_player.lua
@@ -4,14 +4,14 @@ local don = require("dragons_of_norrath")
 
 function event_enter_zone(e)
 	local qglobals = eq.get_qglobals(e.self);
-	if(e.self:GetLevel() >= 15 and qglobals['Wayfarer'] == nil) then
+	if e.self:GetLevel() >= 15 and qglobals.Wayfarer == nil then
 		local zoneid = eq.get_zone_id();
-		if(e.self:GetStartZone() ~= zoneid and (zoneid == 1 or zoneid == 2 or zoneid == 3 or zoneid == 8 or zoneid == 9 
+		if e.self:GetStartZone() ~= zoneid and (zoneid == 1 or zoneid == 2 or zoneid == 3 or zoneid == 8 or zoneid == 9 
 		or zoneid == 10 or zoneid == 19 or zoneid == 22 or zoneid == 23 or zoneid == 24 or zoneid == 29 or zoneid == 30 
 		or zoneid == 34 or zoneid == 35 or zoneid == 40 or zoneid == 41 or zoneid == 42 or zoneid == 45 or zoneid == 49 
 		or zoneid == 52 or zoneid == 54 or zoneid == 55 or zoneid == 60 or zoneid == 61 or zoneid == 62 or zoneid == 67 
 		or zoneid == 68 or zoneid == 75 or zoneid == 82 or zoneid == 106 or zoneid == 155 or zoneid == 202 or zoneid == 382 
-		or zoneid == 383 or zoneid == 392 or zoneid == 393 or zoneid == 408)) then
+		or zoneid == 383 or zoneid == 392 or zoneid == 393 or zoneid == 408) then
 			e.self:Message(MT.Yellow, 
 				"A mysterious voice whispers to you, \'If you can feel me in your thoughts, know this -- "
 				.. "something is changing in the world and I reckon you should be a part of it. I do not know much, but I do know "

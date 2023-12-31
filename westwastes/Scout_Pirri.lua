f5c8545d59db27deac15696f5d427332935fb7ce
JJ <3617814+joligario@users.noreply.github.com>
2023-12-30 22:37:31 -0500

    Typo

--- westwastes/Scout_Pirri.lua
+++ westwastes/Scout_Pirri.lua
@@ -2,7 +2,7 @@
 	
 function event_say(e)
 	local qglobals = eq.get_qglobals(e.other);
-	if (qglobals ~= nil and qglobals["cleric_epic"] >= "5" and e.message:findi("hail")) then
+	if (qglobals["cleric_epic"] ~= nil and qglobals["cleric_epic"] >= "5" and e.message:findi("hail")) then
 		e.self:Say("You are here to \"help\"? I think not. I will not share my glory with anyone. The only way you will get this is by prying it from the clasped fingers of my dead hand");
 		eq.depop_with_timer();
 		eq.spawn2(120112,0,0,2307,889,-21.875,306); -- NPC: #Scout_Pirri

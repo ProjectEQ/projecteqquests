5f9d587ab819d90050b84e2b0448fd20f4db3284
JJ <3617814+joligario@users.noreply.github.com>
2024-02-19 14:05:43 -0500

    Revert PoK/Baz changes

--- poknowledge/player.pl
+++ poknowledge/player.pl
@@ -20,7 +20,7 @@ sub EVENT_CLICKDOOR {
 	if ($doorid == 139) #bazaar
 	{
 		if ($client->CalculateDistance(1452, 347, -113) <= 30) {
-			quest::MovePCInstance(151, 3, -425, 0, -25, 128); # Zone: bazaar
+			quest::movepc(151, -425, 0, -25, 65); # Zone: bazaar
 		}
 	}
 	if ($doorid == 19) {

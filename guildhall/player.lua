1894901072cb68286cb065362f3c358e84b53301
JJ <jeremiah.oligario@gmail.com>
2024-03-05 17:26:12 -0500

    [Guild Hall] Ensure we catch connect Better to do guild check first

--- guildhall/player.lua
+++ guildhall/player.lua
@@ -1,11 +1,17 @@
+function event_connect(e)
+	-- Ensure we catch all methods of entering zone
+	event_enter_zone(e);
+end
+
 function event_enter_zone(e)
-	-- Expired or invalid instance check
+	-- Boot non-guild characters
+	if e.self:IsInAGuild() == false then
+		e.self:MovePC(344, 19, -55, 5, 0); -- Guild Lobby
+	end
+
+	-- Check if in Guild Hall v0
 	if eq.get_zone_instance_version() == 0 then
-		if e.self:IsInAGuild() then
-			e.self:SendToGuildHall();
-		else
-			e.self:MovePC(344, 19, -55, 5, 0); -- Guild Lobby
-		end
+		e.self:SendToGuildHall();
 	end
 
 	eq.set_timer("zone_in", 3 * 1000);

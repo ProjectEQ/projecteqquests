a1ecdab8e3875e9783052b73e1304abc4c688566
regneq <rregenn@gmail.com>
2024-03-04 17:40:55 -0800

    fixed teleport spell to teleport groups.

--- global/spells/610.lua
+++ global/spells/610.lua
@@ -1,11 +1,14 @@
 -- Succor: Lavastorm
 function event_spell_effect(e)
-  local client = eq.get_entity_list():GetClientByID(e.caster_id);
- 
-  if (client.valid and eq.is_dragons_of_norrath_enabled()) then
-    client:MovePC(27, 460, 460, -86, 454) -- Zone: Lavastorm v1
-  else
-    client:MovePC(27, 1389, 1014, 131, 301) -- Zone: Lavastorm v0
+  local mob = e.target;
+  
+  if (mob.valid and mob:IsClient()) then
+    local client = mob:CastToClient();
+    if (client.valid and eq.is_dragons_of_norrath_enabled()) then
+      client:MovePC(27, 460, 460, -86, 454) -- Zone: Lavastorm v1
+    else
+      client:MovePC(27, 1389, 1014, 131, 301) -- Zone: Lavastorm v0
+    end
   end
 
   return 1;

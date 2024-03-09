4de2626df918b3819922336101067723a3c13e24
JJ <jeremiah.oligario@gmail.com>
2024-03-09 12:25:45 -0500

    Clean up Elefan and Zimloro quests

--- shadeweaver/Elefan.pl
+++ shadeweaver/Elefan.pl
@@ -1,28 +1,26 @@
 # items: 4759, 4758
-sub EVENT_SPAWN {
-  $research = 0;
-}
 
 sub EVENT_SAY { 
-  if($text=~/Hail/i){
+  if ($text=~/hail/i) {
     quest::say("Hello, $name. It's a pleasure to meet you as I do not see many visitors while I am working in the field gathering research.");
   }
-  if ($text=~/research/i && $research == 1) {
-    quest::say("Oh I have some different berries and antidote ingredients that I gathered not too long ago. I would imagine these are what he needs. Will you [take the Antidote ingredients] to Zimloro?");
+  if (($text=~/fresh samples/i) && (quest::get_data("$name-zimloro") == 1)) {
+    quest::say("Oh, some antidote ingredients that I stumbled upon not long ago. They should meet his needs. Will you be so kind as to [" . quest::saylink("take the antidote ingredients") . "] to Zimloro?");
   }
-  if ($text=~/take the antidote ingredients/i && $research == 1) {
+  if (($text=~/take the antidote ingredients/i) && (quest::get_data("$name-zimloro") == 1)) {
     quest::say("Here you are, $name. Please bring them to him at once so they do not spoil and thank you for your help.");
-    $research = 0;
     quest::summonitem(4759); # Item: Antidote Ingredients
+    quest::delete_data("$name-zimloro");
   }
 }
 
 sub EVENT_ITEM { 
-  if (plugin::check_handin(\%itemcount, 4758 => 1)) {
-    quest::say("Ah I see you are sent from master Zimloro eh, $name. This note reads that he needs some specific items at once. Perhaps he could find some use out of all of this research I have recently collected.");
+  if (plugin::check_handin(\%itemcount, 4758 => 1)) { #Item: Zimloro Ingredient List
+    quest::say("Ah! Master Zimloro sent you, eh? This note indicates that he wants some specific ingredients. Luckily, I've got several [" . quest::saylink("fresh samples") . "] that I've only recently collected.");
+    quest::set_data("$name-zimloro", 1);
+    quest::exp(500);
   }
   plugin::return_items(\%itemcount);
 }
 
-#END of FILE Zone:shadeweaver  ID:165178 -- Elefan 
-
+#END of FILE Zone:shadeweaver  ID:165178 -- Elefan

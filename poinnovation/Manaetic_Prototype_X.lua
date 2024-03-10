daca1f8be46f30cc261f805cc8bb2a8769ff733b
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2024-03-10 01:29:29 -0500

    Update Manaetic_Prototype_X.lua

--- poinnovation/Manaetic_Prototype_X.lua
+++ poinnovation/Manaetic_Prototype_X.lua
@@ -8,12 +8,12 @@ end
 
 function event_timer(e)
 	if e.timer == "banish" then
-		local cur_target = e.self:GetHateTop():CastToClient();
+		local cur_target = e.self:GetHateTopClient()
 
 		eq.stop_timer(e.timer);
 		eq.set_timer("banish",math.random(15,30) * 1000);
 
-		if cur_target.valid and not cur_target:IsPet() then
+		if cur_target.valid then
 			cur_target:MovePC(206,269.5,-239.5,3.13,128);
 		end
 	end

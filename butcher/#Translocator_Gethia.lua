ba7f6a4566e086f32367a645d68dc83cd8de2eaf
Xackery <xackery@gmail.com>
2024-03-31 07:01:05 -0700

    Disable Translocator Gethia pre-kunark

--- butcher/#Translocator_Gethia.lua
+++ butcher/#Translocator_Gethia.lua
@@ -1,5 +1,7 @@
 local helper = require('translocators');
 
 function event_say(e)
-    helper.hail_text(e, 'Timorous Deep', {zone=96, x=-3282, y=-4613, z=19, heading=326});
+    if eq.is_the_ruins_of_kunark_enabled() then
+        helper.hail_text(e, 'Timorous Deep', {zone=96, x=-3282, y=-4613, z=19, heading=326});
+    end
 end

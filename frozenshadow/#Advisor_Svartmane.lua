4a7e621c3389f2c26d99c3a1523dc98024d3cbd5
JJ <jeremiah.oligario@gmail.com>
2025-03-08 19:08:44 -0500

    Check global exists for #Advisor_Svartmane (ENC Epic)

--- frozenshadow/#Advisor_Svartmane.lua
+++ frozenshadow/#Advisor_Svartmane.lua
@@ -2,7 +2,7 @@
 function event_trade(e)
 	local item_lib = require("items");
 	local qglobals = eq.get_qglobals(e.other);
-	if(qglobals["ench_epic"] >= "2" and e.other:GetFaction(e.self) < 5) then	
+	if(qglobals["ench_epic"] ~= nil and qglobals["ench_epic"] >= "2" and e.other:GetFaction(e.self) < 5) then	
 		if(item_lib.check_turn_in(e.trade, {item1 = 52964})) then		
 			e.self:Say("I can't . . I . . .of course, I do not think that Tserrina has any need for this. I can't imagine that she would miss it at all. Please, take it.");
 			e.other:SummonItem(54289); -- Item: 1st Piece of the Staff

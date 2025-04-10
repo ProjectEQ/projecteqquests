595989e279aeff408aadccd86bfad6d83c6f173c
JJ <jeremiah.oligario@gmail.com>
2025-04-09 22:11:14 -0400

    Fix combined handin

--- misty/#a_tired_farmer.pl
+++ misty/#a_tired_farmer.pl
@@ -21,7 +21,7 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  if (quest::handin({"platinum" => 300}, {62632 => 1})) { #300pp and Poem of the Storms
+  if (quest::handin({"platinum" => 300, 62632 => 1})) { #300pp and Poem of the Storms
     $client->Message(0,"The farmer looks at you with a stunned expression on his face. He carefully sets the coins down in a neat stack on the ground and slowly unfolds the paper. As he reads the poem, heavy tears form in the corners of his eyes and drop onto the page. When he is done reading he carefully folds it back up and hands the page back to you. He then wipes his nose with his sleeve.");
     quest::spawn2(33159, 0, 0, -1152, -362, 1.19, 0); #a_faithful_farmer
     quest::say("I think I understand a little better about faith. Maybe my faith brought you here, maybe yours did, I don't know. I don't think it matters. You've shown me something about kindness as well. Thank you so much!");

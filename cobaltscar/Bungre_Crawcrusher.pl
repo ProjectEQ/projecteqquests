e611a848e8dc3a31cd313db41a85926bed3c9a4c
JJ <3617814+joligario@users.noreply.github.com>
2025-04-21 21:19:34 -0400

    Update Bungre_Crawcrusher.pl
    Testing order of handin

--- cobaltscar/Bungre_Crawcrusher.pl
+++ cobaltscar/Bungre_Crawcrusher.pl
@@ -17,7 +17,7 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  if (quest::handin({"gold" => 10000, 22815 => 1})) { #Check for 10000 gold
+  if (quest::handin({22815 => 1, "gold" => 10000})) { #Check for Section of Lodizal's Shell and 10000 gold
     quest::emote("skillfully crafts the section of Lodizal's shell into the shape of a shield. He then attaches sturdy cured walrus hide leather straps to the inner side of the shield and inscribes intricate glowing runes on the shield's face. When he is finished, he hands you the shield and claps loudly.");
     quest::summonitem(22816); # Item: Lodizal Shell Shield
     quest::exp(150000);

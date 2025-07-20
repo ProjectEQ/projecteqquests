000cc336ba9158a22f5ba9564dc82bcf59a7b593
JJ <jeremiah.oligario@gmail.com>
2025-07-20 13:36:14 -0400

    Another Elder Murkglider adjustment Force cycle reset after 4 hours

--- draniksscar/#Elder_Murkglider_Devourer.lua
+++ draniksscar/#Elder_Murkglider_Devourer.lua
@@ -1,5 +1,15 @@
 -- he is done evolving
+function event_spawn(e)
+  eq.set_timer('depop', 14400 * 1000); -- depop after 4 hours to reset cycle
+end
+
+function event_timer(e)
+  if (e.timer == 'depop') then
+    eq.update_spawn_timer(43327, 14400 * 1000); -- 4 hours for new cycle
+    eq.depop();
+  end
+end
 
 function event_death_complete(e)
-  eq.update_spawn_timer(43327, 14400 * 1000); -- 4 hours
+  eq.update_spawn_timer(43327, 14400 * 1000); -- 4 hours for new cycle
 end

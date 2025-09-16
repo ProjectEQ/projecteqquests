b4d11ac5fa89a31605e96976ee71eff0111a8d01
JJ <3617814+joligario@users.noreply.github.com>
2025-09-16 14:53:52 -0400

    Update Velious_Scion.lua

--- nexus/Velious_Scion.lua
+++ nexus/Velious_Scion.lua
@@ -1,7 +1,5 @@
 function event_say(e)
 	if e.message:findi("hail") then
-		e.self:Say("Greetings " .. e.other:GetName() .. ". I am here to assist and watch over those who wish to return to the continent of Velious. Due to the limited space I share this area with the portal to Odus. The [teleport system] back to Norrath is completely automated. I have no direct control over when the portal will turn on, although I can say that the system works on a schedule of approximately ten minutes.");
-    elseif e.message:findi("teleport system") then
-		e.self:Say("The teleport system is pretty simple from your perspective. Simply listen for the voice saying that the portals will become active soon. When the time nears, you should stand on the elevated pad you see next to me. Shortly after you see the message that the teleporters are active, you will be transported to your destination..");
+		e.self:Say("Greetings, " .. e.other:GetName() .. ". I am here to assist and watch over those who wish to return to the continent of Velious.  Due to the limited space I share this area with the portal to Odus.  It may be a little confusing but if you listen for the voice it will tell you when to step on the pad for Velious or Odus.  Safe travels to you.");
 	end
 end

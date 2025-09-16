7363ab38d13dc66b7a6ecb854149d3571cb33b95
JJ <3617814+joligario@users.noreply.github.com>
2025-09-16 14:51:02 -0400

    Update Faydwer_Scion.lua

--- nexus/Faydwer_Scion.lua
+++ nexus/Faydwer_Scion.lua
@@ -1,7 +1,5 @@
 function event_say(e)
 	if e.message:findi("hail") then
-		e.self:Say("Greetings " .. e.other:GetName() .. ". I am Jucian Featherhigh, the guardian of this teleport. It will take you back to my home in the Faydark. The [teleport system] back to Norrath is completely automated. I have no direct control over when the portal will turn on, although I can say that the system works on a schedule of approximately ten minutes.");
-    elseif e.message:findi("teleport system") then
-		e.self:Say("The teleport system is pretty simple from your perspective. Simply listen for the voice saying that the portals will become active soon. When the time nears, you should stand on the elevated pad you see next to me. Shortly after you see the message that the teleporters are active, you will be transported to your destination..");
+		e.self:Say("Greetings, " .. e.other:GetName() .. ". I am Jucian Featherhigh, the guardian of this teleport. It will take you back to my home in the Faydark. This pad, along with the others, activate in intervals of around fifteen minutes. When directed to do so, step onto the pad next to me and wait to be teleported.");
 	end
 end

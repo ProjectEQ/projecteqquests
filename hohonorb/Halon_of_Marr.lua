06bcb6d1ea063d88f722e2e99d24ea34f8cd3908
JJ <3617814+joligario@users.noreply.github.com>
2026-03-04 19:31:20 -0500

    Hohb cleanup (#1418)
    * Fix formatting and spacing

--- hohonorb/Halon_of_Marr.lua
+++ hohonorb/Halon_of_Marr.lua
@@ -1,6 +1,6 @@
 function event_death_complete(e)
   if not eq.get_entity_list():IsMobSpawnedByNpcTypeID(220012) and not eq.get_entity_list():IsMobSpawnedByNpcTypeID(220013) and not eq.get_entity_list():IsMobSpawnedByNpcTypeID(220014) and eq.get_entity_list():IsMobSpawnedByNpcTypeID(220016) then
-    eq.unique_spawn(220006,0,0,2727,0,471,387); --Lord_Mithaniel_Marr live version
+    eq.unique_spawn(220006, 0, 0, 2727, 0, 471, 387); --Lord_Mithaniel_Marr live version
     eq.depop_all(220015); --depop fake
   end
 end
